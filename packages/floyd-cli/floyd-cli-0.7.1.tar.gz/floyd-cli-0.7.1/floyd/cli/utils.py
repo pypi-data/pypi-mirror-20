b22d4b03cf2edbcad8ab66696af8a9c3da00d4c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:27 +0000

    Add package floyd-cli/0.7.1/floyd-cli-0.7.1.tar.gz

---
+++ packages/floyd-cli/floyd-cli-0.7.1.tar.gz/floyd-cli-0.7.1/floyd/cli/utils.py
@@ -0,0 +1,36 @@
+import floyd
+from floyd.constants import DOCKER_IMAGES
+
+
+def get_task_url(id):
+    """
+    Return the url to proxy to a running task
+    """
+    return "{}/{}".format(floyd.floyd_proxy_host, id)
+
+
+def get_docker_image(env, gpu):
+    gpu_cpu = "gpu" if gpu else "cpu"
+    return DOCKER_IMAGES.get(gpu_cpu).get(env)
+
+
+def get_module_task_instance_id(task_instances):
+    """
+    Return the first task instance that is a module node.
+    """
+    for id in task_instances:
+        if task_instances[id] == 'module_node':
+            return id
+    return None
+
+
+def get_mode_parameter(mode):
+    """
+    Map the mode parameter to the server parameter
+    """
+    if mode == 'job':
+        return 'cli'
+    elif mode == 'serve':
+        return 'serving'
+    else:
+        return mode
