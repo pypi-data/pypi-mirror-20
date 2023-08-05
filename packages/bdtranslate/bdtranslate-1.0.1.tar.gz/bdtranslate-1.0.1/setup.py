95e5a46a316e35d09dc9a3ef7c610483161ebcaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:15 +0000

    Add package bdtranslate/1.0.1/bdtranslate-1.0.1.tar.gz

---
+++ packages/bdtranslate/bdtranslate-1.0.1.tar.gz/bdtranslate-1.0.1/setup.py
@@ -0,0 +1,17 @@
+
+from distutils.core import setup
+
+setup(
+    name = 'bdtranslate',
+    packages = ['bdtranslate'],
+    version = '1.0.1',
+    description = 'Google translate, baidu translate service wrapper',
+    author = 'RUst Chen',
+    author_email = 'rust.ch3n@gmail.com',
+    url = 'http://ch3n.co',
+    download_url = 'https://pypi.python.org/pypi?%3Aaction=pkg_edit&name=bdtranslate',
+    keywords = ['baidu', 'python', 'translator', 'simple', 'google', 'language'],
+    classifiers = [],
+    entry_points={
+      },
+)
