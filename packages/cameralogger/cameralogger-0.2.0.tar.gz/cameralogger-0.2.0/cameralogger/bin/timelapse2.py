4f3921044b04fbdf246f1417a77762f90f9772b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:31 +0000

    Add package cameralogger/0.2.0/cameralogger-0.2.0.tar.gz

---
+++ packages/cameralogger/cameralogger-0.2.0.tar.gz/cameralogger-0.2.0/cameralogger/bin/timelapse2.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+
+# timelapse - create timelapses from camera images by calling FFmpeg.
+# Copyright (C) 2017  Steve Marple.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import argparse
+import datetime
+from fractions import Fraction
+import logging
+import numpy as np
+import os
+from PIL import Image
+import six
+import subprocess
+import time
+from cameralogger import read_config_file, get_config_option, MovieTasks
+from cameralogger.ffmpeg import FFmpeg
+
+
+__author__ = 'Steve Marple'
+__version__ = '0.1.1'
+__license__ = 'MIT'
+
+
+logger = logging.getLogger(__name__)
+
+
+if __name__ == '__main__':
+    default_config_file = os.path.join(os.path.sep, 'etc', 'camera.ini')
+    parser = \
+        argparse.ArgumentParser(description='')
+
+    parser.add_argument('-c', '--config-file',
+                        default=default_config_file,
+                        help='Configuration file')
+    parser.add_argument('-e', '--end-time',
+                        required=True,
+                        help='End time for time lapse')
+    parser.add_argument('-j', '--jitter',
+                        type=int,
+                        default=0,
+                        help='Accept jitter',
+                        metavar='SECONDS')
+    parser.add_argument('--log-level',
+                        choices=['debug', 'info', 'warning',
+                                 'error', 'critical'],
+                        default='info',
+                        help='Control how much detail is printed',
+                        metavar='LEVEL')
+    parser.add_argument('-o', '--output',
+                        required=True,
+                        help='Output filename')
+    parser.add_argument('-r', '--output-frame-rate',
+                        type=float,
+                        help='Input frame rate',
+                        metavar='FPS')
+    parser.add_argument('--size',
+                        help='Video size (WxH or percentage')
+    parser.add_argument('-s', '--start-time',
+                        required=True,
+                        help='Start time for time lapse')
+    parser.add_argument('--step',
+                        default=1,
+                        type=int,
+                        help='Time step between images',
+                        metavar='SECONDS')
+
+    # Offer multiple methods to set the input frame rate
+    ifr_group = parser.add_mutually_exclusive_group()
+    ifr_group.add_argument('-d', '--duration',
+                           type=float,
+                           help='Set output duration for timelapse',
+                           metavar='SECONDS')
+    ifr_group.add_argument('-i', '--input-frame-rate',
+                           type=float,
+                           help='Input frame rate',
+                           metavar='FPS')
+    ifr_group.add_argument('--speed-up',
+                           type=float,
+                           help='Playback speed as ratio to real time',
+                           metavar='FPS')
+
+    args = parser.parse_args()
+
+    logging.basicConfig(level=getattr(logging, args.log_level.upper()))
+
+    start_time = np.datetime64(args.start_time, 's')
+    end_time = np.datetime64(args.end_time, 's')
+
+    kwargs = {}
+    if args.input_frame_rate is not None:
+        kwargs['ifr'] = args.input_frame_rate
+    elif args.speed_up is not None:
+        kwargs['speed_up'] = args.speed_up
+    else:
+        kwargs['duration'] = args.duration
+
+    config = read_config_file(args.config_file)
+    size = [960, 720]
+    ifr = 60
+    ofr = Fraction(60, 1)
+    ffmpeg = FFmpeg(args.output, size, ifr, ofr)
+
+    schedule_info = {
+        'StartTime': np.datetime64(args.start_time, 's').astype(datetime.datetime),
+        'EndTime': np.datetime64(args.end_time, 's').astype(datetime.datetime),
+        'Step': args.step,
+    }
+
+    tasks = MovieTasks(ffmpeg, config, 'movie', schedule_info)
+    task_list = get_config_option(config, 'movie', 'tasks', default='')
+    tasks.run_tasks(task_list.split())
---
+++ packages/cameralogger/cameralogger-0.2.0.tar.gz/cameralogger-0.2.0/cameralogger/zwo.py
@@ -0,0 +1,132 @@
+# Cameralogger - record and decorate camera images for timelapses etc.
+# Copyright (C) 2017  Steve Marple.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import logging
+from PIL import Image
+import threading
+import time
+import zwoasi
+from cameralogger import get_config_option
+
+
+__author__ = 'Steve Marple'
+__version__ = '0.2.0'
+__license__ = 'MIT'
+
+
+class Camera(object):
+    """ZwO ASI camera.
+
+    Uses the :class:`zwoasi.Camera` class."""
+
+    def __init__(self, config):
+        if zwoasi.zwolib is None:
+            # Must initialise the library
+            if config.has_option('camera', 'sdk'):
+                sdk_filename = config.get('camera', 'sdk')
+            else:
+                # Hope the user has set LD_LIBRARY_PATH or similar..
+                sdk_filename = None
+            zwoasi.init(sdk_filename)
+
+        num_cameras = zwoasi.get_num_cameras()
+        if num_cameras == 0:
+            raise Exception('no camera present')
+
+        if config.has_option('camera', 'model'):
+            id_ = config.get('camera', 'model')
+            try:
+                # Assume it is an integer
+                id_ = int(id_)
+            except ValueError:
+                # No it wasn't, must be the model name then
+                pass
+        else:
+            id_ = 0
+
+        self.camera = zwoasi.Camera(id_)
+        self.config = config
+        self.capture_image_lock = threading.Lock()
+
+    def __del__(self):
+        self.camera.stop_video_capture()
+        self.camera.close()
+
+    def apply_settings(self, section):
+        # Initialise
+        controls = self.camera.get_controls()
+        self.camera.start_video_capture()
+
+        # Read all camera controls defined in the config file
+        for c in controls:
+            cl = c.lower()
+            value = get_config_option(self.config, section, cl)
+            if value is not None:
+                default_value = controls[c]['DefaultValue']
+                control_type = getattr(zwoasi, 'ASI_' + c.upper())
+                logger.debug('set control value %s to %s', cl, value)
+                if value == 'auto':
+                    self.camera.set_control_value(control_type, default_value, auto=True)
+                else:
+                    # Cast value to same type as default_value
+                    self.camera.set_control_value(control_type, type(default_value)(value), auto=False)
+
+        image_type = get_config_option(self.config, section, 'image_type')
+        if image_type is not None:
+            logger.debug('set image type to %s', image_type)
+            self.camera.set_image_type(getattr(zwoasi, 'ASI_IMG_' + image_type.upper()))
+
+    def capture_image(self, _):
+        logger.debug('capture_image: acquiring lock')
+        if self.capture_image_lock.acquire(False):
+            try:
+                logger.debug('capture_image: acquired lock')
+                t = time.time()
+                img_info = self.get_control_values()
+                img = Image.fromarray(self.camera.capture_video_frame()[:, :, ::-1])  # Swap from BGR order
+                img_info['DateTime'] = time.strftime('%Y-%m-%d %H:%M:%S+00:00', time.gmtime(t))
+
+                # Take CPU temperature as system temperature
+                img_info['SystemTemperature'] = float('NaN')
+                with open('/sys/class/thermal/thermal_zone0/temp') as f:
+                    img_info['SystemTemperature'] = float(f.read().strip()) / 1000
+                return img, img_info, t
+
+            finally:
+                logging.debug('capture_image: released lock')
+                self.capture_image_lock.release()
+        else:
+            logger.warning('capture_image: could not acquire lock')
+            raise Exception('could not acquire lock')
+
+    def get_control_values(self):
+        controls = self.camera.get_controls()
+        r = {}
+        for k in controls:
+            r[k] = self.camera.get_control_value(controls[k]['ControlType'])[0]
+
+        # Fix up certain keys
+        r['Exposure_s'] = r['Exposure'] / 1000000.0
+        if 'Temperature' in r:
+            r['SensorTemperature'] = r['Temperature'] / 10.0
+        if 'Flip' in r:
+            r['Flip'] = {0: 'None', 1: 'Horizontal', 2: 'Vertical', 3: 'Both'}[r['Flip']]
+
+        return r
+
+
+logger = logging.getLogger(__name__)
