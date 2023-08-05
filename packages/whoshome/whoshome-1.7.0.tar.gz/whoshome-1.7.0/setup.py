c5759a8bf3781bb6f21e47930c6d81e6e13e8eb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:12 +0000

    Add package whoshome/1.7.0/whoshome-1.7.0.tar.gz

---
+++ packages/whoshome/whoshome-1.7.0.tar.gz/whoshome-1.7.0/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+try:
+    with open(path.join(here, 'README.md')) as f:
+        long_description = f.read()
+except:
+    long_description = ''
+
+setup(
+    name='whoshome',
+    version='1.7.0',
+    description='Find out who\'s home based on Wi-Fi connection',
+    long_description=long_description,
+    url='https://github.com/fabiocody/Whos-Home',
+    author='Fabio Codiglioni',
+    author_email='fabiocody@icloud.com',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='whoshome, wifi, mac, home, arp',
+    py_modules=['whoshome'],
+    install_requires=['scapy-python3', 'netifaces'],
+    entry_points={
+        'console_scripts': [
+            'whoshome=whoshome:main',
+        ],
+    },
+)
