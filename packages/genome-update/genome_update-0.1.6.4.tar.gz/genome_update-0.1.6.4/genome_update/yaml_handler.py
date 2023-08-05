8882dfee5ae746f556a3efe75381f516b3789676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:14 +0000

    Add package genome-update/0.1.6.4/genome_update-0.1.6.4.tar.gz

---
+++ packages/genome-update/genome_update-0.1.6.4.tar.gz/genome_update-0.1.6.4/genome_update/yaml_handler.py
@@ -0,0 +1,10 @@
+import yaml
+
+class yaml_handler:
+	def write(file, data):
+		with open(file,'w') as yam:
+			yaml.dump(data,yam, default_flow_style=False)
+
+	def read(file):
+		with open(file,'r') as yam:
+			return next(yaml.load_all(yam))
