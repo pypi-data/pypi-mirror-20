9c71e0a4bb187589bc5896155ac303b98b48203b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:29 +0000

    Add package processout/5.2.1/processout-5.2.1.tar.gz

---
+++ packages/processout/processout-5.2.1.tar.gz/processout-5.2.1/processout/project.py
@@ -0,0 +1,138 @@
+try:
+    from urllib.parse import quote_plus
+except ImportError:
+    from urllib import quote_plus
+
+import processout
+
+from processout.networking.request  import Request
+from processout.networking.response import Response
+
+# The content of this file was automatically generated
+
+class Project(object):
+    def __init__(self, client, prefill = None):
+        self._client = client
+
+        self._id = None
+        self._name = None
+        self._logo_url = None
+        self._email = None
+        self._created_at = None
+        if prefill != None:
+            self.fill_with_data(prefill)
+
+    
+    @property
+    def id(self):
+        """Get id"""
+        return self._id
+
+    @id.setter
+    def id(self, val):
+        """Set id
+        Keyword argument:
+        val -- New id value"""
+        self._id = val
+        return self
+    
+    @property
+    def name(self):
+        """Get name"""
+        return self._name
+
+    @name.setter
+    def name(self, val):
+        """Set name
+        Keyword argument:
+        val -- New name value"""
+        self._name = val
+        return self
+    
+    @property
+    def logo_url(self):
+        """Get logo_url"""
+        return self._logo_url
+
+    @logo_url.setter
+    def logo_url(self, val):
+        """Set logo_url
+        Keyword argument:
+        val -- New logo_url value"""
+        self._logo_url = val
+        return self
+    
+    @property
+    def email(self):
+        """Get email"""
+        return self._email
+
+    @email.setter
+    def email(self, val):
+        """Set email
+        Keyword argument:
+        val -- New email value"""
+        self._email = val
+        return self
+    
+    @property
+    def created_at(self):
+        """Get created_at"""
+        return self._created_at
+
+    @created_at.setter
+    def created_at(self, val):
+        """Set created_at
+        Keyword argument:
+        val -- New created_at value"""
+        self._created_at = val
+        return self
+    
+
+    def fill_with_data(self, data):
+        """Fill the current object with the new values pulled from data
+        Keyword argument:
+        data -- The data from which to pull the new values"""
+        if "id" in data.keys():
+            self.id = data["id"]
+        if "name" in data.keys():
+            self.name = data["name"]
+        if "logo_url" in data.keys():
+            self.logo_url = data["logo_url"]
+        if "email" in data.keys():
+            self.email = data["email"]
+        if "created_at" in data.keys():
+            self.created_at = data["created_at"]
+        
+        return self
+
+    def fetch_gateway_configurations(self, options = {}):
+        """Get all the gateway configurations of the project
+        Keyword argument:
+        
+        options -- Options for the request"""
+        self.fill_with_data(options)
+
+        request = Request(self._client)
+        path    = "/projects/" + quote_plus(self.id) + "/gateway-configurations"
+        data    = {
+
+        }
+
+        response = Response(request.get(path, data, options))
+        return_values = []
+        
+        a    = []
+        body = response.body
+        for v in body['gateway_configurations']:
+            tmp = processout.GatewayConfiguration(self._client)
+            tmp.fill_with_data(v)
+            a.append(tmp)
+
+        return_values.append(a)
+            
+
+        
+        return return_values[0]
+
+    
