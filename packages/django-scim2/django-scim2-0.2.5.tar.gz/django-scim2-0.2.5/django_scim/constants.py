06ec654108e1d4496ec904b58089b16ec695ea70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:51 +0000

    Add package django-scim2/0.2.5/django-scim2-0.2.5.tar.gz

---
+++ packages/django-scim2/django-scim2-0.2.5.tar.gz/django-scim2-0.2.5/django_scim/constants.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django.utils.six import moves
+
+
+SCHEMA_URI_ERROR = 'urn:ietf:params:scim:api:messages:2.0:Error'
+
+SCIM_CONTENT_TYPE = 'application/scim+json'
+
+SCHEMA_URI_SERACH_REQUEST = 'urn:ietf:params:scim:api:messages:2.0:SearchRequest'
+
