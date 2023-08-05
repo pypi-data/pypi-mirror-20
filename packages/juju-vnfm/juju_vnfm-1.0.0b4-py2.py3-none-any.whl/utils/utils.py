9db918748c1b5c62c62b6c05281fd315d3cca799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:43 +0000

    Add package juju-vnfm/1.0.0b4/juju_vnfm-1.0.0b4-py2.py3-none-any.whl

---
+++ packages/juju-vnfm/juju_vnfm-1.0.0b4-py2.py3-none-any.whl/utils/utils.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+#
+# Project:  juju-vnfm
+# file:     utils
+# created:  17/02/2017 
+#
+
+"""
+description goes here
+"""
+__author__ = "lto"
+__maintainer__ = "lto"
+
+
+def get_version():
+    return '1.0.0b4'
