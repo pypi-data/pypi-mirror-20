72c5d23fb2630c2998e5cd171ba73fc205c414f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:16 +0000

    Add package searchcolor/1.1.0/searchcolor-1.1.0.tar.gz

---
+++ packages/searchcolor/searchcolor-1.1.0.tar.gz/searchcolor-1.1.0/test/test_basic.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+#coding=UTF-8
+import os
+import sys
+#installed
+import pytest
+#local
+sys.path.append(os.path.split(os.path.split(__file__)[0])[0])
+import searchcolor
+
+from api_keys import GoogleKeyLocker as Key
+
+Key = Key()
+
+def test_google_average():
+    result = searchcolor.google_average('Death', 10, Key.api(), Key.cse())
+    assert result.get('name') == 'Death'
+    assert result.get('red') >= 0 and result.get('red') <= 255
+    assert result.get('green') >= 0 and result.get('green') <= 255
+    assert result.get('blue') >= 0 and result.get('blue') <= 255
