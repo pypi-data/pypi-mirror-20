11b65bd62b683781a8bc259bfdf86f15e5f076f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:25 +0000

    Add package RedLeader/0.1.4/RedLeader-0.1.4-py3-none-any.whl

---
+++ packages/RedLeader/RedLeader-0.1.4-py3-none-any.whl/redleader/code_deploy.py
@@ -0,0 +1,12 @@
+class CodeDeployManager(object):
+    def __init__(self):
+        pass
+
+    def create_package(self):
+        pass
+
+    # TODO: instance-setup.sh that downloads code deploy
+
+    def upload_package_to_s3(self):
+        pass
+    
