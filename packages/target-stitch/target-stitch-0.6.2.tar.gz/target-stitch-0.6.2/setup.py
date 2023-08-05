149cf67f44efff96d2182006078de23967e23eef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:23 +0000

    Add package target-stitch/0.6.2/target-stitch-0.6.2.tar.gz

---
+++ packages/target-stitch/target-stitch-0.6.2.tar.gz/target-stitch-0.6.2/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='target-stitch',
+      version='0.6.2',
+      description='Singer.io target for the Stitch API',
+      author='Stitch',
+      url='https://singer.io',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['target_stitch'],
+      install_requires=[
+          'stitchclient>=0.4.3',
+          'jsonschema',
+          'strict-rfc3339',
+          'singer-python>=0.1.0',
+      ],
+      entry_points='''
+          [console_scripts]
+          target-stitch=target_stitch:main
+      ''',
+)
