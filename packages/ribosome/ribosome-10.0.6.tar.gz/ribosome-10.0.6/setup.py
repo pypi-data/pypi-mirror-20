034efc48b1022f666793754c02a4ae21210e6c24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:12 +0000

    Add package ribosome/10.0.6/ribosome-10.0.6.tar.gz

---
+++ packages/ribosome/ribosome-10.0.6.tar.gz/ribosome-10.0.6/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+version_parts = (10, 0, 6)
+version = '.'.join(map(str, version_parts))
+
+setup(
+    name='ribosome',
+    description='neovim plugin framework',
+    version=version,
+    author='Torsten Schmits',
+    author_email='torstenschmits@gmail.com',
+    license='MIT',
+    url='https://github.com/tek/ribosome',
+    packages=find_packages(exclude=['unit', 'unit.*']),
+    install_requires=[
+        'amino>=9.5.3',
+        'neovim',
+        'pyrsistent',
+    ]
+)
