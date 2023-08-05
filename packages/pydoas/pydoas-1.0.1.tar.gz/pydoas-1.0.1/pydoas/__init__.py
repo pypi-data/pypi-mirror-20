3fd58068102bac560616a7ae67d407839cb3084a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:11 +0000

    Add package pydoas/1.0.1/pydoas-1.0.1.tar.gz

---
+++ packages/pydoas/pydoas-1.0.1.tar.gz/pydoas-1.0.1/pydoas/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from pkg_resources import get_distribution
+
+__version__ = get_distribution('pydoas').version
+
+from os.path import abspath, dirname
+
+_LIBDIR = abspath(dirname(__file__))
+
+from .inout import *  #from .io import get_data_dirs, get_data_files
+import dataimport
+import analysis
+
