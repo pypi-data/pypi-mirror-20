8140b01b81eedfdba9a1e0d1d7a1778135536a4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:21 +0000

    Add package codestyle/0.4.1/codestyle-0.4.1.tar.gz

---
+++ packages/codestyle/codestyle-0.4.1.tar.gz/codestyle-0.4.1/codestyle/utils.py
@@ -0,0 +1,60 @@
+"""Codestyle checker utils"""
+
+import os
+import subprocess
+from subprocess import PIPE
+
+
+class DependencyError(BaseException):
+    """
+    Raises if no some dependency found
+    """
+
+
+def which(program):
+    """
+    Get path of an executable file or None
+    """
+
+    def is_exe(fpath):
+        "Check file is executable"
+
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, _ = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+    return None
+
+
+def check_external_deps():
+    """
+    Check external dependencies
+    """
+
+    binaries = ['npm', 'jscs', 'jshint', 'phpcs', 'phpcbf',
+                'csscomb', 'htmlcs']
+    for binary in binaries:
+        if not which(binary):
+            raise DependencyError('%s is not installed' % binary)
+
+    nodejs_libs = ['jscs-fixer', 'walk', 'brace-expansion']
+
+    for nodejs_lib in nodejs_libs:
+        proc = subprocess.Popen(
+            'npm view %s version' % nodejs_lib, shell=True,
+            stdout=PIPE, stderr=PIPE
+        )
+        proc.communicate()
+        if proc.returncode != 0:
+            raise DependencyError(
+                '%s is not installed\nRun npm -g install %s'
+                % (nodejs_lib, nodejs_lib)
+            )
---
+++ packages/codestyle/codestyle-0.4.1.tar.gz/codestyle-0.4.1/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='codestyle',
+    version='0.4.1',
+    author=u'Sergey Levitin',
+    author_email='selevit@gmail.com',
+    packages=find_packages(),
+    package_data={
+        'codestyle': ['standards/*'],
+    },
+    include_package_data=True,
+    zip_safe=False,
+    url='https://github.com/webpp-studio/codestyle',
+    license='GPL licence, see LICENCE',
+    description='Extendable codestyle checker and fixer',
+    long_description=open('README.rst').read(),
+    scripts=['scripts/codestyle'],
+    install_requires=[
+        'future',
+        'pep8>=1.5.7,<1.6',
+        'pyflakes>=0.8.1,<0.9',
+        'autopep8>=1.2,<=1.2.99',
+        'flake8>=3.2,<=3.2.99',
+        'autoflake>=0.6,<=0.6.99',
+    ],
+    test_suite='tests',
+    tests_require=[
+        'mock>=2.0,<=2.0.99',
+        'six>=1.9'
+    ]
+)
+
