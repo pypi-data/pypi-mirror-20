01f7f051cc837a4cd92a142bdbfdec56fc4e2df4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:49 +0000

    Add package httpie-token-auth/0.1.3/httpie-token-auth-0.1.3.tar.gz

---
+++ packages/httpie-token-auth/httpie-token-auth-0.1.3.tar.gz/httpie-token-auth-0.1.3/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+repo_url = 'https://github.com/chillaranand/httpie-token-auth'
+
+
+setup(
+    name='httpie-token-auth',
+    description='token Auth plugin for HTTPie.',
+    long_description=open('README.md').read().strip(),
+    version='0.1.3',
+    author='Chillar Anand',
+    author_email='anand21nanda@gmail.com',
+    license='MIT',
+    url=repo_url,
+    download_url=repo_url,
+    py_modules=['httpie_token_auth'],
+    packages=find_packages(),
+    zip_safe=False,
+    entry_points={
+        'httpie.plugins.auth.v1': [
+            'httpie_token_auth = httpie_token_auth:TokenAuthPlugin'
+        ]
+    },
+    install_requires=[
+        'httpie>=0.7.0'
+    ],
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Environment :: Plugins',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Utilities'
+    ],
+)
