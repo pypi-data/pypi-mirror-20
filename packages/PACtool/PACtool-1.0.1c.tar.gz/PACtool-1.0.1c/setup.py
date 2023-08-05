10c1c75ebe8a92e7223d8843a8cb949b11bb1097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:31 +0000

    Add package PACtool/1.0.1c/PACtool-1.0.1c.tar.gz

---
+++ packages/PACtool/PACtool-1.0.1c.tar.gz/PACtool-1.0.1c/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    
+setup(
+      name = "PACtool",
+      version = "1.0.1c",
+      author = "Akis, Christina, Paschalis",
+      author_email = "pnatsidis@hotmail.com",
+      description = "This is our little project...for GWAS",
+      url = "https://github.com/pnatsi/PACtool",
+      keywords = "project python linux gwas bioinfo-grad",
+      packages = ['pactool'],
+      license = 'MIT',
+      install_requires = [
+          'matplotlib',
+          'numpy',
+          'scipy',
+          'pyliftover',
+      ],
+      long_description = read('README.txt'),
+      data_files = [('gwas_data', ['gwas.controls.gen', 'gwas.cases.gen'])]
+)
