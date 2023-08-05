2798b891ab7e5a86c65591af19b2d61018ea2f0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:47 +0000

    Add package globus-sdk/0.5.0/globus_sdk-0.5.0-py2.7.egg

---
+++ packages/globus-sdk/globus_sdk-0.5.0-py2.7.egg/tests/framework/__init__.py
@@ -0,0 +1,19 @@
+from tests.framework.capturedio_testcase import CapturedIOTestCase
+from tests.framework.tools import get_fixture_file_dir, get_client_data
+
+from tests.framework.constants import (
+    GO_EP1_ID, GO_EP2_ID, GO_USER_ID, SDK_USER_ID,
+    SDKTESTER1A_NATIVE1_RT)
+
+__all__ = [
+    "CapturedIOTestCase",
+
+    "get_fixture_file_dir",
+    "get_client_data",
+
+    "GO_EP1_ID",
+    "GO_EP2_ID",
+    "GO_USER_ID",
+    "SDK_USER_ID",
+    "SDKTESTER1A_NATIVE1_RT",
+]
