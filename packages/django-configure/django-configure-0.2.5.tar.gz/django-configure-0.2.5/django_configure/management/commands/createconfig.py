c03a8aa060091fd2cf50b92187d1e99af1a8244f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:43 +0000

    Add package django-configure/0.2.5/django-configure-0.2.5.tar.gz

---
+++ packages/django-configure/django-configure-0.2.5.tar.gz/django-configure-0.2.5/django_configure/management/commands/createconfig.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand, CommandError
+import django_configure
+
+
+class Command(BaseCommand):
+    help = 'Generates config file and wsgi file in designated location'
+
+    def add_arguments(self, parser):
+        parser.add_argument('config_path', type=str)
+        parser.add_argument('wsgi_path', nargs='?', type=str)
+        parser.add_argument('--dev', action='store_true')
+
+    def handle(self, *args, **options):
+        config = django_configure.main_config
+
+        config.config_path = options['config_path']
+        config_output = config.generate(dev=options['dev'])
+
+        self.stdout.write(self.style.SUCCESS('Successfully generated "%s"' % options['config_path']))
+        self.stdout.write('Config output:')
+        self.stdout.write(config_output)
+
+        if options['wsgi_path'] is not None:
+            wsgi_output = config.generate_wsgi(options['wsgi_path'])
+            self.stdout.write(self.style.SUCCESS('Successfully generated "%s"' % options['wsgi_path']))
+            self.stdout.write('Wsgi output:')
+            self.stdout.write(wsgi_output)
