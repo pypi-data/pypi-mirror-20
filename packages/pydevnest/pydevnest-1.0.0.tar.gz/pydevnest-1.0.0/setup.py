f1dbffefd0323099d5dd35a79a0e80eb800044e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:42 +0000

    Add package pydevnest/1.0.0/pydevnest-1.0.0.tar.gz

---
+++ packages/pydevnest/pydevnest-1.0.0.tar.gz/pydevnest-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup 
+setup(
+name= 'pydevnest',
+version= '1.0.0',
+py_modules= ['pydevnest'],
+author= 'Manisankar',
+author_email= 'mani.sankar.s@gmail.com',
+url= 'http://www.ethoughtz.com',
+description= 'A simple printer of pydevnest lists',
+)
