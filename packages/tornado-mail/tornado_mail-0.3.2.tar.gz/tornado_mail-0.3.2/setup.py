2a783d5530ea12adc291083e3223ff9834c94282
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:56 +0000

    Add package tornado-mail/0.3.2/tornado_mail-0.3.2.tar.gz

---
+++ packages/tornado-mail/tornado_mail-0.3.2.tar.gz/tornado_mail-0.3.2/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='tornado_mail',
+    author='ruitian',
+    author_email='ike960705@gmail.com',
+    description='A email plugin for tornado. It is a fake Flask_mail.',
+    url='https://github.com/ruitian/tornado_mail.git',
+    version='0.3.2',
+    # packages=['tornado_mail'],
+    install_requires=[
+        'tornado>=4.0.0'
+    ]
+)
