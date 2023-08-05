08ec350dda7ed784522ea9d23f560a7ee7f9e5db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:51 +0000

    Add package flaskcbv/1.4.1/flaskcbv-1.4.1.tar.gz

---
+++ packages/flaskcbv/flaskcbv-1.4.1.tar.gz/flaskcbv-1.4.1/flaskcbv/view/crud.py
@@ -0,0 +1,52 @@
+from flask import abort, redirect, url_for
+from generic import TemplateView
+
+class FormMixin(object):
+    form_class = None  # Form class
+    form_success_url = None
+    form_unsuccess_url = None
+
+    def get_form_class(self):
+        return self.form_class
+
+    def get_form(self, form_class=None, instance=None):
+        if instance is None:
+            instance = self
+        if form_class is None:
+            form_class = self.get_form_class()
+        return form_class(self.request.form, instance)
+
+    def get_form_postprocess_url(self, is_valid=False):
+        if is_valid and self.form_success_url is not None:
+            return self.form_success_url
+        elif not is_valid and self.form_unsuccess_url is not None:
+            return self.form_unsuccess_url
+        else:
+            raise ValueError('No URL to redirect to.  Either provide a url or define a get_absolute_url method on the Model.')  # FIXME: Je vhodne vyvolat ValueError?
+
+    def form_valid(self, form, *args, **kwargs):
+        return redirect(self.get_form_postprocess_url(True))
+
+    def form_invalid(self, form, *args, **kwargs):
+        return redirect(self.get_form_postprocess_url(False))
+
+
+
+class FormViewMixin(FormMixin):
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(FormViewMixin, self).get_context_data(*args, **kwargs)
+        context['form'] = self.get_form()
+        return context
+
+    def post(self, *args, **kwargs):
+        form = self.get_form()
+        if form.validate():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_invalid(self, form, *args, **kwargs):
+        kwargs['form'] = form
+        return self.render_template(*args, **kwargs)
+
