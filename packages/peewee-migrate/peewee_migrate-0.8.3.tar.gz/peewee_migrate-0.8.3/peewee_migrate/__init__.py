a019ebbe920e04b0c3fe4c01e736349dedab0f53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:14 +0000

    Add package peewee-migrate/0.8.3/peewee_migrate-0.8.3.tar.gz

---
+++ packages/peewee-migrate/peewee_migrate-0.8.3.tar.gz/peewee_migrate-0.8.3/peewee_migrate/__init__.py
@@ -0,0 +1,34 @@
+"""
+    The package description.
+
+"""
+import logging
+import peewee as pw
+import datetime as dt
+
+# Package information
+# ===================
+
+__version__ = "0.8.3"
+__project__ = "peewee_migrate"
+__author__ = "Kirill Klenov <horneds@gmail.com>"
+__license__ = "BSD"
+
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.addHandler(logging.StreamHandler())
+
+
+class MigrateHistory(pw.Model):
+
+    """Presents the migrations in database."""
+
+    name = pw.CharField()
+    migrated_at = pw.DateTimeField(default=dt.datetime.utcnow)
+
+    def __unicode__(self):
+        """String representation."""
+        return self.name
+
+
+from .router import Migrator, Router # noqa
