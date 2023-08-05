882ab6a116943ad374550c05f8da0ac95a4366cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:22 +0000

    Add package aws-console/0.1.3/aws-console-0.1.3.tar.gz

---
+++ packages/aws-console/aws-console-0.1.3.tar.gz/aws-console-0.1.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+README = 'a console for easily using AWS with boto3'
+
+requires = [ 'ipython >= 5.2.2', 'boto3' ]
+
+setup(name='aws-console',
+      version='0.1.3',
+      description=README,
+      long_description=README,
+      url='https://github.com/haarcuba/aws_console',
+      classifiers=[
+          "Programming Language :: Python",
+      ],
+      author='Yoav Kleinberger',
+      author_email='haarcuba@gmail.com',
+      packages=find_packages(),
+      keywords='AWS, boto, amazon',
+      entry_points = {
+          'console_scripts': 'aws-console = aws_console.main:main'
+
+      },
+      zip_safe=False,
+      install_requires=requires,
+      )
