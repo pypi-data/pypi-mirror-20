64cc20cb2ecb115e84cc730ed3c79528332e295b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:19 +0000

    Add package digsandpaper/0.1.2/digsandpaper-0.1.2.tar.gz

---
+++ packages/digsandpaper/digsandpaper-0.1.2.tar.gz/digsandpaper-0.1.2/test/coarse/generate/test_generating.py
@@ -0,0 +1,98 @@
+import unittest
+import json
+import codecs
+from digsandpaper.coarse.generate.generator import Generator
+import os
+
+
+_location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+
+def load_json_file(file_name):
+    rules = json.load(codecs.open(os.path.join(_location__, file_name),
+                                  'r', 'utf-8'))
+    return rules
+
+
+class TestCoarseGenerating(unittest.TestCase):
+
+    def test_basic_coarse_generating(self):
+        config = load_json_file("1_config.json")
+        parameterized_queries = load_json_file("1_query.json")
+        generator = Generator(config)
+
+        generated_queries = [generator.generate(
+            q) for q in parameterized_queries]
+        where0 = generated_queries[0]["SPARQL"]["where"]
+        where1 = generated_queries[1]["SPARQL"]["where"]
+        self.assertEqual(len(generated_queries), 2)
+        self.assertEqual(where0["clauses"][1]["fields"][0]["name"],
+                         "extractors.content_strict.data_extractors.city.result.value")
+        self.assertEqual(where0["clauses"][1]["fields"][0]["weight"],
+                         4)
+        self.assertEqual(where1["clauses"][1]["fields"][1]["name"],
+                         "extractors.content_relaxed.data_extractors.city.result.value")
+        self.assertNotIn("weight", where1["clauses"][1]["fields"][1])
+        #print json.dumps(generated_queries, sort_keys=True, indent=4)
+
+    def test_basic_coarse_generating_elasticsearch_compiler(self):
+        config = load_json_file("1_config_step_two.json")
+        parameterized_queries = load_json_file("1_query_step_two.json")
+        generator = Generator(config)
+
+        generated_queries = [generator.generate(
+            q) for q in parameterized_queries]
+        self.assertEqual(len(generated_queries), 2)
+        self.assertIn("ELASTICSEARCH", generated_queries[0])
+        self.assertIn("search", generated_queries[0]["ELASTICSEARCH"])
+        self.assertIn("query", generated_queries[0]["ELASTICSEARCH"]["search"])
+        self.assertEqual(generated_queries[0]["ELASTICSEARCH"]["search"]["size"], 500)
+        self.assertEqual(generated_queries[0]["ELASTICSEARCH"]["search"]["from"], 0)
+        
+        #print json.dumps(generated_queries, sort_keys=True, indent=4)
+
+    def test_basic_coarse_generating_compound_filter(self):
+        config = load_json_file("2_config.json")
+        parameterized_queries = load_json_file("2_query.json")
+        generator = Generator(config)
+
+        generated_queries = [generator.generate(
+            q) for q in parameterized_queries]
+        self.assertEqual(len(generated_queries), 1)
+        #print "coarse 2\n"
+        #print json.dumps(generated_queries, sort_keys=True, indent=4)
+
+    def test_basic_coarse_generating_elasticsearch_compiler_compound_filter(self):
+        config = load_json_file("2_config_step_two.json")
+        parameterized_queries = load_json_file("2_query_step_two.json")
+        generator = Generator(config)
+
+        generated_queries = [generator.generate(
+            q) for q in parameterized_queries]
+        self.assertEqual(len(generated_queries), 1)
+        #print json.dumps(generated_queries, sort_keys=True, indent=4)
+
+    def test_basic_coarse_generating_no_type_mapping(self):
+        config = load_json_file("3_config.json")
+        parameterized_queries = load_json_file("3_query.json")
+        generator = Generator(config)
+
+        generated_queries = [generator.generate(
+            q) for q in parameterized_queries]
+        self.assertEqual(len(generated_queries), 1)
+        #print "coarse 2\n"
+        #print json.dumps(generated_queries, sort_keys=True, indent=4)
+
+    def test_basic_coarse_generating_elasticsearch_compiler_no_type_mapping(self):
+        config = load_json_file("3_config_step_two.json")
+        parameterized_queries = load_json_file("3_query_step_two.json")
+        generator = Generator(config)
+
+        generated_queries = [generator.generate(
+            q) for q in parameterized_queries]
+        self.assertEqual(len(generated_queries), 1)
+        #print json.dumps(generated_queries, sort_keys=True, indent=4)
+
+if __name__ == '__main__':
+    unittest.main()
