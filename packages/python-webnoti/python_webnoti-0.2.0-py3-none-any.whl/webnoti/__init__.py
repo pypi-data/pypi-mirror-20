ecf652541ac5431b2b1a959231a985790cc99da9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:41 +0000

    Add package python-webnoti/0.2.0/python_webnoti-0.2.0-py3-none-any.whl

---
+++ packages/python-webnoti/python_webnoti-0.2.0-py3-none-any.whl/webnoti/__init__.py
@@ -0,0 +1,12 @@
+"""
+    webnoti
+    ~~~~~~~
+
+    a python library for sending web notification
+    It tries to be simple, concise, but still extensive.
+"""
+
+__version__ = '0.2.0'
+
+from .notification import send_notification, Notification
+from .utils import get_private_key
