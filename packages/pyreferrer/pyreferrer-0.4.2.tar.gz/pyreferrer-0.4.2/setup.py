97f241bfa6f50a1537ecb58225781d134255d3e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:09 +0000

    Add package pyreferrer/0.4.2/pyreferrer-0.4.2.tar.gz

---
+++ packages/pyreferrer/pyreferrer-0.4.2.tar.gz/pyreferrer-0.4.2/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(name='pyreferrer',
+      version='0.4.2',
+      description='A referrer parser for Python.',
+      url='http://github.com/Shopify/pyreferrer',
+      author='Shopify Data Acceleration',
+      author_email='data-acceleration@shopify.com',
+      license='MIT',
+      packages=['pyreferrer'],
+      package_data={'pyreferrer': ['data/*', ]},
+      install_requires=['tldextract==2.0.2'],
+      extras_require={
+          'test': ['nose'],
+      },
+      classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+      ],
+      include_package_data=True,
+      zip_safe=False)
