2ef0e12e8d591f70544b2e0c6d43e9f26f589597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:48 +0000

    Add package nest_le/1.4.0/nest_le-1.4.0.zip

---
+++ packages/nest_le/nest_le-1.4.0.zip/nest_le-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'nest_le',
+        version = '1.4.0',
+        py_modules = ['nest_le'],
+        author = 'zhangle',
+        author_email = 'zhangle_qaz@163.com',
+        url = 'http://localhost:8080/nest',
+        description = 'A simple printer of nested list',
+    )
