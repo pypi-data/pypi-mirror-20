98e7c51e27f9f9e95e547f122b2375f3821b7966
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:09 +0000

    Add package POAP/0.1.26/POAP-0.1.26.tar.gz

---
+++ packages/POAP/POAP-0.1.26.tar.gz/POAP-0.1.26/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='POAP',
+    version='0.1.26',
+    author='David Bindel',
+    author_email='bindel@cornell.edu',
+    packages=['poap', 'poap.test'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/POAP/',
+    license='LICENSE.txt',
+    description='Python Optimization Asynchronous Plumbing.',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',        
+    ],
+    install_requires=[
+    ],
+)
