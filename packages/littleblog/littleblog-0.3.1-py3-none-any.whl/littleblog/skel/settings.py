921b8535c4c1a9dcfd3e0bc3bf5050f37f79725a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:16 +0000

    Add package littleblog/0.3.1/littleblog-0.3.1-py3-none-any.whl

---
+++ packages/littleblog/littleblog-0.3.1-py3-none-any.whl/littleblog/skel/settings.py
@@ -0,0 +1,9 @@
+""" Default settings for littleblog projects """
+
+# Directories are relative to the project directory
+
+CONTENT_DIR = 'posts'
+OUTPUT_DIR = 'html'
+TEMPLATE_DIR = 'templates'
+
+STATIC_DIR = 'static'
