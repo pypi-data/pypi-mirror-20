8c2cefedafd2845e6e8090822634e4915c0643d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:09 +0000

    Add package django-arcutils/2.16.0/django-arcutils-2.16.0.tar.gz

---
+++ packages/django-arcutils/django-arcutils-2.16.0.tar.gz/django-arcutils-2.16.0/setup.py
@@ -0,0 +1,72 @@
+import sys
+
+from setuptools import find_packages, setup
+
+
+with open('VERSION') as version_fp:
+    VERSION = version_fp.read().strip()
+
+
+# Base dependencies
+install_requires = [
+    'certifi>=2017.1.23',
+    'django-local-settings>=1.0b5',
+    'pytz>=2016.10',
+    'raven>=5.32.0',
+    'stashward',
+]
+
+if sys.version_info[:2] < (3, 4):
+    django_version = '1.8'
+    install_requires.append('enum34')
+else:
+    django_version = '1.10'
+
+# Dependencies that are used in multiple places
+deps = {
+    'djangorestframework': 'djangorestframework>=3.5.3',
+    'ldap3': 'ldap3>=2.2.0',
+}
+
+setup(
+    name='django-arcutils',
+    version=VERSION,
+    url='https://github.com/PSU-OIT-ARC/django-arcutils',
+    author='PSU - OIT - ARC',
+    author_email='consultants@pdx.edu',
+    description='Common utilities used in ARC Django projects',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    extras_require={
+        'ldap': [
+            deps['ldap3'],
+        ],
+        'dev': [
+            'django>={django_version},<{django_version}.999'.format_map(locals()),
+            deps['djangorestframework'],
+            'flake8',
+            deps['ldap3'],
+            'psu.oit.arc.tasks',
+            'tox>=2.6.0',
+        ],
+        'tox': [
+            deps['djangorestframework'],
+            deps['ldap3'],
+        ]
+    },
+    entry_points="""
+    [console_scripts]
+    arcutils = arcutils.__main__:main
+
+    """,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
