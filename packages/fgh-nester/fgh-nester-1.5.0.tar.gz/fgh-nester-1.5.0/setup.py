00827fee03c720cf682a8b40fb94390a598e65fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:26 +0000

    Add package fgh-nester/1.5.0/fgh-nester-1.5.0.tar.gz

---
+++ packages/fgh-nester/fgh-nester-1.5.0.tar.gz/fgh-nester-1.5.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name		= 'fgh-nester',
+		version		= '1.5.0',
+		py_modules	= ['nester'],
+		author		= 'fghinzelli',
+		author_email	= 'fghinzelli@gmail.com',
+		url			= 'http://fghz16.com',
+		description		= 'A simple printer of nested lists',
+	)
