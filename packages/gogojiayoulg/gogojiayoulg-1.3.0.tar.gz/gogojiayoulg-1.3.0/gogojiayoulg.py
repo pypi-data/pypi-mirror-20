747af0fd4b62f68c2576706670d139f0d967a0e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:24 +0000

    Add package gogojiayoulg/1.3.0/gogojiayoulg-1.3.0.tar.gz

---
+++ packages/gogojiayoulg/gogojiayoulg-1.3.0.tar.gz/gogojiayoulg-1.3.0/gogojiayoulg.py
@@ -0,0 +1,15 @@
+'''HELLO this is a test
+aaaaa
+bbbbb
+ccccc
+'''
+
+def print_lol(mylist,level=0):
+
+    for x in mylist:
+        if isinstance(x,list):
+            print_lol(x,level+1)
+        else:
+            for y in range(level):
+                print("\t",end="")
+            print(x)
---
+++ packages/gogojiayoulg/gogojiayoulg-1.3.0.tar.gz/gogojiayoulg-1.3.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name = "gogojiayoulg",
+    version = '1.3.0',
+    py_modules = ['gogojiayoulg'],
+    author = "gogojiayoulg"
+)
