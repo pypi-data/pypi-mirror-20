504846e791ab12f21538a3b7376798da93168b17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:08 +0000

    Add package VenC/1.2.0.post0/VenC-1.2.0.post0.tar.gz

---
+++ packages/VenC/VenC-1.2.0.post0.tar.gz/VenC-1.2.0.post0/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+from setuptools import setup
+
+setup(name='VenC',
+    version='1.2.0-0',
+    description='A static blog generator.',
+    author='Denis Salem',
+    author_email='denissalem@tuxfamily.org',
+    url='https://github.com/DenisSalem/VenC',
+    packages=['VenC', 'VenC.languages'],
+    license="GNU/GPLv3",
+    platforms="Linux",
+    long_description="A static blog generator, light and easy to use.",
+    classifiers=[
+        "Environment :: Console",
+        "Development Status :: 5 - Production/Stable"
+    ],
+    scripts=['venc','venc-unit-test']
+)
