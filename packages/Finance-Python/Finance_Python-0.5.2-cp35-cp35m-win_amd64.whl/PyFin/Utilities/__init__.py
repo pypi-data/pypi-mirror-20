c88f0129701d1b8f1cf4d17eddd4982f6dff0b33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:30 +0000

    Add package Finance-Python/0.5.2/Finance_Python-0.5.2-cp35-cp35m-win_amd64.whl

---
+++ packages/Finance-Python/Finance_Python-0.5.2-cp35-cp35m-win_amd64.whl/PyFin/Utilities/__init__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+u"""
+Created on 2015-8-17
+
+@author: cheng.li
+"""
+
+import time
+import numpy as np
+from PyFin.Utilities.Asserts import pyFinAssert
+from PyFin.Utilities.Asserts import pyFinWarning
+from PyFin.Utilities.Asserts import isClose
+from PyFin.Utilities.Tools import to_dict
+
+__all__ = ['pyFinAssert',
+           'pyFinWarning',
+           'isClose',
+           'to_dict']
+
+
+def print_timing(func):
+    def wrapper(*arg):
+        t1 = time.time()
+        res = func(*arg)
+        t2 = time.time()
+        return t2 - t1, res
+    return wrapper
+
+
+
+
+
+
