5246372ca96041a1081e8368984fac69842c0a11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:25 +0000

    Add package Accern/0.1dev/Accern-0.1dev.tar.gz

---
+++ packages/Accern/Accern-0.1dev.tar.gz/Accern-0.1dev/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Accern',
+    version='0.1dev',
+    packages=['accern',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
