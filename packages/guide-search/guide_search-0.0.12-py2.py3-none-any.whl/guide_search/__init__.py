ce7379d334617dbed7c24e725d134ae5e5186f75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:18 +0000

    Add package guide-search/0.0.12/guide_search-0.0.12-py2.py3-none-any.whl

---
+++ packages/guide-search/guide_search-0.0.12-py2.py3-none-any.whl/guide_search/__init__.py
@@ -0,0 +1,19 @@
+
+from guide_search.esearch import Esearch, Dsl
+
+from guide_search.exceptions import (    # noqa: F401
+    BadRequestError,            # 400
+    ResourceNotFoundError,      # 404
+    ConflictError,              # 409
+    PreconditionError,          # 412
+    ServerError,                # 500
+    ServiceUnreachableError,    # 503
+    UnknownError,               # unexpected http response
+    ResultParseError,           # es result not in expected form
+    CommitError,
+    JSONDecodeError,
+    ValidationError)
+
+__version__ = '0.0.12'
+__author__ = 'John Pickerill <john.pickerill@landregistry.gov.uk>'
+__all__ = ['Esearch', 'Dsl',  'ResourceNotFoundError']
