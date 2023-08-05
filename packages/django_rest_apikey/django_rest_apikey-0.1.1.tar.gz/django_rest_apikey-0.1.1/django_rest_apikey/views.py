60818438ac87d7896fe2a3393c9552e9b843f2b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:55 +0000

    Add package django_rest_apikey/0.1.1/django_rest_apikey-0.1.1.tar.gz

---
+++ packages/django_rest_apikey/django_rest_apikey-0.1.1.tar.gz/django_rest_apikey-0.1.1/django_rest_apikey/views.py
@@ -0,0 +1,16 @@
+from rest_framework import viewsets
+
+import models
+import serializers
+import validators
+
+class APIKeyViewSet(viewsets.ModelViewSet):
+	serializer_class = serializers.APIKeySerializer
+	lookup_field = 'key'
+	lookup_value_regex = validators.hexadecimal_regex
+	permission_classes = ()
+	filter_backends = ()
+
+	# Only return API keys owned by the user
+	def get_queryset(self):
+		return models.APIKey.objects.filter(user=self.request.user)
