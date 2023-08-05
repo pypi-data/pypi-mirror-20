672459855696fecfe50e049963f424e7b36ae9c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:06 +0000

    Add package ssp-sdk-python/0.2/ssp-sdk-python-0.2.tar.gz

---
+++ packages/ssp-sdk-python/ssp-sdk-python-0.2.tar.gz/ssp-sdk-python-0.2/src/vips.py
@@ -0,0 +1,47 @@
+# Copyright 2017 NeuStar, Inc.All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import json
+from .network import Network
+from .sponsor import Sponsor
+from .vip_id import VipId
+
+class Vips:
+	def __init__(self, connection, base_uri):
+		self.connection = connection
+		self.base_uri = base_uri+"/vips"
+
+	def get(self):
+		"""Get a list of VIPs."""
+		return self.connection.get(self.base_uri)
+
+	def post(self, network_id, ip_index, name, policy_override_enabled=False):
+		"""Create a new VIP.
+
+		Arguments:
+		network_id -- The VIP network ID
+		ip_index --
+		name -- The name of the VIP
+		policy_override_enabled -- A boolean determining whether policy override is enabled
+
+		"""
+		properties = {"networkId": network_id, "ipIndex": ip_index, "name": name, "policyOverrideEnabled": policy_override_enabled}
+		return self.connection.post(self.base_uri, json.dumps(properties))
+
+	def sponsor(self):
+		"""Create a Sponsor object."""
+		return Sponsor(self.connection, self.base_uri)
+
+	def sponsor(self, vip_id):
+		"""Create a VIP ID object."""
+		return Sponsor(self.connection, self.base_uri, vip_id)
