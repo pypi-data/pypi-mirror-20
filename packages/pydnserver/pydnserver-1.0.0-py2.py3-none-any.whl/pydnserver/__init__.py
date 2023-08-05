641ff5af6aaf654b67fd74342ec4fc666c95d153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:29 +0000

    Add package pydnserver/1.0.0/pydnserver-1.0.0-py2.py3-none-any.whl

---
+++ packages/pydnserver/pydnserver-1.0.0-py2.py3-none-any.whl/pydnserver/__init__.py
@@ -0,0 +1,9 @@
+
+# Get module version
+from _metadata import __version__
+
+from dns_server import DNSServer
+from dns_query import DNSQuery, DNSQueryFailed
+
+import dns_lookup
+import dns_forwarders
