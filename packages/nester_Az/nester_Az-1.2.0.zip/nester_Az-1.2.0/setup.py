b4164863ec0b59bad0df3efc4b98cd334d27ef98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:38 +0000

    Add package nester_Az/1.2.0/nester_Az-1.2.0.zip

---
+++ packages/nester_Az/nester_Az-1.2.0.zip/nester_Az-1.2.0/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup #python发布工具导入setup函数
+setup(
+    name='nester_Az',
+    version='1.2.0',
+    py_modules=['nester'],
+    author='hfpython',
+    author_email='hfpython@headfirstlabs.com',
+    url='http://www.headfirstlabs.com',
+    description='test and study',
+    )
+#构建发布：python setup.py sdist
+#发布安装副本：python setup.py install
+#pypi注册：python setup.py register
+#pypi发布：python setup.py sdist upload
+#pipy-url:http://pypi.python.org/
+#pipy用户信息：admin/jimrain956236374
