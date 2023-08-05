1c7d04099bafcdb3c455ddbae51a00fb44c2e2c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:51 +0000

    Add package mountwizzard/1.1.12/mountwizzard-1.1.12.zip

---
+++ packages/mountwizzard/mountwizzard-1.1.12.zip/mountwizzard-1.1.12/Changelog.md
@@ -0,0 +1,243 @@
+# Changelog:
+1.1.12
+- for loading model points file, TSX (TheSkyX Format is also supported)
+
+1.1.11
+- adding first unittests
+- refactoring
+
+1.1.10
+- changing setup.py
+
+1.1.9
+- changing simulation values
+
+1.1.8
+- optimisations
+- adding feature batch model calculation
+
+1.1.7
+- cyclic logs per day
+- correction simulation ascom telescope simulator
+- bugfix waiting for start slewing while modeling
+
+1.1.6
+- correction relays
+- ui improvements
+- test automation
+
+1.1.5
+- ui refreshment
+- change analyse view
+- update storing data
+
+1.1.4
+- ui reshaping and separate windows for points and analyse
+- updating model algorithm for batch calculation
+- updating tooltips with correct indications
+- bugfixing
+
+1.1.3
+- bugfixing
+
+1.1.2
+- refactoring windows classes
+- refactoring analyse classes
+- bugfix barry talked (spelling issue)
+
+1.1.1
+- bugfix solve message say OK, but timeout plate solving happened
+- bugfix in hint calculation in case of binning
+- reshaping logging while modeling
+- complete new analyse part
+- added polar plots for analysing your modeling
+- bugfixing
+
+1.1.0
+- release of tested version
+
+1.0.26
+- check and change ra/dec hints
+
+1.0.25
+- improved storing of coordinate due to bugfix
+- optimized interface for load horizon file
+
+1.0.24
+- bugfix to reported michel-d bug
+
+1.0.23
+- increased precision for model parameter transfer
+- DSLR should run with SGPro beta 2.5.2.8
+
+1.0.22
+- bugfix modeling
+
+1.0.21
+- preparation for batch modeling / remodeling
+
+1.0.20
+- improved storing while keeping images from modeling
+
+1.0.19
+- bugfix in solving
+
+1.0.18
+- optimizing for time delay between imaging and point add in mount
+
+1.0.17
+- bugfix for not connecting stickstation directly
+
+1.0.16
+- changed waiting for start slewing to avoid a race condition
+
+1.0.15
+- setting default installation without dome
+- increasing wait time for slewing to 2.2 s
+
+1.0.14
+- adaption for Time Flexure -> Delay time
+- adding more option for Grid points
+- adding hysterese analyse
+
+1.0.13
+- turning on tracking while imaging in analyse path
+- show dome status in gray, when no dome driver selected
+- checked for PlateSolve2: should run
+- astronomy.net should work
+
+1.0.12
+- correction readme install
+- bugfix model run already modeled points
+
+1.0.11
+- bugfix in driver selection
+
+1.0.10
+- analyse for time dependant deviations of mount
+- implemented showing position and movement of dome if present
+
+1.0.9
+- removed extended logging
+
+1.08
+- bugfixing dome support with Geremia
+
+1.07
+- bugfixes dome support
+
+1.06
+- dome support
+- utc from computer to iso time from mount in fits
+- canceled run could be rerun
+- some refactoring and bugfixes
+
+1.05
+- second fix
+
+1.04
+- fix bug reported from Geremia Forino
+
+## beta Version:
+0.93
+- change distribution to python package format
+- changed Jnow / J2000 conversion to ASCOM / NOVAS library
+
+0.92
+- improvements an refactoring
+
+0.91
+- improved modeling settings / behaviour
+
+0.90
+! settings ar now under /config subdirectory !
+if you want to reuse you config, just copy is there !
+- save / restore window position for next start
+- adding model analysis first step to test
+- added coloring for mount status
+- added modeling support for pointcloud generation
+- added automatic analyses file naming an storing
+- added colored analysis figures
+- many corrections and bugfixes
+
+0.89
+- bugfixing
+
+0.88
+- testing with out camera optimized
+- added ISO settings
+- added setting for tracking on/off after alt/az slew
+- logging level to debug
+
+0.87
+- bugfixing und maintenance
+
+0.86
+- added canceling of modeling task
+- added coloring for running task, so everybody can see, if tasks run
+- implementing logfile (model.log), actual on debug level
+- improved reconnection to mount
+
+0.85
+- transfer mount site data to astropy loc
+- reverse calculation of model point in az/alt (actually removed)
+- generation of model star setups including visualisation
+- updating GUI
+
+0.84
+- refactoring -> SGPro abstraction to separate class
+
+0.83
+- optimizing main loop
+
+0.82
+- started implementing logging function
+- bugfixing for error cases plate solving through SGPro
+- stability improvements
+- optimizing GUI (Moving Control to Pointing Tab)
+
+0.81
+- first running modelling for test
+- refactoring coordinate transformations to astropy (and away from driver)
+
+0.8
+- Command tools should work
+- base modeling works
+- refinement modeling
+- Optimizing for Target RMS works
+
+
+alpha versions:
+
+1.8
+- cleanup and remove dependency from comtypes and moving to dynamic late binding of ASCOm Drivers
+- If Connection goes away, this could no longer be detected
+- no installation of comtypes needed anymore
+
+1.7
+- cleanup and doing the modelling with SGPro run. modeling should be possible on an beta status
+
+1.6
+- complete refactoring and moving towards multi threading of the tool.
+- need to install some more libraries.
+
+1.5
+- adding SGPro support and moving towards modelling feature - still nonfunctional
+
+1.4
+- including astropy for FITS handling and doing the interface to SGPro
+- cleanup, refactoring and readability of code
+
+1.3
+- optimizing UI for switching on / off dual tracking, refraction, unattended flip
+- improving readme for installation
+
+1.2
+- bugfixing refactor Data
+- New Feature: Direct Transfer Refraction Data from Stick to Mount by button press
+
+1.2
+- minor optimisations
+
+1.0
+- initial release
---
+++ packages/mountwizzard/mountwizzard-1.1.12.zip/mountwizzard-1.1.12/mountwizzard/support/analyse.py
@@ -0,0 +1,323 @@
+############################################################
+# -*- coding: utf-8 -*-
+#
+# Python-based Tool for interaction with the 10micron mounts
+# GUI with PyQT5 for python
+# Python  v3.5
+#
+# Michael Würtenberger
+# (c) 2016, 2017
+#
+# Licence APL2.0
+#
+############################################################
+# standard solutions
+import logging
+import os
+import json
+import numpy
+# import for the PyQt5 Framework
+import PyQt5.QtWidgets
+from support.mw_widget import MwWidget
+from support.analyse_dialog_ui import Ui_AnalyseDialog
+# matplotlib
+import matplotlib
+matplotlib.use('Qt5Agg')
+from matplotlib import pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+
+
+class ShowAnalyseData(FigureCanvas):
+
+    def __init__(self, parent=None):
+        self.plt = plt
+        self.fig = self.plt.figure(dpi=75)
+        rect = self.fig.patch
+        rect.set_facecolor((25/256, 25/256, 25/256))
+        self.axes = self.fig.add_subplot(111)
+        self.axes.grid(True, color='white')
+        self.axes.set_facecolor((32/256, 32/256, 32/256))
+        self.axes.tick_params(axis='x', colors='white')
+        self.axes.tick_params(axis='y', colors='white')
+        self.plt.rcParams['toolbar'] = 'None'
+        self.plt.rcParams['axes.titlesize'] = 'large'
+        self.plt.rcParams['axes.labelsize'] = 'medium'
+        self.plt.tight_layout(rect=[0.05, 0.025, 0.95, 0.925])
+        self.compute_initial_figure()
+        FigureCanvas.__init__(self, self.fig)
+        self.setParent(parent)
+        FigureCanvas.updateGeometry(self)
+
+    def compute_initial_figure(self):
+        self.axes.plot(1, 1)
+
+
+# noinspection PyTypeChecker
+def calculateTimeConstant(x_time, y_value):
+    timeconstant = 0
+    print(x_time)
+    x = 0
+    y = 0
+    return timeconstant, x, y
+
+
+class ShowAnalysePopup(MwWidget):
+    logger = logging.getLogger(__name__)
+
+    def __init__(self, uiMain):
+        super(ShowAnalysePopup, self).__init__()
+
+        self.uiMain = uiMain
+        self.showStatus = False
+        self.scaleRA = 10
+        self.scaleDEC = 10
+        self.scaleError = 10
+        self.data = {}
+        self.analyse = Analyse()
+        self.ui = Ui_AnalyseDialog()
+        self.ui.setupUi(self)
+        self.initUI()
+        self.ui.windowTitle.setPalette(self.palette)
+        self.ui.btn_selectClose.clicked.connect(self.hideAnalyseWindow)
+        self.ui.scalePlotDEC.valueChanged.connect(self.getData)
+        self.ui.scalePlotRA.valueChanged.connect(self.getData)
+        self.ui.scalePlotError.valueChanged.connect(self.getData)
+        self.ui.btn_selectDecError.clicked.connect(self.showDecError)
+        self.ui.btn_selectDecErrorDeviation.clicked.connect(self.showDecErrorDeviation)
+        self.ui.btn_selectRaError.clicked.connect(self.showRaError)
+        self.ui.btn_selectRaErrorDeviation.clicked.connect(self.showRaErrorDeviation)
+        self.ui.btn_selectDecErrorAltitude.clicked.connect(self.showDecErrorAltitude)
+        self.ui.btn_selectRaErrorAltitude.clicked.connect(self.showRaErrorAltitude)
+        self.ui.btn_selectDecErrorAzimuth.clicked.connect(self.showDecErrorAzimuth)
+        self.ui.btn_selectRaErrorAzimuth.clicked.connect(self.showRaErrorAzimuth)
+        self.ui.btn_selectModelPointPolar.clicked.connect(self.showModelPointPolar)
+        self.ui.btn_selectModelPointErrorPolar.clicked.connect(self.showModelPointErrorPolar)
+        helper = PyQt5.QtWidgets.QVBoxLayout(self.ui.plot)
+        self.plotWidget = ShowAnalyseData(self.ui.plot)
+        helper.addWidget(self.plotWidget)
+        self.show()
+        self.setVisible(False)
+
+    def getData(self):
+        filenameData = self.uiMain.le_analyseFileName.text()
+        self.scaleRA = self.ui.scalePlotRA.value()
+        self.scaleDEC = self.ui.scalePlotDEC.value()
+        self.scaleError = self.ui.scalePlotError.value()
+        self.data = self.analyse.loadData(filenameData)
+        if len(self.data) > 0:
+            self.data = self.analyse.prepareData(self.data, self.scaleRA, self.scaleDEC)
+
+    def setFigure(self, projection=None):
+        self.plotWidget.plt.clf()
+        self.plotWidget.axes = self.plotWidget.fig.add_subplot(111, projection=projection)
+        self.plotWidget.axes.grid(True, color='gray')
+        self.plotWidget.axes.set_facecolor((32/256, 32/256, 32/256))
+        self.plotWidget.axes.tick_params(axis='x', colors='white')
+        self.plotWidget.axes.tick_params(axis='y', colors='white')
+        if projection:
+            self.plotWidget.plt.tight_layout(rect=[0.05, 0.025, 0.95, 0.925])
+        else:
+            self.plotWidget.plt.tight_layout(rect=[0.05, 0.025, 0.95, 0.925])
+
+    def hideAnalyseWindow(self):
+        self.showStatus = False
+        self.setVisible(False)
+
+    def showDecError(self):
+        if len(self.data) == 0:
+            return
+        self.setFigure()
+        self.plotWidget.plt.xlabel('Number of Model Point', color='white')                                                  # x axis
+        self.plotWidget.plt.ylabel('DEC Error (arcsec)', color='white')                                                     # y axis
+        self.plotWidget.plt.title('DEC Error over Modeling\n ', color='white')                                              # title
+        self.plotWidget.plt.axis([0, len(self.data['index']), -self.scaleDEC, self.scaleDEC])                               # defining the scaling of the plot
+        self.plotWidget.plt.plot(self.data['index'], self.data['decError'], color='black')                                  # Basic Data
+        colors = numpy.asarray(['blue' if x > 180 else 'green' for x in self.data['azimuth']])
+        self.plotWidget.plt.scatter(self.data['index'], self.data['decError'], c=colors, s=50)
+        self.plotWidget.draw()                                                                                              # put the plot in the widget
+
+    def showDecErrorDeviation(self):
+        if len(self.data) == 0:
+            return
+        #timeconstant, x, y = calculateTimeConstant(self.data['sidereal_time'], self.data['decError'])
+        self.setFigure()
+        self.plotWidget.plt.xlabel('Number of Model Point', color='white')                                                  # x axis
+        self.plotWidget.plt.ylabel('DEC Error(arcsec)', color='white')                                                      # y axis
+        self.plotWidget.plt.title('DEC Error referenced to 0 over Modeling\n ', color='white')                                              # title
+        self.plotWidget.plt.axis([0, len(self.data['index']), -self.scaleDEC, self.scaleDEC])                               # defining the scaling of the plot
+        decErrorDeviation = numpy.asarray(self.data['decError'])
+        self.plotWidget.plt.plot(self.data['index'], decErrorDeviation - decErrorDeviation[0], color='black')               # Basic Data
+        colors = numpy.asarray(['blue' if x > 180 else 'green' for x in self.data['azimuth']])
+        self.plotWidget.plt.scatter(self.data['index'], decErrorDeviation - decErrorDeviation[0], c=colors, s=50)
+        self.plotWidget.draw()                                                                                              # put the plot in the widget
+
+    def showRaError(self):
+        self.setFigure()
+        self.plotWidget.plt.xlabel('Number of Model Point', color='white')
+        self.plotWidget.plt.ylabel('RA Error (arcsec)', color='white')
+        self.plotWidget.plt.title('RA Error over Modeling\n ', color='white')
+        self.plotWidget.plt.axis([0, len(self.data['index']), -self.scaleRA, self.scaleRA])
+        self.plotWidget.plt.plot(self.data['index'], self.data['raError'], color='black')                                   # Basic Data
+        colors = numpy.asarray(['blue' if x > 180 else 'green' for x in self.data['azimuth']])
+        self.plotWidget.plt.scatter(self.data['index'], self.data['raError'], c=colors, s=50)
+        self.plotWidget.draw()
+
+    def showRaErrorDeviation(self):
+        self.setFigure()
+        self.plotWidget.plt.xlabel('Number of Model Point', color='white')
+        self.plotWidget.plt.ylabel('RA Error', color='white')
+        self.plotWidget.plt.title('RA Error referenced to 0 over Modeling\n ', color='white')
+        self.plotWidget.plt.axis([0, len(self.data['index']), -self.scaleRA, self.scaleRA])
+        raErrorDeviation = numpy.asarray(self.data['raError'])
+        self.plotWidget.plt.plot(self.data['index'], raErrorDeviation - raErrorDeviation[0], color='black')                 # Basic Data
+        colors = numpy.asarray(['blue' if x > 180 else 'green' for x in self.data['azimuth']])
+        self.plotWidget.plt.scatter(self.data['index'], raErrorDeviation - raErrorDeviation[0], c=colors, s=50)
+        self.plotWidget.draw()
+
+    def showDecErrorAltitude(self):
+        self.setFigure()
+        self.plotWidget.plt.xlabel('Altitude', color='white')
+        self.plotWidget.plt.ylabel('DEC Error (arcsec)', color='white')
+        self.plotWidget.plt.title('DEC Error over Altitude\n ', color='white')
+        self.plotWidget.plt.axis([0, 90, -self.scaleDEC, self.scaleDEC])
+        self.plotWidget.plt.plot(self.data['altitude'], self.data['decError'], color='black')
+        colors = numpy.asarray(['blue' if x > 180 else 'green' for x in self.data['azimuth']])
+        self.plotWidget.plt.scatter(self.data['altitude'], self.data['decError'], c=colors, s=50)
+        self.plotWidget.draw()
+
+    def showRaErrorAltitude(self):
+        self.setFigure()
+        self.plotWidget.plt.xlabel('Altitude', color='white')
+        self.plotWidget.plt.ylabel('RA Error (arcsec)', color='white')
+        self.plotWidget.plt.title('RA Error over Altitude\n ', color='white')
+        self.plotWidget.plt.axis([0, 90, -self.scaleRA, self.scaleRA])
+        self.plotWidget.plt.plot(self.data['altitude'], self.data['raError'], color='black')
+        colors = numpy.asarray(['blue' if x > 180 else 'green' for x in self.data['azimuth']])
+        self.plotWidget.plt.scatter(self.data['altitude'], self.data['raError'], c=colors, s=50)
+        self.plotWidget.draw()
+
+    def showDecErrorAzimuth(self):
+        self.setFigure()
+        self.plotWidget.plt.xlabel('Altitude', color='white')
+        self.plotWidget.plt.ylabel('DEC Error (arcsec)', color='white')
+        self.plotWidget.plt.title('DEC Error over Azimuth\n ', color='white')
+        self.plotWidget.plt.axis([0, 360, -self.scaleDEC, self.scaleDEC])
+        self.plotWidget.plt.plot(self.data['azimuth'], self.data['decError'], color='black')
+        colors = numpy.asarray(['blue' if x > 180 else 'green' for x in self.data['azimuth']])
+        self.plotWidget.plt.scatter(self.data['azimuth'], self.data['decError'], c=colors, s=50)
+        self.plotWidget.draw()
+
+    def showRaErrorAzimuth(self):
+        self.setFigure()
+        self.plotWidget.plt.xlabel('Altitude', color='white')
+        self.plotWidget.plt.ylabel('RA Error (arcsec)', color='white')
+        self.plotWidget.plt.title('RA Error over Azimuth\n ', color='white')
+        self.plotWidget.plt.axis([0, 360, -self.scaleRA, self.scaleRA])
+        self.plotWidget.plt.plot(self.data['azimuth'], self.data['raError'], color='black')
+        colors = numpy.asarray(['blue' if x > 180 else 'green' for x in self.data['azimuth']])
+        self.plotWidget.plt.scatter(self.data['azimuth'], self.data['raError'], c=colors, s=50)
+        self.plotWidget.draw()
+
+    def showModelPointPolar(self):
+        self.setFigure('polar')
+        self.plotWidget.axes.set_theta_zero_location('N')
+        self.plotWidget.axes.set_theta_direction(-1)
+        self.plotWidget.axes.set_yticks(range(0, 90, 10))
+        yLabel = ['', '80', '', '60', '', '40', '', '20', '', '0']
+        self.plotWidget.axes.set_yticklabels(yLabel, color='white')
+        self.plotWidget.plt.title('Model Points\n ', color='white')
+        azimuth = numpy.asarray(self.data['azimuth'])
+        altitude = numpy.asarray(self.data['altitude'])
+        colors = numpy.asarray(['blue' if x > 180 else 'green' for x in self.data['azimuth']])
+        self.plotWidget.plt.plot(azimuth / 180.0 * 3.141593, 90 - altitude, color='black')
+        self.plotWidget.plt.scatter(azimuth / 180.0 * 3.141593, 90 - altitude, c=colors, s=50)
+        self.plotWidget.axes.set_rmax(90)
+        self.plotWidget.axes.set_rmin(0)
+        self.plotWidget.draw()
+
+    def showModelPointErrorPolar(self):
+        self.setFigure('polar')
+        self.plotWidget.axes.set_theta_zero_location('N')
+        self.plotWidget.axes.set_theta_direction(-1)
+        self.plotWidget.axes.set_yticks(range(0, 90, 10))
+        yLabel = ['', '80', '', '60', '', '40', '', '20', '', '0']
+        self.plotWidget.axes.set_yticklabels(yLabel, color='white')
+        self.plotWidget.plt.title('Model Points Error\n ', color='white')
+        azimuth = numpy.asarray(self.data['azimuth'])
+        altitude = numpy.asarray(self.data['altitude'])
+        self.plotWidget.plt.plot(azimuth / 180.0 * 3.141593, 90 - altitude, color='black')
+        cm = plt.cm.get_cmap('RdYlGn_r')
+        colors = numpy.asarray(self.data['modelError'])
+        area = colors * 100 / self.scaleError + 20
+        theta = azimuth / 180.0 * 3.141593
+        r = 90 - altitude
+        scatter = self.plotWidget.plt.scatter(theta, r, c=colors, vmin=1, vmax=self.scaleError, s=area, cmap=cm)
+        scatter.set_alpha(0.75)
+        colorbar = self.plotWidget.plt.colorbar(scatter)
+        colorbar.set_label('Error [arcsec]', color='white')
+        plt.setp(plt.getp(colorbar.ax.axes, 'yticklabels'), color='white')
+        self.plotWidget.axes.set_rmax(90)
+        self.plotWidget.axes.set_rmin(0)
+        self.plotWidget.draw()
+
+
+class Analyse:
+    logger = logging.getLogger(__name__)
+
+    def __init__(self):
+        self.filepath = '/analysedata'
+
+    def saveData(self, dataProcess, name):                                                                                  # saving data from list to file
+        filenameData = os.getcwd() + self.filepath + '/' + name                                                             # built the filename
+        try:                                                                                                                # write data to disk
+            outfile = open(filenameData, 'w')                                                                               # open for write
+            json.dump(dataProcess, outfile)
+            outfile.close()                                                                                                 # close the save file
+        except Exception as e:                                                                                              # Exception handling
+            self.logger.error('saveData       -> analyse data file {0}, Error : {1}'.format(filenameData, e))
+            return
+
+    def loadData(self, name):                                                                                               # loading data
+        filenameData = os.getcwd() + self.filepath + '/' + name                                                             # generate filename
+        try:                                                                                                                # try to read the file
+            infileData = open(filenameData, 'r')
+            dataJson = json.load(infileData)
+            infileData.close()                                                                                                  # close
+        except Exception as e:                                                                                              # exception handling
+            self.logger.error('loadData       ->  analyse data file {0}, Error : {1}'.format(filenameData, e))
+            return {}                                                                                                       # loading doesn't work
+        resultData = dict()
+        for timestepdict in dataJson:
+            for (keyData, valueData) in timestepdict.items():
+                if keyData in resultData:
+                    resultData[keyData].append(valueData)
+                else:
+                    resultData[keyData] = [valueData]
+        return resultData                                                                                                   # successful loading
+
+    @staticmethod
+    def prepareData(dataProcess, scaleRA, scaleDEC):
+        if len(dataProcess) == 0:                                                                                           # in case no data loaded ->
+            return dataProcess                                                                                              # quit
+        dataProcess['raError'] = [scaleRA if x > scaleRA else x for x in dataProcess['raError']]
+        dataProcess['raError'] = [-scaleRA if x < -scaleRA else x for x in dataProcess['raError']]
+        dataProcess['decError'] = [scaleDEC if x > scaleDEC else x for x in dataProcess['decError']]
+        dataProcess['decError'] = [-scaleDEC if x < -scaleDEC else x for x in dataProcess['decError']]
+        return dataProcess
+
+if __name__ == "__main__":
+    filename = 'C:/Users/mw/Projects/mountwizzard/mountwizzard/analysedata/2017-01-22-19-46-07_test.dat'
+    infile = open(filename, 'r')
+    data = json.load(infile)
+    infile.close()  # close
+    result = dict()
+    for timedict in data:
+        for (key, value) in timedict.items():
+            if key in result:
+                pass
+                result[key].append(value)
+            else:
+                result[key] = [value]
+    print(result['ra'])
+    b = [5 if x > 5 else x for x in result['ra']]
+    print(b)
