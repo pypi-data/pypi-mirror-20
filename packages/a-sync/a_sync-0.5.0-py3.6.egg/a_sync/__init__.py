f47a85ab707bbaa776365312006f680f0ab0f154
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:26 +0000

    Add package a-sync/0.5.0/a_sync-0.5.0-py3.6.egg

---
+++ packages/a-sync/a_sync-0.5.0-py3.6.egg/a_sync/__init__.py
@@ -0,0 +1,31 @@
+"""A module exploring some python asyncio interactions."""
+
+
+# [ Imports ]
+from a_sync.a_sync import (
+    ExitOption,
+    Parallel,
+    Serial,
+    ThreadExecutor,
+    a_input,
+    block,
+    idle_event_loop,
+    queue_background_thread,
+    run,
+    to_async,
+    to_blocking,
+)
+
+
+# silence pyflakes
+assert idle_event_loop
+assert to_async
+assert to_blocking
+assert queue_background_thread
+assert run
+assert block
+assert a_input
+assert Parallel
+assert Serial
+assert ThreadExecutor
+assert ExitOption
