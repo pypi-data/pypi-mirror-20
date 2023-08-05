47424316d17e35ba921011e679e8c5cbf07982d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:23 +0000

    Add package dollars/0.2.0/dollars-0.2.0.tar.gz

---
+++ packages/dollars/dollars-0.2.0.tar.gz/dollars-0.2.0/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from setuptools import find_packages, setup
+
+
+with open('requirements.txt') as requirements:
+    required = requirements.read().splitlines()
+
+with open('README.rst') as readme:
+    long_description = readme.read()
+
+
+setup(
+    name='dollars',
+    version='0.2.0',
+    description='Converts everything to dollars using up to date rates for '
+                'free',
+    long_description=long_description,
+    url='http://github.com/aerupt/dollars',
+    author='Lasse Schuirmann',
+    author_email='lasse.schuirmann@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=required,
+)
