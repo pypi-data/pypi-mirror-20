d8b033bc19e8082cbbdb9ebdf878a0f9cc6aab84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:02 +0000

    Add package dash-core-components/0.2.5/dash_core_components-0.2.5.tar.gz

---
+++ packages/dash-core-components/dash_core_components-0.2.5.tar.gz/dash_core_components-0.2.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='dash_core_components',
+    version='0.2.5',
+    author='Chris Parmer',
+    author_email='chris@plot.ly',
+    packages=['dash_core_components'],
+    include_package_data=True,
+    license='MIT',
+    description='Dash UI core component suite',
+    install_requires=['flask', 'plotly', 'flask-cors', 'dash.ly']
+)
