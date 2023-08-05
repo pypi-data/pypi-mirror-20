73bbd190b66f5328a570593655b573ae7c2e1536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:24 +0000

    Add package SchemaFactory/1.7.7/SchemaFactory-1.7.7.tar.gz

---
+++ packages/SchemaFactory/SchemaFactory-1.7.7.tar.gz/SchemaFactory-1.7.7/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+from schema_factory import __version__
+
+setup(
+    name='SchemaFactory',
+    version=__version__,
+    packages=['schema_factory'],
+    url='https://github.com/agile4you/SchemaFactory',
+    license='GLPv3',
+    author='Papavassiliou Vassilis',
+    author_email='vpapavasil@gmail.com',
+    description='Python schema toolkit'
+)
