7b90afc5d000adaca5df2ed2a5fd9a03163a4f2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:30 +0000

    Add package cleverwrap/0.2.1/cleverwrap-0.2.1.tar.gz

---
+++ packages/cleverwrap/cleverwrap-0.2.1.tar.gz/cleverwrap-0.2.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+          name = 'cleverwrap',
+          packages = ['cleverwrap'],
+          version = '0.2.1',
+          description = 'A wrapper for the official cleverbot.com API',
+          author = 'Andrew Edwards',
+          author_email = 'andrewthomasedwards@gmail.com',
+          url = 'https://github.com/edwardslabs/cleverwrap.py',
+          download_url = 'https://github.com/edwardslabs/cleverwrap.py/tarball/0.2',
+          keywords = ['cleverbot', 'wrapper', 'clever'],
+          classifiers = [],
+)
