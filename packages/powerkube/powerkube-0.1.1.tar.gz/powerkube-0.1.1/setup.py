c2d393756b535bc8984b30d92a85e9325b8802e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:55 +0000

    Add package powerkube/0.1.1/powerkube-0.1.1.tar.gz

---
+++ packages/powerkube/powerkube-0.1.1.tar.gz/powerkube-0.1.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+INSTALL_REQUIRES = [
+    'kubernetes-py',
+    'powerline-status',
+    ]
+
+# Heavily inspired by the powerline-kubernetes library written by Vincent De Smet
+# (vincent.drl@gmail.com) and located at https://github.com/so0k/powerline-kubernetes
+setup(
+    name='powerkube',
+    version='0.1.1',
+    description='A powerline segment to show kubernetes context items, with toggling and alert color functionality',
+    author='Zach Marine',
+    author_email='zcmarine@gmail.com',
+    url='https://github.com/zcmarine/powerkube',
+    download_url='https://github.com/zcmarine/powerkube/tarball/0.1',
+    packages=['powerkube'],
+    install_requires=INSTALL_REQUIRES,
+    license='Copyright',
+    keywords='powerline kubernetes k8s context segment',
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2 :: Only',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Terminals'
+    ]
+)
