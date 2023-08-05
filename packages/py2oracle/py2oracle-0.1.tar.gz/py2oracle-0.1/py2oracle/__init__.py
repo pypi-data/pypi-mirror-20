c9890039fef4467a66346e329bf28b90a3efa53c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:14 +0000

    Add package py2oracle/0.1/py2oracle-0.1.tar.gz

---
+++ packages/py2oracle/py2oracle-0.1.tar.gz/py2oracle-0.1/py2oracle/__init__.py
@@ -0,0 +1,9 @@
+'''
+Ver 20160721 by Jian: init version to test conn to oracle and teradata as "tstJDB.py" under C:\Users\daij12\Documents\Coldz\py\
+Ver 20170216.1 by Jian: recap, and branch oracle to this script as jaydebe2Oracle.py
+assumptions: py2.X, jpype, jaydebeapi
+Ver 20170216.2 by Jian: use pandas to convert query result to data frame
+assumptions: pandas
+Ver 20170216.3 by Jian: packaging exercise
+'''
+
---
+++ packages/py2oracle/py2oracle-0.1.tar.gz/py2oracle-0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+def readme():
+        with open('README.rst') as f:
+                return f.read()
+
+
+setup(
+        name='py2oracle',
+        version='0.1',
+        description='use py to read Oracle DB',
+        #long_description='local test package',
+        long_description=readme(),
+        classifiers=[
+                'Development Status :: 3 - Alpha',
+                'License :: OSI Approved :: MIT License',
+                'Programming Language :: Python :: 2.7'
+        ],
+        keywords='oracle db query',
+        url='http://rkalva89828.kau.roche.com/gpa/py2oracle',
+        author='Jian Dai',
+        author_email='daij12@gene.com',
+        license='MIT',
+        packages=['py2oracle'],
+        install_requires=[
+                'JayDeBeApi',
+                'JPype1'
+        ],
+        dependency_links=[], #Packages Not On PyPI
+        include_package_data=True,
+	package_data={'jar':['jar/ojdbc6.jar']},
+        test_suite='nose.collector',
+        tests_require=['nose'],
+        zip_safe=False)
