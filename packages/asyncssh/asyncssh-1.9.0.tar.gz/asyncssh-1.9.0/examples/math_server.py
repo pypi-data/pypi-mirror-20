0913bf66d2268d24f86d1cf5bd007d2f7bb7508f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:31 +0000

    Add package asyncssh/1.9.0/asyncssh-1.9.0.tar.gz

---
+++ packages/asyncssh/asyncssh-1.9.0.tar.gz/asyncssh-1.9.0/examples/math_server.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3.5
+#
+# Copyright (c) 2013-2016 by Ron Frederick <ronf@timeheart.net>.
+# All rights reserved.
+#
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License v1.0 which accompanies this
+# distribution and is available at:
+#
+#     http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#     Ron Frederick - initial implementation, API, and documentation
+
+# To run this program, the file ``ssh_host_key`` must exist with an SSH
+# private key in it to use as a server host key. An SSH host certificate
+# can optionally be provided in the file ``ssh_host_key-cert.pub``.
+#
+# The file ``ssh_user_ca`` must exist with a cert-authority entry of
+# the certificate authority which can sign valid client certificates.
+
+import asyncio, asyncssh, sys
+
+async def handle_session(stdin, stdout, stderr):
+    stdout.write('Enter numbers one per line, or EOF when done:\n')
+
+    total = 0
+
+    try:
+        async for line in stdin:
+            line = line.rstrip('\n')
+            if line:
+                try:
+                    total += int(line)
+                except ValueError:
+                    stderr.write('Invalid number: %s\n' % line)
+    except asyncssh.BreakReceived:
+        pass
+
+    stdout.write('Total = %s\n' % total)
+    stdout.channel.exit(0)
+
+async def start_server():
+    await asyncssh.listen('', 8022, server_host_keys=['ssh_host_key'],
+                          authorized_client_keys='ssh_user_ca',
+                          session_factory=handle_session)
+
+loop = asyncio.get_event_loop()
+
+try:
+    loop.run_until_complete(start_server())
+except (OSError, asyncssh.Error) as exc:
+    sys.exit('Error starting server: ' + str(exc))
+
+loop.run_forever()
---
+++ packages/asyncssh/asyncssh-1.9.0.tar.gz/asyncssh-1.9.0/examples/show_environment.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3.5
+#
+# Copyright (c) 2013-2016 by Ron Frederick <ronf@timeheart.net>.
+# All rights reserved.
+#
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License v1.0 which accompanies this
+# distribution and is available at:
+#
+#     http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#     Ron Frederick - initial implementation, API, and documentation
+
+# To run this program, the file ``ssh_host_key`` must exist with an SSH
+# private key in it to use as a server host key. An SSH host certificate
+# can optionally be provided in the file ``ssh_host_key-cert.pub``.
+#
+# The file ``ssh_user_ca`` must exist with a cert-authority entry of
+# the certificate authority which can sign valid client certificates.
+
+import asyncio, asyncssh, sys
+
+async def handle_session(stdin, stdout, stderr):
+    env = stdout.channel.get_environment()
+    if env:
+        keywidth = max(map(len, env.keys()))+1
+        stdout.write('Environment:\n')
+        for key, value in env.items():
+            stdout.write('  %-*s %s\n' % (keywidth, key+':', value))
+        stdout.channel.exit(0)
+    else:
+        stderr.write('No environment sent.\n')
+        stdout.channel.exit(1)
+
+async def start_server():
+    await asyncssh.listen('', 8022, server_host_keys=['ssh_host_key'],
+                          authorized_client_keys='ssh_user_ca',
+                          session_factory=handle_session)
+
+loop = asyncio.get_event_loop()
+
+try:
+    loop.run_until_complete(start_server())
+except (OSError, asyncssh.Error) as exc:
+    sys.exit('Error starting server: ' + str(exc))
+
+loop.run_forever()
