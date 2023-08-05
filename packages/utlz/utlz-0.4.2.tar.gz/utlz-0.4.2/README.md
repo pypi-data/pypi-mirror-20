42fa2f3da6e9f00b08755a88ab7e4093bd537d51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:35 +0000

    Add package utlz/0.4.2/utlz-0.4.2.tar.gz

---
+++ packages/utlz/utlz-0.4.2.tar.gz/utlz-0.4.2/README.md
@@ -0,0 +1,10 @@
+# utlz
+
+A Python utils library
+
+## Installation
+
+Install latest version of `utlz` from pypi (https://pypi.python.org/pypi/utlz):
+```shell
+pip install utlz
+```
---
+++ packages/utlz/utlz-0.4.2.tar.gz/utlz-0.4.2/setup.py
@@ -0,0 +1,49 @@
+"""A python utils library
+
+* https://github.com/theno/utlz
+* https://pypi.python.org/pypi/utlz
+"""
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+description = 'A python utils library'
+long_description = description
+this_dir = path.abspath(path.dirname(__file__))
+try:
+    import pypandoc
+    long_description = pypandoc.convert(path.join(this_dir, 'README.md'), 'rst')
+except(IOError, ImportError):
+    with open(path.join(this_dir, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+
+setup(
+    name='utlz',
+    version='0.4.2',
+    description=description,
+    long_description=long_description,
+    url='https://github.com/theno/utlz',
+    author='Theodor Nolte',
+    author_email='utlz@theno.eu',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='python development utilities library',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    data_files=[
+        ('', ['README.md']),
+    ],
+)
