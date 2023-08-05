5730b3e626ada45edbd359309399c5a8b084f3e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:49 +0000

    Add package segment_source/0.9.5/segment_source-0.9.5.tar.gz

---
+++ packages/segment_source/segment_source-0.9.5.tar.gz/segment_source-0.9.5/segment_source/stats.py
@@ -0,0 +1,26 @@
+import logging
+
+from datetime import datetime
+from collections import Counter
+
+class Stats(object):
+
+    def __init__(self, logger=logging.getLogger()):
+        """Creates a new stats object"""
+        self.calls = Counter()
+        self.start_time = datetime.now()
+        self.last_touched = datetime.now()
+        self.log = logger
+
+    def incr(self, call):
+        """Increments a call and indicates that there was data sent recently"""
+        self.calls[call] += 1
+        self.last_touched = datetime.now()
+
+    def log(self):
+        """Logs out the current stats"""
+        log = self.log
+        now = datetime.now()
+
+        for call, count in enumerate(self.calls):
+            log.info("source_client: %s calls: %s, (%s/s)", call, count, count / (now-self.start_time).seconds)
