01adf7cb5b0bae53b1178c0c6be73d463e456cfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:05:42 +0000

    Add package media-transporter/0.1.2/media-transporter-0.1.2.tar.gz

---
+++ packages/media-transporter/media-transporter-0.1.2.tar.gz/media-transporter-0.1.2/media_transporter/config.py
@@ -0,0 +1,12 @@
+safe_capacity_percentage = 100
+media_shares = "/Volumes/Plex/Media,/Users/scoward/Desktop"
+
+logfile = "transmission.log"
+download_path = "~/Downloads"
+unrar_path = "/usr/local/bin/unrar"
+
+regex_movie = "^(.+).(\d{4}).+(240p|480p|720p|1080p|2160p).+$"
+regex_tv = "^(.+).(S(\d+)E(\d+)).+$"
+share_tv_root_path = "%s/TV/%s"
+share_tv_season_path = "%s/TV/%s/Season %s"
+share_movie_root_path = "%s/Movies/%s"
