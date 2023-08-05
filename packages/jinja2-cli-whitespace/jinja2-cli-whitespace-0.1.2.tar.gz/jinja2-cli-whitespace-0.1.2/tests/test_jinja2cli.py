3b1486d91b1f612e58840c18aff8bdac35aa6764
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:15 +0000

    Add package jinja2-cli-whitespace/0.1.2/jinja2-cli-whitespace-0.1.2.tar.gz

---
+++ packages/jinja2-cli-whitespace/jinja2-cli-whitespace-0.1.2.tar.gz/jinja2-cli-whitespace-0.1.2/tests/test_jinja2cli.py
@@ -0,0 +1,24 @@
+import os
+from jinja2cli import cli
+
+# change dir to tests directory to make relative paths possible
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+
+def test_relative_path():
+    path = "./files/template.j2"
+
+    output = cli.render(path, {"title": "Test"}, [])
+    if isinstance(output, cli.binary_type):
+        output = output.decode('utf-8')
+    assert output == "Test"
+
+
+def test_absolute_path():
+    absolute_base_path = os.path.dirname(os.path.realpath(__file__))
+    path = os.path.join(absolute_base_path, "files", "template.j2")
+
+    output = cli.render(path, {"title": "Test"}, [])
+    if isinstance(output, cli.binary_type):
+        output = output.decode('utf-8')
+    assert output == "Test"
