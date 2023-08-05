bf56e479c3a2b2fd19d96bcd29357f53ceabdac9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:38 +0000

    Add package WeixunSDKCore/0.1.20170213/WeixunSDKCore-0.1.20170213.tar.gz

---
+++ packages/WeixunSDKCore/WeixunSDKCore-0.1.20170213.tar.gz/WeixunSDKCore-0.1.20170213/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+PACKAGE = "weixunsdkcore"
+NAME = "WeixunSDKCore"
+DESCRIPTION = "WeixunSDKCore"
+AUTHOR = "Maple Liu"
+AUTHOR_EMAIL = "maple.liu@microfastup.com"
+URL = "http://github.com/weixunsdkcore"
+VERSION = __import__(PACKAGE).__version__
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    # long_description=read("README.md"),  
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license="Apache License, Version 2.0",
+    url=URL,
+    packages=["weixunsdkcore"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+)  
