095379fa32cbd53324e721d9d2c1c2049446711e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:17 +0000

    Add package hdfscontents/0.2/hdfscontents-0.2.tar.gz

---
+++ packages/hdfscontents/hdfscontents-0.2.tar.gz/hdfscontents-0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'hdfscontents',
+  packages = ['hdfscontents'], 
+  version = '0.2',
+  description = 'A HDFS contents plugin for Jupyter Notebook',
+  author = 'Jim Dowling',
+  author_email = 'jim@logicalclocks.com',
+  url = 'https://github.com/hopshadoop/hdfscontents', 
+  download_url = 'https://github.com/hopshadoop/hdfscontents/tarball/0.1', 
+  keywords = ['hops', 'hopsworks', 'hdfs', 'jupyter'], # arbitrary keywords
+  classifiers = [],
+)
