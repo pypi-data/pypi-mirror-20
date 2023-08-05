0b546235728835a981b39acc4264f48c04581b1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:08 +0000

    Add package screenutils/0.0.1.6.2/screenutils-0.0.1.6.2.tar.gz

---
+++ packages/screenutils/screenutils-0.0.1.6.2.tar.gz/screenutils-0.0.1.6.2/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='screenutils',
+    version='0.0.1.6.2',
+    packages=['screenutils',],
+    license='GNU Public License >=2 (ask me if you want other)',
+    author="Christophe Narbonne",
+    author_email="@Christophe31",
+    url="http://github.com/Christophe31/screenutils",
+    description =
+    "lib for gnu-screen: creates/close/list/log sessions, injects commands...",
+    classifiers = [
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    long_description=open('README.rst').read(),
+)
+
