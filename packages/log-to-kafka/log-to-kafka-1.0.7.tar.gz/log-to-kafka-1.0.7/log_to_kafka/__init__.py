45e2a375d3967446a3124a9bff6f50070fa7c97b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:52 +0000

    Add package log-to-kafka/1.0.7/log-to-kafka-1.0.7.tar.gz

---
+++ packages/log-to-kafka/log-to-kafka-1.0.7.tar.gz/log-to-kafka-1.0.7/log_to_kafka/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+
+VERSION = '1.0.5'
+
+AUTHOR = "cn"
+
+AUTHOR_EMAIL = "308299269@qq.com"
+
+HOME_PAGE = "https://www.github.com/ShichaoMa/log_to_kafka"
+
+from .logger import LogFactory, Logger, LogObject, KafkaHandler, FixedConcurrentRotatingFileHandler, ConcurrentRotatingFileHandler
+
+__all__ = ["LogFactory", "Logger", "LogObject", "KafkaHandler",
+           "FixedConcurrentRotatingFileHandler", "ConcurrentRotatingFileHandler",
+           "HOME_PAGE", "AUTHOR_EMAIL", "VERSION", "AUTHOR"]
---
+++ packages/log-to-kafka/log-to-kafka-1.0.7.tar.gz/log-to-kafka-1.0.7/setup.py
@@ -0,0 +1,53 @@
+# -*- coding:utf-8 -*-
+import codecs
+import os
+try:
+    from setuptools import setup, find_packages
+except:
+    from distutils.core import setup
+
+VERSION = '1.0.7'
+
+AUTHOR = "cn"
+
+AUTHOR_EMAIL = "308299269@qq.com"
+
+URL = "https://www.github.com/ShichaoMa/log_to_kafka"
+
+
+def read(fname):
+    return codecs.open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+NAME = "log-to-kafka"
+
+DESCRIPTION = "log to kafka"
+
+LONG_DESCRIPTION = read("README.rst")
+
+KEYWORDS = "log kafka"
+
+LICENSE = "MIT"
+
+PACKAGES = find_packages()
+
+setup(
+    name = NAME,
+    version = VERSION,
+    description = DESCRIPTION,
+    long_description = LONG_DESCRIPTION,
+    classifiers = [
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+    keywords = KEYWORDS,
+    author = AUTHOR,
+    author_email = AUTHOR_EMAIL,
+    url = URL,
+    license = LICENSE,
+    packages = PACKAGES,
+    install_requires=["kafka-python==0.9.5", "python-json-logger>=0.1.2", "ConcurrentLogHandler>=0.9.1"],
+    include_package_data=True,
+    zip_safe=True,
+)
