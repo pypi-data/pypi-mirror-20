3891e52d17adcd9117e85636c20fdb32d581ede0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:55 +0000

    Add package pykechain/1.3.0/pykechain-1.3.0-py2.py3-none-any.whl

---
+++ packages/pykechain/pykechain-1.3.0-py2.py3-none-any.whl/pykechain/models/property.py
@@ -0,0 +1,58 @@
+from pykechain.exceptions import APIError
+from pykechain.models import Base
+
+
+class Property(Base):
+    """A virtual object representing a KE-chain property."""
+
+    def __init__(self, json, **kwargs):
+        """Construct a Property from a json object."""
+        super(Property, self).__init__(json, **kwargs)
+
+        self.output = json.get('output')
+
+        self._value = json.get('value')
+
+    @property
+    def value(self):
+        """Data value of a property.
+
+        Setting this value will immediately update the property in KE-chain.
+        """
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        if value != self._value:
+            self._put_value(value)
+            self._value = value
+
+    @property
+    def part(self):
+        """Retrieve the part that holds this Property."""
+        part_id = self._json_data['part']
+
+        return self._client.part(pk=part_id)
+
+    def _put_value(self, value):
+        url = self._client._build_url('property', property_id=self.id)
+
+        r = self._client._request('PUT', url, json={'value': value})
+
+        if r.status_code != 200:  # pragma: no cover
+            raise APIError("Could not update property value")
+
+    @classmethod
+    def create(cls, json, **kwargs):
+        """Create a property based on the json data.
+
+        This method will attach the right class to a property, enabling the use of type-specific methods.
+        """
+        if json.get('property_type') == 'ATTACHMENT_VALUE':
+            from .property_attachment import AttachmentProperty
+            return AttachmentProperty(json, **kwargs)
+        elif json.get('property_type') == 'SINGLE_SELECT_VALUE':
+            from .property_selectlist import SelectListProperty
+            return SelectListProperty(json, **kwargs)
+        else:
+            return Property(json, **kwargs)
