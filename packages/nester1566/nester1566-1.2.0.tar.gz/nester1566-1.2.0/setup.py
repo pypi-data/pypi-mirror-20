da0002e818b2e12543d7c8f489dd6dac34ae6eb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:20 +0000

    Add package nester1566/1.2.0/nester1566-1.2.0.tar.gz

---
+++ packages/nester1566/nester1566-1.2.0.tar.gz/nester1566-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name = 'nester1566',
+		version = '1.2.0',
+		py_modules = ['nester1566'],
+		author = 'tinyke_psy',
+		author_email = 'aaron1879@163.com',
+		url = 'https://www.zhihu.com/people/moheng-esther',
+		description = 'the first step to python',
+	)
