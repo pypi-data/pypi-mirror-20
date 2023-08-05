a4e693b58cb0ec43c06b39b9dd91a609434efab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:34 +0000

    Add package cuppa/0.9.21/cuppa-0.9.21.tar.gz

---
+++ packages/cuppa/cuppa-0.9.21.tar.gz/cuppa-0.9.21/cuppa/methods/build_profile.py
@@ -0,0 +1,41 @@
+
+#          Copyright Jamie Allsop 2011-2016
+# Distributed under the Boost Software License, Version 1.0.
+#    (See accompanying file LICENSE_1_0.txt or copy at
+#          http://www.boost.org/LICENSE_1_0.txt)
+
+#-------------------------------------------------------------------------------
+#   BuildProfileMethod
+#-------------------------------------------------------------------------------
+
+from cuppa.utility.types import is_string
+
+
+class BuildProfileMethod:
+
+    def __call__( self, env, build_profile ):
+        for profile in build_profile:
+            if is_string( profile ):
+                name = profile
+                if name in env['profiles']:
+                    profile = env['profiles'][name]
+            else:
+                name = str( profile )
+
+            env.AppendUnique( BUILD_PROFILE = name )
+            profile( env, env['toolchain'], env['variant'].name() )
+
+
+    @classmethod
+    def add_to_env( cls, cuppa_env ):
+        cuppa_env.add_method( "BuildProfile", cls() )
+
+
+    @classmethod
+    def init_env_for_variant( cls, sconscript_exports ):
+        env = sconscript_exports['env']
+        if env['default_profiles']:
+            env['_pre_sconscript_phase_'] = True
+            env.BuildProfile( env['default_profiles'] )
+            env['_pre_sconscript_phase_'] = False
+
---
+++ packages/cuppa/cuppa-0.9.21.tar.gz/cuppa-0.9.21/cuppa/methods/relative_recursive_glob.py
@@ -0,0 +1,71 @@
+
+#          Copyright Jamie Allsop 2012-2015
+# Distributed under the Boost Software License, Version 1.0.
+#    (See accompanying file LICENSE_1_0.txt or copy at
+#          http://www.boost.org/LICENSE_1_0.txt)
+
+#-------------------------------------------------------------------------------
+#   RelativeRecursiveGlob
+#-------------------------------------------------------------------------------
+import os
+import fnmatch
+import re
+
+import cuppa.recursive_glob
+
+
+
+class RecursiveGlobMethod:
+
+    default = ()
+
+    def __call__( self, env, pattern, start=default, exclude_dirs=default ):
+
+        if start == self.default:
+            start = env['sconscript_dir']
+
+        start = os.path.expanduser( start )
+
+        if not os.path.isabs( start ):
+            start = os.path.join( env['sconscript_dir'], start )
+
+        if exclude_dirs == self.default:
+            exclude_dirs = [ env['download_root'], env['build_root' ] ]
+
+        exclude_dirs_regex = None
+
+        if exclude_dirs:
+            def up_dir( path ):
+                element = next( e for e in path.split(os.path.sep) if e )
+                return element == ".."
+            exclude_dirs = [ re.escape(d) for d in exclude_dirs if not os.path.isabs(d) and not up_dir(d) ]
+            exclude_dirs = "|".join( exclude_dirs )
+            exclude_dirs_regex = re.compile( exclude_dirs )
+
+        matches = cuppa.recursive_glob.glob( start, pattern, exclude_dirs_pattern=exclude_dirs_regex )
+        nodes   = [ env.File( os.path.relpath( match, env['sconscript_dir'] ) ) for match in matches ]
+        return nodes
+
+    @classmethod
+    def add_to_env( cls, cuppa_env ):
+        cuppa_env.add_method( "RecursiveGlob", cls() )
+
+
+
+class GlobFilesMethod:
+
+    def __call__( self, env, pattern ):
+        filenames = []
+        for filename in os.listdir(env['sconscript_dir']):
+            if fnmatch.fnmatch( filename, pattern):
+                filenames.append( filename )
+        nodes = [ env.File(f) for f in filenames ]
+        return nodes
+
+
+    @classmethod
+    def add_to_env( cls, cuppa_env ):
+        cuppa_env.add_method( "GlobFiles", cls() )
+
+
+
