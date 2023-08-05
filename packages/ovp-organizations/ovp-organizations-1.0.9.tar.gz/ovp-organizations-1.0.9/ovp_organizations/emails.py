b1073a7b3eeb61549956039bfef3ffcfab444bde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:38 +0000

    Add package ovp-organizations/1.0.9/ovp-organizations-1.0.9.tar.gz

---
+++ packages/ovp-organizations/ovp-organizations-1.0.9.tar.gz/ovp-organizations-1.0.9/ovp_organizations/emails.py
@@ -0,0 +1,94 @@
+from ovp_core.emails import BaseMail
+
+class OrganizationMail(BaseMail):
+  """
+  This class is responsible for firing emails for organizations
+  """
+  def __init__(self, organization, async_mail=None, override_receiver=False):
+    self.async_mail=async_mail
+
+    receiver = organization.owner.email
+    if override_receiver:
+      receiver = override_receiver
+
+    super(OrganizationMail, self).__init__(receiver, self.async_mail)
+
+  def sendOrganizationCreated(self, context={}):
+    """
+    Sent when organization is created
+    """
+    return self.sendEmail('organizationCreated', 'Your organization was created', context)
+
+  def sendOrganizationPublished(self, context={}):
+    """
+    Sent when organization is published
+    """
+    return self.sendEmail('organizationPublished', 'Your organization was published', context)
+
+  def sendUserInvited(self, context={}):
+    """
+    Sent when user is invited to organization
+    """
+    # invited user email
+    self.__init__(context['invite'].organization, async_mail=self.async_mail, override_receiver=context['invite'].invited.email)
+    self.sendEmail('userInvited-toUser', 'You are invited to an organization', context)
+
+    if context['invite'].organization.owner == context['invite'].invitator:
+      self.__init__(context['invite'].organization, async_mail=self.async_mail, override_receiver=context['invite'].organization.owner.email)
+      self.sendEmail('userInvited-toOwnerInviter', 'You invited a member to an organization you own', context)
+    else:
+      self.__init__(context['invite'].organization, async_mail=self.async_mail, override_receiver=context['invite'].organization.owner.email)
+      self.sendEmail('userInvited-toOwner', 'A member has been invited to your organization', context)
+
+      self.__init__(context['invite'].organization, async_mail=self.async_mail, override_receiver=context['invite'].invitator.email)
+      self.sendEmail('userInvited-toMemberInviter', 'You invited a member to an organization you are part of', context)
+
+  def sendUserInvitationRevoked(self, context={}):
+    """
+    Sent when user is invitation is revoked
+    """
+    # invited user email
+    self.__init__(context['invite'].organization, async_mail=self.async_mail, override_receiver=context['invite'].invited.email)
+    self.sendEmail('userInvitedRevoked-toUser', 'Your invitation to an organization has been revoked', context)
+
+    if context['invite'].organization.owner == context['invite'].invitator:
+      self.__init__(context['invite'].organization, async_mail=self.async_mail, override_receiver=context['invite'].organization.owner.email)
+      self.sendEmail('userInvitedRevoked-toOwnerInviter', 'You have revoked an user invitation', context)
+    else:
+      self.__init__(context['invite'].organization, async_mail=self.async_mail, override_receiver=context['invite'].organization.owner.email)
+      self.sendEmail('userInvitedRevoked-toOwner', 'An invitation to join your organization has been revoked', context)
+
+      self.__init__(context['invite'].organization, async_mail=self.async_mail, override_receiver=context['invite'].invitator.email)
+      self.sendEmail('userInvitedRevoked-toMemberInviter', 'You have revoked an user invitation', context)
+
+
+  def sendUserLeft(self, context={}):
+    """
+    Sent when user leaves organization
+    """
+    self.__init__(context['organization'], async_mail=self.async_mail, override_receiver=context['user'].email)
+    self.sendEmail('userLeft-toUser', 'You have left an organization', context)
+
+    self.__init__(context['organization'], async_mail=self.async_mail, override_receiver=context['organization'].owner.email)
+    self.sendEmail('userLeft-toOwner', 'An user has left an organization you own', context)
+
+
+  def sendUserRemoved(self, context={}):
+    """
+    Sent when user is removed from organization
+    """
+    self.__init__(context['organization'], async_mail=self.async_mail, override_receiver=context['user'].email)
+    self.sendEmail('userRemoved-toUser', 'You have have been removed from an organization', context)
+
+    self.__init__(context['organization'], async_mail=self.async_mail, override_receiver=context['organization'].owner.email)
+    self.sendEmail('userRemoved-toOwner', 'You have removed an user from an organization you own', context)
+
+  def sendUserJoined(self, context={}):
+    """
+    Sent when user joins organization
+    """
+    self.__init__(context['organization'], async_mail=self.async_mail, override_receiver=context['user'].email)
+    self.sendEmail('userJoined-toUser', 'You have joined an organization', context)
+
+    self.__init__(context['organization'], async_mail=self.async_mail, override_receiver=context['organization'].owner.email)
+    self.sendEmail('userJoined-toOwner', 'An user has joined an organization you own', context)
---
+++ packages/ovp-organizations/ovp-organizations-1.0.9.tar.gz/ovp-organizations-1.0.9/ovp_organizations/serializers.py
@@ -0,0 +1,94 @@
+from django.core.exceptions import ValidationError
+
+from ovp_uploads.serializers import UploadedImageSerializer
+
+from ovp_core import validators as core_validators
+from ovp_core.serializers import GoogleAddressSerializer, GoogleAddressCityStateSerializer
+
+from ovp_organizations import models
+from ovp_organizations import validators
+from ovp_organizations.decorators import hide_address
+
+from rest_framework import serializers
+from rest_framework import permissions
+from rest_framework import fields
+from rest_framework.compat import set_many
+from rest_framework.utils import model_meta
+
+
+class OrganizationCreateSerializer(serializers.ModelSerializer):
+  address = GoogleAddressSerializer(
+      validators=[core_validators.address_validate],
+      required=False,
+    )
+
+  class Meta:
+    model = models.Organization
+    fields = ['id', 'slug', 'owner', 'name', 'website', 'facebook_page', 'address', 'details', 'description', 'type', 'image', 'cover', 'hidden_address']
+
+  def create(self, validated_data):
+    # Address
+    address_data = validated_data.pop('address', None)
+    if address_data:
+      address_sr = GoogleAddressSerializer(data=address_data)
+      address = address_sr.create(address_data)
+      validated_data['address'] = address
+
+    # Organization
+    organization = models.Organization.objects.create(**validated_data)
+    return organization
+
+  def update(self, instance, validated_data):
+    address_data = validated_data.pop('address', None)
+
+    # Iterate and save fields as drf default
+    info = model_meta.get_field_info(instance)
+    for attr, value in validated_data.items():
+      if attr in info.relations and info.relations[attr].to_many: # pragma: no cover
+        set_many(instance, attr, value)
+      else:
+        setattr(instance, attr, value)
+
+    # Save related resources
+    if address_data:
+      address_sr = GoogleAddressSerializer(data=address_data)
+      address = address_sr.create(address_data)
+      instance.address = address
+
+    instance.save()
+
+    return instance
+
+class OrganizationSearchSerializer(serializers.ModelSerializer):
+  address = GoogleAddressCityStateSerializer()
+  image = UploadedImageSerializer()
+
+  class Meta:
+    model = models.Organization
+    fields = ['id', 'slug', 'owner', 'name', 'website', 'facebook_page', 'address', 'details', 'description', 'type', 'image']
+
+class OrganizationRetrieveSerializer(serializers.ModelSerializer):
+  address = GoogleAddressSerializer()
+  image = UploadedImageSerializer()
+  cover = UploadedImageSerializer()
+
+  class Meta:
+    model = models.Organization
+    fields = ['slug', 'owner', 'name', 'website', 'facebook_page', 'address', 'details', 'description', 'type', 'image', 'cover', 'published', 'hidden_address']
+
+  @hide_address
+  def to_representation(self, instance):
+    return super(OrganizationRetrieveSerializer, self).to_representation(instance)
+
+
+class OrganizationInviteSerializer(serializers.Serializer):
+  email = fields.EmailField(validators=[validators.invite_email_validator])
+
+  class Meta:
+    fields = ['email']
+
+class MemberRemoveSerializer(serializers.Serializer):
+  email = fields.EmailField(validators=[validators.invite_email_validator])
+
+  class Meta:
+    fields = ['email']
