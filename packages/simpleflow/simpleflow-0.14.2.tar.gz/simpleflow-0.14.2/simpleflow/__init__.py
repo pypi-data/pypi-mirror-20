59e2d3eb6f4bd3de8af63a1800599d746527315f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:33 +0000

    Add package simpleflow/0.14.2/simpleflow-0.14.2.tar.gz

---
+++ packages/simpleflow/simpleflow-0.14.2.tar.gz/simpleflow-0.14.2/simpleflow/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import logging.config
+
+from .activity import Activity  # NOQA
+from .workflow import Workflow  # NOQA
+
+from . import settings
+
+
+__version__ = '0.14.2'
+__author__ = 'Greg Leclercq'
+__license__ = "MIT"
+
+logging.config.dictConfig(settings.base.load()['LOGGING'])
+logger = logging.getLogger(__name__)
