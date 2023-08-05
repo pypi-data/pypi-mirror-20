b8160f796b224cab2b8c437b99cae9477429a44d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:05 +0000

    Add package statically/0.0.15/statically-0.0.15.tar.gz

---
+++ packages/statically/statically-0.0.15.tar.gz/statically-0.0.15/statically/config/config.py
@@ -0,0 +1,27 @@
+host = "https://cdn.rawgit.com/joajfreitas/statically-resources/9803ba37/"
+config = {
+    "public" : {
+
+        "styles" : {
+            "air.css"      : host + "styles/air.css",
+            "retro.css"    : host + "styles/retro.css",
+            "modest.css"   : host + "styles/modest.css",
+            "splendor.css" : host + "styles/splendor.css"
+        },
+
+        "colorschemes" : {
+            "monokai.css"  : host + "colorschemes/monokai.css"
+        },
+    },
+
+    "templates" : {
+        "default.html" : host + "templates/default.html"
+    },
+
+    "content" : {
+        "example.md"   : host + "content/example.md"
+    },
+
+
+    "config.yml"       : host + "config.yml"
+}
