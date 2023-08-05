e6d8168e685fe19d9927cd8fa55f7183ff7896bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:13 +0000

    Add package Orange3/3.3.12/Orange3-3.3.12-cp36-none-win32.whl

---
+++ packages/Orange3/Orange3-3.3.12-cp36-none-win32.whl/Orange/classification/__init__.py
@@ -0,0 +1,16 @@
+from .base_classification import (ModelClassification as Model,
+                                  LearnerClassification as Learner,
+                                  SklModelClassification as SklModel,
+                                  SklLearnerClassification as SklLearner)
+from .knn import *
+from .logistic_regression import *
+from .majority import *
+from .naive_bayes import *
+from .random_forest import *
+from .softmax_regression import *
+from .svm import *
+from .tree import *
+from .simple_tree import *
+from .simple_random_forest import *
+from .elliptic_envelope import *
+from .rules import *
