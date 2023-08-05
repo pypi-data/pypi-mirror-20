4a6a2c81068aa466d77db6c783b26a8d16ecc559
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:07 +0000

    Add package filer2/0.1.4/filer2-0.1.4.tar.gz

---
+++ packages/filer2/filer2-0.1.4.tar.gz/filer2-0.1.4/README.rst
@@ -0,0 +1,22 @@
+=========
+filer2
+=========
+* file edit library for python2
+
+usage
+=========
+
+* from filer2 import Filer
+* list_csv = Filer.read_csv(path)
+
+list-usage
+=========
+
+* read_csv
+* write_csv
+* read_tsv
+* write_tsv
+* read_txt
+* write_txt
+* read_pkl
+* write_pkl
---
+++ packages/filer2/filer2-0.1.4.tar.gz/filer2-0.1.4/setup.py
@@ -0,0 +1,42 @@
+# coding: utf-8
+"""
+setup
+"""
+import setuptools
+
+version = '0.1.4'
+
+setuptools.setup(
+    name='filer2', # パッケージ名
+    version=version, # バージョン
+    description="A simple library for file edition", # 一言で説明
+    # 詳細は http://pypi.python.org/pypi?:action=list_classifiers を参照
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: MacOS X",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+    # キーワードを書いておく
+    keywords='csv, tsv, pkl, read, write',
+    # 作者の名前
+    author='kenchin110100',
+    # 連絡先
+    author_email='kenchin110100@yahoo.co.jp',
+    # GitHubのリポジトリとか
+    url='https://github.com/kenchin110100',
+    # ライセンス
+    license='MIT',
+    packages=setuptools.find_packages(exclude=['examples', 'tests']),
+    include_package_data=True,
+    zip_safe=True,
+    # Pypiのページで表示する説明文(README)
+    long_description='README.rst',
+    install_requires=[],
+)
