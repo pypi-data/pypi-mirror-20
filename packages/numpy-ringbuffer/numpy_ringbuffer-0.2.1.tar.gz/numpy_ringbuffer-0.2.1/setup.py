c7f709eb327b55edf003a08a1b3001e602ef0311
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:58 +0000

    Add package numpy-ringbuffer/0.2.1/numpy_ringbuffer-0.2.1.tar.gz

---
+++ packages/numpy-ringbuffer/numpy_ringbuffer-0.2.1.tar.gz/numpy_ringbuffer-0.2.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+with open('numpy_ringbuffer/__about__.py') as f:
+    exec(f.read())
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    print("No")
+    long_description = open('README.md').read()
+
+setup(
+    name="numpy_ringbuffer",
+    version=__version__,
+    packages=['numpy_ringbuffer'],
+
+    install_requires=["numpy"],
+
+    author="Eric Wieser",
+    author_email="wieser.eric+numpy@gmail.com",
+    description="Ring buffer implementation for numpy",
+    long_description=long_description,
+    license="MIT",
+    keywords=["numpy", "buffer", "ringbuffer", "circular buffer"],
+    url="https://github.com/eric-wieser/numpy_ringbuffer",
+    download_url="https://github.com/eric-wieser/numpy_ringbuffer/tarball/v"+__version__,
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5"
+    ]
+)
