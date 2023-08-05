823048b784dd96936e944f65f8ae520ce40f7a8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:56 +0000

    Add package Tzara---A-Personal-Assistant/1.1.5/Tzara---A-Personal-Assistant-1.1.5.tar.gz

---
+++ packages/Tzara---A-Personal-Assistant/Tzara---A-Personal-Assistant-1.1.5.tar.gz/Tzara---A-Personal-Assistant-1.1.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+from setuptools.command.install import install
+import os
+
+class MyInstall(install):
+    def run(self):
+        install.run(self)
+        path = os.getcwd().replace(" ", "\ ").replace("(","\(").replace(")","\)") + "/bin/"
+        os.system("chmod +x "+path+"startup.sh")
+        os.system("sh "+path+"startup.sh")
+        
+
+setup(name='Tzara---A-Personal-Assistant',
+      version='1.1.5',
+      description='A Virtual Personal Assistant',
+      url='https://github.com/Suman7495/Tzara---A-Personal-Assistant',
+      author='Suman Pal',
+      author_email='suman7495@gmail.com',
+      license='MIT',
+      packages=['tzara'],
+      classifiers=['Development Status :: 4 - Beta',
+      'Programming Language :: Python :: 2.7',            
+      ],
+      scripts=['bin/startup.sh'],
+      cmdclass={'install': MyInstall},
+     )
