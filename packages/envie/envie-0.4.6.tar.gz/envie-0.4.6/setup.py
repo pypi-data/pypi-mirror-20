5ec12a7896d3ee08f26a87215d3d9d823c58d158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:13 +0000

    Add package envie/0.4.6/envie-0.4.6.tar.gz

---
+++ packages/envie/envie-0.4.6.tar.gz/envie-0.4.6/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from setuptools import setup
+from setuptools.command.install import install as _install
+import subprocess
+
+setup(
+    name='envie',
+    version='0.4.6',
+    description="Bash helpers for navigating and managing Python VirtualEnvs.",
+    long_description=open('README.rst').read(),
+    author='Radomir Stevanovic',
+    author_email='radomir.stevanovic@gmail.com',
+    url='https://github.com/randomir/envie',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Unix Shell',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Topic :: Utilities',
+        'Topic :: System :: Shells'
+    ],
+    keywords='virtualenv bash helper closest virtual environment create mkenv destroy rmenv change cdenv',
+    scripts=['envie']
+)
