e7b1438463a2b418254fbfda7f0f61d30373736b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:48 +0000

    Add package ribosome/10.0.4/ribosome-10.0.4.tar.gz

---
+++ packages/ribosome/ribosome-10.0.4.tar.gz/ribosome-10.0.4/ribosome/test/fixtures.py
@@ -0,0 +1,28 @@
+rplugin_template = '''
+import neovim
+import os
+import logging
+from pathlib import Path
+
+from {plugin_module} import {plugin_class}
+
+import amino
+
+amino.development = True
+
+import amino.logging
+from ribosome.logging import ribosome_root_logger
+
+logfile = Path(os.environ['RIBOSOME_LOG_FILE'])
+fmt = os.environ.get('RIBOSOME_FILE_LOG_FMT')
+amino.logging.amino_root_file_logging(level=amino.logging.VERBOSE,
+                                      logfile=logfile,
+                                      fmt=fmt)
+
+
+@neovim.plugin
+class Plugin({plugin_class}):
+    pass
+'''
+
+__all__ = ('rplugin_template',)
