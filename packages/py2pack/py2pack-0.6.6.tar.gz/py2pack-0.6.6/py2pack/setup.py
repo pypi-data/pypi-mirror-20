f757d5744b13945224959a5875eddda8ee3714e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:46 +0000

    Add package py2pack/0.6.6/py2pack-0.6.6.tar.gz

---
+++ packages/py2pack/py2pack-0.6.6.tar.gz/py2pack-0.6.6/py2pack/setup.py
@@ -0,0 +1,100 @@
+# Copyright 2013 Sascha Peilicke
+# Copyright 2016 Thomas Bechtold
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import re
+import subprocess
+from distutils.core import Command
+
+
+class DocCommand(Command):
+    description = "Generate manpage, HTML and PDF documentation"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            subprocess.call(["xsltproc", "--output", "doc/py2pack.html", "/usr/share/xml/docbook/stylesheet/nwalsh/current/html/docbook.xsl", "doc/src/py2pack.xml.in"])
+            subprocess.call(["xsltproc", "--output", "doc/py2pack.1", "/usr/share/xml/docbook/stylesheet/nwalsh/current/manpages/docbook.xsl", "doc/src/py2pack.xml.in"])
+        except:
+            pass
+
+
+class SPDXUpdateCommand(Command):
+    description = "Update SDPX license map"
+    user_options = []
+    LICENSE_FILE = 'py2pack/spdx_license_map.p'
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        # Not part of any requirements, could happen through setup(setup_requires=...)
+        import pickle
+        import lxml.html
+        import requests
+        response = requests.get('https://docs.google.com/spreadsheet/pub?key=0AqPp4y2wyQsbdGQ1V3pRRDg5NEpGVWpubzdRZ0tjUWc')
+        html = lxml.html.fromstring(response.text)
+        licenses = {}
+        for i, tr in enumerate(html.cssselect('table.waffle > tbody > tr')):
+            _, td_new, td_old = tr.getchildren()
+            licenses[td_old.text] = td_new.text
+            # also add the spdx license as key (i.e. key/value "Apache-2.0"->"Apache-2.0")
+            # Otherwise licenses for packages which already have a SPDX compatible license
+            # are not correctly recognized
+            licenses[td_new.text] = td_new.text
+        pickle.dump(licenses, open(SPDXUpdateCommand.LICENSE_FILE, 'wb'))
+
+
+def get_cmdclass():
+    """Dictionary of all distutils commands defined in this module.
+    """
+    return {
+        "spdx_update": SPDXUpdateCommand,
+    }
+
+
+def parse_requirements(requirements_file='requirements.txt'):
+    requirements = []
+    with open(requirements_file, 'r') as f:
+        for line in f:
+            # For the requirements list, we need to inject only the portion
+            # after egg= so that distutils knows the package it's looking for
+            # such as:
+            # -e git://github.com/openstack/nova/master#egg=nova
+            if re.match(r'\s*-e\s+', line):
+                requirements.append(re.sub(r'\s*-e\s+.*#egg=(.*)$', r'\1',
+                                    line))
+            # such as:
+            # http://github.com/openstack/nova/zipball/master#egg=nova
+            elif re.match(r'\s*https?:', line):
+                requirements.append(re.sub(r'\s*https?:.*#egg=(.*)$', r'\1',
+                                    line))
+            # -f lines are for index locations, and don't get used here
+            elif re.match(r'\s*-f\s+', line):
+                pass
+            # -r lines are for including other files, and don't get used here
+            elif re.match(r'\s*-r\s+', line):
+                pass
+            else:
+                requirements.append(line.strip())
+    return requirements
---
+++ packages/py2pack/py2pack-0.6.6.tar.gz/py2pack-0.6.6/setup.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2015, Sascha Peilicke <sascha@peilicke.de>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import py2pack
+import py2pack.setup
+
+
+install_requires = py2pack.setup.parse_requirements("requirements.txt")
+tests_requires = py2pack.setup.parse_requirements("test-requirements.txt")
+
+with open("README.rst", "r") as f:
+    long_description = f.read()
+
+
+setup(
+    name=py2pack.__name__,
+    version=py2pack.__version__,
+    license="Apache-2.0",
+    description=py2pack.__doc__,
+    long_description=long_description,
+    author=py2pack.__author__.rsplit(' ', 1)[0],
+    author_email=py2pack.__author__.rsplit(' ', 1)[1][1:-1],
+    url='http://github.com/openSUSE/py2pack',
+    scripts=['scripts/py2pack'],
+    packages=['py2pack'],
+    package_data={'py2pack': ['templates/*', 'spdx_license_map.p']},
+    data_files=[('share/doc/py2pack', ['AUTHORS', 'LICENSE', 'README.rst']),
+                ('share/doc/py2pack/html', ['doc/py2pack.html']),
+                # ('share/doc/py2pack/pdf', ['doc/py2pack.pdf']),
+                ('man/man1', ['doc/py2pack.1'])],
+    setup_requires=["cssselect", "lxml", "requests"],
+    install_requires=install_requires,
+    cmdclass=py2pack.setup.get_cmdclass(),
+    tests_require=tests_requires,
+    test_suite="nose.collector",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        'Topic :: Software Development :: Code Generators',
+        'Topic :: Software Development :: Pre-processors',
+    ],
+)
