e73891fca0af3b1f5371091e117554af2fbb29de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:45 +0000

    Add package aldryn-style/1.1.4/aldryn-style-1.1.4.tar.gz

---
+++ packages/aldryn-style/aldryn-style-1.1.4.tar.gz/aldryn-style-1.1.4/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+from aldryn_style import __version__
+
+REQUIREMENTS = []
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development',
+]
+
+setup(
+    name='aldryn-style',
+    version=__version__,
+    description='Style Plugin for django CMS',
+    author='Divio AG',
+    author_email='info@divio.ch',
+    url='https://github.com/divio/aldryn-style',
+    packages=find_packages(),
+    license='LICENSE.txt',
+    platforms=['OS Independent'],
+    install_requires=REQUIREMENTS,
+    classifiers=CLASSIFIERS,
+    include_package_data=True,
+    zip_safe=False,
+    long_description=open('README.rst').read(),
+)
