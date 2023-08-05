a993d93cfadb6ccf1bf24f4852a6e4c55eb9d97a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:34 +0000

    Add package tcconfig/0.7.1a0/tcconfig-0.7.1a0-py2.py3-none-any.whl

---
+++ packages/tcconfig/tcconfig-0.7.1a0-py2.py3-none-any.whl/tcconfig/_const.py
@@ -0,0 +1,30 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <gogogo.vm@gmail.com>
+"""
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+
+VERSION = "0.7.1-alpha"
+
+ANYWHERE_NETWORK = "0.0.0.0/0"
+
+
+class Tc(object):
+
+    class Subcommand(object):
+        CLASS = "class"
+        FILTER = "filter"
+        QDISC = "qdisc"
+        SHOW = "show"
+
+    class Param(object):
+        CLASS_ID = "classid"
+        FLOW_ID = "flowid"
+        HANDLE = "handle"
+        NETWORK = "network"
+        PARENT = "parent"
+        PORT = "port"
