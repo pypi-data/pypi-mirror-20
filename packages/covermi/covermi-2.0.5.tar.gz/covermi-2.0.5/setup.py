7b2dda88240d8cf7ed0a348a34b1c8fa98adddf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:42 +0000

    Add package covermi/2.0.5/covermi-2.0.5.tar.gz

---
+++ packages/covermi/covermi-2.0.5.tar.gz/covermi-2.0.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name="covermi",
+    version= "2.0.5",
+    description="Coverage checking for next generation sequencing panels",
+    url="http://github.com/eawilson/covermi",
+    author="Ed Wilson",
+    author_email="edwardadrianwilson@yahoo.co.uk",
+    license="MIT",
+    packages=["covermi"],
+    include_package_data=True,
+    zip_safe=True,
+    entry_points={"console_scripts" : ["covermi=covermi.covermigui:main",
+                                       "covermicompare=covermi.covermicompare:guimain",
+                                       "covermiverifygui=covermi.covermiverifygui:main", 
+                                       "covermiwgs=covermi.covermiwgsgui:main",
+                                       "covermi_make_canonical=covermi.make_canonical:main",
+                                       "bringmiup=covermi.bringmiup:main",
+                                       "cosmic2variants=covermi.cosmic2variants:main",
+                                       "familycheck=covermi.familycheck:main",
+                                       "whosthedaddy=covermi.whosthedaddy:main"]}
+)
+
