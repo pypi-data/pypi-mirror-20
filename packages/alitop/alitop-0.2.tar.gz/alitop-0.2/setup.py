01fa408e58af4fa6ee31f15e99581b26d4863890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:53 +0000

    Add package alitop/0.2/alitop-0.2.tar.gz

---
+++ packages/alitop/alitop-0.2.tar.gz/alitop-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='alitop',
+      version='0.2',
+      description='taobao open platform SDK',
+      url='https://open.taobao.com',
+      author='zhangjinjie',
+      author_email='zhangjinjie@yimian.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
---
+++ packages/alitop/alitop-0.2.tar.gz/alitop-0.2/top/api/rest/MarketingPromotionKfcRequest.py
@@ -0,0 +1,12 @@
+'''
+Created by auto_sdk on 2016.04.13
+'''
+from top.api.base import RestApi
+class MarketingPromotionKfcRequest(RestApi):
+	def __init__(self,domain='gw.api.taobao.com',port=80):
+		RestApi.__init__(self,domain, port)
+		self.promotion_desc = None
+		self.promotion_title = None
+
+	def getapiname(self):
+		return 'taobao.marketing.promotion.kfc'
