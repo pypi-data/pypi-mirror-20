a0ea39f839623b1c72d8dc20b2e0e2afa8c33738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:14 +0000

    Add package jicson/1.0.0/jicson-1.0.0.tar.gz

---
+++ packages/jicson/jicson-1.0.0.tar.gz/jicson-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name    = 'jicson', 
+    version = '1.0.0', 
+    packages=['jicson'],
+    author = 'jspiner',
+    author_email = 'jspiner@naver.com', 
+    url = 'http://caly.io',
+    description = 'Easy ics file parser',
+)
