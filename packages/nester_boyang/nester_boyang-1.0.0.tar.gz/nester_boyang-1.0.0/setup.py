b4ea0cb462104eacb5fc122c167265fcea781814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:10 +0000

    Add package nester_boyang/1.0.0/nester_boyang-1.0.0.tar.gz

---
+++ packages/nester_boyang/nester_boyang-1.0.0.tar.gz/nester_boyang-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    
+    name = 'nester_boyang',
+    version = '1.0.0',
+    py_module = ['nester_boyang'],
+    author = 'boyang',
+    author_email = 'boyang.wang1993@gmail.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists',
+    
+    )
