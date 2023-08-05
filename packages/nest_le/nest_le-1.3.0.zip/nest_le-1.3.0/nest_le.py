c58b10addbc7d8efb7b73977365ff0fa5cbef369
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:22 +0000

    Add package nest_le/1.3.0/nest_le-1.3.0.zip

---
+++ packages/nest_le/nest_le-1.3.0.zip/nest_le-1.3.0/nest_le.py
@@ -0,0 +1,16 @@
+"""这是nest_le.py模块，提供了一个名为print_lol()的函数用来打印列表，其中包含或不包含嵌套列表。"""
+def print_lol(the_list, indent=False, level=0):
+        """这个函数有一个位置参数，名为"the_list"，这可以是任何python列表(包含或不包含嵌套列表)，所提供列表中的各个数据项会(递归地)打印到屏幕上，而且各占一行。
+        第二个参数(名为"level")用来在遇到嵌套列表时插入制表符"""
+        for each_item in the_list:
+                if isinstance(each_item, list):
+                        level = level + 1
+                        print_lol(each_item, indent, level)
+                else:
+                        if indent:
+                                for tab_stop in range(level):
+                                        print("\t", end='')
+                        print(each_item)
+		
+movies = ['The Holy Grail', 1975, 'Terry Jones & Terry Gilliam', 91, ['Graham Chapman', ['Michael Palin', 'John Cleese', 'Terry Gilliam', 'Eric Idle', 'Terry Jones']]]
+
