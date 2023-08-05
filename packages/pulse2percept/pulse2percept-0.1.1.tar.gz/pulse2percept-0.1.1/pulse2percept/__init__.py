66196d004323a91874d5fc05a1b1f43b44633b72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:11 +0000

    Add package pulse2percept/0.1.1/pulse2percept-0.1.1.tar.gz

---
+++ packages/pulse2percept/pulse2percept-0.1.1.tar.gz/pulse2percept-0.1.1/pulse2percept/__init__.py
@@ -0,0 +1,27 @@
+from __future__ import absolute_import, division, print_function
+from .version import __version__  # noqa
+from . import files
+from . import utils
+from . import electrode2currentmap
+from . import effectivecurrent2brightness
+
+import logging
+
+
+# Disable Jupyter Notebook handlers
+# https://github.com/ipython/ipython/issues/8282
+logging.getLogger().handlers = []
+
+# Set up root logger
+formatstr = '%(asctime)s [%(name)s] [%(levelname)s] %(message)s'
+logging.basicConfig(level=logging.DEBUG,
+                    format=formatstr,
+                    filename='debug.log',
+                    filemode='w')
+logging.getLogger(__name__).info('Welcome to pulse2percept')
+
+# Add streaming to console
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+console.setFormatter(logging.Formatter(formatstr))
+logging.getLogger(__name__).addHandler(console)
