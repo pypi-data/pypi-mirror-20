dad2ee2e57a635c0ccfa3912a4fb2aa5259c3bfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:03 +0000

    Add package scriptorium/2.5.5/scriptorium-2.5.5-py2.py3-none-any.whl

---
+++ packages/scriptorium/scriptorium-2.5.5-py2.py3-none-any.whl/scriptorium/config.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+"""Configuration related functionality for scriptorium."""
+
+import os
+import yaml
+
+import scriptorium
+
+_DEFAULT_DIR = os.path.join(os.path.expanduser("~"), '.scriptorium')
+_CFG_FILE = os.path.join(_DEFAULT_DIR, 'config')
+
+_DEFAULT_CFG = {
+    'TEMPLATE_DIR': os.path.join(_DEFAULT_DIR, 'templates'),
+    'LATEX_CMD': 'xelatex'
+}
+
+def read_config():
+    """Read configuration values for scriptorium."""
+    try:
+        with open(_CFG_FILE, 'Ur') as cfg_fp:
+            cfg = yaml.load(cfg_fp)
+            scriptorium.CONFIG.update(cfg)
+    except EnvironmentError:
+        if not os.path.exists(scriptorium.CONFIG['TEMPLATE_DIR']):
+            os.makedirs(scriptorium.CONFIG['TEMPLATE_DIR'])
+        #Save configuration from first time
+        save_config()
+
+def save_config():
+    """Save configuration values for scriptorium."""
+    with open(_CFG_FILE, 'w') as cfg_fp:
+        yaml.dump(scriptorium.CONFIG, cfg_fp)
