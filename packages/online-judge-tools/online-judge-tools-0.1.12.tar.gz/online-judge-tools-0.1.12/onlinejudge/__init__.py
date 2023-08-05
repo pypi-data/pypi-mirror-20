ea33ec0d46a343b3b3454bd5cb9924cf2d6b2687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:35 +0000

    Add package online-judge-tools/0.1.12/online-judge-tools-0.1.12.tar.gz

---
+++ packages/online-judge-tools/online-judge-tools-0.1.12.tar.gz/online-judge-tools-0.1.12/onlinejudge/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import onlinejudge.atcoder
+import onlinejudge.yukicoder
+import onlinejudge.anarchygolf
+import onlinejudge.codeforces
+import onlinejudge.hackerrank
+import onlinejudge.aoj
+import onlinejudge.dispatch
---
+++ packages/online-judge-tools/online-judge-tools-0.1.12.tar.gz/online-judge-tools-0.1.12/onlinejudge/service.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+class Service(object):
+    def login(self, get_credentials, session=None):
+        raise NotImplementedError
+    def get_url(self):
+        raise NotImplementedError
+    def get_name(self):
+        raise NotImplementedError
+    @classmethod
+    def from_url(self, s):
+        pass
