335628a16a0a32a2c2b9b4365e3e9cc792beef99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:52 +0000

    Add package sheetDB/0.1.2/sheetDB-0.1.2.tar.gz

---
+++ packages/sheetDB/sheetDB-0.1.2.tar.gz/sheetDB-0.1.2/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+    name = 'sheetDB',
+    packages = ['sheetDB'],
+    install_requires = [
+        'gspread==0.6.2',
+        'oauth2client==4.0.0',
+        'PyOpenSSL',
+    ],
+    version = '0.1.2',
+    description = 'a library to use Google Sheets as a backend',
+    author = 'knyte',
+    author_email = 'galactaknyte@gmail.com',
+    url = 'https://github.com/knyte/sheetDB',
+    download_url = 'https://github.com/knyte/sheetDB/tarball/0.1.2',
+    keywords = ['Google Sheets', 'spreadsheets', 'gspread'],
+    classifiers = [],
+)
