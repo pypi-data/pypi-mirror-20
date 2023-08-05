2af5e74e370afc30b0ea4e17be86d8ae38f3db73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:45 +0000

    Add package clatter/0.0.3/clatter-0.0.3.tar.gz

---
+++ packages/clatter/clatter-0.0.3.tar.gz/clatter-0.0.3/clatter/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+'''
+'''
+
+from __future__ import absolute_import
+from clatter.core import Runner
+
+__author__ = """Michael Delgado"""
+__version__ = '0.0.3'
+
+_module_imports = (
+    Runner,
+)
+
+__all__ = list(map(lambda x: x.__name__, _module_imports))
