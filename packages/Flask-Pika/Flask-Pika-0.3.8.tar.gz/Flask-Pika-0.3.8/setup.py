25516e92bcf493156a65b977341a42c49efaa274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:09 +0000

    Add package Flask-Pika/0.3.8/Flask-Pika-0.3.8.tar.gz

---
+++ packages/Flask-Pika/Flask-Pika-0.3.8.tar.gz/Flask-Pika-0.3.8/setup.py
@@ -0,0 +1,36 @@
+"""
+Flask-Pika
+-------------
+This extension provides a simple way to expose a Pika blocking channel inside of Flask.
+
+Once a channel is obtained, use it as you would any normal Pika blocking channel.
+"""
+from setuptools import setup
+
+
+setup(
+    name='Flask-Pika',
+    version='0.3.8',
+    url='https://github.com/WeatherDecisionTechnologies/flask-pika',
+    license='BSD',
+    author='Weather Decision Technologies',
+    author_email='alertingdevelopers@wdtinc.com',
+    description='Pika amqp flask extension',
+    long_description=__doc__,
+    zip_safe=False,
+    include_package_data=True,
+    py_modules=['flask_pika'],
+    platforms='any',
+    install_requires=[
+        'Flask>=0.7',
+        'pika==0.10.0'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
