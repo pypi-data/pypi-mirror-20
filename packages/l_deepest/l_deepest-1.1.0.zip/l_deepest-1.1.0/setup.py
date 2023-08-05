f5a82346ce784226a948f7d475a57e59461323ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:20 +0000

    Add package l_deepest/1.1.0/l_deepest-1.1.0.zip

---
+++ packages/l_deepest/l_deepest-1.1.0.zip/l_deepest-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name      = "l_deepest",
+    version   ="1.1.0",
+    py_modules  = ["deepest"],
+    author    = "liangxuefeng",
+    author_email  ="719265726@qq.com",
+    description ="A simple printer of nested lists",
+    )
