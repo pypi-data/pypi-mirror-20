9d6e0890f00db56da04fdc1dfc95c67e1704940d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:14 +0000

    Add package nodeconductor-openstack/0.19.0/nodeconductor_openstack-0.19.0-py2-none-any.whl

---
+++ packages/nodeconductor-openstack/nodeconductor_openstack-0.19.0-py2-none-any.whl/nodeconductor_openstack/openstack_tenant/apps.py
@@ -0,0 +1,103 @@
+from django.apps import AppConfig
+from django.db.models import signals
+from django_fsm import signals as fsm_signals
+
+
+class OpenStackTenantConfig(AppConfig):
+    """ OpenStack is a toolkit for building private and public clouds.
+        This application adds support for managing OpenStack tenant resources -
+        instances, volumes and snapshots.
+    """
+    name = 'nodeconductor_openstack.openstack_tenant'
+    label = 'openstack_tenant'
+    verbose_name = 'OpenStackTenant'
+    service_name = 'OpenStackTenant'
+
+    def ready(self):
+        from nodeconductor.structure import SupportedServices
+        from .backend import OpenStackTenantBackend
+        SupportedServices.register_backend(OpenStackTenantBackend)
+
+        # Initialize service settings quotas based on tenant.
+        from nodeconductor.structure.models import ServiceSettings
+        from nodeconductor.quotas.fields import QuotaField
+        from nodeconductor_openstack.openstack.models import Tenant, SecurityGroup, FloatingIP
+        for quota in Tenant.get_quotas_fields():
+            ServiceSettings.add_quota_field(
+                name=quota.name,
+                quota_field=QuotaField(
+                    is_backend=True,
+                    default_limit=quota.default_limit,
+                    creation_condition=lambda service_settings:
+                        service_settings.type == OpenStackTenantConfig.service_name
+                )
+            )
+
+        from . import handlers, models
+        for Resource in (models.Instance, models.Volume, models.Snapshot):
+            name = Resource.__name__.lower()
+            signals.post_save.connect(
+                handlers.log_action,
+                sender=Resource,
+                dispatch_uid='openstack_tenant.handlers.log_%s_action' % name,
+            )
+
+        signals.post_save.connect(
+            handlers.log_backup_schedule_creation,
+            sender=models.BackupSchedule,
+            dispatch_uid='openstack_tenant.handlers.log_backup_schedule_creation',
+        )
+
+        signals.post_save.connect(
+            handlers.log_backup_schedule_action,
+            sender=models.BackupSchedule,
+            dispatch_uid='openstack_tenant.handlers.log_backup_schedule_action',
+        )
+
+        signals.pre_delete.connect(
+            handlers.log_backup_schedule_deletion,
+            sender=models.BackupSchedule,
+            dispatch_uid='openstack_tenant.handlers.log_backup_schedule_deletion',
+        )
+
+        fsm_signals.post_transition.connect(
+            handlers.create_floating_ip,
+            sender=FloatingIP,
+            dispatch_uid='openstack_tenant.handlers.create_floating_ip',
+        )
+
+        fsm_signals.post_transition.connect(
+            handlers.update_floating_ip,
+            sender=FloatingIP,
+            dispatch_uid='openstack_tenant.handlers.update_floating_ip',
+        )
+
+        fsm_signals.post_transition.connect(
+            handlers.create_security_group,
+            sender=SecurityGroup,
+            dispatch_uid='openstack_tenant.handlers.create_security_group',
+        )
+
+        fsm_signals.post_transition.connect(
+            handlers.update_security_group,
+            sender=SecurityGroup,
+            dispatch_uid='openstack_tenant.handlers.update_security_group',
+        )
+
+        signals.post_delete.connect(
+            handlers.delete_security_group,
+            sender=SecurityGroup,
+            dispatch_uid='openstack_tenant.handlers.delete_security_group',
+        )
+
+        signals.post_delete.connect(
+            handlers.delete_floating_ip,
+            sender=FloatingIP,
+            dispatch_uid='openstack_tenant.handlers.delete_floating_ip',
+        )
+
+        signals.post_save.connect(
+            handlers.update_service_settings_password,
+            sender=Tenant,
+            dispatch_uid='openstack.handlers.update_service_settings_password',
+        )
