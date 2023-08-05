57c14901e72d74519d8ea52f8a174d8fc1b10424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:41 +0000

    Add package wordhop/0.0.6/wordhop-0.0.6.tar.gz

---
+++ packages/wordhop/wordhop-0.0.6.tar.gz/wordhop-0.0.6/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+setup(
+      name = 'wordhop',
+      packages = ['wordhop'],
+      version = '0.0.6',
+      description = 'A toolkit for creating a hybrid Chatbot + Human UX',
+      author = 'Michael Nathanson',
+      author_email = 'mike@wordhop.io',
+      url = 'https://github.com/wordhop-io/wordhop-python',
+      download_url = 'https://github.com/wordhop-io/wordhop-python/tarball/0.0.6',
+      keywords = ['chatbot', 'bot', 'opitmization', 'bot', 'analytics'],
+      classifiers = [],
+      install_requires=['socketIO_client','requests']
+      )
