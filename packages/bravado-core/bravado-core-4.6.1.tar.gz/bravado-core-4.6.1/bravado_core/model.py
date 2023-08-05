34ac060e46cbb575e72890fb1406c33bbcc71350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:26 +0000

    Add package bravado-core/4.6.1/bravado-core-4.6.1.tar.gz

---
+++ packages/bravado-core/bravado-core-4.6.1.tar.gz/bravado-core-4.6.1/bravado_core/model.py
@@ -0,0 +1,271 @@
+from functools import partial
+import logging
+
+from six import iteritems
+
+from bravado_core.docstring import docstring_property
+from bravado_core.schema import collapsed_properties
+from bravado_core.schema import SWAGGER_PRIMITIVES
+
+
+log = logging.getLogger(__name__)
+
+# Models in #/definitions are tagged with this key so that they can be
+# differentiated from 'object' types.
+MODEL_MARKER = 'x-model'
+
+
+def tag_models(container, key, path, visited_models, swagger_spec):
+    """Callback used during the swagger spec ingestion process to tag models
+    with a 'x-model'. This is only done in the root document.
+
+    A list of visited models is maintained to avoid duplication of tagging.
+
+    :param container: container being visited
+    :param key: attribute in container being visited as a string
+    :param path: list of path segments to the key
+    :type visited_models: dict (k,v) == (model_name, path)
+    :type swagger_spec: :class:`bravado_core.spec.Spec`
+    """
+    if len(path) < 2 or path[-2] != 'definitions':
+        return
+    deref = swagger_spec.deref
+    model_name = key
+    model_spec = deref(container.get(key))
+
+    if not is_object(swagger_spec, model_spec):
+        return
+
+    if deref(model_spec.get(MODEL_MARKER)) is not None:
+        return
+
+    log.debug('Found model: {0}'.format(model_name))
+    if model_name in visited_models:
+        raise ValueError(
+            'Duplicate "{0}" model found at path {1}. '
+            'Original "{0}" model at path {2}'
+            .format(model_name, path, visited_models[model_name]))
+
+    model_spec['x-model'] = model_name
+    visited_models[model_name] = path
+
+
+def collect_models(container, key, path, models, swagger_spec):
+    """Callback used during the swagger spec ingestion to collect all the
+    tagged models and create appropriate python types for them.
+
+    :param container: container being visited
+    :param key: attribute in container being visited as a string
+    :param path: list of path segments to the key
+    :param models: created model types are placed here
+    :type swagger_spec: :class:`bravado_core.spec.Spec`
+    """
+    deref = swagger_spec.deref
+    if key == MODEL_MARKER:
+        model_spec = container
+        model_name = deref(model_spec.get(MODEL_MARKER))
+        models[model_name] = create_model_type(
+            swagger_spec, model_name, model_spec)
+
+
+def create_model_type(swagger_spec, model_name, model_spec):
+    """Create a dynamic class from the model data defined in the swagger
+    spec.
+
+    The docstring for this class is dynamically generated because generating
+    the docstring is relatively expensive, and would only be used in rare
+    cases for interactive debugging in a REPL.
+
+    :type swagger_spec: :class:`bravado_core.spec.Spec`
+    :param model_name: model name
+    :param model_spec: json-like dict that describes a model.
+    :returns: dynamic type created with attributes, docstrings attached
+    :rtype: type
+    """
+    from bravado_core.marshal import marshal_model
+    from bravado_core.unmarshal import unmarshal_model
+    doc = docstring_property(partial(
+        create_model_docstring, swagger_spec, model_spec))
+
+    def create(cls, kwargs):
+        self = cls.__new__(cls)
+        model_constructor(self, model_spec, swagger_spec, kwargs)
+        return self
+
+    methods = dict(
+        __doc__=doc,
+        __eq__=lambda self, other: compare(self, other),
+        __init__=lambda self, **kwargs: model_constructor(self, model_spec,
+                                                          swagger_spec,
+                                                          kwargs),
+        __repr__=lambda self: create_model_repr(self, model_spec,
+                                                swagger_spec),
+        __dir__=lambda self: model_dir(self, model_spec, swagger_spec),
+        create=classmethod(create),
+        marshal=lambda self: marshal_model(swagger_spec, model_spec, self),
+        unmarshal=staticmethod(lambda val: unmarshal_model(swagger_spec, model_spec, val)),
+    )
+    return type(str(model_name), (object,), methods)
+
+
+def model_dir(model, model_spec, swagger_spec):
+    """Responsible for returning the names of the valid attributes on this
+    model object.  This includes any properties defined in this model's spec,
+    any properties that happen to be defined in parent polymorphic models,
+    plus additional attibutes that exist as `additionalProperties`.
+
+    :param model: instance of a model
+    :param model_spec: spec the passed in model in dict form
+    :type swagger_spec: :class:`bravado_core.spec.Spec`
+    :returns: list of str
+    """
+    return (list(collapsed_properties(model_spec, swagger_spec).keys()) +
+            model._additional_props)
+
+
+def compare(first, second):
+    """Compares two model types for equivalence.
+
+    TODO: If a type composes another model type, .__dict__ recurse on those
+          and compare again on those dict values.
+
+    :param first: generated model type
+    :type first: type
+    :param second: generated model type
+    :type second: type
+    :returns: True if equivalent, False otherwise
+    """
+    if not hasattr(first, '__dict__') or not hasattr(second, '__dict__'):
+        return False
+
+    # Ignore any '_raw' keys
+    def norm_dict(d):
+        return dict((k, d[k]) for k in d if k != '_raw')
+
+    return norm_dict(first.__dict__) == norm_dict(second.__dict__)
+
+
+def model_constructor(model, model_spec, swagger_spec, constructor_kwargs):
+    """Constructor for the given model instance. Just assigns kwargs as attrs
+    on the model based on the 'properties' in the model specification.
+
+    :param model: Instance of a model type
+    :type model: type
+    :param model_spec: model specification
+    :type model_spec: dict
+    :type swagger_spec: :class:`bravado_core.spec.Spec`
+    :param constructor_kwargs: kwargs sent in to the constructor invocation
+    :type constructor_kwargs: dict
+    :raises: AttributeError on constructor_kwargs that don't exist in the
+        model specification's list of properties
+    """
+    arg_names = list(constructor_kwargs.keys())
+
+    properties = collapsed_properties(model_spec, swagger_spec)
+
+    for attr_name, attr_spec in iteritems(properties):
+        if attr_name in arg_names:
+            attr_value = constructor_kwargs[attr_name]
+            arg_names.remove(attr_name)
+        else:
+            attr_value = None
+        setattr(model, attr_name, attr_value)
+
+    if arg_names and not model_spec.get('additionalProperties', True):
+        raise AttributeError(
+            "Model {0} does not have attributes for: {1}"
+            .format(type(model), arg_names))
+
+    # we've got additionalProperties to set on the model
+    for arg_name in arg_names:
+        setattr(model, arg_name, constructor_kwargs[arg_name])
+
+    # stash so that dir(model) works
+    model._additional_props = arg_names
+
+
+def create_model_repr(model, model_spec, swagger_spec):
+    """Generates the repr string for the model.
+
+    :param model: Instance of a model
+    :param model_spec: model specification
+    :type model_spec: dict
+    :param swagger_spec: :class:`bravado_core.spec.Spec`
+    :returns: repr string for the model
+    """
+    properties = collapsed_properties(model_spec, swagger_spec)
+    s = [
+        "{0}={1!r}".format(attr_name, getattr(model, attr_name))
+        for attr_name in sorted(properties.keys())
+    ]
+    return "{0}({1})".format(model.__class__.__name__, ', '.join(s))
+
+
+def is_model(swagger_spec, schema_object_spec):
+    """
+    :param swagger_spec: :class:`bravado_core.spec.Spec`
+    :param schema_object_spec: specification for a swagger object
+    :type schema_object_spec: dict
+    :return: True if the spec has been "marked" as a model type, false
+        otherwise.
+    """
+    deref = swagger_spec.deref
+    schema_object_spec = deref(schema_object_spec)
+    return deref(schema_object_spec.get(MODEL_MARKER)) is not None
+
+
+def is_object(swagger_spec, object_spec):
+    """
+    A schema definition is of type object if its type is object or if it uses
+    model composition (i.e. it has an allOf property).
+    :param swagger_spec: :class:`bravado_core.spec.Spec`
+    :param schema_object_spec: specification for a swagger object
+    :type schema_object_spec: dict
+    :return: True if the spec describes an object, False otherwise.
+    """
+    deref = swagger_spec.deref
+    return deref(object_spec.get('type')) == 'object' or 'allOf' in object_spec
+
+
+def create_model_docstring(swagger_spec, model_spec):
+    """
+    :type swagger_spec: :class:`bravado_core.spec.Spec`
+    :param model_spec: specification for a model in dict form
+    :rtype: string or unicode
+    """
+    deref = swagger_spec.deref
+    model_spec = deref(model_spec)
+
+    s = 'Attributes:\n\n\t'
+    properties = collapsed_properties(model_spec, swagger_spec)
+    attr_iter = iter(sorted(iteritems(properties)))
+    # TODO: Add more stuff available in the spec - 'required', 'example', etc
+    for attr_name, attr_spec in attr_iter:
+        attr_spec = deref(attr_spec)
+        schema_type = deref(attr_spec['type'])
+
+        if schema_type in SWAGGER_PRIMITIVES:
+            # TODO: update to python types and take 'format' into account
+            attr_type = schema_type
+
+        elif schema_type == 'array':
+            array_spec = deref(attr_spec['items'])
+            if is_model(swagger_spec, array_spec):
+                array_type = deref(array_spec[MODEL_MARKER])
+            else:
+                array_type = deref(array_spec['type'])
+            attr_type = u'list of {0}'.format(array_type)
+
+        elif is_model(swagger_spec, attr_spec):
+            attr_type = deref(attr_spec[MODEL_MARKER])
+
+        elif schema_type == 'object':
+            attr_type = 'dict'
+
+        s += u'{0}: {1}'.format(attr_name, attr_type)
+
+        if deref(attr_spec.get('description')):
+            s += u' - {0}'.format(deref(attr_spec['description']))
+
+        s += '\n\t'
+    return s
---
+++ packages/bravado-core/bravado-core-4.6.1.tar.gz/bravado-core-4.6.1/bravado_core/swagger20_validator.py
@@ -0,0 +1,156 @@
+import functools
+
+from jsonschema import validators, _validators
+from jsonschema.exceptions import ValidationError
+from jsonschema.validators import Draft4Validator
+
+from bravado_core.schema import is_param_spec
+from bravado_core.schema import is_prop_nullable
+from bravado_core.schema import is_required
+from swagger_spec_validator.ref_validators import in_scope
+
+"""Draft4Validator is not completely compatible with Swagger 2.0 schema
+objects like parameter, etc. Swagger20Validator is an extension of
+Draft4Validator which customizes/wraps some of the operations of the default
+validator.
+"""
+
+
+def type_validator(swagger_spec, validator, types, instance, schema):
+    """Skip the `type` validator when a Swagger parameter value is None.
+    Otherwise it will fail with a "None is not a valid type" failure instead
+    of letting the downstream `required_validator` do its job.
+    Also skip when a Swagger property value is None and the schema contains
+    the extension field `x-nullable` set to True.
+    In all other cases, delegate to the existing Draft4 `type` validator.
+
+    :param swagger_spec: needed for access to deref()
+    :type swagger_spec: :class:`bravado_core.spec.Spec`
+    :param validator: Validator class used to validate the object
+    :type validator: :class:`Swagger20Validator` or
+        :class:`jsonschema.validators.Draft4Validator`
+    :param types: validate types
+    :type types: string or list
+    :param instance: object instance value
+    :param schema: swagger spec for the object
+    :type schema: dict
+    """
+    if (is_param_spec(swagger_spec, schema) or
+            is_prop_nullable(swagger_spec, schema)) and instance is None:
+        return
+
+    for error in _validators.type_draft4(validator, types, instance, schema):
+        yield error
+
+
+def required_validator(swagger_spec, validator, required, instance, schema):
+    """Swagger 2.0 expects `required` to be a bool in the Parameter object,
+    but a list of properties everywhere else.
+
+    :param swagger_spec: needed for access to deref()
+    :type swagger_spec: :class:`bravado_core.spec.Spec`
+    :param validator: Validator class used to validate the object
+    :type validator: :class:`Swagger20Validator` or
+        :class:`jsonschema.validators.Draft4Validator`
+    :param required: value of `required` field
+    :type required: boolean or list or None
+    :param instance: object instance value
+    :param schema: swagger spec for the object
+    :type schema: dict
+    """
+    if is_param_spec(swagger_spec, schema):
+        if required and instance is None:
+            yield ValidationError('{0} is a required parameter.'.format(
+                schema['name']))
+    else:
+        for error in _validators.required_draft4(validator, required, instance,
+                                                 schema):
+            yield error
+
+
+def enum_validator(swagger_spec, validator, enums, instance, schema):
+    """Swagger 2.0 allows enums to be validated against objects of type
+    arrays, like query parameter (collectionFormat: multi)
+
+    :param swagger_spec: needed for access to deref()
+    :type swagger_spec: :class:`bravado_core.spec.Spec`
+    :param validator: Validator class used to validate the object
+    :type validator: :class: `Swagger20Validator` or
+        :class: `jsonschema.validators.Draft4Validator`
+    :param enums: allowed enum values
+    :type enums: list
+    :param instance: enum instance value
+    :param schema: swagger spec for the object
+    :type schema: dict
+    """
+
+    if is_prop_nullable(swagger_spec, schema) and instance is None:
+        return
+
+    if schema.get('type') == 'array':
+        for element in instance:
+            for error in _validators.enum(validator, enums, element, schema):
+                yield error
+        return
+
+    # Handle optional enum params with no value
+    if is_param_spec(swagger_spec, schema):
+        if not is_required(swagger_spec, schema) and instance is None:
+            return
+
+    for error in _validators.enum(validator, enums, instance, schema):
+        yield error
+
+
+def ref_validator(validator, ref, instance, schema):
+    """When validating a request or response that contain $refs, jsonschema's
+     RefResolver only contains scope (RefResolver._scopes_stack) for the
+     request_spec or response_spec that it is fed. This scope doesn't contain
+     the full scope built when ingesting the spec from its root
+     (#/ in swagger.json). So, we need to modify the behavior of ref
+     validation to use the `x-scope` annotations that were created during spec
+     ingestion (see post_process_spec in spec.py).
+
+    :param validator: Validator class used to validate the object
+    :type validator: :class: `Swagger20Validator` or
+        :class: `jsonschema.validators.Draft4Validator`
+    :param ref: the target of the ref. eg. #/foo/bar/Baz
+    :type ref: string
+    :param instance: the object being validated
+    :param schema: swagger spec that contains the ref.
+        eg {'$ref': '#/foo/bar/Baz'}
+    :type schema: dict
+    """
+    # This is a copy of jsonscehama._validators.ref(..) with the
+    # in_scope(..) context manager applied before any refs are resolved.
+    resolve = getattr(validator.resolver, "resolve")
+    if resolve is None:
+        with in_scope(validator.resolver, schema):
+            with validator.resolver.resolving(ref) as resolved:
+                for error in validator.descend(instance, resolved):
+                    yield error
+    else:
+        with in_scope(validator.resolver, schema):
+            scope, resolved = validator.resolver.resolve(ref)
+        validator.resolver.push_scope(scope)
+
+        try:
+            for error in validator.descend(instance, resolved):
+                yield error
+        finally:
+            validator.resolver.pop_scope()
+
+
+def get_validator_type(swagger_spec):
+    """Create a custom jsonschema validator for Swagger 2.0 specs.
+
+    :rtype: Its complicated. See jsonschema.validators.create()
+    """
+    return validators.extend(
+        Draft4Validator,
+        {
+            '$ref': ref_validator,
+            'required': functools.partial(required_validator, swagger_spec),
+            'enum': functools.partial(enum_validator, swagger_spec),
+            'type': functools.partial(type_validator, swagger_spec),
+        })
