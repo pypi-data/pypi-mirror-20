8fd299300ca1fbbc3aadac0b9bc9f861049d98df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:51 +0000

    Add package piforge/1.3.4/piforge-1.3.4.tar.gz

---
+++ packages/piforge/piforge-1.3.4.tar.gz/piforge-1.3.4/setup.py
@@ -0,0 +1,31 @@
+import os, sys, subprocess
+from setuptools import setup
+from setuptools.command.install import install
+class CustomInstall(install):
+	def run(self):
+		install.run(self)
+		print "here is custom command"
+		subprocess.call(['setupAuto.sh'])
+
+setup (name='piforge',
+	version='1.3.4',
+	description='iforge for rPi server component',
+	url='https://github.com/shawt/piforge',
+	author='Trevor Shaw',
+	author_email='shawt@genlrn.com',
+	license='MIT',
+	scripts=['piforge/setupAuto.sh'],
+	entry_points={
+          'console_scripts': [
+              'piforge = piforge.__main__:main'
+          ]
+      },
+	packages=['piforge'],
+	install_requires=[
+		'web.py',
+		'RPi.GPIO',
+		
+	],
+	zip_safe=False,
+	cmdclass={'install': CustomInstall})
+		
