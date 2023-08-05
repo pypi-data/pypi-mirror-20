87ab6eaa67b5b502c4b237e884c11761b969abe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:38 +0000

    Add package webmark/0.0.1/webmark-0.0.1.tar.gz

---
+++ packages/webmark/webmark-0.0.1.tar.gz/webmark-0.0.1/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+
+
+setup(name='webmark',
+      version='0.0.1',
+      description='Console tool for managing bookmarks',
+      author='Dmitriy Moseev',
+      author_email='moseevda@gmail.com',
+      scripts=['mark'],
+      url='https://github.com/DmitriyMoseev/webmark',
+      keyworkds=['bookmarks', 'bookmark', 'console'])
