285386a152855303ab7c37db8ac4079d4e54e8c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:00 +0000

    Add package credulous/0.1.4/credulous-0.1.4.tar.gz

---
+++ packages/credulous/credulous-0.1.4.tar.gz/credulous-0.1.4/credulous/__main__.py
@@ -0,0 +1,19 @@
+from . import credulous
+
+import argparse
+import os
+
+if __name__=='__main__':
+	parser = argparse.ArgumentParser(
+		description="""
+	Tool for generating API credentials. Google API is supported.
+	<scopes> is a json with a list of scope URL. The list should be under 
+	scopes.google""")
+	parser.add_argument('--secret',
+		required=True,
+	  help='Path to your client secret json file')
+	parser.add_argument('scopes')
+	args = parser.parse_args()
+
+	credulous = credulous.Credulous(args.secret, args.scopes)
+	credulous.authenticate()
