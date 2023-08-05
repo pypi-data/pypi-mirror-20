0245f75aece09ef740a1c42804ce1da8953379ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:17 +0000

    Add package flask_reqparse/2.2.1/flask-reqparse-2.2.1.tar.gz

---
+++ packages/flask_reqparse/flask-reqparse-2.2.1.tar.gz/flask-reqparse-2.2.1/setup.py
@@ -0,0 +1,34 @@
+
+"""
+flask-reqparse
+-------------
+flask-reqparse a request parser for parse apps
+"""
+from setuptools import setup
+
+
+setup(
+    name='flask-reqparse',
+    version='2.2.1',
+    license='BSD',
+    author='smit thakkar',
+    author_email='smitthakkar96@gmail.com',
+    description='flask_reqparse is a simple wrapper around flask request module that helps to parse args efficently.',
+    long_description=__doc__,
+    py_modules=['flask_reqparse'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
