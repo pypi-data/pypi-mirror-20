204662562ff72c5619c05e1edee1082a4151e64c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:05:59 +0000

    Add package webmc/0.2/webmc-0.2.tar.gz

---
+++ packages/webmc/webmc-0.2.tar.gz/webmc-0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'webmc',
+  packages = ['webmc'], # this must be the same as the name above
+  version = '0.2',
+  description = 'ffmpeg client for webm convertions',
+  author = 'Nahuel Alonso',
+  author_email = 'blizzard.nna@gmail.com',
+  url = 'https://github.com/nahwar/webmc', # use the URL to the github repo
+  download_url = 'https://github.com/nahwar/webmc/tarball/0.2', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example', 'webm', 'ffmpeg'], # arbitrary keywords
+  classifiers = [],
+  scripts = ['webmc/webmc'],
+)
