219cd263ece63f60639a5a2264bda0a8d1dbe03f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:17 +0000

    Add package django-dynamic-preferences/1.0/django-dynamic-preferences-1.0.tar.gz

---
+++ packages/django-dynamic-preferences/django-dynamic-preferences-1.0.tar.gz/django-dynamic-preferences-1.0/dynamic_preferences/views.py
@@ -0,0 +1,43 @@
+from django.views.generic import TemplateView, FormView
+from .forms import preference_form_builder
+
+
+"""Todo : remove these views and use only context processors"""
+class RegularTemplateView(TemplateView):
+    """Used for testing context"""
+    template_name = "dynamic_preferences/testcontext.html"
+
+
+class PreferenceFormView(FormView):
+    """Display a form for updating preferences of the given section provided via URL arg.
+    If no section is provided, will display a form for all fields of a given registry.
+    """
+
+    #: the registry for preference lookups
+    registry = None
+
+    #: will be used by :py:func:`forms.preference_form_builder` to create the form
+    form_class = None
+
+    template_name = "dynamic_preferences/form.html"
+
+    def get_form_class(self, *args, **kwargs):
+        section = self.kwargs.get('section', None)
+        form_class = preference_form_builder(self.form_class, section=section)
+        return form_class
+
+    def get_context_data(self, *args, **kwargs):
+
+        context = super(PreferenceFormView, self).get_context_data(*args, **kwargs)
+
+        context['registry'] = self.registry
+
+        return context
+
+    def get_success_url(self):
+        return self.request.path
+
+    def form_valid(self, form):
+
+        form.update_preferences()
+        return super(PreferenceFormView, self).form_valid(form)
