726dd5df38d81792cd2618bdef442956a651d805
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:40 +0000

    Add package flowz/1.0.0/flowz-1.0.0.tar.gz

---
+++ packages/flowz/flowz-1.0.0.tar.gz/flowz-1.0.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name = 'flowz',
+    version = '1.0.0',
+    description = 'Async I/O - oriented dependency programming framework',
+    url = 'https://github.com/ethanrowe/flowz',
+    author = 'Ethan Rowe',
+    author_email = 'ethan@the-rowes.com',
+    license = 'MIT',
+    test_suite = 'nose.collector',
+    packages = [
+        'flowz',
+        'flowz.channels',
+        'flowz.examples',
+        'flowz.test',
+        'flowz.test.artifacts',
+        'flowz.test.channels',
+        'flowz.test.util',
+        ],
+    tests_require = [
+        'mock',
+        'nose',
+        'six >= 1.9.0',
+        ],
+    install_requires = [
+        'tornado >= 4.2',
+        'futures >= 3.0.5'
+        ])
