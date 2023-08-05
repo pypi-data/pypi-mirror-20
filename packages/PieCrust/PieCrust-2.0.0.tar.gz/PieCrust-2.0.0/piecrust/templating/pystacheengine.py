c5e11919d5c26668a3b284739e52e833b90bfa5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:23 +0000

    Add package PieCrust/2.0.0/PieCrust-2.0.0.tar.gz

---
+++ packages/PieCrust/PieCrust-2.0.0.tar.gz/PieCrust-2.0.0/piecrust/templating/pystacheengine.py
@@ -0,0 +1,81 @@
+import logging
+import collections.abc
+import pystache
+import pystache.common
+from piecrust.templating.base import (
+        TemplateEngine, TemplateNotFoundError, TemplatingError)
+
+
+logger = logging.getLogger(__name__)
+
+
+class PystacheTemplateEngine(TemplateEngine):
+    ENGINE_NAMES = ['mustache']
+    EXTENSIONS = ['mustache']
+
+    def __init__(self):
+        self.renderer = None
+
+    def renderSegmentPart(self, path, seg_part, data):
+        self._ensureLoaded()
+        try:
+            return self.renderer.render(seg_part.content, data)
+        except pystache.common.TemplateNotFoundError as ex:
+            raise TemplateNotFoundError() from ex
+        except pystache.common.PystacheError as ex:
+            raise TemplatingError(str(ex), path) from ex
+
+    def renderFile(self, paths, data):
+        self._ensureLoaded()
+        tpl = None
+        logger.debug("Looking for template: %s" % paths)
+        for p in paths:
+            if not p.endswith('.mustache'):
+                raise TemplatingError(
+                        "The Mustache template engine only accepts template "
+                        "filenames with a `.mustache` extension. Got: %s" %
+                        p)
+            name = p[:-9]  # strip `.mustache`
+            try:
+                tpl = self.renderer.load_template(name)
+            except Exception as ex:
+                logger.debug("Mustache error: %s" % ex)
+                pass
+
+        if tpl is None:
+            raise TemplateNotFoundError()
+
+        try:
+            return self.renderer.render(tpl, data)
+        except pystache.common.PystacheError as ex:
+            raise TemplatingError(str(ex)) from ex
+
+    def _ensureLoaded(self):
+        if self.renderer:
+            return
+
+        self.renderer = _WorkaroundRenderer(
+                search_dirs=self.app.templates_dirs)
+
+
+_knowns = ['PieCrustData', 'LazyPageConfigData', 'Paginator', 'Assetor',
+           'PageLinkerData']
+
+
+class _WorkaroundRenderer(pystache.Renderer):
+    def _make_resolve_context(self):
+        mrc = super(_WorkaroundRenderer, self)._make_resolve_context()
+
+        def _workaround(stack, name):
+            # Pystache will treat anything that's not a string or a dict as
+            # a list. This is just plain wrong, but it will take a while before
+            # the project can get patches on Pypi.
+            res = mrc(stack, name)
+            if res is not None and (
+                    res.__class__.__name__ in _knowns or
+                    isinstance(res, collections.abc.Mapping)):
+                res = [res]
+            return res
+
+        return _workaround
+
