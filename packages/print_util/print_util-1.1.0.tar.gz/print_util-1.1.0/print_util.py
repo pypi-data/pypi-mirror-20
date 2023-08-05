10d06f622c1cedc5ac951bbb354034235ec9e1fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:18 +0000

    Add package print_util/1.1.0/print_util-1.1.0.tar.gz

---
+++ packages/print_util/print_util-1.1.0.tar.gz/print_util-1.1.0/print_util.py
@@ -0,0 +1,11 @@
+"""
+print_loop just print list all element
+"""
+def print_loop(the_list, level):
+    for the_element in the_list:
+        if isinstance(the_element, list):
+            print_loop(the_element, level+1)
+        else:
+            for i in range(level):
+                print("\t", end="")
+            print(the_element)
