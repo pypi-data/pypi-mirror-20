1fab33bc2db9367fb611165f33439b555cf52e80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:54 +0000

    Add package knossos/0.4.0/knossos-0.4.0-py2.py3-none-any.whl

---
+++ packages/knossos/knossos-0.4.0-py2.py3-none-any.whl/knossos/ui/settings_help.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ui/settings_help.ui'
+#
+# Created by: PyQt5 UI code generator 5.7.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from ..qt import QtCore, QtGui, QtWidgets
+
+class Ui_HelpForm(object):
+    def setupUi(self, HelpForm):
+        HelpForm.setObjectName("HelpForm")
+        HelpForm.resize(400, 300)
+        HelpForm.setWindowTitle("Form")
+        self.verticalLayout = QtWidgets.QVBoxLayout(HelpForm)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.label = QtWidgets.QLabel(HelpForm)
+        self.label.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.label.setWordWrap(True)
+        self.label.setOpenExternalLinks(True)
+        self.label.setObjectName("label")
+        self.verticalLayout.addWidget(self.label)
+
+        self.retranslateUi(HelpForm)
+        QtCore.QMetaObject.connectSlotsByName(HelpForm)
+
+    def retranslateUi(self, HelpForm):
+        _translate = QtCore.QCoreApplication.translate
+        self.label.setText(_translate("HelpForm", "<html><head/><body><p>If you need help, you can either check <a href=\"http://www.hard-light.net/forums/index.php?topic=93144.0\"><span style=\" text-decoration: underline; color:#0000ff;\">this release post</span></a> or <a href=\"https://github.com/ngld/knossos/issues\"><span style=\" text-decoration: underline; color:#0000ff;\">check the reported issues</span></a>.</p></body></html>"))
+
---
+++ packages/knossos/knossos-0.4.0-py2.py3-none-any.whl/knossos/ui/settings_sources.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ui/settings_sources.ui'
+#
+# Created by: PyQt5 UI code generator 5.7.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from ..qt import QtCore, QtGui, QtWidgets
+
+class Ui_ModSourcesForm(object):
+    def setupUi(self, ModSourcesForm):
+        ModSourcesForm.setObjectName("ModSourcesForm")
+        ModSourcesForm.resize(400, 300)
+        ModSourcesForm.setWindowTitle("Form")
+        self.verticalLayout = QtWidgets.QVBoxLayout(ModSourcesForm)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.sourceList = QtWidgets.QListWidget(ModSourcesForm)
+        self.sourceList.setDragEnabled(True)
+        self.sourceList.setDragDropMode(QtWidgets.QAbstractItemView.InternalMove)
+        self.sourceList.setObjectName("sourceList")
+        self.verticalLayout.addWidget(self.sourceList)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.addSource = QtWidgets.QPushButton(ModSourcesForm)
+        self.addSource.setObjectName("addSource")
+        self.horizontalLayout.addWidget(self.addSource)
+        self.editSource = QtWidgets.QPushButton(ModSourcesForm)
+        self.editSource.setObjectName("editSource")
+        self.horizontalLayout.addWidget(self.editSource)
+        self.removeSource = QtWidgets.QPushButton(ModSourcesForm)
+        self.removeSource.setObjectName("removeSource")
+        self.horizontalLayout.addWidget(self.removeSource)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        self.retranslateUi(ModSourcesForm)
+        QtCore.QMetaObject.connectSlotsByName(ModSourcesForm)
+
+    def retranslateUi(self, ModSourcesForm):
+        _translate = QtCore.QCoreApplication.translate
+        self.addSource.setText(_translate("ModSourcesForm", "Add"))
+        self.editSource.setText(_translate("ModSourcesForm", "Edit"))
+        self.removeSource.setText(_translate("ModSourcesForm", "Remove"))
+
