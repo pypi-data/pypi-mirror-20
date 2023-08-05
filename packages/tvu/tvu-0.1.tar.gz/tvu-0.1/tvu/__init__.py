36a7561cc239687ca86f837c0b891d024abbe236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:41 +0000

    Add package tvu/0.1/tvu-0.1.tar.gz

---
+++ packages/tvu/tvu-0.1.tar.gz/tvu-0.1/tvu/__init__.py
@@ -0,0 +1,26 @@
+import sys
+
+
+class TVUModule(object):
+
+    def __init__(self, name):
+        self.__name__ = name
+
+        import tvu._tvu
+        import tvu.tvus
+
+        self.tvus = tvu.tvus
+
+        self.TVU = tvu._tvu.TVU
+        self.EnumTVU = tvu._tvu.EnumTVU
+
+        self.instance = tvu._tvu.instance
+        self.nullable = tvu._tvu.nullable
+
+        self._validate_and_unify = tvu._tvu.validate_and_unify
+
+    def __call__(self, **kwargs):
+        return self._validate_and_unify(**kwargs)
+
+
+sys.modules[__name__] = TVUModule(__name__)
---
+++ packages/tvu/tvu-0.1.tar.gz/tvu-0.1/tvu/tvus.py
@@ -0,0 +1,71 @@
+from tvu._tvu import TVU
+from tvu._compat import text
+
+
+def bounded_int(minimum=None, maximum=None):
+
+    class BoundedInt(TVU):
+
+        TYPES = (int, float)
+
+        def unify(self, value):
+            if isinstance(value, float) and round(value) != value:
+                self.error(u'a round number')
+            return int(value)
+
+        def validate(self, value):
+            if minimum is not None and maximum is not None\
+               and not (minimum <= value <= maximum):
+                err_msg = \
+                    u'an integer between %d and %d (inclusive)' % (minimum,
+                                                                   maximum)
+                self.error(err_msg)
+            elif minimum is not None and value < minimum:
+                err_msg = u'an integer greater or equal to %d' % (minimum,)
+                self.error(err_msg)
+            elif maximum is not None and value > maximum:
+                err_msg = u'an integer lesser or equal to %d' % (maximum,)
+                self.error(err_msg)
+
+    return BoundedInt
+
+PositiveInt = bounded_int(minimum=1)
+
+
+class Iterable(TVU):
+
+    def type_check(self):
+        value = self._value
+        try:
+            iter(value)
+            return
+        except TypeError:
+            err_msg = u'%s must be iterable, not %s' % (self._variable_name,
+                                                        text(self._value))
+            raise TypeError(err_msg)
+
+
+class Text(TVU):
+
+    TYPES = (text, bytes)
+
+    def error(self, msg, soft=False):
+        word = u'could' if soft else u'must'
+        # skip adding unicode(value) to err_msg because there are problems
+        # converting it to unicode
+        raise ValueError((u'%s %s be ' + msg) % (self._variable_name, word))
+
+    def unify(self, value):
+        if isinstance(value, bytes):
+            try:
+                return value.decode('ascii')
+            except UnicodeDecodeError:
+                self.error(u'unicode text, or ascii-only bytestring')
+        return value
+
+
+class NonEmptyText(Text):
+
+    def validate(self, value):
+        if value is u'':
+            self.error(u'non-empty string')
