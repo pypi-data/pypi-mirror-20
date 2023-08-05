97bd5610860cb9824c3872f27056787d2520eaa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:02 +0000

    Add package dml/0.0.16.0/dml-0.0.16.0.zip

---
+++ packages/dml/dml-0.0.16.0.zip/dml-0.0.16.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name             = 'dml',
+    version          = '0.0.16.0',
+    packages         = ['dml',],
+    install_requires = ['pymongo',],
+    license          = 'MIT License',
+	url              = 'http://datamechanics.org',
+	author           = 'Andrei Lapets',
+	author_email     = 'a@lapets.io',
+    description      = 'Common functionalities for building Data Mechanics platform components.',
+    long_description = open('README').read(),
+)
