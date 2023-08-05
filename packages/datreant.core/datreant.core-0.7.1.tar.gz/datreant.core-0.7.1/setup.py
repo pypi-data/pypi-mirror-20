ccbf19c2a74f487347bb3e731c00f3fb7cff8173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:52 +0000

    Add package datreant.core/0.7.1/datreant.core-0.7.1.tar.gz

---
+++ packages/datreant.core/datreant.core-0.7.1.tar.gz/datreant.core-0.7.1/setup.py
@@ -0,0 +1,35 @@
+#! /usr/bin/python
+"""Setuptools-based setup script for datreant.
+
+For a basic installation just type the command::
+
+  python setup.py install
+
+"""
+
+from setuptools import setup, find_packages
+
+setup(name='datreant.core',
+      version='0.7.1',
+      description='persistent, pythonic trees for heterogeneous data',
+      author='David Dotson',
+      author_email='dotsdl@gmail.com',
+      url = 'http://datreant.org/',
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+      packages=find_packages('src'),
+      namespace_packages=['datreant'],
+      package_dir={'': 'src'},
+      scripts=[],
+      license='BSD',
+      long_description=open('README.rst').read(),
+      install_requires=['asciitree', 'pathlib2', 'scandir', 'six', 'fuzzywuzzy']
+      )
