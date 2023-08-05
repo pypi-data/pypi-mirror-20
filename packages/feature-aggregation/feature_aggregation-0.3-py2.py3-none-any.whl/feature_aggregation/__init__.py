aca953390b483eb7af95f2611c7d871bda4bad02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:21 +0000

    Add package feature-aggregation/0.3/feature_aggregation-0.3-py2.py3-none-any.whl

---
+++ packages/feature-aggregation/feature_aggregation-0.3-py2.py3-none-any.whl/feature_aggregation/__init__.py
@@ -0,0 +1,9 @@
+
+from bow import BagOfWords
+from fv import FisherVectors
+from llc import LLC
+from vlad import Vlad
+
+
+__version__ = "0.3"
+__all__ = ["BagOfWords", "FisherVectors", "LLC", "Vlad"]
