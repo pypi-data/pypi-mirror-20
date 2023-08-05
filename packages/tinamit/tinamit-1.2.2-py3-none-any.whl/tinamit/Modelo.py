aaaf7375215f36089451cb6ba1b6b40182507f81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:35 +0000

    Add package tinamit/1.2.2/tinamit-1.2.2-py3-none-any.whl

---
+++ packages/tinamit/tinamit-1.2.2-py3-none-any.whl/tinamit/Modelo.py
@@ -0,0 +1,134 @@
+
+
+class Modelo(object):
+    """
+    Todas las cosas en Tinamit son instancias de Modelo, que sea un modelo de dinámicas de los sistemas, un modelo de
+    cultivos o de suelos o de clima, o un modelo conectado.
+    Cada tipo de modelo se representa por subclases específicas. Por eso, la gran mayoría de los métodos definidos
+    aquí se implementan de manera independiente en cada
+    """
+
+    def __init__(símismo, nombre):
+        """
+        La función de inicialización de todos modelos, conectados o no.
+
+        :param nombre: El nombre del modelo. Sirve para identificar distintos modelos en un modelo conectado.
+        :type nombre: str
+
+        """
+
+        # No se puede incluir nombres de modelos con "_" en el nombre (podría corrumpir el manejo de variables en
+        # modelos jerarquizados).
+        if "_" in nombre:
+            raise ValueError('No se pueden emplear nombres de modelos con "_".')
+
+        # El nombre del modelo (sirve como una referencia a este modelo en el modelo conectado).
+        símismo.nombre = nombre
+
+        # El diccionario de variables necesita la forma siguiente. Se llena con la función símismo.inic_vars().
+        # {var1: {'val': 13, 'unidades': cm, 'ingreso': True, 'egreso': True},
+        #  var2: {...},
+        #  ...}
+        símismo.variables = {}
+        símismo.inic_vars()
+
+        # Listas de los nombres de los variables que sirven de conexión con otro modelo.
+        símismo.vars_saliendo = []
+        símismo.vars_entrando = []
+
+        # Las unidades de tiempo del modelo.
+        símismo.unidad_tiempo = símismo.obt_unidad_tiempo()
+
+    def inic_vars(símismo):
+        """
+        Esta función debe poblar el diccionario de variables del modelo, según la forma siguiente::
+            {'var1': {'val': 13, 'unidades': 'cm', 'ingreso': True, 'egreso': True},
+            'var2': {'val': 2, 'unidades': 'cm', 'ingreso': False, 'egreso': True}}
+            }
+
+        """
+        raise NotImplementedError
+
+    def obt_unidad_tiempo(símismo):
+        """
+        Esta función debe devolver la unidad de tiempo empleada por el modelo.
+
+        :return: La unidad de tiempo (p. ejemplo, 'meses', 'días', etc.
+        :rtype: str
+
+        """
+        raise NotImplementedError
+
+    def iniciar_modelo(símismo, tiempo_final, nombre_corrida):
+        """
+        Esta función llama cualquier acción necesaria para preparar el modelo para la simulación.
+        Si no es necesario, usar "pass".
+
+        :param tiempo_final: El tiempo final de la simulación.
+        :type tiempo_final: int
+
+        :param nombre_corrida: El nombre de la corrida (generalmente para guardar resultados).
+        :type nombre_corrida: str
+
+        """
+        raise NotImplementedError
+
+    def incrementar(símismo, paso):
+        """
+        Esta función debe avanzar el modelo por un periodo de tiempo especificado.
+
+        :param paso: El paso.
+        :type paso: int
+
+        """
+        raise NotImplementedError
+
+    def leer_vals(símismo):
+        """
+        Esta función debe leer los valores del modelo y escribirlos en el diccionario interno de variables. Se
+        implementa frequentement con modelos externos de cuyos egresos hay que leer los resultados de una corrida.
+
+        """
+        raise NotImplementedError
+
+    def cambiar_vals(símismo, valores):
+        """
+        Esta función cambiar el calor de uno o más variables del modelo. Cambia primero el valor en el diccionario
+        interno del :class:`Modelo`, y después llama la función :func:`~Modelo.Modelo.cambiar_vals_modelo` para cambiar,
+        si necesario, los valores de los variables en el modelo externo.
+
+        :param valores: Un diccionario de variables y valores para cambiar, con el formato siguiente:
+            { var1: nuevovalor,
+            var2: nuevovalor,
+            ...
+            }
+        :type valores: dict
+
+        """
+
+        for var in valores:
+            símismo.variables[var]['val'] = valores[var]
+
+        símismo.cambiar_vals_modelo_interno(valores=valores)
+
+    def cambiar_vals_modelo_interno(símismo, valores):
+        """
+        Esta función debe cambiar el valor de variables en el :class:`Modelo`, incluso tomar acciones para asegurarse
+        de que el cambio se hizo en el modelo externo, si aplica.
+
+        :param valores: Un diccionario de variables y valores para cambiar, con el formato siguiente:
+            {var1: nuevovalor,
+            var2: nuevovalor,
+            ...
+            }
+        :type valores: dict
+
+        """
+        raise NotImplementedError
+
+    def cerrar_modelo(símismo):
+        """
+        Esta función debe tomar las acciones necesarias para terminar la simulación y cerrar el modelo, si aplica.
+        Si no aplica, usar "pass".
+        """
+        raise NotImplementedError
---
+++ packages/tinamit/tinamit-1.2.2-py3-none-any.whl/tinamit/Unidades/Unidades.py
@@ -0,0 +1,148 @@
+import json
+import pkg_resources
+import re
+
+# Buscar el archivo de conversiones.
+archivo_json = pkg_resources.resource_filename('tinamit.Unidades', 'equiv_unid.json')
+
+# Leer las conversiones.
+with open(archivo_json, 'r', encoding='utf8') as d:
+    dic_doc = json.load(d)
+    dic_conv = dic_doc['conv']
+    dic_equiv = dic_doc['equiv']
+
+
+def convertir(de, a,  val=1, clase=None, lengua=None):
+    """
+    Esta función convierte un valor de una unidad a otra.
+
+    :param de: La unidad original.
+    :type de: str
+
+    :param a: La unidad final.
+    :type a: str
+
+    :param clase: El tipo de unidad ('tiempo', 'distancia', etc.) No es obligatoria.
+    :type clase: str
+
+    :param val: El valor para convertir.
+    :type val: float
+
+    :param lengua: La lengua en la cuál se están especificando las unidades.
+    :type lengua: str
+
+    :return: El valor convertido.
+    :rtype: float
+
+    """
+
+    # Estar seguro que diferencias en mayúsculos o espacios no causen problemas
+    de = de.lower().replace(' ', '')
+    a = a.lower().replace(' ', '')
+
+    # Si son las mismas unidades, no hay nada que hacer, por supuesto.
+    if de == a:
+        return val
+
+    # Asegurarse que la clase de unidades sea una lista.
+    if clase is None:
+        # Si no se especificó nada, buscar en todas las clases.
+        clases = list(dic_conv)
+    elif type(clase) is list:
+        clases = clase
+    else:
+        clases = [clase]
+
+    if lengua is None:
+        lenguas = list(dic_equiv)
+    elif type(lengua) is list:
+        lenguas = lengua
+    else:
+        lenguas = [lengua]
+
+    #
+    de = 'm2/s'
+    re_unid = r'(?P<unid>[^\W\d]+)(?P<exp>\-?(?=\d)?[\d]*)'
+    regex = r'{}+(?P<oper>[\*\/]?)'.format(re_unid)
+    unidades_simp = re.finditer(regex, de)
+    lista = list(unidades_simp)
+    for l in lista:
+        print(l.group('unid'))
+        print(l.group('exp'))
+        print(l.group('oper'))
+
+    factor = 1
+    for u in unidades_simp:
+        conv = convertir_unid_senc(de=u['de'], a=u['a'], clases=clases, lenguas=lenguas)
+        factor *= conv**u['exp']
+
+    return factor * val
+
+
+def convertir_unid_senc(de, a, clases, lenguas):
+    """
+    Esta función convierte una unidad sencilla a otra.
+
+    :param de: La unidad original.
+    :type de: str
+
+    :param a: La unidad final.
+    :type a: str
+
+    :param clases: El tipo de unidad ('tiempo', 'distancia', etc.)
+    :type clases: list
+
+    :param lenguas: La lengua en la cuál se están especificando las unidades.
+    :type lenguas: list
+
+    :return: El valor convertido.
+    :rtype: float
+
+    """
+
+    for leng in lenguas:
+        for c in clases:
+            try:
+                for unid, equivs in dic_equiv[leng][c].items():
+                    if de in equivs:
+                        de = unid
+                    if a in equivs:
+                        a = unid
+            except KeyError:
+                pass
+
+    factor = None
+
+    for c in clases:
+        try:
+            dic = dic_conv[c]
+        except KeyError:
+            raise KeyError('Clase de unidades "{}" no reconocida. Debe ser uno de:\n{}.'.format(
+                clases,
+                ', '.join(list(dic_conv))
+            ))
+
+        if de in dic:
+            if a in dic[de]:
+                factor = dic[de][a]
+        if factor is None:
+            if a in dic:
+                if de in dic[a]:
+                    factor = 1/dic[a][de]
+
+        if factor is None:
+            # Verificar si las dos unidades tendría conexión a una misma tercera unidad (por ejemplo,
+            # cm y mm están vinculados a m, y por su relacion a m se podría establecer la relación entre ambos.)
+            for unid in dic:
+                if a in dic[unid] and de in dic[unid]:
+                    factor = dic[unid][a] / dic[unid][de]
+                    break
+
+        if factor is not None:
+            break
+
+    if factor is None:
+        raise ValueError('Las unidades de tiempo de los dos modelos ({} y {}) son incompatibles. Si'
+                         'te parece irrazonable, puedes quejarte a nosotros.'.format(de, a))
+
+    return factor
