bdcc6226837c5aa785e5f1d79c6b480893292c1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:24 +0000

    Add package fedfind/3.4.2/fedfind-3.4.2.tar.gz

---
+++ packages/fedfind/fedfind-3.4.2.tar.gz/fedfind-3.4.2/release.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+baddeps=""
+# check deps
+rpm -qi python2-setuptools > /dev/null 2>&1 || baddeps="python2-setuptools"
+rpm -qi python2-setuptools_git > /dev/null 2>&1 || baddeps="${baddeps} python2-setuptools_git"
+if [ -n "${baddeps}" ]; then
+    echo "${baddeps} must be installed!"
+    exit 1
+fi
+
+if [ "$#" != "1" ]; then
+    echo "Must pass release version!"
+    exit 1
+fi
+
+version=$1
+name=fedfind
+sed -i -e "s,version = \".*\",version = \"$version\", g" setup.py
+sed -i -e "s,__version__ = \".*\",__version__ = \"${version}\", g" ${name}/__init__.py
+git add setup.py ${name}/__init__.py
+git commit -s -m "Release $version"
+git push
+git tag -a -m "Release $version" $version
+git push origin $version
+python ./setup.py sdist --formats=tar
+xz dist/$name-$version.tar
+# this bit only works for me, of course...
+scp -pr dist/$name-$version.tar.xz www.happyassassin.net:~/public_html/$name/releases
