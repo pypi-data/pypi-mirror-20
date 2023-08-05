46809fea487347a44532684cdc04c2ac06480dd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:10 +0000

    Add package hurriyetlog/0.7/hurriyetlog-0.7.zip

---
+++ packages/hurriyetlog/hurriyetlog-0.7.zip/hurriyetlog-0.7/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name="hurriyetlog",
+	packages = ["hurriyetlog"],
+    version="0.7",
+    description="Hurriyet.com.tr merkezi log kutuphanesine loglamak icin kullanabilirsiniz",
+
+    author="hurriyet.com.tr",
+    author_email="destek@hurriyet.com.tr",
+
+    url="http://www.hurriyet.com.tr",
+    license="MIT"
+)
