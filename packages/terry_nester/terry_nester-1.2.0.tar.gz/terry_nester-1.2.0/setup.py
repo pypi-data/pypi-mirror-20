5bb26d830527167e013700998da9f882f438f3f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:03 +0000

    Add package terry_nester/1.2.0/terry_nester-1.2.0.tar.gz

---
+++ packages/terry_nester/terry_nester-1.2.0.tar.gz/terry_nester-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name        = 'terry_nester',
+        version     = '1.2.0',
+        py_module   = ['terry_nester'],
+        author      = 'terry.zeng',
+        author_email= 'terryzeng98@gmail.com',
+        url         = 'http://www.headfirstlabs.com',
+        description = 'A simple printer of nested lists',
+      )
