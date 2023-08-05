1abffb094c5a3ccd9b71bc6154ccdfa5c91f48c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:30 +0000

    Add package emrt.necd.content/2.0/emrt.necd.content-2.0-py2.7.egg

---
+++ packages/emrt.necd.content/emrt.necd.content-2.0-py2.7.egg/emrt/necd/content/notifications/observation_finalisation_denied.py
@@ -0,0 +1,25 @@
+from emrt.necd.content.observation import IObservation
+from five import grok
+from Products.CMFCore.interfaces import IActionSucceededEvent
+from Products.Five.browser.pagetemplatefile import PageTemplateFile
+from utils import notify
+
+
+@grok.subscribe(IObservation, IActionSucceededEvent)
+def notification_rev_ph2(context, event):
+    """
+    To:     NECDReviewer
+    When:   Observation finalisation denied
+    """
+    _temp = PageTemplateFile('observation_finalisation_denied.pt')
+
+    if event.action in ['deny-finishing-observation']:
+        observation = context
+        subject = u'Observation finalisation denied'
+        notify(
+            observation,
+            _temp,
+            subject,
+            'NECDReviewer',
+            'observation_finalisation_denied'
+        )
---
+++ packages/emrt.necd.content/emrt.necd.content-2.0-py2.7.egg/emrt/necd/content/notifications/question_to_counterpart.py
@@ -0,0 +1,46 @@
+from Acquisition import aq_parent
+from emrt.necd.content.question import IQuestion
+from five import grok
+from Products.CMFCore.interfaces import IActionSucceededEvent
+from Products.Five.browser.pagetemplatefile import PageTemplateFile
+from utils import notify
+
+
+@grok.subscribe(IQuestion, IActionSucceededEvent)
+def notification_cp(context, event):
+    """
+    To:     CounterParts
+    When:   New draft question to comment on
+    """
+    _temp = PageTemplateFile('question_to_counterpart.pt')
+
+    if event.action in ['request-for-counterpart-comments']:
+        observation = aq_parent(context)
+        subject = u'New draft question to comment'
+        notify(
+            observation,
+            _temp,
+            subject,
+            role='CounterPart',
+            notification_name='question_to_counterpart'
+        )
+
+
+@grok.subscribe(IQuestion, IActionSucceededEvent)
+def notification_lr(context, event):
+    """
+    To:     LeadReviewer
+    When:   New draft question to comment on
+    """
+    _temp = PageTemplateFile('question_to_counterpart.pt')
+
+    if event.action in ['request-for-counterpart-comments']:
+        observation = aq_parent(context)
+        subject = u'New draft question to comment'
+        notify(
+            observation,
+            _temp,
+            subject,
+            role='LeadReviewer',
+            notification_name='question_to_counterpart'
+        )
