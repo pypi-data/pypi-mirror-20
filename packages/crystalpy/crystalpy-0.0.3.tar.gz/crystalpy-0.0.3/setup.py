2f499570dad09cf5eef1cd51dd1180c8b717f953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:21 +0000

    Add package crystalpy/0.0.3/crystalpy-0.0.3.tar.gz

---
+++ packages/crystalpy/crystalpy-0.0.3.tar.gz/crystalpy-0.0.3/setup.py
@@ -0,0 +1,31 @@
+
+__authors__ = ["E Cappelli, M Glass, M Sanchez del Rio - ESRF ISDD Advanced Analysis and Modelling"]
+__license__ = "MIT"
+__date__ = "23/11/2016"
+
+from setuptools import setup
+#
+# memorandum (for pypi)
+#
+# python setup.py sdist upload
+
+
+
+setup(name='crystalpy',
+      version='0.0.3',
+      description='Python crystal polarization calcution',
+      author='Edoardo Cappelli, Mark Glass, Manuel Sanchez del Rio',
+      author_email='srio@esrf.eu',
+      url='https://github.com/edocappelli/crystalpy/',
+      packages=['crystalpy',
+                'crystalpy.util',
+                'crystalpy.diffraction',
+                'crystalpy.polarization',
+                'crystalpy.examples',
+                'crystalpy.tests'],
+      install_requires=[
+                        'numpy',
+                        'scipy'
+                       ],
+      test_suite='tests'
+      )
