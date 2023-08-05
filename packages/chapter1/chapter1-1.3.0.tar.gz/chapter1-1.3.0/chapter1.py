59a980cf5f132a89c182febb4ebb452dfefbe026
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:17 +0000

    Add package chapter1/1.3.0/chapter1-1.3.0.tar.gz

---
+++ packages/chapter1/chapter1-1.3.0.tar.gz/chapter1-1.3.0/chapter1.py
@@ -0,0 +1,17 @@
+# movies = ["the holy grail", 1975, "the life of brain", 91,
+#           ["the meaning of life", ["Graham Chapman", "Micheal Palin", "John Cleese"]]]
+
+
+def print_lol(the_list, indent=False, level=0):
+    for movie in the_list:
+        if isinstance(movie, list):
+            level = level + 1
+            print_lol(movie, indent,level)
+        else:
+            if indent:
+                for num in range(level):
+                    print("\t", end='')
+            print(movie, level)
+
+
+# print_lol(movies, 0)
---
+++ packages/chapter1/chapter1-1.3.0.tar.gz/chapter1-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='chapter1',
+    version='1.3.0',
+    py_modules=['chapter1'],
+    author='mrzhaopython',
+    author_email='zhaomengru2015@gmail.com',
+    url='http://www.headfirstlabs.com',
+    description='A simple printer of nested lists'
+)
