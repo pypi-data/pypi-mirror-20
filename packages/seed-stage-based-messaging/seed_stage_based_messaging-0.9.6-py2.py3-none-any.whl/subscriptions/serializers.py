3981103823d300dd02482fae65202494eed3ecc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:17 +0000

    Add package seed-stage-based-messaging/0.9.6/seed_stage_based_messaging-0.9.6-py2.py3-none-any.whl

---
+++ packages/seed-stage-based-messaging/seed_stage_based_messaging-0.9.6-py2.py3-none-any.whl/subscriptions/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+
+from .models import Subscription, SubscriptionSendFailure
+
+
+class CreateUserSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Subscription
+        read_only_fields = ('url', 'id', 'created_at', 'updated_at')
+        fields = (
+            'url', 'id', 'version', 'identity', 'messageset',
+            'next_sequence_number', 'lang', 'active', 'completed', 'schedule',
+            'process_status', 'metadata', 'created_at', 'updated_at',
+            'initial_sequence_number')
+
+
+class SubscriptionSendFailureSerializer(
+        serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = SubscriptionSendFailure
+        fields = ('url', 'id', 'subscription', 'task_id',
+                  'initiated_at', 'reason')
