6bad1fb787f2b0d1a215d18db149d6ebf45a999d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:40 +0000

    Add package shaman/0.0.7.dev1/shaman-0.0.7.dev1-py2.py3-none-any.whl

---
+++ packages/shaman/shaman-0.0.7.dev1-py2.py3-none-any.whl/shaman/__init__.py
@@ -0,0 +1,16 @@
+__all__ = (
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+)
+
+__title__ = "shaman"
+__summary__ = "Multiprocessing application to download and analyze a content of an html pages."
+__uri__ = 'https://github.com/Landish145/shaman'
+
+__version__ = "0.0.7.dev1"
+
+__author__ = 'eugtsa,azraev'
+__email__ = 'eugtsa@gmail.com,azraev@gmail.com'
+
+__license__ = "MIT"
+__copyright__ = "Copyright 2017, eugtsa, azraev"
