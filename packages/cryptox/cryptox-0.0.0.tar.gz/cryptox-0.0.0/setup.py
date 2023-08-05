a49e8dcc29603bbf6e29cf355f11b46312960b9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:51 +0000

    Add package cryptox/0.0.0/cryptox-0.0.0.tar.gz

---
+++ packages/cryptox/cryptox-0.0.0.tar.gz/cryptox-0.0.0/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='cryptox',
+    packages = ['cryptox'],
+    version='0.0.0',
+    author='Benjamin Levine',
+    author_email='benjamin.r.levine@gmail.com',
+    url='https://bitbucket.org/blevine11/cryptox',
+    license='MIT',
+    description = 'Python3 package for working with various crypto currency exchanges',
+    classifiers=[
+        'Programming Language :: Python :: 3 :: Only',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Financial and Insurance Industry'
+    ],
+    keywords='Python3 Cryptocurrency exchange framework'
+)
