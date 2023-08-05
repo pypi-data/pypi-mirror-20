a7c197499848893512d372fdcdbcf779bd0a0b9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:09 +0000

    Add package lambda_utils/0.1.12/lambda_utils-0.1.12.tar.gz

---
+++ packages/lambda_utils/lambda_utils-0.1.12.tar.gz/lambda_utils-0.1.12/lambda_utils/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+__author__ = """CloudHeads"""
+__email__ = 'theguys@cloudheads.io'
+__version__ = '0.1.12'
+
+
+class Event(object):
+    function = None
+    wrapped_function = None
+
+    def __call__(self, function):
+        self.function = function
+        return self.wrapped_function
