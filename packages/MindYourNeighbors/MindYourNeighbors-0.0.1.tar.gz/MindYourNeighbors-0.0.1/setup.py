8f0ecec7aab7a383c6b37eb5a1d3f1f712b9473c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:39 +0000

    Add package MindYourNeighbors/0.0.1/MindYourNeighbors-0.0.1.tar.gz

---
+++ packages/MindYourNeighbors/MindYourNeighbors-0.0.1.tar.gz/MindYourNeighbors-0.0.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+
+setup(name='MindYourNeighbors',
+      version='0.0.1',
+      description='IP Neighbors table watching script',
+      keywords='ip-neigh arp',
+      classifiers=[
+          "Intended Audience :: System Administrators",
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+          "Topic :: System :: Networking :: Monitoring :: Hardware Watchdog",
+          "Topic :: System :: Networking"],
+      license="GPLv3",
+      author="François Schmidts",
+      author_email="francois@schmidts.fr",
+      maintainer="François Schmidts",
+      maintainer_email="francois@schmidts.fr",
+      scripts=['src/mind_your_neighbors'],
+      url='https://github.com/jaesivsm/MindYourNeighbors',
+      data_files=[('/etc/systemd/system', ['dynhost.service'])],
+      )
