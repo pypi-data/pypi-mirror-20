aaf35b11858d0461062ba6a3a8ebc0030fd4f12b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:18 +0000

    Add package telega-megaimport/0.5.2/telega_megaimport-0.5.2.tar.gz

---
+++ packages/telega-megaimport/telega_megaimport-0.5.2.tar.gz/telega_megaimport-0.5.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(name='telega_megaimport',
+      version='0.5.2',
+      description='Django app for creating parsers',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Code Generators',
+        'Topic :: Database',
+        'Topic :: Office/Business',
+        'Framework :: Django'
+      ],
+      keywords='import csv xls xlsx django',
+      url='http://github.com/django-stars/telega-mega-import',
+      author='Andrew Liashchuk @ DjangoStars',
+      author_email='andrew.luashchuk@djangostars.com',
+      license='MIT',
+      packages=['telega_megaimport'],
+      install_requires=[
+          "Django>=1.7",
+          "xlrd",
+      ],
+      include_package_data=True,
+      zip_safe=False)
