f5d761149dfd91806cdbd48229399adbd1057f7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:43 +0000

    Add package tinker-access-client/2017.2.14.418/tinker-access-client-2017.2.14.418.tar.gz

---
+++ packages/tinker-access-client/tinker-access-client-2017.2.14.418.tar.gz/tinker-access-client-2017.2.14.418/tinker_access_client/Command.py
@@ -0,0 +1,20 @@
+class Command(object):
+    #TODO: add some descriptions
+    START = dict(command='start', description='start desc...')
+    STOP = dict(command='stop', description='stop desc...')
+    STATUS = dict(command='status', description='status desc...')
+    RESTART = dict(command='restart', description='restart...')
+    UPDATE = dict(command='update', description='update...')
+    # RELOAD = dict(command='reload', description='reload...')
+    # REMOVE = dict(command='remove', description='remove...')
+    # UNINSTALL = dict(command='uninstall', description='uninstall...')
+    # RECONFIGURE = dict(command='re-configure', description='re-configure...')
+    # FORCE_RELOAD = dict(command='force_reload', description='force_reload...')
+
+    def __new__(self, command):
+        for key, value in vars(Command).items():
+            if not key.startswith('__'):
+                if command == value['command']:
+                    return value
+
+        return None
