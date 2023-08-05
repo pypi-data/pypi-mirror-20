4ea9c724e8a748c29901fc552ae2f1af73808fef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:14 +0000

    Add package quality-report/1.77.6/quality_report-1.77.6.tar.gz

---
+++ packages/quality-report/quality_report-1.77.6.tar.gz/quality_report-1.77.6/tests/integrationtests/test_no_source_ids/project_definition.py
@@ -0,0 +1,74 @@
+""" Project definition for testing a project with all requirements and sources, but no source ids. """
+
+from hqlib import metric_source, requirement
+from hqlib.domain import Project, Product, Application, Component, Team, Document, Person
+
+# Sources
+HISTORY = metric_source.History('tests/integrationtests/test_no_source_ids/history.json')
+GIT = metric_source.Git(url='http://git/repo')
+SONAR = metric_source.Sonar('http://sonar/')
+JIRA = metric_source.Jira(url='http://jira/', username='user', password='pass')
+
+# The project
+PROJECT = Project(
+    'Integrationtest', name='all requirements and sources, but no source ids',
+    metric_sources={
+        metric_source.History: HISTORY,
+        metric_source.ArchiveSystem: GIT,
+        metric_source.VersionControlSystem: GIT,
+        metric_source.Jira: JIRA,
+        metric_source.Sonar: SONAR},
+    requirements=[requirement.TrustedProductMaintainability, requirement.Web, requirement.JavaScript,
+                  requirement.CSharp, requirement.Java, requirement.TrackManualLTCs,
+                  requirement.TrackBugs, requirement.TrackTechnicalDebt,
+                  requirement.TrackActions, requirement.TrackRisks, requirement.TrackSecurityAndPerformanceRisks,
+                  requirement.TrackReadyUS, requirement.TrackJavaConsistency,
+                  requirement.TrackCIJobs, requirement.TrackSonarVersion, requirement.OpenVAS])
+
+# Documents.
+DOC = Document(name='Document XYZ')
+PROJECT.add_document(DOC)
+
+# Teams of the project.
+TEAM = Team(
+    name='Team 123',
+    requirements=[requirement.ScrumTeam, requirement.TrackSpirit, requirement.TrackAbsence])
+TEAM.add_member(Person(name='Person 1'))
+TEAM.add_member(Person(name='Person 2'))
+PROJECT.add_team(TEAM)
+
+# Products the project(s) develop(s).
+PRODUCT = Product(
+    PROJECT, 'PR', name='Product ABC',
+    requirements=[requirement.OWASPDependencies, requirement.OWASPZAP,
+                  requirement.UserStoriesAndLTCs, requirement.UnitTests, requirement.ART, requirement.ARTCoverage,
+                  requirement.CodeQuality, requirement.PerformanceLoad, requirement.PerformanceEndurance,
+                  requirement.PerformanceScalability, requirement.TrackBranches],
+    art=Product(
+        PROJECT, name='Product ABC ART', requirements=[requirement.CodeQuality, requirement.TrackBranches]),
+    jsf=Product(
+        PROJECT, name='Product ABC JSF', requirements=[requirement.JSFCodeQuality]))
+
+
+APPLICATION = Application(PROJECT, 'AP', name='Application FOO')
+COMPONENT = Component(PROJECT, 'CO', name='Component FOO')
+
+PROJECT.add_product(PRODUCT)
+PROJECT.add_product(APPLICATION)
+PROJECT.add_product(COMPONENT)
+
+# Dashboard layout
+
+# Columns in the dashboard is specified as a list of tuples. Each tuple
+# contains a column header and the column span.
+DASHBOARD_COLUMNS = [('Products', 1), ('Teams', 1), ('Algemeen', 2)]
+
+# Rows in the dashboard is a list of row tuples. Each row tuple consists of
+# tuples that describe a cell in the dashboard. Each cell is a tuple containing
+# the product or team and the color. Optionally the cell tuple can contain a
+# third value which is a tuple containing the column and row span for the cell.
+DASHBOARD_ROWS = [((PRODUCT, 'lightsteelblue'), (TEAM, 'lavender', (1, 3)), ('PD', 'lightgrey'), ('PE', 'lightgrey')),
+                  ((APPLICATION, 'lightsteelblue'), ('PC', 'lightgrey', (1, 2)), ('MM', 'lightgrey', (1, 2))),
+                  ((COMPONENT, 'lightsteelblue'),)]
+
+PROJECT.set_dashboard(DASHBOARD_COLUMNS, DASHBOARD_ROWS)
---
+++ packages/quality-report/quality_report-1.77.6.tar.gz/quality_report-1.77.6/tests/unittests/metric/product/analysis_age_tests.py
@@ -0,0 +1,77 @@
+"""
+Copyright 2012-2017 Ministerie van Sociale Zaken en Werkgelegenheid
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import datetime
+import unittest
+
+from hqlib import metric, domain, metric_source
+
+
+class FakeSonar(object):
+    """ Provide for a fake Sonar object so that the unit test don't need access to an actual Sonar instance. """
+    # pylint: disable=unused-argument
+
+    metric_source_name = metric_source.Sonar.metric_source_name
+    needs_metric_source_id = metric_source.Sonar.needs_metric_source_id
+
+    def __init__(self, age=10):
+        self.age = age
+
+    def datetime(self, *args):
+        """ Return a fake analysis date time. """
+        if self.age == -1:
+            return datetime.datetime.min
+        else:
+            return datetime.datetime.now() - datetime.timedelta(days=self.age, hours=12)
+
+    @staticmethod
+    def dashboard_url(*args):
+        """ Return a fake dashboard url. """
+        return 'http://sonar'
+
+    url = violations_url = dashboard_url
+
+
+class SonarAnalysisAgeTest(unittest.TestCase):
+    """ Unit tests for the sonar analysis age metric. """
+
+    def setUp(self):
+        self.__sonar = FakeSonar()
+        project = domain.Project(metric_sources={metric_source.Sonar: self.__sonar})
+        subject = domain.Product(project, 'PR', name='FakeSubject', metric_source_ids={self.__sonar: 'sonar_id'})
+        self.__metric = metric.SonarAnalysisAge(subject=subject, project=project)
+
+    def test_value(self):
+        """ Test that the value of the metric equals the date of the latest analysis. """
+        self.assertEqual(10, self.__metric.value())
+
+    def test_value_today(self):
+        """ Test that the value of the metric equals zero if the latest analysis was done today. """
+        self.__sonar.age = 0
+        self.assertEqual(0, self.__metric.value())
+
+    def test_missing_analysis(self):
+        """ Test that the value of the metric is -1 when there is no analysis. """
+        self.__sonar.age = -1
+        self.assertEqual(-1, self.__metric.value())
+
+    def test_url(self):
+        """ Test that the url is correct. """
+        self.assertEqual({FakeSonar.metric_source_name: FakeSonar.dashboard_url()}, self.__metric.url())
+
+    def test_report(self):
+        """ Test that the report of the metric is correct. """
+        self.assertEqual('De meest recente Sonar analyse van FakeSubject is 10 dagen oud.', self.__metric.report())
