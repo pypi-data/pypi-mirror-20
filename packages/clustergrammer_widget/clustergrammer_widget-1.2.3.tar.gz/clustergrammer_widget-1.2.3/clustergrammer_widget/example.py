f64d379c4d573b94fbb5c6856b4ef78fcf20e7ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:39 +0000

    Add package clustergrammer_widget/1.2.3/clustergrammer_widget-1.2.3.tar.gz

---
+++ packages/clustergrammer_widget/clustergrammer_widget-1.2.3.tar.gz/clustergrammer_widget-1.2.3/clustergrammer_widget/example.py
@@ -0,0 +1,16 @@
+import ipywidgets as widgets
+import json
+from traitlets import Unicode
+from clustergrammer import Network
+
+@widgets.register('hello.Hello')
+class clustergrammer_widget(widgets.DOMWidget):
+    """"""
+    _view_name = Unicode('hello_view').tag(sync=True)
+    _model_name = Unicode('hello_model').tag(sync=True)
+    _view_module = Unicode('clustergrammer_widget').tag(sync=True)
+    _model_module = Unicode('clustergrammer_widget').tag(sync=True)
+
+    viz_title = Unicode('updating python by restarting kernel').tag(sync=True)
+
+    network = Unicode('').tag(sync=True)
