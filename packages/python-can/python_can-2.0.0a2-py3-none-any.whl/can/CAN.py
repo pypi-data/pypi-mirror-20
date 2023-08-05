572a3c8b99f6929ae6e5b8c4647a311049f5a7f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:23 +0000

    Add package python-can/2.0.0a2/python_can-2.0.0a2-py3-none-any.whl

---
+++ packages/python-can/python_can-2.0.0a2-py3-none-any.whl/can/CAN.py
@@ -0,0 +1,16 @@
+"""
+This module was once the core of python-can, containing
+implementations of all the major classes in the library, now
+however all functionality has been refactored out. This api
+is left intact for version 2.0 to aide with migration.
+"""
+
+from can.message import Message
+from can.listener import Listener, BufferedReader, RedirectReader
+from can.util import set_logging_level
+from can.io import *
+
+import logging
+
+log = logging.getLogger('can')
+log.info("Loading python-can via the old CAN api")
