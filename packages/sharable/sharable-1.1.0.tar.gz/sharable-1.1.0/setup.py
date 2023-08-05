9fa4048c819b11e29e60dbebe7b76a2155a5fdac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:49 +0000

    Add package sharable/1.1.0/sharable-1.1.0.tar.gz

---
+++ packages/sharable/sharable-1.1.0.tar.gz/sharable-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name         = 'sharable',
+    version      = '1.1.0',
+    py_modules   = ['sharable'],
+    author       = 'geety249',
+    author_email = 'gauravtolani444@.com',
+    url          = 'http://google.com',
+    description  = 'A simple program to print the item of a list inside a list and so on with two additional arguments level and indentation'
+    )
+    
