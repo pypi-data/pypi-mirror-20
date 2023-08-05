31eb0d2a6137d35b802de4bf008d654205b170eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:02 +0000

    Add package django-blogging/1.2.9/django-blogging-1.2.9.tar.gz

---
+++ packages/django-blogging/django-blogging-1.2.9.tar.gz/django-blogging-1.2.9/blogging/utils.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""Utils functions for blogging app."""
+
+
+def tokenize(*args):
+    """Return the tokens for the string passed as parameters."""
+    raw_keywords = []
+    for arg in args:
+        raw_keywords += arg.split()
+    cleaned_keywords = []
+    for keyword in raw_keywords:
+        keyword = keyword.strip("(")
+        keyword = keyword.strip(")")
+        keyword = keyword.strip(",")
+        keyword = keyword.strip(".")
+        cleaned_keywords.append(keyword)
