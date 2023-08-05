c42b7f59a33f39da3c893d553da2538626fbb67b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:58 +0000

    Add package uriutil/0.1.2/uriutil-0.1.2.tar.gz

---
+++ packages/uriutil/uriutil-0.1.2.tar.gz/uriutil-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'uriutil',
+  packages = ['uriutil'],
+  version = '0.1.2',
+  description = 'A mediocre URI parsing lib',
+  author = 'Jason Goldberger',
+  author_email = 'jasongoldberger@gmail.com',
+  url = 'https://github.com/elbow-jason/uriutil',
+  keywords = ['uri', 'url'],
+  classifiers = [],
+)
