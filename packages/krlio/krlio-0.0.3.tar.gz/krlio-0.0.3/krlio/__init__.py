147eda2f2e9cddaae9c5b4346ad4c5a8aa3ffd99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:30 +0000

    Add package krlio/0.0.3/krlio-0.0.3.tar.gz

---
+++ packages/krlio/krlio-0.0.3.tar.gz/krlio-0.0.3/krlio/__init__.py
@@ -0,0 +1,24 @@
+import requests
+
+KRL_BASE = 'http://krl.io/v1/'
+
+class KrlExcpetion(Exception):
+    pass
+
+def get_version():
+    r = requests.get(KRL_BASE + 'version')
+    if not r.status_code == 200:
+        return KrlException('Could not get version: {}'.format(r.staus_code))
+    return r.json()['version']
+
+def get_link(code):
+    r = requests.get(KRL_BASE+'link/{}'.format(code))
+    if not r.status_code == 200:
+        return KrlException('Link not found!')
+    return r.json()['url']
+
+def make_anon_link(url):
+    r = requests.post(KRL_BASE+'link/{}'.format(url))
+    if not r.status_code == 200:
+        return KrlException('Could not shorten link: {}'.format(r.status_code))
+    return r.json()['url']
