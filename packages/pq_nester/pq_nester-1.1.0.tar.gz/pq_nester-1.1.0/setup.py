74a1493d30ced2ebf391c4b15d4439d956dc46da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:51 +0000

    Add package pq_nester/1.1.0/pq_nester-1.1.0.tar.gz

---
+++ packages/pq_nester/pq_nester-1.1.0.tar.gz/pq_nester-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='pq_nester',
+    version='1.1.0',
+    py_modules=['pq_nester'],
+    author='pqpython',
+    author_email='511367683@qq.com',
+    url='https://mail.qq.com',
+    description='A simple printer of nested lists',
+    )
