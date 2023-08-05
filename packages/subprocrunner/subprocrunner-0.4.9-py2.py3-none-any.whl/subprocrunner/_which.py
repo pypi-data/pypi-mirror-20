b0f46a64215c9bb6cee0877c2bae104bf902f431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:33 +0000

    Add package subprocrunner/0.4.9/subprocrunner-0.4.9-py2.py3-none-any.whl

---
+++ packages/subprocrunner/subprocrunner-0.4.9-py2.py3-none-any.whl/subprocrunner/_which.py
@@ -0,0 +1,47 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <gogogo.vm@gmail.com>
+"""
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import shutil
+
+import pytypeutil
+import six
+
+from ._error import CommandNotFoundError
+from ._error import InvalidCommandError
+
+
+class Which(object):
+
+    @property
+    def command(self):
+        return self.__command
+
+    def __init__(self, command):
+        if not pytypeutil.is_not_empty_string(command):
+            raise InvalidCommandError("invalid str: " + str(command))
+
+        self.__command = command
+
+    def is_exist(self):
+        if self.which() is None:
+            return False
+
+        return True
+
+    def verify(self):
+        if not self.is_exist():
+            raise CommandNotFoundError(
+                "command not found: '{}'".format(self.command))
+
+    def which(self):
+        if six.PY2:
+            from distutils.spawn import find_executable
+            return find_executable(self.command)
+
+        return shutil.which(self.command)
