fb31b37353b94d25ba2f14d699c1c62e9a8fcaa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:09 +0000

    Add package tap-gitlab/0.1.2/tap-gitlab-0.1.2.tar.gz

---
+++ packages/tap-gitlab/tap-gitlab-0.1.2.tar.gz/tap-gitlab-0.1.2/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import os.path
+
+
+setup(name='tap-gitlab',
+      version='0.1.2',
+      description='Taps GitLab data',
+      author='Stitch',
+      url='https://github.com/stitchstreams/tap-gitlab',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['tap_gitlab'],
+      install_requires=[
+          'stitchstream-python>=0.5.0',
+          'requests==2.12.4',
+          'strict-rfc3339==0.7',
+      ],
+      entry_points='''
+          [console_scripts]
+          tap-gitlab=tap_gitlab:main
+      ''',
+      packages=['tap_gitlab'],
+      package_data = {
+          'tap_gitlab': [
+            "branches.json",
+            "commits.json",
+            "issues.json",
+            "milestones.json",
+            "projects.json",
+            "users.json",
+          ],
+      }
+)
