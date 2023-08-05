9742053420700144734aea83ff48d5abef363b84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:52 +0000

    Add package tensorpy/1.0.11/tensorpy-1.0.11.tar.gz

---
+++ packages/tensorpy/tensorpy-1.0.11.tar.gz/tensorpy-1.0.11/setup.py
@@ -0,0 +1,30 @@
+"""
+The setup package to install TensorPy dependencies.
+*> This does NOT include TensorFlow installation.
+*> To install TensorFlow, use "./install.sh"
+"""
+
+from setuptools import setup, find_packages  # noqa
+
+setup(
+    name='tensorpy',
+    version='1.0.11',
+    url='http://tensorpy.com',
+    author='Michael Mintz',
+    author_email='@mintzworld',
+    maintainer='Michael Mintz',
+    description='Image Classification with TensorFlow made easy!',
+    license='The MIT License',
+    install_requires=[
+        'requests==2.11.1',
+        'six==1.10.0',
+        'Pillow==3.4.2',
+        'BeautifulSoup==3.2.1',
+    ],
+    packages=['tensorpy'],
+    entry_points={
+        'console_scripts': [
+            'classify = tensorpy.classify:main',
+        ],
+    },
+)
