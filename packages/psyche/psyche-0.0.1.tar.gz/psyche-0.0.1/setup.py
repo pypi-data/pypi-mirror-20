f72fa1710ac61726d94013b0bf1fd028c8ee209a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:25 +0000

    Add package psyche/0.0.1/psyche-0.0.1.tar.gz

---
+++ packages/psyche/psyche-0.0.1.tar.gz/psyche-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="psyche",
+    version="0.0.1",
+    author="Matteo Cafasso",
+    author_email="noxdafox@gmail.com",
+    description=(""),
+    license="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+)
