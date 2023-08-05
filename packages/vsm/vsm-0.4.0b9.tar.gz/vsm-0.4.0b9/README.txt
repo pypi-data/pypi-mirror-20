03fc6633d0629da7387f3065cdc3cb4a5a2c8491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:07 +0000

    Add package vsm/0.4.0b9/vsm-0.4.0b9.tar.gz

---
+++ packages/vsm/vsm-0.4.0b9.tar.gz/vsm-0.4.0b9/README.txt
@@ -0,0 +1,37 @@
+VSM
+===
+
+|Build Status| |Appveyor| |Coveralls| |GitHub license| |PyPI|
+
+**Note:** More than likely, you are looking for the `InPhO Topic
+Explorer <http://github.com/inpho/topic-explorer>`__. This library is
+for the machine learning implementations underlying the Topic Explorer
+and is updated much slower than the user interfaces. If you are using
+the Topic Explorer, please file issues there and the developers will
+triage appropriately.
+
+Vector Space Model Framework developed for the `InPhO
+Project <https://inpho.cogs.indiana.edu/>`__.
+
+Documentation can be found in the module and
+`here <http://inpho.github.io/vsm/>`__.
+
+Installation
+------------
+
+::
+
+    git clone git@github.com:inpho/vsm.git
+    cd vsm
+    pip install -r requirements.txt -e .
+
+.. |Build Status| image:: https://img.shields.io/travis/inpho/vsm.svg?label=UNIX+build
+   :target: https://travis-ci.org/inpho/vsm
+.. |Appveyor| image:: https://img.shields.io/appveyor/ci/JaimieMurdock/vsm.svg?label=Windows+build
+   :target: https://ci.appveyor.com/project/JaimieMurdock/vsm
+.. |Coveralls| image:: https://img.shields.io/coveralls/inpho/vsm.svg
+   :target: https://coveralls.io/r/inpho/vsm
+.. |GitHub license| image:: https://img.shields.io/github/license/mashape/apistatus.svg
+   :target: https://github.com/inpho/vsm/blob/master/LICENSE.txt
+.. |PyPI| image:: https://img.shields.io/pypi/v/vsm.svg
+   :target: https://pypi.python.org/pypi/vsm
---
+++ packages/vsm/vsm-0.4.0b9.tar.gz/vsm-0.4.0b9/setup.py
@@ -0,0 +1,66 @@
+from setuptools import setup, Extension, Command, find_packages
+import platform
+import numpy
+
+
+# find packages in vsm subdirectory
+# this will skip the unittests, etc.
+packages = ['vsm.'+pkg for pkg in find_packages('vsm')]
+packages.append('vsm')
+
+
+
+
+install_requires=[
+        "numpy>=1.6.1",
+        "scipy>=0.13.0",
+        "progressbar>=2.3",
+        "chardet>=2.3.0",
+        "sortedcontainers>=1.4.0",
+        "czipfile==1.0.0",
+        "cython"
+    ]
+
+if platform.python_version_tuple()[0] == '2':
+    install_requires.append("futures>=3.0.0")
+
+setup(
+    name = "vsm",
+    version = "0.4.0b9",
+    description = ('Vector Space Semantic Modeling Framework '\
+                   'for the Indiana Philosophy Ontology Project'),
+    author = "The Indiana Philosophy Ontology (InPhO) Project",
+    author_email = "inpho@indiana.edu",
+    url = "http://inpho.cogs.indiana.edu/",
+    download_url = "http://www.github.com/inpho/vsm",
+    keywords = [],
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Text Processing :: Linguistic",
+        ],
+    install_requires=install_requires,
+    license = 'MIT',
+    packages=packages,
+    ext_modules = [
+        Extension('_cgs_update', ['vsm/model/_cgs_update.c'],
+            include_dirs=[numpy.get_include()],
+            extra_compile_args=['-march=native'],
+            extra_link_args=['-march=native'],
+        #    define_macros=[('CYTHON_TRACE','1')]
+        ),
+    ],
+    dependency_links=['https://inpho.cogs.indiana.edu/pypi/czipfile/',
+        'https://inpho.cogs.indiana.edu/pypi/pymmseg/'],
+    test_suite = "unittest2.collector",
+    tests_require=['unittest2'],
+)
