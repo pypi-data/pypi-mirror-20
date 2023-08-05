af9ec08f6c5a8987c6de84f83f51799eca58e2bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:52 +0000

    Add package celery-multibeat/0.0.3/celery-multibeat-0.0.3.tar.gz

---
+++ packages/celery-multibeat/celery-multibeat-0.0.3.tar.gz/celery-multibeat-0.0.3/multibeat/decoders.py
@@ -0,0 +1,71 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy
+# of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+# Original work Copyright 2015 Marc Sibson
+# Modified work Copyright 2016 Ryler Hockenbury
+
+from datetime import datetime
+
+try:
+    import simplejson as json
+except ImportError:
+    import json
+
+from celery.schedules import schedule, crontab
+
+
+class MultiBeatJSONDecoder(json.JSONDecoder):
+    def __init__(self, *args, **kargs):
+        super(MultiBeatJSONDecoder, self).__init__(object_hook=self.dict_to_object, *args, **kargs)
+
+    def dict_to_object(self, d):
+        if '__type__' not in d:
+            return d
+
+        objtype = d.pop('__type__')
+
+        if objtype == 'datetime':
+            return datetime(**d)
+
+        if objtype == 'interval':
+            return schedule(run_every=d['every'], relative=d['relative'])
+
+        if objtype == 'crontab':
+            return crontab(**d)
+
+        d['__type__'] = objtype
+
+        return d
+
+
+class MultiBeatJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            return {
+                '__type__': 'datetime',
+                'year': obj.year,
+                'month': obj.month,
+                'day': obj.day,
+                'hour': obj.hour,
+                'minute': obj.minute,
+                'second': obj.second,
+                'microsecond': obj.microsecond,
+            }
+        if isinstance(obj, crontab):
+            return {
+                '__type__': 'crontab',
+                'minute': obj._orig_minute,
+                'hour': obj._orig_hour,
+                'day_of_week': obj._orig_day_of_week,
+                'day_of_month': obj._orig_day_of_month,
+                'month_of_year': obj._orig_month_of_year,
+            }
+        if isinstance(obj, schedule):
+            return {
+                '__type__': 'interval',
+                'every': obj.run_every.total_seconds(),
+                'relative': bool(obj.relative),
+            }
+
+        return super(MultiBeatJSONEncoder, self).default(obj)
