72cb15a32ff3b3e6c15ca02ad7b7c7a6c8232262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:54 +0000

    Add package relay2slack/0.1/relay2slack-0.1.tar.gz

---
+++ packages/relay2slack/relay2slack-0.1.tar.gz/relay2slack-0.1/README.rst
@@ -0,0 +1,19 @@
+relay2slack
+======
+
+relay2slack is a tool for capturing and forwarding incoming Slack webhook events.
+
+
+Installation
+------------
+To install relay2slack, simply:
+
+.. code-block:: bash
+
+    $ pip install relay2slack
+
+To run relay2slack in the foreground:
+
+.. code-block:: bash
+
+    $ relay2slack
---
+++ packages/relay2slack/relay2slack-0.1.tar.gz/relay2slack-0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst', 'r') as f:
+        return f.read()
+
+
+setup(name='relay2slack',
+      version='0.1',
+      description='relay2slack is a tool for capturing and forwarding incoming Slack webhook events',
+      long_description=readme(),
+      classifiers=['Intended Audience :: Developers',
+                   'Intended Audience :: Information Technology',
+                   'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+                   'Natural Language :: English',
+                   'Operating System :: POSIX :: Linux',
+                   'Programming Language :: Python :: 2.7',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+                   'Topic :: Utilities'],
+      keywords='slack webhook kapacitor proxy',
+      url='http://github.com/blaketmiller/relay2slack',
+      author='Blake Miller',
+      author_email='blakethomasmiller@gmail.com',
+      license='GNU GPL v2.0',
+      packages=['relay2slack'],
+      install_requires=['requests', 'Flask'],
+      scripts=['bin/relay2slack'],
+      zip_safe=False)
