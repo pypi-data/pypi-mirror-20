e2565b738d5550ccc1cdac5d84e72231f65e8abd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:43 +0000

    Add package gwsnester/1.0.0/gwsnester-1.0.1.tar.gz

---
+++ packages/gwsnester/gwsnester-1.0.1.tar.gz/gwsnester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'gwsnester',
+	version		= '1.0.0',
+	py_modules	= ['gwsnester'],
+	author		= 'gws1013',
+	author_email= 'gws1013@naver.com',
+	url			= 'http://www.naver.com',
+	description	= 'A simple printer of nested lists',
+	)
