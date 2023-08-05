69e8ae0e518a8d254b310ea5aa633c5ab5743889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:00 +0000

    Add package credulous/0.1.5/credulous-0.1.5.tar.gz

---
+++ packages/credulous/credulous-0.1.5.tar.gz/credulous-0.1.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+from distutils.core import setup
+setup(
+  name = 'credulous',
+  packages = ['credulous'], # this must be the same as the name above
+  version = '0.1.5',
+  description = 'Tool for generating API credentials. Google API is supported.',
+  author = 'Bo Ogunlana',
+  author_email = 'foluso@brainlabsdigital.com',
+  url = 'https://github.com/bo-brainlabs/pycredulous', # use the URL to the github repo
+  keywords = ['oauth2', 'authentication', 'google', 'googleads-api-client-python'], # arbitrary keywords
+  classifiers = [],
+  install_requires = [
+		'oauth2client',
+    'setuptools'
+  ]
+)
