a34debd193849f1e3d9394923400a4452df256a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:18 +0000

    Add package bob.extension/2.3.9/bob.extension-2.3.9.zip

---
+++ packages/bob.extension/bob.extension-2.3.9.zip/bob.extension-2.3.9/bob/extension/test_extensions.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+# Andre Anjos <andre.anjos@idiap.ch>
+# Thu 20 Mar 2014 12:43:48 CET
+
+"""Tests that the examples work as expected
+"""
+
+import os
+import sys
+import nose.tools
+import tempfile
+import shutil
+import subprocess
+import pkg_resources
+
+
+def _run(package, run_call):
+  tarball = os.path.join(
+      pkg_resources.resource_filename('bob.extension', 'data'),
+      'bob.example.%s.tar.bz2' % package,
+      )
+  temp_dir = tempfile.mkdtemp(prefix="bob_test")
+
+  # redirect output of functions to /dev/null to avoid spamming the console
+  # devnull = open(os.devnull, 'w')
+
+  try:
+    # extract archive
+    import tarfile
+    with tarfile.open(tarball) as tar: tar.extractall(temp_dir)
+    package_dir = os.path.join(temp_dir, "bob.example.%s" % package)
+
+    # bootstrap
+    subprocess.call([sys.executable, "bootstrap-buildout.py"], cwd=package_dir)
+    assert os.path.exists(os.path.join(package_dir, "bin", "buildout"))
+
+    # buildout
+    # if we have a setup.py in our current directory, we develop both (as we might be in the current source directory of bob.extension and use it),
+    # otherwise we only develop the downloaded source package
+    develop = '%s\n.'%os.getcwd() if os.path.exists("setup.py") else '.'
+    subprocess.call(['./bin/buildout', 'buildout:prefer-final=false', 'buildout:develop=%s'%develop], cwd=package_dir)
+    assert os.path.exists(os.path.join(package_dir, "bin", "python"))
+
+    # nosetests
+    subprocess.call(['./bin/nosetests', '-sv'], cwd=package_dir)
+
+    # check that the call is working
+    subprocess.call(run_call, cwd=package_dir)
+
+    subprocess.call(['./bin/sphinx-build', 'doc', 'sphinx'], cwd=package_dir)
+    assert os.path.exists(os.path.join(package_dir, "sphinx", "index.html"))
+
+    subprocess.call('./bin/python -c "import pkg_resources; from bob.example.%s import get_config; print(get_config())"'%package, cwd=package_dir, shell=True)
+
+  finally:
+    shutil.rmtree(temp_dir)
+
+
+def test_project():
+  # Tests that the bob.example.project works
+  _run('project', ['./bin/version.py'])
+
+
+def test_extension():
+  # Tests that the bob.example.extension compiles and works
+  _run('extension', ['./bin/reverse.py', '1', '2', '3', '4', '5'])
+
+
+def test_library():
+  # Tests that the bob.example.library compiles and works
+  _run('library', ['./bin/reverse.py', '1', '2', '3', '4', '5'])
---
+++ packages/bob.extension/bob.extension-2.3.9.zip/bob.extension-2.3.9/doc/links.rst
@@ -0,0 +1,25 @@
+.. vim: set fileencoding=utf-8 :
+.. Andre Anjos <andre.anjos@idiap.ch>
+.. Tue 20 Mar 2012 08:57:32 CET
+..
+.. Copyright (C) 2011-2014 Idiap Research Institute, Martigny, Switzerland
+
+.. _bob's website: https://www.idiap.ch/software/bob
+.. _bob: https://www.idiap.ch/software/bob
+.. _bob's installation: https://www.idiap.ch/software/bob/install
+.. _c++: http://www2.research.att.com/~bs/C++.html
+.. _distutils: https://docs.python.org/distutils/
+.. _git: http://git-scm.com/
+.. _gitlab: http://gitlab.idiap.ch/
+.. _idiap: http://www.idiap.ch
+.. _ipython: http://ipython.scipy.org
+.. _nose: https://nose.readthedocs.org/en/latest/
+.. _pep 386: http://www.python.org/dev/peps/pep-0386/
+.. _python: http://www.python.org
+.. _pypi: http://pypi.python.org
+.. _satellite packages: https://www.idiap.ch/software/bob/packages
+.. _setuptools: https://setuptools.readthedocs.io
+.. _sphinx: http://sphinx.pocoo.org
+.. _zc.buildout: http://pypi.python.org/pypi/zc.buildout/
+.. _mr.developer: http://pypi.python.org/pypi/mr.developer/
+.. _bob.blitz: https://gitlab.idiap.ch/bob/bob.blitz
