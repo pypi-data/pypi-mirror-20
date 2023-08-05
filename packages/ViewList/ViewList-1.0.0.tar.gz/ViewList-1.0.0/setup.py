2fdf849a8a6d50f49168fbc174180cea6369352c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:23 +0000

    Add package ViewList/1.0.0/ViewList-1.0.0.tar.gz

---
+++ packages/ViewList/ViewList-1.0.0.tar.gz/ViewList-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name = "ViewList",
+        version = "1.0.0",
+        py_modules = ["ViewList"],
+        author = "hfpython",
+        author_email = "hfpython@headfirstlabs.com",
+        url = "http://headfirstlabs.com",
+        description = "A simple printer of view list",
+    )
