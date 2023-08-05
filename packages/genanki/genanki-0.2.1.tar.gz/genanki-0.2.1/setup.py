fd7a6619b91e97817b6fa88c22452ba080a8f419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:18 +0000

    Add package genanki/0.2.1/genanki-0.2.1.tar.gz

---
+++ packages/genanki/genanki-0.2.1.tar.gz/genanki-0.2.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(name='genanki',
+      version='0.2.1',
+      description='Generate Anki decks programmatically',
+      url='http://github.com/kerrickstaley/genanki',
+      author='Kerrick Staley',
+      author_email='k@kerrickstaley.com',
+      license='MIT',
+      packages=['genanki'],
+      zip_safe=False,
+      install_requires=[
+        'cached-property',
+        'frozendict',
+        'pystache',
+        'pyyaml',
+      ],
+      keywords=[
+        'anki',
+        'flashcards',
+        'memorization',
+      ])
