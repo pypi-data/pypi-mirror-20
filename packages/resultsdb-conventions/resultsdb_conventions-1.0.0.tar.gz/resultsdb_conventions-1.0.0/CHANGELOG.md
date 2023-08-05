fd000971605a82dd2025d07ad1d9254518adc3ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:49 +0000

    Add package resultsdb-conventions/1.0.0/resultsdb_conventions-1.0.0.tar.gz

---
+++ packages/resultsdb-conventions/resultsdb_conventions-1.0.0.tar.gz/resultsdb_conventions-1.0.0/CHANGELOG.md
@@ -0,0 +1,7 @@
+## Changelog
+
+### 1.0.0 - 2017-02-02
+
+*   [resultsdb_conventions-1.0.0.tar.xz](https://releases.pagure.org/fedora-qa/resultsdb_conventions/resultsdb_conventions-1.0.0.tar.xz)
+
+1.  Initial release of resultsdb_conventions
---
+++ packages/resultsdb-conventions/resultsdb_conventions-1.0.0.tar.gz/resultsdb_conventions-1.0.0/release.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "$#" != "1" ]; then
+    echo "Must pass release version!"
+    exit 1
+fi
+
+version=$1
+name=resultsdb_conventions
+sed -i -e "s,version = \".*\",version = \"${version}\", g" setup.py
+git add setup.py
+git commit -m "release ${version}"
+git push
+git tag -m "release ${version}" -a ${version}
+git push origin ${version}
+# FIXME: this is wrong, but hopefully works until i can find the right
+# version of this damn script
+git archive --format=tar --prefix=${name}-${version}/ ${version} > dist/${name}-${version}.tar
+xz dist/${name}-${version}.tar
+# FIXME: need scp upload for Pagure:
+# https://pagure.io/pagure/issue/851
+# Uploading manually via web UI for now
