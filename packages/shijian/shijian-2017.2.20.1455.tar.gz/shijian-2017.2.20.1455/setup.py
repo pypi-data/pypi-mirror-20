29fc6c1dd7aff1864a8191690973bda6eeaa360a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:29 +0000

    Add package shijian/2017.2.20.1455/shijian-2017.2.20.1455.tar.gz

---
+++ packages/shijian/shijian-2017.2.20.1455.tar.gz/shijian-2017.2.20.1455/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+
+import setuptools
+
+def main():
+
+    setuptools.setup(
+        name             = "shijian",
+        version          = "2017.02.20.1455",
+        description      = "change, time, file, list, statistics and other utilities",
+        long_description = long_description(),
+        url              = "https://github.com/wdbm/shijian",
+        author           = "Will Breaden Madden",
+        author_email     = "wbm@protonmail.ch",
+        license          = "GPLv3",
+        py_modules       = [
+                           "shijian"
+                           ],
+        install_requires = [
+                           "numpy",
+                           "pyprel",
+                           "scipy",
+                           "shijian"
+                           ],
+        entry_points     = """
+            [console_scripts]
+            shijian = shijian:shijian
+        """
+    )
+
+def long_description(
+    filename = "README.md"
+    ):
+
+    if os.path.isfile(os.path.expandvars(filename)):
+        try:
+            import pypandoc
+            long_description = pypandoc.convert_file(filename, "rst")
+        except ImportError:
+            long_description = open(filename).read()
+    else:
+        long_description = ""
+    return long_description
+
+if __name__ == "__main__":
+    main()
