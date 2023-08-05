d0490ee8a8b4cea3d07915f4169ae9a2aa3553cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:56 +0000

    Add package mroy-trans/0.6/mroy-trans-0.6.tar.gz

---
+++ packages/mroy-trans/mroy-trans-0.6.tar.gz/mroy-trans-0.6/trans/cmd.py
@@ -0,0 +1,51 @@
+import sys,argparse,os
+from trans.back_query import  brun,srun,urun
+from trans.Trans.main import main as mainServer
+from qlib.asyn.daemon import run, restart, stop
+
+
+def get_args():
+    DOC = """
+    a translation backend .
+        mac mode: will use display notification center.
+        server mode: will send result to http://localhost:8080/
+    """
+    parser = argparse.ArgumentParser(usage="how to use this", description=DOC)
+    parser.add_argument("-s", "--start", default=False, action="store_true",help="set start")
+    parser.add_argument("-k", "--stop", default=False,  action="store_true",help="set stop")
+    parser.add_argument("-m", "--mac", default=False, action="store_true", help="set mac mode.")
+    parser.add_argument("-w", "--win", default=False, action="store_true", help="set win mode.")
+    # parser.add_argument("-r", "--restart", default=False, action="store_true", help="set restart")
+    parser.add_argument("-S", "--Server", default=False, action="store_true", help="set restart")
+
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+    if args.stop:
+        if sys.platform != "darwin":
+            pass
+
+        else:
+            stop(brun)
+            stop(srun)
+    elif args.Server:
+        if sys.platform != "darwin":
+            pass
+        else:
+            os.popen("sudo Translate -s").read()
+            try:
+                mainServer()
+            except (InterruptedError, KeyboardInterrupt) as p:
+                stop(srun)
+
+    elif args.mac:
+        run(brun)
+    elif args.win:
+        urun()
+    elif args.start:
+        if sys.platform != "darwin":
+            urun()
+        else:
+            run(srun)
