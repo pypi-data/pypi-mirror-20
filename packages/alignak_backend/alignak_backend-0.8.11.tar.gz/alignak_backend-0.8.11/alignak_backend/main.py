dbb65607caef83583ddd5a743763cb63a53e3776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:26 +0000

    Add package alignak_backend/0.8.11/alignak_backend-0.8.11.tar.gz

---
+++ packages/alignak_backend/alignak_backend-0.8.11.tar.gz/alignak_backend-0.8.11/alignak_backend/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Main
+"""
+from alignak_backend.app import app
+
+
+def main():
+    """
+    Main function
+    """
+    app.run()
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
