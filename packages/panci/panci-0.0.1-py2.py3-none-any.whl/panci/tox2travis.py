07423b920189636be4355ace726e5eabff885796
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:31 +0000

    Add package panci/0.0.1/panci-0.0.1-py2.py3-none-any.whl

---
+++ packages/panci/panci-0.0.1-py2.py3-none-any.whl/panci/tox2travis.py
@@ -0,0 +1,23 @@
+"""Utilities for converting from Travis CI configs to Tox configs."""
+
+import textwrap
+
+from .toxconfig import ToxConfig
+
+
+def tox2travis(in_file):
+    config = ToxConfig.from_file(in_file)
+    env_list = ('\n' + 10 * chr(32)).join([
+        '- TOXENV={0}'.format(env) for env in config.envlist
+    ])
+
+    return textwrap.dedent("""
+        language: python
+        cache: pip
+        env:
+          {env_list}
+        install:
+          - travis_retry pip install tox
+        script:
+          - travis_retry tox
+        """.format(env_list=env_list)).strip()
