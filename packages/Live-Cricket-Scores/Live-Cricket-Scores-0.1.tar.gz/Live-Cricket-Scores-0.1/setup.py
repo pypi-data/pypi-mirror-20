433148defb6b7edb06f370b96e729e9adc33a45c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:22 +0000

    Add package Live-Cricket-Scores/0.1/Live-Cricket-Scores-0.1.tar.gz

---
+++ packages/Live-Cricket-Scores/Live-Cricket-Scores-0.1.tar.gz/Live-Cricket-Scores-0.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+  name = 'Live-Cricket-Scores',
+  packages = ['Live-Cricket-Scores'],
+  version = '0.1',
+  description = 'A python script that brings cricket scores right into your terminal in real time!',
+  author = 'Navjot Singh',
+  author_email = 'singh_navjot75@yahoo.ca',
+  url = 'https://github.com/navjot12/Live-Cricket-Scores',
+  download_url = 'https://github.com/navjot12/live-cricket-scores/tarball/0.1',
+  keywords = ['cricket', 'scores', 'live', 'terminal']
+)
