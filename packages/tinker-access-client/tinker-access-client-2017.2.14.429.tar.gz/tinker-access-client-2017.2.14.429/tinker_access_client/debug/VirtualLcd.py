9af41c63a89f019b2e7344a1261bc2c3e4f423cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:06 +0000

    Add package tinker-access-client/2017.2.14.429/tinker-access-client-2017.2.14.429.tar.gz

---
+++ packages/tinker-access-client/tinker-access-client-2017.2.14.429.tar.gz/tinker-access-client-2017.2.14.429/tinker_access_client/debug/VirtualLcd.py
@@ -0,0 +1,19 @@
+
+
+# class _VirtualLcd(object):
+#     LCD_LINE_1 = 0
+#     LCD_LINE_2 = 1
+#
+#     def lcd_init(self, *args):
+#         pass
+
+
+# noinspection PyClassHasNoInit
+class VirtualLcd(object):
+    # LCD = _VirtualLcd()
+    LCD_LINE_1 = 0
+    LCD_LINE_2 = 1
+
+    def lcd_init(self, *args):
+        pass
+
