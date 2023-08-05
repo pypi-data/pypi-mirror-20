1b9433fc5e7f0fba15e822378c5ca8ad215778e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:03 +0000

    Add package conkit/0.6/conkit-0.6.tar.gz

---
+++ packages/conkit/conkit-0.6.tar.gz/conkit-0.6/conkit/constants.py
@@ -0,0 +1,28 @@
+"""
+Collection of constant variables used throught ConKit
+"""
+
+__author__ = "Felix Simkovic"
+__date__ = "09 Sep 2016"
+__version__ = 0.1
+
+# ================================================
+# Amino acid conversions
+# ================================================
+ONE_TO_THREE = {'A': 'ALA', 'C': 'CYS', 'B': 'ASX', 'E': 'GLU', 'D': 'ASP', 'G': 'GLY', 'F': 'PHE', 'I': 'ILE',
+                'H': 'HIS', 'K': 'LYS', 'J': 'XLE', 'M': 'MET', 'L': 'LEU', 'O': 'PYL', 'N': 'ASN', 'Q': 'GLN',
+                'P': 'PRO', 'S': 'SER', 'R': 'ARG', 'U': 'SEC', 'T': 'THR', 'W': 'TRP', 'V': 'VAL', 'Y': 'TYR',
+                'X': 'XAA', 'Z': 'GLX'}
+
+THREE_TO_ONE = {'ALA': 'A', 'ARG': 'R', 'ASN': 'N', 'ASP': 'D', 'CME': 'C', 'CYS': 'C', 'GLN': 'Q', 'GLU': 'E', 
+                'GLY': 'G', 'HIS': 'H', 'ILE': 'I', 'LEU': 'L', 'LYS': 'K', 'MET': 'M', 'MSE': 'M', 'PHE': 'F', 
+                'PRO': 'P', 'PYL': 'O', 'SER': 'S', 'SEC': 'U', 'THR': 'T', 'TRP': 'W', 'TYR': 'Y', 'VAL': 'V', 
+                'ASX': 'B', 'GLX': 'Z', 'XAA': 'X', 'XLE': 'J'}
+
+# ================================================
+# Constants defining Contact().status color coding
+# ================================================
+TPCOLOR = '#2D9D00'     # color true positive
+FPCOLOR = '#AB0000'     # color false positive
+NTCOLOR = '#0482ff'     # color undefined
+RFCOLOR = '#B5B5B5'     # color structure
