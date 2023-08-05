f47d25b76819ecce1fedb514ce317646bd82b68c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:25 +0000

    Add package datnester/1.2.0/datnester-1.2.0.tar.gz

---
+++ packages/datnester/datnester-1.2.0.tar.gz/datnester-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name = 'datnester',
+	version = '1.2.0',
+	py_modules = ['datnester'],
+	author = 'datpython',
+	author_email = 'vdnguyen@gmail.com',
+	description = 'a first test simple',
+)
