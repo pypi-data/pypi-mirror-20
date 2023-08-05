0fa294840ea84d24a6dbd8d64d1a6d104b0632a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:06 +0000

    Add package PyStratum-MySQL/0.10.10/PyStratum-MySQL-0.10.10.tar.gz

---
+++ packages/PyStratum-MySQL/PyStratum-MySQL-0.10.10.tar.gz/PyStratum-MySQL-0.10.10/pystratum_test/StratumTestCase.py
@@ -0,0 +1,32 @@
+"""
+PyStratum
+
+Copyright 2015-2016 Set Based IT Consultancy
+
+Licence MIT
+"""
+import sys
+import unittest
+from io import StringIO
+
+from pystratum_test.TestDataLayer import TestDataLayer
+
+
+class StratumTestCase(unittest.TestCase):
+    # ------------------------------------------------------------------------------------------------------------------
+    def setUp(self):
+        TestDataLayer.config['host'] = 'localhost'
+        TestDataLayer.config['user'] = 'test'
+        TestDataLayer.config['password'] = 'test'
+        TestDataLayer.config['database'] = 'test'
+
+        TestDataLayer.connect()
+
+        self.held, sys.stdout = sys.stdout, StringIO()
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def tearDown(self):
+        sys.stdout = self.held
+        TestDataLayer.disconnect()
+
+# ----------------------------------------------------------------------------------------------------------------------
