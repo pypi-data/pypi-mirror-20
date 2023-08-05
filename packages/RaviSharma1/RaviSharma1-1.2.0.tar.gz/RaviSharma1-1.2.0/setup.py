aa8ee2d811d2904a94576965d5969dde8f246359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:10 +0000

    Add package RaviSharma1/1.2.0/RaviSharma1-1.2.0.tar.gz

---
+++ packages/RaviSharma1/RaviSharma1-1.2.0.tar.gz/RaviSharma1-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name    = 'RaviSharma1',
+	version = '1.2.0',
+	py_modules = ['RaviSharma1'],
+	author = 'RaviSharma',
+	author_email = 'ravi15165@iiitd.ac.in',
+	url = 'http://www.headfirstlabs.com',
+	description = 'A simple printer nested list', 
+	)
