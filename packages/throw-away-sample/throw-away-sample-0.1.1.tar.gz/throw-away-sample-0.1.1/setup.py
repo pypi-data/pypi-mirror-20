b14982049ae8428ce5f233a40485bb73444a31c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:02 +0000

    Add package throw-away-sample/0.1.1/throw-away-sample-0.1.1.tar.gz

---
+++ packages/throw-away-sample/throw-away-sample-0.1.1.tar.gz/throw-away-sample-0.1.1/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='throw-away-sample',
+    version='0.1.1',
+    description='Sample package for Python-Guide.org',
+    long_description=readme,
+    author='Kenneth Reitz',
+    author_email='me@kennethreitz.com',
+    data_files={'': ['LICENSE', 'README.rst']},
+    url='https://github.com/kennethreitz/samplemod',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')))
