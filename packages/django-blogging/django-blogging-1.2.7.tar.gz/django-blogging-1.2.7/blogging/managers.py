fc83ad23a8ec29f16e53b019af5a150e5c9d81e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:51 +0000

    Add package django-blogging/1.2.7/django-blogging-1.2.7.tar.gz

---
+++ packages/django-blogging/django-blogging-1.2.7.tar.gz/django-blogging-1.2.7/blogging/managers.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+# Category related
+class AvailableCategoriesManager(models.Manager):
+    def get_queryset(self):
+        queryset = super(AvailableCategoriesManager, self).get_queryset()
+        queryset = queryset.filter(site__id=settings.SITE_ID)
+        return queryset
+
+    def get_by_natural_key(self, slug, site_id):
+        return self.get(slug=slug, site__id=site_id)
+
+
+# Post related
+class PostManager(models.Manager):
+    def published(self):
+        now = timezone.now()
+        queryset = self.filter(published_on__lte=now)
+        queryset = queryset.filter(status=self.model.PUBLISHED)
+        return queryset
+
+    def related_items(self, item, length=5):
+        """Return items related to the item passed as parameter."""
+        qs = self.availables.published()
+        qs = qs.filter(categories__in=item.categories.all())
+        qs = qs.exclude(id=item.id)
+        qs = qs.distinct()[:length]
+
+
+class AvailableItemsManager(PostManager):
+    def get_queryset(self):
+        queryset = super(AvailableItemsManager, self).get_queryset()
+        queryset = queryset.filter(site__id__exact=settings.SITE_ID)
+        return queryset
+
+    def get_by_natural_key(self, slug, site_id):
+        return self.get(slug=slug, site__id=site_id)
