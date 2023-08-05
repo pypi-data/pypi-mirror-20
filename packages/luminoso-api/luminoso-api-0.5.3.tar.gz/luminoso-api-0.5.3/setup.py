59b597adb3728f828980dfd22206bf6586d678ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:05:46 +0000

    Add package luminoso-api/0.5.3/luminoso-api-0.5.3.tar.gz

---
+++ packages/luminoso-api/luminoso-api-0.5.3.tar.gz/luminoso-api-0.5.3/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+VERSION = "0.5.3"
+
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'Natural Language :: English',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Text Processing :: Linguistic'
+]
+
+GITHUB_URL = 'http://github.com/LuminosoInsight/luminoso-api-client-python'
+
+setup(
+    name="luminoso-api",
+    version=VERSION,
+    maintainer='Luminoso Technologies, Inc.',
+    maintainer_email='info@luminoso.com',
+    url=GITHUB_URL,
+    download_url='%s/tarball/v%s' % (GITHUB_URL, VERSION),
+    platforms=["any"],
+    description="Python client library for communicating with the Luminoso REST API",
+    classifiers=classifiers,
+    packages=find_packages(exclude=('tests',)),
+    install_requires=[
+        'requests >= 1.2.1, < 3.0',
+        'ftfy >= 3.3, < 5',
+    ],
+    entry_points={
+        'console_scripts': [
+            'lumi-upload = luminoso_api.upload:main',
+            'lumi-json-stream = luminoso_api.json_stream:main',
+        ]},
+)
