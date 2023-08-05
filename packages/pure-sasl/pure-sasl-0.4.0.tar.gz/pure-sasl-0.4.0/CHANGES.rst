63121476ed495c6554f1bac736d79b60a81db037
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:41 +0000

    Add package pure-sasl/0.4.0/pure-sasl-0.4.0.tar.gz

---
+++ packages/pure-sasl/pure-sasl-0.4.0.tar.gz/pure-sasl-0.4.0/CHANGES.rst
@@ -0,0 +1,40 @@
+0.4.0
+=====
+February 21st, 2017
+
+* Add support for authorization_id to PlainMechanism and GSSAPIMechanism
+
+0.3.0
+=====
+September 30th, 2016
+
+* Reintroduce support for MD5DigestMechanism
+* Restrict supported QOPs (Quality of Protection) for mechanisms
+  up front
+
+0.2.0
+=====
+February 1st, 2016
+
+* Add support for Python 3
+* Add unit tests
+* Temporarily disable broken MD5DigestMechanism
+
+0.1.7
+=====
+February 17th, 2015
+
+* Add wrap and unwrap to PlainMechanism
+
+0.1.6
+=====
+January 9th, 2015
+
+* Add AUTHZID support for PLAIN mechanism
+* Allow GSS to work with older versions of pykerberos
+
+0.1.5
+=====
+November 15th, 2013
+
+* Fix digest URI order for DigestMD5Mechanism
---
+++ packages/pure-sasl/pure-sasl-0.4.0.tar.gz/pure-sasl-0.4.0/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+import puresasl
+
+setup(name='pure-sasl',
+      version=puresasl.__version__,
+      author='Tyler Hobbs',
+      author_email='tylerlhobbs@gmail.com',
+      description='Pure Python client SASL implementation',
+      long_description=
+"""This package provides a reasonably high-level SASL client written
+in pure Python.  New mechanisms may be integrated easily, but by default,
+support for PLAIN, ANONYMOUS, CRAM-MD5, DIGEST-MD5, and GSSAPI are
+provided.""",
+      license='MIT',
+      url='http://github.com/thobbs/pure-sasl',
+      keywords='sasl',
+      packages=['puresasl'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ]
+    )
