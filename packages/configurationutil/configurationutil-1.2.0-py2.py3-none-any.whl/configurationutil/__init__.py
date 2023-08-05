46801f823156c8c9cbc05c6e33c0b7617407905e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:34 +0000

    Add package configurationutil/1.2.0/configurationutil-1.2.0-py2.py3-none-any.whl

---
+++ packages/configurationutil/configurationutil-1.2.0-py2.py3-none-any.whl/configurationutil/__init__.py
@@ -0,0 +1,13 @@
+
+# Get module version
+from _metadata import __version__
+
+from configurationutil.configuration import (
+    Configuration
+)
+
+from configurationutil.cfg_objects.json_config import (
+    JSONConfig
+)
+
+import configurationutil.configuration as cfg_params
