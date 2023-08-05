2061a8e8f980f6c5221a5a696b74bf856d08c6f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:26 +0000

    Add package ViewList/1.3.0/ViewList-1.3.0.tar.gz

---
+++ packages/ViewList/ViewList-1.3.0.tar.gz/ViewList-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name = "ViewList",
+        version = "1.3.0",
+        py_modules = ["ViewList"],
+        author = "leo",
+        author_email = "yangleics3333@qq.com",
+        url = "",
+        description = "A simple printer of view list",
+    )
