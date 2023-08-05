82a702af3828ce7669fa834f499ae7b2f7d2b0ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:28 +0000

    Add package chainerrl/0.0.2/chainerrl-0.0.2.tar.gz

---
+++ packages/chainerrl/chainerrl-0.0.2.tar.gz/chainerrl-0.0.2/chainerrl/explorers/__init__.py
@@ -0,0 +1,4 @@
+from chainerrl.explorers.additive_gaussian import AdditiveGaussian  # NOQA
+from chainerrl.explorers.additive_ou import AdditiveOU  # NOQA
+from chainerrl.explorers.epsilon_greedy import ConstantEpsilonGreedy  # NOQA
+from chainerrl.explorers.epsilon_greedy import LinearDecayEpsilonGreedy  # NOQA
