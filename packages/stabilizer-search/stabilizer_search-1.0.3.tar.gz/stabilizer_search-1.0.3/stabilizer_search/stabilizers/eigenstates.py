89418804e12b609b103349949fcbffc4f824f65c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:28 +0000

    Add package stabilizer-search/1.0.3/stabilizer_search-1.0.3.tar.gz

---
+++ packages/stabilizer-search/stabilizer_search-1.0.3.tar.gz/stabilizer_search-1.0.3/stabilizer_search/stabilizers/eigenstates.py
@@ -0,0 +1,31 @@
+"""Module which defines methods for converting the QObj Pauli Operators that 
+generate each stabilizer group, building a projector and finding the associated 
++1 eigenstate."""
+
+from math import pow as fpow
+from numpy import allclose, imag
+from numpy.linalg import eig
+
+from ..mat import qeye
+
+def find_projector(generating_set):
+    n_qubits = len(generating_set)
+    _id = qeye(pow(2, n_qubits))
+    res = qeye(pow(2, n_qubits))
+    for _g in generating_set:
+        res = res * (_id+_g)
+    return res/fpow(2, n_qubits)
+
+def find_eigenstate(projector):
+    eigs, vecs = eig(projector)
+    for _n, _eig in enumerate(eigs):
+        if allclose(_eig, complex(1)) or allclose(_eig, 1.):
+            return (vecs[:,_n])
+    return None
+
+def py_find_eigenstates(generating_sets, real_only=False):
+    """ """
+    states = [find_eigenstate(x) for x in map(find_projector, generating_sets)]
+    if real_only:
+        return list(filter(lambda x: allclose(imag(x), 0.), states))
+    return states
