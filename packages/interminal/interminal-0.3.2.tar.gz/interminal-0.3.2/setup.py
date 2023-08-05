d557d3ddd9a258d66054be9ada7ea610a750e1c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:45 +0000

    Add package interminal/0.3.2/interminal-0.3.2.tar.gz

---
+++ packages/interminal/interminal-0.3.2.tar.gz/interminal-0.3.2/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+import sys
+
+if sys.version_info < (3, 0):
+    raise Exception('Python >= 3 required')
+
+VERSION = '0.3.2'
+
+
+setup(name='interminal',
+      version=VERSION,
+      description='Utility for launching commands in a GUI terminal',
+      author='Chris Billington',
+      author_email='chrisjbillington@gmail.com',
+      url='https://github.com/chrisjbillington/interminal',
+      license="BSD",
+      scripts=['bin/interminal', 'bin/inshell'],
+      install_requires=['pexpect'],)
