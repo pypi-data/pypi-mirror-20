e17cf235badef782f26c3fa831ce90003527172e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:22 +0000

    Add package fedfind/1.2/fedfind-1.2.tar.gz

---
+++ packages/fedfind/fedfind-1.2.tar.gz/fedfind-1.2/fedfind/const.py
@@ -0,0 +1,88 @@
+# Copyright (C) 2015 Red Hat
+#
+# This file is part of fedfind.
+#
+# fedfind is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Author: Adam Williamson <awilliam@redhat.com>
+
+"""Various shared constants."""
+
+import collections
+
+# For both of these, we identify each we want to consider unique;
+# that is the 'name'. 'values' is an iterable of values which should
+# map to that name; for instance, we don't want to treat i486, i586
+# and i686 as separate arches, but every time we see any of those we
+# want to treat it as i386. 'weight' is an integer to be used for
+# sort weighting if desired.
+#
+# For ArchTuple, each entry in ARCHES is a primary arch. group is used
+# to group related arches together. current is whether it's currently
+# a primary arch.
+#
+# For LoadTuple, each entry in FLAVORS or LOADOUTS is something we
+# consider a unique flavor or loadout. subs is an iterable of sub-
+# flavors.
+ArchTuple = collections.namedtuple('ArchTuple',
+                                   'name values group weight current')
+LoadTuple = collections.namedtuple('LoadTuple', 'name values subs weight')
+
+KOJI_BASE = 'https://kojipkgs.fedoraproject.org/work'
+RSYNC = 'rsync://dl.fedoraproject.org/fedora-buffet'
+HTTPS = 'https://download.fedoraproject.org/pub'
+HTTPS_DL = 'https://dl.fedoraproject.org/pub'
+KNOWN_PREFIXES = (RSYNC, HTTPS, 'http://download.fedoraproject.org/pub',
+                  HTTPS_DL, 'http://dl.fedoraproject.org/pub')
+MASH_BASE = 'https://kojipkgs.fedoraproject.org/mash'
+
+DISK_EXTS = ('.raw.xz', '.qcow2')
+FILESYSTEM_EXTS = ('.tar.gz',)
+ISO_EXTS = ('.iso',)
+IMAGE_EXTS = DISK_EXTS + FILESYSTEM_EXTS + ISO_EXTS
+ISO_TYPES = ('dvd', 'live', 'netinst', 'disc1', 'disc2', 'disc3', 'disc4',
+             'disc5', 'disc6', 'rescuecd')
+
+ARCHES = (
+    ArchTuple('x86_64', ('x86_64',), 'intel', 2000, True),
+    ArchTuple('i386', ('i386', 'i486', 'i586', 'i686'), 'intel', 2000, True),
+    ArchTuple('armhfp', ('armhfp',), 'arm', 1000, True),
+    ArchTuple('ppc', ('ppc',), 'ppc', 1000, False),
+    ArchTuple('ppc64', ('ppc64',), 'ppc', 1000, False))
+
+FLAVORS = (
+    LoadTuple('cloud', ('cloud',), ('base', 'atomic'), 200),
+    LoadTuple('workstation', ('workstation',), (), 220),
+    LoadTuple('server', ('server',), (), 210))
+
+# This has to be ordered so we get the right result for e.g. 'kde'
+# vs. 'jam_kde'
+LOADOUTS = (
+    LoadTuple('desktop', ('desktop',), (), 200),
+    LoadTuple('kde', ('kde',), (), 190),
+    LoadTuple('xfce', ('xfce',), (), 80),
+    LoadTuple('soas', ('soas',), (), 72),
+    LoadTuple('mate', ('mate_compiz', 'mate-compiz', 'mate'), (), 71),
+    LoadTuple('lxde', ('lxde',), (), 70),
+    LoadTuple('design_suite', ('design_suite', 'design-suite'), (), 60),
+    LoadTuple('electronic_lab', ('electronic_lab', 'electronic-lab'), (), 60),
+    LoadTuple('games', ('games',), (), 60),
+    LoadTuple('robotics', ('robotics',), (), 60),
+    LoadTuple('security', ('security',), (), 60),
+    LoadTuple('jam_kde', ('jam_kde',), (), 60),
+    LoadTuple('scientific_kde', ('scientific_kde', 'scientific-kde'), (), 60),
+    LoadTuple('minimal', ('minimal',), (), 90),
+    # When Docker gets more supported we should bump it up to ~KDE weight
+    LoadTuple('docker', ('docker',), (), 55),
+    LoadTuple('source', ('source',), (), 50))
---
+++ packages/fedfind/fedfind-1.2.tar.gz/fedfind-1.2/fedfind/helpers.py
@@ -0,0 +1,116 @@
+# Copyright (C) 2015 Red Hat
+#
+# This file is part of fedfind.
+#
+# fedfind is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Author: Adam Williamson <awilliam@redhat.com>
+
+"""Miscellaneous small functions that don't need to be class methods."""
+
+import datetime
+import logging
+import os
+import six
+# Upstream docs recommend using subprocess32 if possible, but make
+# this optional as it's not available in EPEL.
+try:
+    import subprocess32 as subprocess
+except ImportError:
+    import subprocess
+from six.moves.urllib.parse import urlparse
+from six.moves.urllib.request import urlopen
+from six.moves.urllib.error import URLError, HTTPError
+
+logger = logging.getLogger(__name__)
+
+def date_check(value, fail_raise=True, out='obj'):
+    """Checks whether a value is a date, and returns it if so. Valid
+    dates are datetime.date instances or a string or int 'YYYYMMDD'.
+    If out is set to 'obj', returns a datetime.date object. If out is
+    set to 'both', returns a (string, object) tuple. If out is set to
+    'str' (or anything else), returns a 'YYYYMMDD' string.
+
+    On failure - empty or non-date-y input - will raise an exception
+    if fail_raise is set, otherwise will return False.
+    """
+    if isinstance(value, datetime.date):
+        dateobj = value
+    else:
+        try:
+            dateobj = datetime.datetime.strptime(str(value), '%Y%m%d')
+        except ValueError:
+            if fail_raise:
+                err = "{0} is not a valid date.".format(value)
+                raise ValueError(err)
+            else:
+                return False
+
+    if out == 'obj':
+        return dateobj
+    else:
+        datestr = dateobj.strftime('%Y%m%d')
+        if out == 'both':
+            return (datestr, dateobj)
+        else:
+            return datestr
+
+def url_exists(url):
+    """Checks whether a URL exists, by trying to open it."""
+    scheme = urlparse(url).scheme
+    if 'http' in scheme:
+        logger.debug("url_exists: checking {0} as HTTP".format(url))
+        try:
+            urlopen(url)
+            return True
+        except (ValueError, URLError, HTTPError):
+            return False
+    elif 'rsync' in scheme:
+        logger.debug("url_exists: checking {0} as rsync".format(url))
+        null = open(os.devnull)
+        args = ('/usr/bin/rsync', url)
+        # Doesn't use check_output as it's not in Python 2.6
+        process = subprocess.Popen(args, stdout=null, stderr=null)
+        process.communicate()
+        retcode = process.poll()
+        if retcode:
+            return False
+        return True
+
+def comma_list(string):
+    """Split a comma-separated list of values into a list (used by the
+    CLI for passing query parameters etc).
+    """
+    return string.split(',')
+
+def ci_match(val1, val2, exact, neg):
+    """Case-insensitive match, handles either term being false-y. More
+    or less expects strings; forces both values to str() to do the
+    comparison so it tolerates ints (though it matches them like
+    strings). Used by the query stuff.
+    """
+    logger.debug(
+        "ci_match: comparing {0} and {1}, exact {2}, neg {3}".format(
+            val1, val2, exact, neg))
+    if not val1 and not val2:
+        ret = True
+    elif not val1 or not val2:
+        ret = False
+    elif exact:
+        ret = str(val1).lower() == str(val2).lower()
+    else:
+        ret = str(val1).lower() in str(val2).lower()
+    if neg:
+        ret = not ret
+    return ret
