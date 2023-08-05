09185cf927bb2c990c0702f23fb830bcd5e096f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:18 +0000

    Add package django-health-monitor/0.1.6/django-health-monitor-0.1.6.tar.gz

---
+++ packages/django-health-monitor/django-health-monitor-0.1.6.tar.gz/django-health-monitor-0.1.6/health_monitor/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.utils import timezone
+from health_monitor import health_helper
+from jsonfield import JSONField
+
+
+class Health(models.Model):
+    uid = models.IntegerField(unique=True, db_index=True, verbose_name="UID")
+    state = JSONField(default={}, blank=True, null=True)
+    severity = JSONField(default={}, blank=True, null=True)
+    change_date = models.DateTimeField(default=None, blank=True, null=True)
+
+    def __unicode__(self):      # For Python 2, use __str__ on Python 3
+        return unicode(self.uid)
+
+    def add_state_keys(self, group, test_name):
+        # Initialize self.state for group if needed
+        if group not in self.state.keys():
+            self.state[group] = {}
+        # Initialize self.state[group] for test if needed
+        if test_name not in self.state[group].keys():
+            self.state[group][test_name] = {}
+
+    def update_score(self, test_name, score):
+        """Update the health based on the test name and score."""
+        now = timezone.now()
+
+        for group in health_helper.get_group_list_for_test(test_name):
+            # update score in state dict
+            if test_name in health_helper.get_health_keys(group):
+                self.add_state_keys(group, test_name)
+                # Update score and updated_at
+                self.state[group][test_name]['score'] = score
+                self.state[group][test_name]['updated_at'] = now
+                # update severity
+                old_severity = self.severity[group] if group in self.severity.keys() else None
+                self.severity[group] = health_helper.calculate_severity(group, self.state)
+
+                if old_severity != self.severity[group]:
+                    self.change_date = now
+        self.save()
---
+++ packages/django-health-monitor/django-health-monitor-0.1.6.tar.gz/django-health-monitor-0.1.6/health_monitor/views.py
@@ -0,0 +1,111 @@
+"""Import user-defined configuration files declared in settings.py"""
+import sys
+
+from django.conf import settings
+sys.path.append(settings.HEALTH_MONITOR_CONFIG)
+
+try:
+    from dispatcher import get_dispatcher
+except ImportError as e:
+    raise ImportError('settings.HEALTH_MONITOR_CONFIG not properly set. See docs at https://django-health-monitor.readthedocs.io/en/latest/usage.html#configure-scoring-logic')
+
+
+"""Imports needed for generic views:
+
+health/read/
+health/update/
+"""
+import datetime
+import json
+
+from django.http import HttpResponse
+from health_monitor import health_helper
+from health_monitor.models import Health
+
+try:
+    from django.db.models.loading import get_model
+except ImportError:
+    from django.apps import apps
+    get_model = apps.get_model
+
+
+"""Generic Views
+
+health/<uid>/read/
+health/<uid>/history/<group>/?start_time=<start_time>&end_time=<end_time>
+health/<uid>/update/
+"""
+
+
+def read(request, uid):
+    """Generic view to read health for a single UID."""
+    try:
+        health = Health.objects.get(uid=uid)
+        response_data = {
+            'uid': health.uid,
+            'state': health.state,
+            'status': 'success',
+            'severity': health.severity,
+        }
+    except Exception as e:
+        response_data = {
+            'uid': uid,
+            'status': 'failure',
+            'message': str(e)
+        }
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
+
+
+def history(request, uid, group):
+    """Generic view to return historical test results.
+
+    Time should be passed in url in the format '%Y-%m-%d %H:%M:%S'
+    Default start_time is two hours in the past.
+    Default end_time is now.
+    """
+    start_time = request.GET['start_time'] if 'start_time' in request.GET.keys() else datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+    end_time = request.GET['end_time'] if 'end_time' in request.GET.keys() else datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+    import ipdb
+    ipdb.set_trace()
+
+    response_data = {}
+    dispatcher = get_dispatcher()
+    health_keys = health_helper.get_health_keys(group)
+
+    for test_name in health_keys:
+        model = get_model('monitoring', dispatcher[test_name]['model'])
+        response_data[test_name] = {}
+        if 'time' in dispatcher[test_name].keys():
+            timerange = dispatcher[test_name]['time'] + '__range'
+            model.objects.filter(**{timerange: (start_time, end_time)})
+        elif 'start_time' in dispatcher[test_name].keys() and 'end_time' in dispatcher[test_name].keys():
+            start_timerange = dispatcher[test_name]['start_time'] + '__gte'
+            end_timerange = dispatcher[test_name]['end_time'] + '__lte'
+            model.objects.filter(**{start_timerange: start_time}).filter(**{end_timerange: end_time})
+
+
+def update(request, uid=None, test_name=None):
+    """Generic view to update health for a single UID."""
+    kwargs = {}
+    response_data = {}
+
+    if request.GET:
+        for key, value in request.GET.items():
+            kwargs[key] = value
+
+    # calculate health score: red, orange, yellow, green
+    try:
+        score = health_helper.get_score(test_name, **kwargs)
+    except LookupError as e:
+        response_data['status'] = 'error'
+        response_data['message'] = str(e)
+        return HttpResponse(json.dumps(response_data), content_type="application/json")
+
+    health = Health.objects.get_or_create(uid=uid)[0]
+    health.update_score(test_name=test_name, score=score)
+
+    response_data['status'] = 'success'
+    response_data['score'] = score
+    response_data['message'] = '{} changed to {} for uid {}'.format(test_name, score, uid)
+
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
