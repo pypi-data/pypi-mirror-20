cb41676eb9fc35590f1dd703c6d8d5901a68e0e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:30 +0000

    Add package pixiedust/0.83/pixiedust-0.83.tar.gz

---
+++ packages/pixiedust/pixiedust-0.83.tar.gz/pixiedust-0.83/pixiedust/__init__.py
@@ -0,0 +1,59 @@
+# -------------------------------------------------------------------------------
+# Copyright IBM Corp. 2016
+# 
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# -------------------------------------------------------------------------------
+
+__all__=['packageManager','display','services','utils']
+
+import warnings
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+
+    try:
+        #Check if we have an python shell available, if not, use our ProxyShell
+        get_ipython()
+    except NameError:
+        from .proxyShell import ProxyInteractiveShell
+        ProxyInteractiveShell.instance()   
+
+    #shortcut to logging
+    import pixiedust.utils.pdLogging as pdLogging
+    logger = pdLogging.getPixiedustLogger()
+    getLogger = pdLogging.getLogger
+
+    #shortcut to packageManager
+    import pixiedust.packageManager as packageManager
+    printAllPackages=packageManager.printAllPackages
+    installPackage=packageManager.installPackage
+    uninstallPackage=packageManager.uninstallPackage
+
+    import pixiedust.display
+    import pixiedust.services
+
+    #automated import into the user namespace
+    try:
+        from IPython.core.getipython import get_ipython
+        get_ipython().user_ns["display"]=display.display
+
+        #javaBridge and scalaBridge only work in the driver, not an executor
+        from pixiedust.utils.javaBridge import *
+        from pixiedust.utils.scalaBridge import *
+
+        #shortcut to Spark job monitoring
+        from pixiedust.utils.sparkJobProgressMonitor import enableSparkJobProgressMonitor
+        from pixiedust.utils.sampleData import sampleData
+        enableJobMonitor = enableSparkJobProgressMonitor
+    except NameError:
+        #IPython not available we must be in a spark executor
+        pass
---
+++ packages/pixiedust/pixiedust-0.83.tar.gz/pixiedust-0.83/pixiedust/display/chart/renderers/commonOptions.py
@@ -0,0 +1,104 @@
+# -------------------------------------------------------------------------------
+# Copyright IBM Corp. 2017
+# 
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# -------------------------------------------------------------------------------
+import math
+
+def clusterBy(displayObject):
+    return { 
+        'name': 'clusterby',
+        'description': 'Cluster By',
+        'metadata': {
+            'type': "dropdown",
+            'values': ["None"] + [f for f in displayObject.getFieldNames() if f not in displayObject.getKeyFields() and f not in displayObject.getValueFields()],
+            'default': ""
+        }
+    }
+
+def barChart(displayObject):
+    options = []
+    options.append(clusterBy(displayObject))
+    options.append({
+        'name': 'orientation',
+        'description': 'Orientation',
+        'metadata': {
+            'type': 'dropdown',
+            'values': ['vertical', 'horizontal'],
+            'default': "vertical"
+        }
+    })
+    if displayObject.options.get("clusterby") != None or len(displayObject.getValueFields()) > 1:
+        options.append({
+            'name': 'charttype',
+            'description': 'Type',
+            'metadata': {
+                'type': 'dropdown',
+                'values': ['grouped', 'stacked', 'subplots'],
+                'default': "grouped"
+            }
+        })
+    return options
+
+def lineChart(displayObject):
+    options = []
+
+    options.append(clusterBy(displayObject))
+
+    if displayObject.options.get("clusterby") != None or len(displayObject.getValueFields()) > 1:
+        options.append({
+            'name': 'lineChartType',
+            'description': 'Type',
+            'metadata': {
+                'type': 'dropdown',
+                'values': ['grouped', 'subplots'],
+                'default': "grouped"
+            }
+        })
+
+    options.append({
+        'name': 'logx',
+        'description': 'log scale on x',
+        'metadata': {
+            'type': 'checkbox',
+            'default': "false"
+        }
+    })
+    options.append({
+        'name': 'logy',
+        'description': 'log scale on y',
+        'metadata': {
+            'type': 'checkbox',
+            'default': "false"
+        }
+    })
+    return options
+
+def histogram(displayObject):
+    count = len(displayObject.getWorkingPandasDataFrame().index)
+    return [
+        {
+            'name': 'binsize',
+            'description': 'Bin size',
+            'metadata': {
+                'type': 'slider',
+                'max': int(max(math.ceil(count / 2), 4)),
+                'min': int(max(math.floor(count / 20), 2)),
+                'default': int(max(math.ceil(count / 4), 3))
+            }
+        }
+    ]
+
+commonOptions = {}
+for f in [barChart,lineChart,histogram]:
+    commonOptions.update({f.__name__:f})
