1c4bdeb2efedd5e294b65ba4d1907603f47a120a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:42 +0000

    Add package petl_ldap3/0.0.5/petl_ldap3-0.0.5.tar.gz

---
+++ packages/petl_ldap3/petl_ldap3-0.0.5.tar.gz/petl_ldap3-0.0.5/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+long_description = 'Please see the documentation at the `project page <https://github.com/PGower/petl_ldap3>`_ .'
+
+packages = [
+    'petl_ldap3',
+]
+
+package_data = {
+    '': ['LICENSE', 'README.md'],
+}
+
+setup(
+    name='petl_ldap3',
+    version='0.0.5',
+    # MAJOR version when you make incompatible API changes,
+    # MINOR version when you add functionality in a backwards-compatible manner, and
+    # PATCH version when you make backwards-compatible bug fixes.
+    description='A petl view class for ldap data.',
+    long_description=long_description,
+    author='Paul Gower',
+    author_email='p.gower@gmail.com',
+    url='https://github.com/PGower/petl_ldap3',
+    download_url='https://github.com/PGower/petl_ldap3/releases',
+    py_modules=['petl_ldap3'],
+    package_data=package_data,
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: System :: Systems Administration :: Authentication/Directory',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords=['petl', 'ldap3'],
+    install_requires=['ldap3', 'petl'],
+)
