d8923ccf437d39f4df8cd9511ee11ccc735ebe9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:34 +0000

    Add package tiffstack2avi/1.1.4/tiffstack2avi-1.1.4.zip

---
+++ packages/tiffstack2avi/tiffstack2avi-1.1.4.zip/tiffstack2avi-1.1.4/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+try:
+    import pypandoc
+    long_description=pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError, RuntimeError):
+    long_description=open('README.md').read()
+
+setup(
+  name = 'tiffstack2avi',
+  packages = [],
+  version = '1.1.4',
+  py_modules = ['tiffstack2avi'],
+  description = 'A small python wrapper for ffmpeg',
+  author = 'Taro Kiritani',
+  author_email = 'taro.kiritani@epfl.ch',
+  url = 'https://github.com/tarokiritani/tiffstack2avi',
+  download_url = 'https://github.com/tarokiritani/tiffstack2avi/tarball/1.1.4',
+  keywords = ['ffmpeg', 'python', 'wrapper'],
+  classifiers = [
+                 'Programming Language :: Python :: 3.5'
+                 ],
+  license='MIT',
+  long_description=long_description,
+  install_requires=[
+          'PyQt5',
+      ],
+)
