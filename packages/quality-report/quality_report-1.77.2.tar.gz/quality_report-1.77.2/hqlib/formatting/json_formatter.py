11042a1b1170e4493248faf97e2d7675463327a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:53 +0000

    Add package quality-report/1.77.2/quality_report-1.77.2.tar.gz

---
+++ packages/quality-report/quality_report-1.77.2.tar.gz/quality_report-1.77.2/hqlib/formatting/json_formatter.py
@@ -0,0 +1,58 @@
+"""
+Copyright 2012-2017 Ministerie van Sociale Zaken en Werkgelegenheid
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+from __future__ import absolute_import
+
+import logging
+
+from . import base_formatter
+from .. import metric_info, metric_source
+
+
+class JSONFormatter(base_formatter.Formatter):
+    """ Format the report in JSON. This is used for generating a history file. """
+
+    def prefix(self, report):
+        """ Return a JSON formatted version of the report prefix. """
+        prefix_elements = []
+        # Add the product versions of trunk versions to the prefix
+        sonar = report.project().metric_source(metric_source.Sonar)
+        for product in report.products():
+            sonar_product_info = metric_info.SonarProductInfo(sonar, product)
+            sonar_id = sonar_product_info.sonar_id()
+            latest_version = sonar_product_info.latest_version()
+            prefix_elements.append('"{sonar_id}-version": "{version}"'.format(sonar_id=sonar_id,
+                                                                              version=latest_version))
+        # Add the current date to the prefix
+        prefix_elements.append('"date": "{date}"'.format(date=report.date().strftime('%Y-%m-%d %H:%M:%S')))
+        return '{' + ', '.join(prefix_elements) + ', '
+
+    def metric(self, metric):
+        """ Return a JSON formatted version of the metric. """
+        # Write numerical values without decimals.
+        logging.info('Formatting metric %s.', metric.stable_id())
+        try:
+            return '"{sid}": ("{val:.0f}", "{stat}", "{date}"), '.format(sid=metric.stable_id(),
+                                                                         val=metric.numerical_value(),
+                                                                         stat=metric.status(),
+                                                                         date=metric.status_start_date())
+        except ValueError:
+            logging.error('Error formatting %s', metric.stable_id())
+            raise
+
+    @staticmethod
+    def postfix():
+        """ Return a JSON formatted version of the report postfix. """
+        return '}\n'
---
+++ packages/quality-report/quality_report-1.77.2.tar.gz/quality_report-1.77.2/tests/unittests/report/section_tests.py
@@ -0,0 +1,140 @@
+"""
+Copyright 2012-2017 Ministerie van Sociale Zaken en Werkgelegenheid
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import unittest
+
+from hqlib.report.section import Section, SectionHeader
+
+
+class FakeMetric(object):
+    """ Fake metric to use in the tests below. """
+    def __init__(self, status=''):
+        self.__status = status
+
+    @staticmethod
+    def set_id_string(id_string):
+        """ Ignore. """
+        pass
+
+    def status(self):
+        """ Return the preset status. """
+        return self.__status
+
+
+class SectionHeaderTest(unittest.TestCase):
+    """ Unit tests for the section header class. """
+
+    def setUp(self):
+        self.__header = SectionHeader('TE', 'title', 'subtitle')
+
+    def test_title(self):
+        """ Test that the title is correct. """
+        self.assertEqual('title', self.__header.title())
+
+    def test_subtitle(self):
+        """ Test that the subtitle is correct. """
+        self.assertEqual('subtitle', self.__header.subtitle())
+
+    def test_id_prefix(self):
+        """ Test that the id prefix is correct. """
+        self.assertEqual('TE', self.__header.id_prefix())
+
+
+class SectionTest(unittest.TestCase):
+    """ Unit tests for the section class. """
+
+    def setUp(self):
+        self.__header = SectionHeader('TE', 'title', 'subtitle')
+        self.__metrics = [FakeMetric('green'), FakeMetric('perfect'), FakeMetric('yellow'), FakeMetric('grey'),
+                          FakeMetric('red')]
+        self.__section = Section(self.__header, self.__metrics)
+
+    def test_title(self):
+        """ Test that the title of the section is the title of the header. """
+        self.assertEqual(self.__header.title(), self.__section.title())
+
+    def test_subtitle(self):
+        """ Test that the subtitle of the section is the subtitle of the header. """
+        self.assertEqual(self.__header.subtitle(), self.__section.subtitle())
+
+    def test_id_prefix(self):
+        """ Test that the id prefix of the section is the id prefix of the header. """
+        self.assertEqual(self.__header.id_prefix(), self.__section.id_prefix())
+
+    def test_str(self):
+        """ Test that str(section) returns the title of the section. """
+        self.assertEqual(self.__section.title(), str(self.__section))
+
+    def test_get_metric(self):
+        """ Test that the section is a list of metrics. """
+        self.assertEqual(self.__metrics[0], self.__section[0])
+
+    def test_get_all_metrics(self):
+        """ Test that the section has a list of all metrics. """
+        self.assertEqual(self.__metrics, self.__section.metrics())
+
+    def test_color_red(self):
+        """ Test that the section is red when one metric is red. """
+        metrics = [FakeMetric('green'), FakeMetric('perfect'), FakeMetric('red'), FakeMetric('grey'),
+                   FakeMetric('yellow'), FakeMetric('missing')]
+        section = Section(self.__header, metrics)
+        self.assertEqual('red', section.color())
+
+    def test_color_red_when_missing(self):
+        """ Test that the section is red when one metric is missing and the rest is green or yellow. """
+        metrics = [FakeMetric('green'), FakeMetric('perfect'), FakeMetric('yellow'), FakeMetric('missing')]
+        section = Section(self.__header, metrics)
+        self.assertEqual('red', section.color())
+
+    def test_color_red_when_missing_source(self):
+        """ Test that the section is red when one metric source is missing and the rest is green or yellow. """
+        metrics = [FakeMetric('green'), FakeMetric('perfect'), FakeMetric('yellow'), FakeMetric('missing_source')]
+        section = Section(self.__header, metrics)
+        self.assertEqual('red', section.color())
+
+    def test_color_yellow(self):
+        """ Test that the section is yellow when no metrics are red and at least one is yellow. """
+        metrics = [FakeMetric('green'), FakeMetric('perfect'), FakeMetric('yellow'), FakeMetric('grey')]
+        section = Section(self.__header, metrics)
+        self.assertEqual('yellow', section.color())
+
+    def test_color_grey(self):
+        """ Test that the section is grey when no metrics are red or yellow and at least one is grey. """
+        metrics = [FakeMetric('green'), FakeMetric('perfect'), FakeMetric('grey')]
+        section = Section(self.__header, metrics)
+        self.assertEqual('grey', section.color())
+
+    def test_color_green(self):
+        """ Test that the section is green when no metrics are red, yellow or grey. """
+        metrics = [FakeMetric('green'), FakeMetric('perfect')]
+        section = Section(self.__header, metrics)
+        self.assertEqual('green', section.color())
+
+    def test_color_perfect(self):
+        """ Test that the section is green when all metrics are perfect. """
+        metrics = [FakeMetric('perfect')]
+        section = Section(self.__header, metrics)
+        self.assertEqual('green', section.color())
+
+    def test_color_white(self):
+        """ Test that the section is white when it contains no metrics. """
+        section = Section(self.__header, [])
+        self.assertEqual('white', section.color())
+
+    def test_product(self):
+        """ Test that the section returns the product. """
+        section = Section(None, [], product='Product')
+        self.assertEqual('Product', section.product())
