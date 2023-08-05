7c1ae14ea0555187cd91935174f6949ac39f84d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:05 +0000

    Add package tinker-access-client/2017.2.22.504/tinker-access-client-2017.2.22.504.tar.gz

---
+++ packages/tinker-access-client/tinker-access-client-2017.2.22.504.tar.gz/tinker-access-client-2017.2.22.504/tinker_access_client/AutoUpdateTimer.py
@@ -0,0 +1,48 @@
+import threading
+
+from PackageInfo import PackageInfo
+from ClientOption import ClientOption
+from CommandExecutor import CommandExecutor
+
+
+class AutoUpdateTimer(object):
+
+    def __init__(self, client, opts):
+        self.__opts = opts
+        self.__client = client
+        self.__auto_update_timer = None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.cancel_auto_update_timer()
+
+    def __cancel_auto_update_timer(self):
+        if self.__auto_update_timer:
+            self.__auto_update_timer.cancel()
+
+        self.__auto_update_timer = None
+
+    def __auto_update_timer_tick(self):
+        if not self.__client.is_in_use():
+            CommandExecutor().execute_commands([
+                '{0} update'.format(PackageInfo.pip_package_name)
+            ])
+
+        self.__start_auto_update_timer()
+
+    def __start_auto_update_timer(self):
+        self.__cancel_auto_update_timer()
+
+        auto_update = self.__opts.get(ClientOption.AUTO_UPDATE)
+        if auto_update:
+            auto_update_interval = self.__opts.get(ClientOption.AUTO_UPDATE_INTERVAL) * 60
+            self.__auto_update_timer = threading.Timer(
+                auto_update_interval,
+                self.__auto_update_timer_tick
+            )
+        self.__auto_update_timer.start()
+
+    def start(self):
+        self.__start_auto_update_timer()
