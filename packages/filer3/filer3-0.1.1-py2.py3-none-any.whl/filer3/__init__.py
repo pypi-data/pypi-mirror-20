13d4fb3409f5dbd18b4adf18a13337898bb3a4f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:08 +0000

    Add package filer3/0.1.1/filer3-0.1.1-py2.py3-none-any.whl

---
+++ packages/filer3/filer3-0.1.1-py2.py3-none-any.whl/filer3/__init__.py
@@ -0,0 +1,7 @@
+import pkg_resources
+
+import filer3
+
+__version__ = pkg_resources.get_distribution('filer3').version
+
+Filer = filer3.Filer
---
+++ packages/filer3/filer3-0.1.1-py2.py3-none-any.whl/filer3/filer3.py
@@ -0,0 +1,121 @@
+# coding: utf-8
+"""
+ファイル操作をするFiler クラス
+"""
+
+import csv
+import pickle
+
+
+class Filer(object):
+
+    @staticmethod
+    def read_csv(path, option='r', encoding=''):
+        if encoding == '':
+            f = open(path, option)
+        else:
+            f = open(path, option, encoding=encoding)
+        try:
+            dataReader = csv.reader(f)
+            arr = [row for row in dataReader]
+            return arr
+        finally:
+            f.close()
+
+    @staticmethod
+    def write_csv(arr, path, option="w", encoding=''):
+        if encoding == '':
+            f = open(path, option)
+        else:
+            f = open(path, option, encoding=encoding)
+        try:
+            dataWriter = csv.writer(f)
+            dataWriter.writerows(arr)
+        finally:
+            f.close()
+
+    @staticmethod
+    def read_tsv(path, option='r', encoding=''):
+        if encoding == '':
+            f = open(path, option)
+        else:
+            f = open(path, option, encoding=encoding)
+        try:
+            dataReader = csv.reader(f, delimiter="\t")
+            arr = [row for row in dataReader]
+            return arr
+        finally:
+            f.close()
+
+    @staticmethod
+    def write_tsv(arr, path, option="w", encoding=''):
+        if encoding == '':
+            f = open(path, option)
+        else:
+            f = open(path, option, encoding=encoding)
+        try:
+            dataWriter = csv.writer(f, delimiter="\t")
+            dataWriter.writerows(arr)
+        finally:
+            f.close()
+
+    @staticmethod
+    def read_pkl(path, option='rb'):
+        f = open(path, option)
+        try:
+            arr = pickle.load(f)
+            return arr
+        finally:
+            f.close()
+
+    @staticmethod
+    def write_pkl(arr, path, option='wb'):
+        f = open(path, option)
+        try:
+            pickle.dump(arr, f)
+        finally:
+            f.close()
+
+    @staticmethod
+    def read_txt(path, option='r', encoding='', LF='\n'):
+        if encoding == '':
+            f = open(path, option)
+        else:
+            f = open(path, option, encoding=encoding)
+        try:
+            lines = f.readlines()
+            lines = [row.rstrip(LF) for row in lines]
+            return lines
+        finally:
+            f.close()
+
+    @staticmethod
+    def write_txt(arr, path, option="w", encoding='', LF='\n'):
+        if encoding == '':
+            f = open(path, option)
+        else:
+            f = open(path, option, encoding=encoding)
+        try:
+            for sentence in arr:
+                f.writelines(sentence+LF)
+        finally:
+            f.close()
+
+    @staticmethod
+    def conv_encoding(data):
+        lookup = ('utf_8', 'euc_jp', 'euc_jis_2004', 'euc_jisx0213',
+                  'shift_jis', 'shift_jis_2004', 'shift_jisx0213',
+                  'iso2022jp', 'iso2022_jp_1', 'iso2022_jp_2', 'iso2022_jp_3',
+                  'iso2022_jp_ext', 'latin_1', 'ascii')
+        encode = 'unicode'
+        for encoding in lookup:
+            try:
+                data = data.decode(encoding)
+                encode = encoding
+                break
+            except:
+                pass
+        if isinstance(data, str):
+            return data, encode
+        else:
+            raise LookupError
