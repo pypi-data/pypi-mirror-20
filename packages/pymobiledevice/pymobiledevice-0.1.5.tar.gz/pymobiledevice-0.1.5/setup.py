4ab97d1e28e24c0ff26110918a9b3d6cca3675db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:09 +0000

    Add package pymobiledevice/0.1.5/pymobiledevice-0.1.5.tar.gz

---
+++ packages/pymobiledevice/pymobiledevice-0.1.5.tar.gz/pymobiledevice-0.1.5/setup.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# vim: fenc=utf-8
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+#
+#
+
+"""
+File name: setup.py
+Version: 0.1
+Author: dhilipsiva <dhilipsiva@gmail.com>
+Date created: 2015-11-24
+"""
+
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+f = path.join(here, 'README.md')
+
+requirements = open('requirements.txt').read().splitlines()
+
+setup(
+    name='pymobiledevice',
+    version='0.1.5',
+    description="Interface with iOS devices",
+    url='https://github.com/appknox/pymobiledevice',
+    author='dhilipsiva',
+    author_email='dhilipsiva@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='pymobiledevice ios iphone ipad ipod',
+    packages=find_packages(),
+    py_modules=['pymobiledevice'],
+    entry_points='',
+    install_requires=requirements,
+    extras_require={
+        'dev': [''],
+        'test': [''],
+    },
+)
