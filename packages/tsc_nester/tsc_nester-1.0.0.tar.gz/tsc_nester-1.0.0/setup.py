309dce5ab51ba00aa59075732ee50a95897f375d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:18 +0000

    Add package tsc_nester/1.0.0/tsc_nester-1.0.0.tar.gz

---
+++ packages/tsc_nester/tsc_nester-1.0.0.tar.gz/tsc_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name         = 'tsc_nester',
+        version      = '1.0.0',
+        py_modules   = ['tst_nester'],
+        author       = 'hfpython by tiscal404',
+        author_email = 'hfpython@headfirstlabs.com',
+        url          = 'http://www.headfirstlabs.com',
+        description  = 'A simple printer of nested lists',
+    )
