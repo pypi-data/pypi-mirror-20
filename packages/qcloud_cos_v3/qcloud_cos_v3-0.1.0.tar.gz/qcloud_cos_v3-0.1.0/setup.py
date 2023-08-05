364391a091e57e646212301f2337e42226cd8716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:06 +0000

    Add package qcloud_cos_v3/0.1.0/qcloud_cos_v3-0.1.0.tar.gz

---
+++ packages/qcloud_cos_v3/qcloud_cos_v3-0.1.0.tar.gz/qcloud_cos_v3-0.1.0/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import sys
+if sys.version_info[0] != 2 or sys.version_info[1] < 6:
+    sys.exit('Sorry, only python 2.6 or 2.7 is supported')
+
+from setuptools import setup, find_packages
+setup(
+        name = 'qcloud_cos_v3',
+        version = '0.1.0',
+        description = 'python sdk for tencent qcloud cos',
+        license = 'MIT License',
+        install_requires=['requests'],
+
+        author = 'chengwu',
+        author_email = 'chengwu@tencent.com',
+
+        packages = find_packages(),
+        )
