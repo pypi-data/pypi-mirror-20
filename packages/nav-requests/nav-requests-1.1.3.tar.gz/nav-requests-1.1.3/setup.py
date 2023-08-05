b559b0f837950ba552bc6d39451e044e711a8a5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:32 +0000

    Add package nav-requests/1.1.3/nav-requests-1.1.3.tar.gz

---
+++ packages/nav-requests/nav-requests-1.1.3.tar.gz/nav-requests-1.1.3/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+
+setup_kwargs = dict(
+    name='nav-requests',
+    version='1.1.3',
+    description='Renamed to `nav`. See https://pypi.python.org/pypi/nav',
+    packages=['nav_requests'],
+    include_package_data=True,
+    author='Jacob Magnusson',
+    author_email='m@jacobian.se',
+    url='https://pypi.python.org/pypi/nav',
+    license='BSD',
+    platforms='any',
+    install_requires=[
+        'argh',
+        'lxml',
+        'requests',
+        'requests-ntlm',
+        'xmltodict',
+    ],
+    extras_require={
+        'test': {
+            'coverage>=4.2',
+            'flake8>=3.0.4',
+            'pytest>=3.0.3',
+        },
+    },
+    entry_points={
+        'console_scripts': [
+            'navrequest = nav_requests:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
+
+if __name__ == '__main__':
+    setup(**setup_kwargs)
