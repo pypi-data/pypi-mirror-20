ff2201a7ee3f20d8e26c964edecbf38203f4c5dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:51 +0000

    Add package multicache/0.1.4/multicache-0.1.4.tar.gz

---
+++ packages/multicache/multicache-0.1.4.tar.gz/multicache-0.1.4/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='multicache',
+    packages=['multicache'],
+    version='0.1.4',
+    description='Simple caching mechanisms',
+    author='Michal Ovciarik',
+    author_email='movciari@gmail.com',
+    url='https://github.com/bargulg/multicache',
+    download_url='https://github.com/bargulg/multicache/tarball/0.1',
+    keywords=['cache', 'caching', 'simple', 'easy', 'multi'],
+    classifiers=[]
+)
