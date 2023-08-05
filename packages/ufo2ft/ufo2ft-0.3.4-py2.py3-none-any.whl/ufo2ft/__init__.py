6614bc93d0b6bdd7f153ac2d2b03e23f3b00d3dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:46 +0000

    Add package ufo2ft/0.3.4/ufo2ft-0.3.4-py2.py3-none-any.whl

---
+++ packages/ufo2ft/ufo2ft-0.3.4-py2.py3-none-any.whl/ufo2ft/__init__.py
@@ -0,0 +1,42 @@
+from __future__ import print_function, division, absolute_import
+
+from ufo2ft.kernFeatureWriter import KernFeatureWriter
+from ufo2ft.makeotfParts import FeatureOTFCompiler
+from ufo2ft.markFeatureWriter import MarkFeatureWriter
+from ufo2ft.otfPostProcessor import OTFPostProcessor
+from ufo2ft.outlineOTF import OutlineOTFCompiler, OutlineTTFCompiler
+
+
+__version__ = "0.3.4"
+
+
+def compileOTF(ufo, outlineCompilerClass=OutlineOTFCompiler,
+               featureCompilerClass=FeatureOTFCompiler, mtiFeaFiles=None,
+               kernWriter=KernFeatureWriter, markWriter=MarkFeatureWriter,
+               glyphOrder=None, convertCubics=True, cubicConversionError=2,
+               useProductionNames=True, optimizeCff=True):
+    """Create FontTools CFF font from a UFO.
+
+    Some arguments are only used when generating CFF or TrueType outlines:
+    `convertCubics` and `cubicConversionError` only apply to TrueType, and
+    `optimizeCff` only applies to CFF.
+    """
+
+    outlineCompiler = outlineCompilerClass(
+        ufo, glyphOrder, convertCubics, cubicConversionError)
+    otf = outlineCompiler.compile()
+
+    featureCompiler = featureCompilerClass(
+        ufo, otf, kernWriter, markWriter, mtiFeaFiles=mtiFeaFiles)
+    featureCompiler.compile()
+
+    postProcessor = OTFPostProcessor(otf, ufo)
+    otf = postProcessor.process(useProductionNames, optimizeCff)
+
+    return otf
+
+
+def compileTTF(ufo, outlineCompilerClass=OutlineTTFCompiler, **kwargs):
+    """Create FontTools TrueType font from a UFO."""
+
+    return compileOTF(ufo, outlineCompilerClass=outlineCompilerClass, **kwargs)
