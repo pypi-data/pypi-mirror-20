a3687ffa134a62723b7c2fd2c9c65d06d6836fd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:00 +0000

    Add package validation21/0.2.5/validation21-0.2.5.tar.gz

---
+++ packages/validation21/validation21-0.2.5.tar.gz/validation21-0.2.5/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='validation21',
+    packages=['validation21'],
+    version='0.2.5',
+    description='Validation library for Python.',
+    author='Brian S Morgan',
+    author_email='brian.s.morgan@gmail.com',
+    url='https://github.com/bmorgan21/python-validation',
+    install_requires=[
+        'enum21>=0.2.0',
+        'python-dateutil'
+    ],
+)
