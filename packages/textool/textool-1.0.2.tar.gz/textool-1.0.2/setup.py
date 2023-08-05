11a448d6c9235047d7120b56e30779ffeab19932
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:50 +0000

    Add package textool/1.0.2/textool-1.0.2.tar.gz

---
+++ packages/textool/textool-1.0.2.tar.gz/textool-1.0.2/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'textool',
+    version = '1.0.2',
+    keywords = ('textool', 'texturepacker'),
+    description = 'A command line tool to convert TexturePacker file format.',
+    license = 'MIT License',
+    install_requires = [
+        'Pillow',
+        'parse'
+    ],
+    url = 'https://github.com/guobin8205/texturetool',
+    author = 'guobin',
+    author_email = 'guobin2013@hotmail.com',
+    packages = find_packages("src"),
+    package_dir = {'':'src'},
+    entry_points = {
+        'console_scripts': [
+            'textool = textool.textool:main',
+        ],
+    }
+)
