57763824114b3ce0a2006acc08c941de74225d73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:10 +0000

    Add package nginxparser_eb/0.0.5/nginxparser_eb-0.0.5.tar.gz

---
+++ packages/nginxparser_eb/nginxparser_eb-0.0.5.tar.gz/nginxparser_eb-0.0.5/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='nginxparser_eb',
+      version='0.0.5',
+      description='Nginx configuration Parser',
+      author='Fatih Erikli',
+      author_email='fatiherikli@gmail.com',
+      url='https://github.com/EnigmaBridge/nginxparser',
+      license=open('LICENSE').read(),
+      long_description=open('README.rst').read(),
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP'
+      ],
+
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=[
+          'pyparsing>=1.5.5'
+      ],
+      extras_require={
+            'dev': ['mockio']
+      },
+)
+
+
