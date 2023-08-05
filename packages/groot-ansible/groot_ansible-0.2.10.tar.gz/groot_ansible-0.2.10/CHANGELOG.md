ea73f53b354e1f67dc65141993d2bcaa6b0759fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:17 +0000

    Add package groot-ansible/0.2.10/groot_ansible-0.2.10.tar.gz

---
+++ packages/groot-ansible/groot_ansible-0.2.10.tar.gz/groot_ansible-0.2.10/CHANGELOG.md
@@ -0,0 +1,108 @@
+# Change Log
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/).
+
+## [Unreleased]
+### Added
+- ...
+
+### Changed
+- ...
+
+## [0.2.10] - 2017-02-12
+### Added
+- [groot/workstation] creating and importing my public/private keys
+
+### Changed
+- [groot/workstation] .netrc configured via a vault variable now
+
+## [0.2.9] - 2017-02-10
+### Added
+- [groot/workstation] ttygif
+- [ubuntu] ttyrec
+- [kubuntu] usb-creator-kde
+
+### Changed
+- [workstation] split between workstation and groot-workstation
+
+## [0.2.8] - 2017-02-06
+### Added
+- [ros2] role, subcommand added
+- [gazebo] role, subcommand added
+
+### Changed
+- [groot/workstation] vcs_extras -> vci
+- [workstation] split between workstation and groot-workstation
+
+## [0.2.7] - 2017-02-06
+### Changed
+- [vci] bugfix non-use of the index variable
+- [bootstrap] comment reminding to login in case of group changes
+- [workstation] bugfix non-creation of ~/.config/fcitx
+
+## [0.2.6] - 2017-02-05
+### Changed
+- [workstation] role renamed to groot/workstation
+- [workstation] help for the drive Makefile
+
+## [0.2.5] - 2017-02-04
+### Added
+- [workstation] python .pypirc configuration template added
+
+## [0.2.4] - 2017-02-04
+### Changed
+- [workstation] google drive helper Makefile
+- [workstation] eclipse startup script for groot-eclipse
+
+## [0.2.3] - 2017-02-02
+### Added
+- [vci] module added
+- [testies] role, playbook, subcommand added
+- [chrome] role, playbook, subcommand added
+- [drive] role, playbook, subcommand added
+- [git] role, playbook, subcommand added
+- [ubuntu] role, playbook, subcommand added
+- [kubuntu] role, playbook, subcommand added
+- [powerline] role, playbook, subcommand added
+
+### Changed
+- [workstation] complete functionality of first iteration
+
+## [0.2.2] - 2017-01-29
+### Added
+- [workstation] enable canonical partner repositories
+- [workstation] create ~/.gnupg directory
+- [workstation] purge unity conflict packages before installing kubuntu-full
+- [workstation] gitconfig setup
+- [workstation] netrc setup
+
+### Changed
+- [workstation] role tasks modularised
+
+## [0.2.1] - 2017-01-26
+### Changed
+- [infra] bugfix install rule trying to install roles dir
+
+## [0.2.0] - 2017-01-26
+### Added
+- [bootstrap] user groups
+- [bootstrap] ansible/snorriheim ppas
+- [ros] generic ros install/update
+- [workstation] lots of ppa's and debs
+- [update] updates itself and ansible
+
+## [0.1.0] - 2017-01-26
+### Added
+- [infra] skeleton structure
+
+[Unreleased]: https://github.com/stonier/groot_ansible/compare/0.2.8...HEAD
+[0.2.8]: https://github.com/stonier/groot_ansible/compare/0.2.7...0.2.8
+[0.2.7]: https://github.com/stonier/groot_ansible/compare/0.2.6...0.2.7
+[0.2.6]: https://github.com/stonier/groot_ansible/compare/0.2.5...0.2.6
+[0.2.5]: https://github.com/stonier/groot_ansible/compare/0.2.4...0.2.5
+[0.2.4]: https://github.com/stonier/groot_ansible/compare/0.2.3...0.2.4
+[0.2.3]: https://github.com/stonier/groot_ansible/compare/0.2.2...0.2.3
+[0.2.2]: https://github.com/stonier/groot_ansible/compare/0.2.1...0.2.2
+[0.2.1]: https://github.com/stonier/groot_ansible/compare/0.2.0...0.2.1
+[0.2.0]: https://github.com/stonier/groot_ansible/compare/0.1.0...0.2.0
+[0.1.0]: https://github.com/stonier/groot_ansible/compare/d85f7d176f25cf9bec221bd309cd3e4c942891ad...0.1.0
---
+++ packages/groot-ansible/groot_ansible-0.2.10.tar.gz/groot_ansible-0.2.10/groot_ansible/__init__.py
@@ -0,0 +1,99 @@
+#
+# License: BSD
+#    https://raw.githubusercontent.com/stonier/groot_ansible/devel/LICENSE
+#
+##############################################################################
+# Documentation
+##############################################################################
+
+"""
+Python wrapped ansible scripts for installing and updating systems for
+various use cases.
+"""
+
+##############################################################################
+# Imports
+##############################################################################
+
+import argparse
+
+from . import common
+from . import console
+from . import ros
+from . import update
+from . import testies
+from . import workstation
+
+##############################################################################
+# Constants
+##############################################################################
+
+__version__ = '0.2.10'
+
+##############################################################################
+# Main
+##############################################################################
+
+
+def version_string():
+    return console.cyan + "Version" + console.reset + " : " + console.yellow + __version__ + console.reset
+
+
+def main(args=None):
+    """
+    Entry point to the console 'groot-ansible' tool.
+    """
+    try:
+        parser = argparse.ArgumentParser(
+            description="A frontend to groot playbooks.",
+            epilog="And his noodly appendage reached forth to tickle the blessed...\n",
+            formatter_class=argparse.RawDescriptionHelpFormatter)
+        parser.add_argument('-v', '--version', action='version', version=version_string())
+        subparsers = parser.add_subparsers(title='commands',
+                                           help='valid commands for groot-ansible interactions')
+        update.add_subparser(subparsers)
+        ros.add_subparser(subparsers)
+        workstation.add_subparser(subparsers)
+        common.add_generic_subparser(subparsers, "ubuntu",
+                                     short_description="extras for a development environment",
+                                     description="Extra packages and configuration for the core of a basic development environment",
+                                     become_sudo=True)
+        common.add_generic_subparser(subparsers, "kubuntu",
+                                     short_description="switch to a kubuntu (full) environment",
+                                     description="Add the kubuntu (full) desktop environment to an ubuntu installation",
+                                     become_sudo=True)
+        common.add_generic_subparser(subparsers, "git",
+                                     short_description="git binaries, modules and configuration",
+                                     description="Git binaries, modules (lfs) and user configuration",
+                                     become_sudo=True)
+        common.add_generic_subparser(subparsers, "chrome",
+                                     short_description="google chrome for ubuntu",
+                                     description="Google chrome setup/install/update",
+                                     become_sudo=True)
+        common.add_generic_subparser(subparsers, "drive",
+                                     short_description="google drive for ubuntu",
+                                     description="Google drive setup/install/update",
+                                     become_sudo=True)
+        common.add_generic_subparser(subparsers, "powerline",
+                                     short_description="powerline in the shell for the user",
+                                     description="Setup powerline in the shell for the user",
+                                     become_sudo=True)
+        common.add_generic_subparser(subparsers, "gazebo",
+                                     short_description="gazebo and related packages for ubuntu",
+                                     description="Gazebo repository setup/install/update",
+                                     become_sudo=True)
+        common.add_generic_subparser(subparsers, "ros2",
+                                     short_description="ros2 environment for ubuntu",
+                                     description="ROS 2 environment setup/install/update",
+                                     become_sudo=True)
+        common.add_generic_subparser(subparsers, "workstation",
+                                     short_description="standard development environment for ubuntu",
+                                     description="Standard setup/install/update for a development workstation",
+                                     become_sudo=True)
+        testies.add_subparser(subparsers)
+        options = parser.parse_args(args)
+        # options, unused_unknown_args = parser.parse_known_args(args)
+        options.func(options)  # relay arg parsing to the subparser configured `set_defaults` function callback
+
+    except KeyboardInterrupt:
+        print('Interrupted by user!')
