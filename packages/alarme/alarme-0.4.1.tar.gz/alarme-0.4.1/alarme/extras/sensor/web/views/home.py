a472a4d00c7bfcf838455f27607f163cd2bf8cda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:35 +0000

    Add package alarme/0.4.1/alarme-0.4.1.tar.gz

---
+++ packages/alarme/alarme-0.4.1.tar.gz/alarme-0.4.1/alarme/extras/sensor/web/views/home.py
@@ -0,0 +1,20 @@
+from aiohttp.web import HTTPFound
+
+from .core import CoreView
+from ..util import login_required, handle_exception
+
+
+class Home(CoreView):
+
+    @login_required
+    async def req(self):
+        return HTTPFound(self.request.app.router.get('control').url())
+
+    @handle_exception
+    async def get(self):
+        self.sensor.app.stop()
+        return await self.req()
+
+    @handle_exception
+    async def post(self):
+        return await self.req()
