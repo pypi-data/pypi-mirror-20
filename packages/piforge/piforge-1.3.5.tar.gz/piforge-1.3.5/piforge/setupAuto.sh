ceee3b39f22c874e4064ae757d19a517a7bdd017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:20 +0000

    Add package piforge/1.3.5/piforge-1.3.5.tar.gz

---
+++ packages/piforge/piforge-1.3.5.tar.gz/piforge-1.3.5/piforge/setupAuto.sh
@@ -0,0 +1,53 @@
+#! /bin/sh
+
+buildFile() {
+if [ -e /etc/init.d/piforgeStart ]; then
+	sudo rm /etc/init.d/piforgeStart
+fi
+
+sudo cat <<EOF >/etc/init.d/piforgeStart
+#! /bin/sh
+#/etc/init.d/piforgeStart
+### BEGIN INIT INFO
+# Provides: piforgeStart
+# Required-Start: \$remote_fs \$syslog
+# Required-Stop: \$remote_fs \$syslog
+# Default-Start: 2 3 4 5
+# Default-Stop: 0 1 6
+# Short-Description: start a program from boot
+# Description: A simple script  which will start a program from boot and stop upon shut-down
+### END INIT INFO
+# Put any commands you always want to run here.
+sudo piforge
+EOF
+return
+}
+
+
+echo "*******WARNING*****"
+echo "piforge is designed to be installed on Raspberry Pi devices"
+echo "in a classroom environment. This will install a webserver on your device"
+echo "that will accept and execute python code with root permission."
+echo "this will be very convenient in a classroom lab environment but presents"
+echo "a major security hole on a production system."
+echo "*******END WARNING*****"
+echo ""
+echo "Do you want piforge to load automatically at startup [y|n]"
+read answer
+
+
+if [ "$answer" = "y" ] ; then
+	echo "configuring piforge startup"
+	buildFile
+	sudo chmod +x /etc/init.d/piforgeStart
+	sudo update-rc.d piforgeStart defaults
+
+else
+	if [ -e /etc/init.d/piforgeStart ]; then
+		echo "removing startup file"
+		sudo update-rc.d piforgeStart remove
+		sudo rm /etc/init.d/piforgeStart
+	fi
+	echo "piforge will NOT load automatically"
+fi
+echo "launch piforge servery by typing: sudo piforge"
