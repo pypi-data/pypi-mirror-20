6cd0ffcd992e083d80cad1bf20159ba58be286da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:53 +0000

    Add package cartoview/1.1.8/cartoview-1.1.8.tar.gz

---
+++ packages/cartoview/cartoview-1.1.8.tar.gz/cartoview-1.1.8/cartoview/settings.py
@@ -0,0 +1,13 @@
+from geonode.settings import *
+INSTALLED_APPS += ('geonode-client', "cartoview", "cartoview.app_manager", "cartoview.user_engage", )
+ROOT_URLCONF = "cartoview.urls"
+import geonode
+import cartoview
+CARTOVIEW_ROOT = os.path.abspath(os.path.dirname(cartoview.__file__))
+GEONODE_ROOT = os.path.abspath(os.path.dirname(geonode.__file__))
+TEMPLATES[0]["DIRS"] = [os.path.join(CARTOVIEW_ROOT, "templates")] + TEMPLATES[0]["DIRS"]
+STATICFILES_DIRS += [os.path.join(CARTOVIEW_ROOT, "static"),]
+cartoview_apps_settings_path = os.path.join(CARTOVIEW_ROOT, 'app_manager', "settings.py")
+execfile(cartoview_apps_settings_path)
+
+LAYER_PREVIEW_LIBRARY = "react"
---
+++ packages/cartoview/cartoview-1.1.8.tar.gz/cartoview-1.1.8/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+  name = 'cartoview',
+  packages = find_packages(),
+  version = '1.1.8',
+  description = 'Cartoview is a GIS web mapping application framework to easily share and deploy apps based on Geonode',
+  author = 'Cartologic',
+  author_email = 'info@cartologic.com',
+  url = 'https://github.com/cartologic/cartoview',
+  include_package_data=True,
+  keywords = ['cartoview', 'gis', 'geonode', "django", "web mapping", "applications", "apps", "application management"],
+  classifiers = [
+    "Development Status :: 3 - Alpha",
+    "Framework :: Django :: 1.8",
+    "Topic :: Scientific/Engineering :: GIS"
+  ],
+  license="BSD",
+  install_requires= [
+    "geonode>=2.5.15",
+    "django-geonode-client"
+  ]
+)
+
