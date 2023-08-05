4fa8010b07543de38d6903278fca3b051682ddb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:05 +0000

    Add package openfigi/0.0.3/openfigi-0.0.3.tar.gz

---
+++ packages/openfigi/openfigi-0.0.3.tar.gz/openfigi-0.0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='openfigi',
+    version='0.0.3',
+    description='A simple wrapper for openfigi.com',
+    long_description=readme,
+    author='Julian Wergieluk',
+    author_email='julian@wergieluk.com',
+    url='https://github.com/jwergieluk/openfigi',
+    license=license,
+    packages=find_packages(),
+    install_requires=['requests', 'click'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+    entry_points={'console_scripts': ['ofg = openfigi.__main__:call_figi']}
+)
