cb411f2a08c9571a3c179ce17c514e609c5d0154
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:15 +0000

    Add package nukecontexts/0.1.1/nukecontexts-0.1.1-py2-none-any.whl

---
+++ packages/nukecontexts/nukecontexts-0.1.1-py2-none-any.whl/nukecontexts/__init__.py
@@ -0,0 +1,23 @@
+import os
+import re
+import sys
+import platform
+
+try:
+    os.environ['DOCS_CONTEXT']
+except:
+    if platform.system() == 'Darwin':
+        application = r'Nuke\d+\.\d+v\d+.app'
+    elif platform.system() == 'Windows':
+        application = r'Nuke\d+\.\d+.exe'
+    else:
+        raise RuntimeError('OS {0} is not supported'.format(platform.system()))
+
+    match = re.search(application, sys.executable)
+    if not match:
+        raise RuntimeError('Import nukecontexts from within Nuke')
+
+__version__ = '0.1.1'
+__all__ = ['ctx']
+
+from ctx import enabled, disabled, set_attr, multiple_contexts
