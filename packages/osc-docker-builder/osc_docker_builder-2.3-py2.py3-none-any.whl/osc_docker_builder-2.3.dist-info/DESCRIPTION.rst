a2862e2d821658f355cb1858c591d6fd59a0087d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:36 +0000

    Add package osc-docker-builder/2.3/osc_docker_builder-2.3-py2.py3-none-any.whl

---
+++ packages/osc-docker-builder/osc_docker_builder-2.3-py2.py3-none-any.whl/osc_docker_builder-2.3.dist-info/DESCRIPTION.rst
@@ -0,0 +1,208 @@
+Docker image builder for Openstack clients (aka OSCs docker builder)
+====================================================================
+
+|Latest Version| |Downloads| |Code Issues| |codecov| |CircleCI|
+
+A lot of people need install/update/upgrade/downgrade any Openstack
+python client in any time. Thanks to Docker containers it is easy to get
+an isolated environment with all Openstack clients of specific upstream
+versions that you wish.
+
+Pre-requisites
+==============
+
+List of prerequisites :
+
+-  docker image: Ensure that there is an official python docker image
+   for the provided python version parameter.
+-  tox : All clients are using tox, and python version parameter will be
+   matched in env list.
+-  common release: All clients have to have the same release, in other
+   cases you must specify a different release (by property 'release' in
+   the osc.yml file) to override global release parameter. For example,
+   in the osc.yml you may see that gnocchi client has 'master' release
+   instead of 'stable/newton'.
+
+Build a docker image with OSCs
+==============================
+
+This module can be installed by pip:
+
+::
+
+    $ pip install osc-docker-builder
+
+Best way to get help about the command is :
+
+::
+
+    osc-builder --help
+    usage: ocs [-h] [-bp BUILD_PATH] [-f CONFIG_FILE] [-pv PYTHON_VERSION]
+               [-r RELEASE] [-c CLIENTS] [-sf] [-v]
+
+    Build a docker image with all Openstack clients that you want for a specific
+    upstream branch and python version
+
+    optional arguments:
+      -h, --help            show this help message and exit
+      -bp BUILD_PATH, --build-path BUILD_PATH
+                            The build path where files are written.
+      -f CONFIG_FILE, --config-file CONFIG_FILE
+                            A YAML config file.
+      -pv PYTHON_VERSION, --python-version PYTHON_VERSION
+                            Python version for docker image
+                            base(https://hub.docker.com/_/python/).For example :
+                            2.7, 3.5.2
+      -r RELEASE, --release RELEASE
+                            Upstream release.
+      -c CLIENTS, --clients CLIENTS
+                            Provide all openstack python clients that you want.By
+                            defaults only python-openstackclient will be
+                            installed.
+      -sf, --skip-fails     Skip failures and create the image.
+      -v, --verbose         Show details.
+
+Ready for action ?, suppose that you want to create a docker image for
+these python version and Openstack clients:
+
+::
+
+    osc-builder --python-version 3.4 --clients openstack --clients heat --release stable/newton --build-path /tmp/osc-docker-builder
+
+I recommend using a config file (which could be controlled by a version
+control system):
+
+::
+
+    osc-builder -f osc.yml
+
+This command can be launched form the source code. The main function
+will be found in osc\_docker\_builder package :
+
+::
+
+    $ osc.py -f osc.yml -bp /tmp/osc-builder
+
+As command execution output we have a docker image ready to be used.
+Push your images to your private registry or use my images at "engapa"
+account in dockerhub.com
+
+Running and using docker container
+==================================
+
+For example, run a container based on latest image for python client 2.7
+and release stable/newton :
+
+::
+
+    $ docker run -it -d --name osc engapa/osc:2.7-stable_newton-latest
+    1f395d7273b99b734725fcbab4ebd05082f21978e0c4e3104cc8332c7430920d
+    $ docker ps
+    CONTAINER ID   IMAGE                               COMMAND     CREATED        STATUS       PORTS  NAMES
+    1f395d7273b9   engapa/osc:2.7-stable_newton-latest "/bin/bash" 2 seconds ago  Up 3 seconds        osc
+
+For example let's view the version of the Heat client installed in this
+container:
+
+::
+
+    $ docker exec -it 1f395d7273b9 /bin/bash -c "heat --version"
+    1.5.0
+
+If you prefer operate within the container :
+
+::
+
+    $ docker attach 1f395d7273b9
+    root@1f395d7273b9:# pip list --format columns | grep -i "^python-.*client"
+    python-openstackclient         3.2.1.dev10 /src/python-openstackclient
+    python-saharaclient            0.18.1.dev1 /src/python-saharaclient
+    python-searchlightclient       1.0.1.dev1  /src/python-searchlightclient
+    python-senlinclient            1.0.1.dev14 /src/python-senlinclient
+    python-solumclient             2.1.2.dev2  /src/python-solumclient
+    python-swiftclient             3.1.1.dev1  /src/python-swiftclient
+    ...
+    root@1f395d7273b9:/#
+
+If you want to update all python client versions (if provided release
+was a branch , e.g. : master) just execute this command within the
+container:
+
+::
+
+    $ docker attach 1f395d7273b9
+    root@1f395d7273b9:/root# pip install -U -r requirements.txt
+
+An interesting module of openstack is 'os-client-config', it aims you to
+use a cloud configuration file shared by volume mount point (at "/root",
+"/root/.config/openstack" or "/etc/openstack" directory):
+
+::
+
+    $ cat << EOF > cloud.yml
+    > clouds:
+    >   spaincloud:
+    >     auth:
+    >       username: engapa
+    >       password: XXXXXXXXX
+    >       project_name: OSOOS
+    >       auth_url: 'https://spaincloud.com:5001/v2.0'
+    >     region_name: SpainSalamanca
+    >     dns_api_version: 1
+    > EOF
+    $ docker run -it --name osc -v $(pwd)/clouds.yml:/root/.config/openstack/clouds.yml engapa/osc:2.7-stable_newton-latest
+    root@1536ac361c6c:~# openstack --os-cloud spaincloud server list
+    ...
+
+Developer mode
+==============
+
+Main tasks with code are managed by
+`tox <https://tox.readthedocs.io/en/latest/>`__
+
+To launch tests just type:
+
+::
+
+    $ tox -r
+    ...
+
+    Name                        Stmts   Miss  Cover
+    -----------------------------------------------
+    osc_docker_builder.py           0      0   100%
+    osc_docker_builder/osc.py     129    102    70%
+    -----------------------------------------------
+    TOTAL                         129    102    70%
+    ----------------------------------------------------------------------
+    Ran 5 tests in 0.097s
+
+    OK
+    ___________________________________________________________________________ summary ____________________________________________________________________________
+      pep8: commands succeeded
+      py27: commands succeeded
+      py35: commands succeeded
+      congratulations :)
+
+Load the virtualenv and build a docker image :
+
+::
+
+    $ source .tox/py27/bin/activate
+    $(py27) osc-builder -f osc.yml
+
+Author
+======
+
+Enrique Garcia Pablos engapa@gmail.com
+
+.. |Latest Version| image:: https://img.shields.io/pypi/v/osc-docker-builder.svg
+   :target: https://pypi.python.org/pypi/osc-docker-builder/
+.. |Downloads| image:: https://img.shields.io/pypi/dm/osc-docker-builder.svg
+   :target: https://pypi.python.org/pypi/osc-docker-builder/
+.. |Code Issues| image:: https://www.quantifiedcode.com/api/v1/project/1a96eb463beb4512a203762481b0c1ab/badge.svg
+   :target: https://www.quantifiedcode.com/app/project/1a96eb463beb4512a203762481b0c1ab
+.. |codecov| image:: https://codecov.io/gh/engapa/osc-docker-builder/branch/master/graph/badge.svg
+   :target: https://codecov.io/gh/engapa/osc-docker-builder
+.. |CircleCI| image:: https://circleci.com/gh/engapa/osc-docker-builder/tree/master.svg?style=svg
+   :target: https://circleci.com/gh/engapa/osc-docker-builder/tree/master
+
---
+++ packages/osc-docker-builder/osc_docker_builder-2.3-py2.py3-none-any.whl/osc_docker_builder/osc.py
@@ -0,0 +1,285 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Author : Enrique Garcia Pablos <engapa@gmail.com>
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import sys
+import os
+import shutil
+import logging
+import multiprocessing
+import argparse
+import urllib
+import tox
+import subprocess
+import time
+import yaml
+
+from jinja2 import Environment, FileSystemLoader
+
+
+if sys.version_info < (2, 7):
+    sys.exit("This script requires Python 2.7 or newer!")
+
+
+LOG = logging.getLogger("osc-builder")
+logging.basicConfig()
+LOG.setLevel(logging.INFO)
+
+BUILD_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'build')
+TEMPLATES_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+
+
+def clean_build_dir(build_path, remove=False, create=True):
+    """
+    Clean build directory
+    :param build_path: Build path
+    :param remove: Remove root build directory, by default False
+    :param create: Create root build directory, by default True
+    :return: void
+    """
+    if os.path.exists(build_path):
+        for filename in os.listdir(build_path):
+            filepath = os.path.join(build_path, filename)
+            try:
+                shutil.rmtree(filepath)
+            except OSError:
+                os.remove(filepath)
+        if remove:
+            os.rmdir(build_path)
+
+    elif create:
+        os.makedirs(build_path)
+
+
+def parse_args(args):
+    """
+    Parses args
+    :param args: arguments
+    :return: parsed args
+    """
+
+    parser = argparse.ArgumentParser(
+        prog="ocs",
+        description="Build a docker image with all Openstack clients"
+                    " that you want for a specific upstream branch and python version")
+    parser.add_argument('-bp', '--build-path', dest='build_path',
+                        help='The build path where files are written.')
+    parser.add_argument('-f', '--config-file', dest='config_file',
+                        help='A YAML config file.')
+    parser.add_argument('-pv', '--python-version', dest='python_version',
+                        help='Python version for docker image base(https://hub.docker.com/_/python/).'
+                             'For example : 2.7, 3.5.2')
+    parser.add_argument('-r', '--release', dest='release',
+                        help='Upstream release.')
+    parser.add_argument('-c', '--clients', dest='clients', action='append',
+                        help='Provide all openstack python clients that you want.'
+                             'By defaults only python-openstackclient will be installed.')
+    parser.add_argument('-sf', '--skip-fails', dest='skip_fails', action='store_true',
+                        help='Skip failures and create the image.')
+    parser.add_argument('-v', '--verbose', dest='verbose', action='store_true',
+                        help='Show details.')
+
+    return parser.parse_args(args)
+
+
+def download_docker_image_base(python_version):
+    """
+    Download docker image base
+    :param python_version: python version
+    :return: void, pull the suitable docker image
+    """
+    child = subprocess.Popen(['docker', 'pull', 'python:{}'.format(python_version)],
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, error = child.communicate()
+    LOG.debug(output)
+    if error and child.returncode != 0:
+        LOG.error(error)
+        raise SystemExit(
+            "Unavailable to download docker image for python version {}".format(python_version))
+
+
+def download_tox_module(args):
+    """
+    Download tox.ini file from openstack client
+    :param args: client_name, url ,build path
+    :return: void, create a <client>-tox.ini file into build directory
+    """
+    client_name, url, build_path = args
+    start_time = time.time()
+    fname, info = urllib.urlretrieve(url,
+                                     filename='{}/{}-tox.ini'.format(build_path, client_name))
+    end_time = time.time()
+    LOG.debug(" Downloaded file : %s, size : %s bytes, time: %s",
+              fname, info.get('content-length'), end_time - start_time)
+
+
+def check_client_pv(client_name, py_version, skip_fails, build_path):
+    """
+    Matches python version with tox env
+    :param client_name: name of openstack client
+    :param py_version: python version
+    :param skip_fails: skip fails for this client and continue
+    :param build_path: Build path
+    :return: True if matches, False in other case
+    """
+    py_env = 'py'
+    if '.' in py_version:
+        py_env += py_version.replace('.', '')[0:2]
+    else:
+        py_env += py_version[0:2]
+
+    config = tox.session.prepare(
+        [
+            "-c{}/{}-tox.ini".format(build_path, client_name),
+            "-l"
+        ]
+    )
+    found = py_env in config.envlist
+
+    if not found:
+        msg = "Not found venv {} for client {}".format(py_env, client_name)
+        if not skip_fails:
+            raise SystemExit(msg)
+        else:
+            LOG.warning(msg)
+
+    return found
+
+
+def render_templates(python_version, client_configs, build_path):
+    """
+    Render Dockerfile.j2 and requirements.txt.j2 templates in files into build directory
+    :param python_version: python version
+    :param client_configs: openstack client configs
+    :param build_path: Build path
+    :return: void, rendered files into build directory
+    """
+
+    env = Environment(loader=FileSystemLoader(searchpath=TEMPLATES_PATH))
+    with open(build_path + '/requirements.txt', 'w') as requirements:
+        LOG.debug("Generating file requirements.txt")
+        requirements.write(
+            env.get_template('requirements.j2').render(
+                client_configs=client_configs
+            )
+        )
+    with open(build_path + '/Dockerfile', 'w') as dockerfile:
+        LOG.debug("Generating file Dockerfile")
+        dockerfile.write(
+            env.get_template('Dockerfile.j2').render(
+                python_version=python_version
+            )
+        )
+
+
+def build_docker_image(python_version, release, build_path):
+    """
+    Build docker image by calling docker local daemon
+    :param python_version: python version
+    :param release: upstream release
+    :param build_path: Build path
+    :return: void, get docker local image
+    """
+    tag = 'engapa/osc:{}-{}-latest'.format(python_version, release.replace('/', '_'))
+    child = subprocess.Popen(['docker', 'build', '-t', tag, build_path], stdin=subprocess.PIPE)
+    output, error = child.communicate()
+    if error and child.returncode != 0:
+        LOG.error(error)
+        raise SystemExit("Unavailable to build docker image")
+
+
+def client_config(client_name, release, egg=None):
+    """
+    Gets Openstack client config
+    :param client_name: Name of client
+    :param release: Release
+    :param egg: egg name
+    :return: Client config
+    """
+    return dict(
+        name=client_name,
+        release=release,
+        url="https://raw.githubusercontent.com/openstack/"
+            "python-{}client/{}/tox.ini".format(client_name, release),
+        egg=egg if egg else "python-{}client".format(client_name)
+    )
+
+
+def main():
+    """
+    Main function
+    """
+
+    args = parse_args(sys.argv[1:])
+
+    if args.config_file:
+        try:
+            with open(args.config_file, 'r') as config_file:
+                config = yaml.load(config_file)
+            python_version = config.get('python_version')
+            if python_version:
+                python_version = str(python_version)
+            release = config.get('release')
+            skip_fails = config.get('skip-fails', False)
+            verbose = config.get('verbose', False)
+            build_path = config.get('build_path', BUILD_PATH)
+            client_configs = [
+                client_config(x['name'], x.get('release', release), x.get('egg')) for x in config['clients']]
+        except yaml.YAMLError as exc:
+            LOG.error('Unable to load configuration from file: {} . Caused by : {}', config_file, exc)
+            sys.exit(1)
+        except Exception as e:
+            LOG.error(e)
+            sys.exit(1)
+    else:
+        python_version = args.python_version
+        release = args.release
+        skip_fails = args.skip_fails
+        verbose = args.verbose
+        build_path = args.build_path or BUILD_PATH
+        client_configs = [client_config(client, release) for client in args.clients]
+
+    # Required values :
+    assert python_version, 'Required python_version value'
+    assert release, 'Required release value'
+    assert client_configs, 'Required clients list'
+
+    if verbose:
+        LOG.setLevel(logging.DEBUG)
+
+    clean_build_dir(build_path)
+
+    download_docker_image_base(python_version)
+
+    pool = multiprocessing.Pool()
+    download_tox_module_args = [(client['name'], client['url'], build_path) for client in client_configs]
+    pool.map(download_tox_module, download_tox_module_args)
+
+    cclients = []
+
+    for client in client_configs:
+        if check_client_pv(client['name'], python_version, skip_fails, build_path):
+            cclients.append(client)
+
+    if cclients:
+        render_templates(python_version, client_configs, build_path)
+        build_docker_image(python_version, release, build_path)
+
+
+if __name__ == "__main__":
+    main()
