f1060a2d4ae5652a6c600729a4160168d78bf51f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:16 +0000

    Add package torequests/3.0.0/torequests-3.0.0.zip

---
+++ packages/torequests/torequests-3.0.0.zip/torequests-3.0.0/setup.py
@@ -0,0 +1,18 @@
+﻿
+
+from setuptools import setup, find_packages
+
+setup(
+    name='torequests',
+    version='3.0.0',
+    keywords=('requests tomorrow async multi-thread concurrent.futures asynchronous'),
+    description='Asynchronous requests. Using concurrent.futures to make requests async(like Tomorrow), and some functions to make async-coding easy. read more :https://github.com/ClericPy/torequests ',
+    license='MIT License',
+    install_requires=['requests','futures'],
+    py_modules=['torequests'],
+    author='ClericPy',
+    author_email='clericpy@gmail.com',
+    url='https://github.com/ClericPy/torequests',
+    packages=find_packages(),
+    platforms='any',
+)
