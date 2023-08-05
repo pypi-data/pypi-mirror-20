c5f84c9996d6658d86b853a39a5929cb0b39e1a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:01 +0000

    Add package my_python_package/0.3/my_python_package-0.3.zip

---
+++ packages/my_python_package/my_python_package-0.3.zip/my_python_package-0.3/my_python_package/__init__.py
@@ -0,0 +1,14 @@
+sdfsf
+sdfsfsdf
+fsfs
+def 
+def sdfsfsdf
+fd
+sdfsfsdfd
+
+
+for sd
+fsdfsf in xrange(1,10):
+	pass():
+	pass():
+	pass
---
+++ packages/my_python_package/my_python_package-0.3.zip/my_python_package-0.3/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'my_python_package',
+    packages = ['my_python_package'],
+    version = '0.3',  # Ideally should be same as your GitHub release tag varsion
+    description = 'description',
+    author = 'zenwraight',
+    author_email = 'kavitmht@gmail.com',
+    keywords = ['tag1', 'tag2'],
+    classifiers = [],
+)
