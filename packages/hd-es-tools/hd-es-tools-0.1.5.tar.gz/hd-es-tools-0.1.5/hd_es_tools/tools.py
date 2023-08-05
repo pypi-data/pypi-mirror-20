5a0b555af7e45dd40fe9e4da00810142c121d794
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:38 +0000

    Add package hd-es-tools/0.1.5/hd-es-tools-0.1.5.tar.gz

---
+++ packages/hd-es-tools/hd-es-tools-0.1.5.tar.gz/hd-es-tools-0.1.5/hd_es_tools/tools.py
@@ -0,0 +1,33 @@
+# -*- coding: UTF-8 -*-
+#
+# @author huangyue<huangyue@haodou.com>
+# @since 17/2/17
+#
+
+import os
+dirname, filename = os.path.split(os.path.abspath(__file__))
+
+
+
+def fav_import_main():
+    config = "%s/mongo_connector_doc_managers/config_fav.json" %(dirname)
+    command = "python mongo-connector -c %s" %(config)
+    res = os.system(command=command)
+    print res
+
+def favdir_import_main():
+    try:
+        config = "%s/mongo_connector_doc_managers/config_favdir.json" % (dirname)
+        command = "nohup mongo-connector -c %s &" % (config)
+        # res = os.system(command=command)
+        # print res
+        output = os.popen(command)
+        print output.read()
+    except Exception as e:
+        print e
+
+
+
+
+
+favdir_import_main()
