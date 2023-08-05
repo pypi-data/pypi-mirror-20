966c53cfec5c759aac4b4d915db56d4ad3375541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:28 +0000

    Add package turbodbc/1.0.2/turbodbc-1.0.2.tar.gz

---
+++ packages/turbodbc/turbodbc-1.0.2.tar.gz/turbodbc-1.0.2/src/turbodbc/connection.cpp
@@ -0,0 +1,40 @@
+#include "turbodbc/connection.h"
+#include <sqlext.h>
+
+namespace turbodbc {
+
+connection::connection(std::shared_ptr<cpp_odbc::connection const> low_level_connection) :
+	parameter_sets_to_buffer(1000),
+	use_async_io(false),
+	buffer_size_(megabytes(20)),
+	connection_(low_level_connection)
+{
+	connection_->set_attribute(SQL_ATTR_AUTOCOMMIT, SQL_AUTOCOMMIT_OFF);
+}
+
+void connection::commit() const
+{
+	connection_->commit();
+}
+
+void connection::rollback() const
+{
+	connection_->rollback();
+}
+
+cursor connection::make_cursor() const
+{
+	return {connection_, buffer_size_, parameter_sets_to_buffer, use_async_io};
+}
+
+turbodbc::buffer_size connection::get_buffer_size() const
+{
+    return buffer_size_;
+}
+
+void connection::set_buffer_size(turbodbc::buffer_size buffer_size)
+{
+    buffer_size_ = buffer_size;
+}
+
+}
---
+++ packages/turbodbc/turbodbc-1.0.2.tar.gz/turbodbc-1.0.2/src/turbodbc_numpy/make_numpy_array.cpp
@@ -0,0 +1,49 @@
+#include <turbodbc_numpy/make_numpy_array.h>
+
+#include <turbodbc_numpy/ndarrayobject.h>
+
+namespace turbodbc_numpy {
+
+using pybind11::reinterpret_steal;
+using pybind11::object;
+
+object make_empty_numpy_array(numpy_type const & type)
+{
+	npy_intp no_elements = 0;
+	int const flags = 0;
+	int const one_dimensional = 1;
+	// __extension__ needed because of some C/C++ incompatibility.
+	// see issue https://github.com/numpy/numpy/issues/2539
+	return reinterpret_steal<object>(__extension__ PyArray_New(&PyArray_Type,
+	                                                           one_dimensional,
+	                                                           &no_elements,
+	                                                           type.code,
+	                                                           nullptr,
+	                                                           nullptr,
+	                                                           type.size,
+	                                                           flags,
+	                                                           nullptr));
+}
+
+
+pybind11::object make_empty_numpy_array(std::string const & descriptor)
+{
+	npy_intp no_elements = 0;
+	int const flags = 0;
+	int const one_dimensional = 1;
+	// __extension__ needed because of some C/C++ incompatibility.
+	// see issue https://github.com/numpy/numpy/issues/2539
+	PyArray_Descr * descriptor_ptr = nullptr;
+	__extension__ PyArray_DescrConverter(pybind11::cast(descriptor).ptr(), &descriptor_ptr);
+
+	return reinterpret_steal<object>(__extension__ PyArray_NewFromDescr(&PyArray_Type,
+	                                                                    descriptor_ptr,
+	                                                                    one_dimensional,
+	                                                                    &no_elements,
+	                                                                    nullptr,
+	                                                                    nullptr,
+	                                                                    flags,
+	                                                                    nullptr));
+}
+
+}
