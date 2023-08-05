5434e0b2dfa95ec2138506c6de58c947ce98195b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:59 +0000

    Add package symbolic-quantum-computation/0.1.14/symbolic-quantum-computation-0.1.14.tar.gz

---
+++ packages/symbolic-quantum-computation/symbolic-quantum-computation-0.1.14.tar.gz/symbolic-quantum-computation-0.1.14/test.py
@@ -0,0 +1,14 @@
+import unittest
+import src.test_module_a
+import src.test_module_b
+
+
+def run_tests():
+    loader = unittest.TestLoader()
+    suite = loader.loadTestsFromModule(src.test_module_a)
+    suite.addTests(loader.loadTestsFromModule(src.test_module_b))
+    suite.addTests(loader.loadTestsFromModule(src.test_module_b.ClassB))
+
+    runner = unittest.TextTestRunner(verbosity=2)
+    result = runner.run(suite)
+
