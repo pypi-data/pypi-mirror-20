e5ee2d0e696664f23e2a02bd14c9ce2f5cfdc21f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:27 +0000

    Add package pythonpm/0.1/pythonpm-0.1.tar.gz

---
+++ packages/pythonpm/pythonpm-0.1.tar.gz/pythonpm-0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pythonpm',
+    version=0.1,
+    description='The Python Package Manager that make your life easier.',
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Build Tools",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+    keywords='easy_install distutils setuptools',
+    url='https://github.com/luisholanda/ppm',
+    author='Luis Holanda',
+    author_email='luiscmholanda@gmail.com',
+    license='MIT',
+    packages=find_packages(exclude=['spec']),
+    install_requires=[
+        'pip>=9.0.1',
+    ],
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'ppm = ppm:main'
+        ]
+    }
+)
