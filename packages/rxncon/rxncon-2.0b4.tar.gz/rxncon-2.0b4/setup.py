620dc2dde5991869b7926a76f868d31bba921ffd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:55 +0000

    Add package rxncon/2.0b4/rxncon-2.0b4.tar.gz

---
+++ packages/rxncon/rxncon-2.0b4.tar.gz/rxncon-2.0b4/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+from sys import version_info, exit
+
+
+if not (version_info.major == 3 and version_info.minor >= 5):
+    exit('The rxncon framework requires Python version 3.5 or higher. Please upgrade.')
+
+
+setup(
+    name='rxncon',
+    packages=find_packages(),
+    package_data={
+        '': ['*.xls', '*.tsv', '*.xgmml']
+    },
+    scripts=[
+        'rxncon2bngl.py',
+        'rxncon2boolnet.py',
+        'rxncon2reactiongraph.py',
+        'rxncon2regulatorygraph.py'
+    ],
+    version='2.0b4',
+    description='The reaction-contingency framework for cellular signalling processes.',
+    author='The rxncon group @ Humboldt University Berlin',
+    author_email='jesper.romers@hu-berlin.de',
+    url='https://github.com/rxncon/rxncon',
+    download_url='https://github.com/rxncon/rxncon/tarball/2.0b4',
+    license='LGPL',
+    keywords=['sysbio', 'signalling', 'systems biology'],
+    classifiers=[],
+    install_requires=[
+        'pytest',
+        'numpy',
+        'scipy',
+        'pyeda',
+        'click',
+        'click_log',
+        'colorama',
+        'xlrd',
+        'networkx',
+
+    ]
+)
