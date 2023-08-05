00c1f4537a25bd3cf7bef77a38b647be9c37ca6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:08 +0000

    Add package Flask-Assistant/0.1.4/Flask-Assistant-0.1.4.tar.gz

---
+++ packages/Flask-Assistant/Flask-Assistant-0.1.4.tar.gz/Flask-Assistant-0.1.4/flask_assistant/__init__.py
@@ -0,0 +1,17 @@
+import logging
+
+logger = logging.getLogger('flask_assistant')
+logger.addHandler(logging.StreamHandler())
+if logger.level == logging.NOTSET:
+    logger.setLevel(logging.INFO)
+
+
+from flask_assistant.core import (
+    Assistant,
+    context_manager
+)
+
+from flask_assistant.response import ask, tell
+from flask_assistant.manager import Context
+
+from flask_assistant.hass import HassRemote
