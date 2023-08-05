a85326aac3403311ae83bd39ce9669ef6b6babdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:35 +0000

    Add package configurationutil/1.1.0/configurationutil-1.1.0.tar.gz

---
+++ packages/configurationutil/configurationutil-1.1.0.tar.gz/configurationutil-1.1.0/configurationutil/__init__.py
@@ -0,0 +1,8 @@
+
+from configurationutil.configuration import (
+    Configuration, CONST, APP_NAME, APP_AUTHOR, APP_VERSION
+)
+
+from configurationutil.cfg_objects.json_config import (
+    JSONConfig
+)
