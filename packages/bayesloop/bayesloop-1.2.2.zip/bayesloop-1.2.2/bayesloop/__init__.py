de41af9a47d4153bd3a97ecc691e2600c975551a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:48 +0000

    Add package bayesloop/1.2.2/bayesloop-1.2.2.zip

---
+++ packages/bayesloop/bayesloop-1.2.2.zip/bayesloop-1.2.2/bayesloop/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# import study types
+from .core import Study, HyperStudy, ChangepointStudy, OnlineStudy
+
+# observation models and transition models need to be distinguishable
+from . import observationModels
+from . import observationModels as om  # short form
+from . import transitionModels
+from . import transitionModels as tm  # short form
+
+# misc
+from .helper import cint, oint
+from .jeffreys import getJeffreysPrior, computeJeffreysPriorAR1
+from .fileIO import save, load
