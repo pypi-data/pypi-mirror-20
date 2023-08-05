c12cf1feb0e80390bae83cc86203711fe02acef0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:47 +0000

    Add package tinker-access-client/2017.2.12.360/tinker-access-client-2017.2.12.360.tar.gz

---
+++ packages/tinker-access-client/tinker-access-client-2017.2.12.360.tar.gz/tinker-access-client-2017.2.12.360/tinker_access_client/Service.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+# Reference: https://wiki.debian.org/LSBInitScripts
+
+### BEGIN INIT INFO
+# Provides:          tinker-access-client
+# Required-Start:    $all
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: tinker-access-client
+# Description:       The tinker-access-client service is responsible for coordinating communication
+# between RPi's modules (i.e. RFID reader, LCD, power relays etc..) and the remote tinker_access_server.
+### END INIT INFO
+
+import sys
+import logging
+from ClientLogger import ClientLogger
+from ClientDaemon import ClientDaemon
+from CommandHandler import CommandHandler
+from ClientOptionParser import ClientOptionParser, Command
+
+
+def run():
+    logger = ClientLogger.setup()
+    (opts, args) = ClientOptionParser().parse_args()
+
+    try:
+        with CommandHandler(opts, args) as handler:
+            handler.on(Command.STOP, ClientDaemon.stop)
+            handler.on(Command.START, ClientDaemon.start)
+            handler.on(Command.STATUS, ClientDaemon.status)
+            handler.on(Command.RESTART, ClientDaemon.restart)
+            return handler.handle_command()
+
+    except Exception as e:
+        logger.exception(e)
+        sys.stdout.write(str(e))
+        sys.stdout.flush()
+        sys.exit(1)
+
+    finally:
+        logging.shutdown()
+
+if __name__ == '__main__':
+    run()
