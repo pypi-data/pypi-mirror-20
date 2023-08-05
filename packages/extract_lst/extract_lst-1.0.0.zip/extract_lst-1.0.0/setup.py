7729005f0671c94b09055a186933e4bb7ba3a9af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:12 +0000

    Add package extract_lst/1.0.0/extract_lst-1.0.0.zip

---
+++ packages/extract_lst/extract_lst-1.0.0.zip/extract_lst-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name         = 'extract_lst',
+    version      = '1.0.0', 
+    py_modules   = ['nester'],
+    author       = 'Sid V',
+    author_email = 'sid194785@gmail.com',
+    url          = '',
+    description  = 'Extracts JAva Codes from Online Source codes provided by Tata McGrew Hill Book for Java - The Complete Reference 9th Edition.',
+)
