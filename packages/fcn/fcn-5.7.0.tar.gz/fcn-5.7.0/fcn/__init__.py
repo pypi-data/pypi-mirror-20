27922d3afaa499463326327f49c0ba568ed8fc61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:55 +0000

    Add package fcn/5.7.0/fcn-5.7.0.tar.gz

---
+++ packages/fcn/fcn-5.7.0.tar.gz/fcn-5.7.0/fcn/__init__.py
@@ -0,0 +1,13 @@
+import fcn.data  # NOQA
+import fcn.datasets  # NOQA
+import fcn.inferencer  # NOQA
+import fcn.models  # NOQA
+import fcn.trainer # NOQA
+import fcn.trainers  # NOQA
+import fcn.training  # NOQA
+import fcn.utils  # NOQA
+import fcn.utils as util  # For Backward-compatibility  # NOQA
+
+
+Inferencer = fcn.inferencer.Inferencer
+Trainer = fcn.trainer.Trainer
