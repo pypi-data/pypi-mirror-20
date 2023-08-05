c50676ffde5fb11a3a64b8b4a66ff840ebad1db3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:10 +0000

    Add package python-intercom/3.0.5/python-intercom-3.0.5.tar.gz

---
+++ packages/python-intercom/python-intercom-3.0.5.tar.gz/python-intercom-3.0.5/intercom/conversation.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Collection module for Conversations."""
+from intercom.traits.api_resource import Resource
+
+
+class Conversation(Resource):
+    """Collection class for Converations."""
---
+++ packages/python-intercom/python-intercom-3.0.5.tar.gz/python-intercom-3.0.5/intercom/event.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from intercom.traits.api_resource import Resource
+
+
+class Event(Resource):
+    pass
