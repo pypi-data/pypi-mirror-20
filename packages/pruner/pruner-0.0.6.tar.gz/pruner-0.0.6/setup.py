678f95267203736efa6223a2fedc078f4c780a97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:47 +0000

    Add package pruner/0.0.6/pruner-0.0.6.tar.gz

---
+++ packages/pruner/pruner-0.0.6.tar.gz/pruner-0.0.6/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(author='Matthew Egan',
+      author_email='matthewj.egan@hotmai.com',
+      description='A CLI tool for pruning your overgrown requirements file',
+      name='pruner',
+      py_modules=[
+          'pruner.pruner',
+      ],
+      entry_points={
+            'console_scripts': [
+                  'pruner = pruner.pruner:main'
+            ]
+      },
+      install_requires=[
+            'crayons==0.1.2',
+      ],
+      url='https://github.com/mattjegan/pruner',
+      version='0.0.6'
+)
