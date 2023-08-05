ed6f5677fcef028fb94cfa94b390617b9a1fc4a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:31 +0000

    Add package tap-freshdesk/0.3.0/tap-freshdesk-0.3.0.tar.gz

---
+++ packages/tap-freshdesk/tap-freshdesk-0.3.0.tar.gz/tap-freshdesk-0.3.0/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='tap-freshdesk',
+      version='0.3.0',
+      description='Singer.io tap for extracting data from the Freshdesk API',
+      author='Stitch',
+      url='https://github.com/singer-io/tap-freshdesk',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['tap_freshdesk'],
+      install_requires=[
+          'singer-python>=0.1.0',
+          'requests==2.12.4',
+      ],
+      entry_points='''
+          [console_scripts]
+          tap-freshdesk=tap_freshdesk:main
+      ''',
+      packages=['tap_freshdesk'],
+      package_data = {
+          'tap_freshdesk/schemas': [
+              'agents.json',
+              'companies.json',
+              'contacts.json',
+              'conversations.json',
+              'groups.json',
+              'roles.json',
+              'satisfaction_ratings.json',
+              'tickets.json',
+              'time_entries.json',
+          ],
+      },
+      include_package_data=True,
+)
