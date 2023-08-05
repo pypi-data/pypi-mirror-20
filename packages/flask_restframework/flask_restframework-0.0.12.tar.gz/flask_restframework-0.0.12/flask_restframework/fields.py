d53f81c86453b1de5ba10c26f15f6fba731b04a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:12 +0000

    Add package flask_restframework/0.0.12/flask_restframework-0.0.12.tar.gz

---
+++ packages/flask_restframework/flask_restframework-0.0.12.tar.gz/flask_restframework-0.0.12/flask_restframework/fields.py
@@ -0,0 +1,328 @@
+import datetime
+
+from flask import json
+
+from flask_restframework.validators import BaseValidator
+from flask_restframework.exceptions import ValidationError
+from mongoengine import fields as db
+
+__author__ = 'stas'
+
+
+class BaseField(object):
+    """
+    Base Field
+    """
+
+    serializer = None   #ref on serializer instance
+
+    @classmethod
+    def from_mongoengine_field(cls, mongoEngineField):
+        validators = None
+        if mongoEngineField.validation:
+
+            if hasattr(mongoEngineField.validation, "original_validator"):
+                validator_adapter = mongoEngineField.validation.original_validator
+            else:
+                validator_adapter = lambda serializer, value: mongoEngineField.validation(value)
+
+            validators = [validator_adapter]
+
+        return cls(
+            required=mongoEngineField.required,
+            default=mongoEngineField.default,
+            validators=validators
+        )
+
+    def __init__(
+            self,
+            required=False,
+            blank=True,
+            default=None,
+            validators=None,
+            read_only=False
+    ):
+        """
+        :param required: if True then field should strictly present in data (but may be null)
+        :param blank: if False then field should not be null or empty ("" for strings and so on)
+        :param default: Default value for this field.
+        """
+
+        self._required = required
+        self._blank = blank
+        self._validators = validators or []
+        self._default = default
+        self._read_only = read_only
+
+    def run_validate(self, validator, value):
+        """
+        :type validator: flask_restframework.serializer.BaseSerializer
+
+        Should return value which will be passed in BaseValidator.cleaned_data
+        """
+
+        if self._read_only:
+            raise ValueError("You can't run validation on read only field!")
+
+        if value is None and self._default is not None:
+            value = self._default
+
+            # Allow to pass lambdas as default (as is in mongoengine)
+            if callable(value):
+                value = value()
+
+        if value is None:
+            if self._required:
+                raise ValidationError("Field is required")
+            else:
+                return value
+
+        for customVal in self._validators:
+            customVal(validator, value)
+
+        if value:
+            value = self.validate(validator, value)
+
+        return value
+
+    def to_python(self, value):
+        """For passed from Model instance value this method should return plain python object
+        which will be used
+        later in serialization
+
+        :param value: value from db object
+        """
+        raise NotImplementedError()
+
+    # TODO: validate MUST be implemented!
+    def validate(self, validator, value):
+        pass
+
+    def get_value_from_model_object(self, doc, field):
+        "returns value for fieldName field and document doc"
+        try:
+            return getattr(doc, field)
+        except:
+            print("ERR")
+
+
+class StringField(BaseField):
+
+    def to_python(self, value):
+        if value:
+            return str(value)
+
+    def __init__(self, choices=None, **k):
+        self.choices = choices
+        super(StringField, self).__init__(**k)
+
+    def validate(self, validator, value):
+        if self.choices and value not in self.choices:
+            raise ValidationError("Value should be one of {}, got {}".format(
+                self.choices, value)
+            )
+
+        return value
+
+
+class BooleanField(BaseField):
+
+    def to_python(self, value):
+        return value
+
+    def validate(self, validator, value):
+        if value not in [True, False]:
+            raise ValidationError("Boolean is required")
+
+        return value
+
+class IntegerField(BaseField):
+
+    def to_python(self, value):
+        return value
+
+    def validate(self, validator, value):
+        try:
+            return int(value)
+        except:
+            raise ValidationError("Integer is required")
+
+# TODO: validate URL
+class URLField(BaseField):
+
+    def to_python(self, value):
+        return value
+
+    def validate(self, validator, value):
+        return value
+
+class DateTimeField(BaseField):
+
+    def to_python(self, value):
+        if value:
+            return value.strftime(self._format)
+
+    def __init__(self, format="%Y-%m-%d %H:%M:%S", **k):
+        self._format = format
+        super(DateTimeField, self).__init__(**k)
+
+    def validate(self, validator, value):
+        try:
+            return datetime.datetime.strptime(value, self._format)
+        except:
+            raise ValidationError("Incorrect DateTime string for {} format".format(self._format))
+
+
+class MongoEngineIdField(BaseField):
+
+    def to_python(self, value):
+        return str(value.id)
+
+    def __init__(self, documentCls, **k):
+        self._documentCls = documentCls
+        super(MongoEngineIdField, self).__init__(**k)
+
+    def validate(self, validator, value):
+        ids = {
+            str(item.id): item
+            for item in self._documentCls.objects.all()
+        }
+
+        if value not in ids:
+            raise ValidationError("Incorrect id: {}".format(value))
+
+        return ids[value]
+
+
+class MethodField(BaseField):
+
+    def __init__(self, methodName):
+        super(MethodField, self).__init__(read_only=True)
+        self.methodName = methodName
+
+    def get_value_from_model_object(self, doc, field):
+        return getattr(self.serializer, self.methodName)(doc)
+
+    def to_python(self, value):
+        return value
+
+
+class BaseRelatedField(BaseField):
+
+    def __init__(self, document_fieldname=None, **k):
+        super(BaseRelatedField, self).__init__(**k)
+        self.document_fieldname = document_fieldname
+
+    def get_value_from_model_object(self, doc, field):
+        field = self.document_fieldname or field
+        parts = field.split("__")
+        out = doc
+        for item in parts:
+            outCls = out.__class__
+
+            if out:
+                out = getattr(out, item)
+
+                try:
+                    outField = getattr(outCls, item)
+                except AttributeError:
+                    outField = None
+
+        if out and outField:
+            from flask_restframework.utils import mongoengine_model_meta
+            return mongoengine_model_meta.FIELD_MAPPING[outField.__class__].from_mongoengine_field(
+                outField).to_python(out)
+
+        return out
+
+
+class ForeignKeyField(BaseRelatedField):
+    """
+    Fields represent ForeignKeyRelation which can be getted with __ notation.
+    It is only READ field, but it subclasses may be also changeable.
+
+    Goal of this field - to allow get inner/related data with __ notation.
+    """
+    def __init__(self, document_fieldname, **k):
+        k["read_only"] = True
+        k["document_fieldname"] = document_fieldname
+        super(ForeignKeyField, self).__init__(**k)
+
+    def to_python(self, value):
+        if isinstance(value, db.Document):
+            return str(value.id)
+        return value
+
+class PrimaryKeyRelatedField(BaseRelatedField):
+
+    @classmethod
+    def from_mongoengine_field(cls, mongoEngineField):
+        return cls(
+            related_model=mongoEngineField.document_type,
+            required=mongoEngineField.required,
+            default=mongoEngineField.default
+        )
+
+    def __init__(self, related_model, **k):
+        super(PrimaryKeyRelatedField, self).__init__(**k)
+        self.related_model = related_model
+
+    def to_python(self, value):
+        if isinstance(value, db.Document):
+            return str(value.id)
+        return value
+
+    def validate(self, validator, value):
+        instance = self.related_model.objects.filter(id=value).first()
+
+        if not instance:
+            raise ValidationError("Object with id {} not found".format(value))
+
+        return instance
+
+
+class ListField(BaseField):
+
+    def to_python(self, value):
+        embedded = EmbeddedField()
+        return list(map(embedded.to_python, value))
+
+    def validate(self, validator, value):
+        if not isinstance(value, list):
+            raise ValidationError("Array is required")
+
+        return value
+
+
+class EmbeddedField(BaseRelatedField):
+
+    def __init__(self, read_only=False, **k):
+        super(EmbeddedField, self).__init__(**k)
+
+        self._read_only = read_only
+
+    def to_python(self, value):
+        # TODO: Use nested serializer
+
+        if value and isinstance(value, db.EmbeddedDocument):
+            return json.loads(value.to_json())
+
+        return value
+
+    def validate(self, validator, value):
+        if not isinstance(value, dict):
+            raise ValidationError("Object is required")
+
+        return value
+
+
+class DictField(BaseField):
+
+    def to_python(self, value):
+        if value:
+            try:
+                return dict(value)
+            except:
+                return value
+
+        return value
---
+++ packages/flask_restframework/flask_restframework-0.0.12.tar.gz/flask_restframework-0.0.12/flask_restframework/tests/test_flask_app.py
@@ -0,0 +1,485 @@
+import datetime
+import json
+import unittest
+from pprint import pprint
+
+import six
+from flask import jsonify
+from flask.app import Flask
+from flask.blueprints import Blueprint
+from flask_mongoengine import MongoEngine
+from flask.globals import request
+from flask.helpers import url_for
+from flask.views import View
+from mongoengine import fields as db
+
+from flask_restframework.validators import RegexpValidator
+from flask_restframework.decorators import list_route, detail_route
+from flask_restframework import fields
+from flask_restframework.adapters import MongoEngineQuerysetAdapter
+from flask_restframework.decorators import validate
+from flask_restframework.model_resource import ModelResource
+from flask_restframework.pagination import DefaultPagination
+from flask_restframework.resource import BaseResource, BaseResourceMetaClass
+from flask_restframework.router import DefaultRouter
+from flask_restframework.serializer import BaseSerializer
+from flask_restframework.serializer.model_serializer import ModelSerializer
+
+
+class SimpleFlaskAppTest(unittest.TestCase):
+
+    def setUp(self):
+        self.app = Flask(__name__)
+
+        self.client = self.app.test_client()
+
+        self.db = MongoEngine(self.app)
+
+        class TestCol(db.Document):
+            value = db.StringField()
+
+            def __unicode__(self):
+                return "TestCol(value={})".format(self.value)
+
+        TestCol.objects.delete()
+
+        TestCol.objects.create(value="1")
+        TestCol.objects.create(value="2")
+
+        self.TestCol = TestCol
+
+    def _parse(self, resp):
+        resp = resp.decode("utf-8")
+        return json.loads(resp)
+
+    def test_validation_mongoengine_will_work_with_model_serializer(self):
+
+        class Doc(db.Document):
+            value = db.StringField(validation=RegexpValidator(r"\d+", message="Bad value").for_mongoengine())
+
+        Doc.drop_collection()
+
+        class Serializer(ModelSerializer):
+            class Meta:
+                model = Doc
+
+        Doc.objects.create(value="123")
+
+        s = Serializer(data={"value": "asd"})
+        self.assertEqual(s.validate(), False)
+        self.assertEqual(s.errors, {"value": ["Bad value"]})
+
+    def test_resource_decorator(self):
+
+        class S(BaseSerializer):
+
+            field = fields.StringField(required=True)
+
+        @self.app.route("/test", methods=["POST"])
+        @validate(S)
+        def resource(cleaned_data):
+            return "OK"
+
+        resp = self.client.post("/test", data=json.dumps({}), headers={"Content-Type": "application/json"})
+        self.assertEqual(resp.status_code, 400)
+        self.assertEqual(
+            json.loads(resp.data.decode("utf-8")), {'field': ['Field is required']}
+        )
+
+    def testSimpleResourceAndRouter(self):
+
+        router = DefaultRouter(self.app)
+
+        class Resource(BaseResource):
+
+            def get(self, request):
+                return "GET"
+
+            def post(self, request):
+                return "POST"
+
+            def put(self, request):
+                return "PUT"
+
+            def patch(self, request):
+                return "PATCH"
+
+            def delete(self, request):
+                return "DELETE"
+
+            @list_route(methods=["GET", "POST"])
+            def listroute(self, request):
+                return "LIST"
+
+            @detail_route(methods=["GET", "POST"])
+            def detailroute(self, request, pk):
+                return "detail"
+
+        self.assertSetEqual(
+            set(Resource.get_allowed_methods()), {"get", "post", "put", "patch", "delete"}
+        )
+
+        router.register("/test", Resource, "test")
+
+        for method in ["get", "post", "put", "patch", "delete"]:
+            resp = getattr(self.client, method)("/test")
+            self.assertEqual(resp.data.decode("utf-8"), method.upper())
+
+        for method in ["GET", "POST"]:
+            resp = getattr(self.client, method.lower())("/test/listroute")
+            self.assertEqual(resp.status_code, 200)
+            self.assertEqual(resp.data.decode("utf-8"), "LIST")
+
+        for method in ["GET", "POST"]:
+            resp = getattr(self.client, method.lower())("/test/detailroute/1")
+            self.assertEqual(resp.status_code, 200)
+            self.assertEqual(resp.data.decode("utf-8"), "detail")
+
+            resp = self.client.get("/test/detailroute")
+            self.assertEqual(resp.status_code, 404)
+
+
+
+    def testRoutingWithBluePrint(self):
+
+        bp = Blueprint("test", __name__)
+        router = DefaultRouter(bp)
+
+        class Res(BaseResource):
+            def get(self, request):
+                return "GET"
+
+        router.register("/blabla", Res, "blabla")
+
+        self.app.register_blueprint(bp, url_prefix="/test")
+
+        with self.app.test_request_context():
+            self.assertEqual(url_for("test.blabla"), "/test/blabla")
+
+    def testModelResource(self):
+
+        router = DefaultRouter(self.app)
+
+        class Base(db.Document):
+
+            title = db.StringField()
+
+        class ED(db.EmbeddedDocument):
+
+            value = db.StringField()
+
+        class Model(db.Document):
+
+            base = db.ReferenceField(Base)
+            f1 = db.StringField()
+            f2 = db.BooleanField()
+            f3 = db.StringField()
+
+            embedded = db.EmbeddedDocumentField(ED)
+            listf = db.EmbeddedDocumentListField(ED)
+
+            dictf = db.DictField()
+
+        Model.objects.delete()
+
+        ins = Model.objects.create(
+            base=Base.objects.create(title="1"),
+            f1="1",
+            f2=True,
+            f3="1",
+            embedded={"value": "123"},
+            listf=[{"value": "234"}],
+            dictf={"key": "value"}
+        )
+
+        Model.objects.create(
+            base=Base.objects.create(title="2"),
+            f1="2",
+            f2=True,
+            f3="2",
+            embedded={"value": "123"},
+            listf=[{"value": "234"}]
+        )
+
+        class S(ModelSerializer):
+            title = fields.ForeignKeyField("base__title")
+            class Meta:
+                model = Model
+                fk_fields = ("base__title", )
+
+        class ModelRes(ModelResource):
+
+            serializer_class = S
+            queryset = Model.objects.all()
+            pagination_class = DefaultPagination
+
+        router.register("/test", ModelRes, "modelres")
+        resp = self.client.get("/test")
+        self.assertEqual(resp.status_code, 200)
+        data = self._parse(resp.data)
+        print(data)
+        self.assertEqual(len(data["results"]), 2)
+        item = data["results"][0]
+        self.assertEqual(item["dictf"], {"key": "value"})
+        self.assertEqual(item["title"], "1")
+        self.assertEqual(item["base__title"], "1")
+
+        # get one object
+        resp = self.client.get("/test/{}".format(ins.id))
+        self.assertEqual(resp.status_code, 200)
+        pprint(self._parse(resp.data))
+
+        #test pagination
+        for i in range(10):
+            Model.objects.create(
+                base=Base.objects.create(title="1"),
+                f1="1",
+                f2=True,
+                f3="2"
+            )
+
+        self.assertEqual(Model.objects.count(), 12)
+
+        resp = self.client.get("/test?page=1")
+        self.assertEqual(resp.status_code, 200)
+        data = self._parse(resp.data)
+        results = data["results"]
+
+        self.assertEqual(results[0]["embedded"], {"value": "123"})
+        self.assertEqual(results[0]["listf"], [{"value": "234"}])
+
+        self.assertEqual(len(data["results"]), 10)
+
+        resp = self.client.get("/test?page=2")
+        self.assertEqual(resp.status_code, 200)
+        data = self._parse(resp.data)
+        self.assertEqual(len(data["results"]), 2)
+
+        resp = self.client.get("/test?page=2&page_size=5")
+        self.assertEqual(resp.status_code, 200)
+        data = self._parse(resp.data)
+        self.assertEqual(len(data["results"]), 5)
+
+        resp = self.client.get("/test?page=3&page_size=5")
+        self.assertEqual(resp.status_code, 200)
+        data = self._parse(resp.data)
+        self.assertEqual(len(data["results"]), 2)
+
+
+        #test put
+        resp = self.client.put("/test/{}".format(ins.id), data=json.dumps({
+            "f3": "OLALA"
+        }), headers={"Content-Type": "application/json"})
+        self.assertEqual(resp.status_code, 200, resp.data)
+        data = self._parse(resp.data)
+        self.assertEqual(data["f1"], "1")
+        self.assertEqual(data["f2"], True)
+        self.assertEqual(data["f3"], "OLALA")
+
+
+    def testMongoEngineForeignKeyField(self):
+
+        self.assertEqual(self.TestCol.objects.count(), 2)
+
+        class Serializer(BaseSerializer):
+            fk = fields.MongoEngineIdField(self.TestCol, required=True)
+
+        v = Serializer({"fk": "123"})
+        self.assertEqual(v.validate(), False)
+        self.assertEqual(v.errors, {'fk': ['Incorrect id: 123']})
+
+        v = Serializer({"fk": str(self.TestCol.objects.first().id)})
+        v.validate()
+        self.assertEqual(v.errors, {})
+        self.assertEqual(v.cleaned_data["fk"], self.TestCol.objects.first())
+
+        class R(BaseResource):
+
+            def post(self, request):
+                errors, data = self.validate_request(Serializer)
+                if errors:
+                    return errors
+
+                return "OK"
+
+        self.app.add_url_rule("/api", view_func=R.as_view("test2"),
+                              methods=["GET", "POST"])
+
+        resp = self.client.post("/api", data=json.dumps({}),
+                                headers={"Content-Type": "application/json"})
+        self.assertEqual(resp.status_code, 400)
+        data = json.loads(resp.data.decode("utf-8"))
+        self.assertEqual(data["fk"], ['Field is required'])
+
+    def testSerialization(self):
+
+        class Col(db.Document):
+
+            value = db.StringField()
+            created = db.DateTimeField(default=datetime.datetime.now)
+
+        Col.objects.delete()
+        Col.objects.create(value="1")
+        Col.objects.create(value="2")
+
+        class S(BaseSerializer):
+
+            value = fields.StringField()
+            created = fields.DateTimeField(read_only=True)
+
+        data = S(Col.objects.all()).to_python()
+        self.assertEqual(len(data), 2)
+        self.assertEqual(
+            list(map(lambda i: i["value"], data)),
+            ["1", "2"]
+        )
+
+        #test can't set read only field
+        ser = S({"value": "1", "created": "2016-01-01 00:00:00"})
+        ser.validate()
+
+        self.assertTrue("created" not in ser.cleaned_data)
+
+    def testModelSerialization(self):
+
+        class DeepInner(db.EmbeddedDocument):
+            value = db.StringField()
+
+        class Inner(db.EmbeddedDocument):
+            value = db.StringField()
+            deep = db.EmbeddedDocumentField(DeepInner)
+
+        class Col(db.Document):
+
+            value = db.StringField()
+            excluded_field = db.StringField(default="excluded")
+            created = db.DateTimeField(default=datetime.datetime.now)
+            inner = db.EmbeddedDocumentField(Inner)
+
+
+        Col.objects.delete()
+        Col.objects.create(value="1", inner={"value": "inner1", "deep": {"value": "123"}})
+        Col.objects.create(value="2", inner={"value": "inner2"})
+
+        class Serializer(ModelSerializer):
+
+            method_field = fields.MethodField("test")
+
+            renamed = fields.ForeignKeyField(document_fieldname="inner__deep__value")
+
+            def test(self, doc):
+                return doc.value
+
+            class Meta:
+                model = Col
+                fields = ("value", "created", "method_field")
+                fk_fields = ("inner__value", "inner__deep__value")
+
+        data = Serializer(Col.objects.all()).to_python()
+
+        for item in data:
+            self.assertTrue("value" in item)
+            self.assertEqual(item["value"], item["method_field"])
+            self.assertTrue(type(item["created"]), datetime.datetime)
+            self.assertEqual(item["renamed"], item["inner__deep__value"])
+
+
+class TestModelResources(SimpleFlaskAppTest):
+
+    def setUp(self):
+        super(TestModelResources, self).setUp()
+
+        class EmbeddedDoc(db.EmbeddedDocument):
+            value = db.StringField()
+
+        class BaseDoc(db.Document):
+            inner = db.EmbeddedDocumentField(EmbeddedDoc)
+            inner_list = db.EmbeddedDocumentListField(EmbeddedDoc)
+
+            req_field = db.StringField(required=True)
+            string = db.StringField()
+            bool = db.BooleanField()
+            integer_field = db.IntField()
+
+        class Serializer(ModelSerializer):
+            class Meta:
+                model = BaseDoc
+
+        class R(ModelResource):
+            serializer_class = Serializer
+
+            def get_queryset(self):
+                return BaseDoc.objects.all()
+
+        BaseDoc.drop_collection()
+
+        router = DefaultRouter(self.app)
+        router.register("/test_resource", R, "test_resource")
+
+        self.BaseDoc = BaseDoc
+
+    def test_patch_several_fields(self):
+
+        id = self.test_creation()
+
+        resp = self.client.patch("/test_resource/{}".format(id), data=json.dumps({
+            "string": "other string",
+            "bool": True
+        }), content_type="application/json")
+
+        self.assertEqual(resp.status_code, 200, resp.data)
+        data = self._parse(resp.data)
+        self.assertEqual(data["string"], "other string")
+        self.assertEqual(data["bool"], True)
+        self.assertEqual(data["inner"], {
+                "value": "1"
+        })
+        self.assertEqual(data["req_field"], "required")
+
+        resp = self.client.patch("/test_resource/{}".format(id), data=json.dumps({
+            "string": "other string",
+            "bool": "Very Bad!",
+            "integer_field": "Not an int!"
+        }), content_type="application/json")
+
+        self.assertEqual(resp.status_code, 400)
+        data = self._parse(resp.data)
+        self.assertEqual(
+            data["bool"], ["Boolean is required"]
+        )
+        self.assertEqual(data["integer_field"], ["Integer is required"])
+
+    def test_creation(self):
+
+        data = {
+            "inner": {
+                "value": "1"
+            },
+            "inner_list": [{
+                "value": "2"
+            }, {
+                "value": "3"
+            }],
+            "string": "string",
+            "bool": False,
+            "req_field": "required"
+        }
+
+        resp = self.client.post("/test_resource", data=json.dumps(data), content_type="application/json")
+        self.assertEqual(resp.status_code, 200)
+        ins = self.BaseDoc.objects.first()
+
+        self.assertEqual(ins.bool, False)
+        self.assertEqual(ins.string, "string")
+        self.assertEqual(ins.inner.value, "1")
+        self.assertEqual(ins.inner_list[0].value, "2")
+        self.assertEqual(ins.inner_list[1].value, "3")
+
+        return ins.id
+
+
+
+
+
+
+
