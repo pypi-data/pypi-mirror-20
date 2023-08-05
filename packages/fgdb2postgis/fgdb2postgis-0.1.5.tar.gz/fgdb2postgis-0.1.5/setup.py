cfde036bf5f7226a02f6e6056b9c37006d2650fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:11 +0000

    Add package fgdb2postgis/0.1.5/fgdb2postgis-0.1.5.tar.gz

---
+++ packages/fgdb2postgis/fgdb2postgis-0.1.5.tar.gz/fgdb2postgis-0.1.5/setup.py
@@ -0,0 +1,39 @@
+
+from pip.req import parse_requirements
+from setuptools import setup
+
+requirements = parse_requirements('./requirements.txt', session=False)
+
+setup(
+    name='fgdb2postgis',
+    version='0.1.5',
+    description="""File geodatabase to postgis convertor""",
+    long_description=open('README.rst').read(),
+    author='George Ioannou',
+    author_email='gmioannou@cartologic.com',
+    url='https://github.com/cartologic/fgdb2postgis',
+    packages=['fgdb2postgis'],
+    package_data={'fgdb2postgis': ['sql_files/*.sql']},
+    include_package_data=True,
+    install_requires=[
+        'numpy>=1.12.0',
+        'psycopg2>=2.6.2',
+        'pyyaml>=3.12',
+        'archook==1.0.0',
+    ],
+    dependency_links=[
+        'https://github.com/gmioannou/archook/tarball/master#egg=archook-1.0.0'
+    ],
+    license="MIT",
+    keywords='fgdb2postgis',
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        'Operating System :: Microsoft :: Windows'
+    ],
+    entry_points={
+        'console_scripts': ['fgdb2postgis = fgdb2postgis.__main__:main']
+    },
+)
