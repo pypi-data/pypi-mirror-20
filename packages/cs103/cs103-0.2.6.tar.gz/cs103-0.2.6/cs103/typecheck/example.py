cd62f1580f683669fe86bdc2ae8d569210f88dde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:27 +0000

    Add package cs103/0.2.6/cs103-0.2.6.tar.gz

---
+++ packages/cs103/cs103-0.2.6.tar.gz/cs103-0.2.6/cs103/typecheck/example.py
@@ -0,0 +1,18 @@
+from typecheck import *
+from typing import *
+
+Person = NamedTuple('Person', [('name', str), ('age', int)])
+
+@typecheck
+def majority_age(p: Person) -> bool:
+    return p.name  # incorrect: typechecker should error out
+
+try:
+    majority_age(123)  # incorrect arg type: should error
+except Exception as e:
+    print(e)
+
+try:
+    majority_age(Person('Gerald', 23))  # should give ReturnError above
+except Exception as e:
+    print(e)
