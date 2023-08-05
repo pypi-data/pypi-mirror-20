6216d8f5b678425fa741c713cea68324c16a9a10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:00 +0000

    Add package tellduslive/0.3.4/tellduslive-0.3.4.tar.gz

---
+++ packages/tellduslive/tellduslive-0.3.4.tar.gz/tellduslive-0.3.4/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+setup(name="tellduslive",
+      version='0.3.4',
+      description="Communicate with Telldus Live",
+      url="https://github.com/molobrakos/tellduslive",
+      license="",
+      author="Erik",
+      author_email="Erik",
+      install_requires=['requests',
+                        'requests_oauthlib'],
+      py_modules=["tellduslive"],
+      provides=["tellduslive"],)
