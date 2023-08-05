b0af0b7219a53a7e28427b3ddbd68d52eba63143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:07 +0000

    Add package pytranslator/1.3.5/pytranslator-1.3.5.tar.gz

---
+++ packages/pytranslator/pytranslator-1.3.5.tar.gz/pytranslator-1.3.5/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# encoding:utf-8
+
+from distutils.core import setup
+
+setup(
+        name='pytranslator',
+        version='1.3.5',
+        description='基于有道云翻译API的python英汉互译模块',
+        author='王一航',
+        author_email='wangyihanger@gmail.com',
+        url='https://git.coding.net/yihangwang/PyTranslator/git',
+        packages=['pytranslator'],
+    )
