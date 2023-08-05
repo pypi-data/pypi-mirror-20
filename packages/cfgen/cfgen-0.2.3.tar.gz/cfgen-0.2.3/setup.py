1f878486d929e40b8ac850590b2ceb756afa42e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:27 +0000

    Add package cfgen/0.2.3/cfgen-0.2.3.tar.gz

---
+++ packages/cfgen/cfgen-0.2.3.tar.gz/cfgen-0.2.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name="cfgen",
+    version="0.2.3",
+    description="Configuration tool for configuration files",
+    author="Renat Zhilkibaev",
+    author_email="rzhilkibaev@gmail.com",
+    license="MIT",
+    url="https://github.com/rzhilkibaev/cfgen",
+    packages=["cfgen"],
+    install_requires=["docopt", "future", "jinja2"],
+    scripts=["bin/cfgen"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Utilities",
+        ],
+)
