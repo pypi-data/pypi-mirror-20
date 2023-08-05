d1e71b7f2346a6400300f7ef8ba146148fe1b56b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:57 +0000

    Add package stack-composed/17.2.21/stack-composed-17.2.21.tar.gz

---
+++ packages/stack-composed/stack-composed-17.2.21.tar.gz/stack-composed-17.2.21/stack_composed/__init__.py
@@ -0,0 +1,29 @@
+
+
+__version__ = '17.2.21'  # year.month.day
+
+
+header = \
+'''====================================================================
+
+StackComposed
+
+  Compute and generate the composed of a raster images stack
+
+Run with '-h' for more options or visit the documentation:
+https://smbyc.bitbucket.io/md/stackcomposed
+
+Copyright (C) 2017 Xavier Corredor Llano
+Sistema de Monitoreo de Bosques y Carbono - SMBYC and FAO
+General Public License - GPLv3
+
+===================================================================='''
+
+epilog = \
+'''
+For more information visit:
+https://smbyc.bitbucket.io/md/stackcomposed
+
+Copyright (C) Xavier Corredor Llano <xcorredorl@ideam.gov.co>
+Sistema de Monitoreo de Bosques y Carbono - SMBYC and FAO
+General Public License - GPLv3'''
