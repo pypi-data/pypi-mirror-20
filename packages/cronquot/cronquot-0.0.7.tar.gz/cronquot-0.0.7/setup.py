5d63cd110f54e9b24a5d8eb8433d1585547a1fb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:21 +0000

    Add package cronquot/0.0.7/cronquot-0.0.7.tar.gz

---
+++ packages/cronquot/cronquot-0.0.7.tar.gz/cronquot-0.0.7/setup.py
@@ -0,0 +1,34 @@
+import codecs
+from setuptools import setup
+import pypandoc
+
+long_desc = ''
+with codecs.open('README.md', 'r', 'utf-8') as f:
+    logn_desc_md = f.read()
+    with codecs.open('README.rst', 'w', 'utf-8') as rf:
+        long_desc = pypandoc.convert('README.md', 'rst')
+        rf.write(long_desc)
+
+setup(name='cronquot',
+      version='0.0.7',
+      description='Cron scheduler.',
+      long_description=long_desc,
+      classifiers=[
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'License :: OSI Approved :: MIT License',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+          ],
+      keywords='cron crontab schedule',
+      author='Shohei Mukai',
+      author_email='mukaishohei76@gmail.com',
+      url='https://github.com/pyohei/cronquot',
+      license='MIT',
+      packages=['cronquot'],
+      entry_points={
+          'console_scripts': [
+              'cronquot = cronquot.cronquot:execute_from_console'],
+          },
+      install_requires=['crontab'],
+      test_suite='test' 
+      )
