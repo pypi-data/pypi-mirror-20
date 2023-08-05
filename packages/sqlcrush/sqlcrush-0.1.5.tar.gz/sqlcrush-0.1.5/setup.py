5f4aed9c76df3774e77346bafb687e33efb6f983
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:29 +0000

    Add package sqlcrush/0.1.5/sqlcrush-0.1.5.tar.gz

---
+++ packages/sqlcrush/sqlcrush-0.1.5.tar.gz/sqlcrush-0.1.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='sqlcrush',
+        version='0.1.5',
+        description='console based database editor',
+        url='http://github.com/coffeeandscripts/sqlcrush',
+        author='coffeeandscripts',
+        author_email='ersari94@gmail.com',
+        license='GNU',
+        scripts=['bin/sqlcrush',],
+        packages=['sqlcrush',],
+        install_requires=['sqlalchemy', 'pymysql', 'psycopg2',],
+        include_package_data=True
+)
