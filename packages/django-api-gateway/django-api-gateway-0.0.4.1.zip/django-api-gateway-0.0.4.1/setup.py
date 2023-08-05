f645a641ba68c61a5a76ddc4a7c257976584314d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:23 +0000

    Add package django-api-gateway/0.0.4.1/django-api-gateway-0.0.4.1.zip

---
+++ packages/django-api-gateway/django-api-gateway-0.0.4.1.zip/django-api-gateway-0.0.4.1/setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name = 'django-api-gateway',
+    version = '0.0.4.1',
+    keywords = ('django', 'apigateway', 'dataproxy'),
+    description = 'apigateway for django',
+    license = 'MIT License',
+
+    url = 'https://github.com/sarar04/djangoapigateway',
+    author = 'sarar04',
+    author_email = 'sarar04@163.com',
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'unix like',
+    install_requires = []
+)
