71053ed0a74502c041d6c51c7e5bcb0995a3675b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:12 +0000

    Add package PyPDT/0.6.0/PyPDT-0.6.0.tar.gz

---
+++ packages/PyPDT/PyPDT-0.6.0.tar.gz/PyPDT-0.6.0/pypdt/__main__.py
@@ -0,0 +1,12 @@
+# -*- python -*-
+
+import optparse
+op = optparse.OptionParser()
+op.add_option("-f", "--dbfile", action="store", dest="FILE", metavar="FILE",
+              help="specify the db file to read", default=None)
+opts, args = op.parse_args()
+
+import pypdt
+t = pypdt.ParticleDataTable(opts.FILE if opts.FILE else None)
+for pid in args:
+    print t[pid]
