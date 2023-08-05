2f31e01d835ba68d9e1f03f2f30048d8076d8154
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:57 +0000

    Add package yeelightsunflower/0.0.2/yeelightsunflower-0.0.2.tar.gz

---
+++ packages/yeelightsunflower/yeelightsunflower-0.0.2.tar.gz/yeelightsunflower-0.0.2/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+from yeelightsunflower import __version__
+
+# TODO check/add Python 2 support
+
+classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    'License :: OSI Approved :: MIT License',
+    "Programming Language :: Python :: 3.2",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6"
+]
+
+setup(
+    name='yeelightsunflower',
+    version=__version__,
+    packages=['yeelightsunflower'],
+    classifiers=classifiers,
+    url='https://github.com/lindsaymarkward/python-yeelight-sunflower',
+    license='MIT',
+    author='Lindsay Ward',
+    author_email='lindsay.ward@jcu.edu.au',
+    description='Python package for interacting with Yeelight Sunflower bulbs'
+)
