fd7cc360998bab4d3aa83b4bf0fda56d1ce4a263
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:22 +0000

    Add package MnemoPwd/1.0.0/MnemoPwd-1.0.0-py3-none-any.whl

---
+++ packages/MnemoPwd/MnemoPwd-1.0.0-py3-none-any.whl/mnemopwd/client/uilayer/uicomponents/TitledBorderWindow.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2016, Thierry Lemeunier <thierry at lemeunier dot net>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this 
+# list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import curses
+
+from .BaseWindow import BaseWindow
+
+
+class TitledBorderWindow(BaseWindow):
+    """
+    A window with a border and a title. Subclass of BaseWindow.
+    """
+    
+    def __init__(self, parent, h, w, y, x, title, modal=False):
+        """Create base window"""
+        BaseWindow.__init__(self, parent, h, w, y, x, modal=modal)
+        self.title = title
+        self._create()
+        
+    def redraw(self):
+        """See mother class"""
+        self._create()
+        BaseWindow.redraw(self)
+        
+    def _create(self):
+        self.window.border()
+        self.window.addstr(1, 2, self.title)
+        self.window.hline(2, 1, curses.ACS_HLINE, self.w - 2)
+        self.window.refresh()
