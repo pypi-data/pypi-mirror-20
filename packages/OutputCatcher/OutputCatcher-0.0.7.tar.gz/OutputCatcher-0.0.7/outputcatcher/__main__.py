5caefd12528eb337dd6471584b81f9df3ec6c226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:20 +0000

    Add package OutputCatcher/0.0.7/OutputCatcher-0.0.7.tar.gz

---
+++ packages/OutputCatcher/OutputCatcher-0.0.7.tar.gz/OutputCatcher-0.0.7/outputcatcher/__main__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from __future__ import print_function
+import sys
+from .catchers import __version__, StdErrCatcher, StdOutCatcher
+
+if __name__ == '__main__':
+    print('Test run for OutputCatcher v. {}\n'.format(__version__))
+    with StdOutCatcher(safe=False, maxlength=0) as fakeout:
+        print('This is a test. you shouldn\'t see it right away.')
+    print('Captured stdout: {}'.format(fakeout.output))
+
+    with StdErrCatcher(safe=False, maxlength=0) as fakeerr:
+        print('Testing stderr output.', file=sys.stderr)
+    print('Captured stderr: {}'.format(fakeerr.output))
+    sys.exit(0)
