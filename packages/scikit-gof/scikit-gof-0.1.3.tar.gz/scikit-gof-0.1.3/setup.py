34544b055081e6577aeb73bf63e669216dc9754d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:28 +0000

    Add package scikit-gof/0.1.3/scikit-gof-0.1.3.tar.gz

---
+++ packages/scikit-gof/scikit-gof-0.1.3.tar.gz/scikit-gof-0.1.3/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+meta = {
+    'name': 'scikit-gof',
+    'version': '0.1.3',
+    'packages': ('skgof',),
+    'install_requires': (
+        'numpy>=1.10',
+        'scipy>=0.16'
+    ),
+    'tests_require': (
+        'flake8-print',
+        'flake8-todo',
+        'pep8-naming',
+        'pytest',
+        'pytest-benchmark',
+        'pytest-flake8',
+        'pytest-isort',
+        'pytest-readme'
+    ),
+    'description': "Variations on goodness of fit tests for SciPy.",
+    'long_description': open('README.rst').read(),
+    'author': "Wojciech Ruszczewski",
+    'author_email': "scipy@wr.waw.pl",
+    'url': "http://github.org/wrwrwr/scikit-gof",
+    'license': "MIT",
+    'classifiers': (
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5"
+    )
+}
+
+if __name__ == '__main__':
+    setup(**meta)
