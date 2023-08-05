806869ea5b7b001ff875f9875eb885362831be92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:17 +0000

    Add package kerio-api/0.1/kerio-api-0.1.tar.gz

---
+++ packages/kerio-api/kerio-api-0.1.tar.gz/kerio-api-0.1/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+class MambaTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import sys
+        import mamba.cli
+        sys.argv = ['mamba', '--format', 'documentation']
+        mamba.cli.main()
+
+setup(
+        name = 'kerio-api',
+        version = '0.1',
+        packages = ['kerio'],
+        install_requires = [
+        ],
+        tests_require = [
+            'mamba',
+            'expects',
+            'doublex',
+            'doublex_expects',
+            'HTTPretty',
+            'mock',
+            'mockito',
+        ],
+        cmdclass = {'test': MambaTest},
+        zip_safe = False,
+)
