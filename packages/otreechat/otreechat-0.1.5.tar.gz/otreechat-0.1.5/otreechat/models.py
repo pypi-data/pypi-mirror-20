5a1bc98fcd7b37e63723e9ec23588e368d363256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:11 +0000

    Add package otreechat/0.1.5/otreechat-0.1.5.tar.gz

---
+++ packages/otreechat/otreechat-0.1.5.tar.gz/otreechat-0.1.5/otreechat/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from otree.models import Participant, Session
+import time
+
+
+class ChatMessage(models.Model):
+    class Meta:
+        index_together = ['channel', 'timestamp']
+
+    # the name "channel" here is unrelated to Django channels
+    channel = models.CharField(max_length=255)
+    participant = models.ForeignKey(Participant)
+    nickname = models.CharField(max_length=255)
+
+    # call it 'body' instead of 'message' or 'content' because those terms
+    # are already used by channels
+    body = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class NicknameRegistration(models.Model):
+    '''For security, have to register the nicknames on the server side'''
+
+    class Meta:
+        unique_together = ['channel', 'participant']
+
+    channel = models.CharField(max_length=255)
+    participant = models.ForeignKey(Participant)
+    nickname = models.CharField(max_length=255)
