6f269fc672c36fd1aadec7ee4ca28b00f7fa2d76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:29 +0000

    Add package pybel/0.3.9/PyBEL-0.3.9.tar.gz

---
+++ packages/pybel/PyBEL-0.3.9.tar.gz/PyBEL-0.3.9/src/pybel/manager/graph_cache.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from . import models
+from .cache import BaseCacheManager
+from ..io import to_bytes, from_bytes
+
+log = logging.getLogger(__name__)
+
+
+class GraphCacheManager(BaseCacheManager):
+    def insert_graph(self, graph):
+        """Stores a graph in the database
+
+        :param graph: a BEL network
+        :type graph: :class:`pybel.BELGraph`
+        """
+        network = models.Network(blob=to_bytes(graph), **graph.document)
+
+        self.session.add(network)
+        self.session.commit()
+
+        return network
+
+    def get_graph_versions(self, name):
+        """Returns all of the versions of a graph with the given name"""
+        return {x for x, in self.session.query(models.Network.version).filter(models.Network.name == name).all()}
+
+    def get_graph(self, name, version=None):
+        """Loads most recent graph, or allows for specification of version
+
+        :param name: The name of the graph
+        :type name: str
+        :param version: The version string of the graph. If not specified, loads most recent graph added with this name
+        :type version: None or str
+        :return:
+        """
+        if version is not None:
+            n = self.session.query(models.Network).filter(models.Network.name == name,
+                                                          models.Network.version == version).one()
+        else:
+            n = self.session.query(models.Network).filter(models.Network.name == name).order_by(
+                models.Network.created.desc()).limit(1).one()
+
+        return from_bytes(n.blob)
+
+    def drop_graph(self, network_id):
+        """Drops a graph by ID"""
+
+        # TODO delete with cascade
+        self.session.query(models.Network).filter(models.Network.id == network_id).delete()
+        self.session.commit()
+
+    def ls(self):
+        return [(network.id, network.name, network.version) for network in self.session.query(models.Network).all()]
---
+++ packages/pybel/PyBEL-0.3.9.tar.gz/PyBEL-0.3.9/src/pybel/parser/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+The pybel.parser module contains utilities for parsing BEL documents and BEL statements
+"""
+
+from .parse_bel import BelParser
+from .parse_control import ControlParser
+from .parse_metadata import MetadataParser
+
+__all__ = [
+    'ControlParser',
+    'BelParser',
+    'MetadataParser',
+]
