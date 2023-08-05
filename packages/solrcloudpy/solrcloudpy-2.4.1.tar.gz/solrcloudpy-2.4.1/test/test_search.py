0451b72d6ac3bfaa8dccb38937cbdd2f6701f039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:01 +0000

    Add package solrcloudpy/2.4.1/solrcloudpy-2.4.1.tar.gz

---
+++ packages/solrcloudpy/solrcloudpy-2.4.1.tar.gz/solrcloudpy-2.4.1/test/test_search.py
@@ -0,0 +1,72 @@
+import unittest
+import time
+import os
+from solr_instance import SolrInstance
+from solrcloudpy import SolrConnection, SearchOptions
+
+
+solrprocess = None
+
+
+class TestCollectionSearch(unittest.TestCase):
+
+    def setUp(self):
+        self.conn = SolrConnection(version=os.getenv('SOLR_VERSION', '6.1.0'))
+        self.collparams = {}
+        confname = os.getenv('SOLR_CONFNAME', '')
+        if confname != '':
+            self.collparams['collection_config_name'] = confname
+
+    def test_add(self):
+        coll2 = self.conn.create_collection('coll2', **self.collparams)
+        docs = [{"id": str(_id), "includes": "silly text"} for _id in range(5)]
+
+        coll2.add(docs)
+        coll2.commit()
+        res = coll2.search({"q": "id:1"}).result
+        self.assertTrue(len(res.response.docs) == 1)
+        coll2.drop()
+
+    def test_delete(self):
+        coll2 = self.conn.create_collection('coll2', **self.collparams)
+        docs = [{"id": str(_id), "includes": "silly text"} for _id in range(5)]
+
+        coll2.add(docs)
+        coll2.commit()
+
+        # delete w/ object
+        so = SearchOptions()
+        so.commonparams.q("id:1")
+        coll2.delete(so)
+        res = coll2.search({"q": "id:1"}).result
+        self.assertTrue(len(res.response.docs) == 0)
+
+        # delete w/ dict
+        so = {"q": "id:2"}
+        coll2.delete(so)
+        res = coll2.search({"q": "id:2"}).result
+        self.assertTrue(len(res.response.docs) == 0)
+
+        coll2.drop()
+
+
+def setUpModule():
+    if os.getenv('SKIP_STARTUP', False):
+        return
+    # start solr
+    solrprocess = SolrInstance("solr2")
+    solrprocess.start()
+    solrprocess.wait_ready()
+    time.sleep(3)
+
+
+def tearDownModule():
+    if os.getenv('SKIP_STARTUP', False):
+        return
+    if solrprocess:
+        solrprocess.terminate()
+
+
+if __name__ == '__main__':
+    # run tests
+    unittest.main()
