c177a923d2c261b995f022dd2e8e24d72174c1da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:36 +0000

    Add package pyflights/0.1/pyflights-0.1.tar.gz

---
+++ packages/pyflights/pyflights-0.1.tar.gz/pyflights-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='pyflights',
+      version='0.1',
+      description='A simple wrapper for the Google Flights Search API.',
+      url='https://github.com/AnthonyBloomer/pyflight',
+      author='Anthony Bloomer',
+      author_email='ant0@protonmail.ch',
+      license='MIT',
+      packages=['pyflights', 'pyflights.models'],
+      keywords=['google flights', 'flights', 'api'],
+      install_requires=[
+          'requests',
+      ],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          "Topic :: Software Development :: Libraries",
+          'Programming Language :: Python :: 2.7'
+      ],
+      zip_safe=False)
