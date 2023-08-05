ad17574e5118b372103a074ba715b978f5b4bf65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:48 +0000

    Add package raccoon/1.1.7/raccoon-1.1.7-py3-none-any.whl

---
+++ packages/raccoon/raccoon-1.1.7-py3-none-any.whl/raccoon/utils.py
@@ -0,0 +1,19 @@
+"""
+Raccoon utilities
+"""
+
+
+def assert_frame_equal(left, right):
+    """
+    For unit testing equality of two DataFrames.
+
+    :param left: first DataFrame
+    :param right: second DataFrame
+    :return: nothing
+    """
+    assert left.data == right.data
+    assert left.index == right.index
+    assert left.columns == right.columns
+    assert left.index_name == right.index_name
+    assert left.sorted == right.sorted
+    assert left.blist == right.blist
