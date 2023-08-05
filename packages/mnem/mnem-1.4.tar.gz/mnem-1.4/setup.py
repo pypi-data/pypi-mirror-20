b5fc273f8b5e05a13bdd7f1c6d8a0c10fa121bc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:02 +0000

    Add package mnem/1.4/mnem-1.4.tar.gz

---
+++ packages/mnem/mnem-1.4.tar.gz/mnem-1.4/setup.py
@@ -0,0 +1,69 @@
+#! /usr/bin/env python2
+
+from distutils.core import setup
+
+setup(name="mnem",
+      version="1.4",
+      description="A note reminder that uses dzen2 to display notes", 
+      scripts=['mnem', 'mnem-daemon'],
+      py_modules = ['mnemcore'],
+      author="Iury O. G. Figueiredo",
+      author_email="ioliveira@id.uff.br",
+      url='https://github.com/iogf/mnem',
+      download_url='https://github.com/iogf/mnem/releases',
+      keywords=['reminder', 'mne', 'note', 'schedule', 'mnemosyne'],
+      classifiers=[])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
