717c96e1eeab3dfac4d56992afc29ee7ab279cf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:10 +0000

    Add package stst/0.1.0/stst-0.1.0.tar.gz

---
+++ packages/stst/stst-0.1.0.tar.gz/stst-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='stst',
+    packages=['stst'],
+    version='0.1.0',
+    description='stst',
+    author='rgtjf',
+    author_email='rgtjf1@163.com',
+    url='https://github.com/rgtjf/Semantic-Texual-Similarity-Toolkits',
+    license='MIT',
+    keywords=['testing', 'logging', 'example'],  # arbitrary keywords
+    classifiers=[
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.7',
+    ]
+)
