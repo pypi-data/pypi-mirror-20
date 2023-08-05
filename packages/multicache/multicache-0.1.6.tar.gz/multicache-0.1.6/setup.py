8c7e9991a97441055e62feb60a20c41c00c20bea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:13 +0000

    Add package multicache/0.1.6/multicache-0.1.6.tar.gz

---
+++ packages/multicache/multicache-0.1.6.tar.gz/multicache-0.1.6/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+tests_require = [
+    'pytest'
+]
+
+setup(
+    name='multicache',
+    packages=['multicache'],
+    version='0.1.6',
+    description='Simple caching mechanisms',
+    author='Michal Ovciarik',
+    author_email='movciari@gmail.com',
+    url='https://github.com/bargulg/multicache',
+    download_url='https://github.com/bargulg/multicache/tarball/0.1',
+    keywords=['cache', 'caching', 'simple', 'easy', 'multi'],
+    classifiers=[],
+    extras_require={
+        'tests': tests_require
+    }
+)
