6aa7cb57817fed2fc40c7b822640ee15b6d3e6fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:17 +0000

    Add package function-profiler/0.4/function-profiler-0.4.tar.gz

---
+++ packages/function-profiler/function-profiler-0.4.tar.gz/function-profiler-0.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='function-profiler',
+    version='0.4',
+    description='A Python decorator to profile function performance.',
+    url='https://github.com/Datamine/Function-Profiler',
+    author='John Loeber',
+    author_email='contact@johnloeber.com',
+    packages=['profiler'],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=['numpy']
+)
