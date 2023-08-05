9123dc4a12a84fb6f2bd67801afff14e9ea270cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:33 +0000

    Add package hansel/1.0.0/hansel-1.0.0-py3-none-any.whl

---
+++ packages/hansel/hansel-1.0.0-py3-none-any.whl/hansel/__init__.py
@@ -0,0 +1,9 @@
+
+from .crumb  import Crumb
+from .mktree import mktree
+from .utils  import (ParameterGrid,
+                     intersection,
+                     difference,
+                     groupby_pattern,
+                     crumb_copy,
+                     crumb_link)
---
+++ packages/hansel/hansel-1.0.0-py3-none-any.whl/hansel/version.py
@@ -0,0 +1,7 @@
+# License: New BSD
+"""
+Unique version information place
+"""
+
+__version__ = "1.0.0"
+VERSION = tuple(int(x) for x in __version__.split("."))
