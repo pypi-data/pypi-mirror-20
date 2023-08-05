8a81dbcd26cc74272963208057568b86d2e63c1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:36 +0000

    Add package tomber/1.0.5/tomber-1.0.5.tar.gz

---
+++ packages/tomber/tomber-1.0.5.tar.gz/tomber-1.0.5/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+requires = ['']
+
+
+setup(name='tomber',
+    version='1.0.5',
+    description='a python Tomb (the Crypto Undertaker) wrapper',
+    long_description=read('README.mkdn'),
+    license="BSD",
+    author='Federico Reiven',
+    author_email='reiven@gmail.com',
+    url='https://github.com/reiven/tomber',
+    keywords='tomb wrapper',
+    packages=find_packages(),
+    install_requires=requires,
+    )
