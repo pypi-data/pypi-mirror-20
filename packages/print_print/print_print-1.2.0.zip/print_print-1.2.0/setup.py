50cc4dcc7e948bead61a421ffc143f8517eb5ace
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:37 +0000

    Add package print_print/1.2.0/print_print-1.2.0.zip

---
+++ packages/print_print/print_print-1.2.0.zip/print_print-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name        =   'print_print',
+        version     =   '1.2.0',
+        py_modules  =   ['print_print'],
+        author      =   'ouyangnan',
+        author_email=   'ouyangnan@126.com',
+        url         =   'http://www.headfirstlabs.com',
+        description =   'A Simple printer of nested lists',
+        )
