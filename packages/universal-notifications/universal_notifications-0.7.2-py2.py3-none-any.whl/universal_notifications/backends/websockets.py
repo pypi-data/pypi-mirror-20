fac566818b263947aa2e4fb574ac3be0d28071f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:40 +0000

    Add package universal-notifications/0.7.2/universal_notifications-0.7.2-py2.py3-none-any.whl

---
+++ packages/universal-notifications/universal_notifications-0.7.2-py2.py3-none-any.whl/universal_notifications/backends/websockets.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from django.conf import settings
+from rest_framework.renderers import JSONRenderer
+from ws4redis.publisher import RedisPublisher
+from ws4redis.redis_store import RedisMessage
+from ws4redis.subscriber import RedisSubscriber
+
+
+def publish(user, item=None, additional_data=None):
+    if additional_data is None:
+        additional_data = {}
+    redis_publisher = RedisPublisher(facility='all', users=[user.email])
+    r = JSONRenderer()
+    if item is None:
+        data = {}
+    else:
+        data = item.as_dict()
+    data.update(additional_data)
+    data = r.render(data)
+    message = RedisMessage(data)
+    if getattr(settings, 'TESTING', False):
+        # Do not send in tests
+        return
+    redis_publisher.publish_message(message)
+
+
+class RedisSubscriber(RedisSubscriber):
+    def publish_message(self, message, expire=None):
+        try:
+            # custom code goes here
+            # for efficiency think about using celery to do the job - has to be freaking FAST!
+            print ('message received!')
+            print (message)
+        except Exception:
+            # I mean it, catch everything, log it if needed but do catch everything
+            pass
+        return super(RedisSubscriber, self).publish_message(message, expire)
