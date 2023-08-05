caedb98e1cd78719200912136dfff59a12680e97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:27 +0000

    Add package rasterio/1.0a7/rasterio-1.0a7.tar.gz

---
+++ packages/rasterio/rasterio-1.0a7.tar.gz/rasterio-1.0a7/rasterio/env.py
@@ -0,0 +1,275 @@
+"""Rasterio's GDAL/AWS environment"""
+
+from functools import wraps
+import itertools as it
+import logging
+
+from rasterio._env import (
+    GDALEnv, del_gdal_config, get_gdal_config, set_gdal_config)
+from rasterio.dtypes import check_dtype
+from rasterio.errors import EnvError
+from rasterio.compat import string_types
+from rasterio.transform import guard_transform
+from rasterio.vfs import parse_path, vsi_path
+
+
+# The currently active GDAL/AWS environment is a private attribute.
+_env = None
+
+
+# When the outermost 'rasterio.Env()' executes '__enter__' it probes the
+# GDAL environment to see if any of the supplied config options already
+# exist, the assumption being that they were set with
+# 'osgeo.gdal.SetConfigOption()' or possibly 'rasterio.env.set_gdal_config()'.
+# The discovered options are reinstated when the outermost Rasterio environment
+# exits.  Without this check any environment options that are present in the
+# GDAL environment and are also passed to 'rasterio.Env()' will be unset
+# when 'rasterio.Env()' tears down, regardless of their value.  For example:
+#
+#   from osgeo import gdal
+#   import rasterio
+#
+#   gdal.SetConfigOption('key', 'value')
+#   with rasterio.Env(key='something'):
+#       pass
+#
+# The config option 'key' would be unset when 'Env()' exits.  A more
+# comprehensive solution would also leverage https://trac.osgeo.org/gdal/changeset/37273
+# but this gets Rasterio + older versions of GDAL halfway there.  One major
+# assumption is that environment variables are not set directly with
+# 'osgeo.gdal.SetConfigOption()' OR 'rasterio.env.set_gdal_config()' inside
+# of a 'rasterio.Env()'.
+_discovered_options = None
+
+
+log = logging.getLogger(__name__)
+
+# Rasterio defaults
+default_options = {
+    'CHECK_WITH_INVERT_PROJ': True,
+    'GTIFF_IMPLICIT_JPEG_OVR': False,
+    "I'M_ON_RASTERIO": True
+}
+
+class Env(object):
+    """Abstraction for GDAL and AWS configuration
+
+    The GDAL library is stateful: it has a registry of format drivers,
+    an error stack, and dozens of configuration options.
+
+    Rasterio's approach to working with GDAL is to wrap all the state
+    up using a Python context manager (see PEP 343,
+    https://www.python.org/dev/peps/pep-0343/). When the context is
+    entered GDAL drivers are registered, error handlers are
+    configured, and configuration options are set. When the context
+    is exited, drivers are removed from the registry and other
+    configurations are removed.
+
+    Example:
+
+        with rasterio.Env(GDAL_CACHEMAX=512) as env:
+            # All drivers are registered, GDAL's raster block cache
+            # size is set to 512MB.
+            # Commence processing...
+            ...
+            # End of processing.
+
+        # At this point, configuration options are set to their
+        # previous (possible unset) values.
+
+    A boto3 session or boto3 session constructor arguments
+    `aws_access_key_id`, `aws_secret_access_key`, `aws_session_token`
+    may be passed to Env's constructor. In the latter case, a session
+    will be created as soon as needed. AWS credentials are configured
+    for GDAL as needed.
+    """
+
+    def __init__(self, aws_session=None, aws_access_key_id=None,
+                 aws_secret_access_key=None, aws_session_token=None,
+                 region_name=None, profile_name=None, **options):
+        """Create a new GDAL/AWS environment.
+
+        Note: this class is a context manager. GDAL isn't configured
+        until the context is entered via `with rasterio.Env():`
+
+        Parameters
+        ----------
+        aws_session: object, optional
+            A boto3 session.
+        aws_access_key_id: string, optional
+            An access key id, as per boto3.
+        aws_secret_access_key: string, optional
+            A secret access key, as per boto3.
+        aws_session_token: string, optional
+            A session token, as per boto3.
+        region_name: string, optional
+            A region name, as per boto3.
+        profile_name: string, optional
+            A shared credentials profile name, as per boto3.
+        **options: optional
+            A mapping of GDAL configuration options, e.g.,
+            `CPL_DEBUG=True, CHECK_WITH_INVERT_PROJ=False`.
+
+        Returns
+        -------
+        A new instance of Env.
+
+        Note: We raise EnvError if the GDAL config options
+        AWS_ACCESS_KEY_ID or AWS_SECRET_ACCESS_KEY are given. AWS
+        credentials are handled exclusively by boto3.
+        """
+        if ('AWS_ACCESS_KEY_ID' in options or
+                'AWS_SECRET_ACCESS_KEY' in options):
+            raise EnvError(
+                "GDAL's AWS config options can not be directly set. "
+                "AWS credentials are handled exclusively by boto3.")
+        self.aws_access_key_id = aws_access_key_id
+        self.aws_secret_access_key = aws_secret_access_key
+        self.aws_session_token = aws_session_token
+        self.region_name = region_name
+        self.profile_name = profile_name
+        self.aws_session = aws_session
+        self._creds = (
+            self.aws_session._session.get_credentials()
+            if self.aws_session else None)
+        self.options = options.copy()
+        self.context_options = {}
+
+    def get_aws_credentials(self):
+        """Get credentials and configure GDAL."""
+        import boto3
+        options = {}
+        if not self.aws_session:
+            self.aws_session = boto3.Session(
+                aws_access_key_id=self.aws_access_key_id,
+                aws_secret_access_key=self.aws_secret_access_key,
+                aws_session_token=self.aws_session_token,
+                region_name=self.region_name,
+                profile_name=self.profile_name)
+        self._creds = self.aws_session._session.get_credentials()
+
+        # Pass these credentials to the GDAL environment.
+        if self._creds.access_key:  # pragma: no branch
+            options.update(aws_access_key_id=self._creds.access_key)
+        if self._creds.secret_key:  # pragma: no branch
+            options.update(aws_secret_access_key=self._creds.secret_key)
+        if self._creds.token:
+            options.update(aws_session_token=self._creds.token)
+        if self.aws_session.region_name:
+            options.update(aws_region=self.aws_session.region_name)
+
+        # Pass these credentials to the GDAL environment.
+        global _env
+        _env.update_config_options(**options)
+
+    def drivers(self):
+        """Return a mapping of registered drivers."""
+        global _env
+        return _env.drivers()
+
+    def __enter__(self):
+        global _env
+        global _discovered_options
+        log.debug("Entering env context: %r", self)
+
+        # No parent Rasterio environment exists.
+        if _env is None:
+            logging.debug("Starting outermost env")
+            self._has_parent_env = False
+
+            # See note directly above where _discovered_options is globally
+            # defined.  This MUST happen before calling 'defenv()'.
+            _discovered_options = {}
+            # Don't want to reinstate the "I'M_ON_RASTERIO" option.
+            probe_env = {k for k in default_options if k != "I'M_ON_RASTERIO"}
+            probe_env |= set(self.options.keys())
+            for key in probe_env:
+                val = get_gdal_config(key, normalize=False)
+                if val is not None:
+                    _discovered_options[key] = val
+                    logging.debug("Discovered option: %s=%s", key, val)
+
+            defenv()
+            self.context_options = {}
+        else:
+            self._has_parent_env = True
+            self.context_options = getenv()
+        setenv(**self.options)
+        log.debug("Entered env context: %r", self)
+        return self
+
+    def __exit__(self, exc_type=None, exc_val=None, exc_tb=None):
+        global _env
+        global _discovered_options
+        log.debug("Exiting env context: %r", self)
+        delenv()
+        if self._has_parent_env:
+            defenv()
+            setenv(**self.context_options)
+        else:
+            logging.debug("Exiting outermost env")
+            # See note directly above where _discovered_options is globally
+            # defined.
+            while _discovered_options:
+                key, val = _discovered_options.popitem()
+                set_gdal_config(key, val, normalize=False)
+                logging.debug(
+                    "Set discovered option back to: '%s=%s", key, val)
+            _discovered_options = None
+        log.debug("Exited env context: %r", self)
+
+
+def defenv():
+    """Create a default environment if necessary."""
+    global _env
+    if _env:
+        log.debug("GDAL environment exists: %r", _env)
+    else:
+        log.debug("No GDAL environment exists")
+        _env = GDALEnv()
+        _env.update_config_options(**default_options)
+        log.debug(
+            "New GDAL environment %r created", _env)
+    _env.start()
+
+
+def getenv():
+    """Get a mapping of current options."""
+    global _env
+    if not _env:
+        raise EnvError("No GDAL environment exists")
+    else:
+        log.debug("Got a copy of environment %r options", _env)
+        return _env.options.copy()
+
+
+def setenv(**options):
+    """Set options in the existing environment."""
+    global _env
+    if not _env:
+        raise EnvError("No GDAL environment exists")
+    else:
+        _env.update_config_options(**options)
+        log.debug("Updated existing %r with options %r", _env, options)
+
+
+def delenv():
+    """Delete options in the existing environment."""
+    global _env
+    if not _env:
+        raise EnvError("No GDAL environment exists")
+    else:
+        _env.clear_config_options()
+        log.debug("Cleared existing %r options", _env)
+    _env.stop()
+    _env = None
+
+
+def ensure_env(f):
+    """A decorator that ensures an env exists before a function
+    calls any GDAL C functions."""
+    @wraps(f)
+    def wrapper(*args, **kwds):
+        with Env(WITH_RASTERIO_ENV=True):
+            return f(*args, **kwds)
+    return wrapper
---
+++ packages/rasterio/rasterio-1.0a7.tar.gz/rasterio-1.0a7/tests/test_read_boundless.py
@@ -0,0 +1,118 @@
+import logging
+import sys
+
+import numpy as np
+import pytest
+
+import rasterio
+
+
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+
+
+def test_read_boundless_natural_extent():
+    with rasterio.open('tests/data/RGB.byte.tif') as src:
+        data = src.read(boundless=True)
+        assert data.shape == (3, src.height, src.width)
+        assert abs(data[0].mean() - src.read(1).mean()) < 0.0001
+        assert data.any()
+
+
+def test_read_boundless_beyond():
+    with rasterio.open('tests/data/RGB.byte.tif') as src:
+        data = src.read(window=((-200, -100), (-200, -100)), boundless=True)
+        assert data.shape == (3, 100, 100)
+        assert not data.any()
+
+
+def test_read_boundless_beyond2():
+    with rasterio.open('tests/data/RGB.byte.tif') as src:
+        data = src.read(window=((1000, 1100), (1000, 1100)), boundless=True)
+        assert data.shape == (3, 100, 100)
+        assert not data.any()
+
+
+def test_read_boundless_overlap():
+    with rasterio.open('tests/data/RGB.byte.tif') as src:
+        data = src.read(window=((-200, 200), (-200, 200)), boundless=True)
+        assert data.shape == (3, 400, 400)
+        assert data.any()
+        assert data[0,399,399] == 13
+
+
+def test_read_boundless_resample():
+    with rasterio.open('tests/data/RGB.byte.tif') as src:
+        out = np.zeros((3, 800, 800), dtype=np.uint8)
+        data = src.read(
+                out=out,
+                window=((-200, 200), (-200, 200)),
+                masked=True,
+                boundless=True)
+        assert data.shape == (3, 800, 800)
+        assert data.any()
+        assert data[0,798,798] == 13
+
+
+def test_read_boundless_masked_no_overlap():
+    with rasterio.open('tests/data/RGB.byte.tif') as src:
+        data = src.read(
+            window=((-200, -100), (-200, -100)), boundless=True, masked=True)
+        assert data.shape == (3, 100, 100)
+        assert data.mask.all()
+
+
+def test_read_boundless_masked_overlap():
+    with rasterio.open('tests/data/RGB.byte.tif') as src:
+        data = src.read(
+            window=((-200, 200), (-200, 200)), boundless=True, masked=True)
+        assert data.shape == (3, 400, 400)
+        assert data.mask.any()
+        assert not data.mask.all()
+        assert data.mask[0,399,399] == False
+        assert data.mask[0,0,0] == True
+
+
+def test_read_boundless_zero_stop():
+    with rasterio.open('tests/data/RGB.byte.tif') as src:
+        data = src.read(
+            window=((-200, 0), (-200, 0)), boundless=True, masked=True)
+        assert data.shape == (3, 200, 200)
+        assert data.mask.all()
+
+
+def test_read_boundless_masks_zero_stop():
+    with rasterio.open('tests/data/RGB.byte.tif') as src:
+        data = src.read_masks(window=((-200, 0), (-200, 0)), boundless=True)
+        assert data.shape == (3, 200, 200)
+        assert data.min() == data.max() == src.nodata
+
+def test_read_boundless_noshift():
+    with rasterio.open('tests/data/rgb4.tif') as src:
+        # the read offsets should be determined by start col/row alone
+        # when col stop exceeds image width
+        c1 = src.read(boundless=True,
+                      window=((100, 101), (-1, src.shape[1])))[0, 0, 0:9]
+        c2 = src.read(boundless=True,
+                      window=((100, 101), (-1, src.shape[1] + 1)))[0, 0, 0:9]
+        assert np.array_equal(c1, c2)
+
+        # when row stop exceeds image height
+        r1 = src.read(boundless=True,
+                      window=((-1, src.shape[0]), (100, 101)))[0, 0, 0:9]
+        r2 = src.read(boundless=True,
+                      window=((-1, src.shape[0] + 1), (100, 101)))[0, 0, 0:9]
+        assert np.array_equal(r1, r2)
+
+
+def test_np_warning(recwarn):
+    """Ensure no deprecation warnings
+    On np 1.11 and previous versions of rasterio you might see:
+        VisibleDeprecationWarning: using a non-integer number
+        instead of an integer will result in an error in the future
+    """
+    import warnings
+    warnings.simplefilter('always')
+    with rasterio.open('tests/data/RGB.byte.tif') as src:
+        window = ((-10, 100), (-10, 100))
+        src.read(1, window=window, boundless=True)
+    assert len(recwarn) == 0
