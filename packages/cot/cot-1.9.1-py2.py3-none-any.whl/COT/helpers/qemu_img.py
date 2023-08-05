99a25c22778a5a009cbd80fa06141068fdd1d348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:30 +0000

    Add package cot/1.9.1/cot-1.9.1-py2.py3-none-any.whl

---
+++ packages/cot/cot-1.9.1-py2.py3-none-any.whl/COT/helpers/qemu_img.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+#
+# qemu_img.py - Helper for 'qemu-img'
+#
+# February 2015, Glenn F. Matthews
+# Copyright (c) 2013-2017 the COT project developers.
+# See the COPYRIGHT.txt file at the top-level directory of this distribution
+# and at https://github.com/glennmatthews/cot/blob/master/COPYRIGHT.txt.
+#
+# This file is part of the Common OVF Tool (COT) project.
+# It is subject to the license terms in the LICENSE.txt file found in the
+# top-level directory of this distribution and at
+# https://github.com/glennmatthews/cot/blob/master/LICENSE.txt. No part
+# of COT, including this file, may be copied, modified, propagated, or
+# distributed except according to the terms contained in the LICENSE.txt file.
+
+"""Give COT access to ``qemu-img`` for manipulating disk image formats.
+
+http://www.qemu.org
+"""
+
+from .helper import Helper
+
+
+class QEMUImg(Helper):
+    """Helper provider for ``qemu-img`` (http://www.qemu.org)."""
+
+    _provider_package = {
+        'apt-get': 'qemu-utils',
+        'brew': 'qemu',
+        'port': 'qemu',
+        'yum': 'qemu-img',
+    }
+
+    def __init__(self):
+        """Initializer."""
+        super(QEMUImg, self).__init__(
+            "qemu-img",
+            info_uri="http://www.qemu.org",
+            version_regexp="qemu-img version ([0-9.]+)")
