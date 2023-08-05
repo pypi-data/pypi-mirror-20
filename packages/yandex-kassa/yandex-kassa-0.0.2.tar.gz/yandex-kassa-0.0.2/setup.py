8600fa94a2f8490231c903b53020078adfd31077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:32 +0000

    Add package yandex-kassa/0.0.2/yandex-kassa-0.0.2.tar.gz

---
+++ packages/yandex-kassa/yandex-kassa-0.0.2.tar.gz/yandex-kassa-0.0.2/setup.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='yandex-kassa',
+    version='0.0.2',
+    author='Konstantin Kruglov',
+    author_email='kruglovk@gmail.com',
+    description='empty',
+    url='https://github.com/k0st1an/yandex-kassa',
+    packages=find_packages(),
+    py_modules=['yandex_kassa'],
+    license='Apache License Version 2.0',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3 :: Only',
+        'Operating System :: POSIX :: Linux',
+    ],
+)
