a0420173857acb9e8874b64310a41a0a393b659a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:11 +0000

    Add package wsl/0.11/wsl-0.11.tar.gz

---
+++ packages/wsl/wsl-0.11.tar.gz/wsl-0.11/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+setuptools.setup(
+    name='wsl',
+    version='0.11',
+    description='Library for reading and writing WSL databases',
+    long_description='Library for reading and writing WSL databases',
+    url='http://jstimpfle.de/projects/wsl/main.html',
+    author='Jens Stimpfle',
+    author_email='jfs@jstimpfle.de',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Other Audience',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Database',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='plain-text relational database schema hierarchical',
+    packages=['wsl', 'wsl.wslh'],
+    install_requires=[],
+    extras_require={
+    },
+    package_data={
+    },
+    data_files=[],
+    entry_points={
+    },
+)
