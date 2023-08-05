f3ff07e14130e9dc999efaf6b819f816c02b2df7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:11 +0000

    Add package pydak/0.1/pydak-0.1.tar.gz

---
+++ packages/pydak/pydak-0.1.tar.gz/pydak-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pydak',
+    version='0.1',
+    # packages=[''],
+    install_requires=['pyserial'],
+    url='https://github.com/fimion/pydak',
+    license='MIT',
+    author='Alex Riviere',
+    author_email='fimion@gmail.com',
+    description='Python Library for Daktronics AllSport 5000 Scoring Controllers'
+)
