622ac93676cbe67f46e30b072969d4097f6d8545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:41 +0000

    Add package robozilla/0.1.7/robozilla-0.1.7.tar.gz

---
+++ packages/robozilla/robozilla-0.1.7.tar.gz/robozilla-0.1.7/robozilla/constants.py
@@ -0,0 +1,26 @@
+
+BUGZILLA_ENVIRON_USER_NAME = 'BUGZILLA_USER_NAME'
+BUGZILLA_ENVIRON_USER_PASSWORD_NAME = 'BUGZILLA_USER_PASSWORD'
+BUGZILLA_QUERY_PRODUCT = 'Red Hat Satellite 6'
+BUGZILLA_URL = 'https://bugzilla.redhat.com/xmlrpc.cgi'
+
+FILE_NAME_PATTERN = '*.py'
+
+DEFAULT_INCLUDE_FIELDS = [
+    'id',
+    'status',
+    'whiteboard',
+    'resolution',
+    'flags',
+    # getting dupe_of, cf_clone_of and depends_on fields
+    # makes request very slow
+    # add this fields explicitly and also follow_duplicates=True
+    # follow_clones=True
+    # 'dupe_of',
+    # 'cf_clone_of',
+    # 'depends_on',
+]
+
+DUPLICATES_FIELD = 'dupe_of'
+CLONES_FIELD = 'cf_clone_of'
+DEPENDENT_FIELD = 'depends_on'
