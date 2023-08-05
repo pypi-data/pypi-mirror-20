2e0f4f8f3d1ddd6b1ddf1920a1f6caa32121f079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:04 +0000

    Add package symbolic-quantum-computation/0.1.18/symbolic-quantum-computation-0.1.18.tar.gz

---
+++ packages/symbolic-quantum-computation/symbolic-quantum-computation-0.1.18.tar.gz/symbolic-quantum-computation-0.1.18/sqc/__main__.py
@@ -0,0 +1,18 @@
+import sys
+from src import foo
+from test_package import test
+
+
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+    # Do something here
+    for flag in args:
+        if flag == '-t':
+            test.run_tests()
+        else:
+            foo.bar()
+
+if __name__ == '__main__':
+    main()
