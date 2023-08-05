3100a17faf87d21384044277677034f3d68a4dff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:15 +0000

    Add package wikitextparser/0.14.3.dev1/wikitextparser-0.14.3.dev1-py3-none-any.whl

---
+++ packages/wikitextparser/wikitextparser-0.14.3.dev1-py3-none-any.whl/wikitextparser/parser_function.py
@@ -0,0 +1,56 @@
+""""Define the ParserFunction class."""
+
+
+from typing import List
+
+from .wikitext import SubWikiText
+from .argument import Argument
+
+
+class ParserFunction(SubWikiText):
+
+    """Create a new ParserFunction object."""
+
+    @property
+    def arguments(self) -> List[Argument]:
+        """Parse template content. Create self.name and self.arguments."""
+        barsplits = self._atomic_split_spans('|')
+        arguments = []
+        spans = self._type_to_spans
+        lststr = self._lststr
+        typeindex = 'pfa' + str(self._index)
+        if typeindex not in spans:
+            spans[typeindex] = []
+        aspans = spans[typeindex]
+        # remove the final '}}' from the last argument.
+        barsplits[-1] = (barsplits[-1][0], barsplits[-1][1] - 2)
+        # first argument
+        aspan = barsplits.pop(0)
+        aspan = (aspan[0] + self.string.find(':'), aspan[1])
+        if aspan not in aspans:
+            aspans.append(aspan)
+        arguments.append(
+            Argument(lststr, spans, aspans.index(aspan), typeindex)
+        )
+        # the rest of the arguments (similar to templates)
+        if barsplits:
+            for aspan in barsplits:
+                # include the the starting '|'
+                aspan = (aspan[0] - 1, aspan[1])
+                if aspan not in aspans:
+                    aspans.append(aspan)
+                arguments.append(
+                    Argument(lststr, spans, aspans.index(aspan), typeindex)
+                )
+        return arguments
+
+    @property
+    def name(self) -> str:
+        """Return name part of the current ParserFunction."""
+        return self.string[2:].partition(':')[0]
+
+    @name.setter
+    def name(self, newname: str) -> None:
+        """Set a new name."""
+        name = self.name
+        self[2:2 + len(name)] = newname
