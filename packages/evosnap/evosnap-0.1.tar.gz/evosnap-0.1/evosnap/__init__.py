c80f09504c5b89520f580087f2467f65640c57c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:38 +0000

    Add package evosnap/0.1/evosnap-0.1.tar.gz

---
+++ packages/evosnap/evosnap-0.1.tar.gz/evosnap-0.1/evosnap/__init__.py
@@ -0,0 +1,6 @@
+from evosnap.data_element_encoder import DataElementEncoder
+from evosnap.request import Request
+from evosnap.response import Response
+from evosnap.transactions import Transaction, TransactionTenderData, TransactionData, CardData
+from evosnap.transactions import Capture, CardSecurityData, EcommerceSecurityData, InternetTransactionData
+from evosnap.transactions import InternationalAVSData, ReturnById, Adjust, Addendum, Unmanaged, Undo
---
+++ packages/evosnap/evosnap-0.1.tar.gz/evosnap-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name='evosnap',
+  packages=['evosnap'],
+  version='0.1',
+  description='Python 3 package that integrates with EVO Snap* Commerce Web Services API',
+  author='Javier Ros Honduvilla',
+  author_email='javier.ros.honduvilla@gmail.com',
+  url='https://github.com/Zertifica/evosnap',
+  download_url='https://github.com/Zertifica/evosnap/tarball/0.3',
+  keywords=['popular payments', 'tpv', 'snap'],
+  classifiers=[],
+)
