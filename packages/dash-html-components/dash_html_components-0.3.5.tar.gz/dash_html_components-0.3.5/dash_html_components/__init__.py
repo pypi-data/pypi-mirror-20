7050694da708997524c3625aaccff965cf100d02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:18 +0000

    Add package dash-html-components/0.3.5/dash_html_components-0.3.5.tar.gz

---
+++ packages/dash-html-components/dash_html_components-0.3.5.tar.gz/dash_html_components-0.3.5/dash_html_components/__init__.py
@@ -0,0 +1,16 @@
+import os as _os
+import dash as _dash
+import sys as _sys
+
+_current_path = _os.path.dirname(_os.path.abspath(__file__))
+
+_components = _dash.development.component_loader.load_components(
+    _os.path.join(_current_path, 'metadata.json'),
+    ['content', 'id', 'key', 'className', 'style'],
+    'dash_html_components'
+)
+
+_this_module = _sys.modules[__name__]
+
+for component in _components:
+    setattr(_this_module, component.__name__, component)
---
+++ packages/dash-html-components/dash_html_components-0.3.5.tar.gz/dash_html_components-0.3.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='dash_html_components',
+    version='0.3.5',
+    author='Chris Parmer',
+    author_email='chris@plot.ly',
+    packages=['dash_html_components'],
+    include_package_data=True,
+    license='MIT',
+    description='Dash UI HTML component suite',
+    install_requires=['flask', 'plotly', 'flask-cors', 'dash.ly']
+)
