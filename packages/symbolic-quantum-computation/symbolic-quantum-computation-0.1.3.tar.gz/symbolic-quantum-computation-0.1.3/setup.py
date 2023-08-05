a433a660238af39fb7d3cd5670ca1cd68c937966
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:39 +0000

    Add package symbolic-quantum-computation/0.1.3/symbolic-quantum-computation-0.1.3.tar.gz

---
+++ packages/symbolic-quantum-computation/symbolic-quantum-computation-0.1.3.tar.gz/symbolic-quantum-computation-0.1.3/setup.py
@@ -0,0 +1,37 @@
+from distutils.core import setup
+
+setup(
+    # Application name:
+    name='symbolic-quantum-computation',
+
+    # Version number (initial):
+    version='0.1.3',
+
+    # Application author details:
+    author='Nikita',
+    author_email='email@addr.ess',
+
+    # Packages
+    packages=[],   # SymbolicQuantumComputation
+    sripts=['scripts/sqc'],
+
+    py_modules=[''],
+
+    # Include additional files into the package
+    include_package_data=True,
+
+    # Details
+    url='https://bitbucket.org/grpdev/symbolic-quantum-computation',
+
+    #
+    license='LICENSE.txt',
+    readme='README.txt',
+    description='symbolic-quantum-computation description',
+
+    # long_description=open('README.txt').read(),
+
+    # Dependent packages (distributions)
+    install_requires=[
+        'numpy'
+    ],
+)
