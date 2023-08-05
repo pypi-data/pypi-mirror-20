45116b1d3fd063c064b3051469a726dc1ad40141
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:28 +0000

    Add package cwltest/1.0.20170214185319/cwltest-1.0.20170214185319.tar.gz

---
+++ packages/cwltest/cwltest-1.0.20170214185319.tar.gz/cwltest-1.0.20170214185319/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+import setuptools.command.egg_info as egg_info_cmd
+from setuptools import setup, find_packages
+
+SETUP_DIR = os.path.dirname(__file__)
+README = os.path.join(SETUP_DIR, 'README.rst')
+
+try:
+    import gittaggers
+    tagger = gittaggers.EggInfoFromGit
+except ImportError:
+    tagger = egg_info_cmd.egg_info
+
+install_requires=[
+    'schema-salad >= 1.14',
+    'typing >= 3.5.2',
+    'junit-xml >= 1.7'
+]
+
+if sys.version_info.major == 2:
+    install_requires.append('futures >= 3.0.5')
+
+setup(name='cwltest',
+      version='1.0',
+      description='Common workflow language testing framework',
+      long_description=open(README).read(),
+      author='Common workflow language working group',
+      author_email='common-workflow-language@googlegroups.com',
+      url="https://github.com/common-workflow-language/cwltest",
+      download_url="https://github.com/common-workflow-language/cwltest",
+      license='Apache 2.0',
+      packages=["cwltest"],
+      install_requires=install_requires,
+      tests_require=[],
+      entry_points={
+          'console_scripts': [ "cwltest=cwltest:main" ]
+      },
+      zip_safe=True,
+      cmdclass={'egg_info': tagger},
+)
