e93395ea10968f0638e207a55f099bb0f3333bf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:55 +0000

    Add package allink_essentials/1.1.4/allink_essentials-1.1.4.tar.gz

---
+++ packages/allink_essentials/allink_essentials-1.1.4.tar.gz/allink_essentials-1.1.4/allink_essentials/mailchimp_api/forms.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from django import forms
+from django.utils.translation import get_language, ugettext_lazy as _
+
+from .config import MailChimpConfig
+from helpers import list_members_put, get_status_if_new
+
+config = MailChimpConfig()
+
+
+class SignupForm(forms.Form):
+    email = forms.EmailField(label=_(u'E-Mail'))
+    language = forms.CharField(max_length=3, required=False)
+
+    def save(self):
+        email = self.cleaned_data['email']
+        language = self.cleaned_data['language'] if 'language' in self.cleaned_data else get_language()
+
+        data = {
+            'email_address': email,
+            'status': 'subscribed',
+            'status_if_new': get_status_if_new(),
+            'language': language
+        }
+
+        if config.merge_vars:
+            data = data.append(config.merge_vars)
+
+        list_members_put(data)
