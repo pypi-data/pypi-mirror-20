bc5ab9ce5034fcb0d5cb1fbc743fdc865e5cad1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:02 +0000

    Add package js.tinymce/4.5.4/js.tinymce-4.5.4.tar.gz

---
+++ packages/js.tinymce/js.tinymce-4.5.4.tar.gz/js.tinymce-4.5.4/setup.py
@@ -0,0 +1,61 @@
+from setuptools import setup, find_packages
+import os
+
+# The version of the wrapped library is the starting point for the
+# version number of the python package.
+# In bugfix releases of the python package, add a '-' suffix and an
+# incrementing integer.
+# For example, a packaging bugfix release version 1.4.4 of the
+# js.jquery package would be version 1.4.4-1 .
+
+version = '4.5.4'
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    read('js', 'tinymce', 'test_tinymce.txt')
+    + '\n' +
+    read('CHANGES.txt'))
+
+setup(
+    name='js.tinymce',
+    version=version,
+    description="Fanstatic packaging of TinyMCE",
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: JavaScript',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware',
+        'Topic :: Text Editors',
+        'Topic :: Text Processing :: Markup :: HTML',
+    ],
+    keywords='fanstatic TinyMCE',
+    author='Fanstatic Developers',
+    author_email='fanstatic@googlegroups.com',
+    license='BSD',
+    packages=find_packages(),
+    namespace_packages=['js'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'fanstatic',
+        'setuptools',
+    ],
+    entry_points={
+        'fanstatic.libraries': [
+            'tinymce = js.tinymce:library',
+        ],
+    },
+)
