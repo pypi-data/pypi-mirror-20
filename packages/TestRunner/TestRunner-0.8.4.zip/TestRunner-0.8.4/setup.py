c2ff9ec334f367c9f91886404c59c97dd088ceee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:45 +0000

    Add package TestRunner/0.8.4/TestRunner-0.8.4.zip

---
+++ packages/TestRunner/TestRunner-0.8.4.zip/TestRunner-0.8.4/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+name='TestRunner',
+version='0.8.4',
+py_modules=['TestRunner'],
+author='Kion',
+author_email='lovefancy2009@gmail.com',
+description='generate a report in Chinaese')
+ 
