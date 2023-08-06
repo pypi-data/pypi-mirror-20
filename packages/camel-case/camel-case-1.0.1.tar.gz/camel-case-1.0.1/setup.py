6f467c8262a0e272d860a77435f1dd40802ac930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:11 +0000

    Add package camel-case/1.0.0/camel-case-1.0.1.tar.gz

---
+++ packages/camel-case/camel-case-1.0.1.tar.gz/camel-case-1.0.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name='camel-case',
+    version='1.0.1',
+    description='Convert to camel case',
+    author='Daniel Eckermann',
+    author_email='danieleckermann@gmail.com',
+    url='http://danieleckermann.com',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
