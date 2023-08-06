bc3ee08c29b2b86d6d0881b4a3544c1e5afbf9c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:19 +0000

    Add package anyconfig/0.9.1/anyconfig-0.9.1.tar.gz

---
+++ packages/anyconfig/anyconfig-0.9.1.tar.gz/anyconfig-0.9.1/tests/query.py
@@ -0,0 +1,45 @@
+#
+# Copyright (C) 2017 Satoru SATOH <ssato at redhat.com>
+# License: MIT
+#
+# pylint: disable=missing-docstring, invalid-name
+from __future__ import absolute_import
+
+import unittest
+import anyconfig.query as TT
+
+from tests.common import dicts_equal
+
+
+class Test_00_Functions(unittest.TestCase):
+
+    def _assert_dicts_equal(self, dic, ref):
+        self.assertTrue(dicts_equal(dic, ref), "%r\nvs.\n%r" % (dic, ref))
+
+    def test_10_query(self):
+        try:
+            if TT.jmespath:
+                self.assertEquals(TT.query({"a": 1}, ac_query="a"), 1)
+                self.assertEquals(TT.query({"a": {"b": 2}}, ac_query="a.b"),
+                                  2)
+        except (NameError, AttributeError):
+            pass
+
+    def test_12_invalid_query(self):
+        data = {"a": 1}
+        try:
+            if TT.jmespath:
+                self._assert_dicts_equal(TT.query(data, ac_query="b."), data)
+        except (NameError, AttributeError):
+            pass
+
+    def test_14_empty_query(self):
+        data = {"a": 1}
+        try:
+            if TT.jmespath:
+                self._assert_dicts_equal(TT.query(data, ac_query=None), data)
+                self._assert_dicts_equal(TT.query(data, ac_query=''), data)
+        except (NameError, AttributeError):
+            pass
+
+# vim:sw=4:ts=4:et:
