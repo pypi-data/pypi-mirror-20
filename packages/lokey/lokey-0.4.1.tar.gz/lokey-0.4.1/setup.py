17c9326ef02b10985b099e9b190cf3d3fdb14f8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:10 +0000

    Add package lokey/0.4.1/lokey-0.4.1.tar.gz

---
+++ packages/lokey/lokey-0.4.1.tar.gz/lokey-0.4.1/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+from setuptools import find_packages
+setup(
+    name = 'lokey',
+    packages = find_packages(),
+    version = '0.4.1',
+    description = 'A tool to convert between different cryptographic key formats',
+    author = 'Joel Franusic',
+    author_email = 'jfranusic@gmail.com',
+    url = 'https://github.com/jpf/lokey',
+    download_url = 'https://github.com/jpf/lokey/archive/0.4.0.tar.gz',
+    keywords = ['rsa', 'ssh', 'pgp', 'x509', 'jwk'], 
+    classifiers = [],
+    install_requires = ['click', 'cryptography', 'requests', 'python-hkp', 'pgpy', 'paramiko'],
+    entry_points = '''
+        [console_scripts]
+        lokey=lokey:cli
+    '''
+    
+)
