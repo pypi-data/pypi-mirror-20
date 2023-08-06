b0243fc37470096f405566f8b7c13f257530e09f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:45 +0000

    Add package origin/1.0.2/origin-1.0.2.tar.gz

---
+++ packages/origin/origin-1.0.2.tar.gz/origin-1.0.2/origin/publisher/null.py
@@ -0,0 +1,11 @@
+from origin.publisher.base import BaseEventPublisher
+
+
+class NullEventPublisher(BaseEventPublisher):
+    """An implementation of the :class:`BaseEventPublisher`
+    interface that does not transmit events. Use for
+    mocking and prototyping purposes.
+    """
+
+    def publish(self, *args, **kwargs):
+        pass
