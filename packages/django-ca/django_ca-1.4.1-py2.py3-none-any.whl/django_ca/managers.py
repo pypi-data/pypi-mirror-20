6267a7e375dfc3e95ae475efe84a9de093f2677a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:36 +0000

    Add package django-ca/1.4.1/django_ca-1.4.1-py2.py3-none-any.whl

---
+++ packages/django-ca/django_ca-1.4.1-py2.py3-none-any.whl/django_ca/managers.py
@@ -0,0 +1,247 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of django-ca (https://github.com/mathiasertl/django-ca).
+#
+# django-ca is free software: you can redistribute it and/or modify it under the terms of the GNU
+# General Public License as published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# django-ca is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with django-ca.  If not,
+# see <http://www.gnu.org/licenses/>.
+
+import os
+import re
+
+from OpenSSL import crypto
+
+from django.db import models
+from django.utils.encoding import force_bytes
+
+from . import ca_settings
+from .utils import SAN_OPTIONS_RE
+from .utils import get_basic_cert
+from .utils import get_subjectAltName
+from .utils import is_power2
+from .utils import sort_subject_dict
+
+
+class CertificateManagerMixin(object):
+    def get_common_extensions(self, issuer_url=None, crl_url=None, ocsp_url=None):
+        extensions = []
+
+        # Set CRL distribution points:
+        if crl_url:
+            if isinstance(crl_url, str):
+                crl_url = [url.strip() for url in crl_url.split()]
+            value = force_bytes(','.join(['URI:%s' % uri for uri in crl_url]))
+            extensions.append(crypto.X509Extension(b'crlDistributionPoints', 0, value))
+
+        auth_info_access = []
+        if ocsp_url:
+            auth_info_access.append('OCSP;URI:%s' % ocsp_url)
+        if issuer_url:
+            auth_info_access.append('caIssuers;URI:%s' % issuer_url)
+        if auth_info_access:
+            auth_info_access = force_bytes(','.join(auth_info_access))
+            extensions.append(crypto.X509Extension(b'authorityInfoAccess', 0, auth_info_access))
+
+        return extensions
+
+
+class CertificateAuthorityManager(CertificateManagerMixin, models.Manager):
+    def init(self, name, key_size, key_type, algorithm, expires, parent, pathlen, subject,
+             issuer_url=None, issuer_alt_name=None, crl_url=None, ocsp_url=None,
+             ca_issuer_url=None, ca_crl_url=None, ca_ocsp_url=None,
+             name_constraints=None, password=None):
+        """Create a Certificate Authority."""
+
+        # NOTE: This is already verified by KeySizeAction, so none of these checks should ever be
+        #       True in the real world. None the less they are here as a safety precaution.
+        if not is_power2(key_size):
+            raise RuntimeError("%s: Key size must be a power of two." % key_size)
+        elif key_size < ca_settings.CA_MIN_KEY_SIZE:
+            raise RuntimeError("%s: Key size must be least %s bits."
+                               % (key_size, ca_settings.CA_MIN_KEY_SIZE))
+
+        private_key = crypto.PKey()
+        private_key.generate_key(getattr(crypto, 'TYPE_%s' % key_type), key_size)
+
+        # set basic properties
+        cert = get_basic_cert(expires)
+        for key, value in sort_subject_dict(subject):
+            setattr(cert.get_subject(), key, force_bytes(value))
+        cert.set_pubkey(private_key)
+
+        basicConstraints = 'CA:TRUE'
+        if pathlen is not False:
+            basicConstraints += ', pathlen:%s' % pathlen
+
+        cert.add_extensions([
+            crypto.X509Extension(b'basicConstraints', True, basicConstraints.encode('utf-8')),
+            crypto.X509Extension(b'keyUsage', 0, b'keyCertSign,cRLSign'),
+            crypto.X509Extension(b'subjectKeyIdentifier', False, b'hash', subject=cert),
+        ])
+
+        extensions = self.get_common_extensions(ca_issuer_url, ca_crl_url, ca_ocsp_url)
+
+        if name_constraints:
+            name_constraints = ','.join(name_constraints).encode('utf-8')
+            extensions.append(crypto.X509Extension(b'nameConstraints', True, name_constraints))
+
+        if parent is None:
+            cert.set_issuer(cert.get_subject())
+            extensions.append(crypto.X509Extension(b'authorityKeyIdentifier', False,
+                                                   b'keyid:always', issuer=cert))
+        else:
+            cert.set_issuer(parent.x509.get_subject())
+            extensions.append(crypto.X509Extension(b'authorityKeyIdentifier', False,
+                                                   b'keyid,issuer', issuer=parent.x509))
+        cert.add_extensions(extensions)
+
+        # sign the certificate
+        if parent is None:
+            cert.sign(private_key, algorithm)
+        else:
+            cert.sign(parent.key, algorithm)
+
+        if crl_url is not None:
+            crl_url = '\n'.join(crl_url)
+
+        # create certificate in database
+        ca = self.model(name=name, issuer_url=issuer_url, issuer_alt_name=issuer_alt_name,
+                        ocsp_url=ocsp_url, crl_url=crl_url, parent=parent)
+        ca.x509 = cert
+        ca.private_key_path = os.path.join(ca_settings.CA_DIR, '%s.key' % ca.serial)
+        ca.save()
+
+        dump_args = []
+        if password is not None:  # pragma: no cover
+            dump_args = ['des3', password]
+
+        # write private key to file
+        oldmask = os.umask(247)
+        with open(ca.private_key_path, 'w') as key_file:
+            key = crypto.dump_privatekey(crypto.FILETYPE_PEM, private_key, *dump_args)
+            key_file.write(key.decode('utf-8'))
+        os.umask(oldmask)
+
+        return ca
+
+
+class CertificateManager(CertificateManagerMixin, models.Manager):
+    def init(self, ca, csr, expires, algorithm, subject=None, cn_in_san=True,
+             csr_format=crypto.FILETYPE_PEM, subjectAltName=None, keyUsage=None,
+             extendedKeyUsage=None, tlsfeature=None):
+        """Create a signed certificate from a CSR.
+
+        X509 extensions (`key_usage`, `ext_key_usage`) may either be None (in which case they are
+        not added) or a tuple with the first value being a bool indicating if the value is critical
+        and the second value being a byte-array indicating the extension value. Example::
+
+            (True, b'value')
+
+        Parameters
+        ----------
+
+        ca : django_ca.models.CertificateAuthority
+            The certificate authority to sign the certificate with.
+        csr : str
+            A valid CSR in PEM format. If none is given, `self.csr` will be used.
+        expires : int
+            When the certificate should expire (passed to :py:func:`get_basic_cert`).
+        algorithm : {'sha512', 'sha256', ...}
+            Algorithm used to sign the certificate. The default is the CA_DIGEST_ALGORITHM setting.
+        subject : dict, optional
+            The Subject to use in the certificate.  The keys of this dict are the fields of an X509
+            subject, that is `"C"`, `"ST"`, `"L"`, `"OU"` and `"CN"`. If ommited or if the value
+            does not contain a `"CN"` key, the first value of the `subjectAltName` parameter is
+            used as CommonName (and is obviously mandatory in this case).
+        cn_in_san : bool, optional
+            Wether the CommonName should also be included as subjectAlternativeName. The default is
+            `True`, but the parameter is ignored if no CommonName is given. This is typically set
+            to `False` when creating a client certificate, where the subjects CommonName has no
+            meaningful value as subjectAltName.
+        csr_format : int, optional
+            The format of the submitted CSR request. One of the OpenSSL.crypto.FILETYPE_*
+            constants. The default is PEM.
+        subjectAltName : list of str, optional
+            A list of values for the subjectAltName extension. Values are passed to
+            `get_subjectAltName`, see function documentation for how this value is parsed.
+        keyUsage : tuple or None
+            Value for the `keyUsage` X509 extension. See description for format details.
+        extendedKeyUsage : tuple or None
+            Value for the `extendedKeyUsage` X509 extension. See description for format details.
+        tlsfeature : tuple or None
+            Value for the `tlsfeature` extension. See description for format details.
+
+        Returns
+        -------
+
+        OpenSSL.crypto.X509
+            The signed certificate.
+        """
+        if subject is None:
+            subject = {}
+        if not subject.get('CN') and not subjectAltName:
+            raise ValueError("Must at least cn or subjectAltName parameter.")
+
+        req = crypto.load_certificate_request(csr_format, csr)
+
+        # Process CommonName and subjectAltName extension.
+        if subject.get('CN') is None:
+            subject['CN'] = re.sub('^%s' % SAN_OPTIONS_RE, '', subjectAltName[0])
+            subjectAltName = get_subjectAltName(subjectAltName)
+        elif cn_in_san is True:
+            if subjectAltName:
+                subjectAltName = get_subjectAltName(subjectAltName, cn=subject['CN'])
+            else:
+                subjectAltName = get_subjectAltName([subject['CN']])
+
+        # subjectAltName might still be None, in which case the extension is not added.
+        elif subjectAltName:
+            subjectAltName = get_subjectAltName(subjectAltName)
+
+        # Create signed certificate
+        cert = get_basic_cert(expires)
+        cert.set_issuer(ca.x509.get_subject())
+        for key, value in sort_subject_dict(subject):
+            setattr(cert.get_subject(), key, force_bytes(value))
+        cert.set_pubkey(req.get_pubkey())
+
+        extensions = self.get_common_extensions(ca.issuer_url, ca.crl_url, ca.ocsp_url)
+        extensions += [
+            crypto.X509Extension(b'subjectKeyIdentifier', 0, b'hash', subject=cert),
+            crypto.X509Extension(b'authorityKeyIdentifier', 0, b'keyid,issuer', issuer=ca.x509),
+            crypto.X509Extension(b'basicConstraints', True, b'CA:FALSE'),
+        ]
+
+        if keyUsage is not None:
+            extensions.append(crypto.X509Extension(b'keyUsage', *keyUsage))
+        if extendedKeyUsage is not None:
+            extensions.append(crypto.X509Extension(b'extendedKeyUsage', *extendedKeyUsage))
+
+        if tlsfeature is not None:  # pragma: no cover
+            extensions.append(crypto.X509Extension(b'tlsFeature', *tlsfeature))
+
+        # Add subjectAltNames, always also contains the CommonName
+        if subjectAltName:
+            extensions.append(crypto.X509Extension(b'subjectAltName', 0, subjectAltName))
+
+        # Add issuerAltName
+        if ca.issuer_alt_name:
+            issuerAltName = force_bytes('URI:%s' % ca.issuer_alt_name)
+        else:
+            issuerAltName = b'issuer:copy'
+        extensions.append(crypto.X509Extension(b'issuerAltName', 0, issuerAltName, issuer=ca.x509))
+
+        # Add collected extensions
+        cert.add_extensions(extensions)
+
+        # Finally sign the certificate:
+        cert.sign(ca.key, str(algorithm))  # str() to force py2 unicode to str
+
+        return cert
---
+++ packages/django-ca/django_ca-1.4.1-py2.py3-none-any.whl/django_ca/utils.py
@@ -0,0 +1,304 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of django-ca (https://github.com/mathiasertl/django-ca).
+#
+# django-ca is free software: you can redistribute it and/or modify it under the terms of the GNU
+# General Public License as published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# django-ca is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with django-ca.  If not,
+# see <http://www.gnu.org/licenses/>.
+
+"""Central functions to load CA key and cert as PKey/X509 objects."""
+
+import re
+import uuid
+from copy import deepcopy
+from datetime import datetime
+from ipaddress import ip_address
+
+from cryptography import x509
+from cryptography.x509.oid import NameOID
+from OpenSSL import crypto
+
+from django.core.serializers.json import DjangoJSONEncoder
+from django.core.validators import URLValidator
+from django.utils.encoding import force_bytes
+from django.utils.encoding import force_text
+from django.utils.functional import Promise
+from django.utils.translation import ugettext_lazy as _
+
+from django_ca import ca_settings
+
+# List of possible subject fields, in order
+SUBJECT_FIELDS = ['C', 'ST', 'L', 'O', 'OU', 'CN', 'emailAddress', ]
+
+# Description strings for various X509 extensions, taken from "man x509v3_config".
+EXTENDED_KEY_USAGE_DESC = _('Purposes for which the certificate public key can be used for.')
+KEY_USAGE_DESC = _('Permitted key usages.')
+SAN_OPTIONS_RE = '(email|URI|IP|DNS|RID|dirName|otherName):'
+_datetime_format = '%Y%m%d%H%M%SZ'
+
+SAN_NAME_MAPPINGS = {
+    x509.DNSName: 'DNS',
+    x509.RFC822Name: 'email',
+    x509.DirectoryName: 'dirname',
+    x509.UniformResourceIdentifier: 'URI',
+    x509.IPAddress: 'IP',
+    x509.RegisteredID: 'RID',
+    x509.OtherName: 'otherName',
+}
+
+OID_NAME_MAPPINGS = {
+    NameOID.COUNTRY_NAME: 'C',
+    NameOID.STATE_OR_PROVINCE_NAME: 'ST',
+    NameOID.LOCALITY_NAME: 'L',
+    NameOID.ORGANIZATION_NAME: 'O',
+    NameOID.ORGANIZATIONAL_UNIT_NAME: 'OU',
+    NameOID.COMMON_NAME: 'CN',
+    NameOID.EMAIL_ADDRESS: 'emailAddress',
+}
+
+
+class LazyEncoder(DjangoJSONEncoder):
+    """Encoder that also encodes strings translated with ugettext_lazy."""
+
+    def default(self, obj):
+        if isinstance(obj, Promise):
+            return force_text(obj)
+        return super(LazyEncoder, self).default(obj)
+
+
+def format_date(date):
+    """Format date as ASN1 GENERALIZEDTIME, as required by various fields."""
+    return date.strftime(_datetime_format)
+
+
+def parse_subject(raw):
+    """Parses a subject string as used in OpenSSLs command line utilities.
+
+    Examples of where this string is used are:
+
+    .. code-block:: console
+
+        # openssl req -new -key priv.key -out csr -utf8 -batch -sha256 -subj '/C=AT/CN=example.com'
+        # openssl x509 -in cert.pem -noout -subject -nameopt compat
+        /C=AT/L=Vienna/CN=example.com
+
+    .. NOTE:: This isn't a terribly smart format, it doesn't account for any escaping of special
+        characters, doesn't account for duplicate attribute fields (e.g. two ``C`` attributes) and
+        happily outputs completely broken data if any subject field happens to ambiguous. For
+        exampe, consider a certificate where the subject has ``C=US`` and ``OU=example.com/C=AT``.
+        OpenSSL will happily outpu
+
+        .. code-block:: console
+
+            # openssl x509 -in cert.pem -noout -subject -nameopt compat
+            /C=US/OU=example.com/C=AT
+
+        .. which is of course not meaningful.
+
+    The function tries to be forgiving to user input, in particular it
+
+    * strips any leading or trailing spaces anywhere (e.g. ``" / CN = example.com /..."``
+    * ignores case in fields (e.g. ``"OU"`` is the same as ``"ou"``
+    * throws an error on duplicate or unknown field names
+    * order of the given fields is ignored
+
+    Examples of how to use this function::
+
+        >>> parse_subject('')
+        {}
+        >>> parse_subject('/CN=example.com')
+        {'CN': 'example.com'}
+        >>> parse_subject(' / CN  = example.com    ')
+        {'CN': 'example.com'}
+        >>> parse_subject('/eMAILadreSs=user@example.com')
+        {'emailAddress: 'user@example.com'}
+
+    """
+    raw = raw.strip()
+    if not raw:  # empty subjects are ok
+        return {}
+    if not raw.startswith('/'):
+        raise ValueError('Unparseable subject: Does not start with a "/".')
+
+    # find all subject elements
+    matches = re.findall('/\s*([^=/]+)=([^/]+)?', raw)
+
+    # remove any spaces on beginning/end
+    matches = [(k.strip(), v.strip()) for k, v in matches]
+
+    subject = {}
+    for k, v in matches:
+        if k.lower() == 'emailaddress':
+            key = 'emailAddress'
+        elif k.upper() in SUBJECT_FIELDS:
+            key = k.upper()
+        else:
+            raise ValueError('Unparseable subject: Unknown field "%s".' % k)
+
+        if key in subject:
+            raise ValueError('Unparseable subject: Duplicate field "%s".' % key)
+
+        subject[key] = v
+
+    return subject
+
+
+def sort_subject_dict(d):
+    """Returns an itemized dictionary in the correct order for a x509 subject."""
+    return sorted(d.items(), key=lambda e: SUBJECT_FIELDS.index(e[0]))
+
+
+def format_subject(subject):
+    """Convert a subject into the canonical form for distinguished names.
+
+    Examples::
+
+        >>> format_subject(ca.x509.get_subject())
+        '/CN=example.com'
+        >>> format_subject([('CN', 'example.com'), ])
+        '/CN=example.com'
+        >>> format_subject({'CN': 'example.com'})
+        '/CN=example.com'
+
+    """
+    if isinstance(subject, crypto.X509Name):
+        subject = subject.get_components()
+    if isinstance(subject, dict):
+        subject = sort_subject_dict(subject)
+    return '/%s' % ('/'.join(['%s=%s' % (force_text(k), force_text(v)) for k, v in subject]))
+
+
+def format_general_names(names):
+    """Format a list of general names."""
+
+    return ', '.join(['%s:%s' % (SAN_NAME_MAPPINGS[type(s)], s.value) for s in names])
+
+
+def is_power2(num):
+    """Return True if num is a power of 2."""
+    return num != 0 and ((num & (num - 1)) == 0)
+
+
+def multiline_url_validator(value):
+    """Validate that a TextField contains one valid URL per line.
+
+    .. seealso:: https://docs.djangoproject.com/en/1.9/ref/validators/
+    """
+    validator = URLValidator()
+
+    for line in value.splitlines():
+        validator(line)
+
+
+def add_colons(s):
+    return ':'.join(a + b for a, b in zip(s[::2], s[1::2]))
+
+
+def serial_from_int(i):
+    s = hex(i)[2:].upper()
+    return add_colons(s)
+
+
+def get_basic_cert(expires, now=None):
+    """Get a basic X509 cert object.
+
+    Parameters
+    ----------
+
+    expires : datetime
+        When this certificate will expire.
+    """
+    if now is None:
+        now = datetime.utcnow()
+    now = now.replace(second=0, microsecond=0)
+
+    if expires < now:
+        raise ValueError("Expires must not be negative.")
+
+    not_before = format_date(now).encode('utf-8')
+    not_after = format_date(expires).encode('utf-8')
+
+    cert = crypto.X509()
+    cert.set_version(2)  # V3 certificate
+    cert.set_serial_number(uuid.uuid4().int)
+    cert.set_notBefore(not_before)
+    cert.set_notAfter(not_after)
+    return cert
+
+
+def get_cert_profile_kwargs(name=None):
+    """Get kwargs suitable for get_cert X509 keyword arguments from the given profile."""
+
+    if name is None:
+        name = ca_settings.CA_DEFAULT_PROFILE
+
+    profile = deepcopy(ca_settings.CA_PROFILES[name])
+    kwargs = {
+        'cn_in_san': profile['cn_in_san'],
+        'subject': profile['subject'],
+    }
+    for arg in ['keyUsage', 'extendedKeyUsage']:
+        config = profile.get(arg)
+        if config is None or not config.get('value'):
+            continue
+
+        critical = config.get('critical', 'True')
+        if isinstance(config['value'], str):
+            kwargs[arg] = (critical, force_bytes(config['value']))
+        elif isinstance(config['value'], bytes):
+            kwargs[arg] = (critical, config['value'])
+        else:
+            kwargs[arg] = (critical, force_bytes(','.join(config['value'])))
+    return kwargs
+
+
+def get_subjectAltName(names, cn=None):
+    """Compute the value of the subjectAltName extension based on the given list of names.
+
+    The `cn` parameter, if provided, is prepended if not present in the list of names.
+
+    This method supports the `IP`, `email`, `URI` and `DNS` options automatically, if you need a
+    different option (or think the automatic parsing is wrong), give the full value verbatim (e.g.
+    `otherName:1.2.3.4;UTF8:some other identifier`.
+    """
+    values = []
+    names = sorted(set(names))
+
+    for name in names:
+        if not name:
+            continue
+        if isinstance(name, bytes):
+            name = name.decode('utf-8')
+
+        # Match any known literal values
+        if re.match(SAN_OPTIONS_RE, name):
+            values.append(name)
+            continue
+
+        try:
+            ip_address(name)
+            values.append('IP:%s' % name)
+            continue
+        except ValueError:
+            pass
+
+        if re.match('[a-z0-9]{2,}://', name):
+            values.append('URI:%s' % name)
+        elif '@' in name:
+            values.append('email:%s' % name)
+        else:
+            values.append('DNS:%s' % name)
+
+    if cn is not None:
+        value = 'DNS:%s' % cn
+        if value not in values:
+            values.insert(0, value)
+
+    return force_bytes(','.join(values))
