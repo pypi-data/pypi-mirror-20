db487d489eb4185f6e97b2939f01e880dc75aefa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:12 +0000

    Add package pandoc-img-glob/0.1.0/pandoc-img-glob-0.1.0.tar.gz

---
+++ packages/pandoc-img-glob/pandoc-img-glob-0.1.0.tar.gz/pandoc-img-glob-0.1.0/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+import re
+
+from pandoc_img_glob import __version__
+
+REPOSITORY = 'https://github.com/shoeffner/pandoc-img-glob'
+
+README = ''
+with open('README.rst', 'r') as f:
+    README = f.read()
+README = re.sub(r' _(.+): ([^(http)].+)', r' _\1: {}/blob/master/\2'
+                .format(REPOSITORY), README)
+
+setup(
+  name='pandoc-img-glob',
+  version=__version__,
+  description='',
+  long_description=README,
+  entry_points={'console_scripts': ['pandoc-img-glob = pandoc_img_glob:main']},
+  author='Sebastian Höffner',
+  author_email='info@sebastian-hoeffner.de',
+  url=REPOSITORY,
+  download_url='{}/tarball/{}'.format(REPOSITORY, __version__),
+  packages=['pandoc_img_glob'],
+  classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Science/Research',
+      'License :: OSI Approved :: MIT License',
+      'Natural Language :: English',
+      'Programming Language :: Python :: 3.6',
+      'Topic :: Text Processing :: Filters',
+  ],
+  install_requires=['panflute'],
+  license='MIT',
+  keywords=['pandoc', 'image', 'multifile', 'filter'],
+)
