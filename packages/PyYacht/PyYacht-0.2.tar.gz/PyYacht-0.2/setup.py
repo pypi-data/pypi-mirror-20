0419150d71552f4f49096715c8472aaa6d96f3ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:44 +0000

    Add package PyYacht/0.2/PyYacht-0.2.tar.gz

---
+++ packages/PyYacht/PyYacht-0.2.tar.gz/PyYacht-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='PyYacht',
+      version='0.2',
+      description='Yacht race starter',
+      url='https://github.com/HUg0005/PyYacht',
+      author='Hayden Hughes',
+      author_email='mrhaydenhughes@gmail.com',
+      license='MIT',
+      scripts=['bin/PyStart.py'],
+      zip_safe=False)
