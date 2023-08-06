d7cfcd9fda9401c5c59c4381bc0c003892bb404e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:51 +0000

    Add package xiyuhaochi/1.1.0/xiyuhaochi-1.1.0.tar.gz

---
+++ packages/xiyuhaochi/xiyuhaochi-1.1.0.tar.gz/xiyuhaochi-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+      name = 'xiyuhaochi',
+      version ='1.1.0',
+      py_modules=['xiyuhaochi'],
+      author = 'xiyu',
+      author_email ='wjiang@ips.ac.cn',
+      url = 'http://sina.com',
+      description ='funny',
+      )
