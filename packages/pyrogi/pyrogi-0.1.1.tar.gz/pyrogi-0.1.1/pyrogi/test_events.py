93b6cea16b6555ebf6e8228ae8f81c5339862bb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:07 +0000

    Add package pyrogi/0.1.1/pyrogi-0.1.1.tar.gz

---
+++ packages/pyrogi/pyrogi-0.1.1.tar.gz/pyrogi-0.1.1/pyrogi/test_events.py
@@ -0,0 +1,21 @@
+import unittest
+import pyrogi
+from pyrogi.util.vector import Vec2
+from pyrogi.events import pixel_position_to_tile_position
+
+class TestPixelToTileConversion(unittest.TestCase):
+    def setUp(self):
+        pyrogi.tile_dimensions = Vec2(2, 5)
+
+    def test_pixel_position_to_tile_position(self):
+        self.assertEqual(pixel_position_to_tile_position(Vec2(0, 0)), Vec2(0, 0))
+        self.assertEqual(pixel_position_to_tile_position(Vec2(1, 0)), Vec2(0, 0))
+        self.assertEqual(pixel_position_to_tile_position(Vec2(1, 1)), Vec2(0, 0))
+        self.assertEqual(pixel_position_to_tile_position(Vec2(1.99, 4.99)), Vec2(0, 0))
+
+        self.assertEqual(pixel_position_to_tile_position(Vec2(2, 5)), Vec2(1, 1))
+        self.assertEqual(pixel_position_to_tile_position(Vec2(2.1, 5.1)), Vec2(1, 1))
+        self.assertEqual(pixel_position_to_tile_position(Vec2(3.999, 9.999)), Vec2(1, 1))
+
+        self.assertEqual(pixel_position_to_tile_position(Vec2(30, 3)), Vec2(15, 0))
+        self.assertEqual(pixel_position_to_tile_position(Vec2(5, 17)), Vec2(2, 3))
