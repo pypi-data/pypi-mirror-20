c8b63a990cc5ab5e88fee74e75a8c8b596513463
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:15 +0000

    Add package spiderframework/1.1/spiderframework-1.1.tar.gz

---
+++ packages/spiderframework/spiderframework-1.1.tar.gz/spiderframework-1.1/BaseGHCallback.py
@@ -0,0 +1,38 @@
+#coding:utf-8
+import sys
+sys.path.append("../..")
+import HttpRequest
+import TCPRequest
+'''
+BaseGHCallback is a craw framework.
+You can create a file with the name 'data', which stores raw data
+
+>>> import BaseGHCallback
+class Demo(BaseGHCallback):
+    def api(self):
+        return <api>
+    def response(self,res):
+        res.headers res.text.encode('utf-8')
+BaseGHCallback(Demo).request()
+'''
+class BaseGHCallback(object):
+    call = None
+    def __init__(self,Callback):
+        self.call = Callback
+    '''
+        return request`s api
+    '''
+    def api(self):
+        pass
+    '''
+        Returns the result set of the request
+    '''
+    def response(self,res):
+        pass
+    '''
+        BaseGHCallback(Demo).request()
+    '''
+    def httpRequest(self):
+        HttpRequest.ghNetWork(self.call)
+    def tcpRequest(self):
+        TCPRequest.tcpRequest(self.call)
