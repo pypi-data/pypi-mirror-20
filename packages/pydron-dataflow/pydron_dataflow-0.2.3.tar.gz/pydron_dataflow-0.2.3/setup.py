faa20ae0de10be6fb99f6ef03c7f76982e47c1ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:33 +0000

    Add package pydron-dataflow/0.2.3/pydron_dataflow-0.2.3.tar.gz

---
+++ packages/pydron-dataflow/pydron_dataflow-0.2.3.tar.gz/pydron_dataflow-0.2.3/setup.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2014 Stefan C. Mueller
+
+import os.path
+from setuptools import setup, find_packages
+
+if os.path.exists('README.rst'):
+    with open('README.rst') as f:
+        long_description = f.read()
+else:
+    long_description = None
+    
+setup(
+    name = 'pydron_dataflow',
+    version = '0.2.3',
+    description='Dataflow framework to define and traverse directional graphs.',
+    long_description=long_description,
+    author='Ivo Nussbaumer',
+    author_email='ivo.nussbaumer@fhnw.ch',
+    url='https://github.com/i4Ds/pydron-dataflow',
+    packages = find_packages(),
+    install_requires = ['astor>=0.4', 
+                        'enum34>=1.0.4', 
+                        'frozendict>=0.4',
+                        'twisted_sshtools>=2.2.3',
+                        'sortedcontainers>=0.9.5']
+)
