2f684d783793c3def4d3fdc23b569fa067a7819e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:31 +0000

    Add package trakt.py/2.14.1/trakt.py-2.14.1.tar.gz

---
+++ packages/trakt.py/trakt.py-2.14.1.tar.gz/trakt.py-2.14.1/docs/sourcecode/trakt/trakt.interfaces.shows.rst
@@ -0,0 +1,9 @@
+======================
+:code:`Trakt['shows']`
+======================
+
+.. automodule:: trakt.interfaces.shows
+
+  .. autoclass:: ShowsInterface
+    :members: episode, get, last_episode, next_episode, season, seasons, trending
+    :undoc-members:
---
+++ packages/trakt.py/trakt.py-2.14.1.tar.gz/trakt.py-2.14.1/tests/test_http.py
@@ -0,0 +1,30 @@
+from __future__ import absolute_import, division, print_function
+
+from tests.core import mock
+from trakt import Trakt
+
+from httmock import HTTMock
+
+
+def test_shows_updates():
+    with HTTMock(mock.fixtures, mock.unknown):
+        response = Trakt.http.get('/shows/updates')
+
+    items = response.json()
+
+    assert [item['show']['title'] for item in items] == [
+        'The Pop Game',
+        'The Pop Game',
+        'Abandoned Engineering'
+    ]
+
+
+def test_show_progress_collection():
+    # TODO Add missing fixture
+    with HTTMock(mock.fixtures, mock.unknown):
+        response = Trakt.http.get('/shows/tt0944947/progress/collection')
+
+    data = response.json()
+
+    assert data['aired'] == 10
+    assert data['completed'] == 6
