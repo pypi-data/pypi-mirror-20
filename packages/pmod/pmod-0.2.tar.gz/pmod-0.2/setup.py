855465f9559657f9e0a8ec35b9f5768147508b6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:46 +0000

    Add package pmod/0.2/pmod-0.2.tar.gz

---
+++ packages/pmod/pmod-0.2.tar.gz/pmod-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='pmod',
+      version='0.2',
+      description='A personal package for personal use. Use for anyone is allowed.',
+      author='InTheShadows',
+      author_email='paolocastro128@gmail.com',
+      packages=['pmod'],
+      url='https://github.com/ShadowPerson/pmod',
+      zip_sage=False,
+      install_requires=['winshell']
+    
+    )
