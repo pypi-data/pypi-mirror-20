c00a5d070a50a97b0243e6c2b73eecb6272e5604
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:45 +0000

    Add package ADLES/0.7.4/ADLES-0.7.4-py3.4.egg

---
+++ packages/ADLES/ADLES-0.7.4-py3.4.egg/EGG-INFO/scripts/vsphere_info.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Query information about a vSphere environment and objects within it.
+
+Usage:
+    vsphere_info.py [options]
+    vsphere_info.py --version
+    vsphere_info.py (-h | --help)
+
+Options:
+    -h, --help          Prints this page
+    --version           Prints current version
+    --no-color          Do not color terminal output
+    -v, --verbose       Emit debugging logs to terminal
+    -f, --file FILE     Name of JSON file with server connection information
+
+"""
+
+import logging
+
+from docopt import docopt
+
+from adles.vsphere import vm_utils, vsphere_utils
+from adles.utils import script_setup, name_or_path
+from adles.vsphere.folder_utils import enumerate_folder, format_structure
+
+__version__ = "0.4.3"
+
+
+def main():
+    args = docopt(__doc__, version=__version__, help=True)
+    server = script_setup('vsphere_info.log', args, (__file__, __version__))
+
+    # List of possible useful things
+    #   open_console
+    #   upload_file
+    #   get_status    Status of the overall environment (what VMs are on/off, etc.)
+
+    thing_type = str(input("What type of thing do you want to get information on?"
+                           " (vm | datastore | vsphere | folder) "))
+
+    if thing_type == "vm":
+        vm, vm_name = name_or_path(server, "vm", "you want to get information on")
+        logging.info(vm_utils.get_vm_info(vm, detailed=True, uuids=True, snapshot=True))
+
+    elif thing_type == "datastore":
+        ds = server.get_datastore(str(input("What is the name of the Datastore you want "
+                                            "to get information on? (default: first datastore found")))
+        logging.info(vsphere_utils.get_datastore_info(ds))
+
+    elif thing_type == "vsphere":
+        logging.info("%s", str(server))
+
+    elif thing_type == "folder":
+        folder, folder_name = name_or_path(server, "folder")
+        logging.info("Folder: %s\n%s", folder_name,
+                     format_structure(enumerate_folder(folder)))
+
+    else:
+        logging.info("Invalid type: %s", thing_type)
+
+
+if __name__ == '__main__':
+    main()
