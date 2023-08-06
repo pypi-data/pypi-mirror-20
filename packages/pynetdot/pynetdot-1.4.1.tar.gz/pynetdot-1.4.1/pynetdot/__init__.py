417d1f3642fc27e4bd77c80bb111cfc9c0f27190
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:23 +0000

    Add package pynetdot/1.4.1/pynetdot-1.4.1.tar.gz

---
+++ packages/pynetdot/pynetdot-1.4.1.tar.gz/pynetdot-1.4.1/pynetdot/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '1.4.1'
+
+from .netdot import setup, load_settings
+from .models import *
+
+settings = load_settings()
+setup(url=settings['url'], username=settings['username'], password=settings['password'], kerberos=settings['kerberos'])
---
+++ packages/pynetdot/pynetdot-1.4.1.tar.gz/pynetdot-1.4.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+long_description = ''
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    l = f.readline()
+    while not l.startswith('*'):
+        long_description += l
+        l = f.readline()
+
+setup(
+    name='pynetdot',
+    version = '1.4.1',
+
+    description='Python client for netdot REST API',
+    long_description=long_description,
+    url='https://github.com/ArnesSI/pynetdot',
+    author='Matej Vadnjal',
+    author_email='matej.vadnjal@arnes.si',
+    license='LGPLv3',
+
+    packages=['pynetdot', 'pynetdot/models'],
+
+    install_requires=['python-dateutil', 'requests', 'PyYAML'],
+    requires=['dateutil', 'requests', 'yaml'],
+
+)
