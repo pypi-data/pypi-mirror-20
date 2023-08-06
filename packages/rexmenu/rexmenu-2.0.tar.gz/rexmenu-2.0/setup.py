e7e9f73bb5aa6f1e48b53528c6ea953d05f8af7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:09 +0000

    Add package rexmenu/2.0/rexmenu-2.0.tar.gz

---
+++ packages/rexmenu/rexmenu-2.0.tar.gz/rexmenu-2.0/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+scripts = ["rexmenu.py"]
+if os.path.exists("/usr/bin/tvservice"):
+    scripts.append("scripts/rpi-screen-blank.py")
+
+with open("README.rst", "r") as fp:
+    long_description = fp.read()
+
+setup(name='rexmenu',
+      version='2.0',
+      description='Simple image tiling program launcher/MAME frontend using Pygame',
+      long_description=long_description,
+      author='Rob McMullen',
+      author_email='feedback@playermissile.com',
+      url='https://playermissile.com',
+      scripts=scripts,
+      license="GPL",
+      install_requires = [
+          'pygame',
+          ],
+     )
