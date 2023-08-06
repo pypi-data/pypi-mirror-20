b329f422cbf57c6fe1e6587a169df1a881e577f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:31 +0000

    Add package electron-inject/0.2/electron-inject-0.2.tar.gz

---
+++ packages/electron-inject/electron-inject-0.2.tar.gz/electron-inject-0.2/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup
+
+def read(fname):
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    except Exception:
+        return "Not available"
+setup(
+    name="electron-inject",
+    version="0.2",
+    packages=["electron_inject"],
+    author="tintinweb",
+    author_email="tintinweb@oststrom.com",
+    description=(
+        "An electron application wrapper that utilizes the remote debug console to inject code into electron applications to enable developer tools"),
+    license="GPLv3",
+    keywords=["electron", "inject", "devtools", "developer tools"],
+    url="https://github.com/tintinweb/electron-inject/",
+    download_url="https://github.com/tintinweb/electron-inject/tarball/v0.2",
+    #python setup.py register -r https://testpypi.python.org/pypi
+    long_description=read("README.rst") if os.path.isfile("README.rst") else read("README.md"),
+    install_requires=['websocket','requests'],
+    package_data={
+                  'electron_inject': ['electron_inject'],
+                  },
+)
