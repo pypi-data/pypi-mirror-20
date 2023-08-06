8fab4b16f30b3807c897be1fedc3525095710f10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:22 +0000

    Add package better-exceptions/0.1.3/better_exceptions-0.1.3.tar.gz

---
+++ packages/better-exceptions/better_exceptions-0.1.3.tar.gz/better_exceptions-0.1.3/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+VERSION = '0.1.3'
+
+setup(
+    name = 'better_exceptions',
+    packages = ['better_exceptions'],
+    version = VERSION,
+    description = 'Pretty and helpful exceptions, automatically',
+    author = 'Josh Junon',
+    author_email = 'josh@junon.me',
+    url = 'https://github.com/qix-/better-exceptions',
+    download_url = 'https://github.com/qix-/better-exceptions/archive/{}.tar.gz'.format(VERSION),
+    keywords = ['pretty', 'better', 'exceptions', 'exception', 'error', 'local', 'debug', 'debugging', 'locals'],
+    classifiers = [],
+)
