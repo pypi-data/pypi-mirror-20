8b774663642b87791549ddd443e3b6a03d8dd2ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:07 +0000

    Add package print_zw/1.0.3/print_zw-1.0.3.zip

---
+++ packages/print_zw/print_zw-1.0.3.zip/print_zw-1.0.3/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='print_zw',
+    version='1.0.3',
+    py_modules=['print_lol'],
+    author='zhouwei',
+    author_email='790333479@qq.com',
+    descripton='print lists',
+)
