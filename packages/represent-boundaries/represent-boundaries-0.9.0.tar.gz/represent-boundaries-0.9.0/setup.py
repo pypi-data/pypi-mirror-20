ec6b5d11358fb3106cc29ae55db6e85d043493e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:44 +0000

    Add package represent-boundaries/0.9.0/represent-boundaries-0.9.0.tar.gz

---
+++ packages/represent-boundaries/represent-boundaries-0.9.0.tar.gz/represent-boundaries-0.9.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+setup(
+    name="represent-boundaries",
+    version="0.9.0",
+    description="A web API to geographic boundaries loaded from shapefiles, packaged as a Django app.",
+    author="Open North Inc.",
+    author_email="represent@opennorth.ca",
+    url="http://represent.poplus.org/",
+    license="MIT",
+    # If packaged as a zip/egg, Django will by default not find static files.
+    zip_safe=False,
+    packages=[
+        'boundaries',
+        'boundaries.management',
+        'boundaries.management.commands',
+        'boundaries.migrations',
+    ],
+    install_requires=[
+        'django-appconf',
+    ],
+    extras_require={
+        'test': 'testfixtures',
+    },
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+        'Framework :: Django',
+        'Topic :: Scientific/Engineering :: GIS',
+    ],
+)
