e929f932b0b3ea219a6e59ea477a877ce5ba61e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:58 +0000

    Add package flixbusapi/0.1/flixbusapi-0.1.tar.gz

---
+++ packages/flixbusapi/flixbusapi-0.1.tar.gz/flixbusapi-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='flixbusapi',
+      version='0.1',
+      description='FlixBus API client for Python',
+      url='https://github.com/zlatkoc/flixbusapi',
+      author='Zlatko Cajic',
+      license='MIT',
+      packages=['flixbusapi'],
+      zip_safe=False)
