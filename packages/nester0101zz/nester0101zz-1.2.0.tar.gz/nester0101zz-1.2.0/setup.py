09ace9e308e736dcf04448c9237f21607ec0c3d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:08 +0000

    Add package nester0101zz/1.2.0/nester0101zz-1.2.0.tar.gz

---
+++ packages/nester0101zz/nester0101zz-1.2.0.tar.gz/nester0101zz-1.2.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester0101zz',
+    version='1.2.0',
+    py_modules = ['nester0101zz'],
+    author='seoyee',
+    author_email='lee.seoyee9011@gmail.com',
+    url = 'http://www.headfirstlabs.com',
+    description='A simple printer of nested lists',
+
+)
