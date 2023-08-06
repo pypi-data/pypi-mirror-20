592b8ba318b76d34d6438aaa1a499718d391cfd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:14 +0000

    Add package rapclust/0.1.2/rapclust-0.1.2.tar.gz

---
+++ packages/rapclust/rapclust-0.1.2.tar.gz/rapclust-0.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='rapclust',
+      version='0.1.2',
+      scripts=['bin/RapClust'],
+      description='Accurate, Fast and Lightweight Clustering of de novo Transcriptomes using Fragment Equivalence Classes',
+      url='https://github.com/COMBINE-lab/RapClust',
+      author='Avi Srivastava, Hirak Sarkar, Laraib Malik, Rob Patro',
+      author_email='rob.patro@cs.stonybrook.edu',
+      license='BSD with attribution',
+      packages=['rapclust'],
+      install_requires=[
+          'PyYAML',
+          'coloredlogs',
+          'click',
+          'networkx',
+          'numpy',
+          'pandas',
+          'tqdm'
+      ],
+      zip_safe=False)
