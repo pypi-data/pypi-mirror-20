c081bd317f72e3fc0a3bc988c17f4ce5dfed6dd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:49 +0000

    Add package jorugi/1.2.0/jorugi-1.2.0.zip

---
+++ packages/jorugi/jorugi-1.2.0.zip/jorugi-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'jorugi',
+    version = '1.2.0',
+    py_modules = ['jorugi'],
+    author = 'hfpython',
+    author_email = 'hfpython@headforstlabs.com',
+    url = 'http://headfirstlabs.com',
+    description = 'A simple printer of nested lists',
+    )
