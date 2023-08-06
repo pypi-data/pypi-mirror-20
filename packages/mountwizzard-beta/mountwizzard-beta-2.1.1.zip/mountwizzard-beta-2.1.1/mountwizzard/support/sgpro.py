6e04c5969788165bc021e4a8f8e383a83c66efa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:48 +0000

    Add package mountwizzard-beta/2.1.1/mountwizzard-beta-2.1.1.zip

---
+++ packages/mountwizzard-beta/mountwizzard-beta-2.1.1.zip/mountwizzard-beta-2.1.1/mountwizzard/support/sgpro.py
@@ -0,0 +1,183 @@
+############################################################
+# -*- coding: utf-8 -*-
+#
+# Python-based Tool for interaction with the 10micron mounts
+# GUI with PyQT5 for python
+# Python  v3.5
+#
+# Michael Würtenberger
+# (c) 2016, 2017
+#
+# Licence APL2.0
+#
+############################################################
+
+import logging
+# packages for handling web interface to SGPro
+from urllib import request
+import json
+
+
+class SGPro:
+    logger = logging.getLogger(__name__)                                         # logging enabling
+
+    def __init__(self):
+        self.ipSGProBase = 'http://localhost:59590'
+        self.ipSGPro = 'http://localhost:59590/json/reply/'
+        self.captureImagePath = 'SgCaptureImage'
+        self.connectDevicePath = 'SgConnectDevicePath'
+        self.disconnectDevicePath = 'SgDisconnectDevicePath'
+        self.getCameraPropsPath = 'SgGetCameraProps'
+        self.getDeviceStatusPath = 'SgGetDeviceStatus'
+        self.enumerateDevicePath = 'SgEnumerateDevices'
+        self.getImagePath = 'SgGetImagePath'
+        self.getSolvedImageDataPath = 'SgGetSolvedImageData'
+        self.solveImagePath = 'SgSolveImage'
+
+    def checkConnection(self):
+        try:
+            reply = request.urlopen(self.ipSGProBase, None, .5).getcode()
+            connected = True
+        except Exception as e:
+            self.logger.error('checkConnection-> error: {0}'.format(e))
+            connected = False
+        finally:
+            if connected:
+                if str(reply) == '200':
+                    success, response = self.SgGetDeviceStatus('Camera')
+                    if success and response != 'DISCONNECTED':
+                        if self.SgGetDeviceStatus('PlateSolver'):
+                            return True, 'Camera and Solver OK'
+                        else:
+                            return False, 'PlateSolver not available !'
+                    else:
+                        return False, 'Camera not available !'
+            return False, 'SGPro Server not running'
+
+    def SgEnumerateDevice(self, device):
+        # reference {"Device": "Camera"}, devices are "Camera", "FilterWheel", "Focuser", "Telescope" and "PlateSolver"}
+        data = {'Device': device}
+        try:
+            req = request.Request(self.ipSGPro + self.enumerateDevicePath, data=bytes(json.dumps(data).encode('utf-8')), method='POST')
+            req.add_header('Content-Type', 'application/json')
+            with request.urlopen(req) as f:
+                captureResponse = json.loads(f.read().decode('utf-8'))
+            # {"Devices":["String"],"Success":false,"Message":"String"}
+            return captureResponse['Devices'], captureResponse['Success'], 'Request OK'
+        except Exception as e:
+            self.logger.error('SgEnumerateDevi-> error: {0}'.format(e))
+            return '', False, 'Request failed'
+
+    def SgCaptureImage(self, binningMode=1, exposureLength=1,
+                       gain=None, iso=None, speed=None, frameType=None, filename=None,
+                       path=None, useSubframe=False, posX=0, posY=0,
+                       width=1, height=1):
+        # reference {"BinningMode":0,"ExposureLength":0,"Gain":"String","Speed":"Normal","FrameType":"Light",
+        # reference "Path":"String","UseSubframe":false,"X":0,"Y":0,"Width":0,"Height":0}
+        data = {"BinningMode": binningMode, "ExposureLength": exposureLength, "UseSubframe": useSubframe, "X": posX, "Y ": posY,
+                "Width": width, "Height": height}
+        if gain:
+            data['Gain'] = gain
+        if iso:
+            data['Iso'] = iso
+        if speed:
+            data['Speed'] = speed
+        if frameType:
+            data['FrameType'] = frameType
+        if path and filename:
+            data['Path'] = path + '/' + filename
+        try:
+            req = request.Request(self.ipSGPro + self.captureImagePath, data=bytes(json.dumps(data).encode('utf-8')), method='POST')
+            req.add_header('Content-Type', 'application/json')
+            with request.urlopen(req) as f:
+                captureResponse = json.loads(f.read().decode('utf-8'))
+            # {"Success":false,"Message":"String","Receipt":"00000000000000000000000000000000"}
+            return captureResponse['Success'], captureResponse['Message'], captureResponse['Receipt']
+        except Exception as e:
+            self.logger.error('SgCaptureImage -> error: {0}'.format(e))
+            return False, 'Request failed', ''
+
+    def SgGetCameraProps(self):
+        # reference {}
+        data = {}
+        try:
+            req = request.Request(self.ipSGPro + self.getCameraPropsPath, data=bytes(json.dumps(data).encode('utf-8')), method='POST')
+            req.add_header('Content-Type', 'application/json')
+            with request.urlopen(req) as f:
+                captureResponse = json.loads(f.read().decode('utf-8'))
+            # {"Success":false,"Message":"String","NumPixelsX":0,"NumPixelsY":0,"SupportsSubframe":false}
+            if 'GainValues' not in captureResponse:
+                captureResponse['GainValues'] = ['High']
+            return captureResponse['Success'], captureResponse['Message'], int(captureResponse['NumPixelsX']), int(captureResponse['NumPixelsY']), captureResponse['SupportsSubframe'], captureResponse['GainValues'][0]
+        except Exception as e:
+            self.logger.error('SgGetCameraProp-> error: {0}'.format(e))
+            return False, 'Request failed', '', '', ''
+
+    def SgGetDeviceStatus(self, device):
+        # reference {"Device": "Camera"}, devices are "Camera", "FilterWheel", "Focuser", "Telescope" and "PlateSolver"}
+        data = {'Device': device}
+        try:
+            req = request.Request(self.ipSGPro + self.getDeviceStatusPath, data=bytes(json.dumps(data).encode('utf-8')), method='POST')
+            req.add_header('Content-Type', 'application/json')
+            with request.urlopen(req) as f:
+                captureResponse = json.loads(f.read().decode('utf-8'))
+            # states are  "IDLE", "CAPTURING", "BUSY", "MOVING", "DISCONNECTED", "PARKED"
+            # {"State":"IDLE","Success":false,"Message":"String"}
+            return captureResponse['Success'], captureResponse['State']
+        except Exception as e:
+            self.logger.error('SgGetDeviceStat-> error: {0}'.format(e))
+            return False, 'Request failed'
+
+    def SgGetImagePath(self, _guid):
+        # reference {"Receipt":"00000000000000000000000000000000"}
+        data = {'Receipt': _guid}
+        try:
+            req = request.Request(self.ipSGPro + self.getImagePath, data=bytes(json.dumps(data).encode('utf-8')), method='POST')
+            req.add_header('Content-Type', 'application/json')
+            with request.urlopen(req) as f:
+                captureResponse = json.loads(f.read().decode('utf-8'))
+            # {"Success":false,"Message":"String"}
+            return captureResponse['Success'], captureResponse['Message']
+        except Exception as e:
+            self.logger.error('SgGetImagePath -> error: {0}'.format(e))
+            return False, 'Request failed'
+
+    def SgGetSolvedImageData(self, _guid):
+        # reference {"Receipt":"00000000000000000000000000000000"}
+        data = {'Receipt': _guid}
+        try:
+            req = request.Request(self.ipSGPro + self.getSolvedImageDataPath, data=bytes(json.dumps(data).encode('utf-8')), method='POST')
+            req.add_header('Content-Type', 'application/json')
+            with request.urlopen(req) as f:
+                captureResponse = json.loads(f.read().decode('utf-8'))
+            # {"Success":false,"Message":"String","Ra":0,"Dec":0,"Scale":0,"Angle":0,"TimeToSolve":0}
+            return captureResponse['Success'], captureResponse['Message'], captureResponse['Ra'], captureResponse['Dec'], captureResponse['Scale'], captureResponse['Angle'], captureResponse['TimeToSolve']
+        except Exception as e:
+            self.logger.error('SgGetSolvedImag-> error: {0}'.format(e))
+            return False, 'Request failed', '', '', '', '', ''
+
+    def SgSolveImage(self, path, raHint=None, decHint=None, scaleHint=None, blindSolve=False, useFitsHeaders=False):
+        # reference {"ImagePath":"String","RaHint":0,"DecHint":0,"ScaleHint":0,"BlindSolve":false,"UseFitsHeadersForHints":false}
+        data = {"ImagePath": path, "BlindSolve": blindSolve, "UseFitsHeadersForHints": useFitsHeaders}
+        if raHint:
+            data['RaHint'] = raHint
+        if decHint:
+            data['DecHint'] = decHint
+        if scaleHint:
+            data['ScaleHint'] = scaleHint
+        try:
+            req = request.Request(self.ipSGPro + self.solveImagePath, data=bytes(json.dumps(data).encode('utf-8')), method='POST')
+            req.add_header('Content-Type', 'application/json')
+            with request.urlopen(req) as f:
+                captureResponse = json.loads(f.read().decode('utf-8'))                                                      # {"Success":false,"Message":"String","Receipt":"00000000000000000000000000000000"}
+            return captureResponse['Success'], captureResponse['Message'], captureResponse['Receipt']
+        except Exception as e:
+            self.logger.error('SgSolveImage   -> error: {0}'.format(e))
+            return False, 'Request failed', ''
+
+if __name__ == "__main__":
+
+    import os
+    cam = SGPro()
+    suc, mes, x, y, can = cam.SgGetCameraProps()
+    print(x, y, can)
