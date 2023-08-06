f8c1424ed7c0f2455ca72a590bd74f465f008bcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:53 +0000

    Add package dhis2-pocket-knife/0.3.10/dhis2-pocket-knife-0.3.10.tar.gz

---
+++ packages/dhis2-pocket-knife/dhis2-pocket-knife-0.3.10.tar.gz/dhis2-pocket-knife-0.3.10/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='dhis2-pocket-knife',
+    version='0.3.10',
+    description='Command-line tools for interacting with DHIS2 API in bulk',
+    author='David Huser',
+    author_email='dhuser@baosystems.com',
+    url='https://github.com/davidhuser/dhis2-pocket-knife',
+    keywords='dhis2',
+    install_requires=[
+        "requests>=2.4.2"
+    ],
+    scripts=[
+        'scripts/dhis2-pk-share-objects',
+        'scripts/dhis2-pk-user-orgunits'
+    ],
+    packages=find_packages()
+)
