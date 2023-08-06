f443969a218f9178bb19c65bbb9a1bd0fc5fe4b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:23 +0000

    Add package pycrest/0.0.6/pycrest-0.0.6.tar.gz

---
+++ packages/pycrest/pycrest-0.0.6.tar.gz/pycrest-0.0.6/pycrest/__init__.py
@@ -0,0 +1,14 @@
+import logging
+
+
+class NullHandler(logging.Handler):
+
+    def emit(self, record):
+        pass
+
+logger = logging.getLogger('pycrest')
+logger.addHandler(NullHandler())
+
+version = "0.0.6"
+
+from .eve import EVE
