3db70833ad90bf85ac7107993108b261ede9157f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:34 +0000

    Add package renodiff/0.1.0/renodiff-0.1.0.tar.gz

---
+++ packages/renodiff/renodiff-0.1.0.tar.gz/renodiff-0.1.0/setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+    'Click>=6.0',
+    # TODO: put package requirements here
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='renodiff',
+    version='0.1.0',
+    description="Convert git patches to reno release notes",
+    long_description=readme,
+    author="Honza Pokorny",
+    author_email='me@honza.ca',
+    url='https://github.com/honza/renodiff',
+    packages=[
+        'renodiff',
+    ],
+    package_dir={'renodiff':
+                 'renodiff'},
+    entry_points={
+        'console_scripts': [
+            'renodiff=renodiff.cli:main'
+        ]
+    },
+    include_package_data=True,
+    install_requires=requirements,
+    license="GNU General Public License v3",
+    zip_safe=False,
+    keywords='renodiff',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
