aef9ca81dd39fe76f8de1a5cd70c78e66402ad7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:34 +0000

    Add package mytest20170226/0.0.4/mytest20170226-0.0.4.tar.gz

---
+++ packages/mytest20170226/mytest20170226-0.0.4.tar.gz/mytest20170226-0.0.4/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import re
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+version='0.0.4'
+
+# requirements = [line.rstrip() for line in open(os.path.join(os.path.dirname(__file__), "requirements.txt"))]
+
+setup(
+    name='mytest20170226',
+    version=version,
+    description='mytest',
+    # long_description=readme,
+    packages=['mytest20170226'],
+    # install_requires=requirements,
+    include_package_data=True,
+    url="https://www.mytest.com",
+    classifiers=[
+	    'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ]
+)
