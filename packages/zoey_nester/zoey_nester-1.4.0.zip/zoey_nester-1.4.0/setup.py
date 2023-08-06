2a23e8a9bc6739981415c588dae028e69a26a908
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:45 +0000

    Add package zoey_nester/1.4.0/zoey_nester-1.4.0.zip

---
+++ packages/zoey_nester/zoey_nester-1.4.0.zip/zoey_nester-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		='zoey_nester',
+	version		='1.4.0',
+	py_modules	=['zoey_nester'],
+	author		='zoey',
+	author_email='zoey@163.com',
+	url			='http://www.headfirstlabs.com',
+	description	='A simple printer of nested lists',
+	)
