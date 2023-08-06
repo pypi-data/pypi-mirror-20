e631185cbb39764723f40b9de3d595100f36d212
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:06 +0000

    Add package worldpay/0.2/worldpay-0.2.tar.gz

---
+++ packages/worldpay/worldpay-0.2.tar.gz/worldpay-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='worldpay',
+      version='0.2',
+      description='Wrapper for Worldpay Orders API',
+      url='https://github.com/TestingPays',
+      author='Luke Rickard',
+      author_email='luke.rickard@thepayment.works',
+      license='MIT',
+      packages=['worldpay'],
+      zip_safe=False)
