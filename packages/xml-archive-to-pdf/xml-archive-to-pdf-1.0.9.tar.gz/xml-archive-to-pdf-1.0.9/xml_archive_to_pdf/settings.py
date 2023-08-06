343fd374590de1eff2dc1f571932262720c9fcf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:43 +0000

    Add package xml-archive-to-pdf/1.0.9/xml-archive-to-pdf-1.0.9.tar.gz

---
+++ packages/xml-archive-to-pdf/xml-archive-to-pdf-1.0.9.tar.gz/xml-archive-to-pdf-1.0.9/xml_archive_to_pdf/settings.py
@@ -0,0 +1,8 @@
+ATTR_NAME = "name"
+ATTR_STYLE = "style"
+ATTR_STYLE_TABLE = "table"
+ATTR_STYLE_TITLE = "title"
+EVENT_START = "start"
+EVENT_END = "end"
+SPACE_UNIT = 12
+DEFAULT_FONT = "Helvetica"
