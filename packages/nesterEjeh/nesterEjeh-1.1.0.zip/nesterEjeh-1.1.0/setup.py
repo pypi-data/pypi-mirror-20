20a28c293411d01fe07057e0c8ec9e1a936d3dd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:37 +0000

    Add package nesterEjeh/1.1.0/nesterEjeh-1.1.0.zip

---
+++ packages/nesterEjeh/nesterEjeh-1.1.0.zip/nesterEjeh-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup( 
+        name         = 'nesterEjeh', 
+        version      = '1.1.0', 
+        ## TODO: be sure to change these next few lines to match your details!
+        py_modules   = ['nester'],
+        author       = 'augustine',
+        author_email = 'augustine.ejeh@aun.edu.ng',
+        url          = 'http://www.headfirstlabs.com',
+        description  = 'A simple printer of nested lists',
+     )
