dffaadd80be65ea5adb299aa09c706e90ed6b77d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:24 +0000

    Add package pymiaopai/2017.3.9/pymiaopai-2017.3.9.tar.gz

---
+++ packages/pymiaopai/pymiaopai-2017.3.9.tar.gz/pymiaopai-2017.3.9/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env   python
+import os
+import setuptools
+
+setuptools.setup(
+
+        scripts = ['bin/pymiaopai_cli'],
+	name='pymiaopai',
+	version = '2017.3.9',
+	keywords = 'miaopai',
+	description = 'A python library for downloading miao pai vidoe.',
+	long_description=open(
+		os.path.join(os.path.dirname(__file__),'README.rst')).read(),
+		author = 'fanlt',
+		author_email = 'litton.van@gmail.com',
+		url = 'https://github.com/litton/MiaoPaiVideo',
+		packages = setuptools.find_packages(),
+		license = 'APACHE'
+      		)
