cf9911c2ce0e67063de0466c00615ecc8d1914d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:43 +0000

    Add package network/0.1/network-0.1.tar.gz

---
+++ packages/network/network-0.1.tar.gz/network-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='network',
+      version='0.1',
+      description='A very simple networking library written by David Whale (@whaleygeek).',
+      url='https://www.raspberrypi.org/learning/networking-lessons/',
+      maintainer='Andrew Mulholland',
+      maintainer_email='gbaman@users.noreply.github.com',
+      author='David Whale',
+      author_email='gbaman@users.noreply.github.com',
+      license='MIT',
+      py_modules=['network'],
+      zip_safe=False)
