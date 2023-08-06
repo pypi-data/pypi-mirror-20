aa76ba14f89a73e18b49ef514152928e7f92d7d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:14 +0000

    Add package cumtbscore/0.1.5/cumtbscore-0.1.5.tar.gz

---
+++ packages/cumtbscore/cumtbscore-0.1.5.tar.gz/cumtbscore-0.1.5/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='cumtbscore',
+    version='0.1.5',
+    description='Student score interface for CUMTB',
+    url='https://github.com/x1ah/Daily_scripts/tree/restructure/StuScore',
+    author='x1ah',
+    author_email='x1ahgxq@gmail.com',
+    license='MIT',
+    install_requires=['bs4>=0.0.1', 'prettytable>=0.7.2'],
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='cumtb score spider python cli',
+    packages=find_packages('src'),
+    packages_dir = {'': 'src'}
+)
