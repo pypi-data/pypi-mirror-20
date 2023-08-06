d8e8963dfe26d4449af1c33730062b3c9c7eef2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:47 +0000

    Add package funxuner/1.6.1/funxuner-1.6.1.tar.gz

---
+++ packages/funxuner/funxuner-1.6.1.tar.gz/funxuner-1.6.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name         = 'funxuner',
+        version      = '1.6.1',
+        py_modules   = ['funxuner'],
+        author       = 'Eric Chen',
+        author_email = 'chenyongyuan@sina.com',
+        url          = 'http://www.sina.com',
+        description  = 'A simple printer of nested lists ,hanyupinyin used',
+     )
