c9d9dc282264e263dbe666564efee9fb902a276c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:07 +0000

    Add package jfxsnester/1.1.0/jfxsnester-1.1.0.zip

---
+++ packages/jfxsnester/jfxsnester-1.1.0.zip/jfxsnester-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'jfxsnester',
+    version='1.1.0',
+    
+py_modules = ['nester'],
+    author= 'hentai',
+    author_email = '424056848@qq.com',
+    url = 'http://www.baidu.com',
+    description='A simple printer of nested lists',
+    )
