31b56d675367f31429fbda6e05efd6292f8e1ead
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:19 +0000

    Add package django-web-console/0.1/django-web-console-0.1.tar.gz

---
+++ packages/django-web-console/django-web-console-0.1.tar.gz/django-web-console-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+  name = 'django-web-console',
+  packages = find_packages(),
+  version = '0.1',
+  description = 'Django Console on the Browser.',
+  author = 'Rigoberto Molina',
+  author_email = 'rigothedev@gmail.com',
+  url = 'https://github.com/rigothedev/django-web-console',
+  download_url = 'https://github.com/rigothedev/django-web-console/archive/0.1.tar.gz',
+  keywords = ['django', 'console', 'django-console'], # arbitrary keywords
+  classifiers = [],
+)
