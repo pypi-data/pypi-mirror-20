5f72a4ab6f977c49c38b8c5545eb485a474ae42d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:30 +0000

    Add package etherwatch/0.1/etherwatch-0.1.tar.gz

---
+++ packages/etherwatch/etherwatch-0.1.tar.gz/etherwatch-0.1/setup.py
@@ -0,0 +1,20 @@
+import os
+from setuptools import setup
+
+REQUIREMENTS = [
+    line.strip() for line in open(os.path.join(os.path.dirname(__file__),
+                                               'requirements.txt')).readlines()]
+
+setup(name='etherwatch',
+      packages=['etherwatch'],
+      version='0.1',
+      description='Notifies you about ether prices.',
+      author = 'Ryan Culligan',
+      author_email='rrculligan@gmail.com',
+      license='MIT',
+      install_requires=REQUIREMENTS,
+      url='http://github.com/theculliganman/ether_watch',
+      download_url='https://codeload.github.com/TheCulliganMan/ether_watch/archive/master.tar.gz',
+      keywords = ['ether', 'ethereum', 'coinbase'],
+      classifiers = [],
+)
