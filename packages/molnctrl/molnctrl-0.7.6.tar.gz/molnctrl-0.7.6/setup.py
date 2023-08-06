03c713235ec427af368a43795a3fc02701a7dbcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:51 +0000

    Add package molnctrl/0.7.6/molnctrl-0.7.6.tar.gz

---
+++ packages/molnctrl/molnctrl-0.7.6.tar.gz/molnctrl-0.7.6/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+from __future__ import absolute_import
+from setuptools import setup
+
+name = 'molnctrl'
+version = '0.7.6'
+
+setup(name=name,
+      version=version,
+      description='A simple python Apache Cloudstack API',
+      author='Magnus Bengtsson',
+      author_email='magnus.bengtsson@redbridge.se',
+      url='http://github.com/redbridge/molnctrl',
+      install_requires=[
+          'requests',
+          'six',
+          'futurize',
+      ],
+      packages=['molnctrl'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Environment :: Other Environment',
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: Apache Software License',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+
+          # Specify the Python versions you support here. In particular, ensure
+          # that you indicate whether you support Python 2, Python 3 or both.
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+      ])
