78dc2630f4497ef2c110a0ed818208ec61f36386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:18 +0000

    Add package nodeconductor-auth-social/0.4.0/nodeconductor_auth_social-0.4.0-py2.7.egg

---
+++ packages/nodeconductor-auth-social/nodeconductor_auth_social-0.4.0-py2.7.egg/nodeconductor_auth_social/urls.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals
+
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^api-auth/google/$', views.GoogleView.as_view(), name='auth_google'),
+    url(r'^api-auth/facebook/$', views.FacebookView.as_view(), name='auth_facebook'),
+    url(r'^api-auth/registration/$', views.RegistrationView.as_view()),
+    url(r'^api-auth/activation/$', views.ActivationView.as_view()),
+]
