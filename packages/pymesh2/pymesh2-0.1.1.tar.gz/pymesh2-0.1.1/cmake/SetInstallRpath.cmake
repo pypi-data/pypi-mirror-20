087991c4158e402b7403e88cb5780024b4c0af1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:42 +0000

    Add package pymesh2/0.1.1/pymesh2-0.1.1.tar.gz

---
+++ packages/pymesh2/pymesh2-0.1.1.tar.gz/pymesh2-0.1.1/cmake/SetInstallRpath.cmake
@@ -0,0 +1,2 @@
+SET(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/pymesh-0.1-py3.5.egg/pymesh/lib;${CMAKE_INSTALL_PREFIX}/pymesh-0.1-py3.5.egg/pymesh/third_party/lib;/Users/qzhou/test/virtualenv/test/PyMesh/python/pymesh/lib;/Users/qzhou/test/virtualenv/test/PyMesh/python/pymesh/third_party/lib")
+MESSAGE(STATUS "CMAKE_INSTALL_RPATH: ${CMAKE_INSTALL_RPATH}")
