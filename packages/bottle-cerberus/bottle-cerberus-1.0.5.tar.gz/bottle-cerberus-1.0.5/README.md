64b93a875af469cebbae344e64aa970445c13354
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:51 +0000

    Add package bottle-cerberus/1.0.5/bottle-cerberus-1.0.5.tar.gz

---
+++ packages/bottle-cerberus/bottle-cerberus-1.0.5.tar.gz/bottle-cerberus-1.0.5/README.md
@@ -0,0 +1,48 @@
+# Cerberus plugin
+Cerberus plugin for bottle
+
+## installation
+
+Via pip:
+```pip install bottle-cerberus```
+
+Or clone:
+```git clone https://github.com/kianxineki/bottle-cerberus.git```
+
+
+## example:
+```python
+from bottle import get, install, run
+from bottle_cerberus import CerberusPlugin
+
+
+@get('/cerberus/<ex>', schemas={'body': {'ex': {'type': 'integer'}},
+                                'url': {'ex': {'coerce': int}},
+                                'query_string': {'ex': {'coerce': int}}})
+def test_cerberus(ex):
+    from bottle import request
+    print("query_string", request.query['ex'], type(request.query['ex']))
+    print("url", ex, type(ex))
+    print("body", request.json.get('ex'), type(request.json.get('ex')))
+
+
+install(CerberusPlugin())
+run(host="0.0.0.0", port="9988")
+
+
+```
+
+# Schemas
+
+## Optional keys
+
+body: schema for request.json
+
+url: schema for url (no query string)
+
+query_string: schema for query strings
+
+# Schema
+
+Github Schema: https://github.com/nicolaiarocci/cerberus
+Doc schema: http://docs.python-cerberus.org/en/stable/
---
+++ packages/bottle-cerberus/bottle-cerberus-1.0.5.tar.gz/bottle-cerberus-1.0.5/bottle_cerberus.py
@@ -0,0 +1,52 @@
+from bottle import HTTPError, ConfigDict, request
+from functools import wraps
+from cerberus import Validator
+
+
+class MyValidator(Validator):
+    def _validate_type_objectid(*args, **kwargs):
+        return True
+
+
+class CerberusPlugin(object):
+    name = 'CerberusPlugin'
+    api = 2
+    keyword = 'schemas'
+
+    def apply(self, callback, route):
+        schemas = {}
+        for namespace in route.config:
+            if self.keyword == namespace[:len(self.keyword)]:
+                old = schemas
+                final = namespace.split('.')[-1]
+                for key in namespace.split('.')[1:]:
+                    if key not in old:
+                        if final == key:
+                            old[key] = route.config[namespace]
+                            continue
+                        old[key] = {}
+                    old = old[key]
+
+        if schemas == {}:
+            return callback
+
+        validators = {k: MyValidator(schemas[k],
+                                     allow_unknown=True) for k in schemas}
+
+        @wraps(callback)
+        def wrapper(*args, **kwargs):
+            shortcut = {'url': kwargs,
+                        'body': request.json or {},
+                        'query_string': {k: v for k, v in request.query.items()}}
+
+            for k in validators:
+                # modify original reference
+                shortcut[k] = validators[k].normalized(shortcut[k])
+
+                if (shortcut[k] is None or
+                        not validators[k].validate(shortcut[k])):
+                    raise HTTPError(400, "bad request")
+
+            return callback(*args, **kwargs)
+
+        return wrapper
