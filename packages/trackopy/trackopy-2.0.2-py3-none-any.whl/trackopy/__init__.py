6404afe5b825e2cc20087a8be73723e8844dd14e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:41 +0000

    Add package trackopy/2.0.2/trackopy-2.0.2-py3-none-any.whl

---
+++ packages/trackopy/trackopy-2.0.2-py3-none-any.whl/trackopy/__init__.py
@@ -0,0 +1,20 @@
+__title__ = 'trackopy'
+__version__ = '2.0.2'
+__author__ = 'Sean Beck'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 Sean Beck'
+
+from .trackobot import Trackobot
+
+__all__ = ['Trackobot']
+
+import logging
+try:
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.handler):
+        def emit(self, record):
+            pass
+
+logging.getLogger(__name__).addHandler(NullHandler())
+
