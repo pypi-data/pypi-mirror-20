89886c1a5e0b8cc5f788b0a21d3fbc22f2f9a0a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:37 +0000

    Add package comiccrawler/2017.3.6/comiccrawler-2017.3.6.tar.gz

---
+++ packages/comiccrawler/comiccrawler-2017.3.6.tar.gz/comiccrawler-2017.3.6/comiccrawler/io.py
@@ -0,0 +1,169 @@
+#! python3
+
+"""Simple io module depressing exceptions"""
+
+import os, os.path as path, pprint, glob, time, shutil
+import io
+
+from contextlib import contextmanager, suppress
+
+CHUNK_LIMIT = 500 * 1000 * 1000
+
+def write_big(file, content):
+	for piece in range(0, len(content), CHUNK_LIMIT):
+		file.write(content[piece:piece + CHUNK_LIMIT])
+
+def exists(file):
+	file = path.expanduser(file)
+	
+	return path.exists(file)
+
+def is_file(file):
+	"""Check if the file is file."""
+	file = path.expanduser(file)
+
+	return path.isfile(file)
+
+def content_write(file, content, append=False):
+	"""Write content to file. Content may be str or bytes."""
+	file = path.expanduser(file)
+
+	prepare_folder(path.dirname(file))
+
+	original = file
+
+	if append:
+		mode = "a"
+	else:
+		mode = "w"
+		file = file + time.strftime("@%Y-%m-%d_%H%M%S")
+
+	if isinstance(content, bytes):
+		mode += "b"
+		with io.open(file, mode) as f:
+			write_big(f, content)
+
+	else:
+		if not isinstance(content, str):
+			content = pprint.pformat(content)
+
+		with io.open(file, mode, encoding="utf-8") as f:
+			write_big(f, content)
+
+	if file != original:
+		os.replace(file, original)
+
+def content_read(file, raw=False):
+	"""Read content from file. Return str."""
+	file = path.expanduser(file)
+
+	if not path.isfile(file):
+		return ""
+
+	if raw:
+		with io.open(file, "rb") as f:
+			return f.read()
+	else:
+		with io.open(file, "r", encoding="utf-8-sig") as f:
+			return f.read()
+
+def prepare_folder(folder):
+	"""If the folder does not exist, create it."""
+	folder = path.expanduser(folder)
+
+	if not path.isdir(folder):
+		os.makedirs(folder)
+
+	return folder
+
+def prepare_file(file):
+	"""If the file does not exist, create it."""
+	file = path.expanduser(file)
+
+	prepare_folder(path.dirname(file))
+
+	if not path.isfile(file):
+		io.open(file, "w").close()
+
+	return file
+	
+def move(src, dest):
+	"""Move src files to dest. Should support wildcard."""
+	src = path.expanduser(src)
+	dest = path.expanduser(dest)
+
+	if "*" in src:
+		# Wildcard multiple move
+		prepare_folder(dest)
+
+		for file in glob.iglob(src):
+			os.rename(file, path.join(dest, path.basename(file)))
+	else:
+		# just a rename
+		if not exists(src):
+			return
+
+		prepare_folder(path.dirname(dest))
+		os.rename(src, dest)
+
+def backup(file):
+	"""Create backup file."""
+	file = path.expanduser(file)
+	if "*" in file:
+		# Wildcard multiple copy
+		for file in glob.iglob(file):
+			shutil.copyfile(file, file + time.strftime("@%Y-%m-%d_%H%M%S"))
+	else:
+		if not is_file(file):
+			return
+		shutil.copyfile(file, file + time.strftime("@%Y-%m-%d_%H%M%S"))
+
+def path_each(folder, callback, mode="f"):
+	"""Iter through all file in the folder.
+
+	`folder` may contain "*", or it will use the children in the folder.
+	`callback` will recieve a filename param.
+	`mode` is a string containing f or/and d, to filter file, directory.
+	"""
+	folder = path.expanduser(folder)
+	if "*" in folder:
+		files = glob.iglob(folder)
+	elif path.isdir(folder):
+		files = [path.join(folder, file) for file in os.listdir(folder)]
+	else:
+		return
+
+	for file in files:
+		if path.isfile(file) and "f" in mode or path.isdir(file) and "d" in mode:
+			callback(file)
+			
+def expand(file):
+	"""Expand user"""
+	return path.expanduser(file)
+	
+def dirname(file):
+	"""Get dirname"""
+	return path.dirname(file)
+
+@contextmanager	
+def open(file, mode="r"):
+	file = path.expanduser(file)
+	prepare_folder(path.dirname(file))
+	
+	original = file
+	if "w" in mode and is_file(file):
+		file = file + time.strftime("@%Y-%m-%d_%H%M%S")
+		encoding = "utf-8"
+	else:
+		encoding = "utf-8-sig"
+		
+	with io.open(file, mode, encoding=encoding) as fp:
+		yield fp
+	
+	if file != original:
+		os.replace(file, original)
+		
+def remove(file):
+	file = path.expanduser(file)
+	with suppress(FileNotFoundError):
+		os.remove(file)
---
+++ packages/comiccrawler/comiccrawler-2017.3.6.tar.gz/comiccrawler-2017.3.6/comiccrawler/mods/deviantart.py
@@ -0,0 +1,71 @@
+#! python3
+
+"""this is deviant module for comiccrawler, support deviantart.com
+
+2014/7/13
+JDownloader2 now support　deviantart.com!
+
+"""
+
+import re
+
+from html import unescape
+from urllib.parse import urljoin
+
+from ..error import PauseDownloadError
+from ..core import Episode
+
+domain = ["deviantart.com"]
+name = "dA"
+noepfolder = True
+config = {
+	"cookie_auth": "請輸入Cookie中的auth",
+	"cookie_userinfo": "請輸入Cookie中的userinfo"
+}
+
+def get_title(html, url):
+	return unescape(re.search("<title>(.+?)</title>", html).group(1))
+	
+def check_login(html, url):
+	if not re.search('"loggedin":true', html, re.I):
+		raise PauseDownloadError("you didn't log in!")
+
+def get_episodes(html, url):
+	check_login(html, url)
+	
+	base = re.search("(https?://[^/]+)", url).group(1)
+	s = []
+	startpos = html.index('id="gmi-ResourceStream"')
+	ex = re.compile('<a class="thumb[^"]*?" href="({}/art/.+?)" title="(.+?)"'.format(base))
+
+	for match in ex.finditer(html, startpos):
+		id = re.search("\d+$", match.group(1)).group()
+		title = match.group(2).rpartition(" by ")[0]
+		# WTF r u doing deviantArt?
+		title = unescape(unescape(title))
+
+		s.append(
+			Episode(
+				"{} - {}".format(id, title),
+				match.group(1)
+			)
+		)
+
+	return s[::-1]
+
+def get_images(html, url):
+	check_login(html, url)
+
+	html = html.replace("\n", "")
+	try:
+		i = re.search('dev-page-download"\s+href="([^"]+)"', html).group(1)
+		return [unescape(i)]
+	except Exception:
+		pass
+	i = re.search('<img[^>]+?src="([^"]+)"[^>]+?class="dev-content-full[^"]*">', html).group(1)
+	return [i]
+	
+def get_next_page(html, url):
+	next = re.search('id="gmi-GPageButton"[^>]+?href="([^"]+?)"><span>Next</span>', html)
+	if next:
+		return urljoin(url, unescape(next.group(1)))
