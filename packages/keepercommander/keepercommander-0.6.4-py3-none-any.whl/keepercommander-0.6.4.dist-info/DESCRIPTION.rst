6f3018bc392f51ce40f8314be966b25253ca8181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:13 +0000

    Add package keepercommander/0.6.4/keepercommander-0.6.4-py3-none-any.whl

---
+++ packages/keepercommander/keepercommander-0.6.4-py3-none-any.whl/keepercommander-0.6.4.dist-info/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+.. figure:: https://raw.githubusercontent.com/Keeper-Security/Commander/master/keepercommander/images/commander_logo_512x205.png
+
+
---
+++ packages/keepercommander/keepercommander-0.6.4-py3-none-any.whl/keepercommander/display.py
@@ -0,0 +1,83 @@
+#  _  __  
+# | |/ /___ ___ _ __  ___ _ _ ®
+# | ' </ -_) -_) '_ \/ -_) '_|
+# |_|\_\___\___| .__/\___|_|
+#              |_|            
+#
+# Keeper Commander 
+# Copyright 2015 Keeper Security Inc.
+# Contact: ops@keepersecurity.com
+#
+import json
+
+from colorama import init
+from tabulate import tabulate
+
+init()
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def welcome():
+    print('\n')
+    print(bcolors.OKBLUE,' _  __  ' + bcolors.ENDC)
+    print(bcolors.OKBLUE,'| |/ /___ ___ _ __  ___ _ _ ' + bcolors.ENDC)
+    print(bcolors.OKBLUE,'| \' </ -_) -_) \'_ \\/ -_) \'_|' + bcolors.ENDC)
+    print(bcolors.OKBLUE,'|_|\\_\\___\\___| .__/\\___|_|' + bcolors.ENDC)
+    print(bcolors.OKBLUE,'             |_|            ' + bcolors.ENDC)
+    print('')
+    print(bcolors.FAIL,'password manager & digital vault' + bcolors.ENDC)
+    print('')
+    print('')
+
+def formatted_records(records):
+    """Display folders/titles/uids for the supplied records"""
+
+    # Sort by folder+title
+    records.sort(key=lambda x: ((x.folder if x.folder else ' ') + x.title).lower(), reverse=False)
+
+    if len(records) > 0:
+
+        table = [[i + 1, r.record_uid, r.folder, r.title] for i, r in enumerate(records)]
+        print(tabulate(table, headers=["#", 'Record UID', 'Folder', 'Title']))
+
+        print('')
+
+    # Under 5 recs, just display on the screen
+    if len(records) < 5:
+        for r in records:
+            r.display()
+
+def formatted_history(history):
+    """ Show the history of commands"""
+
+    if not history: return
+    if len(history) == 0: return
+
+    print('')
+    print('Command history:')
+    print('----------------')
+
+    for h in history:
+        print(h)
+
+    print('')
+
+def print_record(params, record_uid):
+    """ Show record content """
+
+    try:
+        cached_rec = params.record_cache[record_uid]
+    except KeyError as e:
+        raise Exception('Record not found: ' + record_uid)
+    data = json.loads(cached_rec['data'].decode('utf-8'))
+    print(data)
+
+
