7b908adfcfd5b1545c7e0cedc2124aae273427d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:58 +0000

    Add package oii/0.11/oii-0.11.tar.gz

---
+++ packages/oii/oii-0.11.tar.gz/oii-0.11/oii/__init__.py
@@ -0,0 +1,24 @@
+# coding:utf-8
+import pytz
+import datetime
+
+
+def test():
+    print 'hello world'
+
+
+def convert_tz(utc_time_str, tz='Asia/Shanghai'):
+    # 将存储在数据库中的UTC时间转换成指定时间
+    utc_datetime = datetime.strptime(utc_time_str, "%Y-%m-%d %H:%M:%S")
+
+    current_tz = pytz.timezone(tz)
+    utc_tz = pytz.timezone('UTC')
+    utc_tz_datetime = utc_tz.localize(utc_datetime, is_dst=None)
+    value = utc_tz_datetime.astimezone(current_tz)
+
+    return value.strftime("%Y-%m-%d %H:%M:%S")
+
+
+if __name__ == '__main__':
+    print 'test ...'
+    test()
