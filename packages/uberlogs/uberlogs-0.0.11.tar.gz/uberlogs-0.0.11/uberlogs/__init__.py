67104fde020bd57f632418dc1c4042ad251ada57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:58 +0000

    Add package uberlogs/0.0.11/uberlogs-0.0.11.tar.gz

---
+++ packages/uberlogs/uberlogs-0.0.11.tar.gz/uberlogs-0.0.11/uberlogs/__init__.py
@@ -0,0 +1,5 @@
+# NOQA -> silence flake8 warnings
+from six.moves import builtins  # NOQA
+from .formatters import *  # NOQA
+from .handlers import *  # NOQA
+from .public import getLogger, install  # NOQA
