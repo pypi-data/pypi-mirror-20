6d5d96b42906b3a9dacef17a00f113be5f233e7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:52 +0000

    Add package PyFor/0.1/PyFor-0.1.zip

---
+++ packages/PyFor/PyFor-0.1.zip/PyFor-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='PyFor',
+    version='0.1',
+    author='Bryce Frank',
+    author_email='bryce.frank@oregonstate.edu',
+    packages=['PyFor'],
+    url='https://github.com/brycefrank/pyfor',
+    license='LICENSE.txt',
+    description='Tools for forest resource LiDAR.',
+)
