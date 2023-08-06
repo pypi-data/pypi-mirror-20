760e5a82bd342f04105fec0878657d73222feddf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:23 +0000

    Add package anom/0.0.1/anom-0.0.1-py3-none-any.whl

---
+++ packages/anom/anom-0.0.1-py3-none-any.whl/anom/adapters/datastore_adapter.py
@@ -0,0 +1,141 @@
+from google.cloud import datastore
+from threading import local
+
+from .. import Adapter, Key
+from ..adapter import QueryResponse
+
+
+class DatastoreAdapter(Adapter):
+    """A Google Cloud Datastore adapter based on :mod:`google.cloud.datastore`.
+    """
+
+    _state = local()
+
+    @property
+    def client(self):
+        """datastore.Client: The underlying datastore client for this
+        adapter instance.  This property is thread-local.
+        """
+        client = getattr(self._state, "client", None)
+        if client is None:
+            client = self._state.client = datastore.Client(
+                project=self.project,
+                namespace=self.namespace,
+            )
+        return client
+
+    def delete_multi(self, keys):
+        self.client.delete_multi(self._convert_key_to_datastore(key) for key in keys)
+
+    def get_multi(self, keys):
+        datastore_keys = [self._convert_key_to_datastore(key) for key in keys]
+        request_keys = set(datastore_keys)
+        found, missing, deferred = [], [], []
+        while True:
+            found.extend(self.client.get_multi(
+                request_keys,
+                missing=missing,
+                deferred=deferred
+            ))
+            if not deferred:
+                break
+
+            for entity in found:
+                request_keys.remove(entity.key)
+
+            for key in missing:
+                request_keys.remove(key)
+
+        results = [None] * len(keys)
+        for entity in found:
+            index = datastore_keys.index(entity.key)
+            results[index] = self._prepare_to_load(entity)
+
+        return results
+
+    def put_multi(self, requests):
+        entities = [self._prepare_to_store(*request) for request in requests]
+        self.client.put_multi(entities)
+        return [self._convert_key_from_datastore(entity.key) for entity in entities]
+
+    def query(self, query, options):
+        ancestor = None
+        if query.ancestor:
+            ancestor = self._convert_key_to_datastore(query.ancestor)
+
+        query = self.client.query(
+            kind=query.kind,
+            ancestor=ancestor,
+            namespace=query.namespace,
+            projection=query.projection,
+            filters=query.filters,
+            order=query.orders,
+        )
+        if options.keys_only:
+            query.keys_only()
+
+        result_iterator = query.fetch(
+            limit=options.batch_size,
+            offset=options.offset,
+            start_cursor=options.cursor,
+        )
+
+        entities = []
+        for entity in result_iterator:
+            key, data = self._convert_key_from_datastore(entity.key), None
+            if not options.keys_only:
+                data = entity
+
+            entities.append((key, data))
+
+        return QueryResponse(entities=entities, cursor=result_iterator.next_page_token)
+
+    def _convert_key_to_datastore(self, anom_key):
+        return self.client.key(*anom_key.path, namespace=anom_key.namespace)
+
+    def _convert_key_from_datastore(self, ds_key):
+        namespace, key = ds_key.namespace, None
+        for i in range(0, len(ds_key.flat_path), 2):
+            kind, id_or_name = ds_key.flat_path[i:i + 2]
+            key = Key(kind, id_or_name, parent=key, namespace=namespace)
+
+        return key
+
+    def _prepare_to_store(self, key, unindexed, data):
+        """Populate an Entity with data from the Model.
+
+        Parameters:
+          key(Key)
+          unindexed(str list)
+          data(iterable)
+
+        Returns:
+          datastore.Entity
+        """
+        datastore_key = self._convert_key_to_datastore(key)
+        entity = datastore.Entity(datastore_key, unindexed)
+        for name, value in data:
+            if isinstance(value, Key):
+                value = self._convert_key_to_datastore(value)
+
+            entity[name] = value
+
+        return entity
+
+    def _prepare_to_load(self, entity):
+        """Preprocess Entity data before returning it to the Model.
+
+        Parameters:
+          entity(datastore.Entity)
+
+        Returns:
+          dict
+        """
+        data = {}
+        for name, value in entity.items():
+            if isinstance(value, datastore.Key):
+                value = self._convert_key_from_datastore(value)
+
+            data[name] = value
+
+        return data
---
+++ packages/anom/anom-0.0.1-py3-none-any.whl/anom/properties.py
@@ -0,0 +1,392 @@
+# TODO(bogdan): Add support for computed, msgpack and pickle properties.
+import json
+import zlib
+
+from datetime import datetime
+from dateutil import tz
+
+from .model import Key as DSKey, model, Model, Property
+
+
+#: The maximum length of indexed properties.
+_max_indexed_length = 1500
+
+
+class Blob:
+    """Mixin for Properties whose values cannot be indexed.
+    """
+
+    def __init__(self, **options):
+        if options.get("indexed"):
+            classname = type(self).__name__
+            raise TypeError(f"{classname} properties cannot be indexed.")
+
+        super().__init__(**options)
+
+
+class Compressable(Blob):
+    """Mixin for Properties whose values can be gzipped before being
+    persisted.
+
+    Parameters:
+      compressed(bool): Whether or not values belonging to this
+        Property should be stored gzipped in Datastore.
+      compression_level(int): The amount of compression to apply.
+        See :func:`zlib.compress` for details.
+    """
+
+    def __init__(self, *, compressed=False, compression_level=-1, **options):
+        if not (-1 <= compression_level <= 9):
+            raise ValueError("compression_level must be an integer between -1 and 9.")
+
+        super().__init__(**options)
+
+        self.compressed = compressed
+        self.compression_level = compression_level
+
+    def prepare_to_load(self, entity, value):
+        if value is None:
+            return value
+
+        if self.compressed:
+            value = zlib.decompress(value)
+
+        return super().prepare_to_load(entity, value)
+
+    def prepare_to_store(self, entity, value):
+        value = super().prepare_to_store(entity, value)
+        if self.compressed and value is not None:
+            value = zlib.compress(value, level=self.compression_level)
+
+        return value
+
+
+class Encodable:
+    """Mixins for string properties that have an encoding.
+
+    Parameters:
+      encoding(str): The encoding to use when persisting this Property
+        to Datastore.  Defaults to ``utf-8``.
+    """
+
+    def __init__(self, *, encoding="utf-8", **options):
+        super().__init__(**options)
+
+        self.encoding = encoding
+
+    def prepare_to_load(self, entity, value):
+        if value is None:
+            return value
+
+        value = super().prepare_to_load(entity, value)
+        if isinstance(value, str):
+            # BUG(gcloud): Projections seem to cause bytes to be loaded as strings.
+            return value
+
+        return value.decode(self.encoding)
+
+    def prepare_to_store(self, entity, value):
+        if value is not None:
+            value = value.encode(self.encoding)
+
+        return super().prepare_to_store(entity, value)
+
+
+class Bool(Property):
+    """A Property for boolean values.
+
+    Parameters:
+      name(str, optional): The name of this property on the Datastore
+        entity.  Defaults to the name of this property on the model.
+      default(object, optional): The property's default value.
+      indexed(bool, optional): Whether or not this property should be
+        indexed.  Defaults to ``False``.
+      optional(bool, optional): Whether or not this property is
+        optional.  Defaults to ``False``.  Required but empty values
+        cause models to raise an exception before data is persisted.
+      repeated(bool, optional): Whether or not this property is
+        repeated.  Defaults to ``False``.  Optional repeated
+        properties default to an empty list.
+    """
+
+    _types = (bool,)
+
+    @property
+    def is_true(self):
+        "PropertyFilter: A filter that checks if this value is True."
+        return self == True  # noqa
+
+    @property
+    def is_false(self):
+        "PropertyFilter: A filter that checks if this value is False."
+        return self == False  # noqa
+
+
+class Bytes(Compressable, Property):
+    """A Property for bytestring values.
+
+    Parameters:
+      name(str, optional): The name of this property on the Datastore
+        entity.  Defaults to the name of this property on the model.
+      default(object, optional): The property's default value.
+      indexed(bool, optional): Whether or not this property should be
+        indexed.  Defaults to ``False``.
+      optional(bool, optional): Whether or not this property is
+        optional.  Defaults to ``False``.  Required but empty values
+        cause models to raise an exception before data is persisted.
+      repeated(bool, optional): Whether or not this property is
+        repeated.  Defaults to ``False``.  Optional repeated
+        properties default to an empty list.
+      compressed(bool, optional): Whether or not this property should
+        be compressed before being persisted.
+      compression_level(int, optional): The amount of compression to
+        apply when compressing values.
+    """
+
+    _types = (bytes,)
+
+
+class DateTime(Property):
+    """A Property for :class:`datetime.datetime` values.
+
+    Parameters:
+      name(str, optional): The name of this property on the Datastore
+        entity.  Defaults to the name of this property on the model.
+      default(object, optional): The property's default value.
+      indexed(bool, optional): Whether or not this property should be
+        indexed.  Defaults to ``False``.
+      optional(bool, optional): Whether or not this property is
+        optional.  Defaults to ``False``.  Required but empty values
+        cause models to raise an exception before data is persisted.
+      repeated(bool, optional): Whether or not this property is
+        repeated.  Defaults to ``False``.  Optional repeated
+        properties default to an empty list.
+      auto_now_add(bool, optional): Whether or not to set this
+        property's value to the current time the first time it's
+        stored.
+      auto_now(bool, optional): Whether or not this property's value
+        should be set to the current time every time it is stored.
+    """
+
+    _types = (datetime,)
+
+    def __init__(self, *, auto_now_add=False, auto_now=False, **options):
+        super().__init__(**options)
+
+        self.auto_now_add = auto_now_add
+        self.auto_now = auto_now
+
+        if self.repeated and (auto_now_add or auto_now):
+            raise TypeError("Cannot use auto_now{,_add} with repeated properties.")
+
+    def _current_value(self):
+        return datetime.now(tz.tzlocal())
+
+    def prepare_to_load(self, entity, value):
+        # BUG(gcloud): Projections seem to cause datetimes to be
+        # loaded as ints in microseconds.
+        if value is not None and isinstance(value, int):
+            value = datetime.fromtimestamp(value / 1000000, tz.tzutc())
+
+        return super().prepare_to_load(entity, value)
+
+    def prepare_to_store(self, entity, value):
+        if value is None and self.auto_now_add:
+            value = entity._data[self.name_on_entity] = self._current_value()
+        elif self.auto_now:
+            value = entity._data[self.name_on_entity] = self._current_value()
+
+        if value is not None:
+            value = entity._data[self.name_on_entity] = value.astimezone(tz.tzutc())
+
+        return super().prepare_to_store(entity, value)
+
+    def validate(self, value):
+        value = super().validate(value)
+        if value is not None and not value.tzinfo:
+            return value.replace(tzinfo=tz.tzlocal())
+        return value
+
+
+class Float(Property):
+    """A Property for floating point values.
+
+    Parameters:
+      name(str, optional): The name of this property on the Datastore
+        entity.  Defaults to the name of this property on the model.
+      default(object, optional): The property's default value.
+      indexed(bool, optional): Whether or not this property should be
+        indexed.  Defaults to ``False``.
+      optional(bool, optional): Whether or not this property is
+        optional.  Defaults to ``False``.  Required but empty values
+        cause models to raise an exception before data is persisted.
+      repeated(bool, optional): Whether or not this property is
+        repeated.  Defaults to ``False``.  Optional repeated
+        properties default to an empty list.
+    """
+
+    _types = (float,)
+
+
+class Integer(Property):
+    """A Property for integer values.
+
+    Parameters:
+      name(str, optional): The name of this property on the Datastore
+        entity.  Defaults to the name of this property on the model.
+      default(object, optional): The property's default value.
+      indexed(bool, optional): Whether or not this property should be
+        indexed.  Defaults to ``False``.
+      optional(bool, optional): Whether or not this property is
+        optional.  Defaults to ``False``.  Required but empty values
+        cause models to raise an exception before data is persisted.
+      repeated(bool, optional): Whether or not this property is
+        repeated.  Defaults to ``False``.  Optional repeated
+        properties default to an empty list.
+    """
+
+    _types = (int,)
+
+
+class Json(Compressable, Property):
+    """A Property for values that should be stored as JSON.
+
+    Parameters:
+      name(str, optional): The name of this property on the Datastore
+        entity.  Defaults to the name of this property on the model.
+      default(object, optional): The property's default value.
+      indexed(bool, optional): Whether or not this property should be
+        indexed.  Defaults to ``False``.
+      optional(bool, optional): Whether or not this property is
+        optional.  Defaults to ``False``.  Required but empty values
+        cause models to raise an exception before data is persisted.
+      repeated(bool, optional): Whether or not this property is
+        repeated.  Defaults to ``False``.  Optional repeated
+        properties default to an empty list.
+      compressed(bool, optional): Whether or not this property should
+        be compressed before being persisted.
+      compression_level(int, optional): The amount of compression to
+        apply when compressing values.
+    """
+
+    # TODO(bogdan): Add support for `datetime` and `Model`.
+    _types = (bool, bytes, float, int, str)
+
+    def prepare_to_load(self, entity, value):
+        if value is not None:
+            value = json.loads(value)
+
+        return super().prepare_to_load(entity, value)
+
+    def prepare_to_store(self, entity, value):
+        if value is not None:
+            value = json.dumps(value, separators=(",", ":"))
+
+        return super().prepare_to_store(entity, value)
+
+
+class Key(Property):
+    """A Property for :class:`anom.Key` values.
+
+    Parameters:
+      name(str, optional): The name of this property on the Datastore
+        entity.  Defaults to the name of this property on the model.
+      default(object, optional): The property's default value.
+      kind(str or model, optional): The kinds of keys that may be
+        assigned to this property.
+      indexed(bool, optional): Whether or not this property should be
+        indexed.  Defaults to ``False``.
+      optional(bool, optional): Whether or not this property is
+        optional.  Defaults to ``False``.  Required but empty values
+        cause models to raise an exception before data is persisted.
+      repeated(bool, optional): Whether or not this property is
+        repeated.  Defaults to ``False``.  Optional repeated
+        properties default to an empty list.
+    """
+
+    _types = (DSKey,)
+
+    def __init__(self, *, kind=None, **options):
+        super().__init__(**options)
+
+        if isinstance(kind, model):
+            self.kind = kind._kind
+        else:
+            self.kind = kind
+
+    def validate(self, value):
+        if value is not None and isinstance(value, Model):
+            value = value.key
+
+        value = super().validate(value)
+        if value is not None:
+            if not value.is_complete:
+                raise ValueError("Cannot assign incomplete Keys to Key properties.")
+
+            elif self.kind and self.kind != value.kind:
+                raise ValueError(f"Property {self.name_on_model} is cannot be assigned keys of kind {value.kind}.")
+
+        return value
+
+
+class String(Encodable, Property):
+    """A Property for indexable string values.
+
+    Parameters:
+      name(str, optional): The name of this property on the Datastore
+        entity.  Defaults to the name of this property on the model.
+      default(object, optional): The property's default value.
+      indexed(bool, optional): Whether or not this property should be
+        indexed.  Defaults to ``False``.
+      optional(bool, optional): Whether or not this property is
+        optional.  Defaults to ``False``.  Required but empty values
+        cause models to raise an exception before data is persisted.
+      repeated(bool, optional): Whether or not this property is
+        repeated.  Defaults to ``False``.  Optional repeated
+        properties default to an empty list.
+      encoding(str): The encoding to use when persisting this Property
+        to Datastore.  Defaults to ``utf-8``.
+    """
+
+    _types = (str,)
+
+    def validate(self, value):
+        value = super().validate(value)
+        if not self.indexed:
+            return value
+
+        if len(value) > _max_indexed_length and \
+           len(value.encode(self.encoding)) > _max_indexed_length:
+            raise ValueError(
+                f"String value is longer than the maximum allowed length "
+                f"({_max_indexed_length}) for indexed properties. Set "
+                f"indexed to False if the value should not be indexed."
+            )
+
+        return value
+
+
+class Text(Compressable, Encodable, Property):
+    """A Property for long string values that are never indexed.
+
+    Parameters:
+      name(str, optional): The name of this property on the Datastore
+        entity.  Defaults to the name of this property on the model.
+      default(object, optional): The property's default value.
+      indexed(bool, optional): Whether or not this property should be
+        indexed.  Defaults to ``False``.
+      optional(bool, optional): Whether or not this property is
+        optional.  Defaults to ``False``.  Required but empty values
+        cause models to raise an exception before data is persisted.
+      repeated(bool, optional): Whether or not this property is
+        repeated.  Defaults to ``False``.  Optional repeated
+        properties default to an empty list.
+      compressed(bool, optional): Whether or not this property should
+        be compressed before being persisted.
+      compression_level(int, optional): The amount of compression to
+        apply when compressing values.
+      encoding(str): The encoding to use when persisting this Property
+        to Datastore.  Defaults to ``utf-8``.
+    """
+
+    _types = (str,)
