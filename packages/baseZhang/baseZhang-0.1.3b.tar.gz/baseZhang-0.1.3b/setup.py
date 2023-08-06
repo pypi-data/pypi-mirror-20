1f3e25e0ee0f65c85d404998d566cbce668c6811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:54 +0000

    Add package baseZhang/0.1.3b/baseZhang-0.1.3b.tar.gz

---
+++ packages/baseZhang/baseZhang-0.1.3b.tar.gz/baseZhang-0.1.3b/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+NAME = 'baseZhang'
+VERSION = '0.1.3b'
+DOWNLOAD = "https://github.com/zhangxulong/baseZhang/archive/baseZhang-0.1.1.tar.gz"
+DESCRIPTION = "Own base util code"
+setup(
+    py_modules=['baseCodes', 'demo', 'getMFCC'],
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    author="ZHANG Xu-long",
+    author_email="fudan0027zxl@gmail.com",
+    license="BSD",
+    url="http://zhangxulong.site",
+    download_url=DOWNLOAD,
+
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ],
+    zip_safe=False,
+)
