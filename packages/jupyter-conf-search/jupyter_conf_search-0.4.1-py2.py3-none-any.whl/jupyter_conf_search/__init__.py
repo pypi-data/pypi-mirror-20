e516cb557763ca6f1bd64e67a72a2abecea4a415
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:43 +0000

    Add package jupyter-conf-search/0.4.1/jupyter_conf_search-0.4.1-py2.py3-none-any.whl

---
+++ packages/jupyter-conf-search/jupyter_conf_search-0.4.1-py2.py3-none-any.whl/jupyter_conf_search/__init__.py
@@ -0,0 +1,8 @@
+"""Utility for searching through jupyter configuration files, using jupyter's path definitions to find their locations dynamically.
+
+"""
+
+__version__="0.4.1"
+
+from .jupyter_conf_search import main
+
