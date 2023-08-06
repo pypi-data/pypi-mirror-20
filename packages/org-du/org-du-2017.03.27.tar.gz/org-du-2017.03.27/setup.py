e59d02066e8d1e7771fb1bd07fd38878d2181263
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:40 +0000

    Add package org-du/2017.03.27/org-du-2017.03.27.tar.gz

---
+++ packages/org-du/org-du-2017.03.27.tar.gz/org-du-2017.03.27/setup.py
@@ -0,0 +1,36 @@
+from distutils.core import setup
+
+setup(
+    name="org-du",
+    version="2017.03.27",
+    description="du-like output for lines of GNU/Emacs Org-mode files",
+    author="Karl Voit",
+    author_email="tools@Karl-Voit.at",
+    url="https://github.com/novoid/du-org",
+    download_url="https://github.com/novoid/du-org/zipball/master",
+    keywords=["orgmode", "org-mode", "du", "xdu", "visualization"],
+    install_requires=["importlib", "logging", "argparse", "time", "codecs"],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        ],
+    long_description="""\
+org-du
+-----------------------------
+This Python 3 script parses parses a list of Org-mode files and
+generates output similar to "du" (disk usage) but with lines of
+Org-mode instead of kilobytes.
+
+The purpose of this script is to use its output as the input for "xdu" in order
+to get a graphical visualization:
+
+: org-du.py my_org_file.org another_org_file.org | xdu
+
+The script accepts an arbitrary number of files (see your shell for
+possible length limitations).
+
+Please read https://github.com/novoid/org-de/ for further information and descriptions.
+"""
+)
