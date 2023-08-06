9f74880b7c518698b02ee1be24b2ae6273a7dc48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:59 +0000

    Add package oommfc/0.7.1/oommfc-0.7.1-py3-none-any.whl

---
+++ packages/oommfc/oommfc-0.7.1-py3-none-any.whl/oommfc/tests/test_zeeman.py
@@ -0,0 +1,28 @@
+import oommfc as oc
+import micromagneticmodel.tests as mmt
+
+
+class TestZeeman(mmt.TestZeeman):
+    def test_script(self):
+        for H in self.valid_args:
+            name = 'zeeman_test'
+            zeeman = oc.Zeeman(H, name=name)
+
+            script = zeeman._script
+            assert script.count("\n") == 10
+            assert script[0] == "#"
+            assert script[-1] == "\n"
+
+            lines = script.split("\n")
+            assert len(lines) == 11
+            assert lines[0] == "# FixedZeeman"
+            assert lines[1] == "Specify Oxs_FixedZeeman:{} {{".format(name)
+            assert lines[2] == "  field {"
+            assert lines[3] == "    Oxs_UniformVectorField {"
+            assert lines[4] == "      vector {{{} {} {}}}".format(H[0],
+                                                                  H[1],
+                                                                  H[2])
+            assert lines[5] == "    }"
+            assert lines[6] == "  }"
+            assert lines[7] == "  multiplier 1"
+            assert lines[8] == "}"
