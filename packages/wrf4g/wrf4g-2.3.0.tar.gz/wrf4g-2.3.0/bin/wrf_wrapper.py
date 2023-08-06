bd1310767031c0ff2780a43a74d79b7c575825e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:29 +0000

    Add package wrf4g/2.3.0/wrf4g-2.3.0.tar.gz

---
+++ packages/wrf4g/wrf4g-2.3.0.tar.gz/wrf4g-2.3.0/bin/wrf_wrapper.py
@@ -0,0 +1,45 @@
+#
+# Copyright 2016 Universidad de Cantabria
+#
+# Licensed under the EUPL, Version 1.1 only (the
+# "Licence");
+# You may not use this work except in compliance with the
+# Licence.
+# You may obtain a copy of the Licence at:
+#
+# http://ec.europa.eu/idabc/eupl
+#
+# Unless required by applicable law or agreed to in
+# writing, software distributed under the Licence is
+# distributed on an "AS IS" basis,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied.
+# See the Licence for the specific language governing
+# permissions and limitations under the Licence.
+#
+
+import os
+import sys
+import logging
+from wrf4g.wrapper   import launch_wrapper, JobError, PilotParams
+
+__version__  = '2.3.0'
+__author__   = 'Carlos Blanco'
+__revision__ = "$Id$"
+
+if __name__ == '__main__':
+    try :
+        ##
+        # Define variables
+        ##
+        params = PilotParams()
+        launch_wrapper( params )
+    except JobError as err :
+        try :
+            logging.error( err.msg )
+        except :
+            sys.stderr.write( err.msg )
+        sys.exit( err.exit_code )
+    except Exception as err :
+        sys.stderr.write( str( err ) )
+        sys.exit( -1 )
---
+++ packages/wrf4g/wrf4g-2.3.0.tar.gz/wrf4g-2.3.0/wrf4g/commands/stop.py
@@ -0,0 +1,45 @@
+#
+# Copyright 2016 Universidad de Cantabria
+#
+# Licensed under the EUPL, Version 1.1 only (the
+# "Licence");
+# You may not use this work except in compliance with the
+# Licence.
+# You may obtain a copy of the Licence at:
+#
+# http://ec.europa.eu/idabc/eupl
+#
+# Unless required by applicable law or agreed to in
+# writing, software distributed under the Licence is
+# distributed on an "AS IS" basis,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied.
+# See the Licence for the specific language governing
+# permissions and limitations under the Licence.
+#
+
+"""
+Stop DRM4G daemon and ssh-agent. 
+    
+Usage: 
+    wrf4g stop [ --dbg ] 
+   
+Options:
+   --dbg    Debug mode.
+"""
+
+import logging
+import sys
+from drm4g.commands    import Daemon, Agent
+
+def run( arg ) :
+    try:
+        logging.basicConfig( format = '%(message)s',
+                         level  = logging.DEBUG if arg[ '--dbg' ] else logging.INFO,
+                         stream = sys.stdout )
+        Daemon().stop()
+        Agent().stop()
+    except KeyboardInterrupt :
+        pass
+    except Exception as err :
+        logging.error( str( err ) )
