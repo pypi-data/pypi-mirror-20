b40462adf68af2baf9e2047f5bfb4f3c44423ae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:37 +0000

    Add package pydojo/1.0/pydojo-1.0.tar.gz

---
+++ packages/pydojo/pydojo-1.0.tar.gz/pydojo-1.0/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+    name='pydojo',
+    packages=['pydojo'],
+    version='1.0',
+    description='A playful way to learn coding with Python',
+    author='Alessandro Norfo',
+    author_email='sprintingkiwi@gmail.com',
+    url='https://github.com/sprintingkiwi/PYDOJO',
+    download_url='https://github.com/sprintingkiwi/PYDOJO/tarball/1.0',
+    keywords=['game', 'development', 'learning', 'education'],
+    classifiers=[],
+    data_files=[('', ['pensurface.png', 'turtle.png'])],
+    include_package_data=True
+)
