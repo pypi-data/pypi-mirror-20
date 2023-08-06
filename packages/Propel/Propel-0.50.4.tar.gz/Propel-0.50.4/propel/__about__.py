48bb49c7f4647173bb521806f7e27b6f3639b258
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:30 +0000

    Add package Propel/0.50.4/Propel-0.50.4.tar.gz

---
+++ packages/Propel/Propel-0.50.4.tar.gz/Propel-0.50.4/propel/__about__.py
@@ -0,0 +1,23 @@
+
+""" __about__ """
+
+__all__ = [
+    "__title__",
+    "__summary__",
+    "__uri__",
+    "__version__",
+    "__author__",
+    "__email__",
+    "__license__",
+    "__copyright__",
+]
+
+
+__title__ = "Propel"
+__version__ = "0.50.4"
+__summary__ = "A package to deploy sites in virtualenv, run scripts, and deploy workers with supervisor"
+__uri__ = "http://github.com/mardix/propel/"
+__author__ = "Mardix"
+__email__ = "mardix@pylot.io"
+__license__ = "MIT"
+__copyright__ = "(c) 2017 %s" % __author__
