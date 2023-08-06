7aa25a8df86997f303a97838ed56dbb53d4992ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:45 +0000

    Add package reselpy_randomizer/0.3/reselpy_randomizer-0.3.tar.gz

---
+++ packages/reselpy_randomizer/reselpy_randomizer-0.3.tar.gz/reselpy_randomizer-0.3/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='reselpy_randomizer',
+    version='0.3',
+    description="A simple project that provides methods for the return random data",
+    author="Bob Reselman",
+    author_email='reselbob@gmail.com',
+    url='https://github.com/reselbob/reselpy_randomizer',
+    download_url='https://github.com/reselbob/reselpy_randomizer/archive/0.3.tar.gz',
+    packages=['reselpy_randomizer'],
+    include_package_data=True,
+    license="MIT license",
+    keywords=['reselpy_randomizer', 'randomizer', 'randomization', 'random'],
+    test_suite='tests'
+)
