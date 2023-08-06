a34b92814511aec40703c4c67be31ae02bd24eb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:05 +0000

    Add package antopy/0.2.0/antopy-0.2.0.zip

---
+++ packages/antopy/antopy-0.2.0.zip/antopy-0.2.0/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+setup(
+    name = 'antopy',
+    packages = ['antopy'],
+    install_requires=[
+          'paho-mqtt',
+    ],
+    version = '0.2.0',
+    description = 'Python Library for using anto.',
+    author = 'Isara Naranirattisai',
+    author_email = 'isaradream@gmail.com',
+    url = 'https://github.com/DreamN/AntoPY',
+    download_url = 'https://github.com/DreamN/AntoPY/archive/0.2.0.tar.gz',
+    keywords = ['anto'],
+    classifiers = [
+      'Topic :: Education',
+      'Topic :: Utilities'
+      ],
+)
