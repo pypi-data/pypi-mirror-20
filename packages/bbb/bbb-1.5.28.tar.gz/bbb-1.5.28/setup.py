42dae1d9209471b5c68067844f880a5971b2c5d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:05 +0000

    Add package bbb/1.5.28/bbb-1.5.28.tar.gz

---
+++ packages/bbb/bbb-1.5.28.tar.gz/bbb-1.5.28/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+setup(
+    name="bbb",
+    version="1.5.28",
+    description="Buildbot <-> Taskcluster Bridge",
+    author="Mozilla Release Engineering",
+    packages=["bbb", "bbb.schemas"],
+    entry_points={
+        "console_scripts": [
+            "buildbot-bridge = bbb.runner:main",
+        ],
+    },
+    # Package contains data files -> not safe.
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+        # Because taskcluster hard pins this version...
+        "requests==2.4.3",
+        "arrow",
+        "taskcluster>=0.0.26",
+        "sqlalchemy",
+        "kombu",
+        "redo",
+        "mysql-python",
+        "jsonschema",
+        "PyYAML",
+        "slugid",
+    ],
+    tests_require=[
+        "mock",
+        "flake8",
+        "pytest",
+        "pytest-cov",
+        "pytest-capturelog",
+    ],
+)
