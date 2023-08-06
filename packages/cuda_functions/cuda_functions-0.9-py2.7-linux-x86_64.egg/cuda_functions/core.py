e008aa56561028e4248688b0af4085f2469ae73a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:41 +0000

    Add package cuda_functions/0.9/cuda_functions-0.9-py2.7-linux-x86_64.egg

---
+++ packages/cuda_functions/cuda_functions-0.9-py2.7-linux-x86_64.egg/cuda_functions/core.py
@@ -0,0 +1,40 @@
+from cuda_functions.bin import gpu_correlate
+from cuda_functions.bin import gpu_fft
+
+import numpy as np
+
+
+def cuda_acorrelate(array, mode='valid', safe_mode=True):
+    if safe_mode:
+        array = np.ascontiguousarray(array)
+
+    if array.dtype == 'complex64':
+        return gpu_correlate.acorrelate(array, mode=mode)
+    elif array.dtype == 'complex128':
+        return gpu_correlate.dacorrelate(array, mode=mode)
+    else:
+        raise TypeError('{} type not supported (complex only)'.format(array.dtype))
+
+
+def cuda_fft(array, safe_mode=True):
+    if safe_mode:
+        array = np.ascontiguousarray(array)
+
+    if array.dtype == 'complex64':
+        return gpu_fft.fft(array)
+    elif array.dtype == 'complex128':
+        return gpu_fft.dfft(array)
+    else:
+        raise TypeError('{} type not supported (complex only)'.format(array.dtype))
+
+
+def cuda_ifft(array, safe_mode=True):
+    if safe_mode:
+        array = np.ascontiguousarray(array)
+
+    if array.dtype == 'complex64':
+        return gpu_fft.ifft(array)
+    elif array.dtype == 'complex128':
+        return gpu_fft.difft(array)
+    else:
+        raise TypeError('{} type not supported (complex only)'.format(array.dtype))
