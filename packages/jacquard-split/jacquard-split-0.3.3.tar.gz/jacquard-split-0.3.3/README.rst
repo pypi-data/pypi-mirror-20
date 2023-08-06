429602087da331073892d713566d2482d13c1e40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:27 +0000

    Add package jacquard-split/0.3.3/jacquard-split-0.3.3.tar.gz

---
+++ packages/jacquard-split/jacquard-split-0.3.3.tar.gz/jacquard-split-0.3.3/README.rst
@@ -0,0 +1,29 @@
+jacquard
+========
+
+.. image:: https://badge.fury.io/py/jacquard-split.svg
+    :target: https://badge.fury.io/py/jacquard-split
+
+.. image:: https://circleci.com/gh/prophile/jacquard.svg?style=shield
+    :target: https://circleci.com/gh/prophile/jacquard
+
+.. image:: https://readthedocs.org/projects/jacquard-split/badge/?version=latest
+    :target: http://jacquard-split.readthedocs.io/en/latest/
+
+Split-testing server.
+
+Installation
+------------
+
+Jacquard can be installed through `pip`:
+
+.. code:: bash
+
+    pip install jacquard-split
+
+Alternatively it can be built from `GitHub <https://github.com/prophile/jacquard>`_.
+
+Documentation
+-------------
+
+Full documentation is available in `ReadTheDocs <http://jacquard-split.readthedocs.io/en/latest/>`_.
---
+++ packages/jacquard-split/jacquard-split-0.3.3.tar.gz/jacquard-split-0.3.3/jacquard/users/commands.py
@@ -0,0 +1,151 @@
+"""General user settings commands."""
+
+import sys
+import contextlib
+
+import yaml
+
+from jacquard.users import get_settings
+from jacquard.commands import BaseCommand, CommandError
+from jacquard.storage.utils import retrying
+
+
+class SetDefault(BaseCommand):
+    """
+    Manipulate the current defaults.
+
+    This is one of the main commands used when adding new features. The
+    defaults are, as their name suggests, shared between all users.
+    """
+
+    help = "set (or clear) a default setting"
+
+    def add_arguments(self, parser):
+        """Add argparse arguments."""
+        parser.add_argument('setting', help="setting key")
+        mutex_group = parser.add_mutually_exclusive_group(required=True)
+        mutex_group.add_argument(
+            'value',
+            help="value to set",
+            nargs='?',
+        )
+        mutex_group.add_argument(
+            '-d',
+            '--delete',
+            help="clear the associated value",
+            action='store_true',
+        )
+
+    @retrying
+    def handle(self, config, options):
+        """Run command."""
+        with config.storage.transaction() as store:
+            defaults = dict(store.get('defaults', {}))
+
+            if options.delete:
+                with contextlib.suppress(KeyError):
+                    del defaults[options.setting]
+
+            else:
+                try:
+                    value = yaml.safe_load(options.value)
+                except ValueError:
+                    raise CommandError("Could not decode %r" % options.value)
+
+                defaults[options.setting] = value
+
+            store['defaults'] = defaults
+
+
+class Override(BaseCommand):
+    """
+    Configure per-user overrides.
+
+    Occasionally it is useful to set specific settings for specific users,
+    overriding the defaults and any experiments they may be in. This could
+    be for testing purposes on test or admin accounts, or even to give specific
+    users experiences they want in the name of customer support.
+    """
+
+    help = "control user overrides"
+
+    def add_arguments(self, parser):
+        """Add argparse arguments."""
+        parser.add_argument('user', help="user to override for")
+        parser.add_argument('setting', help="setting key")
+        mutex_group = parser.add_mutually_exclusive_group(required=False)
+        mutex_group.add_argument(
+            'value',
+            help="value to set",
+            nargs='?',
+        )
+        mutex_group.add_argument(
+            '-d',
+            '--delete',
+            help="clear the associated value",
+            action='store_true',
+        )
+
+    @retrying
+    def handle(self, config, options):
+        """Run command."""
+        with config.storage.transaction() as store:
+            key = 'overrides/%s' % options.user
+
+            overrides = dict(store.get(key, {}))
+
+            if options.delete:
+                with contextlib.suppress(KeyError):
+                    del overrides[options.setting]
+
+                if overrides == {}:
+                    with contextlib.suppress(KeyError):
+                        del store[key]
+                else:
+                    store[key] = overrides
+
+            elif options.value:
+                try:
+                    value = yaml.safe_load(options.value)
+                except ValueError:
+                    raise CommandError("Could not decode %r" % options.value)
+
+                overrides[options.setting] = value
+                store[key] = overrides
+
+            else:
+                yaml.dump(overrides, sys.stdout, default_flow_style=False)
+
+
+class Show(BaseCommand):
+    """
+    Show current settings for a given user.
+
+    This mirrors the main endpoint in the HTTP API and useful to see at a
+    glance what a specific user's settings are. Also can be used with no
+    arguments to show the current defaults.
+    """
+
+    help = "show settings for user"
+
+    def add_arguments(self, parser):
+        """Add argparse arguments."""
+        parser.add_argument(
+            'user',
+            help="user to show settings for",
+            nargs='?',
+        )
+
+    def handle(self, config, options):
+        """Run command."""
+        if options.user:
+            settings = get_settings(
+                options.user,
+                config.storage,
+                config.directory,
+            )
+        else:
+            with config.storage.transaction(read_only=True) as store:
+                settings = store.get('defaults', {})
+
+        yaml.dump(settings, sys.stdout, default_flow_style=False)
