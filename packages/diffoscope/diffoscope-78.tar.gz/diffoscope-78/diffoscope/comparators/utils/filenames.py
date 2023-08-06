c3df443fad595d828fbe3027c6d0bb4770721579
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:00 +0000

    Add package diffoscope/78/diffoscope-78.tar.gz

---
+++ packages/diffoscope/diffoscope-78.tar.gz/diffoscope-78/diffoscope/comparators/utils/filenames.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+#
+# diffoscope: in-depth comparison of files, archives, and directories
+#
+# Copyright © 2016 Chris Lamb <lamby@debian.org>
+#
+# diffoscope is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# diffoscope is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with diffoscope.  If not, see <https://www.gnu.org/licenses/>.
+
+import os
+
+
+def get_compressed_content_name(path, expected_extension):
+    basename = os.path.basename(path)
+    if basename.endswith(expected_extension):
+        name = basename[:-len(expected_extension)]
+    else:
+        name = "%s-content" % basename
+    return name
