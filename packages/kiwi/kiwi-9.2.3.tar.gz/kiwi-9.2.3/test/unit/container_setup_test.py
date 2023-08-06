c568c20725236a1b4cf6ffcc5ba8a898b341bd1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:48 +0000

    Add package kiwi/9.2.3/kiwi-9.2.3.tar.gz

---
+++ packages/kiwi/kiwi-9.2.3.tar.gz/kiwi-9.2.3/test/unit/container_setup_test.py
@@ -0,0 +1,20 @@
+
+from mock import patch
+
+import mock
+
+from .test_helper import *
+
+from kiwi.exceptions import *
+from kiwi.container.setup import ContainerSetup
+
+
+class TestContainerSetup(object):
+    @raises(KiwiContainerSetupError)
+    def test_container_not_implemented(self):
+        ContainerSetup('foo', 'root_dir')
+
+    @patch('kiwi.container.setup.ContainerSetupDocker')
+    def test_container_docker(self, mock_docker):
+        ContainerSetup('docker', 'root_dir')
+        mock_docker.assert_called_once_with('root_dir', None)
