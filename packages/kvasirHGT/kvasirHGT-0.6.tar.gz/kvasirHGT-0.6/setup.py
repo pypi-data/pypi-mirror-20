7833e804ce17edaa46cc7282e0ff52260e549703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:31 +0000

    Add package kvasirHGT/0.6/kvasirHGT-0.6.tar.gz

---
+++ packages/kvasirHGT/kvasirHGT-0.6.tar.gz/kvasirHGT-0.6/setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "kvasirHGT",
+    version = "0.6",
+    author = "Kevin Bonham, PhD",
+    author_email = "kevbonham@gmail.com",
+    description = "A package to identify HGT in bacterial genomes",
+    license = "MIT",
+    keywords = ["HGT", "biology", "bacteria", "genomics"],
+    url = "http://github.com/kescobo/kvasir",
+    download_url = 'https://github.com/kescobo/kvasir/archive/0.6-beta.tar.gz',
+    packages=['kvasir', 'tests'],
+    scripts=[os.path.join('bin', 'blast.py'),
+             os.path.join('bin', 'import_genomes.py')],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
