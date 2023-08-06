689341c2044eee053183632a375e93a2841eed8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:52 +0000

    Add package guillotina/1.0.0a2/guillotina-1.0.0a2-cp35-cp35m-macosx_10_12_x86_64.whl

---
+++ packages/guillotina/guillotina-1.0.0a2-cp35-cp35m-macosx_10_12_x86_64.whl/guillotina/cookiecutter/application/{{cookiecutter.package_name}}/{{cookiecutter.package_name}}/api.py
@@ -0,0 +1,13 @@
+from guillotina import configure
+from guillotina.api.service import Service
+from guillotina.browser import Response
+from guillotina.interfaces import IContainer
+
+
+@configure.service(context=IContainer, method='POST', permission='guillotina.AccessContent')
+class ExampleService(Service):
+
+    async def __call__(self):
+        return Response({
+            'foo': 'bar'
+        })
---
+++ packages/guillotina/guillotina-1.0.0a2-cp35-cp35m-macosx_10_12_x86_64.whl/guillotina/cookiecutter/application/{{cookiecutter.package_name}}/{{cookiecutter.package_name}}/install.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from guillotina.addons import Addon
+
+
+class ManageAddon(Addon):
+
+    @classmethod
+    def install(cls, container, request):
+        registry = request.container_settings  # noqa
+        # install logic here...
+
+    @classmethod
+    def uninstall(cls, container, request):
+        registry = request.container_settings  # noqa
+        # uninstall logic here...
