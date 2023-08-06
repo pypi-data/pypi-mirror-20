e50ad0559f9f8549726ec03f5dc62c8944e131f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:45 +0000

    Add package automodinit/0.13/automodinit-0.14.tar.gz

---
+++ packages/automodinit/automodinit-0.14.tar.gz/automodinit-0.14/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# automodinit
+# Solves the problem of forgetting to keep __init__.py files up to date
+# (C) 2012 Niall Douglas http://www.nedproductions.biz/
+# See http://pypi.python.org/pypi/automodinit for latest version
+# Go to http://github.com/ned14/automodinit to report bugs
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='automodinit',
+    version="0.14",
+    description='Solves the problem of forgetting to keep __init__.py files up to date',
+    author='Niall Douglas',
+    url='http://pypi.python.org/pypi/automodinit',
+    packages=["automodinit", ],
+    test_suite='tests',
+    install_requires=[],
+    )
