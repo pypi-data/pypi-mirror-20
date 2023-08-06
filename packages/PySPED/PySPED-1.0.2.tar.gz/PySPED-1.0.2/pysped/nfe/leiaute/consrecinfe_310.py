d3fba9cc1e0461439e2a3684e3ccc6ebadbd0107
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:54 +0000

    Add package PySPED/1.0.2/PySPED-1.0.2.tar.gz

---
+++ packages/PySPED/PySPED-1.0.2.tar.gz/PySPED-1.0.2/pysped/nfe/leiaute/consrecinfe_310.py
@@ -0,0 +1,184 @@
+# -*- coding: utf-8 -*-
+#
+# PySPED - Python libraries to deal with Brazil's SPED Project
+#
+# Copyright (C) 2010-2012
+# Copyright (C) Aristides Caldeira <aristides.caldeira at tauga.com.br>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# PySPED - Bibliotecas Python para o
+#          SPED - Sistema Público de Escrituração Digital
+#
+# Copyright (C) 2010-2012
+# Copyright (C) Aristides Caldeira <aristides.caldeira arroba tauga.com.br>
+#
+# Este programa é um software livre: você pode redistribuir e/ou modificar
+# este programa sob os termos da licença GNU Affero General Public License,
+# publicada pela Free Software Foundation, em sua versão 3 ou, de acordo
+# com sua opção, qualquer versão posterior.
+#
+# Este programa é distribuido na esperança de que venha a ser útil,
+# porém SEM QUAISQUER GARANTIAS, nem mesmo a garantia implícita de
+# COMERCIABILIDADE ou ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Veja a
+# GNU Affero General Public License para mais detalhes.
+#
+# Você deve ter recebido uma cópia da GNU Affero General Public License
+# juntamente com este programa. Caso esse não seja o caso, acesse:
+# <http://www.gnu.org/licenses/>
+#
+
+from __future__ import division, print_function, unicode_literals
+
+from pysped.xml_sped import *
+from pysped.nfe.leiaute import ESQUEMA_ATUAL_VERSAO_3 as ESQUEMA_ATUAL
+from pysped.nfe.leiaute import consrecinfe_200
+import os
+from nfe_310 import NFe
+from nfce_310 import NFCe
+
+
+DIRNAME = os.path.dirname(__file__)
+
+
+class ConsReciNFe(consrecinfe_200.ConsReciNFe):
+    def __init__(self):
+        super(ConsReciNFe, self).__init__()
+        self.versao  = TagDecimal(nome='consReciNFe', codigo='BP02', propriedade='versao', namespace=NAMESPACE_NFE, valor='3.10', raiz='/')
+        self.caminho_esquema = os.path.join(DIRNAME, 'schema/', ESQUEMA_ATUAL + '/')
+        self.arquivo_esquema = 'consReciNFe_v3.10.xsd'
+
+
+class InfProt(consrecinfe_200.InfProt):
+    def __init__(self):
+        super(InfProt, self).__init__()
+        self.dhRecbto = TagDataHoraUTC(nome='dhRecbto', codigo='PR08', raiz='//infProt')
+
+    def get_xml(self):
+        xml = XMLNFe.get_xml(self)
+
+        if self.Id.valor:
+            xml += self.Id.xml
+        else:
+            xml += '<infProt>'
+
+        xml += self.tpAmb.xml
+        xml += self.verAplic.xml
+        xml += self.chNFe.xml
+        xml += self.dhRecbto.xml
+        xml += self.nProt.xml
+        xml += self.digVal.xml
+        xml += self.cStat.xml
+        xml += self.xMotivo.xml
+        xml += '</infProt>'
+        return xml
+
+    def set_xml(self, arquivo):
+        if self._le_xml(arquivo):
+            self.Id.xml        = arquivo
+            self.tpAmb.xml     = arquivo
+            self.verAplic.xml  = arquivo
+            self.chNFe.xml     = arquivo
+            self.dhRecbto.xml  = arquivo
+            self.nProt.xml     = arquivo
+            self.digVal.xml    = arquivo
+            self.cStat.xml     = arquivo
+            self.xMotivo.xml   = arquivo
+
+    xml = property(get_xml, set_xml)
+
+
+class ProtNFe(consrecinfe_200.ProtNFe):
+    def __init__(self):
+        super(ProtNFe, self).__init__()
+        self.versao  = TagDecimal(nome='protNFe', codigo='PR02' , propriedade='versao', namespace=NAMESPACE_NFE, valor='3.10', raiz='/')
+        self.infProt = InfProt()
+
+
+class RetConsReciNFe(consrecinfe_200.RetConsReciNFe):
+    def __init__(self):
+        super(RetConsReciNFe, self).__init__()
+        self.versao   = TagDecimal(nome='retConsReciNFe', codigo='BR02' , propriedade='versao', namespace=NAMESPACE_NFE, valor='3.10', raiz='/')
+        self.caminho_esquema = os.path.join(DIRNAME, 'schema/', ESQUEMA_ATUAL + '/')
+        self.arquivo_esquema = 'retConsReciNFe_v3.10.xsd'
+
+    def get_xml(self):
+        xml = XMLNFe.get_xml(self)
+        xml += self.versao.xml
+        xml += self.tpAmb.xml
+        xml += self.verAplic.xml
+        xml += self.nRec.xml
+        xml += self.cStat.xml
+        xml += self.xMotivo.xml
+        xml += self.cUF.xml
+        xml += self.cMsg.xml
+        xml += self.xMsg.xml
+
+        for pn in self.protNFe:
+            xml += pn.xml
+
+        xml += '</retConsReciNFe>'
+        return xml
+
+    def set_xml(self, arquivo):
+        if self._le_xml(arquivo):
+            self.versao.xml   = arquivo
+            self.tpAmb.xml    = arquivo
+            self.verAplic.xml = arquivo
+            self.nRec.xml     = arquivo
+            self.cStat.xml    = arquivo
+            self.xMotivo.xml  = arquivo
+            self.cUF.xml      = arquivo
+            self.cMsg.xml     = arquivo
+            self.xMsg.xml     = arquivo
+            self.protNFe      = self.le_grupo('//retConsReciNFe/protNFe', ProtNFe)
+
+            #
+            # Monta o dicionário dos protocolos
+            #
+            for pn in self.protNFe:
+                self.dic_protNFe[pn.infProt.chNFe.valor] = pn
+
+    xml = property(get_xml, set_xml)
+
+
+class ProcNFe(consrecinfe_200.ProcNFe):
+    def __init__(self):
+        super(ProcNFe, self).__init__()
+        self.versao  = TagDecimal(nome='nfeProc', propriedade='versao', namespace=NAMESPACE_NFE, valor='3.10', raiz='/')
+        self.NFe     = NFe()
+        self.protNFe = ProtNFe()
+        self.caminho_esquema = os.path.join(DIRNAME, 'schema/', ESQUEMA_ATUAL + '/')
+        self.arquivo_esquema = 'procNFe_v3.10.xsd'
+
+    def get_xml(self):
+        xml = XMLNFe.get_xml(self)
+        xml += ABERTURA
+        xml += self.versao.xml
+        xml += self.NFe.xml.replace(ABERTURA, '')
+        xml += self.protNFe.xml.replace(ABERTURA, '')
+        xml += '</nfeProc>'
+        return xml
+
+    def set_xml(self, arquivo):
+        if self._le_xml(arquivo):
+            self.NFe.xml     = arquivo
+
+            if self.NFe.infNFe.ide.mod.valor == 65:
+                self.NFe = NFCe()
+                self.NFe.xml = arquivo
+
+            self.protNFe.xml = arquivo
+
+    xml = property(get_xml, set_xml)
---
+++ packages/PySPED/PySPED-1.0.2.tar.gz/PySPED-1.0.2/setup.py
@@ -0,0 +1,59 @@
+from setuptools import setup
+
+setup(
+    name = "PySPED",
+    version = "1.0.2",
+    author = "Aristides Caldeira",
+    author_email = 'aristides.caldeira@tauga.com.br',
+    test_suite='tests',
+    keywords = ['nfe', 'nfse', 'cte', 'sped', 'edf', 'ecd'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    packages = [
+        'pysped',
+        'pysped.nfe',
+        'pysped.nfe.leiaute',
+        'pysped.nfe.danfe',
+        'pysped.nfe.manual_300',
+        'pysped.nfe.manual_401',
+        'pysped.cte',
+        'pysped.cte.leiaute',
+        'pysped.cte.dacte',
+        'pysped.efd',
+        #'pysped.nfse',
+        'pysped.xml_sped',
+        'pysped.ecd',
+        'pysped.nf_paulista',
+        'pysped.relato_sped',
+        'pysped.exemplos',
+    ],
+    package_data = {
+        'pysped.nfe.danfe': ['fonts/*', '*.odt'],
+        'pysped.relato_sped': ['fonts/*'],
+        'pysped.nfe.leiaute': ['schema/*/*'],
+        'pysped.cte.leiaute': ['schema/*/*'],
+        'pysped.xml_sped': ['cadeia-certificadora/*/*']
+    },
+    url = 'https://github.com/aricaldeira/PySPED',
+    license = 'LGPL-v2.1+',
+    description = 'PySPED is a library to implement all requirements of the Brazilian Public System of Digital Bookkeeping',
+    long_description = open('README.rst').read(),
+    requires=[
+        'lxml(>=3.7.3)',
+        'xmlsec(>=1.0.7)',
+        'Geraldo(>=0.4.16)',
+        'qrcode(>=5.3)',
+        'py3o.template(>=0.9.11)',
+        'sh(>=1.12.9)'
+    ],
+    tests_require=[
+        'pyflakes(>=0.6.1)',
+    ],
+)
