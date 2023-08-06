e9e4515b682b8e9878e91527454268d68ef65c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:14 +0000

    Add package HinetPy/0.3.2/HinetPy-0.3.2-py3-none-any.whl

---
+++ packages/HinetPy/HinetPy-0.3.2-py3-none-any.whl/HinetPy/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+.. include:: ../README.rst
+"""
+
+__title__ = 'HinetPy'
+__repo__ = 'https://github.com/seisman/HinetPy'
+__version__ = '0.3.2'
+__author__ = 'Dongdong Tian'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2013-2017 Dongdong Tian'
+
+from HinetPy.client import Client
+from HinetPy import win32
+
+__all__ = ["Client", "win32"]
