3992813a53ad7f67ecbc1208bdc88839aba47e15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:50 +0000

    Add package get-proxy/1.0.4/get_proxy-1.0.4.tar.gz

---
+++ packages/get-proxy/get_proxy-1.0.4.tar.gz/get_proxy-1.0.4/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='get_proxy',
+      version='1.0.4',
+      description='get proxies',
+      author='Lewie Luo',
+      author_email='luoyang941122@163.com',
+      py_modules=['get_proxy'],
+     )
