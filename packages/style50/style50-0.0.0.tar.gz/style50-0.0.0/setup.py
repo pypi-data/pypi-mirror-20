1973110ade40a17b20fc00b5708698e54f75a5bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:48 +0000

    Add package style50/0.0.0/style50-0.0.0.tar.gz

---
+++ packages/style50/style50-0.0.0.tar.gz/style50-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    author="CS50",
+    author_email="sysadmins@cs50.harvard.edu",
+    classifiers=["Topic :: Utilities"],
+    description="This is CS50 style.",
+    keywords="style50",
+    name="style50",
+    url="https://github.com/cs50/style50"
+)
