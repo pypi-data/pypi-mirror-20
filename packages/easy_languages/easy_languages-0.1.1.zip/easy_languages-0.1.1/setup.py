6040fb6144950d76afa92209169eab12d8e72c7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:35 +0000

    Add package easy_languages/0.1.1/easy_languages-0.1.1.zip

---
+++ packages/easy_languages/easy_languages-0.1.1.zip/easy_languages-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from os.path import join, dirname
+
+from setuptools import setup, find_packages
+
+import easy_languages
+
+__autor__ = 'Larty'
+
+
+setup(
+    name='easy_languages',
+    version=easy_languages.__version__,
+    packages=find_packages(),
+    long_description=open(join(dirname(__file__), 'README.txt')).read(),
+    include_package_data=True
+)
