1fba91622bf5f687e9ae61aa7d8836830bafc8f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:14 +0000

    Add package grafana-cassandra/0.0.0.29/grafana-cassandra-0.0.0.29.tar.gz

---
+++ packages/grafana-cassandra/grafana-cassandra-0.0.0.29.tar.gz/grafana-cassandra-0.0.0.29/setup.py
@@ -0,0 +1,23 @@
+import os
+
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='grafana-cassandra',
+    version='0.0.0.29',
+    packages=find_packages(),
+    include_package_data=True,
+    description=("Json responder to interface between cassandra and grafana"),
+    long_description=read('README'),
+    install_requires=[
+        'Cassandra-driver',
+        'pyYAML', 'cherrypy',
+    ],
+    entry_points='''
+        [console_scripts]
+        grafana-cassandra=grafana_cassandra.server:main
+    ''',
+)
