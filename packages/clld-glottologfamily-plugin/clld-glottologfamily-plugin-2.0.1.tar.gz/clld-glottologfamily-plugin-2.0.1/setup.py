d92a9acf3e3c36ef2ce94ac8527c16a56c858198
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:12 +0000

    Add package clld-glottologfamily-plugin/2.0.1/clld-glottologfamily-plugin-2.0.1.tar.gz

---
+++ packages/clld-glottologfamily-plugin/clld-glottologfamily-plugin-2.0.1.tar.gz/clld-glottologfamily-plugin-2.0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='clld-glottologfamily-plugin',
+    version='2.0.1',
+    description='clld-glottologfamily-plugin',
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+    ],
+    author='Robert Forkel',
+    author_email='xrotwang@googlemail.com',
+    url='https://github.com/clld/clld-glottologfamily-plugin',
+    keywords='web pyramid pylons',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'clld>=3.0.1',
+        'pyglottolog>=0.3.1',
+    ],
+    tests_require=[
+        'mock>=2.0',
+    ],
+    test_suite="clld_glottologfamily_plugin",
+    entry_points="""\
+    """)
