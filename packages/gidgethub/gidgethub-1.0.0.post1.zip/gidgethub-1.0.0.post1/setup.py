58c67b7c9964b00c61138b040770e1d91b58c24f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:39 +0000

    Add package gidgethub/1.0.0.post1/gidgethub-1.0.0.post1.zip

---
+++ packages/gidgethub/gidgethub-1.0.0.post1.zip/gidgethub-1.0.0.post1/setup.py
@@ -0,0 +1,38 @@
+import pathlib
+
+import setuptools
+
+
+tests_require = ['pytest>=3.0.0', 'pytest-asyncio']
+
+long_description = pathlib.Path("README.rst").read_text("utf-8")
+
+setuptools.setup(
+    name="gidgethub",
+    version="1.0.0.post1",
+    description="An async GitHub API library",
+    long_description=long_description,
+    url="https://gidgethub.readthedocs.io",
+    author="Brett Cannon",
+    author_email="brett@python.org",
+    license="Apache",
+    classifiers=[
+        'Intended Audience :: Developers',
+        "License :: OSI Approved :: Apache Software License",
+        "Development Status :: 4 - Beta",
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords="github sans-io async",
+    packages=setuptools.find_packages(),
+    zip_safe=True,
+    python_requires=">=3.6.0",
+    setup_requires=['pytest-runner>=2.11.0'],
+    tests_require=tests_require,
+    install_requires=['uritemplate>=3.0.0'],
+    extras_require={
+        "docs": ["sphinx"],
+        "test": tests_require,
+        "aiohttp": ["aiohttp"],
+    },
+)
