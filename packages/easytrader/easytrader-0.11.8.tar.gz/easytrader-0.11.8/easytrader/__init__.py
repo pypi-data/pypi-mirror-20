0e6caaa8ce833d851f6dbd3e5c6c00c062ac155b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:24 +0000

    Add package easytrader/0.11.8/easytrader-0.11.8.tar.gz

---
+++ packages/easytrader/easytrader-0.11.8.tar.gz/easytrader-0.11.8/easytrader/__init__.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+from .api import *
+from .httrader import HTTrader
+from .webtrader import WebTrader
+from .yhtrader import YHTrader
+from .yjbtrader import YJBTrader
+from .gftrader import GFTrader
+from .joinquant_follower import JoinQuantFollower
+from .ricequant_follower import RiceQuantFollower
+
+__version__ = '0.11.8'
+__author__ = 'shidenggui'
