ecc85c46c83eb2760476e1c8e6874529ec898315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:53 +0000

    Add package guillotina/1.0.0a3/guillotina-1.0.0a3-cp35-cp35m-macosx_10_12_x86_64.whl

---
+++ packages/guillotina/guillotina-1.0.0a3-cp35-cp35m-macosx_10_12_x86_64.whl/guillotina/api/__init__.py
@@ -0,0 +1,12 @@
+# these imports are done to force loading services
+from . import addons  # noqa
+from . import app  # noqa
+from . import behaviors  # noqa
+from . import container  # noqa
+from . import content  # noqa
+from . import files  # noqa
+from . import registry  # noqa
+from . import search  # noqa
+from . import types  # noqa
+from . import user  # noqa
+from . import ws  # noqa
