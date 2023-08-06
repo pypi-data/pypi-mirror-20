dc2da0dede1e5aa3f248f8f7dc7af0b717b1390c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:31 +0000

    Add package hdl/0.0.0/hdl-0.0.0.tar.gz

---
+++ packages/hdl/hdl-0.0.0.tar.gz/hdl-0.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hdl',
+    author='Keerthan Jaic',
+    author_email='jckeerthan@gmail.com',
+    # version=version,
+    # url='http://github.com/jck/hdl',
+    description='HDL',
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+)
