562aebb769039375a899163a33a475c8ddb4627a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:34 +0000

    Add package mkdocs-tamerdocs/0.0.2/mkdocs-tamerdocs-0.0.2.tar.gz

---
+++ packages/mkdocs-tamerdocs/mkdocs-tamerdocs-0.0.2.tar.gz/mkdocs-tamerdocs-0.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+VERSION='0.0.2'
+
+
+setup(
+    name="mkdocs-tamerdocs",
+    version=VERSION,
+    url='https://anantshri.github.io/mkdocs-tamerdocs/',
+    license='GPL',
+    description='Mkdocs theme used on AndroidTamer Documentation portals',
+    author='Anant Shrivastava',
+    author_email='anant@anantshri.info',
+    keywords = [ 'mkdocs','documentation','theme'], # 
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'mkdocs.themes':[
+            'tamerdocs=tamerdocs',
+        ]
+    },
+    zip_safe=False
+)
