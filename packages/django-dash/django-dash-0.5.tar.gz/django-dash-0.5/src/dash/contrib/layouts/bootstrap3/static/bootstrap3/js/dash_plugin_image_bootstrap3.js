514bfb681442043f71c8b64d97488bba8c129aa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:17 +0000

    Add package django-dash/0.5/django-dash-0.5.tar.gz

---
+++ packages/django-dash/django-dash-0.5.tar.gz/django-dash-0.5/src/dash/contrib/layouts/bootstrap3/static/bootstrap3/js/dash_plugin_image_bootstrap3.js
@@ -0,0 +1,17 @@
+/*
+    Document   : dash_plugin_image_bootstrap3.js
+    Created on : June 4, 2015, 00:04:26 PM
+    Author     : Artur Barseghyan (artur.barseghyan@gmail.com)
+    Description:
+        Image plugin for django-dash.
+
+        The following piece of code insures that images are opened using the
+        bootstrap 3 modal.
+*/
+;
+$(document).ready(function() {
+    // Add dashboard plugin.
+    //$('.pictonic .plugin-content-wrapper a').colorbox({
+    //    'opacity': '0.5'
+    //});
+});
