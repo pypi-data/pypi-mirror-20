334d87ff390a8e9a80cb8011b98a161aed4e6138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:10 +0000

    Add package gocept.pseudonymize/1.0/gocept.pseudonymize-1.0.tar.gz

---
+++ packages/gocept.pseudonymize/gocept.pseudonymize-1.0.tar.gz/gocept.pseudonymize-1.0/src/gocept/pseudonymize/__init__.py
@@ -0,0 +1,17 @@
+# flake8: NOQA
+from gocept.pseudonymize.pseudonymize import (
+    date,
+    datestring,
+    day,
+    decimal,
+    email,
+    iban,
+    integer,
+    license_tag,
+    month,
+    name,
+    phone,
+    text,
+    time,
+    year,
+    )
