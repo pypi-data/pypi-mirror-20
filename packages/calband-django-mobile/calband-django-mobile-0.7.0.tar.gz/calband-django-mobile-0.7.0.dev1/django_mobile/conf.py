e102f6a15b1bb4ce0cea5af3ee8d2339eae81163
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:17 +0000

    Add package calband-django-mobile/0.7.0.dev1/calband-django-mobile-0.7.0.tar.gz

---
+++ packages/calband-django-mobile/calband-django-mobile-0.7.0.tar.gz/calband-django-mobile-0.7.0.dev1/django_mobile/conf.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from django import VERSION as django_version
+from django.conf import settings as django_settings
+
+CACHE_LOADER_NAME = 'django_mobile.loader.CachedLoader'
+DJANGO_MOBILE_LOADER = 'django_mobile.loader.Loader'
+
+
+class SettingsProxy(object):
+    def __init__(self, settings, defaults):
+        self.settings = settings
+        self.defaults = defaults
+
+    def __getattr__(self, attr):
+        try:
+            return getattr(self.settings, attr)
+        except AttributeError:
+            try:
+                return getattr(self.defaults, attr)
+            except AttributeError:
+                raise AttributeError(u'settings object has no attribute "%s"' % attr)
+
+
+class defaults(object):
+    FLAVOURS = (u'full', u'mobile',)
+    DEFAULT_MOBILE_FLAVOUR = u'mobile'
+    FLAVOURS_TEMPLATE_PREFIX = u''
+    FLAVOURS_GET_PARAMETER = u'flavour'
+    FLAVOURS_STORAGE_BACKEND = u'cookie'
+    FLAVOURS_COOKIE_KEY = u'flavour'
+    FLAVOURS_COOKIE_HTTPONLY = False
+    FLAVOURS_SESSION_KEY = u'flavour'
+    FLAVOURS_TEMPLATE_LOADERS = []
+
+    if django_version >= (1, 8):
+        loaders = []
+        for template_engine in django_settings.TEMPLATES:
+            loaders += template_engine.get('OPTIONS', {}).get('loaders', [])
+    else:
+        loaders = django_settings.TEMPLATE_LOADERS
+
+    for loader in loaders:
+        if isinstance(loader, (tuple, list)) and loader[0] == CACHE_LOADER_NAME:
+            for cached_loader in loader[1]:
+                if cached_loader != DJANGO_MOBILE_LOADER:
+                    FLAVOURS_TEMPLATE_LOADERS.append(cached_loader)
+        elif loader != DJANGO_MOBILE_LOADER:
+            FLAVOURS_TEMPLATE_LOADERS.append(loader)
+    FLAVOURS_TEMPLATE_LOADERS = tuple(FLAVOURS_TEMPLATE_LOADERS)
+
+settings = SettingsProxy(django_settings, defaults)
