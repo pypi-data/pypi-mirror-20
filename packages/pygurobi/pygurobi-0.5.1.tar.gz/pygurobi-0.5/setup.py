4d73d5293c19a03e4359891956027f0eb9ec3497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:33 +0000

    Add package pygurobi/0.5/pygurobi-0.5.1.tar.gz

---
+++ packages/pygurobi/pygurobi-0.5.1.tar.gz/pygurobi-0.5/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pygurobi',
+  packages = ['pygurobi'], # this must be the same as the name above
+  version = '0.5',
+  description = 'Rapid interactive Gurobi model modification and analysis',
+  author = 'Andrew B. Martin',
+  author_email = 'andrew.brown.martin@gmail.com',
+  url = 'https://github.com/AndrewBMartin/pygurobi', # use the URL to the github repo
+  download_url = 'https://github.com/AndrewBMartin/pygurobi/tarball/0.5',
+  keywords = ['operations research', 'optimization', 'interactive'], # arbitrary keywords
+  classifiers = [],
+)
