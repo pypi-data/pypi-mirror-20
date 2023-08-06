ed25bbf6d75894f1f5b7ab5976c6b32b37d75a80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:44 +0000

    Add package tinnitus/0.1.4/tinnitus-0.1.4.tar.gz

---
+++ packages/tinnitus/tinnitus-0.1.4.tar.gz/tinnitus-0.1.4/setup.py
@@ -0,0 +1,100 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='tinnitus',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.1.4',
+
+    description='A pluggable playback queue service',
+    long_description=long_description,
+
+    # The project's main homepage.
+    # url='https://github.com/pypa/sampleproject',
+
+    # Author details
+    author='strangedev',
+    author_email='strangedev@posteo.net',
+
+    url="https://github.com/strangedev/tinnitus",
+    download_url="https://github.com/strangedev/tinnitus/tarball/0.1.4",
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Multimedia :: Sound/Audio',
+        'Topic :: Multimedia :: Sound/Audio :: Players',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+
+    # What does your project relate to?
+    keywords='tinnitus audio playback background service',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    # packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    packages=['tinnitus'],
+    package_dir={'tinnitus': 'tinnitus'},
+    package_data={'tinnitus': ['plugins/*.py', 'plugins/*.txt']},
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    #   py_modules=["my_module"],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['rpyc', 'python-vlc'],
+
+    entry_points={
+        'console_scripts': [
+            'tinnitusd = tinnitus.server:run_command',
+            'tinnitus-include = tinnitus.pluggable:configure_include_paths',
+        ],
+    },
+
+)
