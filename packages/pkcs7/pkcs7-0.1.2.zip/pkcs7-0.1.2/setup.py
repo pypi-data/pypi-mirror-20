77ad8ba8d570813382693c74efaa6c6ccb4155b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:20 +0000

    Add package pkcs7/0.1.2/pkcs7-0.1.2.zip

---
+++ packages/pkcs7/pkcs7-0.1.2.zip/pkcs7-0.1.2/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+from setuptools import setup
+import os
+
+setup(name='pkcs7',
+	version='0.1.2',
+	description='extension class for argparse with json configuration',
+	url='http://github.com/jeppeter/extargsparse',
+	author='jeppeter Wang',
+	author_email='jeppeter@gmail.com',
+	license='MIT',
+	packages=['pkcs7'],
+	zip_safe=False)
