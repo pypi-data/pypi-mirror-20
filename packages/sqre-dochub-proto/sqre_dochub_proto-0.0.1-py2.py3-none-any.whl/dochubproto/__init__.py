d57ba1e9069e6c5f2ad71ac49aca699a3bc75219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:45 +0000

    Add package sqre-dochub-proto/0.0.1/sqre_dochub_proto-0.0.1-py2.py3-none-any.whl

---
+++ packages/sqre-dochub-proto/sqre_dochub_proto-0.0.1-py2.py3-none-any.whl/dochubproto/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+from .sections import SECTIONS
+from .dochubproto import DocHubProto
+__all__ = ["SECTIONS", "DocHubProto"]
---
+++ packages/sqre-dochub-proto/sqre_dochub_proto-0.0.1-py2.py3-none-any.whl/dochubproto/sections.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+"""Top-level sections of DocHub content.
+"""
+
+SECTIONS = ["DMTN", "SQR", "SMTN"]
