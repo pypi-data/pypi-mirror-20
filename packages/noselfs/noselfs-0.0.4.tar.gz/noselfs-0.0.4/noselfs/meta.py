2b3d124038a2a7ac6f469dbee4b4a185002fffdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:46 +0000

    Add package noselfs/0.0.4/noselfs-0.0.4.tar.gz

---
+++ packages/noselfs/noselfs-0.0.4.tar.gz/noselfs-0.0.4/noselfs/meta.py
@@ -0,0 +1,11 @@
+__version__ = "0.0.4"
+
+"""
+Changelog:
+0.0.4: Addd Base test class for sandboxed tests
+0.0.3: Change to the way lfs looks for files- has a greater chance of finding
+    them now.
+0.0.2: Change order of arguments- allows users to put they tests in more
+    places, i.e.doesn't restrict to data/'type' anymore
+0.0.1: Initial version. Probably buggy
+"""
