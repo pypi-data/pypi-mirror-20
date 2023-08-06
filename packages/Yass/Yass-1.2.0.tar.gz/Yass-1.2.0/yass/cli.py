dce022ec6b6e0408aa101dae94d8221aaf7ffa92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:11 +0000

    Add package Yass/1.2.0/Yass-1.2.0.tar.gz

---
+++ packages/Yass/Yass-1.2.0.tar.gz/Yass-1.2.0/yass/cli.py
@@ -0,0 +1,374 @@
+
+import os
+import sys
+import time
+import click
+import pkg_resources
+from livereload import Server, shell
+from . import Yass, publisher
+from .yass import PAGE_FORMAT
+from .__about__ import *
+
+CWD = os.getcwd()
+
+
+TPL_HEADER = """
+---
+title: Page Title
+description: Page Description
+meta:
+    key: value
+---
+
+"""
+
+TPL_BODY = {
+
+# JADE
+    "jade": """
+.row
+    .col-md-12.text-center
+        h1
+            strong.
+                {{ page.title }}
+        h3.
+            Ok Yass!
+
+.row
+    .col-md-12
+""",
+
+# HTML
+
+    "html": """
+<div class=\"row\">
+    <div class=\"col-md-12\">
+            This is Yass!
+    </div>
+</div>
+""",
+
+# MD
+    "md": """
+
+# My markdown Yass!
+
+"""
+
+}
+
+
+def copy_resource(src, dest):
+    """
+    To copy package data to destination
+    """
+    package_name = "yass"
+    dest = (dest + "/" + os.path.basename(src)).rstrip("/")
+    if pkg_resources.resource_isdir(package_name, src):
+        if not os.path.isdir(dest):
+            os.makedirs(dest)
+        for res in pkg_resources.resource_listdir(__name__, src):
+            copy_resource(src + "/" + res, dest)
+    else:
+        if not os.path.isfile(dest) \
+                and os.path.splitext(src)[1] not in [".pyc"]:
+            with open(dest, "wb") as f:
+                f.write(pkg_resources.resource_string(__name__, src))
+        else:
+            print("File exists: %s " % dest)
+
+
+def stamp_yass_current_version(dir):
+    f = os.path.join(dir, "yass.yml")
+    if os.path.isfile(f):
+        with open(f, "r+") as file:
+            content = file.read()
+            content = content.replace("##VERSION##", __version__)
+            file.seek(0)
+            file.write(content)
+            file.truncate()
+
+
+def footer():
+    print("-" * 80)
+
+
+def alert(message):
+    print("::ALERT::")
+    print(message)
+
+
+def error(message):
+    print("::ERROR::")
+    print(message)
+
+
+@click.group()
+def cli():
+    """
+    Yass: Yet Another Static Site (generator)
+    """
+    pass
+
+@cli.command("version")
+def version():
+    """Return the vesion of Yass"""
+    print(__version__)
+    footer()
+
+@cli.command("build")
+def build():
+    """Build everything"""
+    print("Building pages...")
+    Yass(CWD).build()
+    print("Done!")
+
+    footer()
+
+
+@cli.command("publish")
+@click.argument("endpoint", default="s3")
+@click.option("--purge-files", is_flag=True)
+@click.option("--rebuild-manifest", is_flag=True)
+@click.option("--skip-upload", is_flag=True)
+def publish(endpoint, purge_files, rebuild_manifest, skip_upload):
+    """Publish the site"""
+    print("Publishing site to %s ..." % endpoint.upper())
+
+    yass = Yass(CWD)
+    target = endpoint.lower()
+
+    sitename = yass.sitename
+    if not sitename:
+        raise ValueError("Missing site name")
+
+    endpoint = yass.config.get("hosting.%s" % target)
+    if not endpoint:
+        raise ValueError("%s endpoint is missing in the config" % target.upper())
+
+    if target == "s3":
+        p = publisher.S3Website(sitename=sitename,
+                                aws_access_key_id=endpoint.get("aws_access_key_id"),
+                                aws_secret_access_key=endpoint.get("aws_secret_access_key"),
+                                region=endpoint.get("aws_region"))
+
+        if not p.website_exists:
+            print(">>>")
+            print("Setting S3 site...")
+            if p.create_website() is True:
+                # Need to give it enough time to create it
+                # Should be a one time thing
+                time.sleep(10)
+                p.create_www_website()
+                print("New bucket created: %s" % p.sitename)
+
+        if rebuild_manifest:
+            print(">>>")
+            print("Rebuilding site's manifest...")
+            p.create_manifest_from_s3_files()
+
+        if purge_files is True or endpoint.get("purge_files") is True:
+            print(">>>")
+            print("Purging files...")
+            exclude_files = endpoint.get("purge_exclude_files", [])
+            p.purge_files(exclude_files=exclude_files)
+
+        if not skip_upload:
+            print(">>>")
+            print("Uploading your site...")
+            p.upload(yass.build_dir)
+        else:
+            print(">>>")
+            print("WARNING: files upload was skipped because of the use of --skip-upload")
+
+        print("")
+        print("Yass! Your site has been successfully published to: ")
+        print(p.website_endpoint_url)
+
+    footer()
+
+
+
+@cli.command("setup-dns")
+@click.argument("endpoint", default="s3")
+def setup_dns(endpoint):
+    """Setup site domain to route to static site"""
+    print("Setting up DNS...")
+
+    yass = Yass(CWD)
+    target = endpoint.lower()
+
+    sitename = yass.sitename
+    if not sitename:
+        raise ValueError("Missing site name")
+
+    endpoint = yass.config.get("hosting.%s" % target)
+    if not endpoint:
+        raise ValueError(
+            "%s endpoint is missing in the hosting config" % target.upper())
+
+    if target == "s3":
+        p = publisher.S3Website(sitename=sitename,
+                                aws_access_key_id=endpoint.get("aws_access_key_id"),
+                                aws_secret_access_key=endpoint.get("aws_secret_access_key"),
+                                region=endpoint.get("aws_region"))
+
+        print("Setting AWS Route53 for: %s ..." % p.sitename)
+        p.setup_dns()
+        print("")
+        print("Yass! Route53 setup successfully!")
+        print("You can now visit the site at :")
+        print(p.sitename_endpoint)
+    footer()
+
+
+@cli.command("create-site")
+@click.argument("sitename")
+def create_site(sitename):
+    """Create a new site directory and init Yass"""
+    sitepath = os.path.join(CWD, sitename)
+    if os.path.isdir(sitepath):
+        print("Site directory '%s' exists already!" % sitename)
+    else:
+        print("Creating site: %s..." % sitename)
+        os.makedirs(sitepath)
+        copy_resource("skel/", sitepath)
+        stamp_yass_current_version(sitepath)
+        print("Site created successfully!")
+        print("CD into '%s' and run 'yass serve' to view the site" % sitename)
+
+    footer()
+
+
+@cli.command("init")
+def init():
+    """Initialize Yass in the current directory """
+    yass_conf = os.path.join(CWD, "yass.yml")
+    if os.path.isfile(yass_conf):
+        print("::ALERT::")
+        print("It seems like Yass is already initialized here.")
+        print("If it's a mistake, delete 'yass.yml' in this directory")
+    else:
+        print("Init Yass in %s ..." % CWD)
+        copy_resource("skel/", CWD)
+        stamp_yass_current_version(CWD)
+        print("Yass init successfully!")
+        print("Run 'yass serve' to view the site")
+
+    footer()
+
+
+@cli.command("create-page")
+@click.argument("pagename")
+def create_page(pagename):
+    """ Create a new page Omit the extension, it will create it as .jade file """
+    page = pagename.lstrip("/").rstrip("/")
+    _, _ext = os.path.splitext(pagename)
+
+    # If the file doesn't have an extension, we'll just create one
+    if not _ext or _ext == "":
+        page += ".jade"
+
+    if not page.endswith(PAGE_FORMAT):
+        error("Can't create '%s'" % page)
+        print("Invalid filename format")
+        print("Filename must be in: '%s'" % " | ".join(PAGE_FORMAT))
+    else:
+        engine = Yass(CWD)
+        markup = "jade"
+        if page.endswith(".md"):
+            markup = "md"
+        if page.endswith(".html"):
+            markup = "html"
+
+        dest_file = os.path.join(engine.pages_dir, page)
+        dest_dir = os.path.dirname(dest_file)
+
+        content = TPL_HEADER
+        content += TPL_BODY[markup]
+
+        if os.path.isfile(dest_file):
+            error("File exists already")
+            print("Location: %s" % dest_file)
+
+        else:
+            if not os.path.isdir(dest_dir):
+                os.makedirs(dest_dir)
+            with open(dest_file, "w") as f:
+                f.write(content)
+
+            print("New page created: '%s'" % page)
+            print("Location: %s" % dest_file)
+
+
+    footer()
+
+@cli.command()
+@click.option("-p", "--port", default=None)
+@click.option("--no-livereload", default=None)
+@click.option("--open-url", default=None)
+def serve(port, no_livereload, open_url):
+    """Serve the site """
+
+    engine = Yass(CWD)
+    if not port:
+        port = engine.config.get("local_server.port", 8000)
+    if no_livereload is None:
+        no_livereload = True if engine.config.get("local_server.livereload") is False else False
+    if open_url is None:
+        open_url = False if engine.config.get("local_server.open_url") is False else True
+
+    print("Serving at %s" % port)
+    print("Livereload is %s" % ("OFF" if no_livereload else "ON"))
+
+    def build_static():
+        engine.build_static()
+
+    def build_pages():
+        engine.build_pages()
+
+    engine.build()
+
+    server = Server()
+    if no_livereload is False:
+        server.watch(engine.static_dir + "/", build_static)
+        server.watch(engine.pages_dir + "/", build_pages)
+        server.watch(engine.templates_dir + "/", build_pages)
+        server.watch(engine.data_dir + "/", build_pages)
+
+    server.serve(open_url_delay=open_url, port=port, root=engine.build_dir)
+
+
+@cli.command("clean")
+def clean():
+    """Clean the build dir """
+    print("Cleaning build dir...")
+    Yass(CWD).clean_build_dir()
+    print("Done!")
+    footer()
+
+
+def cmd():
+    try:
+        print("*" * 80)
+        print("=" * 80)
+        print("Yass %s!" % __version__)
+        print("-" * 80)
+        yass_conf = os.path.join(CWD, "yass.yml")
+        yass_init = os.path.isfile(yass_conf)
+        sys_argv = sys.argv
+        exempt_argv = ["init", "create-site", "version"]
+        if len(sys_argv) > 1:
+            if not yass_init and sys_argv[1] not in exempt_argv:
+                error("Yass is not initialized yet in this directory: %s" % CWD)
+                print("Run 'yass init' to initialize Yass in the current directory")
+                footer()
+            else:
+                cli()
+        else:
+            cli()
+    except Exception as e:
+        print("Ohhh noooooo! Something bad happens")
+        print(">> %s " % e.__repr__())
+
+
---
+++ packages/Yass/Yass-1.2.0.tar.gz/Yass-1.2.0/yass/publisher.py
@@ -0,0 +1,291 @@
+
+import botocore
+import boto3
+import json
+import os
+import threading
+import uuid
+import tempfile
+from . import utils
+
+
+class S3Website(object):
+    """
+    To manage S3 website and domain on Route53
+    """
+    S3_HOSTED_ZONE_IDS = {
+        'us-east-1': 'Z3AQBSTGFYJSTF',
+        'us-west-1': 'Z2F56UZL2M1ACD',
+        'us-west-2': 'Z3BJ6K6RIION7M',
+        'ap-south-1': 'Z11RGJOFQNVJUP',
+        'ap-northeast-1': 'Z2M4EHUR26P7ZW',
+        'ap-northeast-2': 'Z3W03O7B5YMIYP',
+        'ap-southeast-1': 'Z3O0J2DXBE1FTB',
+        'ap-southeast-2': 'Z1WCIGYICN2BYD',
+        'eu-central-1': 'Z21DNDUVLTQW6Q',
+        'eu-west-1': 'Z1BKCTXD74EZPE',
+        'sa-east-1': 'Z7KQH4QJS55SO',
+        'us-gov-west-1': 'Z31GFT0UA1I2HV',
+    }
+
+    manifest_file = ".yass-manifest"
+
+    def __init__(self,
+                 sitename,
+                 region="us-east-1",
+                 aws_access_key_id=None,
+                 aws_secret_access_key=None):
+        """
+
+        :param sitename: the website name to create, without WWW.
+        :param region: the region of the site
+        :param access_key_id: AWS
+        :param secret_access_key: AWS
+        :param setup_dns: bool - If True it will create route53
+        :param allow_www: Bool - If true, it will create a second bucket with www.
+        """
+
+        # This will be used to pass to concurrent upload
+        self.aws_params = {
+            "aws_access_key_id": aws_access_key_id,
+            "aws_secret_access_key": aws_secret_access_key,
+            "region_name": region
+        }
+        self.region = region
+        self.s3 = boto3.client('s3', **self.aws_params)
+        self.sitename = sitename
+        self.www_sitename = "www." + self.sitename
+        self.website_endpoint = "%s.s3-website-%s.amazonaws.com" % (self.sitename, region)
+        self.website_endpoint_url = "http://" + self.website_endpoint
+        self.sitename_endpoint = "http://" + self.sitename
+        exists, error_code, error_message = self.head_bucket(self.sitename)
+        self.website_exists = exists
+
+    def setup_dns(self):
+        route53 = boto3.client('route53', **self.aws_params)
+
+        hosted_zone_id = self._get_route53_hosted_zone_by_domain(self.sitename)
+        if not hosted_zone_id:
+            caller_reference_uuid = "%s" % (uuid.uuid4())
+            response = route53.create_hosted_zone(
+                Name=self.sitename,
+                CallerReference=caller_reference_uuid,
+                HostedZoneConfig={'Comment': "HostedZone created by YASS!", 'PrivateZone': False})
+            hosted_zone_id = response['HostedZone']['Id']
+
+        website_dns_name = "s3-website-%s.amazonaws.com" % self.region
+        redirect_dns_name = "s3-website-%s.amazonaws.com" % self.region
+
+        change_batch_payload = {
+            'Changes': [
+                {
+                    'Action': 'UPSERT',
+                    'ResourceRecordSet': {
+                        'Name': self.sitename,
+                        'Type': 'A',
+                        'AliasTarget': {
+                            'HostedZoneId': self.S3_HOSTED_ZONE_IDS[self.region],
+                            'DNSName': website_dns_name,
+                            'EvaluateTargetHealth': False
+                        }
+                    }
+                },
+                {
+                    'Action': 'UPSERT',
+                    'ResourceRecordSet': {
+                        'Name': self.www_sitename,
+                        'Type': 'A',
+                        'AliasTarget': {
+                            'HostedZoneId': self.S3_HOSTED_ZONE_IDS[self.region],
+                            'DNSName': redirect_dns_name,
+                            'EvaluateTargetHealth': False
+                        }
+                    }
+                }
+            ]
+        }
+
+        response = route53.change_resource_record_sets(
+            HostedZoneId=hosted_zone_id,
+            ChangeBatch=change_batch_payload)
+        return True if response and "ChangeInfo" in response else False
+
+    def _get_route53_hosted_zone_by_domain(self, domain):
+        route53 = boto3.client('route53', **self.aws_params)
+        hosted_zone = route53.list_hosted_zones()
+
+        if hosted_zone or "HostedZones" in hosted_zone:
+            for hz in hosted_zone["HostedZones"]:
+                if hz["Name"].rstrip(".") == domain:
+                    return hz["Id"]
+        return None
+
+    def head_bucket(self, name):
+        """
+        Check if a bucket exists
+        :param name:
+        :return:
+        """
+        try:
+            self.s3.head_bucket(Bucket=name)
+            info = self.s3.get_bucket_website(Bucket=self.sitename)
+            if not info:
+                return False, 404, "Configure improrperly"
+            return True, None, None
+        except botocore.exceptions.ClientError as e:
+            if e.response["Error"]["Code"] in ["403", "404"]:
+                return False, e.response["Error"]["Code"], e.response["Error"]["Message"]
+            else:
+                raise e
+
+    def create_website(self):
+        exists, error_code, error_message = self.head_bucket(self.sitename)
+        if not exists:
+            if error_code == "404":
+                # Allow read access
+                policy_payload = {
+                    "Version": "2012-10-17",
+                    "Statement": [{
+                        "Sid": "Allow Public Access to All Objects",
+                        "Effect": "Allow",
+                        "Principal": "*",
+                        "Action": "s3:GetObject",
+                        "Resource": "arn:aws:s3:::%s/*" % (self.sitename)
+                    }
+                    ]
+                }
+                # Make bucket website and add index.html and error.html
+                website_payload = {
+                    'ErrorDocument': {
+                        'Key': 'error.html'
+                    },
+                    'IndexDocument': {
+                        'Suffix': 'index.html'
+                    }
+                }
+                self.s3.create_bucket(Bucket=self.sitename)
+                self.s3.put_bucket_policy(Bucket=self.sitename,
+                                          Policy=json.dumps(policy_payload))
+                self.s3.put_bucket_website(Bucket=self.sitename,
+                                           WebsiteConfiguration=website_payload)
+                return True
+            else:
+                raise Exception("Can't create website's bucket '%s' on AWS S3. "
+                                "Error: %s" % (self.sitename, error_message))
+        return False
+
+    def create_www_website(self):
+        exists, error_code, error_message = self.head_bucket(self.sitename)
+        if not exists:
+            if error_code == "404":
+                self.s3.create_bucket(Bucket=self.www_sitename)
+                redirect_payload = {
+                    'RedirectAllRequestsTo': {
+                        'HostName': self.sitename,
+                        'Protocol': 'http'
+                    }
+                }
+                bucket_website_redirect = self.s3.BucketWebsite(self.www_sitename)
+                bucket_website_redirect.put(WebsiteConfiguration=redirect_payload)
+                return True
+            else:
+                raise Exception("Can't create website's bucket '%s' on AWS S3. "
+                                "Error: %s" % (self.www_sitename, error_message))
+        return False
+
+    def upload(self, build_dir):
+        """
+
+        :param build_dir: The directory to upload
+        :param save_manifest: bool: To save manifest file
+        :param purge: bool : To delete previously uploaded files
+        :return:
+        """
+        files_list = []
+        for root, dirs, files in os.walk(build_dir):
+            for filename in files:
+                local_path = os.path.join(root, filename)
+                s3_path = os.path.relpath(local_path, build_dir)
+                mimetype = utils.get_mimetype(local_path)
+
+                kwargs = dict(aws_params=self.aws_params,
+                              bucket_name=self.sitename,
+                              local_path=local_path,
+                              s3_path=s3_path,
+                              mimetype=mimetype)
+
+                files_list.append(s3_path)
+                threading.Thread(target=self._upload_file, kwargs=kwargs)\
+                    .start()
+
+        # Save the files that have been uploaded
+        self._set_manifest_data(files_list)
+
+    def purge_files(self, exclude_files=["index.html", "error.html"]):
+        """
+        To delete files that are in the manifest
+        :param excludes_files: list : files to not delete
+        :return:
+        """
+        for chunk in utils.chunk_list(self._get_manifest_data(), 1000):
+            try:
+                self.s3.delete_objects(
+                    Bucket=self.sitename,
+                    Delete={
+                        'Objects': [{"Key": f} for f in chunk
+                                    if f not in exclude_files]
+                    }
+                )
+            except Exception as ex:
+                pass
+
+    def create_manifest_from_s3_files(self):
+        """
+        To create a manifest db for the current
+        :return:
+        """
+        for k in self.s3.list_objects(Bucket=self.sitename)['Contents']:
+            key = k["Key"]
+            files = []
+            if key not in [self.manifest_file]:
+                files.append(key)
+            self._set_manifest_data(files)
+
+    def _set_manifest_data(self, files_list):
+        """
+        Write manifest files
+        :param files_list: list
+        :return:
+        """
+        if files_list:
+            data = ",".join(files_list)
+            self.s3.put_object(Bucket=self.sitename,
+                               Key=self.manifest_file,
+                               Body=data,
+                               ACL='private')
+
+    def _get_manifest_data(self):
+        """
+        Return the list of items in the manifest
+        :return: list
+        """
+        with tempfile.NamedTemporaryFile(delete=True) as tmp:
+            try:
+                self.s3.download_fileobj(self.sitename, self.manifest_file, tmp)
+                tmp.seek(0)
+                data = tmp.read()
+                if data is not None:
+                    return data.split(",")
+            except Exception as ex:
+                pass
+        return []
+
+    @staticmethod
+    def _upload_file(aws_params, bucket_name, local_path, s3_path, mimetype):
+        s3 = boto3.client("s3", **aws_params)
+        s3.upload_file(local_path,
+                       Bucket=bucket_name,
+                       Key=s3_path,
+                       ExtraArgs={"ContentType": mimetype})
+
+
