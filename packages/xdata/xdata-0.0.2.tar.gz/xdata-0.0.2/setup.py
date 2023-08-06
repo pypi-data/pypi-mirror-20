59f1eea55a7e83eb2966dee80ed2343fe0290c1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:32 +0000

    Add package xdata/0.0.2/xdata-0.0.2.tar.gz

---
+++ packages/xdata/xdata-0.0.2.tar.gz/xdata-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='xdata',
+    version='0.0.2',
+    author='gaojiuli',
+    author_email='gaojiuli@gmail.com',
+    description="Simple data validation library",
+    license="MIT",
+    keywords="schema json validation",
+    url="https://github.com/gaojiuli/xdata",
+    package=['xdata'],
+    platforms='any',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
