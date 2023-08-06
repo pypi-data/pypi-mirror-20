ae5f16da094fe166cd3b3a44c030044aa7eb4c85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:35 +0000

    Add package PyLMNN/1.2.2/PyLMNN-1.2.2.tar.gz

---
+++ packages/PyLMNN/PyLMNN-1.2.2.tar.gz/PyLMNN-1.2.2/pylmnn/bayesopt.py
@@ -0,0 +1,95 @@
+import numpy as np
+from GPyOpt.methods import BayesianOptimization
+from sklearn.neighbors import KNeighborsClassifier
+from .lmnn import LargeMarginNearestNeighbor
+
+
+def find_hyperparams(x_tr, y_tr, x_va, y_va, params, max_trials=12):
+    """Find the best hyperparameters for LMNN for a specified number of trials using Bayesian Optimisation
+
+    Args:
+        x_tr (array_like): [N, D], the training samples
+        y_tr (array_like): [N,], the training labels
+        x_va (array_like): [M, D], the testing samples
+        y_va (array_like): [M,], the testing samples
+        params (dict):     parameters to be passed to the LargeMarginNearestNeighbor classifier
+        max_trials (int):  maximum number of hyperparameter configurations to test (default: 12)
+
+    Returns:
+        tuple:
+
+        (int, int, int, int) The best hyperparameters found (k_tr, k_te, dim_out, max_iter)
+    """
+
+    unique_labels, class_sizes = np.unique(y_tr, return_counts=True)
+    min_class_size = min(class_sizes)
+
+    # Setting parameters for Bayesian Global Optimization
+    class BOptions: pass
+
+    opt = BOptions()
+    opt.max_trials = max_trials
+    opt.min_k = 1
+    opt.max_k = int(min(min_class_size - 1, 15))
+    opt.min_iter = 10
+    opt.max_iter = 200
+    opt.min_dim = min(x_tr.shape[1], 2)
+    opt.max_dim = x_tr.shape[1]
+
+    bo_iter = 0
+
+    def optimize_clf(hp_vec):
+        """The actual objective function with packing and unpacking of hyperparameters
+
+        Args:
+            hp_vec (array_like): Vector of hyperparameters to evaluate
+
+        Returns:
+            float: the validation error obtained
+
+        """
+
+        hp_vec = hp_vec[0]
+        k_tr = int(round(hp_vec[0]))
+        k_te = int(round(hp_vec[1]))
+        dim_out = int(np.ceil(hp_vec[2]))
+        max_iter = int(np.ceil(hp_vec[3]))
+
+        nonlocal bo_iter
+        bo_iter += 1
+        print('Iteration {} of Bayesian Optimisation'.format(bo_iter))
+        print('Trying K(lmnn)={} K(knn)={} dim_out={} max_iter={} ...\n'.format(k_tr, k_te, dim_out, max_iter))
+        lmnn_clf = LargeMarginNearestNeighbor(k=k_tr, max_iter=max_iter, dim_out=dim_out, **params)
+        knn_clf = KNeighborsClassifier(n_neighbors=k_te)
+
+        lmnn_clf = lmnn_clf.fit(x_tr, y_tr)
+        Lx_tr = lmnn_clf.transform(x_tr)
+
+        print('Evaluating the found transformation on validation set of size {}...'.format(len(y_va)))
+        knn_clf.fit(Lx_tr, y_tr)
+        Lx_va = lmnn_clf.transform(x_va)
+        y_pred = knn_clf.predict(Lx_va)
+        val_err = np.mean(np.not_equal(y_pred, y_va))
+
+        print('\nValidation error={:2.4f}\n'.format(val_err))
+        return val_err
+
+    # Parameters are discrete but treating them as continuous yields better parameters
+    domain = [{'name': 'k_tr', 'type': 'continuous', 'domain': (opt.min_k, opt.max_k)},
+              {'name': 'k_te', 'type': 'continuous', 'domain': (opt.min_k, opt.max_k)},
+              {'name': 'dim_out', 'type': 'continuous', 'domain': (opt.min_dim, opt.max_dim)},
+              {'name': 'max_iter', 'type': 'continuous', 'domain': (opt.min_iter, opt.max_iter)}]
+
+    bo = BayesianOptimization(f=optimize_clf, domain=domain)
+    bo.run_optimization(max_iter=opt.max_trials)
+
+    hp = bo.x_opt
+    print(hp)
+    k_tr_bo = int(round(hp[0]))
+    k_te_bo = int(round(hp[1]))
+    dim_out_bo = int(np.ceil(hp[2]))
+    max_iter_bo = int(np.ceil(hp[3]))
+    print('Best parameters: K(lmnn)={} K(knn)={} dim_out={} max_iter={}\n'.
+          format(k_tr_bo, k_te_bo, dim_out_bo, max_iter_bo))
+
+    return k_tr_bo, k_te_bo, dim_out_bo, max_iter_bo
---
+++ packages/PyLMNN/PyLMNN-1.2.2.tar.gz/PyLMNN-1.2.2/pylmnn/helpers.py
@@ -0,0 +1,109 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D  # Needed for projection='3d'
+import numpy as np
+import numpy.linalg as LA
+from sklearn import manifold
+from sklearn.neighbors import KNeighborsClassifier
+
+
+def pca_transform(x, var_ratio=1):
+    """
+
+    Args:
+        x (array_like):     N inputs of dimension D
+        var_ratio (float):  the variance ratio to be captured
+
+    Returns:
+        array_like:         N inputs projected onto m principal components
+
+    """
+
+    cc = np.cov(x, rowvar=False)  # Mean is removed
+    evals, evecs = LA.eigh(cc)  # Get evals in ascending order, evecs in columns
+    evecs = np.fliplr(evecs)  # Flip evecs to get them in descending eigenvalue order
+
+    if var_ratio == 1:
+        L = evecs.T
+    else:
+        evals = np.flip(evals, axis=0)
+        var_exp = np.cumsum(evals)
+        var_exp = var_exp / var_exp[-1]
+        outdim = np.argmax(np.greater_equal(var_exp, var_ratio))
+        # Set first dim_out eigenvectors as rows of L
+        L = evecs.T[:outdim]
+    Lx = x.dot(L.T)
+    return Lx
+
+
+def test_knn(x_tr, y_tr, x_te, y_te, k, L=None):
+    """Compute the k-nearest neighbor accuracy
+
+    Args:
+        x_tr (array_like):  [N, D] training inputs
+        y_tr (array_like):  [N,] training labels
+        x_te (array_like):  [M, D] testing inputs
+        y_te (array_like):  [M,] testing labels (ground truth)
+        k (int):            the number of neighbors to consider
+        L (array_like):     the learned linear transformation (default: None)
+
+    Returns:
+        float:    the k-nn accuracy
+
+    """
+    knn_clf = KNeighborsClassifier(n_neighbors=k)
+    if L is None:
+        knn_clf.fit(x_tr, y_tr)
+        y_pred = knn_clf.predict(x_te)
+        acc = np.mean(np.equal(y_pred, y_te))
+        print('kNN accuracy on test set of {} points: {:.4f}'.format(x_te.shape[0], acc))
+    else:
+        knn_clf.fit(x_tr.dot(L.T), y_tr)
+        y_pred = knn_clf.predict(x_te.dot(L.T))
+        acc = np.mean(np.equal(y_pred, y_te))
+        print('LMNN accuracy on test set of {} points: {:.4f}\n'.format(x_te.shape[0], acc))
+    return acc
+
+
+def plot_comparison(L, x, y, dim_pref=2, t_sne=False):
+    """Draw a scatter plot of points, colored by their labels, before and after applying a learned transformation
+
+    Args:
+        L (array_like): [d, D] the learned transformation
+        x (array_like): [N, D] inputs
+        y (array_like): [N,]  labels
+        dim_pref (int): the preferred number of dimensions to plot (default: 2)
+        t_sne (bool):   whether to use t-SNE to produce the plot or just use the first two dimensions
+                        of the inputs (default: False)
+
+    """
+    if dim_pref < 2 or dim_pref > 3:
+        print('Preferred plot dimensionality must be 2 or 3, setting to 2!')
+        dim_pref = 2
+
+    if t_sne:
+        print("Computing t-SNE embedding")
+        tsne = manifold.TSNE(n_components=dim_pref, init='pca', random_state=0)
+        x_tsne = tsne.fit_transform(x)
+        Lx_tsne = tsne.fit_transform(x.dot(L.T))
+        x = x_tsne
+        Lx = Lx_tsne
+    else:
+        Lx = x.dot(L.T)
+
+    fig = plt.figure()
+    if x.shape[1] > 2 and dim_pref == 3:
+        ax = fig.add_subplot(121, projection='3d')
+        ax.scatter(x[:, 0], x[:, 1], x[:, 2], c=y)
+        ax.set_title('Original Data')
+        ax = fig.add_subplot(122, projection='3d')
+        ax.scatter(Lx[:, 0], Lx[:, 1], Lx[:, 2], c=y)
+        ax.set_title('Transformed Data')
+    elif x.shape[1] >= 2:
+        ax = fig.add_subplot(121)
+        ax.scatter(x[:, 0], x[:, 1], c=y)
+        ax.set_title('Original Data')
+        ax = fig.add_subplot(122)
+        ax.scatter(Lx[:, 0], Lx[:, 1], c=y)
+        ax.set_title('Transformed Data')
+
+    plt.show()
