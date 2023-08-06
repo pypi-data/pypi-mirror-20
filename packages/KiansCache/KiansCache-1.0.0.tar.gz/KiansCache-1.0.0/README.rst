b13bf171350a8e797dc662534c8d9e3764acc387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:46 +0000

    Add package KiansCache/1.0.0/KiansCache-1.0.0.tar.gz

---
+++ packages/KiansCache/KiansCache-1.0.0.tar.gz/KiansCache-1.0.0/README.rst
@@ -0,0 +1,5 @@
+N-Way Set Associative Cache
+
+Usage:
+	Choose the "set size", "block size", and replacement algorithm for the cache.
+	
---
+++ packages/KiansCache/KiansCache-1.0.0.tar.gz/KiansCache-1.0.0/setup.py
@@ -0,0 +1,59 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='KiansCache',
+    version='1.0.0',
+
+    description='N-Way, Set-Associative Cache',
+    long_description=long_description,
+
+
+    # Author details
+    author='Kian Solomon',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(),
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    #   py_modules=["my_module"],
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'buildCache=buildCache:main',
+        ],
+    },
+)
+
+
