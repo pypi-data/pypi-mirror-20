a5d183f647056b150e034a1d76cabaf3d80950f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:10 +0000

    Add package budgettracker/0.6.4/budgettracker-0.6.4.tar.gz

---
+++ packages/budgettracker/budgettracker-0.6.4.tar.gz/budgettracker-0.6.4/budgettracker/budget.py
@@ -0,0 +1,119 @@
+from collections import namedtuple
+from .data import split_income_expenses, extract_transactions_by_label, filter_transactions_period, sort_transactions
+import datetime
+
+
+Budget = namedtuple('Budget', ['transactions', 'income_transactions', 'recurring_expenses_transactions',
+    'expenses_transactions', 'real_balance', 'balance', 'income', 'recurring_expenses', 'expenses',
+    'savings',  'savings_goal', 'expected_real_balance', 'expected_balance', 'expected_income',
+    'expected_recurring_expenses', 'expected_savings', 'expected_remaining'])
+
+
+class IncomeSource(namedtuple('IncomeSource', ['label', 'amount', 'match'])):
+    @classmethod
+    def from_dict(cls, dct):
+        return cls(label=dct['label'], amount=dct['amount'], match=dct.get('match'))
+
+    def to_dict(self):
+        return {"label": self.label,
+                "amount": self.amount,
+                "match": self.match}
+
+
+class RecurringExpense(namedtuple('RecurringExpense', ['label', 'amount', 'recurrence', 'match'])):
+    WEEKLY = 0.25
+    MONTHLY = 1
+    YEARLY = 12
+
+    @classmethod
+    def from_dict(cls, dct):
+        recurrence = dct.get('recurrence', cls.MONTHLY)
+        if isinstance(recurrence, (str, unicode)):
+            recurrence = getattr(cls, recurrence.upper(), 1)
+        return cls(label=dct['label'], amount=dct['amount'], recurrence=recurrence, match=dct.get('match'))
+
+    @property
+    def amount_per_month(self):
+        return round(self.amount / self.recurrence, 0)
+
+    @property
+    def recurrence_label(self):
+        return dict([
+            (0.25, "WEEKLY"),
+            (1, "MONTHLY"),
+            (12, "YEARLY")
+        ]).get(self.recurrence, self.recurrence)
+
+    def to_dict(self):
+        return {"label": self.label,
+                "amount": self.amount,
+                "recurrence": self.recurrence_label,
+                "match": self.match}
+
+
+class SavingsGoal(namedtuple('SavingsGoal', ['label', 'amount'])):
+    @classmethod
+    def from_dict(cls, dct):
+        return cls(**dct)
+
+    def to_dict(self):
+        return {"label": self.label,
+                "amount": self.amount}
+
+
+def budgetize(transactions, income_sources=None, recurring_expenses=None, savings_goals=None,
+              start_date=None, end_date=None, income_delay=0):
+    if start_date and end_date and income_delay:
+      income_transactions, _ = split_income_expenses(filter_transactions_period(
+        transactions, start_date + datetime.timedelta(days=income_delay), end_date + datetime.timedelta(days=income_delay)))
+      _, expenses_transactions = split_income_expenses(filter_transactions_period(transactions, start_date, end_date))
+      transactions = sort_transactions(income_transactions + expenses_transactions)
+    else:
+      transactions = sort_transactions(filter_transactions_period(transactions, start_date, end_date))
+      income_transactions, expenses_transactions = split_income_expenses(transactions)
+
+    expected_income = 0
+    if income_sources:
+      expected_income = sum([src.amount for src in income_sources])
+
+    recurring_expenses_transactions = []
+    expected_recurring_expenses = 0
+    if recurring_expenses:
+        recurring_expenses_labels = [exp.match for exp in recurring_expenses if exp.match]
+        expected_recurring_expenses = sum([exp.amount_per_month for exp in recurring_expenses])
+        recurring_expenses_transactions, expenses_transactions = extract_transactions_by_label(
+            expenses_transactions, recurring_expenses_labels)
+
+    savings_goal = 0
+    if savings_goals:
+        savings_goal = sum([s.amount for s in savings_goals]) / 12
+    
+    real_balance = sum([tx.amount for tx in transactions])
+    income = sum([tx.amount for tx in income_transactions])
+    expenses = abs(sum([tx.amount for tx in expenses_transactions]))
+    recurring_expenses = abs(sum([tx.amount for tx in recurring_expenses_transactions]))
+    savings = income - expected_recurring_expenses - expenses
+    balance = savings - savings_goal
+
+    expected_real_balance = max(income, expected_income) - recurring_expenses - expenses
+    expected_savings = max(income, expected_income) - expected_recurring_expenses - expenses
+    expected_balance = expected_savings - savings_goal
+    expected_remaining = max(expected_balance, 0)
+
+    return Budget(transactions=transactions,
+                  income_transactions=income_transactions,
+                  recurring_expenses_transactions=recurring_expenses_transactions,
+                  expenses_transactions=expenses_transactions,
+                  real_balance=round(real_balance, 2),
+                  balance=round(balance, 2),
+                  income=round(income, 2),
+                  recurring_expenses=round(recurring_expenses, 2),
+                  expenses=round(expenses, 2),
+                  savings=round(savings, 2),
+                  savings_goal=round(savings_goal, 2),
+                  expected_real_balance=round(expected_real_balance, 2),
+                  expected_balance=round(expected_balance, 2),
+                  expected_income=round(expected_income, 2),
+                  expected_recurring_expenses=round(expected_recurring_expenses, 2),
+                  expected_savings=round(expected_savings, 2),
+                  expected_remaining=round(expected_remaining, 2))
---
+++ packages/budgettracker/budgettracker-0.6.4.tar.gz/budgettracker-0.6.4/budgettracker/helpers.py
@@ -0,0 +1,147 @@
+import requests, time, os, json, datetime
+from .data import dump_transactions, load_transactions, extract_inter_account_transactions
+from .budget import budgetize, IncomeSource, RecurringExpense, SavingsGoal
+from monthdelta import monthdelta
+
+
+CONFIG_FILENAME = 'config.json'
+
+
+def load_config(filename=CONFIG_FILENAME):
+    with open(filename) as f:
+        return json.load(f)
+
+
+def load_adapter(package, name):
+    return getattr(__import__(package + '.' + name, globals(), locals(), [], -1), name)
+
+
+def create_logged_in_session(login_adapter, identifier, password, reuse=True, filename='session.json'):
+    session = requests.Session()
+    exp = time.time() - 1800 # cookie jar expires after 30min
+
+    if reuse and os.path.exists(filename) and os.path.getmtime(filename) > exp:
+        with open(filename) as f:
+            cookies = json.load(f)
+        session.cookies.update(cookies)
+    else:
+        login_adapter(session, identifier, password)
+        with open(filename, 'w') as f:
+            json.dump(session.cookies.get_dict(), f)
+
+    return session
+
+
+def create_adapter_and_session_from_config(config):
+    adapter = load_adapter('bank_adapters', config['bank_adapter'])
+    session = create_logged_in_session(adapter.login, config['bank_username'], config['bank_password'])
+    return adapter, session
+
+
+def save_balance(adapter, session, filename='balance'):
+    balance = sum([acc.amount for acc in adapter.fetch_accounts(session)])
+    with open(filename, 'w') as f:
+        f.write(str(balance))
+    return balance
+
+
+def load_balance(adapter=None, session=None, filename='balance', refresh=False):
+    if refresh and adapter and session:
+        return save_balance(adapter, session, filename)
+    if os.path.exists(filename):
+        with open(filename) as f:
+            return float(f.read())
+    return None
+
+
+def get_transactions_of_month_dump_filename(date, directory=None, filename=None):
+    if not filename:
+        filename = date.replace(day=1).strftime("%Y-%m.json")
+    if directory:
+        filename = os.path.join(directory, filename)
+    return filename
+
+
+def save_transactions_of_month(date, adapter, session, filename=None, directory=None):
+    filename = get_transactions_of_month_dump_filename(date, directory, filename)
+    if not os.path.exists(os.path.dirname(filename)):
+        os.makedirs(os.path.dirname(filename))
+    start_date = date.replace(day=1)
+    end_date = start_date + monthdelta(1)
+    transactions = adapter.fetch_transactions_from_all_accounts(session, start_date, end_date)
+    dump_transactions(transactions, filename)
+    return transactions
+
+
+def load_transactions_of_month(date, refresh=False, adapter=None, session=None, filename=None, directory=None):
+    filename = get_transactions_of_month_dump_filename(date, directory, filename)
+    start_date = date.replace(day=1)
+    end_date = start_date + monthdelta(1)
+    if refresh and adapter and session:
+        return save_transactions_of_month(date, adapter, session, filename)
+    if os.path.exists(filename):
+        return load_transactions(filename)
+    return None
+
+
+def make_budget_from_config(transactions, config, start_date=None, end_date=None):
+    if 'inter_account_labels_out' in config and 'inter_account_labels_in' in config:
+        _, transactions = extract_inter_account_transactions(transactions,
+                config['inter_account_labels_out'], config['inter_account_labels_in'])
+
+    income_sources = map(IncomeSource.from_dict, config.get('income_sources', []))
+    income_delay = config.get('income_delay', 0)
+    recurring_expenses = map(RecurringExpense.from_dict, config.get('recurring_expenses', []))
+    savings_goals = map(SavingsGoal.from_dict, config.get('savings_goals', []))
+
+    return budgetize(transactions, income_sources, recurring_expenses, savings_goals,
+        start_date, end_date, income_delay)
+
+
+def load_budget_of_month_from_config(config, date, refresh=False, adapter=None, session=None):
+    if refresh and not adapter and not session:
+        adapter, session = create_adapter_and_session_from_config(config)
+
+    start_date = date.replace(day=1)
+    end_date = start_date + monthdelta(1)
+
+    transactions = load_transactions_of_month(date, refresh=refresh, adapter=adapter,
+        session=session, directory=config.get('transactions_dir'))
+    if transactions is None:
+        return None
+
+    if config.get('income_delay'):
+        next_transactions = load_transactions_of_month(end_date, directory=config.get('transactions_dir'))
+        if next_transactions:
+            transactions.extend(next_transactions)
+
+    return make_budget_from_config(transactions, config, start_date, end_date)
+
+
+def notify_using_config(config, message):
+    print message
+    if config.get('notify_adapter'):
+        adapter = load_adapter('notify_adapters', config['notify_adapter'])
+        session = adapter.login(requests.Session(), config['notify_username'], config['notify_password'])
+        adapter.send(session, config['notify_numbers'], message)
+
+
+def update_local_data(config, notify=True, date=None, adapter=None, session=None):
+    if not adapter:
+        adapter, session = create_adapter_and_session_from_config(config)
+
+    if not date:
+        if datetime.date.today().day <= 5:
+            # if we are still in the early days of a new month, keep updating the previous month
+            update_local_data(config, False, datetime.date.today() - monthdelta(1), adapter, session)
+        date = datetime.date.today().replace(day=1)
+
+    prev_budget = load_budget_of_month_from_config(config, date)
+    budget = load_budget_of_month_from_config(config, date, refresh=True, adapter=adapter, session=session)
+    save_balance(adapter, session)
+
+    if notify and prev_budget:
+        if config.get('notify_remaining') and prev_budget.expected_remaining > config['notify_remaining'] and budget.expected_remaining <= config['notify_remaining']:
+            notify_using_config(config, 'BUDGET: /!\ LOW SAFE TO SPEND: %se' % budget.expected_remaining)
+        elif config.get('notify_delta') and (prev_budget.expected_remaining - budget.expected_remaining) > config['notify_delta']:
+            notify_using_config(config, 'BUDGET: Remaining funds: %se' % budget.expected_remaining)
