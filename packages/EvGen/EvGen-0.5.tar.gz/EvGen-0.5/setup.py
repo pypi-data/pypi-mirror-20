89c8b45a0a3006d8fb9831e1c62f5cc6b150517d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:47 +0000

    Add package EvGen/0.5/EvGen-0.5.tar.gz

---
+++ packages/EvGen/EvGen-0.5.tar.gz/EvGen-0.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name="EvGen", 
+      author = "Michal Zylinski",
+      author_email = "michal.zylinski@gmail.com",
+      license = "MIT",
+      url="https://github.com/MichalZylinski/EvGen",
+      version="0.5", 
+      description="Comprehensive event generation framework",
+      packages=['evgen', 'examples'],
+      
+      install_requires=["Faker", "azure", "user_agent"]
+      )
+
+#temporary python setup.py bdist
+#python setup.py sdist
+#easy_install -U dist\...egg
