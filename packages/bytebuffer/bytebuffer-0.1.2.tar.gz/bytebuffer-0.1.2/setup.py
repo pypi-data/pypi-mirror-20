bbfaac95bc36cbd78377069248a01e82703219b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:53 +0000

    Add package bytebuffer/0.1.2/bytebuffer-0.1.2.tar.gz

---
+++ packages/bytebuffer/bytebuffer-0.1.2.tar.gz/bytebuffer-0.1.2/setup.py
@@ -0,0 +1,16 @@
+# encoding: UTF-8
+
+from distutils.core import setup
+
+setup(
+    name='bytebuffer',
+    version='0.1.2',
+    author='Ivan Babintsev',
+    author_email='alon.sage@gmail.com',
+    license='Apache License 2.0',
+    url='https://github.com/alon-sage/python-bytebuffer',
+    description='High performance library for reading and writing values of primitive types. '
+                'Primitive values are translated to (or from) sequences of bytes.',
+    packages=[],
+    py_modules=['bytebuffer'],
+)
