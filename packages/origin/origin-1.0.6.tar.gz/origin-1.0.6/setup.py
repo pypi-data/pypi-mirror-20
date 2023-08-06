ef7cd21d6a901239986fd18c1c62ba80dab2f9ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:46 +0000

    Add package origin/1.0.6/origin-1.0.6.tar.gz

---
+++ packages/origin/origin-1.0.6.tar.gz/origin-1.0.6/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+
+setup(
+    name='origin',
+    version='1.0.6',
+    description='Python library for the Origin Event Publisher',
+    author='Cochise Ruhulessin',
+    author_email='cochise.ruhulessin@wizardsofindustry.net',
+    url='https://www.wizardsofindustry.net',
+    install_requires=[
+        'python-qpid-proton>=0.17.0',
+        'dsnparse>=0.1.4'
+    ],
+    packages=find_packages()
+)
