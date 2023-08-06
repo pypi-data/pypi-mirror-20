74fcacaa17a884254dada338b74edae12b4eb54e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:33 +0000

    Add package cetus/0.3.0/cetus-0.3.0.tar.gz

---
+++ packages/cetus/cetus-0.3.0.tar.gz/cetus-0.3.0/cetus/data_access/execution.py
@@ -0,0 +1,27 @@
+from typing import (Union,
+                    List)
+
+from cetus.types import (ConnectionType,
+                         RecordType)
+
+
+async def execute(query: str, *args: RecordType,
+                  is_mysql: bool,
+                  connection: ConnectionType) -> Union[int, str]:
+    if is_mysql:
+        async with connection.cursor() as cursor:
+            resp = await cursor.execute(query, args=args)
+    else:
+        resp = await connection.execute(query, *args)
+    return resp
+
+
+async def execute_many(query: str, *,
+                       args: List[RecordType],
+                       is_mysql: bool,
+                       connection: ConnectionType) -> None:
+    if is_mysql:
+        async with connection.cursor() as cursor:
+            await cursor.executemany(query, args=args)
+    else:
+        await connection.executemany(query, args=args)
---
+++ packages/cetus/cetus-0.3.0.tar.gz/cetus-0.3.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+packages = find_packages()
+setup(name='cetus',
+      version='0.3.0',
+      packages=packages,
+      description='asynchronous working with PostgreSQL/MySQL '
+                  'based on asyncpg/aiomysql',
+      author='Azat Ibrakov',
+      author_email='azatibrakov@gmail.com',
+      url='https://github.com/lycantropos/cetus',
+      download_url='https://github.com/lycantropos/cetus/archive/master.tar.gz',
+      keywords=['async', 'MySQL', 'PostgreSQL'],
+      install_requires=[
+          'SQLAlchemy>=1.0.12',
+          # async
+          'asyncio_extras',  # for async context managers
+          'aiohttp',  # for working with http
+          'asyncpg',  # for working with Postgres
+          'aiomysql',  # for working with MySQL
+      ],
+      setup_requires=['pytest-runner'],
+      tests_require=['SQLAlchemy-Utils>=0.32.12',  # for database creation/cleaning
+                     'psycopg2==2.6.2',  # for working with Postgres
+                     'PyMySQL==0.7.10',  # for working with MySQL
+                     'pydevd',  # debugging
+                     'pytest==3.0.5',
+                     'pytest-asyncio',
+                     'pytest-cov>=2.4.0',
+                     'hypothesis>=3.6.1',
+                     'pytz'  # working with datetime objects in hypothesis
+                     ])
