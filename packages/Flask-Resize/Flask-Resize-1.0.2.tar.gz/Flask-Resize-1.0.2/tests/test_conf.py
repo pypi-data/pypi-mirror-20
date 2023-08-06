a33828cf0586646a7c7af7653e4d270fd9c5a05d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:42 +0000

    Add package Flask-Resize/1.0.2/Flask-Resize-1.0.2.tar.gz

---
+++ packages/Flask-Resize/Flask-Resize-1.0.2.tar.gz/Flask-Resize-1.0.2/tests/test_conf.py
@@ -0,0 +1,26 @@
+import flask
+import pytest
+
+from flask_resize import resize
+
+from .base import create_resizeapp
+
+
+def test_resize_settings():
+    with pytest.raises(RuntimeError):
+        create_resizeapp()
+    with pytest.raises(RuntimeError):
+        create_resizeapp(RESIZE_URL='http://test.dev')
+    with pytest.raises(RuntimeError):
+        create_resizeapp(RESIZE_ROOT='/')
+
+    working_app = create_resizeapp(RESIZE_URL='http://test.dev',
+                                   RESIZE_ROOT='/')
+    assert isinstance(working_app, flask.Flask)
+
+
+def test_resize_noop():
+    app = create_resizeapp(RESIZE_NOOP=True)
+    with app.test_request_context():
+        generated_image_path = resize('xyz.png', 'WxH')
+    assert generated_image_path == 'xyz.png'
