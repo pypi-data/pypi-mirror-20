5390a5475a08b76e260e0e34cb538d1140e7b889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:47 +0000

    Add package korolishut/0.1/korolishut-0.1.tar.gz

---
+++ packages/korolishut/korolishut-0.1.tar.gz/korolishut-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='korolishut',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='mxfsq',
+    author_email='simsim0331@gmail.com',
+    description='Text of "Korol i shut" songs'
+)
