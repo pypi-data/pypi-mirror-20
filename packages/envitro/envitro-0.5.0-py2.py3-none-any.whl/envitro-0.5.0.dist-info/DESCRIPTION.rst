1215b7a3577516ee09a513cda15fc26eefe09af7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:23 +0000

    Add package envitro/0.5.0/envitro-0.5.0-py2.py3-none-any.whl

---
+++ packages/envitro/envitro-0.5.0-py2.py3-none-any.whl/envitro-0.5.0.dist-info/DESCRIPTION.rst
@@ -0,0 +1,88 @@
+envitro
+=======
+
+A module for reading values from OS environment variables.
+
+Compared to using os.getenv(), this module provides convenience functions,
+for parsing basic datatypes. It also allows specifying optional default values if
+the environment variable does not exist. Basic environment variable parsing and
+sanitizing is also performed.
+
+Usage
+-----
+
+.. code-block:: python
+
+    import os
+    import envitro
+
+    # fails when environment variables are missing
+    bool_required = envitro.bool("BOOL_ENV")
+    int_required = envitro.int("INTEGER_ENV")
+    float_required = envitro.float("FLOAT_ENV")
+    str_required = envitro.str("STRING_ENV")
+
+    # basic sanitizing
+    os.environ["STR_ENV"] = "  var with spaces  "
+    envitro.str("STR_ENV") # returns "var with spaces"
+
+    # falls back to defaults
+    bool_default = envitro.bool("BOOL_NOT_FOUND", default=False)
+    int_default = envitro.int("INTEGER_NOT_FOUND", default=42)
+    float_default = envitro.float("FLOAT_NOT_FOUND", default=42.44)
+    str_default = envitro.str("STRING_NOT_FOUND", default="my_default")
+
+    # try multiple fallback ENV variables
+    os.environ["FALLBACK_ENV"] = "fallback_val"
+    single_fallback = envitro.str("MISSING", fallback="FALLBACK_ENV")
+    multiple_fallback = envitro.str("MISSING", fallback=["FALL_MISSING_1", "FALL_MISSING_2", "FALLBACK_ENV"])
+
+    # get and set raw environment variables
+    envitro.write("EXISTING_VAR", None) # clear the environment variable
+    envitro.write("RAW_STRING", " raw_string ")
+    envitro.read("RAW_STRING") # returns " raw_string "
+    envitro.read("MISSING_RAW_STRING", default=" defaultval ") # returns " defaultval "
+
+    # lists/tuples
+    os.environ["LIST_ENV"] = "item1,item2,item3"
+    list_required = envitro.list("LIST_ENV") # returns ["item1", "item2", "item3"]
+    tuple_required = envitro.tuple("LIST_ENV") # returns ("item1", "item2", "item3")
+    os.environ["LIST_ENV2"] = "item1;item2;item3"
+    list_required2 = envitro.list("LIST_ENV2", separator=";") # returns ["item1", "item2", "item3"]
+    tuple_required2 = envitro.tuple("LIST_ENV2", separator=";") # returns ("item1", "item2", "item3")
+
+    # utility functions
+    envitro.isset("MAYBE_SET_VARIABLE") # return True/False
+
+
+Decorators
+----------
+
+There are also decorators available to selectively enable or disable functions based on environment
+variables.
+
+.. code-block:: python
+
+    import envitro
+
+    @envitro.decorators.isset('ALLOW_FOO')
+    def foo():
+        return "Hello World"
+
+    envitro.write('ALLOW_FOO', '1')
+    foo() # allowed
+
+    envitro.write('ALLOW_FOO', None)
+    foo() # return "None" and is not executed
+
+    @envitro.decorators.bool('ALLOW_REMOTE')
+    def get_remote(arg1, arg2):
+        return call_remote_service(arg1, arg2)
+
+    envitro.write('ALLOW_REMOTE', 'True')
+    get_remote('hello', 'world') # calls remote service
+
+    envitro.write('ALLOW_REMOTE', 'False')
+    get_remote('hello', 'world') # returns "None" and is not executed
+
+
---
+++ packages/envitro/envitro-0.5.0-py2.py3-none-any.whl/envitro/docker.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=W0141,W0406,W0622
+"""Docker specific environment variable reading.
+
+A set of functions to read environment variables for docker links.
+"""
+from __future__ import absolute_import
+
+import re
+import warnings
+
+from . import core
+
+def _split_docker_link(alias_name):
+    """
+    Splits a docker link string into a list of 3 items (protocol, host, port).
+    - Assumes IPv4 Docker links
+
+    ex: _split_docker_link('DB') -> ['tcp', '172.17.0.82', '8080']
+    """
+    sanitized_name = alias_name.strip().upper()
+    split_list = re.split(r':|//', core.str('{0}_PORT'.format(sanitized_name)))
+    # filter out empty '' vals from the list with filter and
+    # cast to list (required for python3)
+    return list(filter(None, split_list))
+
+
+def read(alias_name, allow_none=False):
+    """Get the raw docker link value.
+
+    Get the raw environment variable for the docker link
+
+    Args:
+        alias_name: The environment variable name
+        default: The default value if the link isn't available
+        allow_none: If the return value can be `None` (i.e. optional)
+    """
+    warnings.warn('Will be removed in v1.0', DeprecationWarning, stacklevel=2)
+    return core.read('{0}_PORT'.format(alias_name), default=None, allow_none=allow_none)
+
+
+def get(alias_name, allow_none=False):
+    warnings.warn('Will be removed in v1.0', DeprecationWarning, stacklevel=2)
+    return read(alias_name, allow_none)
+
+
+def isset(alias_name):
+    """Return a boolean if the docker link is set or not and is a valid looking docker link value.
+
+    Args:
+        alias_name: The link alias name
+    """
+    warnings.warn('Will be removed in v1.0', DeprecationWarning, stacklevel=2)
+    raw_value = read(alias_name, allow_none=True)
+    if raw_value:
+        if re.compile(r'.+://.+:\d+').match(raw_value):
+            return True
+        else:
+            warnings.warn('"{0}_PORT={1}" does not look like a docker link.'.format(alias_name, raw_value), stacklevel=2)
+            return False
+
+    return False
+
+def protocol(alias_name, default=None, allow_none=False):
+    """Get the protocol from the docker link alias or return the default.
+
+    Args:
+        alias_name: The docker link alias
+        default: The default value if the link isn't available
+        allow_none: If the return value can be `None` (i.e. optional)
+
+    Examples:
+        Assuming a Docker link was created with ``docker --link postgres:db``
+        and the resulting environment variable is ``DB_PORT=tcp://172.17.0.82:5432``.
+
+        >>> envitro.docker.protocol('DB')
+        tcp
+    """
+    warnings.warn('Will be removed in v1.0', DeprecationWarning, stacklevel=2)
+    try:
+        return _split_docker_link(alias_name)[0]
+    except KeyError as err:
+        if default or allow_none:
+            return default
+        else:
+            raise err
+
+
+def host(alias_name, default=None, allow_none=False):
+    """Get the host from the docker link alias or return the default.
+
+    Args:
+        alias_name: The docker link alias
+        default: The default value if the link isn't available
+        allow_none: If the return value can be `None` (i.e. optional)
+
+    Examples:
+        Assuming a Docker link was created with ``docker --link postgres:db``
+        and the resulting environment variable is ``DB_PORT=tcp://172.17.0.82:5432``.
+
+        >>> envitro.docker.host('DB')
+        172.17.0.82
+    """
+    warnings.warn('Will be removed in v1.0', DeprecationWarning, stacklevel=2)
+    try:
+        return _split_docker_link(alias_name)[1]
+    except KeyError as err:
+        if default or allow_none:
+            return default
+        else:
+            raise err
+
+
+def port(alias_name, default=None, allow_none=False):
+    """Get the port from the docker link alias or return the default.
+
+    Args:
+        alias_name: The docker link alias
+        default: The default value if the link isn't available
+        allow_none: If the return value can be `None` (i.e. optional)
+
+    Examples:
+        Assuming a Docker link was created with ``docker --link postgres:db``
+        and the resulting environment variable is ``DB_PORT=tcp://172.17.0.82:5432``.
+
+        >>> envitro.docker.port('DB')
+        5432
+    """
+    warnings.warn('Will be removed in v1.0', DeprecationWarning, stacklevel=2)
+    try:
+        return int(_split_docker_link(alias_name)[2])
+    except KeyError as err:
+        if default or allow_none:
+            return default
+        else:
+            raise err
