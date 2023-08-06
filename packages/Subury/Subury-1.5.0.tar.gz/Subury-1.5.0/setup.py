31656a1e545e1c09c48e706c045e09cea324da24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:27 +0000

    Add package Subury/1.5.0/Subury-1.5.0.tar.gz

---
+++ packages/Subury/Subury-1.5.0.tar.gz/Subury-1.5.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name='Subury',
+        version='1.5.0',
+        py_modules=['Subury_Print_lol'],
+        author='Subury',
+        author_email='1446849325@qq.com',
+        url='http://www.surbury.me',
+        description='a no mean test',
+    )
