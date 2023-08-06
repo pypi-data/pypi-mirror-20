0336cdb192bfcb53ba6300b5ddd0d01156d54074
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:58 +0000

    Add package suibianshishi/1.0.0/suibianshishi-1.0.0.tar.gz

---
+++ packages/suibianshishi/suibianshishi-1.0.0.tar.gz/suibianshishi-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(
+    name = 'suibianshishi',
+    version = '1.0.0',
+    py_modules = ['suibianshishi'],
+    author = 'laowang',
+    description = 'A simple printer of nested lists',
+    )
---
+++ packages/suibianshishi/suibianshishi-1.0.0.tar.gz/suibianshishi-1.0.0/suibianshishi.py
@@ -0,0 +1,7 @@
+def print_lol(the_list):
+    for each in the_list:
+        if isinstance(each,list):
+            print_lol(each)
+        else:
+            print(each)
+            
