bc1b63045ababdd14d215dd316f1b5b6307ebded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:02 +0000

    Add package pydart2/0.6.3/pydart2-0.6.3.tar.gz

---
+++ packages/pydart2/pydart2-0.6.3.tar.gz/pydart2-0.6.3/pydart2/gui/pyqt4/__init__.py
@@ -0,0 +1,9 @@
+try:
+    from __future__ import absolute_import
+    from pydart2.gui.pyqt4 import window
+    from pydart2.gui.pyqt4 import side_panel
+
+    assert window
+    assert side_panel
+except:
+    print("failed to load pyqt4")
