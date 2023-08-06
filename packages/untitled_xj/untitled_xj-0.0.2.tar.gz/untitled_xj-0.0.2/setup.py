ec4c00c872db0478f30bb92901769915f7f87a9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:09 +0000

    Add package untitled_xj/0.0.2/untitled_xj-0.0.2.tar.gz

---
+++ packages/untitled_xj/untitled_xj-0.0.2.tar.gz/untitled_xj-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='untitled_xj',
+    version='0.0.2',
+    packages=[''],
+    url='http://www.xjie.cn',
+    license='MIT',
+    author='xiaojie',
+    author_email='iefsou@163.com',
+    description='None'
+)
