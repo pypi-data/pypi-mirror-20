309c46e03ebac9de90614e6417a0f0933d34323d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:51 +0000

    Add package emrt.necd.content/2.0.4/emrt.necd.content-2.0.4-py2.7.egg

---
+++ packages/emrt.necd.content/emrt.necd.content-2.0.4-py2.7.egg/emrt/necd/content/notifications/conclusion_to_comment.py
@@ -0,0 +1,45 @@
+from emrt.necd.content.observation import IObservation
+from five import grok
+from Products.CMFCore.interfaces import IActionSucceededEvent
+from Products.Five.browser.pagetemplatefile import PageTemplateFile
+from utils import notify
+
+
+@grok.subscribe(IObservation, IActionSucceededEvent)
+def notification_cp(context, event):
+    """
+    To:     CounterParts
+    When:   New draft conclusion to comment on
+    """
+    _temp = PageTemplateFile('conclusion_to_comment.pt')
+
+    if event.action in ['request-comments']:
+        observation = context
+        subject = u'New draft conclusion to comment on'
+        notify(
+            observation,
+            _temp,
+            subject,
+            'CounterPart',
+            'conclusion_to_comment'
+        )
+
+
+@grok.subscribe(IObservation, IActionSucceededEvent)
+def notification_lr(context, event):
+    """
+    To:     LeadReviewer
+    When:   New draft question to comment on
+    """
+    _temp = PageTemplateFile('conclusion_to_comment.pt')
+
+    if event.action in ['request-comments']:
+        observation = context
+        subject = u'New draft conclusion to comment on'
+        notify(
+            observation,
+            _temp,
+            subject,
+            'LeadReviewer',
+            'conclusion_to_comment'
+        )
