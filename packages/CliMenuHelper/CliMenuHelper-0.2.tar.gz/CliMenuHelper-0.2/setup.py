44f7aa9ea2f70d75b2d80fd4b9438bf24f44a62a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:19 +0000

    Add package CliMenuHelper/0.2/CliMenuHelper-0.2.tar.gz

---
+++ packages/CliMenuHelper/CliMenuHelper-0.2.tar.gz/CliMenuHelper-0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'CliMenuHelper',
+  packages = ['CliMenuHelper'],
+  version = '0.2',
+  description = 'Library to create cli menus',
+  author = 'Roberto Torres',
+  author_email = 'roberto.torres.dev@gmail.com',
+  url = 'https://github.com/rtorres90/cli-menu-helper/',
+  download_url = 'https://github.com/rtorres90/cli-menu-helper/archive/v0.4.tar.gz',
+  keywords = ['menu', 'cli'],
+  classifiers = ['Programming Language :: Python :: 2.7'],
+)
