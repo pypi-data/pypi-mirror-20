e3e3cebe2f75e8e7efd78a743c989a6bcbc4ffb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:14 +0000

    Add package imposter/0.0.7/imposter-0.0.7.tar.gz

---
+++ packages/imposter/imposter-0.0.7.tar.gz/imposter-0.0.7/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(name='imposter',
+      version='0.0.7',
+      description='AWS EC2 Metadata Imposter Service',
+      author='James Boehmer',
+      author_email='james.boehmer@gmail.com',
+      url='https://github.com/jamesboehmer/imposter',
+      packages=find_packages(),
+      py_modules=['imposter'],
+      include_package_data=True,
+      install_requires=['Flask==0.12', 'gunicorn==19.6.0', 'boto==2.45.0', 'netifaces==0.10.5', 'requests==2.13.0'],
+      entry_points={
+          'console_scripts': [
+              'imposter=imposter.service:main',
+          ],
+      },
+      )
