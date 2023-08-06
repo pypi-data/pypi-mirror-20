bbc2d637f7229d7b2420c842781610d487ed6857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:34 +0000

    Add package Tailbone/0.5.90/Tailbone-0.5.90.tar.gz

---
+++ packages/Tailbone/Tailbone-0.5.90.tar.gz/Tailbone-0.5.90/tailbone/scaffolds.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#  Rattail -- Retail Software Framework
+#  Copyright © 2010-2016 Lance Edgar
+#
+#  This file is part of Rattail.
+#
+#  Rattail is free software: you can redistribute it and/or modify it under the
+#  terms of the GNU Affero General Public License as published by the Free
+#  Software Foundation, either version 3 of the License, or (at your option)
+#  any later version.
+#
+#  Rattail is distributed in the hope that it will be useful, but WITHOUT ANY
+#  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+#  FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for
+#  more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with Rattail.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+"""
+Pyramid scaffold templates
+"""
+
+from __future__ import unicode_literals, absolute_import
+
+from rattail.files import resource_path
+
+from pyramid.scaffolds import PyramidTemplate
+
+
+class RattailTemplate(PyramidTemplate):
+    _template_dir = resource_path('rattail:data/project')
+    summary = "Starter project based on Rattail / Tailbone"
