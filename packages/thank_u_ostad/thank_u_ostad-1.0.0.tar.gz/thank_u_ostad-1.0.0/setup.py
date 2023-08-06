87257f430a3034f426ffb797bd1770adfcc8c713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:50 +0000

    Add package thank_u_ostad/1.0.0/thank_u_ostad-1.0.0.tar.gz

---
+++ packages/thank_u_ostad/thank_u_ostad-1.0.0.tar.gz/thank_u_ostad-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+name = 'thank_u_ostad',
+version = '1.0.0',
+py_modules = ['thank_u_ostad'],
+author = 'Parisa Babamohammadi',
+author_email = 'mohammadi.p.shz@gmail.com',
+url = 'http://www.headfirstlabs.com',
+description = 'print some text to thank my teacher',
+)
---
+++ packages/thank_u_ostad/thank_u_ostad-1.0.0.tar.gz/thank_u_ostad-1.0.0/thank_u_ostad.py
@@ -0,0 +1,5 @@
+"""this is my first module"""
+def pnt():
+	"""This function print some text"""
+	print("***Taghdim Be Ostad, Jahandideh***")
+	return			
