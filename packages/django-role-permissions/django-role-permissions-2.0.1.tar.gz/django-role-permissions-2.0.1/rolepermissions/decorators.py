f3462075029e0260d086b61b656df345e446101f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:02 +0000

    Add package django-role-permissions/2.0.1/django-role-permissions-2.0.1.tar.gz

---
+++ packages/django-role-permissions/django-role-permissions-2.0.1.tar.gz/django-role-permissions-2.0.1/rolepermissions/decorators.py
@@ -0,0 +1,40 @@
+from __future__ import unicode_literals
+
+from functools import wraps
+
+from django.conf import settings
+from django.contrib.auth.views import redirect_to_login
+from django.core.exceptions import PermissionDenied
+
+from rolepermissions.checkers import has_role, has_permission
+from rolepermissions.utils import user_is_authenticated
+
+
+def has_role_decorator(role):
+    def request_decorator(dispatch):
+        @wraps(dispatch)
+        def wrapper(request, *args, **kwargs):
+            user = request.user
+            if user_is_authenticated(user):
+                if has_role(user, role):
+                    return dispatch(request, *args, **kwargs)
+            if hasattr(settings, 'ROLEPERMISSIONS_REDIRECT_TO_LOGIN'):
+                return redirect_to_login(request.get_full_path())
+            raise PermissionDenied
+        return wrapper
+    return request_decorator
+
+
+def has_permission_decorator(permission_name):
+    def request_decorator(dispatch):
+        @wraps(dispatch)
+        def wrapper(request, *args, **kwargs):
+            user = request.user
+            if user_is_authenticated(user):
+                if has_permission(user, permission_name):
+                    return dispatch(request, *args, **kwargs)
+            if hasattr(settings, 'ROLEPERMISSIONS_REDIRECT_TO_LOGIN'):
+                return redirect_to_login(request.get_full_path())
+            raise PermissionDenied
+        return wrapper
+    return request_decorator
