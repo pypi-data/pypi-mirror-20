c08a0b51c73b7ba7bbbb61e9447d848b808d2103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:18 +0000

    Add package find_it_bouyang/1.0.0/find_it_bouyang-1.0.0.zip

---
+++ packages/find_it_bouyang/find_it_bouyang-1.0.0.zip/find_it_bouyang-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name        =   'find_it_bouyang',
+        version     =   '1.0.0',
+        py_modules  =   ['find_it_bouyang'],
+        author      =   'ouyangnan',
+        author_email=   'ouyangnan@126.com',
+        url         =   'http://www.headfirstlabs.com',
+        description =   'A Simple printer of nested lists',
+        )
