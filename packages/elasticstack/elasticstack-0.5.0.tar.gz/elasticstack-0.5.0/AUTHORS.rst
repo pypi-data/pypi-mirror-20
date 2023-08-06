6b0dc7ef453617f559f2792656c7aad96a6ecb23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:12 +0000

    Add package elasticstack/0.5.0/elasticstack-0.5.0.tar.gz

---
+++ packages/elasticstack/elasticstack-0.5.0.tar.gz/elasticstack-0.5.0/AUTHORS.rst
@@ -0,0 +1,19 @@
+=======
+Credits
+=======
+
+Author
+------
+
+* Ben Lopatin @bennylope
+
+Contributors
+------------
+
+* Basil Shubin @bashu
+* Joe Stump @joestump
+* Mario César @mariocesar
+* Martin Svoboda @martinsvoboda
+* Mike Brown @m3brown
+* Seizan Shimazaki @seizans
+* @rjor2
---
+++ packages/elasticstack/elasticstack-0.5.0.tar.gz/elasticstack-0.5.0/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+import elasticstack
+
+version = elasticstack.__version__
+
+try:
+    readme = open("README.rst", encoding="utf-8").read()
+    history = open("HISTORY.rst", encoding="utf-8").read().replace(".. :changelog:", "")
+except TypeError:
+    # Python 2. Encoding errors may occur.
+    readme = open("README.rst").read()
+    history = open("HISTORY.rst").read().replace(".. :changelog:", "")
+
+
+setup(
+    name='elasticstack',
+    version=version,
+    description="""Configurable indexing and other extras for Haystack (with ElasticSearch biases).""",
+    long_description=readme + '\n\n' + history,
+    author='Ben Lopatin',
+    author_email='ben@wellfire.co',
+    url='https://github.com/bennylope/elasticstack',
+    packages=[
+        'elasticstack',
+    ],
+    include_package_data=True,
+    install_requires=[
+        'Django>=1.8',
+        'django-haystack>=2.0.0',
+    ],
+    license="BSD",
+    zip_safe=False,
+    keywords='elasticstack',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
