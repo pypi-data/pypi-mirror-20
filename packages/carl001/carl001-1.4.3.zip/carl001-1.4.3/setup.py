256982f69df45b3651c5855d4d51086c1b583f75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:59 +0000

    Add package carl001/1.4.3/carl001-1.4.3.zip

---
+++ packages/carl001/carl001-1.4.3.zip/carl001-1.4.3/setup.py
@@ -0,0 +1,13 @@
+#coding=utf-8
+
+from distutils.core import setup
+
+setup(
+        name          = 'carl001',
+        version        = '1.4.3',
+        py_modules    = ['carl001'],
+        author        = 'Carl_HM',
+        author_email  = '2396875467@qq.com',
+        url           = '',
+        description   = 'A simple printer of nester lists',
+        )
