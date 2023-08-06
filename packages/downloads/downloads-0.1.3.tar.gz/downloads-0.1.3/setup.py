2a22cd58276078add92ae5053b1392dad744ea1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:45 +0000

    Add package downloads/0.1.3/downloads-0.1.3.tar.gz

---
+++ packages/downloads/downloads-0.1.3.tar.gz/downloads-0.1.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+import sys
+
+setup(name='downloads',
+      version='0.1.3',
+      description='Easy HTTP downloads',
+      url='https://github.com/audy/downloads',
+      author='Austin Davis-Richardson',
+      author_email='harekrishna@gmail.com',
+      packages=['downloads'])
