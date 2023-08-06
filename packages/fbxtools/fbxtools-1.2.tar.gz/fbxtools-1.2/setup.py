bbc4a92144255f9ae5fc423aac77970fba5a14e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:49 +0000

    Add package fbxtools/1.2/fbxtools-1.2.tar.gz

---
+++ packages/fbxtools/fbxtools-1.2.tar.gz/fbxtools-1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(name = 'fbxtools',
+	version = '1.2',
+	author = 'herrersystem',
+	author_email = 'contact@evhunter.fr',
+	
+	url = "http://github.com/herrersystem/fbxtools",
+	keywords = 'fbx freebox',
+	description = 'Provide intialisation, connect and disconnect functions for Freebox OS application.',
+	license = 'GNU General Public License (GPL)',
+	packages = ['fbxtools'],
+	install_requires = ['apize', 'netifaces'],
+	
+	classifiers = [
+		'Development Status :: 4 - Beta',
+		'License :: OSI Approved :: GNU General Public License (GPL)',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
+	],
+)
