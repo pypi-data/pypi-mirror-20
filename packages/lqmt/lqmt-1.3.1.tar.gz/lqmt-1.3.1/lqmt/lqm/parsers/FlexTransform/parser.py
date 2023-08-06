0489133a535498b89f7cad06cfe4c20d087bb949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:17 +0000

    Add package lqmt/1.3.1/lqmt-1.3.1.tar.gz

---
+++ packages/lqmt/lqmt-1.3.1.tar.gz/lqmt-1.3.1/lqmt/lqm/parsers/FlexTransform/parser.py
@@ -0,0 +1,119 @@
+"""
+Created on Jan 12, 2015
+
+@author: taxon
+"""
+import FlexTransform
+from lqmt.lqm.data import Alert
+import os
+import inspect
+import logging
+import io
+
+
+class FlexTransformParser(object):
+    def __init__(self, config=None):
+        self._logger = logging.getLogger("LQMT.Parsers")
+
+        # get the FlexTransform directory
+        self._current_dir, name = os.path.split(inspect.getfile(FlexTransform.FlexTransform))
+        self._transform = FlexTransform.FlexTransform.FlexTransform()
+
+        # config
+        if config is not None:
+            for p, c in config.items():
+                self.add_parser(p, c)
+
+    def add_parser(self, parserName, parserConfiguration):
+        """
+        Function that extends the AddParser function from FlexTransform.
+        :param parserName: Name of the parser being added
+        :param parserConfiguration: Path to the configuration file being added
+        """
+        if parserName not in self._transform.Parsers:
+            config_file = open(os.path.join(self._current_dir, parserConfiguration), 'r')
+            self._transform.add_parser(parserName, config_file)
+
+    def parse(self, datafile, meta=None):
+        """
+        Parse the datafile using the metadata dictionary.
+
+        Note: Removed sourceMetaData from the parser for now due to conflicts with STIX-TLP. Currently assessing
+        if it needs to be included. Currently the meta file is just being used to identify the sourceParser.
+
+        :param datafile: Contains path to the file containing the alert data.
+        :param meta: Contains meta data about the datafile. Examples includes PayloadFormat, FileName, PayloadType, and
+        more.
+        :return: Returns parsed alert data.
+        """
+        alerts = []
+
+        # TODO: Stix-tlp parser currently doesn't support meta files. Until it does, meta files are for the cfm format
+        try:
+            if meta['PayloadFormat'] == 'stix-tlp' or meta is None:
+                data = self._transform.transform(
+                    datafile,
+                    meta['PayloadFormat'],
+                    "LQMTools"
+                )
+            else:
+                data = self._transform.transform(
+                    datafile,
+                    meta['PayloadFormat'],
+                    "LQMTools",
+                    source_meta_data=meta
+                )
+
+        except Exception as e:
+            data = []
+            self._logger.error(
+                "LQMT-FlexTransform-Parser: Error parsing file file='{0}' exception='{1}'".format(datafile, e))
+
+        for d in data:
+            alert = Alert()
+            alert.setFromDict(d)
+            alerts.append(alert)
+
+        return alerts
+
+    def parseflextext(self, datafile, meta, destination_file_obj, config_str):
+        """
+        Flextext parser. Transforms intermediate data into a user defined structure.
+
+        :param datafile: dir path to the alert datafile that will be transformed
+        :param meta: Contains meta data about the datafile. Examples includes PayloadFormat, FileName, PayloadType, and
+        more.
+        :param destination_file_obj: File object used to write contents to file.
+        :param config_str: Parser configuration in the form of a string. Is wrapped in an IO wrapped and then passed
+        to FlexT to configure FlexText parser
+        :return: Returns none. Data is outputted to file specified by destination_file_obj
+        """
+
+        # Add FlexText parser using config_str wrapped in StringIO object
+        if 'FlexText' not in self._transform.Parsers:
+            config_str_io = io.StringIO(config_str)
+            config_str_txt = io.StringIO(config_str_io.read())
+            self._transform.add_parser("FlexText", config_str_txt)
+
+        # Run FlexText parser
+        try:
+            if meta['PayloadFormat'] == 'stix-tlp':
+                self._transform.transform(
+                    datafile,
+                    meta['PayloadFormat'],
+                    "FlexText",
+                    target_file=destination_file_obj
+                )
+
+            else:
+                self._transform.transform(
+                    datafile,
+                    meta['PayloadFormat'],
+                    "FlexText",
+                    target_file=destination_file_obj,
+                    source_meta_data=meta
+                )
+
+        except Exception as e:
+            self._logger.error(
+                "LQMT-FlexText-Parser: Error parsing file file='{0}' exception='{1}'".format(datafile, e))
---
+++ packages/lqmt/lqmt-1.3.1.tar.gz/lqmt-1.3.1/lqmt/test/parser_test.py
@@ -0,0 +1,110 @@
+from unittest import TestCase, main
+from lqmt.lqm.parsers.FlexTransform.parser import FlexTransformParser
+from lqmt.lqm.systemconfig import SystemConfig
+from lqmt.test.test_data.sample_inputs import CFM13ALERT, CFM20ALERT
+import io
+import time
+import uuid
+
+
+class TestParser(TestCase):
+    """
+    Testing class for the parser alert parser.
+    """
+
+    def setUp(self):
+        sysconf = SystemConfig()
+        self.sys_config = sysconf.getConfig()
+        self.sys_config = self.sys_config['parsers']
+        self.flext = FlexTransformParser()
+
+        # Add parsers for the different data formats
+        self.flext.add_parser('LQMTools', 'resources/sampleConfigurations/lqmtools.cfg')
+        self.flext.add_parser('Cfm13Alert', 'resources/sampleConfigurations/cfm13.cfg')
+        self.flext.add_parser('Cfm20Alert', 'resources/sampleConfigurations/cfm20alert.cfg')
+        self.flext.add_parser('stixtlp', 'resources/sampleConfigurations/stix_tlp.cfg')
+
+        # Standard timestamp that can be used so all meta files use the same time and for easier validation
+        self.time = str(time.time()).split('.')[0]
+
+        # Parse all the data ahead of the tests
+        self.cfm13_parsed_data = self.flext.parse(io.StringIO(CFM13ALERT), self.getMeta("Cfm13Alert"))
+        self.cfm20_parsed_data = self.flext.parse(io.StringIO(CFM20ALERT), self.getMeta("Cfm20Alert"))
+
+    def getMeta(self, payloadFormat):
+        """
+        Method for generating metadata. Relatively static for now, but can be expanded to be randomized on each test run
+
+        :param payloadFormat: Expected format of the payload.
+        :return: Returns metadata in a dictionary
+        """
+        meta = {
+            "PayloadFormat": payloadFormat,
+            "SendingSite": "ANL",
+            "PayloadType": "Alert",
+            "UploadID": str(uuid.uuid4()).upper(),
+            "FileName": "TestAlert",
+            "SentTimestamp": self.time,
+        }
+
+        return meta
+
+    # CFM13 format tests
+    def test_cfm13_content_returned(self):
+        self.assertEquals(len(self.cfm13_parsed_data), 1)
+
+    def test_cfm13_indicator(self):
+        self.assertEquals(self.cfm13_parsed_data[0]._indicator, "10.10.10.10")
+
+    def test_cfm13_indicator_type(self):
+        self.assertEquals(self.cfm13_parsed_data[0]._indicatorType, "IPv4Address")
+
+    def test_cfm13_action(self):
+        self.assertEquals(self.cfm13_parsed_data[0]._action1, "Block")
+
+    def test_cfm13_detectedTime(self):
+        pass
+        # self.assertEquals(self.cfm13_parsed_data[0]._detectedTime, 1456116353.0)
+
+    def test_cfm13_duration(self):
+        self.assertEquals(self.cfm13_parsed_data[0]._duration1, "86400")
+        self.assertIsNone(self.cfm13_parsed_data[0]._duration2)
+
+    def test_cfm13_sensitivity(self):
+        self.assertEquals(self.cfm13_parsed_data[0]._sensitivity, "noSensitivity")
+
+    def test_cfm13_restriction(self):
+        self.assertEquals(self.cfm13_parsed_data[0]._restriction, "AMBER")
+
+    # CFM20 format tests
+    def test_cfm20_content_returned(self):
+        self.assertEquals(len(self.cfm20_parsed_data), 1)
+
+    def test_cfm20_indicator(self):
+        self.assertEquals(self.cfm20_parsed_data[0]._indicator, "8675:a289:5:102c::bd8:baac")
+
+    def test_cfm20_indicator_type(self):
+        self.assertEquals(self.cfm20_parsed_data[0]._indicatorType, "IPv6Address")
+
+    def test_cfm20_action(self):
+        self.assertEquals(self.cfm13_parsed_data[0]._action1, "Block")
+
+    def test_cfm20_detectedTime(self):
+        pass
+
+    def test_cfm20_duration(self):
+        self.assertEquals(self.cfm20_parsed_data[0]._duration1, "86400")
+        self.assertIsNone(self.cfm20_parsed_data[0]._duration2)
+
+    def test_cfm20_sensitivity(self):
+        self.assertEquals(self.cfm20_parsed_data[0]._sensitivity, "noSensitivity")
+
+    def test_cfm20_restriction(self):
+        self.assertIsNone(self.cfm20_parsed_data[0]._restriction)
+
+    # STIX TLP format tests
+    def test_stix_tlp(self):
+        pass
+
+if __name__ == '__main__':
+    main()
