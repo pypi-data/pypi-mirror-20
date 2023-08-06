8039d1c57e184f8c58fb3ef35d3a094ac941e756
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:48 +0000

    Add package limix-inference/1.0.14/limix-inference-1.0.14.tar.gz

---
+++ packages/limix-inference/limix-inference-1.0.14.tar.gz/limix-inference-1.0.14/limix_inference/lmm/__init__.py
@@ -0,0 +1,23 @@
+"""
+*******************
+Linear Mixed Models
+*******************
+
+- :class:`.FastLMM`
+- :class:`.SlowLMM`
+
+Fast implementation
+^^^^^^^^^^^^^^^^^^^
+
+.. autoclass:: FastLMM
+  :members:
+
+General implementation
+^^^^^^^^^^^^^^^^^^^^^^
+
+.. autoclass:: SlowLMM
+  :members:
+"""
+
+from ._fastlmm import FastLMM
+from ._slowlmm import SlowLMM
