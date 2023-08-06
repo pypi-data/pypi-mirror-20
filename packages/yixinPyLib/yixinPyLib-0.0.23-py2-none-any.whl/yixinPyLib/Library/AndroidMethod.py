b53f55da438c0018ad9915170de0ae9d998341a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:07 +0000

    Add package yixinPyLib/0.0.23/yixinPyLib-0.0.23-py2-none-any.whl

---
+++ packages/yixinPyLib/yixinPyLib-0.0.23-py2-none-any.whl/yixinPyLib/Library/AndroidMethod.py
@@ -0,0 +1,101 @@
+# -*- coding: UTF-8 -*- 
+import time
+from appium import webdriver
+
+class androidMethod():
+    '''appium公共方法封装集'''
+    def __init__(self,driver):
+        self.driver = driver
+    def click(self,locator):
+        self.wait(locator)
+        self.driver.find_element(*locator[0]).click()
+    def isExit(self,locator):
+        res = True
+        try:
+            self.wait(locator)
+            self.driver.find_element(*locator[0])
+        except Exception,e:
+            res = False
+        return res
+    def setText(self,locator,text):
+        self.wait(locator)
+        ime = self.driver.active_ime_engine
+        self.driver.deactivate_ime_engine()
+        self.driver.find_element(*locator[0]).send_keys(text)
+        if ime != None:
+            self.driver.activate_ime_engine(ime)
+    def getText(self,locator):
+        self.wait(locator)
+        self.driver.find_element(*locator[0]).get_attribute('text')
+    def Sleep(self,t):
+        time.sleep(t)
+    #轻触一个界面元素
+    def tap(self,locator):
+        self.wait(locator)
+        a = self.driver.find_element(*locator[0])
+        l = a.location
+        x = int(l.get('x'))+20
+        y = int(l.get('y'))+20
+        self.driver.tap([(x,y)],1000)
+    #获取一个列表的数量
+    def getLength(self,locator):
+        self.wait(locator)
+        return len(self.driver.find_elements(*locator[0]))
+    #判断一个列表中的元素对应个数的位置是否存在
+    def isExit2(self,locator,count):
+        self.wait(locator)
+        res = False
+        try:
+            list = self.driver.find_elements(*locator[0])
+            for i,one in enumerate(list):
+                if i == count:
+                    res = True
+        except Exception,e:
+            res = False
+        return res
+    def getText2(self,locator,count):
+        self.wait(locator)
+        result =''
+        list = self.driver.find_elements(*locator[0])
+        for i,one in enumerate(list):
+            if i == count:
+                result = one.get_attribute('text')
+                break
+        if len(result) > 0 :
+            pass
+            #self.logger.info(u'查找到信息：'+result)
+        else:
+            pass
+            #self.logger.info(u'未找到信息')
+    def click2(self,locator,count):
+        self.wait(locator)
+        result =''
+        list = self.driver.find_elements(*locator[0])
+        for i,one in enumerate(list):
+            if i == count:
+                one.click()
+                break
+        return
+    def setText2(self,locator,count,text):
+        self.wait(locator)
+        ime = self.driver.active_ime_engine
+        self.driver.deactivate_ime_engine()
+        result =''
+        list = self.driver.find_elements(*locator[0])
+        for i,one in enumerate(list):
+            if i == count:
+                self.DeleteStr()
+                one.send_keys(text)
+                break
+        if ime != None:
+            self.driver.activate_ime_engine(ime)
+        return
+    def findelement(self,locator):
+        self.wait(locator)
+        return self.driver.find_elements(*locator[0])
+    def DeleteStr(self):
+        self.driver.press_keycode(29,28672)
+        self.driver.press_keycode(112)
+
+    def wait(self, locator):
+        self.driver.implicitly_wait(locator[2])
