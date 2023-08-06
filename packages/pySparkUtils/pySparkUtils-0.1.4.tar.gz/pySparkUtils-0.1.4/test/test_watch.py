c649a836ec855a85c78ba3fd27ad9d8d16b4bd7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:18 +0000

    Add package pySparkUtils/0.1.4/pySparkUtils-0.1.4.tar.gz

---
+++ packages/pySparkUtils/pySparkUtils-0.1.4.tar.gz/pySparkUtils-0.1.4/test/test_watch.py
@@ -0,0 +1,46 @@
+from pySparkUtils.utils import watch
+import pytest
+import time
+pytestmark = pytest.mark.usefixtures("eng")
+
+def test_no_sc():
+    @watch
+    def test_func(sleep_time):
+        time.sleep(sleep_time)
+        return
+    with pytest.raises(ValueError) as excinfo:
+        test_func(10)
+    assert 'Could not find sc in the input params' in str(excinfo.value)
+
+
+def test_no_fail_long(eng):
+    @watch
+    def test_func(sc):
+        def test_inner(i):
+            time.sleep(0.1)
+            return i
+        result = sc.parallelize(range(100)).map(test_inner).reduce(lambda x, y: x+y)
+        return result
+    answer = test_func(eng)
+    assert answer == sum(range(100))
+
+
+def test_no_fail_short(eng):
+    @watch
+    def test_func(sc):
+        result = sc.parallelize(range(10)).reduce(lambda x, y: x+y)
+        return result
+    answer = test_func(eng)
+    assert answer == sum(range(10))
+
+
+def test_fail(eng):
+    @watch
+    def test_func(sc):
+        def failing(x):
+            time.sleep(x)
+            return x/0
+        return sc.parallelize(range(10)).map(failing).collect()
+
+    result = test_func(eng)
+    assert result is None
