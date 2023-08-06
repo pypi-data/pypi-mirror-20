36d0306c1e4436f9e9de1da38fb4786239d08eb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:07 +0000

    Add package mulpro/0.1.3/mulpro-0.1.3.tar.gz

---
+++ packages/mulpro/mulpro-0.1.3.tar.gz/mulpro-0.1.3/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from warnings import warn
+
+try:
+    import multiprocessing
+    multiprocessing_av = True
+except ImportError:
+    multiprocessing_av = False
+        
+if multiprocessing_av:
+        
+    from setuptools import setup
+
+    setup(name='mulpro',
+          version='0.1.3',
+          description='Running any worker function in parallel and producing output in a separate process',
+          author='Max F. Hantke',
+          author_email='maxhantke@gmail.com',
+          url='https://github.com/mhantke/mulpro',
+          #install_requires=['numpy', 'h5py', 'mpi4py>=2.0.0'],
+          packages = ['mulpro'],
+          #package_dir={'h5writer':'src'},
+    )   
+    
+else:
+    
+    print 100*"*"
+
+    print "\tThe multiprocessing package cannot be imported! Please install multiprocessing."
+
+    print 100*"*"
+
