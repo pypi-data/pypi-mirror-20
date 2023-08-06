9dfe7f587f8b3eff2852149c741c165f447fa96f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:54 +0000

    Add package pumpp/0.1.2/pumpp-0.1.2.tar.gz

---
+++ packages/pumpp/pumpp-0.1.2.tar.gz/pumpp-0.1.2/pumpp/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''Practically universal music pre-processing'''
+
+from .version import version
+from .core import *
+from .exceptions import *
+from . import feature
+from . import task
+from .sampler import Sampler
