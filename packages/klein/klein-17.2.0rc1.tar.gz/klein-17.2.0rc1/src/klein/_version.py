517012d6736e023d2e93b27644f59cac6984a8c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:06 +0000

    Add package klein/17.2.0rc1/klein-17.2.0rc1.tar.gz

---
+++ packages/klein/klein-17.2.0rc1.tar.gz/klein-17.2.0rc1/src/klein/_version.py
@@ -0,0 +1,11 @@
+"""
+Provides Klein version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update Klein` to change this file.
+
+from incremental import Version
+
+__version__ = Version('Klein', 17, 2, 0, release_candidate=1)
+__all__ = ["__version__"]
