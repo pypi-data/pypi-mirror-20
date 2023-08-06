32f94b075b339e58efccfd26a662d058993af39f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:01 +0000

    Add package nester130/1.3.0/nester130-1.3.0.tar.gz

---
+++ packages/nester130/nester130-1.3.0.tar.gz/nester130-1.3.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+
+		name       = 'nester130',
+		version    = '1.3.0',
+		py_modules = ['nester'],
+		author	   = 'hfpython',
+		author_email  = 'hfpython@headfirstlabs.com',
+		url			  = 'http://www.headfirstlab.com',
+		description   = 'A simple printer of nested list',
+
+	)
