b45ccabc399f2aafab9de1123ce2e0765a7c7b9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:45 +0000

    Add package score.ws/0.0.7/score.ws-0.0.7.tar.gz

---
+++ packages/score.ws/score.ws-0.0.7.tar.gz/score.ws-0.0.7/score/ws/worker.py
@@ -0,0 +1,63 @@
+import asyncio
+import websockets
+import score.serve
+import concurrent
+
+
+class WebsocketWorker(score.serve.AsyncioWorker):
+
+    def __init__(self, conf, handler):
+        if not asyncio.iscoroutinefunction(handler):
+            raise ValueError('Handler must be a coroutine function')
+        self.conf = conf
+        self.handler = handler
+
+    @asyncio.coroutine
+    def create_connection(self, protocol, uri):
+        with self.conf.ctx.Context() as ctx:
+            ctx.websocket = protocol
+            coroutine = self.handler(ctx)
+            future = self.loop.create_task(coroutine)
+            self.connections.append(future)
+            try:
+                yield from future
+            except websockets.ConnectionClosed:
+                pass
+            except concurrent.futures.CancelledError:
+                pass
+            except Exception as e:
+                self.conf.log.exception(e)
+            finally:
+                self.connections.remove(future)
+
+    def _prepare(self):
+        self.connections = []
+
+    @asyncio.coroutine
+    def _start(self):
+        extra_kwargs = {}
+        if self.conf.reuse_port:
+            extra_kwargs['reuse_port'] = True
+        self.server = yield from websockets.serve(
+            self.create_connection,
+            self.conf.host, self.conf.port, loop=self.loop, **extra_kwargs)
+
+    @asyncio.coroutine
+    def _pause(self):
+        if self.connections and self.conf.stop_timeout != 0:
+            self.server.server.close()
+            if self.conf.stop_timeout is None:
+                yield from asyncio.wait(self.connections, loop=self.loop)
+            else:
+                all_closed = asyncio.wait(self.connections, loop=self.loop)
+                try:
+                    yield from asyncio.wait_for(
+                        asyncio.shield(all_closed, loop=self.loop),
+                        self.conf.stop_timeout, loop=self.loop)
+                except asyncio.TimeoutError:
+                    pass
+        self.server.close()
+        yield from self.server.wait_closed()
+
+    def _cleanup(self, exception):
+        pass
