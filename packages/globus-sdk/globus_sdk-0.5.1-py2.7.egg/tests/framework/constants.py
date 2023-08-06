8671589addfc43970733dc743ad353c30a7acd31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:55 +0000

    Add package globus-sdk/0.5.1/globus_sdk-0.5.1-py2.7.egg

---
+++ packages/globus-sdk/globus_sdk-0.5.1-py2.7.egg/tests/framework/constants.py
@@ -0,0 +1,17 @@
+GO_EP1_ID = "ddb59aef-6d04-11e5-ba46-22000b92c6ec"
+GO_EP2_ID = "ddb59af0-6d04-11e5-ba46-22000b92c6ec"
+
+# ------ #
+# tokens #
+# ------ #
+# sdktester1a@globusid.org transfer refresh token for Native App 1
+SDKTESTER1A_NATIVE1_TRANSFER_RT = (
+    "AQEAAAAAAASDACYy_O7mQx-CCTX9ZGg7FAHiv5v3bU"
+    "2b1puK1jxRXnt69ClBpJbtygAjS7BHLokxKRyRQ-7H")
+# sdktester1a@globusid.org auth refresh token for Native App 1
+SDKTESTER1A_NATIVE1_AUTH_RT = (
+    "AQEAAAAAAASC_0CX1ozEsZ-F4SjhFhMPgi-MgyxWTn"
+    "e0_Tl6Wx3HnVMO3QUnVhDx65xXrR0Y0p8mBAJWjl-R")
+# ---------- #
+# end tokens #
+# ---------- #
