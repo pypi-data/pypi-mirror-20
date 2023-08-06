b84c1fbab51b80156c08e82621ef45e22c58649d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:29 +0000

    Add package 7xydothis/0.1/7xydothis-0.1.tar.gz

---
+++ packages/7xydothis/7xydothis-0.1.tar.gz/7xydothis-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+    name="7xydothis",
+    version="0.1",
+    packages=find_packages(),
+    description = "Utitilies to use the APIs",
+    long_description = "Utitilies to use the APIs",
+    author = "bernardfrk",
+    author_email = "bernard.frk@gmail.com",
+    maintainer='bernardfrk',
+    maintainer_email='bernard.frk@gmail.com',    
+    url='https://github.com/frkb/7xydothis',
+    classifiers = [],
+    
+)
