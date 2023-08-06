7abe5e8dee9c3c9b49b4e1b409eedcbfb1988843
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:02 +0000

    Add package scales-rpc/1.0.7/scales-rpc-1.0.7.tar.gz

---
+++ packages/scales-rpc/scales-rpc-1.0.7.tar.gz/scales-rpc-1.0.7/scales/http/builder.py
@@ -0,0 +1,52 @@
+from .sink import (
+  HttpTransportSink
+)
+from ..core import Scales
+from ..loadbalancer.aperture import ApertureBalancerSink
+from ..pool import SingletonPoolSink
+
+
+class _HttpIface(object):
+  def Get(self, url, **kwargs):
+    """Issue an HTTP GET to url.
+
+    kwargs aligns with arguments to requests, although timeout is ignored.
+    """
+    pass
+
+  def Post(self, url, **kwargs):
+    """Issue an HTTP POST to url.
+
+    kwargs aligns with arguments to requests, although timeout is ignored.
+    """
+    pass
+
+  def Put(self, url, **kwargs):
+    """Issue an HTTP PUT to url.
+
+    kwargs aligns with arguments to requests, although timeout is ignored.
+    """
+    pass
+
+  def Delete(self, url, **kwargs):
+    """Issue an HTTP DELETE to url.
+
+    kwargs aligns with arguments to requests, although timeout is ignored.
+    """
+    pass
+
+
+class Http(object):
+  @staticmethod
+  def NewBuilder():
+    return Scales.NewBuilder(_HttpIface)\
+      .WithSink(ApertureBalancerSink.Builder())\
+      .WithSink(SingletonPoolSink.Builder())\
+      .WithSink(HttpTransportSink.Builder())
+
+  @staticmethod
+  def NewClient(uri, timeout=60):
+    return Http.NewBuilder()\
+      .SetUri(uri)\
+      .SetTimeout(timeout)\
+      .Build()
