3a8e99bff9f066f074c0520c3ea6e61c9b7bd4d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:11 +0000

    Add package stacked_text_analysis/0.1/stacked_text_analysis-0.1.tar.gz

---
+++ packages/stacked_text_analysis/stacked_text_analysis-0.1.tar.gz/stacked_text_analysis-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+from codecs import open
+from os import path
+
+setup(name='stacked_text_analysis',
+      version='0.1',
+      description='post text analyzer',
+      author='Stacked Sports',
+      author_email="info@stackedsports.com",
+      install_requires=['nltk']
+      )
