5d25aebfd515fcaf2a64a37def1bccede63be33b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:01 +0000

    Add package PyColorizer/1.0.0/PyColorizer-1.0.0.tar.gz

---
+++ packages/PyColorizer/PyColorizer-1.0.0.tar.gz/PyColorizer-1.0.0/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='PyColorizer',
+    version='1.0.0',
+    description='Add color in your python shell scripts.',
+    author='Pierre Rambaud (GoT)',
+    author_email='pierre.rambaud86@gmail.com',
+    url='https://github.com/PierreRambaud/pycolorizer',
+    license='LGPLv3',
+    py_modules=['pycolorizer'],
+    install_requires=[],
+    tests_require=[
+        'nose',
+        'pep8',
+        'flake8'
+    ],
+)
