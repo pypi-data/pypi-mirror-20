6821fce6d742f2c848fb25b34b110f29290e4d39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:46 +0000

    Add package aioyoyo/1.2.1/aioyoyo-1.2.1.tar.gz

---
+++ packages/aioyoyo/aioyoyo-1.2.1.tar.gz/aioyoyo-1.2.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+long_description = """Aioyoyo
+A port of oyoyo to Asyncio for Python 3.5
+
+Uses Asyncio instead of its original threading client. Creating an IRCClient instance will create the protocol instance.
+To start the connection run IRCClient.connect(); (coroutine)
+
+Uses oyoyo from [illuminatedWax](https://github.com/illuminatedwax)'s Pesterchum, slightly modified
+"""
+
+setup(
+    name='aioyoyo',
+    version='1.2.1',
+    description='An Asyncio oyoyo port',
+    long_description=long_description,
+    url='https://github.com/henry232323/aioyoyo',
+    author='henry232323',
+    author_email='henry@rhodochrosite.xyz',
+    license='GPL',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Communications :: Chat :: Internet Relay Chat',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='IRC Asyncio',
+    packages=["aioyoyo", "aioyoyo.oyoyo"],
+)
