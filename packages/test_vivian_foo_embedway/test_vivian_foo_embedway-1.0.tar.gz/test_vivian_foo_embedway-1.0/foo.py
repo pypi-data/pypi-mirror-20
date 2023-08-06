947668f73cc24f2e95dc8f0e62bc232b796661f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:51 +0000

    Add package test_vivian_foo_embedway/1.0/test_vivian_foo_embedway-1.0.tar.gz

---
+++ packages/test_vivian_foo_embedway/test_vivian_foo_embedway-1.0.tar.gz/test_vivian_foo_embedway-1.0/foo.py
@@ -0,0 +1,5 @@
+# coding = utf-8
+
+
+def my_foo():
+    print 'hello world'
---
+++ packages/test_vivian_foo_embedway/test_vivian_foo_embedway-1.0.tar.gz/test_vivian_foo_embedway-1.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='test_vivian_foo_embedway',
+      version='1.0',
+      py_modules=['foo'],
+      author='vivianzhou',
+      author_email='18602755192@163.com',
+      url='http://www.baidu.com',
+      )
