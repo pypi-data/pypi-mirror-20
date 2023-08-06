04fcac06e1db86e78bc45f90ec7f18b2d24de6f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:55 +0000

    Add package fepy/0.1/fepy-0.1.tar.gz

---
+++ packages/fepy/fepy-0.1.tar.gz/fepy-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='fepy',
+      version='0.01',
+      description='The python library for financial engineering',
+      url='https://github.com/eggachecat/fepy',
+      author='sunao',
+      author_email='sunao_0626@hotmail.com',
+      license='MIT',
+      packages=['fepy'],
+      zip_safe=False)
