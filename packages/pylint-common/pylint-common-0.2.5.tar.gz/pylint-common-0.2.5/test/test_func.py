ae342ef9f31939fcd78b32562935d9128d6a4775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:23 +0000

    Add package pylint-common/0.2.5/pylint-common-0.2.5.tar.gz

---
+++ packages/pylint-common/pylint-common-0.2.5.tar.gz/pylint-common-0.2.5/test/test_func.py
@@ -0,0 +1,28 @@
+
+from os.path import join, dirname, abspath
+import unittest
+from pylint.testutils import make_tests, LintTestUsingModule, LintTestUsingFile, cb_test_gen, linter
+import sys
+
+
+INPUT_DIR = join(dirname(abspath(__file__)), 'input')
+MESSAGES_DIR = join(dirname(abspath(__file__)), 'messages')
+CALLBACKS = [cb_test_gen(LintTestUsingModule), cb_test_gen(LintTestUsingFile)]
+FILTER_RGX = None
+
+
+linter.load_plugin_modules(['pylint_common'])
+linter.global_set_option('required-attributes', ())  # remove required __revision__
+
+
+def suite():
+    return unittest.TestSuite([unittest.makeSuite(test, suiteClass=unittest.TestSuite)
+                              for test in make_tests(INPUT_DIR, MESSAGES_DIR,
+                                                     FILTER_RGX, CALLBACKS)])
+
+if __name__=='__main__':
+    if len(sys.argv) > 1:
+        FILTER_RGX = sys.argv[1]
+        del sys.argv[1]
+    unittest.main(defaultTest='suite')
+
