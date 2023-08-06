d7f8f17b9dca2ebf01265276caa4835c8d138a41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:04 +0000

    Add package django-databrowse/2016.3.16/django-databrowse-2016.3.16.tar.gz

---
+++ packages/django-databrowse/django-databrowse-2016.3.16.tar.gz/django-databrowse-2016.3.16/django_databrowse/plugins/objects.py
@@ -0,0 +1,33 @@
+from django import http
+from django.core.exceptions import ObjectDoesNotExist
+from django_databrowse.datastructures import EasyModel
+from django_databrowse.sites import DatabrowsePlugin
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+import urlparse
+
+class ObjectDetailPlugin(DatabrowsePlugin):
+    def model_view(self, request, model_databrowse, url):
+        # If the object ID wasn't provided, redirect to the model page,
+        # Which is one level up.
+        if url is None:
+            return http.HttpResponseRedirect(
+                urlparse.urljoin(request.path, '../')
+            )
+        easy_model = EasyModel(
+            model_databrowse.site,
+            model_databrowse.model
+        )
+        try:
+            obj = easy_model.object_by_pk(url)
+        except ObjectDoesNotExist, e:
+            raise http.Http404('Id not found')
+        except ValueError, e:
+            raise http.Http404('Invalid format key provided')
+        return render_to_response(
+            'databrowse/object_detail.html',
+            {
+                'object': obj,
+                'root_url': model_databrowse.site.root_url
+            }, context_instance=RequestContext(request)
+        )
