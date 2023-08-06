65411b7ec4655cdfdeecaf73d0ef8d4029a4acdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:33 +0000

    Add package plot/0.4.1/plot-0.4.1-py2.py3-none-any.whl

---
+++ packages/plot/plot-0.4.1-py2.py3-none-any.whl/plot/finetune/global_axis/main.py
@@ -0,0 +1,24 @@
+"""
+Adjust the global figure axis
+"""
+from typing import Dict
+from ...tk.fnTK import compose
+from .add_axis_labels import add_axis_labels
+from .add_figure_title import add_figure_title
+
+
+def main(params):
+    # type: (Dict) -> Dict
+    """Adjust global parameters
+
+    Args:
+        params (dict): plotting parameter dictionary
+
+    Returns:
+        same as input
+    """
+    aux = compose([
+        add_axis_labels,
+        add_figure_title,
+        ])
+    return aux(params)
