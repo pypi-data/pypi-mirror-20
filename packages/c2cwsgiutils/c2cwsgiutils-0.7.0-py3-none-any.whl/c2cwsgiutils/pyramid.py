2824ac9974016412b43b1ec1a6e688607737a0bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:09 +0000

    Add package c2cwsgiutils/0.7.0/c2cwsgiutils-0.7.0-py3-none-any.whl

---
+++ packages/c2cwsgiutils/c2cwsgiutils-0.7.0-py3-none-any.whl/c2cwsgiutils/pyramid.py
@@ -0,0 +1,19 @@
+import cornice
+import pyramid_tm
+
+from c2cwsgiutils import stats, pyramid_logging, sql_profiler
+
+
+def includeme(config):
+    """
+    Setup all the pyramid services and event handlers provided by this library.
+    :param config: The pyramid Configuration
+    """
+    config.add_settings(handle_exceptions=False)
+    config.include(pyramid_tm.includeme)
+    config.include(cornice.includeme)
+    stats.init(config)
+    pyramid_logging.install_subscriber(config)
+    sql_profiler.init(config)
+    config.scan("c2cwsgiutils.services")
+    config.scan("c2cwsgiutils.errors")
