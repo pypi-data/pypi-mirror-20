74b29390abfb9c176bdc980ef610aeebf80e21de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:58 +0000

    Add package tvdoon/0.1.0/tvdoon-0.1.0.tar.gz

---
+++ packages/tvdoon/tvdoon-0.1.0.tar.gz/tvdoon-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name="tvdoon",
+    version="0.1.0",
+    author="LinArcX",
+    author_email="linarcx@gmail.com",
+    description="Show Information About Tv Show's, Ratings And So Many Cool Features!",
+    license=" GPLv3",
+    keywords="tvShow, upcoming tvShow",
+    url="https://github.com/LinArcX/tvdoon",
+    download_url='https://github.com/LinArcX/tvdoon/archive/0.1.0.tar.gz',
+    packages=['tvdoon'],
+    install_requires= ['tvdb_api','prettytable', 'pyfiglet'],
+    setup_requires = ['tvdb_api', 'prettytable', 'pyfiglet'],
+    scripts=['tvdoon/tvdoon'],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
