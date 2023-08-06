3e6e1caf8944bad7b4b7e801b8447774edd71c5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:30 +0000

    Add package tropo/0.2.0/tropo-0.2.0-py2.py3-none-any.whl

---
+++ packages/tropo/tropo-0.2.0-py2.py3-none-any.whl/tropo/__init__.py
@@ -0,0 +1,6 @@
+from .base import Template, dump, dumps
+from .compute import VirtualMachine
+from .storage import StorageAccount
+from .network import VirtualNetwork, PublicIPAddress, NetworkInterface
+
+
