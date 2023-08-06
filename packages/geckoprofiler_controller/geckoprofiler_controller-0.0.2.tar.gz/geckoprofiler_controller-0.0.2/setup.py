bae7b2e63a0ae9dd20a862b16f6711444f6f120c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:49 +0000

    Add package geckoprofiler_controller/0.0.2/geckoprofiler_controller-0.0.2.tar.gz

---
+++ packages/geckoprofiler_controller/geckoprofiler_controller-0.0.2.tar.gz/geckoprofiler_controller-0.0.2/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import os
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2'
+CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
+
+# dependencies
+try:
+    with open('requirements.txt') as f:
+        deps = f.read().splitlines()
+except:
+    deps = [
+        'tornado==4.4.2',
+        'websocket-client==0.40.0'
+    ]
+
+# get documentation from the README and HISTORY
+try:
+    with open(os.path.join(CURRENT_PATH, 'README.rst')) as doc:
+        readme = doc.read()
+except:
+    readme = ''
+
+try:
+    with open(os.path.join(CURRENT_PATH, 'HISTORY.rst')) as doc:
+        history = doc.read()
+except:
+    history = ''
+
+long_description = readme + '\n\n' + history
+
+# main setup script
+setup(
+    name='geckoprofiler_controller',
+    version=VERSION,
+    description='Control the Gecko Profiler by Python and Web Socket',
+    long_description=long_description,
+    author='Askeing Yen',
+    author_email='askeing@gmail.com',
+    url='https://github.com/askeing/geckoprofiler_controller',
+    keywords='mozilla firefox gecko profiler utilities ',
+    license='MPL',
+    install_requires=deps,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False
+)
