0a272e095567f6507b60399c7f4716fe819e47b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:42 +0000

    Add package ConnectorDB/0.3.5/ConnectorDB-0.3.5.tar.gz

---
+++ packages/ConnectorDB/ConnectorDB-0.3.5.tar.gz/ConnectorDB-0.3.5/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# python setup.py sdist
+# python setup.py bdist_wheel --universal
+# twine upload dist/*
+from __future__ import absolute_import
+
+
+from setuptools import setup, find_packages
+
+
+setup(name='ConnectorDB',
+      version='0.3.5',  # The a.b of a.b.c follows connectordb version. c is the version of python. Remember to change __version__ in __init__
+      description='ConnectorDB Python Interface',
+      author='ConnectorDB contributors',
+      license="Apache-2.0",
+      author_email='support@connectordb.com',
+      url='https://github.com/connectordb/connectordb-python',
+      packages=find_packages(exclude=['contrib', 'docs', '*_test']),
+      classifiers=[#'Development Status :: 3 - Alpha',
+                      'Intended Audience :: Developers',
+                      'Intended Audience :: Science/Research',
+                      'License :: OSI Approved :: MIT License',
+                      'Programming Language :: Python :: 2',
+                      'Programming Language :: Python :: 3'],
+      install_requires=["requests", "websocket-client", "jsonschema"])
