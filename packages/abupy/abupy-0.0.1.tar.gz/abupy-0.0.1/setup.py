8c52364f05f73b99807aa7348b3888ad94ea2601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:07 +0000

    Add package abupy/0.0.1/abupy-0.0.1.tar.gz

---
+++ packages/abupy/abupy-0.0.1.tar.gz/abupy-0.0.1/setup.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+from setuptools import setup, find_packages
+
+setup(
+    name='abupy',
+    version='0.0.1',
+    keywords=('阿布', 'abu', 'quant', 'quantization', '量化', '爬虫', 'crawler', 'spider', 'scrapy', 'stock', '股票',
+                'ML', '机器学习', '深度学习', 'machine learning', 'deep learning'),
+    description='股票量化',
+
+    author=u'阿布',
+    author_email='steven.wu.ven@gmail.com',
+
+    packages=find_packages(),
+    platforms='any',
+)
