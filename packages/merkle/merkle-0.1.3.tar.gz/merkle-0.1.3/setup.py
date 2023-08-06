8c37a67c48445bef07b855d7016181d5cf7fe9f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:19 +0000

    Add package merkle/0.1.3/merkle-0.1.3.tar.gz

---
+++ packages/merkle/merkle-0.1.3.tar.gz/merkle-0.1.3/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='merkle',
+      version='0.1.3',
+      description='Python Merkle Tree Implementation',
+      author='Jamie Steiner',
+      author_email='jvsteiner@gmail.com',
+      url='https://github.com/jvsteiner/merkletree',
+      download_url='https://github.com/jvsteiner/merkletree/tarball/0.1.2',
+      py_modules=['merkle'],
+      keywords=['merkle', 'tree', 'hash', 'root', 'chain'],
+      )
