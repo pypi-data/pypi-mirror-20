448835b4abccf100578b3767dfd391a55dfcdff3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:02 +0000

    Add package yixinPyLib/0.0.45/yixinPyLib-0.0.45.zip

---
+++ packages/yixinPyLib/yixinPyLib-0.0.45.zip/yixinPyLib-0.0.45/yixinPyLib/Library/AppMethod.py
@@ -0,0 +1,43 @@
+# -*- coding: UTF-8 -*- 
+from yixinPyLib.testcaseupload import uploadTestcase 
+import time
+
+class AppMethod():
+    logger = None
+    pid = 0
+    #初始化
+    def __init__(self,driver):
+        self.driver = driver
+    #日志处理
+    def __setlog__(self,log):
+        self.logger = log
+    def __log__(self,msg):
+        if self.logger == None:
+            try:
+                print msg
+            except:
+                pass
+        else:
+            self.logger.info(msg)
+    def __setpid__(self,p):
+        self.pid = p
+    #有延迟的完成完成一个测试用例
+    def FinishTestCase_timeout(self,caseid,resstr,t):
+        if self.pid != 0:
+            time.sleep(t)
+            self.FinishTestCase(caseid,resstr)
+
+    #完成一个测试用例
+    def FinishTestCase(self,caseid,resstr):
+        if self.pid != 0:
+            http = uploadTestcase.uploadTestcase(self.pid)
+            testcase = http.gettestcase(caseid)
+            self.__log__(u'记录测试用例：' + testcase[u'name'])
+            picpath = "temp.png"
+            self.driver.get_screenshot_as_file(picpath)
+            picurl = http.uploadPic(picpath,caseid)
+            #result = http.uploadResult(caseid,picurl)
+            result = http.uploadResultAssertRes(caseid,picurl,resstr)
+            self.__log__(u'当前测试用例：' +result)
+
+
