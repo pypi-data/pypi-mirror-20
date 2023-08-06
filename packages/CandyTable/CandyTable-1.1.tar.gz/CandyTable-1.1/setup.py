7ab8cf79b2d1c0d2d385fcc8572a7a5dd5109f04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:41 +0000

    Add package CandyTable/1.1/CandyTable-1.1.tar.gz

---
+++ packages/CandyTable/CandyTable-1.1.tar.gz/CandyTable-1.1/setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name = 'CandyTable',
+    packages = ['CandyTable'],
+    version = '1.1',
+    description = 'CandyTable compatible to python2 and python3',
+    author = 'davidtnfsh',
+    author_email = 'davidtnfsh@gmail.com',
+    url = 'https://github.com/david30907d/HomeWork/tree/master/Python-%E6%9B%BE%E5%AD%B8%E6%96%87/hw1-CandyCrush',
+    download_url = 'https://github.com/david30907d/HomeWork/archive/v1.1.tar.gz',
+    keywords = ['CandyTable',],
+    classifiers = [],
+    license='GPL3.0',
+    install_requires=[
+    ],
+    zip_safe=True
+)
