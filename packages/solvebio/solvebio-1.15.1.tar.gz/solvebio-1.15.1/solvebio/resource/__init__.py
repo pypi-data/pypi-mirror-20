446b7297c742a36cb5d53a19cc753ea56617dd37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:33 +0000

    Add package solvebio/1.15.1/solvebio-1.15.1.tar.gz

---
+++ packages/solvebio/solvebio-1.15.1.tar.gz/solvebio-1.15.1/solvebio/resource/__init__.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import
+
+from .depository import Depository
+from .depositoryversion import DepositoryVersion
+from .apiresource import ListObject
+from .user import User
+from .dataset import Dataset
+from .datasetfield import DatasetField
+from .datasetimport import DatasetImport
+from .datasetcommit import DatasetCommit
+from .datasettemplate import DatasetTemplate
+from .upload import Upload
+from .manifest import Manifest
+
+
+types = {
+    'Dataset': Dataset,
+    'DatasetImport': DatasetImport,
+    'DatasetCommit': DatasetCommit,
+    'DatasetTemplate': DatasetTemplate,
+    'DatasetField': DatasetField,
+    'Depository': Depository,
+    'DepositoryVersion': DepositoryVersion,
+    'Upload': Upload,
+    'Manifest': Manifest,
+    'User': User,
+    'list': ListObject
+}
