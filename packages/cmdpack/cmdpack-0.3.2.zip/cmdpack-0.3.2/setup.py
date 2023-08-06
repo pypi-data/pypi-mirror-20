b4c32c61fa25a55ecde7dbd983f030f6b9adde66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:53 +0000

    Add package cmdpack/0.3.2/cmdpack-0.3.2.zip

---
+++ packages/cmdpack/cmdpack-0.3.2.zip/cmdpack-0.3.2/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+from setuptools import setup
+import os
+
+setup(name='cmdpack',
+	version='0.3.2',
+	description='cmd run packages',
+	url='http://github.com/jeppeter/cmdpack',
+	author='jeppeter Wang',
+	author_email='jeppeter@gmail.com',
+	license='MIT',
+	packages=['cmdpack'],
+	zip_safe=False)
