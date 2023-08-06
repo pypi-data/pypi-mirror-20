2e33af64b2625aa12a47e2d88c3306acca3e09dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:30 +0000

    Add package git-up-all/0.3.2.4/git-up-all-0.3.2.4.tar.gz

---
+++ packages/git-up-all/git-up-all-0.3.2.4.tar.gz/git-up-all-0.3.2.4/setup.py
@@ -0,0 +1,42 @@
+# coding=utf-8
+from setuptools import setup, find_packages
+
+setup(
+    name="git-up-all",
+    version="0.3.2.4",
+    packages=find_packages(exclude=["tests"]),
+    scripts=['PyGitUpAll/gitupall.py'],
+    install_requires=['git-up==1.4.1', 'biplist==0.7'],
+
+    # Tests
+    test_suite="nose.collector",
+    tests_require='nose',
+
+    # Executable
+    entry_points={
+        'console_scripts': [
+            'git-up-all = gitupall:run'
+        ]
+    },
+
+    zip_safe=False,
+
+    # Metadata
+    author="Dimitrios Kanellopoulos",
+    author_email="jimmykane9@gmail.com",
+    description="Run git-up trough a json list of repos",
+    license="GPLv3",
+    keywords="git git-up git-up-all",
+    url="https://github.com/jimmykane/PyGitUpAll",
+    download_url='https://github.com/jimmykane/PyGitUpAll/tarball/master/0.3.2.4',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Topic :: Software Development :: Version Control",
+        "Topic :: Utilities"
+    ],
+
+    long_description=open('README.rst').read()
+)
