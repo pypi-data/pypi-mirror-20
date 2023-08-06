c9c64a75dc3979e5813b24777b49f09c4b01beaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:35 +0000

    Add package planemo/0.39.0/planemo-0.39.0-py2.7.egg

---
+++ packages/planemo/planemo-0.39.0-py2.7.egg/planemo/network_util.py
@@ -0,0 +1,52 @@
+import socket
+from time import time as now
+
+
+def get_free_port():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('localhost', 0))
+    port = sock.getsockname()[1]
+    sock.close()
+    return port
+
+
+# code.activestate.com/recipes/576655-wait-for-network-service-to-appear
+def wait_net_service(server, port, timeout=None):
+    """ Wait for network service to appear
+        @param timeout: in seconds, if None or 0 wait forever
+        @return: True of False, if timeout is None may return only True or
+                 throw unhandled network exception
+    """
+    if port is None:
+        raise TypeError("wait_net_service passed NoneType port value.")
+
+    port = int(port)
+
+    s = socket.socket()
+    # Following line prevents this method from interfering with process
+    # it is waiting for on localhost.
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    if timeout:
+        end = now() + timeout
+
+    while True:
+        try:
+            if timeout:
+                next_timeout = end - now()
+                if next_timeout < 0:
+                    return False
+                else:
+                    s.settimeout(next_timeout)
+
+            s.connect((server, port))
+
+        except socket.timeout:
+            # this exception occurs only if timeout is set
+            if timeout:
+                return False
+
+        except socket.error:
+            pass
+        else:
+            s.close()
+            return True
