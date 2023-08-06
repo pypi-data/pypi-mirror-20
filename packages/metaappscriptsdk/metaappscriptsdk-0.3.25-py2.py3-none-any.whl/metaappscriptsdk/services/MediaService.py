1b2ddd92da197bbcb923bdc4b4f1b8ebd3cb2c96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:57 +0000

    Add package metaappscriptsdk/0.3.25/metaappscriptsdk-0.3.25-py2.py3-none-any.whl

---
+++ packages/metaappscriptsdk/metaappscriptsdk-0.3.25-py2.py3-none-any.whl/metaappscriptsdk/services/MediaService.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+
+
+class MediaService:
+    def __init__(self, app, default_headers):
+        """
+        :type app: metaappscriptsdk.MetaApp
+        """
+        self.__app = app
+        self.__default_headers = default_headers
+
+    def persist_one(self, file_base64_content, filename, extension, mime, is_private=True):
+        """
+        Загружает файл в облако
+        :type origin: string Принимает значения ROBOT, USER
+        """
+        return self.__app.api_call("MediaService", "persist_one", locals(), {})
