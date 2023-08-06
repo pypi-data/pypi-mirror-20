d4b1d5146aa37f8b804e6d275971e955d46bc66f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:08 +0000

    Add package sqlite_checkforeignkeys/1.0.3/sqlite_checkforeignkeys-1.0.3.tar.gz

---
+++ packages/sqlite_checkforeignkeys/sqlite_checkforeignkeys-1.0.3.tar.gz/sqlite_checkforeignkeys-1.0.3/sample_app/test_foreignkey.py
@@ -0,0 +1,12 @@
+import pytest
+from django.db.utils import IntegrityError
+from django.db.backends import sqlite3
+
+from sample_app.models import Referencer
+
+
+@pytest.mark.django_db
+def test_break_foreign_key():
+    """Tries to violate the foreign key"""
+    with pytest.raises(IntegrityError):
+        Referencer.objects.create(fk_id=-1)
