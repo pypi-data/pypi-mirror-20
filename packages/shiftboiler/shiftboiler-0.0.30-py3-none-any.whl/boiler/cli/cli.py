1fda6c6cd0d79318cada54f5d1510d74e91b8d08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:01 +0000

    Add package shiftboiler/0.0.30/shiftboiler-0.0.30-py3-none-any.whl

---
+++ packages/shiftboiler/shiftboiler-0.0.30-py3-none-any.whl/boiler/cli/cli.py
@@ -0,0 +1,81 @@
+import click, os, sys, shutil
+from boiler.cli.colors import *
+from click import echo
+
+
+# -----------------------------------------------------------------------------
+# Group setup
+# -----------------------------------------------------------------------------
+
+
+@click.group(help=yellow('Welcome to project console!'))
+def cli():
+    pass
+
+
+# -----------------------------------------------------------------------------
+# Commands
+# -----------------------------------------------------------------------------
+
+@cli.command(name='run')
+@click.option('--host', '-h', default='0.0.0.0', help='Bind to')
+@click.option('--port', '-p', default=5000, help='Listen on port')
+@click.option('--reload/--no-reload', default=True, help='Reload on change?')
+@click.option('--debug/--no-debug', default=True, help='Use debugger?')
+def run(host='0.0.0.0', port=5000, reload=True, debug=True):
+    """ Run development server """
+    from werkzeug.serving import run_simple
+    from boiler.bootstrap import create_middleware
+    from config.config import DevConfig
+
+    app = create_middleware(config=DevConfig())
+    return run_simple(
+        hostname=host,
+        port=port,
+        application=app,
+        use_reloader=reload,
+        use_debugger=debug,
+    )
+
+
+@cli.command(name='shell')
+def shell():
+    """ Start application-aware shell """
+    context = dict()
+
+    # mount apps
+    from boiler.bootstrap import create_middleware
+    middleware = create_middleware()
+    context['apps'] = dict(frontend=middleware.app)
+    for mount in middleware.mounts:
+        context['apps'][mount] = middleware.mounts[mount]
+
+    # and push app context
+    app_context = middleware.app.app_context()
+    app_context.push()
+
+    # and run
+    try:
+        from IPython import embed
+        embed(user_ns=context)
+    except ImportError:
+        import code
+        code.interact(local=context)
+
+
+# -----------------------------------------------------------------------------
+# Testing commands
+# -----------------------------------------------------------------------------
+
+@cli.command(name='test',context_settings=dict(ignore_unknown_options=True))
+@click.argument('nose_argsuments', nargs=-1, type=click.UNPROCESSED)
+def test(nose_argsuments):
+    """ Run application tests """
+    from nose import run
+    params = ['__main__', '-c', 'nose.ini']
+    params.extend(nose_argsuments)
+    run(argv=params)
+
+
+
+
