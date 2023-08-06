05f21270feaf49190d27fe5884fa5047ed8da5a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:55 +0000

    Add package devour/0.4.1/devour-0.4.1.tar.gz

---
+++ packages/devour/devour-0.4.1.tar.gz/devour-0.4.1/devour/schemas.py
@@ -0,0 +1,35 @@
+
+class SchemaMeta(object):
+    pass
+
+class Schema(object):
+    def __init__(self, data, *args, **kwargs):
+        self._init_data = data
+        self._serialized_data = None
+
+        assert hasattr(self, 'Meta'), (
+            '{0} requires a Meta class to be declared'.format(self.__class__.__name__)
+        )
+
+    @property
+    def data(self):
+        if not hasattr(self, 'attributes'):
+            return self._init_data
+
+        if not self._serialized_data:
+            self._serialized_data = {}
+            for key in self.Meta.attributes:
+                val = self._init_data.get(key)
+                if type(val) == dict:
+                    try:
+                        schema = getattr(self, key)
+                        val = schema(val).data
+                    except AttributeError:
+                        # look for schema provided for
+                        # attr. let pass and add full dict
+                        # if none declared
+                        pass
+
+                self._serialized_data[key] = val
+
+        return self._serialized_data
