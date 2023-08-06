1af662b9da3aa00d1f8e26af336b9c370fe7a74b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:29 +0000

    Add package timd/0.0.3/timd-0.0.3-py2.py3-none-any.whl

---
+++ packages/timd/timd-0.0.3-py2.py3-none-any.whl/timd/__init__.py
@@ -0,0 +1,5 @@
+"""timd a simplified way to time your python applications"""
+
+__version__ = '0.0.3'
+
+from timd.measure import Measure
---
+++ packages/timd/timd-0.0.3-py2.py3-none-any.whl/timd/measure.py
@@ -0,0 +1,32 @@
+import time
+import logging
+
+
+class Measure(object):
+
+    def __init__(self, msg='Execution took {}s', logger=logging):
+        self.msg = msg
+        self.logger = logger
+        self.times = []
+        self.t0 = None
+
+    def __str__(self):
+        return ('timd.Measure: best of {} runs took {}s'
+                .format(len(self.times), min(self.times)))
+
+    def __enter__(self):
+        self.t0 = time.time()
+
+    def __exit__(self, *args):
+        dt = time.time() - self.t0
+        self.t0 = None
+        self.times.append(dt)
+        self.logger.info(self.msg.format(dt))
+
+    def __call__(self, fun):
+
+        def newfun(*args, **kwargs):
+            with self:
+                return fun(*args, **kwargs)
+
+        return newfun
