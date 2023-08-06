7e31ac328125b91c976a1a64ad6618d0e6906c8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:39 +0000

    Add package safety/1.0.0/safety-1.0.0.tar.gz

---
+++ packages/safety/safety-1.0.0.tar.gz/safety-1.0.0/HISTORY.rst
@@ -0,0 +1,60 @@
+=======
+History
+=======
+
+1.0.0 (2017-03-22)
+------------------
+
+* Added terminal size detection. Terminals with fewer than 80 columns should now display nicer reports.
+* Added an option to load the database from the filesystem or a mirror that's reachable via http(s).
+ This can be done by using the --db flag.
+* Added an API Key option that uses pyup.io's vulnerability database.
+* Added an option to cache the database locally for 2 hours. The default still is to not use the cache. Use the --cache flag.
+
+
+0.6.0 (2017-03-10)
+------------------
+
+* Made the requirements parser more robust. The parser should no longer fail on editable requirements
+  and requirements that are supplied by package URL.
+* Running safety requires setuptools >= 16
+
+0.5.1 (2016-11-08)
+------------------
+
+* Fixed a bug where not all requirement files were read correctly.
+
+0.5.0 (2016-11-08)
+------------------
+
+* Added option to read requirements from files.
+
+0.4.0 (2016-11-07)
+------------------
+
+* Filter out non-requirements when reading from stdin.
+
+0.3.0 (2016-10-28)
+------------------
+
+* Added option to read from stdin.
+
+0.2.2 (2016-10-21)
+------------------
+
+* Fix import errors on python 2.6 and 2.7.
+
+0.2.1 (2016-10-21)
+------------------
+
+* Fix packaging bug.
+
+0.2.0 (2016-10-20)
+------------------
+
+* Releasing first prototype.
+
+0.1.0 (2016-10-19)
+------------------
+
+* First release on PyPI.
---
+++ packages/safety/safety-1.0.0.tar.gz/safety-1.0.0/safety/formatter.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+try:
+    from shutil import get_terminal_size
+except ImportError:
+    # fallback for python < 3
+    import subprocess
+    from collections import namedtuple
+
+    def get_terminal_size():
+        size = namedtuple("_", "rows", "columns")
+        try:
+            rows, columns = subprocess.check_output(['stty', 'size']).split()
+            return size(rows=int(rows), columns=int(columns))
+        except (ValueError, FileNotFoundError):
+            return size(rows=0, columns=0)
+
+
+def get_advisory(vuln):
+    return vuln.advisory if vuln.advisory else "No advisory found for this vulnerability."
+
+
+class SheetReport(object):
+    REPORT_BANNER = """
+╒══════════════════════════════════════════════════════════════════════════════╕
+│                                                                              │
+│                               /$$$$$$            /$$                         │
+│                              /$$__  $$          | $$                         │
+│           /$$$$$$$  /$$$$$$ | $$  \__//$$$$$$  /$$$$$$   /$$   /$$           │
+│          /$$_____/ |____  $$| $$$$   /$$__  $$|_  $$_/  | $$  | $$           │
+│         |  $$$$$$   /$$$$$$$| $$_/  | $$$$$$$$  | $$    | $$  | $$           │
+│          \____  $$ /$$__  $$| $$    | $$_____/  | $$ /$$| $$  | $$           │
+│          /$$$$$$$/|  $$$$$$$| $$    |  $$$$$$$  |  $$$$/|  $$$$$$$           │
+│         |_______/  \_______/|__/     \_______/   \___/   \____  $$           │
+│                                                          /$$  | $$           │
+│                                                         |  $$$$$$/           │
+│  by pyup.io                                              \______/            │
+│                                                                              │
+╞══════════════════════════════════════════════════════════════════════════════╡
+    """.strip()
+
+    TABLE_HEADING = """
+╞════════════════════════════════╤═══════════════╤═════════════════════════════╡
+│ package                        │ installed     │ affected                    │
+╞════════════════════════════════╧═══════════════╧═════════════════════════════╡
+    """.strip()
+
+    TABLE_FOOTER = """
+╘════════════════════════════════╧═══════════════╧═════════════════════════════╛
+    """.strip()
+
+    TABLE_BREAK = """
+╞════════════════════════════════╡═══════════════╡═════════════════════════════╡
+    """.strip()
+
+    REPORT_HEADING = """
+│ REPORT                                                                       │
+    """.strip()
+
+    REPORT_SECTION = """
+╞══════════════════════════════════════════════════════════════════════════════╡
+    """.strip()
+
+    REPORT_FOOTER = """
+╘══════════════════════════════════════════════════════════════════════════════╛
+    """.strip()
+
+    @staticmethod
+    def render(vulns, full):
+        if vulns:
+            table = []
+            for n, vuln in enumerate(vulns):
+                table.append("│ {:30} │ {:13} │ {:27} │".format(
+                    vuln.name[:30],
+                    vuln.version[:13],
+                    vuln.spec[:27]
+                ))
+                if full:
+                    table.append(SheetReport.REPORT_SECTION)
+
+                    descr = get_advisory(vuln)
+
+                    for chunk in [descr[i:i + 76] for i in range(0, len(descr), 76)]:
+
+                        for line in chunk.splitlines():
+                            table.append("│ {:76} │".format(line))
+                    # append the REPORT_SECTION only if this isn't the last entry
+                    if n + 1 < len(vulns):
+                        table.append(SheetReport.REPORT_SECTION)
+            table = "\n".join(table)
+            return "\n".join(
+                [SheetReport.REPORT_BANNER, SheetReport.REPORT_HEADING, SheetReport.TABLE_HEADING,
+                 table, SheetReport.REPORT_FOOTER]
+            )
+        else:
+            content = "│ {:76} │".format("No known security vulnerabilities found.")
+            return "\n".join(
+                [SheetReport.REPORT_BANNER, SheetReport.REPORT_HEADING, SheetReport.REPORT_SECTION,
+                 content, SheetReport.REPORT_FOOTER]
+            )
+
+
+class BasicReport(object):
+    """Basic report, intented to be used for terminals with < 80 columns"""
+
+    @staticmethod
+    def render(vulns, full):
+        table = ["safety report", "---"]
+        if vulns:
+
+            for vuln in vulns:
+                table.append("-> {}, installed {}, affected {}".format(
+                    vuln.name,
+                    vuln.version[:13],
+                    vuln.spec[:27]
+                ))
+                if full:
+                    table.append(get_advisory(vuln))
+                    table.append("--")
+        else:
+            table.append("No known security vulnerabilities found.")
+        return "\n".join(
+            table
+        )
+
+
+def report(vulns, full=False):
+    size = get_terminal_size()
+    if size.columns >= 80:
+        return SheetReport.render(vulns, full=full)
+    return BasicReport.render(vulns, full=full)
