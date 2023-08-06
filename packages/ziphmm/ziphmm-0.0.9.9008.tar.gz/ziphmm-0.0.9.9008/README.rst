8953562a56d701d694d5b8266aa46d3b6d21aed1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:07 +0000

    Add package ziphmm/0.0.9.9008/ziphmm-0.0.9.9008.tar.gz

---
+++ packages/ziphmm/ziphmm-0.0.9.9008.tar.gz/ziphmm-0.0.9.9008/README.rst
@@ -0,0 +1,30 @@
+===========
+mini-ziphmm
+===========
+
+.. |ci-status| image:: 	https://img.shields.io/travis/mailund/mini-ziphmm.svg
+    :target: https://travis-ci.org/mailund/mini-ziphmm
+    :alt: Build status
+.. |coveralls| image:: https://img.shields.io/coveralls/mailund/mini-ziphmm.svg
+    :target: https://coveralls.io/github/mailund/mini-ziphmm
+    :alt: Coverage
+.. |versions| image:: 	https://img.shields.io/pypi/v/ziphmm.svg
+    :target: https://pypi.python.org/pypi/ziphmm
+    :alt: Packgage version
+.. |status| image:: https://img.shields.io/pypi/status/ziphmm.svg
+    :target: https://pypi.python.org/pypi/ziphmm
+    :alt: Package stability
+
+
+This is a minimal reimplementation of zipHMM in python.
+Also contains cython and weave implementations of standard hidden markov models
+for comparison.
+
+Undocumented and no install procedure - check test.py for usage
+
+To run:
+
+```bash
+make
+python test.py
+```
---
+++ packages/ziphmm/ziphmm-0.0.9.9008.tar.gz/ziphmm-0.0.9.9008/setup.py
@@ -0,0 +1,44 @@
+from setuptools import find_packages, setup
+from Cython.Build import cythonize
+import os
+from codecs import open
+import numpy
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long description from the README file
+with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="ziphmm",
+    version="0.0.9.9008",
+    packages=find_packages(),
+    ext_modules=cythonize([
+        "ziphmm/cython_funcs.pyx",
+        ]),
+    include_dirs=[numpy.get_include()],
+
+    # metadata for upload to PyPI
+    author="Anders Egerup Halager",
+    author_email="aeh@birc.au.dk",
+    license="MIT",
+    keywords="hmm zip ziphmm",
+    url="https://github.com/birc-aeh/mini-ziphmm",
+    description="An implementation of the ziphmm algorithm",
+    long_description=long_description,
+
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+)
