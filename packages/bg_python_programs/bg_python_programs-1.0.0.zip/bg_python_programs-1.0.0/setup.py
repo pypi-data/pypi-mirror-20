73ea8f6606179faf1705e2f6dbf0f0380c4c600b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:44 +0000

    Add package bg_python_programs/1.0.0/bg_python_programs-1.0.0.zip

---
+++ packages/bg_python_programs/bg_python_programs-1.0.0.zip/bg_python_programs-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name='bg_python_programs',
+        version='1.0.0',
+        py_module=['nester'],
+        author='hfpython',
+        author_email='hfpython@headfirstlabs.com',
+        url='http://www.headfristlabs.com',
+        description='A simple printer of nested lists',
+      )
