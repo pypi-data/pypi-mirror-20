643616579de14cd98f21d03e83217719d69929f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:47 +0000

    Add package libcnmc/0.16.21/libcnmc-0.16.21.tar.gz

---
+++ packages/libcnmc/libcnmc-0.16.21.tar.gz/libcnmc-0.16.21/libcnmc/models/f6_4771.py
@@ -0,0 +1,33 @@
+from cnmcmodel import CNMCModel
+from collections import OrderedDict
+
+from .fields import String, Integer
+
+class F6Res4771(CNMCModel):
+    """
+        Class for sixth file of resolution 4771(Despatxos)
+    """
+
+    schema = OrderedDict([
+        ('identificador', String()),
+        ('cini', String()),
+        ('denominacion', String()),
+        ('anio_aps', String()),
+        ('valor_inversion', Integer())
+    ])
+
+    @property
+    def ref(self):
+        return self.store.identificador
+
+    def __cmp__(self, other):
+        comp_fields = []
+        if self.diff(other, comp_fields):
+            return True
+        else:
+            return False
+
+
+
+
+
