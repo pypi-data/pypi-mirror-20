26b9ab0f24ba1086ce46963e78fb69d883715c14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:00 +0000

    Add package asyncorm/0.0.7/asyncorm-0.0.7-py2.py3-none-any.whl

---
+++ packages/asyncorm/asyncorm-0.0.7-py2.py3-none-any.whl/asyncorm/exceptions.py
@@ -0,0 +1,32 @@
+__all__ = ('FieldError', 'ManagerError', 'ModelError', 'ModuleError',
+           'QuerysetError', 'SerializerError')
+
+
+class FieldError(Exception):
+    '''to be raised when there are field errors detected'''
+    pass
+
+
+class ManagerError(Exception):
+    '''to be raised when there are queryset errors detected'''
+    pass
+
+
+class ModelError(Exception):
+    '''to be raised when there are model errors detected'''
+    pass
+
+
+class SerializerError(Exception):
+    '''to be raised when there are model errors detected'''
+    pass
+
+
+class ModuleError(Exception):
+    '''to be raised when there are model module or config errors detected'''
+    pass
+
+
+class QuerysetError(Exception):
+    '''to be raised when there are queryset errors detected'''
+    pass
