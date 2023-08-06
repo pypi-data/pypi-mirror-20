98e9477fdcb4cdd9a42d7446971ab12e78c86ad4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:59 +0000

    Add package nglview/0.6.2.3/nglview-0.6.2.3.tar.gz

---
+++ packages/nglview/nglview-0.6.2.3.tar.gz/nglview-0.6.2.3/nglview/color.py
@@ -0,0 +1,17 @@
+COLOR_SCHEMES = [
+    " ",
+    "picking",
+    "random",
+    "uniform",
+    "atomindex",
+    "residueindex",
+    "chainindex",
+    "modelindex",
+    "sstruc",
+    "element",
+    "resname",
+    "bfactor",
+    "hydrophobicity",
+    "value",
+    "volume",
+    "occupancy"]
