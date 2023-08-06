3c708a6a3dcd0698ba27bbe91ee0deca908e14d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:51 +0000

    Add package eea.facetednavigation/10.1/eea.facetednavigation-10.1.zip

---
+++ packages/eea.facetednavigation/eea.facetednavigation-10.1.zip/eea.facetednavigation-10.1/setup.py
@@ -0,0 +1,55 @@
+""" EEA Faceted Navigation Installer
+"""
+import os
+from os.path import join
+from setuptools import setup, find_packages
+
+NAME = 'eea.facetednavigation'
+PATH = NAME.split('.') + ['version.txt']
+VERSION = open(join(*PATH)).read().strip()
+
+setup(name=NAME,
+      version=VERSION,
+      description="EEA Faceted Navigation",
+      long_description=(
+          open("README.rst").read() + "\n" +
+          open(os.path.join("docs", "HISTORY.txt")).read()
+      ),
+      classifiers=[
+          "Environment :: Web Environment",
+          "Framework :: Zope2",
+          "Framework :: Zope3",
+          "Framework :: Plone",
+          "Framework :: Plone :: 4.3",
+          "Framework :: Plone :: 5.0",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.7",
+          "Operating System :: OS Independent",
+          "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+      ],
+      keywords='EEA Faceted Navigation FacetedNavigation Add-ons Plone Zope',
+      author='European Environment Agency: IDM2 A-Team',
+      author_email="eea-edw-a-team-alerts@googlegroups.com",
+      url='http://github.com/collective/eea.facetednavigation',
+      license='GPL version 2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['eea'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'eea.faceted.vocabularies > 5.9',
+          'collective.js.jqueryui',
+          'eea.jquery > 9.1',
+      ],
+      extras_require={
+          'test': [
+              'plone.app.testing',
+          ],
+      },
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """
+      )
