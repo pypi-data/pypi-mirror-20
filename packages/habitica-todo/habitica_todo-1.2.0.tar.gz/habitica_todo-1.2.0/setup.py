c06ec95381892f39e4dbeee81ec4a5d790b7b79d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:41 +0000

    Add package habitica-todo/1.2.0/habitica_todo-1.2.0.tar.gz

---
+++ packages/habitica-todo/habitica_todo-1.2.0.tar.gz/habitica_todo-1.2.0/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='habitica_todo',
+    version='1.2.0',
+    url='https://github.com/eringiglio/Habitica-todo',
+    description='An API app for syncing todoist and habitica tasks',
+    packages=['habiticaTodo'],
+    zip_safe=False)
