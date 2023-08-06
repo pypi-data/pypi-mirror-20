b1d9a4174cdbc6c84c6b9e17ef305dfa44da7141
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:25 +0000

    Add package alias_nester/1.2.0/alias_nester-1.2.0.zip

---
+++ packages/alias_nester/alias_nester-1.2.0.zip/alias_nester-1.2.0/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#_*_ coding:utf-8 _*_
+
+from distutils.core import  setup
+
+setup(
+      name='alias_nester',
+      version='1.2.0',
+      py_modules=['alias_nester'],
+      author='aliasxu',
+      description='A simple printer of nested lists by test')
