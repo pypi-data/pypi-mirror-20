dd6d9a822777e10e15cb4c4ef43d0cee8aad31de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:16 +0000

    Add package TracWikinfo/0.2/TracWikinfo-0.2.tar.gz

---
+++ packages/TracWikinfo/TracWikinfo-0.2.tar.gz/TracWikinfo-0.2/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2012 Ryan J Ollos
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+
+from setuptools import setup
+
+setup(
+    name='TracWikinfo',
+    version='0.2',
+    packages=['wikinfo'],
+    package_data={},
+    author="Ryan J Ollos",
+    author_email="ryan.j.ollos@gmail.com",
+    description="Provides a few useful macros to get data about wiki pages",
+    license="3-Clause BSD",
+    keywords="trac wiki info macro",
+    url="https://trac-hacks.org/WikinfoPlugin",
+    entry_points={
+        'trac.plugins': [
+            'wikinfo.macro = wikinfo.macro'
+        ]
+    }
+)
