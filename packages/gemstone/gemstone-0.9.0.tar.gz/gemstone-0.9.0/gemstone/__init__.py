08f66296f3720c1421332efc9b42a7cda6995ec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:24 +0000

    Add package gemstone/0.9.0/gemstone-0.9.0.tar.gz

---
+++ packages/gemstone/gemstone-0.9.0.tar.gz/gemstone-0.9.0/gemstone/__init__.py
@@ -0,0 +1,38 @@
+"""
+Build microservices with Python
+"""
+
+from gemstone.core.microservice import MicroService
+from gemstone.core.decorators import private_api_method, public_method, event_handler, \
+    requires_handler_reference, async_method, exposed_method
+from gemstone.core.handlers import TornadoJsonRpcHandler, GemstoneCustomHandler
+from gemstone.client.remote_service import RemoteService
+
+from gemstone.util import as_completed, first_completed, make_callbacks
+
+__author__ = "Vlad Calin"
+__email__ = "vlad.s.calin@gmail.com"
+
+__version__ = "0.9.0"
+
+__all__ = [
+    # core classes
+    'MicroService',
+    'RemoteService',
+
+    # decorators
+    'public_method',
+    'private_api_method',
+    'event_handler',
+    'requires_handler_reference',
+    'exposed_method',
+
+    # tornado handler
+    'TornadoJsonRpcHandler',
+    'GemstoneCustomHandler',
+
+    # async utilities
+    'as_completed',
+    'first_completed',
+    'make_callbacks'
+]
