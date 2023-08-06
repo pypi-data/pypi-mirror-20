856228ad0a4502446d039eb6c78c33c3480b6b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:49 +0000

    Add package django-perf-rec/2.0.1/django-perf-rec-2.0.1.tar.gz

---
+++ packages/django-perf-rec/django-perf-rec-2.0.1.tar.gz/django-perf-rec-2.0.1/django_perf_rec/utils.py
@@ -0,0 +1,77 @@
+# -*- coding:utf-8 -*-
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import difflib
+import inspect
+from collections import namedtuple
+
+
+TestDetails = namedtuple('TestDetails', ['file_path', 'class_name', 'test_name'])
+
+
+def current_test():
+    """
+    Use a little harmless stack inspection to determine the test that is currently running.
+    """
+    frame = inspect.currentframe()
+    try:
+        while True:
+            if frame.f_code.co_name.startswith('test_'):
+                break
+
+            # Next frame
+            frame = frame.f_back
+            if frame is None:
+                break
+
+        if frame is None:
+            raise RuntimeError("Could not automatically determine the test name.")
+
+        file_path = frame.f_globals['__file__']
+
+        # May be a pytest function test so we can't assume 'self' exists
+        its_self = frame.f_locals.get('self', None)
+        if its_self is None:
+            class_name = None
+        else:
+            class_name = its_self.__class__.__name__
+
+        test_name = frame.f_code.co_name
+
+        return TestDetails(
+            file_path=file_path,
+            class_name=class_name,
+            test_name=test_name,
+        )
+    finally:
+        # Always delete frame references to help garbage collector
+        del frame
+
+
+def sorted_names(names):
+    """
+    Sort a list of names but keep the word 'default' first if it's there.
+    """
+    names = list(names)
+
+    have_default = False
+    if 'default' in names:
+        names.remove('default')
+        have_default = True
+
+    sorted_names = sorted(names)
+
+    if have_default:
+        sorted_names = ['default'] + sorted_names
+
+    return sorted_names
+
+
+def record_diff(old, new):
+    """
+    Generate a human-readable diff of two performance records.
+    """
+    return '\n'.join(difflib.ndiff(
+        ['%s: %s' % (k, v) for op in old for k, v in op.items()],
+        ['%s: %s' % (k, v) for op in new for k, v in op.items()]
+    ))
---
+++ packages/django-perf-rec/django-perf-rec-2.0.1.tar.gz/django-perf-rec-2.0.1/setup.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import os
+import re
+import sys
+
+from setuptools import find_packages, setup
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+version = get_version('django_perf_rec')
+
+
+if sys.argv[-1] == 'publish':
+    if os.system("pip freeze | grep twine"):
+        print("twine not installed.\nUse `pip install twine`.\nExiting.")
+        sys.exit()
+    os.system("python setup.py sdist bdist_wheel")
+    os.system("twine upload dist/*")
+    print("You probably want to also tag the version now:")
+    print("  git tag -a %s -m 'version %s'" % (version, version))
+    print("  git push --tags")
+    sys.exit()
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read().replace('.. :changelog:', '')
+
+setup(
+    name='django-perf-rec',
+    version=version,
+    description="Keep detailed records of the performance of your Django "
+                "code.",
+    long_description=readme + '\n\n' + history,
+    author='YPlan',
+    author_email='adam@yplanapp.com',
+    url='https://github.com/YPlan/django-perf-rec',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    include_package_data=True,
+    install_requires=[
+        'Django',
+        'patchy',
+        'PyYAML',
+        'six',
+        'sqlparse>=0.2.0',
+    ],
+    license='MIT',
+    zip_safe=False,
+    keywords='Django',
+    entry_points={
+        'pytest11': ['django_perf_rec = django_perf_rec.pytest_plugin'],
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
