2cc6b59e6a81152f5816be3a091c7495ab72e28b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:49 +0000

    Add package just/0.5.54/just-0.5.54-py2.py3-none-any.whl

---
+++ packages/just/just-0.5.54-py2.py3-none-any.whl/just/log.py
@@ -0,0 +1,11 @@
+import time
+import sys
+import just
+
+START = "_".join(time.asctime().split())
+
+LOG_FILE = "logs/{}_{}.jsonl".format(sys.argv[0].rstrip(".py"), START)
+
+
+def log(obj, *tags):
+    just.append({"tags": tags, "object": obj}, LOG_FILE)
