95f8f10afe8dc17fe58caff1022dbadd25488ac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:15 +0000

    Add package batch-rename/1.0.4b/batch-rename-1.0.4b.tar.gz

---
+++ packages/batch-rename/batch-rename-1.0.4b.tar.gz/batch-rename-1.0.4b/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import os
+import shutil
+from distutils.core import setup
+
+if not os.path.exists('scripts'):
+    os.makedirs('scripts')
+
+try:
+    shutil.copyfile('batch-rename.py', 'scripts/batch-rename')
+except IOError:
+    print("Could not copy batch-rename.py file. You can ignore this if you're installing with pip.")
+
+setup(
+    name='batch-rename',
+    version='1.0.4b',
+    packages=['batch_renamer_cli'],
+    scripts=['scripts/batch-rename'],
+    url='https://github.com/darko3/batch-renamer',
+    download_url='https://github.com/darko3/batch-renamer/archive/1.0.4.tar.gz',
+    keywords=['batch-renamer', 'files', 'python'],
+    license='MIT',
+    author='Girish Oemrawsingh',
+    author_email='girish.oemrawsingh@protonmail.com',
+    description='An easy to use python script for quickly renaming multiple files ending with incrementing numbers.'
+)
