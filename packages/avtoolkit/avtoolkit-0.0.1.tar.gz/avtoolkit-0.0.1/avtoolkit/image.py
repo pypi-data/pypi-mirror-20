8d34793eb0d6632dbee7e3ed296b19c85056cbea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:49 +0000

    Add package avtoolkit/0.0.1/avtoolkit-0.0.1.tar.gz

---
+++ packages/avtoolkit/avtoolkit-0.0.1.tar.gz/avtoolkit-0.0.1/avtoolkit/image.py
@@ -0,0 +1,10 @@
+import os
+
+from PIL import Image as PILImage
+
+
+class Image(object):
+    def __init__(self, source):
+        if not os.path.exists(source):
+            raise IOError("File does not exist at %r" % source)
+        self.source = source
