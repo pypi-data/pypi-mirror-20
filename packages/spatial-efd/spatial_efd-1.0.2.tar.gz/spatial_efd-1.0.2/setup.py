4c7779cd3bc80bee72e4cdace02c67395d81d9ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:05 +0000

    Add package spatial-efd/1.0.2/spatial_efd-1.0.2.tar.gz

---
+++ packages/spatial-efd/spatial_efd-1.0.2.tar.gz/spatial_efd-1.0.2/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import string
+from setuptools import setup
+
+
+def readme():
+    '''
+    Turn relative figure paths to urls
+    '''
+    url = ('https://raw.githubusercontent.com/sgrieve/'
+           'spatial_efd/master/_static/figure_')
+    lines = []
+    with open('README.rst') as f:
+        for l in f.readlines():
+            tmp = string.replace(l, '_static/figure_', url)
+            print tmp[:-1]
+            lines.append(tmp)
+
+    return string.join(lines)
+
+
+setup(name='spatial_efd',
+      version='1.0.2',
+      description='Spatial elliptical fourier analysis',
+      url='http://github.com/sgrieve',
+      long_description=readme(),
+      keywords='GIS elliptical fourier analysis shapefile',
+      classifiers=['Development Status :: 5 - Production/Stable',
+                   'License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python :: 2.7',
+                   'Intended Audience :: Science/Research',
+                   'Topic :: Scientific/Engineering :: GIS'],
+      author='Stuart WD Grieve',
+      author_email='s.grieve@ed.ac.uk',
+      license='MIT',
+      packages=['spatial_efd'],
+      install_requires=['matplotlib>=2.0.0', 'numpy>=1.12.0',
+                        'pyshp>=1.2.10'],
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='nose.collector',
+      tests_require=['nose>=1.3.7'])
