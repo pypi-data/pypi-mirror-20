07b604f14ab24f90ac9d97915680c4146a5f7e8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:10 +0000

    Add package lehrex/0.4/lehrex-0.4.tar.gz

---
+++ packages/lehrex/lehrex-0.4.tar.gz/lehrex-0.4/lehrex/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""Python package to support the research during
+the annual Lehrexkursion at Universität Hamburg.
+"""
+from . import csv
+from . import math
+from . import plots
+
+
+__all__ = [
+    'csv',
+    'math',
+    'plots',
+]
+
+__version__ = '0.4'
