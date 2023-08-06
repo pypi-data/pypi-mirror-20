e6ea0ffc4c0c6d2b27346f7a7699cbeeab496c70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:30 +0000

    Add package manga-dl/0.1.2/manga_dl-0.1.2-py2-none-any.whl

---
+++ packages/manga-dl/manga_dl-0.1.2-py2-none-any.whl/manga_dl/tests/entities/test_MangaChapter.py
@@ -0,0 +1,39 @@
+u"""
+LICENSE:
+Copyright 2016 Hermann Krumrey
+
+This file is part of manga_dl.
+
+    manga_dl is a program that allows downloading manga files from various
+    sources
+
+    manga_dl is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    manga_dl is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with manga_dl.  If not, see <http://www.gnu.org/licenses/>.
+LICENSE
+"""
+
+# imports
+from __future__ import absolute_import
+import unittest
+
+
+class UnitTests(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test(self):
+        pass
