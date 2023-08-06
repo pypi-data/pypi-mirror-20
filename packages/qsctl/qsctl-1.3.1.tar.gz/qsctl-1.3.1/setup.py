c629bdedcc9f1c0af9355ad41668dbe9c0024aa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:13 +0000

    Add package qsctl/1.3.1/qsctl-1.3.1.tar.gz

---
+++ packages/qsctl/qsctl-1.3.1.tar.gz/qsctl-1.3.1/setup.py
@@ -0,0 +1,25 @@
+# coding:utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='qsctl',
+    version='1.3.1',
+    description='Advanced command line tool for QingStor.',
+    long_description=open('README.rst', 'rb').read().decode('utf-8'),
+    keywords='yunify qingcloud qingstor qsctl object_storage',
+    author='QingStor Dev Team',
+    author_email='qs_dev_group@yunify.com ',
+    url='https://www.qingstor.com',
+    scripts=['bin/qsctl', 'bin/qsctl.cmd'],
+    packages=find_packages('.'),
+    package_dir={'qsctl': 'qingstor'},
+    namespace_packages=['qingstor'],
+    include_package_data=True,
+    install_requires=[
+        'argparse >= 1.1',
+        'PyYAML >= 3.1',
+        'qingstor-sdk >= 2.1.0',
+        'docutils >= 0.10',
+        'tqdm >= 4.0.0'
+    ])
