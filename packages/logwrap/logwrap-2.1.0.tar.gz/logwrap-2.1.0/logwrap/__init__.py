13d7c4cef2741b977fbc2b05b603bf2815ed9382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:09 +0000

    Add package logwrap/2.1.0/logwrap-2.1.0.tar.gz

---
+++ packages/logwrap/logwrap-2.1.0.tar.gz/logwrap-2.1.0/logwrap/__init__.py
@@ -0,0 +1,48 @@
+#    Copyright 2016 Alexey Stepanov aka penguinolog
+
+#    Copyright 2016 Mirantis, Inc.
+
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""logwrap module.
+
+Contents: 'logwrap', 'pretty_repr', 'pretty_str'
+
+Original code was made for Mirantis Inc by Alexey Stepanov,
+later it has been reworked and extended for support of special cases.
+"""
+
+from __future__ import absolute_import
+
+import sys
+
+from ._log_wrap import logwrap
+from ._repr_utils import PrettyFormat
+from ._repr_utils import pretty_repr
+from ._repr_utils import pretty_str
+
+__version__ = '2.1.0'
+
+__all__ = (
+    'logwrap',
+    'PrettyFormat',
+    'pretty_repr',
+    'pretty_str'
+)
+
+# pylint: disable=ungrouped-imports, no-name-in-module
+if sys.version_info[0:2] >= (3, 5):
+    from ._alogwrap import async_logwrap
+
+    __all__ += ('async_logwrap', )
+# pylint: enable=ungrouped-imports, no-name-in-module
