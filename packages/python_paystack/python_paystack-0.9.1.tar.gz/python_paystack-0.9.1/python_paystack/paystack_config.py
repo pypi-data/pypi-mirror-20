ac23b46dcb2c32c9d19567b5c3d9b64b46105b8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:31 +0000

    Add package python_paystack/0.9.1/python_paystack-0.9.1.tar.gz

---
+++ packages/python_paystack/python_paystack-0.9.1.tar.gz/python_paystack-0.9.1/python_paystack/paystack_config.py
@@ -0,0 +1,17 @@
+#Paystack settings
+
+class PaystackConfig():
+    PAYSTACK_URL = "https://api.paystack.co"
+
+    SECRET_KEY = ""
+
+    PUBLIC_KEY = ""
+
+    PASS_ON_TRANSACTION_COST = True
+
+    LOCAL_COST = 0.015
+    INTL_COST = 0.039
+
+    def __new__(cls):
+        raise TypeError("Can not make instance of class")
+    
