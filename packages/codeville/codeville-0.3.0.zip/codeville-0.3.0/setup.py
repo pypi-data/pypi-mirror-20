bd02beeff4dd22f0699ba44f4f16e360c306a969
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:16 +0000

    Add package codeville/0.3.0/codeville-0.3.0.zip

---
+++ packages/codeville/codeville-0.3.0.zip/codeville-0.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup (
+    name = 'codeville',
+    version = '0.3.0',
+    description = 'CodeVille Python Package',
+    author = 'Helen',
+    py_modules = ['codeville'],
+    install_requires = [],
+)
