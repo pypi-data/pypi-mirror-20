473f877f26ce82e7b07aaefb39de5fad5efbe7ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:33 +0000

    Add package TTMPy/0.1/TTMPy-0.1.tar.gz

---
+++ packages/TTMPy/TTMPy-0.1.tar.gz/TTMPy-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='TTMPy',
+      version='0.1',
+      description='toolbelt',
+      url='https://github.com/TurkishTinMan/TTMPy',
+      author='TurkishTinMan',
+      author_email='riccardo.salladini@gmail.com',
+      license='CC0',
+      packages=['TTMPy'],
+      zip_safe=False)
