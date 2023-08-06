7d042ed0d823f4b3b0691e9793c0f342c7023abf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:39 +0000

    Add package pathvalidate/0.14.1/pathvalidate-0.14.1-py2.py3-none-any.whl

---
+++ packages/pathvalidate/pathvalidate-0.14.1-py2.py3-none-any.whl/pathvalidate/__init__.py
@@ -0,0 +1,49 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <gogogo.vm@gmail.com>
+"""
+
+from __future__ import absolute_import
+
+from ._error import (
+    NullNameError,
+    InvalidNameError,
+    InvalidCharError,
+    InvalidCharWindowsError,
+    InvalidLengthError,
+    ReservedNameError,
+    ValidReservedNameError,
+    InvalidReservedNameError
+)
+
+from ._app import (
+    validate_excel_sheet_name,
+    sanitize_excel_sheet_name
+)
+from ._file import (
+    validate_filename,
+    validate_file_path,
+    sanitize_filename,
+    sanitize_file_path
+)
+from ._ltsv import (
+    validate_ltsv_label,
+    sanitize_ltsv_label
+)
+from ._symbol import (
+    validate_symbol,
+    replace_symbol
+)
+from ._sqlite import (
+    validate_sqlite_table_name,
+    validate_sqlite_attr_name
+)
+from ._python_var_name import (
+    validate_python_var_name,
+    sanitize_python_var_name
+)
+from ._js_var_name import (
+    validate_js_var_name,
+    sanitize_js_var_name
+)
