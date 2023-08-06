b8ba32e738f9a42fbb7b6f6280ff6af2a785bf8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:55 +0000

    Add package pytablereader/0.8.10/pytablereader-0.8.10.tar.gz

---
+++ packages/pytablereader/pytablereader-0.8.10.tar.gz/pytablereader-0.8.10/test/test_common.py
@@ -0,0 +1,58 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <gogogo.vm@gmail.com>
+"""
+
+from __future__ import unicode_literals
+
+import pytest
+
+from pytablereader._common import *
+from pytablereader import *
+
+
+class Test_get_extension:
+
+    @pytest.mark.parametrize(["value", "expected"], [
+        ["test.txt", "txt"],
+        [".csv", ""],
+        ["html", ""],
+    ])
+    def test_normal(self, value, expected):
+        assert get_extension(value) == expected
+
+    @pytest.mark.parametrize(["value", "expected"], [
+        ["", InvalidFilePathError],
+        [None, InvalidFilePathError],
+    ])
+    def test_null_table_name(self, value, expected):
+        with pytest.raises(expected):
+            get_extension(value)
+
+
+class Test_make_temp_file_path_from_url:
+
+    @pytest.mark.parametrize(["temp_dir_path", "value", "expected"], [
+        [
+            "/tmp",
+            "https://raw.githubusercontent.com/valid/test/data/validext.csv",
+            "/tmp/validext.csv"
+        ],
+        [
+            "/tmp",
+            "https://raw.githubusercontent.com/valid/test/data/validext/",
+            "/tmp/validext"
+        ],
+    ])
+    def test_normal(self, temp_dir_path, value, expected):
+        assert make_temp_file_path_from_url(temp_dir_path, value) == expected
+
+    @pytest.mark.parametrize(["temp_dir_path", "value", "expected"], [
+        [None, "tmp", InvalidFilePathError],
+        ["tmp", "", InvalidFilePathError],
+        ["tmp", None, InvalidFilePathError],
+    ])
+    def test_null_table_name(self, temp_dir_path, value, expected):
+        with pytest.raises(expected):
+            make_temp_file_path_from_url(temp_dir_path, value)
