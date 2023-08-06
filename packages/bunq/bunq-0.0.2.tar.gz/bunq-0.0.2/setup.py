3b82ce56ea6b72b0de7fe0c0244cb2a257ff77d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:12 +0000

    Add package bunq/0.0.2/bunq-0.0.2.tar.gz

---
+++ packages/bunq/bunq-0.0.2.tar.gz/bunq-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name='bunq',
+    version='0.0.2',
+    description='Simple wrapper around the bunq API',
+    long_description=readme,
+    author='Edwin Hermans',
+    author_email='edwin@madtech.cx',
+    url='https://github.com/madeddie/python-bunq',
+    license='MIT',
+    install_requires=['requests', 'cryptography'],
+    py_modules=['bunq']
+)
