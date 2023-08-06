324d675be008f83167fe60060a6d09877326f91f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:24 +0000

    Add package funniesting/0.2/funniesting-0.2.tar.gz

---
+++ packages/funniesting/funniesting-0.2.tar.gz/funniesting-0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+setup(name='funniesting',
+      version='0.2',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funniesting'],
+      install_requires = [
+          'markdown',
+      ],
+      zip_safe=False)
+
