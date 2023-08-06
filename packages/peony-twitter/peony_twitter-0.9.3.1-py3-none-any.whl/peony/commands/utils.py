dcc798827e8063139a0a5ca0f2ccd989d35ae402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:45 +0000

    Add package peony-twitter/0.9.3.1/peony_twitter-0.9.3.1-py3-none-any.whl

---
+++ packages/peony-twitter/peony_twitter-0.9.3.1-py3-none-any.whl/peony/commands/utils.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+import asyncio
+
+
+def doc(value):
+    stripped_chars = " \t"
+
+    if hasattr(value, '__doc__'):
+        docstring = value.__doc__.lstrip(" \n\t")
+        if "\n" in docstring:
+            i = docstring.index("\n")
+            return docstring[:i].rstrip(stripped_chars)
+        elif docstring:
+            return docstring.rstrip(stripped_chars)
+
+    return ""
+
+
+async def execute(coro):
+    if asyncio.iscoroutine(coro):
+        return await coro
+    else:
+        return coro
+
+
+def permission_check(data, _permissions, command=None, permissions=None):
+    if permissions:
+        pass
+    elif command:
+        if hasattr(command, 'permissions'):
+            permissions = command.permissions
+        else:
+            return True  # true if no permission is required
+    else:
+        msg = "{name} must be called with command or permissions argument"
+        raise RuntimeError(msg.format(name="_permission_check"))
+
+    return any(data.sender.id in _permissions[permission]
+               for permission in permissions
+               if permission in _permissions)
+
+
+def restart_on(exc):
+    def decorator(func):
+        while True:
+            try:
+                return func
+            except exc:
+                pass
+            except:
+                raise
+
+    return decorator
