33cd79422a3e011c699400cf2087c2bd4789ca58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:54 +0000

    Add package fxapom/1.10.0/fxapom-1.10.0.tar.gz

---
+++ packages/fxapom/fxapom-1.10.0.tar.gz/fxapom-1.10.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(name='fxapom',
+      use_scm_version=True,
+      description='Mozilla Firefox Accounts Page Object Model',
+      long_description=open('README.rst').read(),
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+          'Operating System :: POSIX',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: MacOS :: MacOS X',
+          'Topic :: Software Development :: Testing',
+          'Topic :: Utilities',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7'],
+      keywords='mozilla',
+      author='Mozilla Web QA',
+      author_email='mozwebqa@mozilla.org',
+      url='https://github.com/mozilla/fxapom',
+      license='MPL 2.0',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      setup_requires=['setuptools_scm'],
+      install_requires=['PyFxA==0.3.0', 'PyPOM'],
+      include_package_data=True)
