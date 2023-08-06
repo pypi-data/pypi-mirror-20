bf8586b287a08af51612427df7891340faa6de4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:13 +0000

    Add package apidev-coop-bar/1.3.3/apidev-coop_bar-1.3.3.tar.gz

---
+++ packages/apidev-coop-bar/apidev-coop_bar-1.3.3.tar.gz/apidev-coop_bar-1.3.3/coop_bar/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+VERSION = (1, 3, 3)
+
+
+def get_version():
+    """version"""
+    return '%s.%s.%s' % (VERSION[0], VERSION[1], VERSION[2])
+
+__version__ = get_version()
+
+default_app_config = 'coop_bar.apps.CoopBarAppConfig'
---
+++ packages/apidev-coop-bar/apidev-coop_bar-1.3.3.tar.gz/apidev-coop_bar-1.3.3/coop_bar/urls.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from coop_bar.bar import CoopBar
+
+urlpatterns = []
+
+bar = CoopBar()  # This is a Borg
