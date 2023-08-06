614c1fb25298dc2e5fe14c8e6451364d9f5a1fc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:18 +0000

    Add package djangoApiDec/1.7/djangoApiDec-1.7.tar.gz

---
+++ packages/djangoApiDec/djangoApiDec-1.7.tar.gz/djangoApiDec-1.7/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name = 'djangoApiDec',
+    packages = ['djangoApiDec'],
+    version = '1.7',
+    description = 'Convenient python decorator usually used for building api',
+    author = 'davidtnfsh',
+    author_email = 'davidtnfsh@gmail.com',
+    url = 'https://github.com/Stufinite/djangoApiDec',
+    download_url = 'https://github.com/Stufinite/djangoApiDec/archive/v1.7.tar.gz',
+    keywords = ['django', 'decorator', 'api'],
+    classifiers = [],
+    license='MIT',
+    install_requires=[
+        'django',
+        'requests',
+        'simplejson'
+    ],
+    zip_safe=True
+)
