6be64fcd8b4d9e1d6d00fbb44d014586d860f82e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:19 +0000

    Add package at-address-compare/0.2.0/at-address-compare-0.2.0.tar.gz

---
+++ packages/at-address-compare/at-address-compare-0.2.0.tar.gz/at-address-compare-0.2.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+  name = 'at-address-compare',
+  packages = ['ataddrcompare'],
+  version = '0.2.0',
+  entry_points = {
+    'console_scripts': ['ataddrcompare = ataddrcompare.ataddrcompare:main']
+  },
+  install_requires = ['overpass'],
+  description = 'Address comparison of OSM data with official open data for Austria.',
+  author = 'Michael Glanznig',
+  url = 'https://github.com/gmgeo/at-address-compare',
+  download_url = 'https://github.com/gmgeo/at-address-compare/tarball/0.2.0',
+  keywords = ['Austria', 'OSM', 'OpenStreetMap', 'OpenData', 'addresses', 'address', 'compare'],
+  license = 'MIT',
+  classifiers = [],
+  package_data =  {
+    'ataddrcompare': ['template.txt', 'template.html']
+  }
+)
