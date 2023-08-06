5d41cbaf6503f9eba76f2689a6159af2a43255b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:28 +0000

    Add package observe/0.0.3/observe-0.0.3.tar.gz

---
+++ packages/observe/observe-0.0.3.tar.gz/observe-0.0.3/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.rst").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="observe",
+    version="0.0.3",
+    description="A powerfull dataframe describer with pandas.",
+    license="MIT",
+    author="cph",
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+    ],
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+    ]
+)
