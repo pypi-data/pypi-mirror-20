bde67a3fb41e96dcf679b29563b0ede028e80ee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:05 +0000

    Add package TracAdvancedTicketWorkflow/1.2.0/TracAdvancedTicketWorkflow-1.2.0.tar.gz

---
+++ packages/TracAdvancedTicketWorkflow/TracAdvancedTicketWorkflow-1.2.0.tar.gz/TracAdvancedTicketWorkflow-1.2.0/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2008-2014 Eli Carter <elicarter@retracile.net>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+
+from setuptools import setup, find_packages
+
+setup(
+    name='TracAdvancedTicketWorkflow',
+    version='1.2.0',
+    author='Eli Carter',
+    author_email='elicarter@retracile.net',
+    license='3-Clause BSD',
+    description='Advanced workflow operations Trac plugin',
+    long_description='Provides more advanced workflow operations for Trac 1.2',
+    url='https://trac-hacks.org/wiki/AdvancedTicketWorkflowPlugin',
+
+    packages=find_packages(),
+    package_data={},
+    entry_points={'trac.plugins': [
+        'advancedworkflow.controller = advancedworkflow.controller'
+    ]},
+    install_requires=['Trac'],
+    # zip_safe = False,
+)
