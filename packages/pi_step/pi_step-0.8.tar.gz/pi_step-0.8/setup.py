70ae9cc7b923a2ab5c3ccacc4df6ac29da1be16c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:09 +0000

    Add package pi_step/0.8/pi_step-0.8.tar.gz

---
+++ packages/pi_step/pi_step-0.8.tar.gz/pi_step-0.8/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='pi_step',   		        # This is the name of your PyPI-package.
+    version='0.8',                      # Update the version number for new releases
+    author='andiroid',
+    url='http://andiroid.info',
+    author_email='andi-le@hotmail.com',
+    scripts=['rot']                     # The name of your scipt, and also the command you'll be using for calling it
+)
+
