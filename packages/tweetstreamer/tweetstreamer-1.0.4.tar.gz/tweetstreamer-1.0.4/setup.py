38827fe9e5821917559a3d96fb9720cb3f9ae4c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:23 +0000

    Add package tweetstreamer/1.0.4/tweetstreamer-1.0.4.tar.gz

---
+++ packages/tweetstreamer/tweetstreamer-1.0.4.tar.gz/tweetstreamer-1.0.4/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name = 'tweetstreamer',
+    packages = ['tweetstreamer'],
+    version = '1.0.4',
+    description = 'A Tweepy streamer to collect tweets from Twitter\'s Streaming API based on criteria',
+    license = 'MIT',
+    author = 'Sudharshan Ravindran',
+    author_email = 'suddir@gmail.com',
+    url = 'https://github.com/suddi/tweetstreamer',
+    download_url = 'https://github.com/suddi/tweetstreamer/tarball/1.0.3',
+    keywords = ['twitter', 'streamer', 'streaming', 'api', 'tweet', 'tweets', 'tweepy', 'tweetstream', 'tweetstreamer'],
+    classifiers = [],
+)
