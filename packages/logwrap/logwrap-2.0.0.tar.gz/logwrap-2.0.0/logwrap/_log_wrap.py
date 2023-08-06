51fd65f67bb3e5fd24517e8badb66a1195fdfdb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:10 +0000

    Add package logwrap/2.0.0/logwrap-2.0.0.tar.gz

---
+++ packages/logwrap/logwrap-2.0.0.tar.gz/logwrap-2.0.0/logwrap/_log_wrap.py
@@ -0,0 +1,143 @@
+#    Copyright 2016-2017 Alexey Stepanov aka penguinolog
+
+#    Copyright 2016 Mirantis, Inc.
+
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""log_wrap module.
+
+This is no reason to import this submodule directly, all required methods is
+available from the main module.
+"""
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import functools
+import logging
+import sys
+
+import logwrap as core
+
+from . import _log_wrap_shared
+
+# pylint: disable=ungrouped-imports, no-name-in-module
+if sys.version_info[0:2] > (3, 0):
+    from inspect import signature
+else:
+    # noinspection PyUnresolvedReferences
+    from funcsigs import signature
+# pylint: enable=ungrouped-imports, no-name-in-module
+
+
+def logwrap(
+    log=_log_wrap_shared.logger,
+    log_level=logging.DEBUG,
+    exc_level=logging.ERROR,
+    max_indent=20,
+    spec=None,
+    blacklisted_names=None,
+):
+    """Log function calls and return values.
+
+    :param log: logger object for decorator, by default used 'logwrap'
+    :type log: logging.Logger
+    :param log_level: log level for successful calls
+    :type log_level: int
+    :param exc_level: log level for exception cases
+    :type exc_level: int
+    :param max_indent: maximal indent before classic repr() call.
+    :type max_indent: int
+    :param spec: callable object used as spec for arguments bind.
+                 This is designed for the special cases only,
+                 when impossible to change signature of target object,
+                 but processed/redirected signature is accessible.
+                 Note: this object should provide fully compatible signature
+                 with decorated function, or arguments bind will be failed!
+    :type spec: callable
+    :param blacklisted_names: Blacklisted argument names.
+                              Arguments with this names will be skipped in log.
+    :type blacklisted_names: list
+    :return: built real decorator
+    :rtype: callable
+    """
+    if blacklisted_names is None:
+        blacklisted_names = []
+
+    def real_decorator(func):
+        """Log function calls and return values.
+
+        This decorator could be extracted as configured from outer function.
+
+        :param func: function to log calls from
+        :type func: callable
+        :return: wrapped function
+        :rtype: callable
+        """
+        # Get signature _before_ call
+        sig = signature(obj=func if not spec else spec)
+
+        # pylint: disable=missing-docstring
+        @functools.wraps(func)
+        def wrapped(*args, **kwargs):
+            args_repr = _log_wrap_shared.get_func_args_repr(
+                sig=sig,
+                args=args,
+                kwargs=kwargs,
+                max_indent=max_indent,
+                blacklisted_names=blacklisted_names
+            )
+
+            log.log(
+                level=log_level,
+                msg="Calling: \n{name!r}({arguments})".format(
+                    name=func.__name__,
+                    arguments=args_repr
+                )
+            )
+            try:
+                result = func(*args, **kwargs)
+                log.log(
+                    level=log_level,
+                    msg="Done: {name!r} with result:\n{result}".format(
+                        name=func.__name__,
+                        result=core.pretty_repr(
+                            result,
+                            max_indent=max_indent,
+                        )
+                    )
+                )
+            except BaseException:
+                log.log(
+                    level=exc_level,
+                    msg="Failed: \n{name!r}({arguments})".format(
+                        name=func.__name__,
+                        arguments=args_repr,
+                    ),
+                    exc_info=True
+                )
+                raise
+            return result
+
+        # pylint: enable=missing-docstring
+        return wrapped
+
+    if not isinstance(log, logging.Logger):
+        func, log = log, _log_wrap_shared.logger
+
+        return real_decorator(func)
+
+    return real_decorator
+
+
+__all__ = ('logwrap', )
---
+++ packages/logwrap/logwrap-2.0.0.tar.gz/logwrap-2.0.0/test/test_log_wrap.py
@@ -0,0 +1,608 @@
+#    Copyright 2016 - 2017 Alexey Stepanov aka penguinolog
+
+#    Copyright 2016 Mirantis, Inc.
+
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+# pylint: disable=missing-docstring, unused-argument
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import functools
+import logging
+import sys
+import unittest
+
+import logwrap
+
+# pylint: disable=import-error
+if sys.version_info.major == 2:
+    import mock
+else:
+    from unittest import mock
+
+
+@mock.patch('logwrap._log_wrap_shared.logger', autospec=True)
+class TestLogWrap(unittest.TestCase):
+    def test_no_args(self, logger):
+        @logwrap.logwrap
+        def func():
+            return 'No args'
+
+        result = func()
+        self.assertEqual(result, 'No args')
+        logger.assert_has_calls((
+            mock.call.log(
+                level=logging.DEBUG,
+                msg="Calling: \n'func'()"
+            ),
+            mock.call.log(
+                level=logging.DEBUG,
+                msg="Done: 'func' with result:\n{}".format(
+                    logwrap.pretty_repr(result))
+            ),
+        ))
+
+    def test_args_simple(self, logger):
+        arg = 'test arg'
+
+        @logwrap.logwrap
+        def func(tst):
+            return tst
+
+        result = func(arg)
+        self.assertEqual(result, arg)
+        logger.assert_has_calls((
+            mock.call.log(
+                level=logging.DEBUG,
+                msg="Calling: \n"
+                    "'func'(\n"
+                    "    # POSITIONAL_OR_KEYWORD:\n"
+                    "    'tst'={},\n"
+                    ")".format(
+                        logwrap.pretty_repr(
+                            arg,
+                            indent=8,
+                            no_indent_start=True
+                        )
+                    )
+            ),
+            mock.call.log(
+                level=logging.DEBUG,
+                msg="Done: 'func' with result:\n{}".format(
+                    logwrap.pretty_repr(result))
+            ),
+        ))
+
+    def test_args_defaults(self, logger):
+        arg = 'test arg'
+
+        @logwrap.logwrap
+        def func(tst=arg):
+            return tst
+
+        result = func()
+        self.assertEqual(result, arg)
+        logger.assert_has_calls((
+            mock.call.log(
+                level=logging.DEBUG,
+                msg="Calling: \n"
+                    "'func'(\n"
+                    "    # POSITIONAL_OR_KEYWORD:\n"
+                    "    'tst'={},\n"
+                    ")".format(
+                        logwrap.pretty_repr(
+                            arg,
+                            indent=8,
+                            no_indent_start=True)
+                    )
+            ),
+            mock.call.log(
+                level=logging.DEBUG,
+                msg="Done: 'func' with result:\n{}".format(
+                    logwrap.pretty_repr(result))
+            ),
+        ))
+
+    def test_args_complex(self, logger):
+        string = 'string'
+        dictionary = {'key': 'dictionary'}
+
+        @logwrap.logwrap
+        def func(param_string, param_dictionary):
+            return param_string, param_dictionary
+
+        result = func(string, dictionary)
+        self.assertEqual(result, (string, dictionary))
+        # raise ValueError(logger.mock_calls)
+        logger.assert_has_calls((
+            mock.call.log(
+                level=logging.DEBUG,
+                msg="Calling: \n"
+                    "'func'(\n"
+                    "    # POSITIONAL_OR_KEYWORD:\n"
+                    "    'param_string'={string},\n"
+                    "    'param_dictionary'={dictionary},\n"
+                    ")".format(
+                        string=logwrap.pretty_repr(
+                            string,
+                            indent=8, no_indent_start=True),
+                        dictionary=logwrap.pretty_repr(
+                            dictionary,
+                            indent=8, no_indent_start=True)
+                    )
+            ),
+            mock.call.log(
+                level=logging.DEBUG,
+                msg="Done: 'func' with result:\n{}".format(
+                    logwrap.pretty_repr(result))
+            ),
+        ))
+
+    def test_args_kwargs(self, logger):
+        targs = ['string1', 'string2']
+        tkwargs = {'key': 'tkwargs'}
+
+        @logwrap.logwrap
+        def func(*args, **kwargs):
+            return tuple(args), kwargs
+
+        result = func(*targs, **tkwargs)
+        self.assertEqual(result, (tuple(targs), tkwargs))
+        # raise ValueError(logger.mock_calls)
+        logger.assert_has_calls((
+            mock.call.log(
+                level=logging.DEBUG,
+                msg="Calling: \n"
+                    "'func'(\n"
+                    "    # VAR_POSITIONAL:\n"
+                    "    'args'={args},\n"
+                    "    # VAR_KEYWORD:\n"
+                    "    'kwargs'={kwargs},\n)".format(
+                        args=logwrap.pretty_repr(
+                            tuple(targs),
+                            indent=8, no_indent_start=True),
+                        kwargs=logwrap.pretty_repr(
+                            tkwargs,
+                            indent=8, no_indent_start=True)
+                    )
+            ),
+            mock.call.log(
+                level=logging.DEBUG,
+                msg="Done: 'func' with result:\n{}".format(
+                    logwrap.pretty_repr(result))
+            ),
+        ))
+
+    def test_renamed_args_kwargs(self, logger):
+        arg = 'arg'
+        targs = ['string1', 'string2']
+        tkwargs = {'key': 'tkwargs'}
+
+        @logwrap.logwrap
+        def func(arg, *positional, **named):
+            return arg, tuple(positional), named
+
+        result = func(arg, *targs, **tkwargs)
+        self.assertEqual(result, (arg, tuple(targs), tkwargs))
+        # raise ValueError(logger.mock_calls)
+        logger.assert_has_calls((
+            mock.call.log(
+                level=logging.DEBUG,
+                msg="Calling: \n"
+                    "'func'(\n"
+                    "    # POSITIONAL_OR_KEYWORD:\n"
+                    "    'arg'={arg},\n"
+                    "    # VAR_POSITIONAL:\n"
+                    "    'positional'={args},\n"
+                    "    # VAR_KEYWORD:\n"
+                    "    'named'={kwargs},\n)".format(
+                        arg=logwrap.pretty_repr(
+                            arg,
+                            indent=8, no_indent_start=True),
+                        args=logwrap.pretty_repr(
+                            tuple(targs),
+                            indent=8, no_indent_start=True),
+                        kwargs=logwrap.pretty_repr(
+                            tkwargs,
+                            indent=8, no_indent_start=True)
+                    )
+            ),
+            mock.call.log(
+                level=logging.DEBUG,
+                msg="Done: 'func' with result:\n{}".format(
+                    logwrap.pretty_repr(result))
+            ),
+        ))
+
+    def test_negative(self, logger):
+        @logwrap.logwrap
+        def func():
+            raise ValueError('as expected')
+
+        with self.assertRaises(ValueError):
+            func()
+
+        logger.assert_has_calls((
+            mock.call.log(
+                level=logging.DEBUG,
+                msg="Calling: \n'func'()"
+            ),
+            mock.call.log(
+                level=logging.ERROR,
+                msg="Failed: \n'func'()",
+                exc_info=True
+            ),
+        ))
+
+    def test_negative_substitutions(self, logger):
+        new_logger = mock.Mock(spec=logging.Logger, name='logger')
+        log = mock.Mock(name='log')
+        new_logger.attach_mock(log, 'log')
+
+        @logwrap.logwrap(
+            log=new_logger,
+            log_level=logging.INFO,
+            exc_level=logging.WARNING
+        )
+        def func():
+            raise ValueError('as expected')
+
+        with self.assertRaises(ValueError):
+            func()
+
+        self.assertEqual(len(logger.mock_calls), 0)
+        log.assert_has_calls((
+            mock.call(
+                level=logging.INFO,
+                msg="Calling: \n'func'()"
+            ),
+            mock.call(
+                level=logging.WARNING,
+                msg="Failed: \n'func'()",
+                exc_info=True
+            ),
+        ))
+
+    def test_spec(self, logger):
+        new_logger = mock.Mock(spec=logging.Logger, name='logger')
+        log = mock.Mock(name='log')
+        new_logger.attach_mock(log, 'log')
+
+        arg = 'test arg'
+
+        def spec_func(arg=arg):
+            pass
+
+        @logwrap.logwrap(
+            log=new_logger,
+            spec=spec_func,
+        )
+        def func(*args, **kwargs):
+            return args[0] if args else kwargs.get('arg', arg)
+
+        result = func()
+        self.assertEqual(result, arg)
+        log.assert_has_calls((
+            mock.call(
+                level=logging.DEBUG,
+                msg="Calling: \n"
+                    "'func'(\n"
+                    "    # POSITIONAL_OR_KEYWORD:\n"
+                    "    'arg'=u'''test arg''',\n"
+                    ")"),
+            mock.call(
+                level=logging.DEBUG,
+                msg="Done: 'func' with result:\n"
+                    "u'''test arg'''"),
+        ))
+
+    def test_indent(self, logger):
+        new_logger = mock.Mock(spec=logging.Logger, name='logger')
+        log = mock.Mock(name='log')
+        new_logger.attach_mock(log, 'log')
+
+        @logwrap.logwrap(log=new_logger, max_indent=10)
+        def func():
+            return [[[[[[[[[[123]]]]]]]]]]
+
+        func()
+
+        log.assert_has_calls((
+            mock.call(
+                level=logging.DEBUG,
+                msg="Calling: \n"
+                    "'func'()"),
+            mock.call(
+                level=logging.DEBUG,
+                msg="Done: 'func' with result:\n"
+                    "list([\n"
+                    "    list([\n"
+                    "        list([\n"
+                    "            [[[[[[[123]]]]]]],\n"
+                    "        ]),\n"
+                    "    ]),\n"
+                    "])"),
+        ))
+
+    @unittest.skipUnless(
+        sys.version_info[0:2] > (3, 0),
+        'Strict python 3 syntax'
+    )
+    def test_py3_args(self, logger):
+        new_logger = mock.Mock(spec=logging.Logger, name='logger')
+        log = mock.Mock(name='log')
+        new_logger.attach_mock(log, 'log')
+
+        log_call = logwrap.logwrap(log=new_logger)
+
+        namespace = {}
+
+        exec("""
+def tst(arg, darg=1, *args, kwarg, dkwarg=4, **kwargs):
+    pass
+        """,
+             namespace
+             )
+        wrapped = log_call(namespace['tst'])
+        wrapped(0, 1, 2, kwarg=3, somekwarg=5)
+
+        log.assert_has_calls((
+            mock.call(
+                level=logging.DEBUG,
+                msg="Calling: \n"
+                    "'tst'(\n"
+                    "    # POSITIONAL_OR_KEYWORD:\n"
+                    "    'arg'=0,\n"
+                    "    'darg'=1,\n"
+                    "    # VAR_POSITIONAL:\n"
+                    "    'args'=\n"
+                    "        tuple((\n"
+                    "            2,\n"
+                    "        )),\n"
+                    "    # KEYWORD_ONLY:\n"
+                    "    'kwarg'=3,\n"
+                    "    'dkwarg'=4,\n"
+                    "    # VAR_KEYWORD:\n"
+                    "    'kwargs'=\n"
+                    "        dict({\n"
+                    "            'somekwarg': 5,\n"
+                    "        }),\n"
+                    ")"),
+            mock.call(
+                level=logging.DEBUG,
+                msg="Done: 'tst' with result:\n"
+                    "None"),
+        ))
+
+    @unittest.skipUnless(
+        sys.version_info[0:2] > (3, 0),
+        'Wrap expanding is not supported under python 2.7: funcsigs limitation'
+    )
+    def test_wrapped(self, logger):
+        def simpledeco(func):
+            @functools.wraps(func)
+            def wrapped(*args, **kwargs):
+                return func(*args, **kwargs)
+            return wrapped
+
+        @logwrap.logwrap
+        @simpledeco
+        def func(arg, darg=1, *args, **kwargs):
+            return arg, darg, args, kwargs
+
+        result = func(0, 1, 2, arg3=3)
+        self.assertEqual(
+            result,
+            (0, 1, (2, ), {'arg3': 3})
+        )
+        logger.assert_has_calls((
+            mock.call.log(
+                level=10,
+                msg="Calling: \n"
+                    "'func'(\n"
+                    "    # POSITIONAL_OR_KEYWORD:\n"
+                    "    'arg'=0,\n"
+                    "    'darg'=1,\n"
+                    "    # VAR_POSITIONAL:\n"
+                    "    'args'=\n"
+                    "        tuple((\n"
+                    "            2,\n"
+                    "        )),\n"
+                    "    # VAR_KEYWORD:\n"
+                    "    'kwargs'=\n"
+                    "        dict({\n"
+                    "            'arg3': 3,\n"
+                    "        }),\n"
+                    ")"),
+            mock.call.log(
+                level=10,
+                msg="Done: 'func' with result:\n"
+                    "tuple((\n"
+                    "    0,\n"
+                    "    1,\n"
+                    "    tuple((\n"
+                    "        2,\n"
+                    "    )),\n"
+                    "    dict({\n"
+                    "        'arg3': 3,\n"
+                    "    }),\n"
+                    "))")
+        ))
+
+    def test_args_blacklist(self, logger):
+        new_logger = mock.Mock(spec=logging.Logger, name='logger')
+        log = mock.Mock(name='log')
+        new_logger.attach_mock(log, 'log')
+
+        arg1 = 'test arg 1'
+        arg2 = 'test arg 2'
+
+        @logwrap.logwrap(log=new_logger, blacklisted_names=['test_arg2'])
+        def func(test_arg1, test_arg2):
+            return test_arg1, test_arg2
+
+        result = func(arg1, arg2)
+        self.assertEqual(result, (arg1, arg2))
+        log.assert_has_calls((
+            mock.call(
+                level=logging.DEBUG,
+                msg="Calling: \n"
+                    "'func'(\n"
+                    "    # POSITIONAL_OR_KEYWORD:\n"
+                    "    'test_arg1'={},\n"
+                    ")".format(
+                        logwrap.pretty_repr(
+                            arg1,
+                            indent=8,
+                            no_indent_start=True
+                        )
+                    )
+            ),
+            mock.call(
+                level=logging.DEBUG,
+                msg="Done: 'func' with result:\n{}".format(
+                    logwrap.pretty_repr(result))
+            ),
+        ))
+
+
+@mock.patch('logwrap._log_wrap_shared.logger', autospec=True)
+@unittest.skipUnless(
+    sys.version_info[0:2] > (3, 4),
+    'Strict python 3.5+ API'
+)
+class TestLogWrapAsync(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        namespace = {}
+
+        exec("""
+import asyncio
+
+loop = asyncio.get_event_loop()
+        """,
+             namespace
+             )
+        cls.loop = namespace['loop']
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.loop.close()
+
+    def test_coroutine_sync(self, logger):
+            namespace = {'logwrap': logwrap, 'loop': self.loop}
+
+            exec("""
+@logwrap.logwrap
+async def func():
+    pass
+
+loop.run_until_complete(func())
+            """,
+                 namespace
+                 )
+            # While we're not expanding result coroutine object from namespace,
+            # do not check execution result
+            logger.assert_has_calls((
+                mock.call.log(
+                    level=logging.DEBUG,
+                    msg="Calling: \n'func'()"
+                ),
+            ))
+
+    def test_coroutine_async(self, logger):
+        namespace = {'logwrap': logwrap, 'loop': self.loop}
+
+        exec("""
+@logwrap.async_logwrap
+async def func():
+    pass
+
+loop.run_until_complete(func())
+        """,
+             namespace
+             )
+        # While we're not expanding result coroutine object from namespace,
+        # do not check execution result
+        logger.assert_has_calls((
+            mock.call.log(
+                level=logging.DEBUG,
+                msg="Calling: \n'func'()"
+            ),
+            mock.call.log(
+                level=logging.DEBUG,
+                msg="Done: 'func' with result:\nNone"
+            )
+        ))
+
+    def test_coroutine_async_as_argumented(self, logger):
+        new_logger = mock.Mock(spec=logging.Logger, name='logger')
+        log = mock.Mock(name='log')
+        new_logger.attach_mock(log, 'log')
+
+        namespace = {
+            'logwrap': logwrap,
+            'loop': self.loop,
+            'new_logger': new_logger
+        }
+
+        exec("""
+@logwrap.async_logwrap(log=new_logger)
+async def func():
+    pass
+
+loop.run_until_complete(func())
+        """,
+             namespace
+             )
+        # While we're not expanding result coroutine object from namespace,
+        # do not check execution result
+        log.assert_has_calls((
+            mock.call.log(
+                level=logging.DEBUG,
+                msg="Calling: \n'func'()"
+            ),
+            mock.call.log(
+                level=logging.DEBUG,
+                msg="Done: 'func' with result:\nNone"
+            )
+        ))
+
+    def test_coroutine_fail(self, logger):
+        namespace = {'logwrap': logwrap, 'self': self}
+
+        exec("""
+@logwrap.async_logwrap
+async def func():
+    raise Exception('Expected')
+
+with self.assertRaises(Exception):
+    self.loop.run_until_complete(func())
+        """,
+             namespace
+             )
+        # While we're not expanding result coroutine object from namespace,
+        # do not check execution result
+        logger.assert_has_calls((
+            mock.call.log(
+                level=logging.DEBUG,
+                msg="Calling: \n'func'()"
+            ),
+            mock.call.log(
+                level=logging.ERROR,
+                msg="Failed: \n'func'()",
+                exc_info=True
+            )
+        ))
