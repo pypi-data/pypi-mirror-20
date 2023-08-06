1f90d06cfcba62c50747902808557b21963de258
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:04 +0000

    Add package quilt/2.2.0/quilt-2.2.0.tar.gz

---
+++ packages/quilt/quilt-2.2.0.tar.gz/quilt-2.2.0/quilt/tools/const.py
@@ -0,0 +1,80 @@
+"""
+Constants
+"""
+from enum import Enum
+
+class TargetType(Enum):
+    PANDAS = 'pandas'
+    FILE = 'file'
+
+class PackageFormat(Enum):    
+    HDF5 = 'HDF5'
+    FASTPARQUET = 'FAST_PARQUET'
+    ARROW = 'ARROW_PARQUET'
+    SPARK = 'SPARK_PARQUET'
+    default = HDF5
+
+DATEF = '%F'
+TIMEF = '%T'
+DTIMEF = '%s %s' % (DATEF, TIMEF)
+LATEST_TAG = 'latest'
+PACKAGE_DIR_NAME = 'quilt_packages'
+
+# SHA-2 Family
+HASH_TYPE = 'sha256'
+# RSA digital signature key Size
+RSA_BITS = 2048
+    
+# TODO nan probably not a safe choice and may pollute number cols with strs
+NA_VALS = ['nan']
+KEEP_NA = False
+# Supported build targets and file types
+# BUILD[target][file_extension]
+# file_extension should be lowercase
+TARGET = {
+    'pandas': {
+        'csv': {
+            'attr': 'read_csv',
+            'failover' : {'engine' : 'python'},
+            'kwargs': {
+                'keep_default_na': KEEP_NA,
+                'na_values': NA_VALS,
+            }
+        },
+        'ssv': {
+            'attr': 'read_csv',
+            'failover' : {'engine' : 'python'},
+            'kwargs': {
+                'keep_default_na': KEEP_NA,
+                'na_values': NA_VALS,
+                'sep': ';'
+            }
+        },
+        'tsv': {
+            'attr': 'read_csv',
+            'failover' : {'engine' : 'python'},
+            'kwargs': {
+                'keep_default_na': KEEP_NA,
+                'na_values': NA_VALS,
+                'sep': '\t'
+            }
+        },
+        'xls': {
+            'attr': 'read_excel',
+            # TODO set sheetname='None' to get all sheets?
+            # Currently defaults to sheetname=0, which imports first sheet only
+            'kwargs': {
+                'keep_default_na': KEEP_NA,
+                'na_values': NA_VALS
+            }
+        },
+        'xlsx': {
+            'attr': 'read_excel',
+            # see comments under 'xls'
+            'kwargs': {
+                'keep_default_na': KEEP_NA,
+                'na_values': NA_VALS
+            }
+        }
+    }
+}
---
+++ packages/quilt/quilt-2.2.0.tar.gz/quilt-2.2.0/setup.py
@@ -0,0 +1,53 @@
+from setuptools import setup, find_packages
+
+def readme():
+    readme_short = """
+    [Quilt](https://beta.quiltdata.com/) is a data package manager.
+    You can use data packages from the community, or publish packages for others to use.
+
+    `quilt` is the command-line client that builds, retrieves, and stores
+    packages. `quilt` works in conjunction with a server-side registry,
+    not covered in this document. `quilt` currently pushes to and pulls from
+    the registry at [beta.quiltdata.com](https://beta.quiltdata.com/). In the near
+    future users will be able to browse packages in the registry.
+    """
+    return readme_short
+
+setup(
+    name="quilt",
+    version="2.2.0",
+    packages=find_packages(),
+    description='Quilt is an open-source data frame registry',
+    long_description=readme(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+    ],
+    author='quiltdata',
+    author_email='founders@quiltdata.io',
+    license='LICENSE',
+    url='https://github.com/quiltdata/quilt',
+    download_url='https://github.com/quiltdata/quilt/releases/tag/v2.2.0-beta',
+    keywords='quilt quiltdata shareable data dataframe package platform pandas',
+    install_requires=[
+        'appdirs>=1.4.0',
+        'future>=0.16.0',
+        'packaging>=16.8',
+        'pandas>=0.19.2',
+        'pyOpenSSL>=16.2.0',
+        'pyyaml>=3.12',
+        'requests>=2.12.4',
+        'responses>=0.5.1',
+        'tables>=3.3.0',
+        'tqdm>=4.11.2',
+        'xlrd>=1.0.0',
+    ],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': ['quilt=quilt.tools.command:main'],
+    }
+)
