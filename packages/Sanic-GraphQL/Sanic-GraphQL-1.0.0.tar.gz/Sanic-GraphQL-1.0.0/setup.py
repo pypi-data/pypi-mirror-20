dcff458027a7a7cde5a2c67652dfc7d8fbc2e948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:34 +0000

    Add package Sanic-GraphQL/1.0.0/Sanic-GraphQL-1.0.0.tar.gz

---
+++ packages/Sanic-GraphQL/Sanic-GraphQL-1.0.0.tar.gz/Sanic-GraphQL-1.0.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+required_packages = ['graphql-core>=1.0', 'sanic>=0.4.0', 'pytest-runner']
+
+setup(
+    name='Sanic-GraphQL',
+    version='1.0.0',
+    description='Adds GraphQL support to your Sanic application',
+    long_description=open('README.rst').read(),
+    url='https://github.com/grazor/sanic-graphql',
+    download_url='https://github.com/grazor/sanic-graphql/releases',
+    author='Sergey Porivaev',
+    author_email='porivaevs@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+    ],
+    keywords='api graphql protocol sanic',
+    packages=find_packages(exclude=['tests']),
+    install_requires=required_packages,
+    tests_require=['pytest>=2.7.3', 'aiohttp>=1.3.0', 'yarl>=0.9.6', 'jinja2>=2.9.0'],
+    include_package_data=True,
+    platforms='any',
+)
