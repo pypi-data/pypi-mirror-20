cf62948179baf18937b79d7ed3bac77a59827e70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:10 +0000

    Add package nester_shcf/1.0.0/nester_shcf-1.0.0.tar.gz

---
+++ packages/nester_shcf/nester_shcf-1.0.0.tar.gz/nester_shcf-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name            = 'nester_shcf',
+        version         = '1.0.0',
+        py_modules      = ['nester_shcf'],
+        author          = 'shaocf',
+        author_email    = "shaochangfu001@163.com",
+        url             = "http://www.headfirstlabs.com",
+        description     = 'A simple printer of nested lists',
+)
