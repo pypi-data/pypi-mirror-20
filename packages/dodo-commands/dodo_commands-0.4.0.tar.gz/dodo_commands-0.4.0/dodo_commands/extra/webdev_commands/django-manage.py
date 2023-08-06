f0052101b79fab56a9c0e7deeb889c26eb87dbd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:07 +0000

    Add package dodo-commands/0.4.0/dodo_commands-0.4.0.tar.gz

---
+++ packages/dodo-commands/dodo_commands-0.4.0.tar.gz/dodo_commands-0.4.0/dodo_commands/extra/webdev_commands/django-manage.py
@@ -0,0 +1,27 @@
+"""Run a django-manage command."""
+import argparse
+from dodo_commands.extra.standard_commands import DodoCommand
+
+
+class Command(DodoCommand):  # noqa
+    decorators = ['docker']
+    docker_options = [
+        '--link=pg:pg',
+    ]
+
+    def add_arguments_imp(self, parser):  # noqa
+        parser.add_argument(
+            'manage_args',
+            nargs=argparse.REMAINDER
+        )
+
+    def handle_imp(  # noqa
+        self, *args, **kwargs
+    ):
+        self.runcmd(
+            [
+                self.get_config("/DJANGO/python"),
+                "manage.py",
+            ] + kwargs['manage_args'],
+            cwd=self.get_config("/DJANGO/src_dir")
+        )
---
+++ packages/dodo-commands/dodo_commands-0.4.0.tar.gz/dodo_commands-0.4.0/dodo_commands/extra/webdev_commands/pytest.py
@@ -0,0 +1,25 @@
+# noqa
+import argparse
+from dodo_commands.extra.standard_commands import DodoCommand
+
+
+class Command(DodoCommand):  # noqa
+    help = ""
+    decorators = ["docker"]
+
+    def add_arguments_imp(self, parser):  # noqa
+        parser.add_argument(
+            'pytest_args',
+            nargs=argparse.REMAINDER
+        )
+
+    def handle_imp(self, pytest_args, **kwargs):  # noqa
+        if pytest_args[:1] == ['-']:
+            pytest_args = pytest_args[1:]
+
+        self.runcmd(
+            [
+                self.get_config("/PYTEST/pytest"),
+            ] + pytest_args,
+            cwd=self.get_config("/PYTEST/src_dir")
+        )
