1fafa7d03786c73113fdd2cf49ec46516c064db2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:53 +0000

    Add package component_builder/1.0.1.187/component_builder-1.0.1.187.tar.gz

---
+++ packages/component_builder/component_builder-1.0.1.187.tar.gz/component_builder-1.0.1.187/src/component_builder/__main__.py
@@ -0,0 +1,119 @@
+import json
+import sys
+
+from docopt import docopt
+
+from . import build, discover, envs, github, release
+
+USAGE = """
+Intelligent builder for working with component-based repositories
+
+Usage:
+  compbuild discover [--with-versions] {common}
+  compbuild declare [<component>...] {common}
+  compbuild build [<component>...] {common}
+  compbuild test [<component>...] {common}
+  compbuild label <label> [<component>...] {common}
+  compbuild tag [<component>...] {common}
+  compbuild release [<component>...] {common}
+  compbuild get <attr> [<component>...] {common}
+  compbuild env [<component>...] {common}
+  compbuild <action> [<component>...] {common}
+  compbuild -h | --help
+  compbuild --version
+
+Options:
+  -h --help            Show this screen.
+  --all                Do all the components
+  --filter=SELECTOR    Filter components based on selector
+                       (e.g. --filter=relase-process=docker)
+  --version            Show version.
+  --conf=FILE          Configuration file location [default: builder.ini]
+  --with-versions      Print out all items of interest, with versions
+  --vs-branch=BRANCH   Discover changes made compared to BRANCH. If not set,
+                       comparison will be to latest staging branch for each
+                       component.
+  --exclude-downstream  Only include directly changed things, not things that
+                        declare them as upstreams.
+
+
+""".format(
+    common=('[--vs-branch=BRANCH] [--all] [--exclude-downstream] '
+            '[--filter=SELECTOR ...] [--conf=FILE]')
+)
+
+
+def cli(out=sys.stdout):
+    arguments = docopt(USAGE, version='1.0')
+
+    b = build.Builder(arguments['--conf'])
+    b.configure()
+
+    selectors = set()
+    filters = arguments['--filter']
+    for fltr in filters:
+        selectors.update(fltr.split(','))
+    selectors = list(selectors)
+
+    components = discover.run(
+        b.components,
+        arguments.get('<component>', []),
+        arguments['--all'],
+        compare_branch=arguments['--vs-branch'],
+        selectors=selectors,
+        include_downstream=not arguments['--exclude-downstream']
+    )
+    envs.set_envs(components)
+
+    if arguments['discover']:
+        tmpl = u"{title}"
+        if arguments['--with-versions']:
+            tmpl += u':{version}'
+        for c in components:
+            out.write(
+                tmpl.format(title=c.title, version=c.env['VERSION']) + '\n'
+            )
+    elif arguments['declare']:
+        build.declare_components_usage(components)
+    elif arguments['build']:
+        b.pre('build', components)
+        build.run('build', components)
+        b.post('build', components)
+    elif arguments['test']:
+        b.pre('test', components)
+        build.run('test', components)
+        b.post('test', components)
+    elif arguments['tag']:
+        for comp in components:
+            github.create_tag("{0}-{1}".format(
+                comp.title, comp.env['VERSION'])
+            )
+    elif arguments['label']:
+        for comp in components:
+            github.update_branch(comp.branch_name(arguments['<label>']))
+    elif arguments['release']:
+        release.run(components)
+    elif arguments['env']:
+        out.write("\n".join(json.dumps(c.env, indent=4) for c in components))
+    elif arguments['get']:
+        tmpl = u"{title}:{attr}"
+        for c in components:
+            value = c.ini.get(arguments['<attr>'], "")
+            if isinstance(value, list):
+                value = ','.join(value)
+            out.write(
+                tmpl.format(
+                    title=c.title,
+                    attr=value) + '\n'
+            )
+    elif arguments['<action>']:
+        b.pre(arguments['<action>'], components)
+        for bash_out in build.run(arguments['<action>'], components,
+                                  make_options="-s"):
+            print(bash_out)
+            out.write(u'{0}'.format(bash_out.value()))
+        b.post(arguments['<action>'], components)
+
+
+if __name__ == '__main__':
+    cli()
---
+++ packages/component_builder/component_builder-1.0.1.187.tar.gz/component_builder-1.0.1.187/src/component_builder/component.py
@@ -0,0 +1,93 @@
+from .utils import convert_dict_to_env_string
+
+
+class Component(object):
+
+    all = {}
+
+    def __init__(self, title, path, release_process='', upstream=None,
+                 ini=None):
+        """
+        title: Name of the component. Used for docker images and more...
+        path: Path to the component (within which a makefile will be found)
+        release_process: Type of release process. Supported are 'docker' or
+                         'custom'. In the future, pypi. Leave blank if no
+                         release required.
+        upstream: List of components that this component relies on.
+        """
+        self.title = title
+        self.upstream = upstream or []
+        self.release_process = release_process
+        self.env = {}
+        self.all[title] = self
+        self.path = path
+        self.ini = ini
+
+    def branch_name(self, label='stable'):
+        return "{0}-{1}".format(label, self.title)
+
+    @property
+    def env_string(self):
+        return convert_dict_to_env_string(self.env)
+
+    def get_upstream_builds(self):
+        upstream = []
+        for ds in self.upstream:
+            component = self.all[ds]
+            upstream.append(component)
+            upstream.extend(component.get_upstream_builds())
+        return upstream
+
+    def get_downstream_builds(self):
+        downstream = []
+        for component in self.all.values():
+            if self.title in component.upstream:
+                downstream.append(component)
+                downstream.extend(component.get_downstream_builds())
+        return downstream
+
+    def __repr__(self):
+        return "<Component: {0}>".format(self.title)
+
+
+class Tree(object):
+
+    @classmethod
+    def merge_branches(cls, *branches):
+        if not branches:
+            return branches
+        if len(branches) > 1:
+            left = branches[0]
+            right = branches[1]
+            rest = branches[2:]
+            left_root = left[0]
+
+            if left_root in right:
+                first = right
+                second = left
+            else:
+                first = left
+                second = right
+
+            merged = []
+            for item in first:
+                if item not in second:
+                    merged.append(item)
+            for item in second:
+                if item not in merged:
+                    merged.append(item)
+
+            return cls.merge_branches(merged, *rest)
+        return branches[0]
+
+    @classmethod
+    def ordered(cls, root_candidates, include_downstream=True):
+        branches = []
+
+        for c in root_candidates:
+            branch = [c]
+            if include_downstream:
+                branch += [c for c in c.get_downstream_builds()]
+            branches.append(branch)
+
+        return cls.merge_branches(*branches)
