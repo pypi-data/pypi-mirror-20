74f9b322654516ebbd149b07731e60dc44fca321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:20 +0000

    Add package area-under-curve/0.9.3/area_under_curve-0.9.3.tar.gz

---
+++ packages/area-under-curve/area_under_curve-0.9.3.tar.gz/area_under_curve-0.9.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name='area_under_curve',
+      version='0.9.3',
+      description='Calculate area under curve',
+      long_description=open('README.rst').read(),
+      url='https://github.com/smycynek/area_under_curve',
+      author='Steven Mycynek',
+      author_email='sv@stevenvictor.net',
+      license='MIT',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3'
+      ],
+
+      packages=['area_under_curve'],
+      install_requires=[
+          'numpy',
+      ],
+      keywords='riemann-sum calculus',
+      zip_safe=False)
