ea2fd572d1a27208196d924caf670736b31002d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:44 +0000

    Add package mdt/0.9.26/mdt-0.9.26-py2.py3-none-any.whl

---
+++ packages/mdt/mdt-0.9.26-py2.py3-none-any.whl/mdt/data/components/standard/compartment_models/ExpT2DecSTEAM.py
@@ -0,0 +1,14 @@
+from mdt.components_config.compartment_models import CompartmentConfig
+
+__author__ = 'Francisco J. Fritz'
+__date__ = "2016-09-06"
+__maintainer__ = "Robbert Harms"
+__email__ = "robbert.harms@maastrichtuniversity.nl"
+
+
+class ExpT2DecSTEAM(CompartmentConfig):
+
+    parameter_list = ('SEf', 'TE', 'flip_angle', 'Refoc_fa1', 'Refoc_fa2', 'T2')
+    cl_code = """
+        return pow(0.5, SEf) * sin(flip_angle) * sin(Refoc_fa1) * sin(Refoc_fa2) * exp(-TE / T2);
+    """
