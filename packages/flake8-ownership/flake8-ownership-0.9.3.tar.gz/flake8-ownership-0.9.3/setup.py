8221b4d8347e0042fe102717a329314d786fc8e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:22 +0000

    Add package flake8-ownership/0.9.3/flake8-ownership-0.9.3.tar.gz

---
+++ packages/flake8-ownership/flake8-ownership-0.9.3.tar.gz/flake8-ownership-0.9.3/setup.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Package configuration for flake8-ownership.
+
+:author: Joe Joyce <joe@decafjoe.com>
+:copyright: Copyright (c) Joe Joyce, 2016-2017. All rights reserved.
+:license: BSD
+"""
+import os
+
+from setuptools import setup
+
+
+name = 'flake8-ownership'
+version = '0.9.3'
+requires = ()
+
+root = os.path.abspath(os.path.dirname(__file__))
+changelog = open(os.path.join(root, 'CHANGELOG.rst')).read()
+readme = open(os.path.join(root, 'README.rst')).read()
+long_description = '%s\n\n%s' % (readme, changelog)
+
+
+setup(
+    author='Joe Joyce',
+    author_email='joe@decafjoe.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Flake8',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+    description='Checks for author, copyright, and license info.',
+    entry_points={
+        'flake8.extension': [
+            '%(name)s = %(name)s:Checker' % dict(name=name.replace('-', '_')),
+        ],
+    },
+    install_requires=requires,
+    license='BSD',
+    long_description=long_description,
+    name=name,
+    package_dir={'': 'src'},
+    py_modules=[name.replace('-', '_')],
+    test_suite='test',
+    url='https://bitbucket.org/decafjoe/%s' % name,
+    version=version,
+    zip_safe=False,
+)
