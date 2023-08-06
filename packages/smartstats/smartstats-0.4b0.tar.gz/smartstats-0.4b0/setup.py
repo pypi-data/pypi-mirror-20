08bd8e1ae4159ce188e8d9bd64220a994f83a6ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:26 +0000

    Add package smartstats/0.4b0/smartstats-0.4b0.tar.gz

---
+++ packages/smartstats/smartstats-0.4b0.tar.gz/smartstats-0.4b0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='smartstats',
+      version='0.4b',
+      description='Pretty Statistics',
+      url='http://github.com/andrewbschneider/smartstats',
+      author='Andrew Schneider',
+      author_email='andrewbschneider@gmail.com',
+      license='MIT',
+      packages=['smartstats'],
+      zip_safe=False)
