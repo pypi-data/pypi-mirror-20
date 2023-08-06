e0477f6a3dc075d42ae4c6e4ead41a28c35e53cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:59 +0000

    Add package beamtools/0.2/beamtools-0.2.tar.gz

---
+++ packages/beamtools/beamtools-0.2.tar.gz/beamtools-0.2/beamtools/__init__.py
@@ -0,0 +1,1 @@
+from tests import h,c,energy,freq
---
+++ packages/beamtools/beamtools-0.2.tar.gz/beamtools-0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'beamtools',
+  packages = ['beamtools'], # this must be the same as the name above
+  version = '0.2',
+  description = 'A suite of tools for analysis optical beams',
+  author = 'Kyle Manchee',
+  author_email = 'cpkmanchee@gmail.com',
+  url = 'https://github.com/kikimaroca/beamtools',
+  download_url = 'https://github.com/kikimaroca/beamtools/archive/v0.2.tar.gz',
+  keywords = ['optics', 'analysis', 'beams', 'spectrum'],
+  classifiers = [],
+)
