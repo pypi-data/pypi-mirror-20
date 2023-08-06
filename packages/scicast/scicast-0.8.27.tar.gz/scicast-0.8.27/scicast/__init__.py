401a3b8efa87d7354cb5daac818353b4c9637148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:34 +0000

    Add package scicast/0.8.27/scicast-0.8.27.tar.gz

---
+++ packages/scicast/scicast-0.8.27.tar.gz/scicast-0.8.27/scicast/__init__.py
@@ -0,0 +1,17 @@
+from .cluster import *
+from .scicast_argparse import *
+from .sci_load import *
+from .tkinter_scicast import *
+from .matrix_filter import *
+from .dim_reduction import *
+from .heatmaps import *
+from .correlation import *
+from .significance_testing import *
+from .R_qgraph import run_qgraph
+from .stability_test import *
+import matplotlib
+matplotlib.use('TkAgg')
+
+set()
+__all__ = ["scicast_argparse", "sci_load", "tkinter_scicast", "matrix_filter", "dim_reduction", "heatmaps", "correlation", "significance_testing", "R_qgraph", "stability_test"]
+__version__ = "0.8.27"
