b95189cb0446407758fee4d181a39e9c3a0ae706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:33 +0000

    Add package spamfinder/0.1.1/spamfinder-0.1.1.tar.gz

---
+++ packages/spamfinder/spamfinder-0.1.1.tar.gz/spamfinder-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='spamfinder',
+    version='0.1.1',
+    package_dir={'spam': 'spam'},
+    packages=['spam'],
+    url='https://github.com/orf/spam/',
+    license='',
+    author='orf',
+    author_email='tom@tomforb.es',
+    description='Find the most common senders in your inbox',
+    entry_points={
+        'console_scripts': [
+            'spam = spam.command:main'
+        ]
+    },
+    install_requires=[
+        'tqdm',
+        'ascii-graph',
+        'docopt'
+    ]
+)
