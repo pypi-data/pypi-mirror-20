d442b07f91e96603d29a7cd865b7241439f3c572
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:54 +0000

    Add package deeperdighenrydist/1.1.0/deeperdighenrydist-1.1.0.tar.gz

---
+++ packages/deeperdighenrydist/deeperdighenrydist-1.1.0.tar.gz/deeperdighenrydist-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name = 'deeperdighenrydist',
+	version = '1.1.0',
+	py_modules = ['deeperdighenrymodule'],
+	author = 'henry',
+	author_email = 'henry@gmail.com',
+	description = 'deeper description',
+)
