c92c8364199abed4218aea1c7b1525dd85e13d91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:49 +0000

    Add package PyBarCodeScan/0.1.50/PyBarCodeScan-0.1.50.tar.gz

---
+++ packages/PyBarCodeScan/PyBarCodeScan-0.1.50.tar.gz/PyBarCodeScan-0.1.50/barcodescan/Push_client.py
@@ -0,0 +1,19 @@
+import httplib2
+import json
+import logging
+
+class Push_client(object):
+  logger = logging.getLogger(__name__)
+  def __init__(self, url):
+    self.url = url
+    self.logger.info("Push_client configured to {}".format(self.url))
+
+  def push_data(self, data_to_push):
+    json_to_send = json.dumps({'data': data_to_push})
+    h = httplib2.Http()
+    self.logger.debug("sending {} to server {}".format(data_to_push, self.url))
+    (resp_headers, _) = h.request(self.url, body=json_to_send, method="PUT",
+                                  headers={'Content-Type': 'application/json'})
+
+    if resp_headers.status != 200:
+      logging.warning("server returns {}".format(resp_headers.status))
