4a7fc61a34ee0fe8c7af1aa0de2f5808f440e920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:05 +0000

    Add package gslrandom/0.1.2/gslrandom-0.1.2.tar.gz

---
+++ packages/gslrandom/gslrandom-0.1.2.tar.gz/gslrandom-0.1.2/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+from Cython.Build import cythonize
+import numpy as np
+import os
+
+setup(name='gslrandom',
+      version='0.1.2',
+      description='Cython wrappers for GSL random number generators',
+      author='Scott Linderman, Aaron Schein, and Matthew Johnson',
+      author_email='slinderman@seas.harvard.edu',
+      url='http://www.github.com/slinderman/gslrandom',
+      license="MIT",
+      packages=['gslrandom'],
+      include_dirs=[np.get_include()],
+      ext_modules=cythonize('**/*.pyx'),
+      install_requires=[
+          'Cython >= 0.20.1',
+          'numpy'
+      ],
+      classifiers=[
+          'Intended Audience :: Science/Research',
+          'Programming Language :: Python',
+          'Programming Language :: C++',
+      ],
+      keywords=['monte-carlo'],
+      platforms="ALL",
+     )
