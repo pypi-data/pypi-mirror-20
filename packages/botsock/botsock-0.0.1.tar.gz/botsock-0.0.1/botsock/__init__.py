1af9c41ab58b44cfda58c38fdfaddd7c0d9088b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:34 +0000

    Add package botsock/0.0.1/botsock-0.0.1.tar.gz

---
+++ packages/botsock/botsock-0.0.1.tar.gz/botsock-0.0.1/botsock/__init__.py
@@ -0,0 +1,4 @@
+# flake8: noqa
+
+from .client import send_data
+from .server import Server
---
+++ packages/botsock/botsock-0.0.1.tar.gz/botsock-0.0.1/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+def requirements():
+    with open('requirements.txt') as req:
+        return req.read().splitlines()
+
+
+setup(
+    name='botsock',
+    version='0.0.1',
+    description='secure socket server for bot communication',
+    long_description=readme(),
+    author='Boris Ostretsov',
+    license='MIT',
+    author_email='ostrbor@gmail.com',
+    keywords='bot botnet socket server',
+    url='https://github.com/ostrbor/botsock',
+    packages=['botsock'],
+    install_requires=requirements())
