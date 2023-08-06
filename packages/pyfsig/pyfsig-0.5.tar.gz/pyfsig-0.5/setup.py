48974284ef4c7279615a3cd18ae3476bf7ebe54c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:34 +0000

    Add package pyfsig/0.5/pyfsig-0.5.tar.gz

---
+++ packages/pyfsig/pyfsig-0.5.tar.gz/pyfsig-0.5/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+def readme():
+    with open('README') as f:
+        return f.read()
+
+setup(name='pyfsig',
+      version='0.5',
+      description='A python library for identifying files by headers (magic bytes)',
+      long_description=readme(),
+      author='schlerp',
+      author_email='schlerpderpson@gmail.com',
+      url='https://github.com/schlerp/pyfsig',
+      packages=['pyfsig'],
+      package_dir={'pyfsig': 'src/pyfsig'},
+      classifiers=['Operating System :: MacOS :: MacOS X',
+                   'Operating System :: Microsoft :: Windows',
+                   'Operating System :: POSIX',
+                   'Programming Language :: Python',
+                   ],
+      )
