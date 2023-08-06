a4de30d84daced7c496d260a9e226a23ae1d6c63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:33 +0000

    Add package prob140/0.2.6.3/prob140-0.2.6.3.tar.gz

---
+++ packages/prob140/prob140-0.2.6.3.tar.gz/prob140-0.2.6.3/prob140/__init__.py
@@ -0,0 +1,8 @@
+from .version import __version__
+from .rebinding import Table,ProbabilityTable,Plot,Plots,JointDistribution
+from .markov_chains import MarkovChain
+
+from .single_variable import emp_dist
+
+from .plots import Plot_continuous, Plot_norm
+from .symbolic_math import *
