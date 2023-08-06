9731dd970eb8cdc1f0ce60defe51de6ffa2a260f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:44 +0000

    Add package bzoinq/0.1.0/bzoinq-0.1.0.tar.gz

---
+++ packages/bzoinq/bzoinq-0.1.0.tar.gz/bzoinq-0.1.0/tests/test_bzoinq.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_bzoinq
+----------------------------------
+
+Tests for `bzoinq` module.
+"""
+
+import pytest
+
+
+from bzoinq import bzoinq
+
+
+# @pytest.fixture
+# def response():
+#     """Sample pytest fixture.
+#     See more at: http://doc.pytest.org/en/latest/fixture.html
+#     """
+#     # import requests
+#     # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+#
+#
+# def test_content(response):
+#     """Sample pytest test function with the pytest fixture as an argument.
+#     """
+#     # from bs4 import BeautifulSoup
+#     # assert 'GitHub' in BeautifulSoup(response.content).title.string
+
+def test_sound():
+    a = bzoinq.Bzoinq()
+    a.create_task()
+    # a.monitor() commented because travis has no default sound output
+    assert a.task_id == 1
