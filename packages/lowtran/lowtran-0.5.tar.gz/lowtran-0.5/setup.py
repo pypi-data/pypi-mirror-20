7411d4ca0d6ead001301f6c5dd7a6609a45f5f83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:48 +0000

    Add package lowtran/0.5/lowtran-0.5.tar.gz

---
+++ packages/lowtran/lowtran-0.5.tar.gz/lowtran-0.5/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import setuptools #enables develop
+
+#%% Monkey patch necessary since setup_requires is not adequate
+import pip
+pip.main(['install','numpy'])
+#%% install
+from numpy.distutils.core import setup,Extension
+
+req=['python-dateutil','pytz','nose','numpy','xarray','matplotlib','seaborn']
+
+setup(name='lowtran',
+      packages=['lowtran'],
+      author='Michael Hirsch, Ph.D',
+      description='Model of Earth atmosphere absorption and transmission vs. wavelength and location on Earth.',
+      version='0.5',
+      url = 'https://github.com/scienceopen/lowtran',
+      classifiers=[
+      'Intended Audience :: Science/Research',
+      'Development Status :: 3 - Alpha',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 3.6',
+      ],
+      install_requires=req,
+      ext_modules=[Extension(name='lowtran7',sources=['lowtran7.f'],
+                    f2py_options=['--quiet'])]
+	  )
