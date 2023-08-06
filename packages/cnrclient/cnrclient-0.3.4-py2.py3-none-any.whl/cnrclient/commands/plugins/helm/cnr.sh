479020f2b84d6044cceac71e85812d6188fa1fff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:18 +0000

    Add package cnrclient/0.3.4/cnrclient-0.3.4-py2.py3-none-any.whl

---
+++ packages/cnrclient/cnrclient-0.3.4-py2.py3-none-any.whl/cnrclient/commands/plugins/helm/cnr.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+function pull {
+    #echo "pull $@"
+    release=`cnr pull --media-type helm --tarball ${@} |tail -n1`
+    echo $release
+}
+
+function install {
+    pull $@ --dest=/tmp
+    sleep 1
+    helm install $release
+}
+
+function cnr_helm {
+    cnr $@ --media-type=helm
+}
+
+
+case "$1" in
+    install)
+        install "${@:2}"
+        ;;
+    pull)
+        pull "${@:2}"
+        ;;
+    push)
+        cnr_helm "$@"
+        ;;
+    list)
+        cnr_helm "$@"
+        ;;
+    show)
+        cnr_helm "$@"
+        ;;
+    delete-package)
+        cnr_helm "$@"
+        ;;
+    inspect)
+        cnr_helm "$@"
+        ;;
+    *)
+        cnr $@
+        ;;
+
+esac
