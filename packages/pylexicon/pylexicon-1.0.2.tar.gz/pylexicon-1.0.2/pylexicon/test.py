740a061043d93e2bd1782ae50dbef46629f38876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:29 +0000

    Add package pylexicon/1.0.1/pylexicon-1.0.2.tar.gz

---
+++ packages/pylexicon/pylexicon-1.0.2.tar.gz/pylexicon-1.0.2/pylexicon/test.py
@@ -0,0 +1,5 @@
+from .core import define, synonym, antonym
+
+print(define('python'))
+print(synonym('happy'))
+print(antonym('happy'))
---
+++ packages/pylexicon/pylexicon-1.0.2.tar.gz/pylexicon-1.0.2/pylexicon/testPyDictionary.py
@@ -0,0 +1,31 @@
+from .core import define, synonym, antonym
+import unittest
+
+
+def testDefine(word):
+    return define(word)
+
+
+def testSynonym(word):
+    return synonym(word)
+
+
+def testAntonym(word):
+    return antonym(word)
+
+
+class MyTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(testDefine('python'),
+                         {'Noun': ['large Old World boas',
+                                   'a soothsaying spirit or a person who is possessed by such a spirit',
+                                   '(Greek mythology) dragon killed by Apollo at Delphi']})
+        self.assertEqual(testSynonym('happy'), ['cheerful',
+                                                'contented', 'overjoyed', 'ecstatic', 'elated'])
+
+        self.assertEqual(testAntonym('happy'), ['melancholy',
+                                                'upset', 'disappointed', 'sorrowful', 'unfriendly'])
+
+
+if __name__ == '__main__':
+    unittest.main()
