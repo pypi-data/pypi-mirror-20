c6cf8ad1dacf5b9872eeefc4443c4011c972cc17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:17 +0000

    Add package flasgger/0.6.1/flasgger-0.6.1.tar.gz

---
+++ packages/flasgger/flasgger-0.6.1.tar.gz/flasgger-0.6.1/flasgger/__init__.py
@@ -0,0 +1,10 @@
+
+__version__ = '0.6.1'
+__author__ = 'Bruno Rocha'
+__email__ = 'rochacbruno@gmail.com'
+
+
+from jsonschema import ValidationError  # noqa
+from .base import Swagger, NO_SANITIZER, BR_SANITIZER, MK_SANITIZER  # noqa
+from .utils import swag_from, validate  # noqa
+from .marshmallow_apispec import SwaggerView, Schema, fields  # noqa
