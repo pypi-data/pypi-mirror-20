e2920379bab4b619a6573a5fdb5437c2f4a181a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:06 +0000

    Add package Bauzak/0.0.3/Bauzak-0.0.3.tar.gz

---
+++ packages/Bauzak/Bauzak-0.0.3.tar.gz/Bauzak-0.0.3/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='Bauzak',
+    packages=['bauzak'],
+    version='0.0.3',
+    description='PLC工具',
+    long_description='PLC工具',
+    author='薛丞宏',
+    author_email='ihcaoe@gmail.com',
+    url='https://xn--v0qr21b.xn--kpry57d/',
+    download_url='https://github.com/i3thuan5/Bauzak',
+    keywords=[
+        'PLC',
+        'TDD',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    install_requires=[
+        'fatek-fbs-lib',
+    ],
+)
