45b10d1662fe59dbe5806a7693f81fd2661ced4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:00 +0000

    Add package spiceypy/1.1.0/spiceypy-1.1.0-cp36-none-win32.whl

---
+++ packages/spiceypy/spiceypy-1.1.0-cp36-none-win32.whl/spiceypy/__init__.py
@@ -0,0 +1,8 @@
+from .spiceypy import *
+from .utils import support_types
+
+__author__ = 'AndrewAnnex'
+
+# Default setting for error reporting so that programs don't just exit out!
+erract("set", 10, "return")
+errdev("set", 10, "null")
