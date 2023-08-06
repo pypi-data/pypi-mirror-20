9f2860c2ad0c5fc337196a53db3999d8bb6a9c7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:06 +0000

    Add package nestercpp/1.0.0/nestercpp-1.0.0.tar.gz

---
+++ packages/nestercpp/nestercpp-1.0.0.tar.gz/nestercpp-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name="nestercpp",
+    version="1.0.0",
+    py_modules=["nestercpp"],
+    author="belindac",
+    url='https://pypi.python.org/id/belindac',
+    description="example",
+)
