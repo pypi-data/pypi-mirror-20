acfdd26d0923f64190ff2a0afbe4ec96534f87c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:20 +0000

    Add package udger/4.0.0/udger-4.0.0.tar.gz

---
+++ packages/udger/udger-4.0.0.tar.gz/udger-4.0.0/setup.py
@@ -0,0 +1,35 @@
+from os.path import dirname, join
+from setuptools import setup, find_packages
+
+from udger import __version__
+
+
+with open(join(dirname(__file__), 'README.rst')) as readme_file:
+    long_description = readme_file.read()
+
+
+setup(
+    name='udger',
+    version=__version__,
+    license='BSD',
+    author='The Udger Team',
+    author_email='info@udger.com',
+    description="Fast and reliable User Agent parser and IP classifier for Python",
+    long_description=long_description,
+    url='https://github.com/udger/udger-python',
+    packages=find_packages(
+        exclude=('udger.tests',),
+    ),
+    platforms='any',
+    zip_safe=True,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries",
+    ],
+)
