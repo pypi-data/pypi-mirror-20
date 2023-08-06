1fc14c836d0fae2391615e72470eb343428f3cf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:16 +0000

    Add package prandtl/0.1/prandtl-0.1.tar.gz

---
+++ packages/prandtl/prandtl-0.1.tar.gz/prandtl-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'prandtl',
+  packages = ['prandtl'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Package for front-end interfacing @ prandtl.design',
+  author = 'Lochie Ferrier',
+  author_email = 'lochie@prandtl.design',
+  url = 'https://github.com/lochieferrier/prandtl-python', # use the URL to the github repo
+  download_url = 'https://github.com/lochieferrier/prandtl-python/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['design', 'engineering'], # arbitrary keywords
+  classifiers = [],
+)
