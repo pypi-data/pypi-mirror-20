38f1022e8f62303867afa851be6dab1330131eb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:50 +0000

    Add package espn-scraper/0.14/espn_scraper-0.14.tar.gz

---
+++ packages/espn-scraper/espn_scraper-0.14.tar.gz/espn_scraper-0.14/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='espn_scraper',
+      version='0.14',
+      description='ESPN scraper for major sports',
+      url='http://github.com/andr3w321/espn_scraper',
+      author='Andrew Rennhack',
+      author_email='andr3w321@gmail.com',
+      license='MIT',
+      install_requires=[
+          'pytz',
+          'python-dateutil'
+      ],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      packages=['espn_scraper'],
+      zip_safe=False)
