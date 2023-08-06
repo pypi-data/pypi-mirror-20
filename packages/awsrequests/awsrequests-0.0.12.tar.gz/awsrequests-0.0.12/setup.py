9ea3ce6aac002d2a260565add6c068782aedf15d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:10 +0000

    Add package awsrequests/0.0.12/awsrequests-0.0.12.tar.gz

---
+++ packages/awsrequests/awsrequests-0.0.12.tar.gz/awsrequests-0.0.12/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import sys
+
+install_requires = [
+    'requests',
+    'boto3'
+]
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name='awsrequests',
+    version='0.0.12',
+    packages=['awsrequests'],
+    url='https://github.com/djcrabhat/aws-requests',
+    license='MIT License',
+    author='ClearDATA',
+    author_email='support@cleardata.com',
+    description='For making signed requests to AWS API Gateway endpoints',
+    install_requires=install_requires,
+    #download_url='https://github.com/djcrabhat/awsrequests/tarball/0.0.4',
+    keywords=['AWS API Gateway signing', 'API Gateway signing', 'AWS API Gateway requests', 'API Gateway requests'],
+    classifiers=[],
+)
