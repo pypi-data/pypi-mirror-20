e1a1ce2f9d5ac843201efd0c76fa245c0b9fa67c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:38 +0000

    Add package pyther/0.6/pyther-0.6.tar.gz

---
+++ packages/pyther/pyther-0.6.tar.gz/pyther-0.6/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+	name = "pyther",
+	packages = ["pyther"],
+	version = "0.6",
+	description = "A open source library for processing thermodynamics data",
+	author = "pysg",
+	author_email = "andres.pyther@gmail.com",
+	url = "https://github.com/pysg/pyther",
+	keyword = ["data analytics", "phases diagram", "thermodynamics"]
+
+)
