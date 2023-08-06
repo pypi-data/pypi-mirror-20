59335e00603a0263996969ae76aa44f92d97e6df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:57 +0000

    Add package django-presto-templates/0.4.1/django_presto_templates-0.4.1-py2.py3-none-any.whl

---
+++ packages/django-presto-templates/django_presto_templates-0.4.1-py2.py3-none-any.whl/presto/test.py
@@ -0,0 +1,7 @@
+def test_message():
+    '''
+    This is my first PIP package, so I'm seeing that it works.
+    This will just print out "Hello World" but I promise I'm not just going
+    to take up space on pip: I'll turn this into something useful soon!
+    '''
+    return "Hello World!"
