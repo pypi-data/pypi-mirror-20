a84e85a9f0af3bd5389b4944e12fb95c5c949569
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:23 +0000

    Add package anom/0.0.1/anom-0.0.1.tar.gz

---
+++ packages/anom/anom-0.0.1.tar.gz/anom-0.0.1/anom/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+from . import properties, properties as props
+from .adapter import Adapter, get_adapter, set_adapter
+from .model import Key, Model, Property, delete_multi, get_multi, put_multi, lookup_model_by_kind
+from .query import Query, Resultset, Page, Pages
+
+__version__ = "0.0.1"
