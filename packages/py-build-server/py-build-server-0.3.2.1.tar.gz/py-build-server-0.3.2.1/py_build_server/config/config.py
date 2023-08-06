f9d91f0281c272ae5f1dbb3f374cb3c39e183b27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:42 +0000

    Add package py-build-server/0.3.2.1/py-build-server-0.3.2.1.tar.gz

---
+++ packages/py-build-server/py-build-server-0.3.2.1.tar.gz/py-build-server-0.3.2.1/py_build_server/config/config.py
@@ -0,0 +1,48 @@
+import sys
+import yaml
+import logging
+
+
+class Config(object):
+    def __init__(self):
+        with open('/etc/py-build-server/config.yaml', 'r') as config_file:
+            config = yaml.load(config_file)
+
+        if isinstance(config, dict):
+            self.logging = Logging(config.get('logging', {}))
+            self.repos = {name: Repo(name, conf) for name, conf in
+                          config.get('repositories', {}).items()}
+            self.update_method = config.get('repository_update_method', 'polling')
+
+        self._sanity_check()
+        logging.basicConfig(level=self.logging.level)
+
+    def get(self, name):
+        return self.repos.get(name)
+
+    def _sanity_check(self):
+        if len(self.repos) < 1:
+            raise Exception(msg='no repos detected in yaml')
+        for name, repo in self.repos.items():
+            if repo.dir is None:
+                raise Exception(msg='repo {} has no dir set'.format(name))
+
+
+class Logging(object):
+    def __init__(self, conf):
+        try:
+            level_map = logging._nameToLevel
+        except:
+            level_map = logging._levelNames
+        self.level = level_map.get(conf.get('level', 'info').upper())
+        self.implement_journald = conf.get('implement_journald', False)
+
+
+class Repo(object):
+    def __init__(self, name, conf):
+        self.name = name
+        self.dir = conf.get('dir')
+        self.fetch_frequency = conf.get('interval', 10)
+        self.branch = conf.get('branch')
+        self.remote = conf.get('remote', 'origin')
+        self.twine_conf = conf.get('twine_conf')
---
+++ packages/py-build-server/py-build-server-0.3.2.1.tar.gz/py-build-server-0.3.2.1/py_build_server/lib/updater.py
@@ -0,0 +1,105 @@
+import json
+import time
+import cherrypy
+
+from multiprocessing import Process
+
+from py_build_server.lib.logger import Logger
+
+
+def get_updater(config):
+    if 'polling' in config.update_method:
+        return PollingUpdater()
+    if 'webhook' in config.update_method:
+        return GithubWebhookUpdater()
+
+
+class Updater(object):
+    def __init__(self):
+        self.repositories = {}
+        self.logger = Logger('polling-updater')
+
+    def register_new_repo(self, repo):
+        self.repositories[repo.name] = repo
+
+    def start(self):
+        raise NotImplementedError
+
+    def check_repo(self, repo):
+        raise NotImplementedError
+
+    def load_config(self, config):
+        pass
+
+
+class GithubWebhookUpdater(Updater):
+    class WebhookResponse(object):
+        def __init__(self, in_dict):
+            self.ref = in_dict.get('ref')
+            self.tag = self.ref.split('/')[-1]
+            self.is_tagged_push = 'tags' in self.ref
+            self.repository = in_dict.get('repository').get('name')
+
+    class Root(object):
+        def __init__(self, updater):
+            self.updater = updater
+        @cherrypy.expose
+        @cherrypy.tools.json_in()
+        def index(self):
+            request = GithubWebhookUpdater.WebhookResponse(cherrypy.request.json)
+            if request.is_tagged_push:
+                repo = self.updater.repositories.get(request.repository)
+                repo.latest_tag = request.tag
+                repo.queue.put(json.dumps(dict(event='new_tag', latest=request.tag)))
+
+    def __init__(self):
+        super(GithubWebhookUpdater, self).__init__()
+        self.subdomain = None
+
+    def start(self):
+        cherrypy.quickstart(self.Root(self), self.subdomain)
+
+    def load_config(self, config):
+        cherrypy.config.update({'log.screen': False,
+                                'log.access_file': '',
+                                'log.error_file': ''})
+        cherrypy.server.socket_host = config.update_method.get('listen_address', '0.0.0.0')
+        self.subdomain = config.update_method.get('subdomain', '/')
+
+
+class PollingUpdater(Updater):
+    def __init__(self):
+        super(PollingUpdater, self).__init__()
+        self.processes = []
+
+    def start(self):
+        for repo in self.repositories.values():
+            repo.logger.debug('creating PollingUpdater process for {}'.format(repo.name))
+            p = Process(target=self.check_repo, args=(repo, ))
+            p.start()
+            self.processes.append(p)
+
+        self.logger.debug('Finished creating processes for PollingUpdater')
+        for p in self.processes:
+            p.join()
+
+    def check_repo(self, repo):
+        while True:
+            self.logger.info('Checking status of {}'.format(repo.name))
+            if repo.config.branch is not None:
+                if repo.config.branch != str(repo.active_branch):
+                    raise Exception(msg='repository is not on the correct branch({} != {})'
+                                    .format(repo.active_branch, repo.config.branch))
+
+            status = repo.get_status()
+            latest_tag = [tag for tag in reversed(sorted(
+                str(tag) for tag in repo.tags
+            )) if tag != 'origin'][0]
+            if status.behind:
+                repo.queue.put(json.dumps(dict(event='new_tag', latest=latest_tag)))
+            self.logger.debug('waiting {} minutes'.format(repo.config.fetch_frequency))
+            try:
+                time.sleep(repo.config.fetch_frequency * 60)
+            except KeyboardInterrupt:
+                self.logger.debug('exited while waiting to check repo again')
+                return
