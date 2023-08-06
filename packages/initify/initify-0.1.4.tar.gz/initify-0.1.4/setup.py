52dd890361e46f7c13edcfa39f0fa954abfcbf46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:39 +0000

    Add package initify/0.1.4/initify-0.1.4.tar.gz

---
+++ packages/initify/initify-0.1.4.tar.gz/initify-0.1.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='initify',
+      version='0.1.4',
+      description='Simplify your __init__(self) function.',
+      url='https://github.com/prankymat/initify.py',
+      author='Matthew Wo',
+      author_email='9029537@gmail.com',
+      license='MIT',
+      packages=['initify'],
+      zip_safe=False)
