84c4feb38bece8c1b5b6215f63bb648f57e7d4c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:58 +0000

    Add package django-request-token/0.5/django-request-token-0.5.tar.gz

---
+++ packages/django-request-token/django-request-token-0.5.tar.gz/django-request-token-0.5/setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""request_token package setup."""
+import os
+from setuptools import setup, find_packages
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+# requirements.txt must be included in MANIFEST.in and include_package_data must be True
+# in order for this to work; ensures that tox can use the setup to enforce requirements
+REQUIREMENTS = '\n'.join(open(os.path.join(os.path.dirname(__file__), 'requirements.txt')).readlines())  # noqa
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name="django-request-token",
+    version="0.5",
+    packages=find_packages(),
+    install_requires=REQUIREMENTS,
+    include_package_data=True,
+    description='JWT-backed Django app for managing querystring tokens.',
+    license='MIT',
+    long_description=README,
+    url='https://github.com/yunojuno/django-request-token',
+    author='Hugo Rodger-Brown',
+    author_email='hugo@yunojuno.com',
+    maintainer='Hugo Rodger-Brown',
+    maintainer_email='hugo@yunojuno.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
