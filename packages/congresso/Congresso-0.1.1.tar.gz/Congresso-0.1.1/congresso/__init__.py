39112642d8cfbfcf7a7ff190386c54a171f33ac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:48 +0000

    Add package congresso/0.1.1/Congresso-0.1.1.tar.gz

---
+++ packages/congresso/Congresso-0.1.1.tar.gz/Congresso-0.1.1/congresso/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import pkg_resources
+
+from .camara import Camara
+
+try:
+    __version__ = '0.1.0'
+except:
+    __version__ = '0.1.0'
+
+
---
+++ packages/congresso/Congresso-0.1.1.tar.gz/Congresso-0.1.1/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Setup file for congresso.
+
+    This file was generated with PyScaffold 2.5.7, a tool that easily
+    puts up a scaffold for your new Python project. Learn more under:
+    http://pyscaffold.readthedocs.org/
+"""
+"""
+
+from setuptools import setup
+from congresso import __version__
+
+
+
+def setup_package():
+    needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
+    sphinx = ['sphinx'] if needs_sphinx else []
+    setup(
+        setup_requires=['six', 'pyscaffold>=2.5a0,<2.6a0'] + sphinx,
+        use_pyscaffold=True,
+        version=__version__,
+        install_requires=['pip', 'requests'],
+    )
+"""
+
+
+from distutils.core import setup
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='Congresso',
+      version='0.1.1',
+      description='Low Level Python Client of Brazilian Camara e Senado Federal API',
+      author='jaotta',
+      author_email='joao.carabetta@gmail.com',
+      long_description=read('README.rst'),
+      license = "BSD",
+       packages=['congresso'],
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Topic :: Utilities",
+          "License :: OSI Approved :: BSD License",
+      ],
+)
+
+
+
+if __name__ == "__main__":
+    setup()
