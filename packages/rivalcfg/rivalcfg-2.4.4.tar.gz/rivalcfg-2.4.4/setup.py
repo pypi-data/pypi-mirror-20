e9228c7777b4d36c67c8103df7f98ca0f9cec399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:05 +0000

    Add package rivalcfg/2.4.4/rivalcfg-2.4.4.tar.gz

---
+++ packages/rivalcfg/rivalcfg-2.4.4.tar.gz/rivalcfg-2.4.4/setup.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# encoding: UTF-8
+
+import os
+import shutil
+import subprocess
+from setuptools import setup, find_packages
+from setuptools.command.install import install as _install
+
+from rivalcfg import VERSION
+
+
+class install(_install):
+    def run(self):
+        _install.run(self)
+        print("Installing udev rules...")
+        if not os.path.isdir("/etc/udev/rules.d"):
+            print("WARNING: udev rules have not been installed (/etc/udev/rules.d is not a directory)")
+            return
+        try:
+            shutil.copy("./rivalcfg/data/99-steelseries-rival.rules", "/etc/udev/rules.d/")
+        except IOError:
+            print("WARNING: udev rules have not been installed (permission denied)")
+            return
+        try:
+            subprocess.call(["udevadm", "trigger"])
+        except OSError:
+            print("WARNING: unable to update udev rules, please run the 'udevadm trigger' command")
+            return
+        print("Done!")
+
+
+setup(
+    name="rivalcfg",
+    version=VERSION,
+    description="Configure SteelSeries Rival gaming mice",
+    url="https://github.com/flozz/rivalcfg",
+    license="WTFPL",
+
+    long_description="""
+    rivalcfg is a small CLI utility program that allows you to configure
+    SteelSeries Rival gaming mice on Linux.
+
+    Supported mice:
+
+    * SteelSeries Rival
+    * SteelSeries Rival 100
+    * SteelSeries Rival 300
+
+    If you have trouble running this software, please open an issue on Github:
+
+    * https://github.com/flozz/rivalcfg/issues
+
+
+    Usage: rivalcfg [options]
+
+    Type "rivalcfg --help" to list available options.
+    """,
+
+    author="Fabien LOISON",
+
+    keywords="steelseries rival rival100 mouse",
+    platforms=["Linux"],
+
+    packages=find_packages(),
+
+    install_requires=[
+        "pyudev>=0.19.0"
+    ],
+
+    entry_points={
+        "console_scripts": [
+            "rivalcfg = rivalcfg.cli:main"
+        ]
+    },
+
+    package_data={
+        "rivalcfg": ["data/*"]
+    },
+
+    cmdclass={"install": install}
+)
+
