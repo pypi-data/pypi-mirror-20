728c34755633e034a0be2e8b6bf3b7f0c103e5fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:27 +0000

    Add package libdw/3.1.4/libdw-3.1.4-py2-none-any.whl

---
+++ packages/libdw/libdw-3.1.4-py2-none-any.whl/soar/runsoar.py
@@ -0,0 +1,12 @@
+#import form.main
+#import soar
+#import soar.application
+
+def start():
+    import form.main
+    import soar
+    import soar.application
+
+    form.main.Application(soar.application.application)
+
+start()
