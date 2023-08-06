0dd08d6217bc56cc6d69e5984913b0091399b958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:09 +0000

    Add package django-lokalise/0.1.6/django_lokalise-0.1.6-py3-none-any.whl

---
+++ packages/django-lokalise/django_lokalise-0.1.6-py3-none-any.whl/lokalise/middleware.py
@@ -0,0 +1,20 @@
+import threading
+from os.path import getmtime
+
+from django.utils.autoreload import reset_translations
+from django.utils.deprecation import MiddlewareMixin
+
+from . import get_locale_path
+
+
+class ReloadTranslationsMiddleware(MiddlewareMixin):
+    @staticmethod
+    def process_request(request):
+        locale_mtime = getmtime(get_locale_path())
+        try:
+            thread_local = threading.local
+            if locale_mtime > thread_local.locale_mtime:
+                thread_local.locale_mtime = locale_mtime
+                reset_translations()
+        except:
+            pass
