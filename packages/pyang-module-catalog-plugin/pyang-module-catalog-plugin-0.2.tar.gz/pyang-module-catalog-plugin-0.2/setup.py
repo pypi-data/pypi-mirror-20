39e2bdd7d1404b52c58b3c1c209b21116ffbaed1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:46 +0000

    Add package pyang-module-catalog-plugin/0.2/pyang-module-catalog-plugin-0.2.tar.gz

---
+++ packages/pyang-module-catalog-plugin/pyang-module-catalog-plugin-0.2.tar.gz/pyang-module-catalog-plugin-0.2/setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='pyang-module-catalog-plugin',
+    version='0.2',
+    description=('A pyang plugin to extract OpenConfig module catalog data from YANG modules'),
+    long_description=read('README.md'),
+    packages=['modulecatalog'],
+    author='Carl Moberg',
+    author_email='camoberg@cisco.com',
+    license='New-style BSD',
+    url='https://github.com/cmoberg/pyang-module-catalog-plugin',
+    install_requires=['pyang'],
+    include_package_data=True,
+    keywords=['yang', 'extraction'],
+    classifiers=[],
+    entry_points={'pyang.plugin': 'module_catalog_plugin=modulecatalog.modulecatalog:pyang_plugin_init'}
+)
