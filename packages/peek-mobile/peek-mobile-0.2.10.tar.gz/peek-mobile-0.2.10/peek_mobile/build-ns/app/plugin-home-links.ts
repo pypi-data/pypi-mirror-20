9165bcdea29f9e67ac0dc1b247fa7ebc88cf77a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:58 +0000

    Add package peek-mobile/0.2.10/peek-mobile-0.2.10.tar.gz

---
+++ packages/peek-mobile/peek-mobile-0.2.10.tar.gz/peek-mobile-0.2.10/peek_mobile/build-ns/app/plugin-home-links.ts
@@ -0,0 +1,9 @@
+// This file is auto generated, the git version is blank and .gitignored
+export const homeLinks = [
+    {
+        "name": "peek_plugin_tutorial", 
+        "pluginIconPath": "/assets/peek_plugin_tutorial/icon.png", 
+        "resourcePath": "/peek_plugin_tutorial", 
+        "title": "Tutorial Plugin"
+    }
+];
---
+++ packages/peek-mobile/peek-mobile-0.2.10.tar.gz/peek-mobile-0.2.10/peek_mobile/build-web/src/plugin-routes.ts
@@ -0,0 +1,7 @@
+// This file is auto generated, the git version is blank and .gitignored
+export const pluginRoutes = [
+    {
+        path: 'peek_plugin_tutorial',
+        loadChildren: "peek_plugin_tutorial/tutorial.module#TutorialModule"
+    }
+];
