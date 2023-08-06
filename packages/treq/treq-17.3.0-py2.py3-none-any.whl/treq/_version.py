349943fd64ec0f2a43ef56433ddd6e0ec9b4d5cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:07 +0000

    Add package treq/17.3.0/treq-17.3.0-py2.py3-none-any.whl

---
+++ packages/treq/treq-17.3.0-py2.py3-none-any.whl/treq/_version.py
@@ -0,0 +1,11 @@
+"""
+Provides Treq version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update Treq` to change this file.
+
+from incremental import Version
+
+__version__ = Version('Treq', 17, 3, 0)
+__all__ = ["__version__"]
