917261211069c6ac2f14cde69fea66fb6055ae3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:12 +0000

    Add package cpc.stats/1.3.3/cpc.stats-1.3.3-py3-none-any.whl

---
+++ packages/cpc.stats/cpc.stats-1.3.3-py3-none-any.whl/cpc/stats/__init__.py
@@ -0,0 +1,2 @@
+from .stats import full_fields_to_ptiles, ptiles_to_full_fields, probs_to_ptiles_using_poe, \
+    linear_interp_constant, find_nearest_index, poe_to_moments, make_poe, poe_to_terciles
