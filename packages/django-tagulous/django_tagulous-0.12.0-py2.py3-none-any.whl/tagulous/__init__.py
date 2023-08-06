23350576402db6de961e68a00d2220023a8e8fc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:47 +0000

    Add package django-tagulous/0.12.0/django_tagulous-0.12.0-py2.py3-none-any.whl

---
+++ packages/django-tagulous/django_tagulous-0.12.0-py2.py3-none-any.whl/tagulous/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Django Tagulous - Fabulous Tags
+"""
+from __future__ import unicode_literals
+
+
+__version__ = "0.12.0"
+__license__ = "BSD"
+__author__ = "Richard Terry"
+__credits__ = [
+    "Pamela McA'Nulty (PamelaM)",
+    "James Pic (jpic)",
+    "Robert Erb (rerb) at AASHE",
+    "Mary (minidietcoke)",
+    "Gaël Utard (gutard)",
+]
