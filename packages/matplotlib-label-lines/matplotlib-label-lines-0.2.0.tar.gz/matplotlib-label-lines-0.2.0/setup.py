c7257398badec486fd91ba6a92a8272cdf069299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:14 +0000

    Add package matplotlib-label-lines/0.2.0/matplotlib-label-lines-0.2.0.tar.gz

---
+++ packages/matplotlib-label-lines/matplotlib-label-lines-0.2.0.tar.gz/matplotlib-label-lines-0.2.0/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='matplotlib-label-lines',
+    version='0.2.0',
+    description='Label lines in matplotlib.',
+    author='NauticalMile',
+    url='https://github.com/cphyc/matplotlib-label-lines',
+    license='MIT',
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=[
+        'numpy',
+        'matplotlib'
+    ],
+    include_package_data=True
+)
