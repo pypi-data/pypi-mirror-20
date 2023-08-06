a6ea3cc92a78a2cbf96107de38a36607c0308569
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:38 +0000

    Add package norduniclient/1.0.8/norduniclient-1.0.8.tar.gz

---
+++ packages/norduniclient/norduniclient-1.0.8.tar.gz/norduniclient-1.0.8/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+version = '1.0.8'
+
+requires = [
+    'neo4j-driver>=1.0.0,<1.1.0',
+]
+
+testing_requires = [
+    'nose',
+    'coverage',
+    'nosexcover',
+]
+
+setup(
+    name='norduniclient',
+    version=version,
+    url='https://github.com/NORDUnet/python-norduniclient',
+    license='Apache License, Version 2.0',
+    author='Johan Lundberg',
+    author_email='lundberg@nordu.net',
+    description='Neo4j (>=3.0) database client using bolt for NORDUnet network inventory',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=requires,
+    tests_require=testing_requires,
+    test_suite='nose.collector',
+    extras_require={
+        'testing': testing_requires
+    }
+)
