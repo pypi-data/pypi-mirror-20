9e841eada21a58798ae773b454089dbb9a2f9284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:51 +0000

    Add package fyve/1.0.1/fyve-1.0.1.tar.gz

---
+++ packages/fyve/fyve-1.0.1.tar.gz/fyve-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup 
+
+setup(name='fyve',
+      version='1.0.1',
+      description='A library to overcomplicated five--the Python way.',
+      url='https://github.com/th13/fyve',
+      author='Trevor Helms',
+      author_email='me@trevorhelms.com',
+      license='MIT',
+      packages=['fyve'],
+      zip_safe=False)
