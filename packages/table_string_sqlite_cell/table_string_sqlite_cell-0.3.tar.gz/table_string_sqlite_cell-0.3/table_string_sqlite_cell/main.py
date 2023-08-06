4923028c9c8a84f021263b9d590d7d81b6b49119
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:28 +0000

    Add package table_string_sqlite_cell/0.3/table_string_sqlite_cell-0.3.tar.gz

---
+++ packages/table_string_sqlite_cell/table_string_sqlite_cell-0.3.tar.gz/table_string_sqlite_cell-0.3/table_string_sqlite_cell/main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import argparse
+
+import pandas as pd
+import sqlalchemy
+
+def main():
+    parser = argparse.ArgumentParser('table_name and cell_value to sqlite cell')
+    parser.add_argument('--cell_value',
+                        required = True
+    )
+    parser.add_argument('--table_name',
+                        required = True
+    )
+
+    args = parser.parse_args()
+    cell_value = args.cell_value
+    table_name = args.table_name
+
+    sqlite_name = 'output.db'
+    engine_path = 'sqlite:///' + sqlite_name
+    engine = sqlalchemy.create_engine(engine_path, isolation_level='SERIALIZABLE')
+
+    data_dict = dict()
+    df = pd.DataFrame(data_dict)
+    df['value'] = cell_value
+    df.to_sql(table_name, engine)
+    return
+
+if __name__ == '__main__':
+    main()
