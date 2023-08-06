89401c0f2294e443f9a4b12faf83cde1134a3bff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:41 +0000

    Add package infi.traceback/0.3.14/infi.traceback-0.3.14-py2.7.egg

---
+++ packages/infi.traceback/infi.traceback-0.3.14-py2.7.egg/infi/traceback/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.3.14"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.3.14)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '747ec8f9208bd4dbf44f40eba6f90f2710d56dcd'
+__git_head_subject__ = u'Finished Release v0.3.14'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
