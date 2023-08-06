bbe35fc6fe1eea1c238950785557f978f69c3ef9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:45 +0000

    Add package peony-twitter/0.9.3.1/peony-twitter-0.9.3.1.tar.gz

---
+++ packages/peony-twitter/peony-twitter-0.9.3.1.tar.gz/peony-twitter-0.9.3.1/peony/commands/event_handlers.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+
+from . import utils
+from .commands import Commands
+from .tasks import task
+from ..utils import print_error, get_args
+
+
+class EventHandler(task):
+
+    def __init__(self, *args, func, event, prefix=None, **values):
+        super().__init__(func)
+
+        self.prefix = prefix
+        self.is_event = event
+
+        if prefix is not None:
+            self.command = Commands(prefix=prefix)
+
+    def __call__(self, *args):
+        argcount = self.__wrapped__.__code__.co_argcount
+
+        if hasattr(self, 'command'):
+            args = (*args, self.command,)
+
+        args = args[:argcount]
+        return super().__call__(*args)
+
+    def __repr__(self):
+        return "<{clsname}: event:{event} prefix:{prefix}>".format(
+            clsname=self.__class__.__name__,
+            prefix=self.prefix,
+            event=self.is_event
+        )
+
+    @classmethod
+    def event_handler(cls, *args, event, prefix=None, **values):
+
+        def decorator(func):
+            event_handler = cls(
+                *args,
+                func=func,
+                event=event,
+                prefix=prefix,
+                **values
+            )
+
+            return event_handler
+
+        return decorator
+
+
+class EventStream:
+
+    def __init__(self, client):
+        self._client = client
+        self.functions = [getattr(self, func)
+                          for func in dir(self) if self._check(func)]
+
+    def __getitem__(self, key):
+        return self._client[key]
+
+    def __getattr__(self, key):
+        return getattr(self._client, key)
+
+    @property
+    def stream_request(self):
+        clsname = self.__class__.__name__
+        msg = "You must overload stream_request property in " + clsname
+        raise RuntimeError(msg)
+
+    @utils.restart_on(Exception)
+    async def start(self):
+        if callable(self.stream_request):
+            stream_request = self.stream_request()
+        else:
+            stream_request = self.stream_request
+
+        async with stream_request as resource:
+            async for data in resource:
+                try:
+                    await self._run(data)
+                except Exception as e:
+                    msg = "error in %s._start:\n" % self.__class__.__name__
+                    print_error(msg)
+
+    def _check(self, func):
+        if not func.startswith("_"):
+            return isinstance(getattr(self, func), EventHandler)
+        else:
+            return False
+
+    def _get(self, data):
+        try:
+            for event_handler in self.functions:
+                if event_handler.is_event(data):
+                    return event_handler
+
+        except:
+            msg = "error in %s._get:\n" % self.__class__.__name__
+            print_error(msg)
+
+    async def _run(self, data):
+        event_handler = self._get(data)
+
+        try:
+            if event_handler:
+                coro = event_handler(self, data)
+                return await utils.execute(coro)
+
+        except:
+            fmt = "error occurred while running {classname} {handler}:\n"
+            msg = fmt.format(classname=self.__class__.__name__,
+                             handler=event_handler.__name__)
+
+            print_error(msg)
+
+
+def check_setup(func):
+    def decorated(self, client):
+        if not self.is_setup:
+            self.setup(client)
+
+        return func(self, client)
+
+    return decorated
+
+
+class EventStreams(list):
+
+    def __init__(self):
+        super().__init__()
+        self.is_setup = False
+
+    @check_setup
+    def get_tasks(self, client):
+        return [client.loop.create_task(stream.start()) for stream in self]
+
+    @check_setup
+    def get_task(self, client):
+        if len(self) == 1:
+            return client.loop.create_task(self[0].start())
+        elif self:
+            raise RuntimeError("more than 1 event stream")
+        else:
+            raise RuntimeError("no event stream")
+
+    def setup(self, client):
+        for i in range(len(self)):
+            self[i] = self[i](client=client)
+
+        self.is_setup = True
---
+++ packages/peony-twitter/peony-twitter-0.9.3.1.tar.gz/peony-twitter-0.9.3.1/peony/stream.py
@@ -0,0 +1,270 @@
+# -*- coding: utf-8 -*-
+
+import asyncio
+import sys
+
+import aiohttp
+
+from . import exceptions, utils
+from .exceptions import StreamLimit
+from .general import rate_limit_notices
+
+if int(aiohttp.__version__.split('.')[0]) < 2:
+    ClientPayloadError = aiohttp.errors.ContentEncodingError
+    ClientConnectionError = aiohttp.errors.ClientDisconnectedError
+else:
+    ClientPayloadError = aiohttp.ClientPayloadError
+    ClientConnectionError = aiohttp.ClientConnectionError
+
+RECONNECTION_TIMEOUT = 5
+MAX_RECONNECTION_TIMEOUT = 320
+DISCONNECTION_TIMEOUT = 0.25
+MAX_DISCONNECTION_TIMEOUT = 16
+ENHANCE_YOUR_CALM_TIMEOUT = 60
+
+
+class StreamResponse:
+    """
+        Asynchronous iterator for streams
+
+    Parameters
+    ----------
+    *args : optional
+        Positional arguments
+    _headers : dict
+        Headers to authorize the request
+    session : :obj:`aiohttp.Session`, optional
+        Session used by the request
+    reconnect : :obj:`int`, optional
+        Time to wait for on error
+    loads : function, optional
+        function used to decode the JSON data received
+    timeout : :obj:`int`, optional
+        Timeout for requests
+    _timeout : :obj:`int`, optional
+        Stream timeout, the connection will be closed if this timeout
+        is exceeded
+    _error_handler : function, optional
+        Request's error handler
+    **kwargs
+        Keyword parameters of the request
+    """
+
+    def __init__(self, *args,
+                 client,
+                 session=None,
+                 loads=utils.loads,
+                 timeout=10,
+                 **kwargs):
+
+        self.client = client
+        self.session = self.client._session if session is None else session
+        self.loads = loads
+        self.timeout = timeout
+        self.args = args
+        self.kwargs = kwargs
+        self.reconnecting = False
+        self.reconnection = 0
+        self.enhance_calm = 0
+        self.disconnection = 0
+
+    def connect(self):
+        """
+            Connect to the stream
+
+        Returns
+        -------
+        asyncio.coroutine
+            The streaming response
+        """
+        kwargs = self.client.headers.prepare_request(**self.kwargs)
+        request = self.client.error_handler(self.session.request)
+
+        if 'proxy' not in kwargs:
+            kwargs['proxy'] = self.client.proxy
+
+        return request(*self.args, timeout=0, **kwargs)
+
+    def initialize_timeouts(self):
+        self.reconnection = 0
+        self.enhance_calm = 0
+        self.disconnection = 0
+
+    async def __aiter__(self):
+        """
+            Create the connection
+
+        Returns
+        -------
+        self
+
+        Raises
+        ------
+        exception.PeonyException
+            On a response status != 2xx
+        """
+        with aiohttp.Timeout(self.timeout):
+            self.response = await self.connect()
+
+        if self.response.status == 200:
+            self.initialize_timeouts()
+        elif self.response.status == 500:
+            self.increase_disconnetion_timeout()
+        elif self.response.status in range(501, 600):
+            self.increase_reconnection_timeout()
+        elif self.response.status in (420, 429):
+            self.increase_enhance_calm_timeout()
+
+            print("Enhance Your Calm response received from Twitter. "
+                  "If you didn't restart your program frenetically "
+                  "then there is probably something wrong with it. "
+                  "Make sure you are not opening too many connections to "
+                  "the endpoint you are currently using by checking "
+                  "Twitter's Streaming API documentation out: "
+                  "https://dev.twitter.com/streaming/overview\n"
+                  "The stream will restart in %ss." % self.enhance_calm,
+                  file=sys.stderr)
+        else:
+            raise await exceptions.throw(self.response)
+
+        return self
+
+    async def __anext__(self):
+        """
+            Decode each line using json
+
+        Returns
+        -------
+        dict
+            Decoded JSON data
+        """
+        line = b''
+        try:
+            timeouts = self.disconnection, self.reconnection, self.enhance_calm
+            reconnect = max(timeouts)
+
+            if self.reconnecting is not False:
+                return await self.restart_stream()
+            elif reconnect:
+                return await self.initialize_restart(reconnect=reconnect)
+
+            while not line:
+                with aiohttp.Timeout(90):
+                    line = await self.response.content.readline()
+                    line = line.rstrip(b'\r\n')
+
+            if line in rate_limit_notices:
+                raise StreamLimit(line)
+
+            return self.loads(line)
+
+        except asyncio.TimeoutError:
+            return await self.initialize_restart()
+
+        except ClientPayloadError:
+            return await self.initialize_restart()
+
+        except ClientConnectionError:
+            self.increase_disconnetion_timeout()
+            return await self.initialize_restart(reconnect=self.disconnection)
+
+        except Exception as e:
+            return await self.initialize_restart(error=e)
+
+    def increase_disconnetion_timeout(self):
+        if not self.disconnection >= MAX_DISCONNECTION_TIMEOUT:
+            self.disconnection += DISCONNECTION_TIMEOUT
+
+    def increase_reconnection_timeout(self):
+        if self.reconnection == 0:
+            self.reconnection = RECONNECTION_TIMEOUT
+        elif not self.reconnection >= MAX_RECONNECTION_TIMEOUT:
+            self.reconnection *= 2
+
+    def increase_enhance_calm_timeout(self):
+        if self.enhance_calm == 0:
+            self.enhance_calm = ENHANCE_YOUR_CALM_TIMEOUT
+        else:
+            self.enhance_calm *= 2
+
+    async def initialize_restart(self, reconnect=0, error=None):
+        """
+            Restart the stream on error
+
+        Parameters
+        ----------
+        reconnect : :obj:`int`, optional
+            Time to wait for before reconnecting
+        error : :class:`Exception`, optional
+            Whether to print the error or not
+        """
+        if reconnect is not None:
+            if error:
+                utils.print_error()
+
+            self.reconnecting = reconnect
+            return {
+                'reconnecting_in': reconnect,
+                'error': error
+            }
+        else:
+            if error is not None:
+                raise error
+
+    async def restart_stream(self):
+        """
+            Restart the stream on error
+        """
+        await self.response.release()
+        await asyncio.sleep(self.reconnecting)
+        await self.__aiter__()
+        self.reconnecting = False
+        return {'stream_restart': True}
+
+
+class StreamContext:
+    """
+        A context that should close the request on exit
+
+    Parameters
+    ----------
+    method : str
+        HTTP method used to make the request
+    url : str
+        The API endpoint
+    *args : optional
+        Positional arguments
+    **kwargs
+        Keyword parameters of the request and of :class:`StreamResponse`
+    """
+
+    def __init__(self, method, url, client, *args, **kwargs):
+        self.method = method
+        self.url = url
+        self.client = client
+        self.args = args
+        self.kwargs = kwargs
+
+    async def __aenter__(self):
+        """
+            Create stream
+
+        Returns
+        -------
+        StreamResponse
+            The stream iterator
+        """
+        await self.client.setup()
+        self.stream = StreamResponse(method=self.method, url=self.url,
+                                     *self.args, client=self.client,
+                                     **self.kwargs)
+
+        return self.stream
+
+    async def __aexit__(self, *args, **kwargs):
+        """
+            Close the response on error
+        """
+
+        if hasattr(self.stream, "response"):
+            self.stream.response.close()
