e20a89944a08adf02ceba27ed9b19e838278a1ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:11 +0000

    Add package django-chunkator/1.1.0/django-chunkator-1.1.0.tar.gz

---
+++ packages/django-chunkator/django-chunkator-1.1.0.tar.gz/django-chunkator-1.1.0/chunkator/__init__.py
@@ -0,0 +1,49 @@
+"""
+Toolbox for chunking / slicing querysets
+"""
+
+
+class MissingPkFieldException(Exception):
+    """
+    Exception raised when the "pk" field is missing in the query.
+
+    (when using ``values()``).
+    """
+    pass
+
+
+def chunkator(source_qs, chunk_size, query_log=None):
+    """
+    Yield over a queryset by chunks.
+
+    This method does not involve counting elements or measuring the iterable
+    length. We're saving at least a ``count()`` query on QuerySets, or a
+    CPU-and-RAM-consuming ``len(queryset)`` query.
+    """
+    pk = None
+    # In django 1.9, _fields is always present and `None` if 'values()' is used
+    # In Django 1.8 and below, _fields will only be present if using `values()`
+    has_fields = hasattr(source_qs, '_fields') and source_qs._fields
+    if has_fields:
+        if "pk" not in source_qs._fields:
+            raise MissingPkFieldException("The values() call must include the `pk` field")  # noqa
+
+    source_qs = source_qs.order_by('pk')
+    queryset = source_qs
+    while True:
+        if pk:
+            queryset = source_qs.filter(pk__gt=pk)
+        page = queryset[:chunk_size]
+        if query_log is not None:
+            query_log.write('{page.query}\n'.format(page=page))
+        nb_items = 0
+        for item in page:
+            # source_qs._fields exists *and* is not none when using "values()"
+            if has_fields:
+                pk = item["pk"]
+            else:
+                pk = item.pk
+            nb_items += 1
+            yield item
+        if nb_items < chunk_size:
+            raise StopIteration
