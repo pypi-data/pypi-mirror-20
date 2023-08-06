c2326a70fb5fb682bc448a1181f1080e44ddef9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:41 +0000

    Add package torcms/0.5.23/torcms-0.5.23.tar.gz

---
+++ packages/torcms/torcms-0.5.23.tar.gz/torcms-0.5.23/torcms/handlers/post_ajax_handler.py
@@ -0,0 +1,58 @@
+# -*- coding:utf-8 -*-
+
+
+
+import json
+import tornado.web
+from torcms.handlers.post_handler import PostHandler
+from torcms.model.post_model import MPost
+
+
+class PostAjaxHandler(PostHandler):
+    def initialize(self):
+        super(PostAjaxHandler, self).initialize()
+
+    def get(self, *args):
+
+        url_arr = self.parse_url(args[0])
+        if url_arr[0] == 'delete':
+            self.delete(url_arr[1])
+        elif url_arr[0] in ['count_plus']:
+            self.count_plus(url_arr[1])
+
+    @tornado.web.authenticated
+    def delete(self, *args):
+        '''
+        Delete the post via Ajax request.
+        :param args:
+        :return:
+        '''
+        uid = args[0]
+        if self.check_post_role()['DELETE']:
+            pass
+        else:
+            return False
+        is_deleted = MPost.delete(uid)
+
+        if is_deleted:
+            output = {
+                'del_info ': 1,
+            }
+        else:
+            output = {
+                'del_info ': 0,
+            }
+        return json.dump(output, self)
+
+    def count_plus(self, uid):
+        '''
+        Ajax request, that the view count will plus 1.
+        :param uid:
+        :return:
+        '''
+        self.set_header("Content-Type", "application/json")
+        output = {
+            'status': 1  # if MPost.__update_view_count_by_uid(uid) else 0,
+        }
+        # return json.dump(output, self)
+        self.write(json.dumps(output))
