76755fb937354474e78660b59a0fa25f98d7ce53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:58 +0000

    Add package payu_biz/0.1/payu_biz-0.1.tar.gz

---
+++ packages/payu_biz/payu_biz-0.1.tar.gz/payu_biz-0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'payu_biz',
+  packages = ['payu_biz'], # this must be the same as the name above
+  version = '0.1',
+  description = ' django_payubiz is the wrapper for payu payment gateway for django based applications',
+  author = 'Renjith S Raj',
+  author_email = 'renjithsraj@live.com',
+  url = 'https://github.com/renjithsraj/django_payubiz/tree/master/payu_biz', # use the URL to the github repo
+  download_url = 'https://github.com/renjithsraj/django_payubiz/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['payu', 'payment gateway', 'payu gateway django', 'django gateway package'], # arbitrary keywords
+  classifiers = [],
+)
