11917960c56132e1d7d6737a55f51dcaeb5d39b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:11 +0000

    Add package aldryn-apphooks-config/0.3.2/aldryn_apphooks_config-0.3.2-py2.py3-none-any.whl

---
+++ packages/aldryn-apphooks-config/aldryn_apphooks_config-0.3.2-py2.py3-none-any.whl/aldryn_apphooks_config/tests/utils/example/cms_appconfig.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+
+from aldryn_apphooks_config.models import AppHookConfig
+from aldryn_apphooks_config.utils import setup_config
+from app_data import AppDataForm
+from django import forms
+from django.db import models
+
+
+class ExampleConfig(AppHookConfig):
+    """Adds some translatable, per-app-instance fields."""
+    app_title = models.CharField('application title', max_length=234)
+
+
+class AnotherExampleConfig(AppHookConfig):
+    max_entries = models.SmallIntegerField(default=5)
+
+
+class ExampleConfigForm(AppDataForm):
+    property = forms.CharField()
+    published_default = forms.BooleanField(initial=True, required=False)
+setup_config(ExampleConfigForm, ExampleConfig)
+
+
+class AnotherExampleConfigForm(AppDataForm):
+    property = forms.CharField()
+setup_config(AnotherExampleConfigForm, AnotherExampleConfig)
