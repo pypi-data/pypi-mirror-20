4554866c24f20baa21ddc85b9d0105f9be70cca6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:51 +0000

    Add package gosms/0.0.0/gosms-0.0.0.tar.gz

---
+++ packages/gosms/gosms-0.0.0.tar.gz/gosms-0.0.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='gosms',
+    version='0.0.0',
+    url='https://github.com/ondrejsika/python-gosms',
+    license='MIT',
+    description='GOSMS.cz client.',
+    author='Ondrej Sika',
+    author_email='ondrej@ondrejsika.com',
+)
+
