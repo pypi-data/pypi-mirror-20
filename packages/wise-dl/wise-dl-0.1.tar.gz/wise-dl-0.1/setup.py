6c23be82d4d06de87e3486c65bc26fa6157ff188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:47 +0000

    Add package wise-dl/0.1/wise-dl-0.1.tar.gz

---
+++ packages/wise-dl/wise-dl-0.1.tar.gz/wise-dl-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wise-dl",
+    version="0.1",
+    # 包含所有src目录下的包
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.10.0',
+        'pycrypto>=2.6.1',
+    ],
+    entry_points='''
+        [console_scripts]
+        netease-dl=netease.main:run
+    ''',
+    author='wise',
+    author_email='wisecsj@gmail.com',
+    url='http://hfut.oyj.cn',
+    description='网易云音乐歌曲下载器',
+    keywords=['music', 'netease', 'download', 'command tool'],
+)
