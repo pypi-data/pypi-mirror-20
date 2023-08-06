9491c2f5dc20690078c56de621af10a310a26a43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:36 +0000

    Add package titanic/4.1/titanic-4.1.tar.gz

---
+++ packages/titanic/titanic-4.1.tar.gz/titanic-4.1/setup.py
@@ -0,0 +1,34 @@
+
+from setuptools import setup
+setup(name='titanic',
+      version='4.1',
+      description='Practice ML & travis/coveralls with titanic data set',
+      long_description='',
+      author='Brooke V Mosby',
+      author_email='brooke.mosby.byu@gmail.com',
+      url='https://github.com/brookemosby/titanic',
+      license='MIT',
+      setup_requires=['pytest-runner',],
+      tests_require=['pytest', 'python-coveralls'],
+      install_requires=[
+          "pandas",
+          "numpy",
+          "sklearn"
+      ],
+      packages=['TitanicAttempt'],
+      include_package_data=True,
+      scripts=['TitanicAttempt/TitanicAttempt.py'],
+              
+      classifiers=[
+          'Development Status :: 2 - Pre-Alpha',
+          'Intended Audience :: Other Audience',
+          'Natural Language :: English',
+          'Operating System :: MacOS',
+          'Operating System :: Microsoft :: Windows',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+      ],
+)
