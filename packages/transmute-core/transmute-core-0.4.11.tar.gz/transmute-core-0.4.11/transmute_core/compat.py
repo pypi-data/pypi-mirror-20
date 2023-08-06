b76c3958ec6dc4854cd1a48e83de3175bac9caf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:48 +0000

    Add package transmute-core/0.4.11/transmute-core-0.4.11.tar.gz

---
+++ packages/transmute-core/transmute-core-0.4.11.tar.gz/transmute-core-0.4.11/transmute_core/compat.py
@@ -0,0 +1,42 @@
+import sys
+import inspect
+from collections import namedtuple
+
+is_py3 = sys.version_info[0] >= 3
+
+FullArgSpec = namedtuple(
+    "FullArgSpec",
+    ["args", "varargs", "varkw", "defaults",
+     "kwonlyargs", "kwonlydefaults", "annotations"]
+)
+
+
+def getfullargspec(func):
+    if is_py3:
+        return inspect.getfullargspec(func)
+    else:
+        argspec = inspect.getargspec(func)
+        annotations = getattr(func, "__annotations__", {})
+        return FullArgSpec(
+            argspec.args, argspec.varargs,
+            argspec.keywords, argspec.defaults,
+            [], None, annotations
+        )
+
+if is_py3:
+    string_type = str  # noqa
+    all_string_types = [str]
+else:
+    string_type = basestring  # noqa
+    all_string_types = [basestring, str, unicode]
+
+
+if is_py3:
+    def reraise(exc):
+        raise exc
+else:
+    reraise = eval("""
+def reraise(exc):
+    traceback = getattr(exc, '__traceback__', None)
+    raise exc, None, traceback
+    """.strip())
