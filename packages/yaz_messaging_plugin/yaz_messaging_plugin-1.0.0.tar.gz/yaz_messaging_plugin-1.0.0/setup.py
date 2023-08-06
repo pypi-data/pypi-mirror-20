052c23f9a5d432c3f44b8e675e10e4d2027fe301
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:30 +0000

    Add package yaz_messaging_plugin/1.0.0/yaz_messaging_plugin-1.0.0.tar.gz

---
+++ packages/yaz_messaging_plugin/yaz_messaging_plugin-1.0.0.tar.gz/yaz_messaging_plugin-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name="yaz_messaging_plugin",
+      packages=["yaz_messaging_plugin"],
+      version="1.0.0",
+      description="A symfony message translation plugin for YAZ",
+      author="Boudewijn Schoon",
+      author_email="boudewijn@zicht.nl",
+      url="http://github.com/boudewijn-zicht/yaz_messaging_plugin",
+      license="MIT",
+      zip_safe=False,
+      install_requires=["yaz", "pyyaml"],
+      test_suite="nose.collector",
+      tests_require=["nose"],
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Environment :: Console",
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6"
+      ])
