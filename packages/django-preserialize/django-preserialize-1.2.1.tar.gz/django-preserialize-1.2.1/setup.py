d4b46a5ff13a2611f3e38b4efcc724df3c57cba3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:18 +0000

    Add package django-preserialize/1.2.1/django-preserialize-1.2.1.tar.gz

---
+++ packages/django-preserialize/django-preserialize-1.2.1.tar.gz/django-preserialize-1.2.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+kwargs = {
+    'packages': ['preserialize'],
+    'include_package_data': True,
+    'install_requires': [
+        'django>=1.8,<1.11',
+    ],
+    'test_suite': 'test_suite',
+    'name': 'django-preserialize',
+    'version': __import__('preserialize').get_version(),
+    'author': 'Byron Ruth',
+    'author_email': 'b@devel.io',
+    'description': 'Pre-serialize model instances and querysets',
+    'license': 'BSD',
+    'keywords': 'serialize model queryset django',
+    'url': 'https://github.com/bruth/django-preserialize/',
+    'classifiers': [
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+}
+
+setup(**kwargs)
