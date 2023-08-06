bb43108a713dd78dbc09f5fa5661d92d31157681
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:37 +0000

    Add package dodo-commands/0.5.2/dodo_commands-0.5.2.tar.gz

---
+++ packages/dodo-commands/dodo_commands-0.5.2.tar.gz/dodo_commands-0.5.2/dodo_commands/extra/webdev_commands/tape.py
@@ -0,0 +1,40 @@
+# noqa
+import argparse
+from dodo_commands.extra.standard_commands import DodoCommand
+from dodo_commands.framework.util import remove_trailing_dashes
+import json
+import os
+
+
+class Command(DodoCommand):  # noqa
+    help = ""
+    decorators = ["docker"]
+    docker_options = [
+        '--name=tape',
+    ]
+
+    def add_arguments_imp(self, parser):  # noqa
+        parser.add_argument(
+            'tape_args',
+            nargs=argparse.REMAINDER
+        )
+
+    def handle_imp(self, tape_args, **kwargs):  # noqa
+        stats_file = os.path.join(
+            self.get_config("/ROOT/src_dir"),
+            "webpack/webpack-stats.json"
+        )
+        stats = json.loads(open(stats_file).read())
+        bundle = os.path.join(
+            self.get_config("/VIRT_ROOT/src_dir"),
+            "static/bundles",
+            stats["chunks"]["main"][0]["name"]
+        )
+
+        self.runcmd(
+            [
+                "tape-run",
+                bundle,
+            ] + remove_trailing_dashes(tape_args),
+            cwd=self.get_config("/TAPE/src_dir")
+        )
---
+++ packages/dodo-commands/dodo_commands-0.5.2.tar.gz/dodo_commands-0.5.2/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+
+
+setup(name='dodo_commands',
+      version='0.5.2',
+      description='Project-aware development environments, inspired by django-manage',
+      url='https://github.com/mnieber/dodo_commands',
+      download_url='https://github.com/mnieber/dodo_commands/tarball/0.5.2',
+      author='Maarten Nieber',
+      author_email='hallomaarten@yahoo.com',
+      license='MIT',
+      packages=[
+          'dodo_commands',
+          'dodo_commands.framework',
+      ],
+      package_data={
+          'dodo_commands': [
+              'extra/__init__.py',
+              'extra/git_commands/*.py',
+              'extra/git_commands/*.meta',
+              'extra/standard_commands/*.py',
+              'extra/standard_commands/*.meta',
+              'extra/standard_commands/decorators/*.py',
+              'extra/webdev_commands/*.py',
+              'extra/webdev_commands/*.meta',
+          ]
+      },
+      entry_points={
+          'console_scripts': [
+              'dodo-upgrade=dodo_commands.dodo_upgrade:main',
+              'dodo-activate=dodo_commands.dodo_activate:main',
+              'dodo-install-default-commands=dodo_commands.dodo_install_commands:main',
+          ]
+      },
+      install_requires=[
+          'argcomplete',
+          'plumbum',
+          'PyYaml',
+          'six',
+      ],
+      zip_safe=False)
