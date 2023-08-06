0136daf3dd4dd3584d190cc1cee905b88e493895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:53 +0000

    Add package roshelper/0.1.6/roshelper-0.1.6.tar.gz

---
+++ packages/roshelper/roshelper-0.1.6.tar.gz/roshelper-0.1.6/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+
+setup(name='roshelper',
+      version='0.1.6',
+      description='Making rospy just a bit easier',
+      author='Alex Wallar',
+      author_email='wallarelvo@gmail.com',
+      url='http://wallarelvo.github.io/roshelper',
+      license="GNU GPL V3",
+      packages=['roshelper'],)
