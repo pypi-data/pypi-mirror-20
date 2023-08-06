3b2fc516dee15b86082a6fb0d3863f804c7979b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:16 +0000

    Add package collective.catalogcleanup/1.7/collective.catalogcleanup-1.7.tar.gz

---
+++ packages/collective.catalogcleanup/collective.catalogcleanup-1.7.tar.gz/collective.catalogcleanup-1.7/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+
+version = '1.7'
+
+setup(name='collective.catalogcleanup',
+      version=version,
+      description="Remove outdated items from the catalog",
+      long_description=(open("README.rst").read() + "\n" +
+                        open("CHANGES.rst").read()),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          "Framework :: Plone",
+          "Framework :: Plone :: 3.3",
+          "Framework :: Plone :: 4.0",
+          "Framework :: Plone :: 4.1",
+          "Framework :: Plone :: 4.2",
+          "Framework :: Plone :: 4.3",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.4",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          ],
+      keywords='plone catalog cleanup',
+      author='Maurits van Rees',
+      author_email='m.van.rees@zestsoftware.nl',
+      url='https://github.com/collective/collective.catalogcleanup',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      extras_require={
+          'test': [
+              'collective.noindexing',
+              'plone.app.robotframework',  # required by plone.app.event
+              'plone.app.testing',
+          ],
+      },
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
