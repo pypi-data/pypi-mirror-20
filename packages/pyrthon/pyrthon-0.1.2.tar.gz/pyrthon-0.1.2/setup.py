0af2a262ddf5b2b06891f2bbe7937c1a42529f40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:48 +0000

    Add package pyrthon/0.1.2/pyrthon-0.1.2.tar.gz

---
+++ packages/pyrthon/pyrthon-0.1.2.tar.gz/pyrthon-0.1.2/setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup
+from pyrthon._version import __version__
+
+
+f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
+readme = f.read()
+f.close()
+
+
+setup(
+    name='pyrthon',
+    version=__version__,
+    description='Literal syntax for Pyrsistent data structures',
+    long_description=readme,
+    author='Tobias Gustafsson',
+    author_email='tobias.l.gustafsson@gmail.com',
+    url='http://github.com/tobgu/pyrthon/',
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    test_suite='tests',
+    scripts=[],
+    install_requires=['pyrsistent'],
+    packages=['pyrthon']
+)
