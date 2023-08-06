a479af2cd615b14bdc1f6a13b3cd9d9e53b60271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:25 +0000

    Add package iromlab/0.2.5/iromlab-0.2.5.tar.gz

---
+++ packages/iromlab/iromlab-0.2.5.tar.gz/iromlab-0.2.5/iromlab/config.py
@@ -0,0 +1,23 @@
+import sys
+
+cdDriveLetter = ""
+cdDeviceName = ""
+cdInfoExe = ""
+prebatchExe = ""
+loadExe = ""
+unloadExe = ""
+rejectExe = ""
+isoBusterExe = ""
+dBpowerampConsoleRipExe = ""
+shntoolExe = ""
+flacExe = ""
+tempDir = ""
+rootDir= ""
+batchFolder = ""
+batchManifest = ""
+jobsFolder = ""
+secondsToTimeout = ""
+prefixBatch = ""
+quitFlag = False
+readyToStart = False
+finishedBatch = False
