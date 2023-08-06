acdbd3f327004a3b0de16eac5f5f0b87718d6d31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:02 +0000

    Add package django-prices/0.6.0/django-prices-0.6.0.tar.gz

---
+++ packages/django-prices/django-prices-0.6.0.tar.gz/django-prices-0.6.0/django_prices/templatetags/prices.py
@@ -0,0 +1,34 @@
+from __future__ import unicode_literals
+
+from django import template
+
+register = template.Library()
+
+
+def normalize_price(price, normalize):
+    if normalize:
+        return price.normalize()
+    return price
+
+
+@register.inclusion_tag('prices/price.html')
+def gross(price, normalize=False):
+    return {'amount': normalize_price(price, normalize),
+            'currency': price.currency}
+
+
+@register.inclusion_tag('prices/price.html')
+def net(price, normalize=False):
+    return {'amount': normalize_price(price, normalize),
+            'currency': price.currency}
+
+
+@register.inclusion_tag('prices/price.html')
+def tax(price, normalize=False):
+    return {'amount': normalize_price(price, normalize),
+            'currency': price.currency}
+
+
+@register.filter
+def discount_amount_for(discount, price):
+    return (price | discount) - price
