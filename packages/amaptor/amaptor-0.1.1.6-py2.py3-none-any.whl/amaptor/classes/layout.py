db732c7f3de4e7531d036e515c8c12ff7e376b9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:16 +0000

    Add package amaptor/0.1.1.6/amaptor-0.1.1.6-py2.py3-none-any.whl

---
+++ packages/amaptor/amaptor-0.1.1.6-py2.py3-none-any.whl/amaptor/classes/layout.py
@@ -0,0 +1,28 @@
+import logging
+log = logging.getLogger("amaptor")
+
+from amaptor.classes.map_frame import MapFrame
+
+class Layout(object):
+	"""
+		Replicates Layouts so that we can do some nice things behind the scenes
+	"""
+
+	def __init__(self, layout_object, project):
+		self._layout_object = layout_object
+		self.project = project
+		self.frames = [MapFrame(frame, self) for frame in self._layout_object.listElements("MAPFRAME_ELEMENT")]  # frames connect back to maps, this connects to a project
+
+	@property
+	def name(self):
+		return self._layout_object.name
+
+	@name.setter
+	def name(self, value):
+		self._layout_object.name = value
+
+	def export_to_png(self, out_path, resolution=300):
+		self._layout_object.exportToPNG(out_path, resolution)
+
+	def export_to_pdf(self, out_path, **kwargs):
+		self._layout_object.exportToPDF(out_path, **kwargs)
