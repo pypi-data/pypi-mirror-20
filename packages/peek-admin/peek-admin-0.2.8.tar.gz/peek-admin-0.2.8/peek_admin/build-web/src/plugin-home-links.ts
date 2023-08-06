2c1123b03e1e28473fc4b455b9c54aafa5f55926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:09 +0000

    Add package peek-admin/0.2.8/peek-admin-0.2.8.tar.gz

---
+++ packages/peek-admin/peek-admin-0.2.8.tar.gz/peek-admin-0.2.8/peek_admin/build-web/src/plugin-home-links.ts
@@ -0,0 +1,9 @@
+// This file is auto generated, the git version is blank and .gitignored
+export const homeLinks = [
+    {
+        "name": "peek_plugin_tutorial", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_tutorial", 
+        "title": "Tutorial Plugin"
+    }
+];
