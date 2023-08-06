522efccc93ed4c73370ae6acdcf7f97361a08207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:04 +0000

    Add package lim/1.2.7/lim-1.2.7-py3-none-any.whl

---
+++ packages/lim/lim-1.2.7-py3-none-any.whl/lim/genetics/__init__.py
@@ -0,0 +1,12 @@
+"""Genetic related analysis.
+
+The provided analysis are:
+    - Heritability estimation;
+    - Quantitative trait locus (QTL) discovery.
+"""
+
+from . import heritability
+from . import phenotype
+from . import qtl
+from . import variance
+from .model import CanonicalModel
