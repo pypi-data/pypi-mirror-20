7b8a41f3bb5d12f8d2ba9c9f04fd6979ea3f6900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:24 +0000

    Add package populus/1.6.4b1/populus-1.6.4b1.tar.gz

---
+++ packages/populus/populus-1.6.4b1.tar.gz/populus-1.6.4b1/populus/compilation.py
@@ -0,0 +1,62 @@
+import itertools
+import json
+
+from solc import (
+    compile_files,
+)
+from solc.exceptions import (
+    ContractsNotFound,
+)
+
+from populus.utils.compile import (
+    process_compiler_output,
+    get_project_source_paths,
+    get_test_source_paths,
+)
+from populus.utils.filesystem import (
+    ensure_file_exists,
+)
+
+
+DEFAULT_COMPILER_OUTPUT_VALUES = ['abi', 'bin', 'bin-runtime', 'devdoc', 'metadata', 'userdoc']
+
+
+def compile_project_contracts(project, compiler_settings=None):
+    if compiler_settings is None:
+        compiler_settings = {}
+
+    compiler_settings.setdefault('output_values', DEFAULT_COMPILER_OUTPUT_VALUES)
+
+    project_contract_source_paths = get_project_source_paths(project.contracts_source_dir)
+    test_contract_source_paths = get_test_source_paths(project.tests_dir)
+
+    all_source_paths = tuple(itertools.chain(
+        project_contract_source_paths,
+        test_contract_source_paths,
+    ))
+
+    try:
+        compiled_contracts = compile_files(all_source_paths, **compiler_settings)
+    except ContractsNotFound:
+        return all_source_paths, {}
+
+    normalized_compiled_contracts = dict(
+        process_compiler_output(contract_name, contract_data)
+        for contract_name, contract_data
+        in compiled_contracts.items()
+    )
+
+    return all_source_paths, normalized_compiled_contracts
+
+
+def write_compiled_sources(compiled_contracts_asset_path, compiled_sources):
+    ensure_file_exists(compiled_contracts_asset_path)
+
+    with open(compiled_contracts_asset_path, 'w') as outfile:
+        outfile.write(
+            json.dumps(compiled_sources,
+                       sort_keys=True,
+                       indent=4,
+                       separators=(',', ': '))
+        )
+    return compiled_contracts_asset_path
