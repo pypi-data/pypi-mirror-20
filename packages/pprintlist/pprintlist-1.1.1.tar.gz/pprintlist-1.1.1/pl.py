8f3c39993178c1abc37c513e59346e7874593707
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:18 +0000

    Add package pprintlist/1.1.1/pprintlist-1.1.1.tar.gz

---
+++ packages/pprintlist/pprintlist-1.1.1.tar.gz/pprintlist-1.1.1/pl.py
@@ -0,0 +1,10 @@
+'''这是一个打印列表数据项的模块'''
+def pprintlist(thelist,level):
+#'''函数'''
+	for x in thelist:
+		if isinstance(x,list):
+			pprintlist(x,leve +2 )
+		else:
+			for fff in range(level):
+				print('\t',end="")
+			print(x)
---
+++ packages/pprintlist/pprintlist-1.1.1.tar.gz/pprintlist-1.1.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name = 'pprintlist',
+	version = '1.1.1',
+	py_modules = ['pl'],
+	author = '齐万',
+	author_email = 'qi-wan@163.com',
+	url = 'http://www.zptw.net',
+	description = '打印列表',
+)
+
