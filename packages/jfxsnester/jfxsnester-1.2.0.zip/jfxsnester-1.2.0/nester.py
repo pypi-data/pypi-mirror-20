48921f66d5cab43da97132ea5dfe4eddd3a74bdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:51 +0000

    Add package jfxsnester/1.2.0/jfxsnester-1.2.0.zip

---
+++ packages/jfxsnester/jfxsnester-1.2.0.zip/jfxsnester-1.2.0/nester.py
@@ -0,0 +1,14 @@
+"""这是“nester.py”模块，提供了一个函数，作用为打印列表
+，其中有可能嵌套列表"""
+def print_lol(the_list,indent=False,level=0):
+        #此函数取一个位置参数，递归输出数据项;一个level参数，缩进
+        for each_item in the_list:
+                if isinstance(each_item, list):
+                        level+=1
+                        print_lol(each_item,indent,level)
+                else:
+                        if indent:
+                                for tab_stop in range(level):
+                                        print("\t",end='')
+                        print(each_item)
+			
