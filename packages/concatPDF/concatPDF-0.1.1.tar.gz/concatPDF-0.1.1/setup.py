8e13c9f934f689fecd608d6ebbd2589acd2be259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:34 +0000

    Add package concatPDF/0.1.1/concatPDF-0.1.1.tar.gz

---
+++ packages/concatPDF/concatPDF-0.1.1.tar.gz/concatPDF-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="concatPDF",
+    version="0.1.1",
+    author="Ritchie Vink",
+    license='MIT License',
+    entry_points = {
+        'console_scripts': ['concatPDF-init=concatPDF.quickstart:run'],
+    },
+    packages=["concatPDF", "concatPDF/res"],
+    install_requires=[
+        "PyPDF2>=1.26.0"
+    ]
+)
