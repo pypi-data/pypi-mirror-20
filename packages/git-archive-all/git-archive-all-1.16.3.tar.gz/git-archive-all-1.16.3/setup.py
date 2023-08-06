1ca0d34f4333ae4474514c9c34eb970b13806711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:10 +0000

    Add package git-archive-all/1.16.3/git-archive-all-1.16.3.tar.gz

---
+++ packages/git-archive-all/git-archive-all-1.16.3.tar.gz/git-archive-all-1.16.3/setup.py
@@ -0,0 +1,21 @@
+import re
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+# Parse the version from the file.
+verstrline = open('git_archive_all.py', "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in git_archive_all.py")
+
+setup(
+    version=verstr,
+    py_modules=['git_archive_all'],
+    entry_points={'console_scripts': 'git-archive-all=git_archive_all:main'}
+)
