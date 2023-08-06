d8e762c92dec2a8bc9f12a42495f2c37a681fcbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:55 +0000

    Add package voiceplay/0.2.5.7/voiceplay-0.2.5.7.tar.gz

---
+++ packages/voiceplay/voiceplay-0.2.5.7.tar.gz/voiceplay-0.2.5.7/voiceplay/logger/logger.py
@@ -0,0 +1,13 @@
+#-*- coding: utf-8 -*-
+"""
+Configure/Initialize custom logger
+TODO: use configuration file
+"""
+
+import logging
+import sys
+
+from voiceplay import __title__
+logging.basicConfig()
+logger = logging.getLogger(__title__)
+logger.setLevel(logging.INFO)
