0cf8e98a063848d22e3a97da267ea5b352eb15ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:12 +0000

    Add package sanic-envconfig/0.2/sanic_envconfig-0.2.tar.gz

---
+++ packages/sanic-envconfig/sanic_envconfig-0.2.tar.gz/sanic_envconfig-0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='sanic_envconfig',
+    version='0.2',
+    packages=['sanic_envconfig'],
+    url='https://github.com/jamesstidard/sanic-envconfig',
+    license='MIT',
+    author='James Stidard',
+    author_email='jamesstidard@gmail.com',
+    description='Pull environment variables into your sanic config class.',
+    keywords='sanic config environment variables extension',
+    platforms=['any'],
+    tests_require=['pytest', 'pytest-mock', 'pytest-cov'])
