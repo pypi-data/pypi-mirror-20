0ccef14d21b637a9f27c76dee37ca4711114a02b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:27 +0000

    Add package hipsaint/0.7/hipsaint-0.7.tar.gz

---
+++ packages/hipsaint/hipsaint-0.7.tar.gz/hipsaint-0.7/hipsaint/options.py
@@ -0,0 +1,16 @@
+COLORS = {
+    'PROBLEM': 'red',
+    'RECOVERY': 'green',
+    'UP': 'green',
+    'ACKNOWLEDGEMENT': 'purple',
+    'FLAPPINGSTART': 'yellow',
+    'WARNING': 'yellow',
+    'UNKNOWN': 'gray',
+    'CRITICAL': 'red',
+    'FLAPPINGEND': 'green',
+    'FLAPPINGSTOP': 'green',
+    'FLAPPINGDISABLED': 'purple',
+    'DOWNTIMESTART': 'red',
+    'DOWNTIMESTOP': 'green',
+    'DOWNTIMEEND': 'green'
+}
