448d3e4e21e9f38e0a6e33eb4a1e39e0678b90a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:27 +0000

    Add package downloads/0.1.0/downloads-0.1.0.tar.gz

---
+++ packages/downloads/downloads-0.1.0.tar.gz/downloads-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+import sys
+
+if sys.version_info < (3, 0):
+    sys.exit('Sorry, Python < 3 is not supported')
+
+setup(name='downloads',
+      version='0.1.0',
+      description='Easy HTTP downloads',
+      url='https://github.com/audy/downloads',
+      author='Austin Davis-Richardson',
+      author_email='harekrishna@gmail.com',
+      packages=['downloads'])
