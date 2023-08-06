5211323c875125d991e5b49fc98e4f43cbe71f1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:31 +0000

    Add package xlwrap/0.1.1/xlwrap-0.1.1.tar.gz

---
+++ packages/xlwrap/xlwrap-0.1.1.tar.gz/xlwrap-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='xlwrap',
+    version='0.1.1',
+    description='A library to manipulate excel files of multiple types with an identical, simple API',
+    url='https://github.com/erik-sn/xlwrap.git',
+    author='Erik Niehaus',
+    author_email='nieh.erik@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='excel development',
+    py_modules=["xlwrap"],
+    install_requires=['xlrd', 'openpyxl'],
+)
