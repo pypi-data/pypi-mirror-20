1f685cba8f10f7a86615a72882f98f69ab3be1d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:26 +0000

    Add package pyaxmlparser/0.1.2/pyaxmlparser-0.1.2.tar.gz

---
+++ packages/pyaxmlparser/pyaxmlparser-0.1.2.tar.gz/pyaxmlparser-0.1.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+
+dependencies = open('requirements.txt').read().splitlines()
+
+setup(
+    name='pyaxmlparser',
+    version='0.1.2',
+    url='https://github.com/appknox/pyaxmlparser',
+    packages=find_packages(exclude=['tests']),
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    package_data={
+        '': ['*.txt'],
+    },
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+
+        'Intended Audience :: Developers',
+
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
