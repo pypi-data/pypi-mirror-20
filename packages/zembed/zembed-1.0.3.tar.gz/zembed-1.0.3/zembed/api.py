b027f58a33043fc3ed5473d231631ec999ab421f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:51 +0000

    Add package zembed/1.0.3/zembed-1.0.3.tar.gz

---
+++ packages/zembed/zembed-1.0.3.tar.gz/zembed-1.0.3/zembed/api.py
@@ -0,0 +1,30 @@
+import requests
+from zembed.exceptions import ZembedException
+
+
+class API:
+    _api_key = None
+    _base_url = 'http://35.162.233.79'
+
+    def __init__(self, api_key=''):
+        self._api_key = api_key
+
+    @classmethod
+    def get_embed(cls, url):
+        return cls.response(cls._get('{}/{}'.format(cls._base_url, 'crawler'), {
+            'url': url, 'api_key': cls._api_key, 'format': 'json'
+        }))
+
+    @staticmethod
+    def _get(url, params=None, **kwargs):
+        result = requests.get(url, params=params, **kwargs)
+        _response = result.json()
+
+        if _response and 'error' in _response or result.status_code != 200:
+            raise ZembedException(_response['status_code'], _response['error'], _response)
+
+        return result
+
+    @staticmethod
+    def response(response):
+        return response.json()
