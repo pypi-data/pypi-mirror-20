551d275fe00913bc5b43a34c4c8f2bb11de00545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:16 +0000

    Add package zhuzuwei_nester/1.0.0/zhuzuwei_nester-1.0.0.zip

---
+++ packages/zhuzuwei_nester/zhuzuwei_nester-1.0.0.zip/zhuzuwei_nester-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'zhuzuwei_nester',
+    version = '1.0.0',
+    py_modules = ['zhuzuwei_nester'],
+    author = 'hfpython',
+    author_email = 'hfpython@headfirstlabs.com',
+    url = 'http://hfpython@headfirstlabs.com',
+    description = 'A simple printer of nested lists',
+
+    )
