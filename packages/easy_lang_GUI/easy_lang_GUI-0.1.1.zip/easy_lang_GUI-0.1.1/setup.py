f424f6bf6c2745e0d447f2a46b07c97c488bcc64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:03 +0000

    Add package easy_lang_GUI/0.1.1/easy_lang_GUI-0.1.1.zip

---
+++ packages/easy_lang_GUI/easy_lang_GUI-0.1.1.zip/easy_lang_GUI-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from os.path import join, dirname
+
+from setuptools import setup, find_packages
+
+import easy_lang_GUI
+
+
+
+setup(
+    name='easy_lang_GUI',
+    version=easy_lang_GUI.__version__,
+    packages=find_packages(),
+	description="GUI addon for easy_languages.",
+	long_description="...",
+	author="Larty",
+    include_package_data=True,
+    install_requires=['easy_languages==0.1.3']
+)
