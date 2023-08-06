ebb99fbac886473988be2b3294188e17255ec407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:36 +0000

    Add package django-bureaucracy/0.2/django-bureaucracy-0.2.tar.gz

---
+++ packages/django-bureaucracy/django-bureaucracy-0.2.tar.gz/django-bureaucracy-0.2/django_bureaucracy/engine.py
@@ -0,0 +1,25 @@
+from django.template.backends.django import DjangoTemplates
+
+from bureaucracy.powerpoint.engines import BaseEngine
+
+
+class DjangoSlideEngine(BaseEngine):
+
+    def __init__(self, extra_builtins=None):
+        extra_builtins = extra_builtins or []
+
+        options = {
+            'NAME': '',
+            'OPTIONS': {
+                'builtins': ['django_bureaucracy.templatetags', ] + extra_builtins
+
+            },
+            'DIRS': [],
+            'APP_DIRS': True
+        }
+
+        self.django_tpls = DjangoTemplates(options)
+
+    def render(self, fragment, context):
+        tpl = self.django_tpls.from_string(fragment)
+        return tpl.render(context)
