b2ea6b8f77704ca285258b00e810085615381ee3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:17 +0000

    Add package litesockets/0.7.0/litesockets-0.7.0.tar.gz

---
+++ packages/litesockets/litesockets-0.7.0.tar.gz/litesockets-0.7.0/litesockets/__init__.py
@@ -0,0 +1,7 @@
+__all__ = ["Stats", "SocketExecuter", "TCPClient", "TCPServer", "UDPServer", "UDPClient"]
+
+from litesockets.stats import Stats
+from litesockets.socketexecuter import SocketExecuter
+from litesockets.tcp import TCPClient, TCPServer
+from litesockets.udp import UDPClient, UDPServer
+
