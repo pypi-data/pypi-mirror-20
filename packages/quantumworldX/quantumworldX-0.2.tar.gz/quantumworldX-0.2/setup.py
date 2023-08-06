db73f34305b0b4ccf7fb6c0f26d862fa2c215a04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:54 +0000

    Add package quantumworldX/0.2/quantumworldX-0.2.tar.gz

---
+++ packages/quantumworldX/quantumworldX-0.2.tar.gz/quantumworldX-0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+setup(name='quantumworldX',
+      version='0.2',
+      description='Basic library for the QuantumWorld edX course',
+      url='https://pypi.python.org/pypi/quantumworld',
+      author='Benjamin Sanchez Lengeling',
+      author_email='beangoben@gmail.com',
+      license='MIT',
+      packages=['quantumworldX'],
+      install_requires=['numpy>=1.11',
+                        'matplotlib>=1.5',
+                        'scipy>=0.12.0',
+                        'ipython'
+                        ],
+      # download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1',
+      keywords=['Quantum Chemistry', 'edX',
+                'Quantum Mechanics'],  # arbitrary keywords
+      classifiers=[],
+      )
