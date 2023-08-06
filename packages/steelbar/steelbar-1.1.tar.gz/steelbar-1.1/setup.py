41e8e57288e23d5f8b70fe001886734364460ae9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:16 +0000

    Add package steelbar/1.1/steelbar-1.1.tar.gz

---
+++ packages/steelbar/steelbar-1.1.tar.gz/steelbar-1.1/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+long_desc = '''SteelBar is a simple multilayered progress bar for Python 3
+console applications.'''
+
+setup(
+    name='steelbar',
+    packages=['steelbar'],
+    version='1.1',
+    license='WTFPL Version 2',
+    description='Simple multilayered progress bar for Python 3.',
+    author='Vytautas Valiukonis',
+    author_email='vytautas.valiukonis@outlook.com',
+    url='https://github.com/SSteel2/SteelBar',
+    download_url='https://github.com/SSteel2/SteelBar/archive/1.1.tar.gz',
+    keywords=['progress', 'bar', 'steelbar', 'multilayer'],
+    classifiers=[],
+    long_description=long_desc
+)
