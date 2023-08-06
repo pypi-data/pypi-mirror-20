9fa03ea1a7bce6d7c166f2e29f4bdf1eac2702ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:50 +0000

    Add package LicenseCheckBear/0.10.0/LicenseCheckBear-0.10.0.tar.gz

---
+++ packages/LicenseCheckBear/LicenseCheckBear-0.10.0.tar.gz/LicenseCheckBear-0.10.0/setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+import os
+import locale
+
+from setuptools import find_packages, setup
+
+
+try:
+    locale.getlocale()
+except (ValueError, UnicodeError):
+    locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+
+required = ''
+if os.path.exists('requirements.txt'):
+    with open('requirements.txt') as requirements:
+        required = requirements.read().splitlines()
+
+if __name__ == '__main__':
+    try:
+        setup(name='LicenseCheckBear',
+              version='0.10.0',
+              description=''''LicenseCheckBear' bear for coala (http://coala.rtfd.org/)''',
+              authors={'The coala developers'},
+              authors_emails={'coala-devel@googlegroups.com'},
+              maintainers={'The coala developers'},
+              maintainers_emails={'coala-devel@googlegroups.com'},
+              platforms={'any'},
+              license='AGPL-3.0',
+              packages=find_packages(exclude=['build.*']),
+              install_requires=required,
+              long_description='''
+    Attempts to check the given file for a license, by searching the start
+    of the file for text belonging to various licenses.
+
+    For Ubuntu/Debian users, the ``licensecheck_lines`` option has to be used
+    in accordance with the ``licensecheck_tail`` option.
+    ''',
+              entry_points={'coalabears': ['LicenseCheckBear = coalaLicenseCheckBear']},
+              classifiers=[
+                   'Development Status :: 4 - Beta',
+                   'Environment :: Console',
+                   'Environment :: Win32 (MS Windows)',
+                   'Intended Audience :: Science/Research',
+                   'Intended Audience :: Developers',
+                   'Programming Language :: '
+                   'Python :: Implementation :: CPython',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3 :: Only',
+                   'Topic :: Scientific/Engineering :: Information Analysis',
+                   'Topic :: Software Development :: Quality Assurance',
+                   'Topic :: Text Processing :: Linguistic'])
+
+    finally:
+        print('[WARN] If you do not install the bears using the coala '
+              'installation tool, there may be problems with the dependencies '
+              'and they may not work.')
