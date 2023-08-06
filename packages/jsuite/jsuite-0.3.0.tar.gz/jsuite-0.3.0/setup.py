41e1ce49d0c09f658349a65e387d5d2f21e6ceb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:45 +0000

    Add package jsuite/0.3.0/jsuite-0.3.0.tar.gz

---
+++ packages/jsuite/jsuite-0.3.0.tar.gz/jsuite-0.3.0/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+  name = 'jsuite',
+  packages = ['jsuite'], # this must be the same as the name above
+  version = '0.3.0',
+
+  scripts=['bin/jsuite'],
+  install_requires = [
+    'lxml==3.6.4',
+    'clint==0.5.1'
+  ],
+
+  description = 'Parsing and manipulation tools for JATS XML files.',
+  author = 'Dipanjan Mukherjee',
+  author_email = 'dipanjan.mu@gmail.com',
+  url = 'https://github.com/schatten/jsuite', # use the URL to the github repo
+  download_url = 'https://github.com/schatten/jsuite/tarball/0.1', # I'll explain this in a second
+  keywords = ['xml', 'parsing', 'JATS', 'tools'], # arbitrary keywords
+  classifiers = [],
+)
