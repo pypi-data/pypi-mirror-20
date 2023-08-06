f016ae482c72d136c00ee4e418a257b5ddc73b34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:42 +0000

    Add package cds_utils/0.0.2/cds_utils-0.0.2.zip

---
+++ packages/cds_utils/cds_utils-0.0.2.zip/cds_utils-0.0.2/setup.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+from setuptools import setup, find_packages
+
+setup(
+    name="cds_utils",
+    version='0.0.2',
+    keywords=("图灵CDS", "工具类"),
+    description="图灵CDS工具类",
+    license="MIT License",
+    author="hc",
+    author_email="huangcheng_cc@sina.com",
+    package_data={'': ['*.*']},
+    packages=find_packages(),
+    platforms="any"
+)
