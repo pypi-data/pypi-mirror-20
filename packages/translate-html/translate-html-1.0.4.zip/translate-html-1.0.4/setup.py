a6f293014e7ff78fed47814a49f33cb3d563e3c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:16 +0000

    Add package translate-html/1.0.4/translate-html-1.0.4.zip

---
+++ packages/translate-html/translate-html-1.0.4.zip/translate-html-1.0.4/setup.py
@@ -0,0 +1,52 @@
+# -*- coding:utf-8 -*-
+try:
+    from setuptools import setup, find_packages
+except:
+    from distutils.core import setup
+
+VERSION = '1.0.4'
+
+AUTHOR = "cn"
+
+AUTHOR_EMAIL = "308299269@qq.com"
+
+URL = "https://www.github.com/ShichaoMa/translate_html"
+
+NAME = "translate-html"
+
+DESCRIPTION = "translate html to chinese without tag. "
+
+LONG_DESCRIPTION = open("README.rst").read()
+
+KEYWORDS = "translate html chinese tag"
+
+LICENSE = "MIT"
+
+MODULES = ["translate"]
+
+setup(
+    name = NAME,
+    version = VERSION,
+    description = DESCRIPTION,
+    long_description = LONG_DESCRIPTION,
+    classifiers = [
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+    entry_points={
+        'console_scripts': [
+            'translate = translate:main',
+        ],
+    },
+    keywords = KEYWORDS,
+    author = AUTHOR,
+    author_email = AUTHOR_EMAIL,
+    url = URL,
+    license = LICENSE,
+    py_modules = MODULES,
+    install_requires=["requests"],
+    include_package_data=True,
+    zip_safe=True,
+)
