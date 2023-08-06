48315a46cf8c559fce040272747208239bba9361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:33 +0000

    Add package shelfdb/0.2.0rc3/shelfdb-0.2.0rc3.tar.gz

---
+++ packages/shelfdb/shelfdb-0.2.0rc3.tar.gz/shelfdb-0.2.0rc3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='shelfdb',
+    version='0.2.0-rc3',
+    description='Python dict/json DB, Done right for Efficiency and Simplicity',
+    long_description='Python dict/json DB, Done right for Efficiency and Simplicity',
+    url='https://github.com/nitipit/shelfdb',
+    author='Nitipit Nontasuwan',
+    author_email='nitipit@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='dict json database',
+    packages=find_packages(),
+    install_requires=['dill',],
+    entry_points={
+        'console_scripts': [
+            'shelfdb=shelfdb.server:main',
+        ],
+    },
+)
