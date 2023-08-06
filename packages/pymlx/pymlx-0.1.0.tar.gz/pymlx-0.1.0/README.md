8ce7396b7d465e7096d0d20b220c1655bf797ca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:40 +0000

    Add package pymlx/0.1.0/pymlx-0.1.0.tar.gz

---
+++ packages/pymlx/pymlx-0.1.0.tar.gz/pymlx-0.1.0/README.md
@@ -0,0 +1,11 @@
+# MLx
+*Yet another machine learning framework.*
+
+## How to install
+`pip install pymlx`
+
+Note that PyMLx is only available on Linux at this point.
+
+##Q&As
+1. What's the OSS license of this library
+  - BSD3
---
+++ packages/pymlx/pymlx-0.1.0.tar.gz/pymlx-0.1.0/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+from setuptools import setup
+
+with open('README.md') as file:
+    long_description = file.read()
+
+setup(
+    name='pymlx',
+    packages=['pymlx'],
+    version='0.1.0',
+    description='Yet another machine learning framework',
+    long_description=long_description,
+    author='Kenneth Tran',
+    author_email='one@kentran.net',
+    url='https://github.com/zer0n/MLx',
+    keywords="machine learning",
+    license="BSD3",
+    install_requires=[
+        'numpy>=1.9.2',
+        'scipy>=0.14.0',
+        'pandas>=0.15.0',
+        'scikit_learn>=0.16',
+        'xgboost',
+        'matplotlib>=1.4.3',
+        'dill>=0.2.2',
+        'ipywidgets'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Topic :: Database",
+        "Topic :: Database :: Database Engines/Servers",
+        "Operating System :: OS Independent"
+    ]
+)
