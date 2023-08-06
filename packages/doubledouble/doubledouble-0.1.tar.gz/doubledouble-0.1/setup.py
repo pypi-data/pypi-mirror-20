9458dd3249579181c5a309e5efd3d68a6147e94e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:17 +0000

    Add package doubledouble/0.1/doubledouble-0.1.tar.gz

---
+++ packages/doubledouble/doubledouble-0.1.tar.gz/doubledouble-0.1/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name = 'doubledouble',
+    description = 'Double-double aritmetic for Python',
+    py_modules = ['doubledouble'],
+    version = '0.1',
+    author = 'Juraj Sukop',
+    author_email = 'contact@xxyxyz.org',
+    url = 'https://github.com/sukop/doubledouble',
+    license = 'ISC',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules'])
