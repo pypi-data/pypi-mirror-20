b3a4aeff263ccc715f975cec9967549c94f6fd9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:46 +0000

    Add package webassets-webpack/0.1.0/webassets-webpack-0.1.0.tar.gz

---
+++ packages/webassets-webpack/webassets-webpack-0.1.0.tar.gz/webassets-webpack-0.1.0/webassets_webpack/tests/test_webpack.py
@@ -0,0 +1,45 @@
+from unittest import TestCase
+from distutils.spawn import find_executable
+from nose import SkipTest
+
+
+from webassets.filter import register_filter
+from webassets.test import TempEnvironmentHelper
+
+from webassets_webpack import Webpack
+
+register_filter(Webpack)
+
+
+class WebpackFilterTestCase(TempEnvironmentHelper, TestCase):
+
+    default_files = {
+        'main.js': """
+            import _ from 'lodash';
+
+            function component () {
+              var element = document.createElement('div');
+
+              /* lodash is required for the next line to work */
+              element.innerHTML = _.join(['Hello','webpack'], ' ');
+
+              return element;
+            }
+
+            document.body.appendChild(component());
+        """
+    }
+
+    def setUp(self):
+        super(WebpackFilterTestCase, self).setup()
+
+    def test_webpack_filter(self):
+        self.env.config['WEBPACK_BIN'] = './node_modules/.bin/webpack'
+        self.env.config['WEBPACK_CONFIG'] = './webpack.config.js'
+        self.env.config['WEBPACK_TEMP'] = 'temp.js'
+
+        # if not find_executable('webpack'):
+        #     raise SkipTest()
+
+        self.mkbundle('main.js', filters='webpack',
+                      output='bundle.js').build()
