d8eab85050675e27a215360e89f325f0da16da35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:46 +0000

    Add package glue-ginga/0.1.1/glue-ginga-0.1.1.tar.gz

---
+++ packages/glue-ginga/glue-ginga-0.1.1.tar.gz/glue-ginga-0.1.1/glue_ginga/__init__.py
@@ -0,0 +1,11 @@
+from .version import __version__
+
+def setup():
+    from .qt import mouse_modes
+    try:
+        from .qt.viewer_widget import GingaWidget
+    except ImportError:
+        raise ImportError("ginga is required")
+    else:
+        from glue.config import qt_client
+        qt_client.add(GingaWidget)
