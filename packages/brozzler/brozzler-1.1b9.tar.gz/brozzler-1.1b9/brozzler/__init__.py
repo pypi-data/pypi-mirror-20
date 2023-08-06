e686662855d9c6f73ae60ee716a1ab756c84d67f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:04 +0000

    Add package brozzler/1.1b9/brozzler-1.1b9.tar.gz

---
+++ packages/brozzler/brozzler-1.1b9.tar.gz/brozzler-1.1b9/brozzler/__init__.py
@@ -0,0 +1,171 @@
+"""
+brozzler/__init__.py - __init__.py for brozzler package, contains some common
+code
+
+Copyright (C) 2014-2016 Internet Archive
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from pkg_resources import get_distribution as _get_distribution
+__version__ = _get_distribution('brozzler').version
+
+class ShutdownRequested(Exception):
+    pass
+
+class NothingToClaim(Exception):
+    pass
+
+class CrawlJobStopped(Exception):
+    pass
+
+class ReachedLimit(Exception):
+    def __init__(self, http_error=None, warcprox_meta=None, http_payload=None):
+        import json
+        if http_error:
+            if "warcprox-meta" in http_error.headers:
+                self.warcprox_meta = json.loads(
+                        http_error.headers["warcprox-meta"])
+            else:
+                self.warcprox_meta = None
+            self.http_payload = http_error.read()
+        elif warcprox_meta:
+            self.warcprox_meta = warcprox_meta
+            self.http_payload = http_payload
+
+    def __repr__(self):
+        return "ReachedLimit(warcprox_meta=%s,http_payload=%s)" % (
+                repr(self.warcprox_meta), repr(self.http_payload))
+
+    def __str__(self):
+        return self.__repr__()
+
+# monkey-patch log level TRACE
+TRACE = 5
+import logging
+def _logging_trace(msg, *args, **kwargs):
+    logging.root.trace(msg, *args, **kwargs)
+def _logger_trace(self, msg, *args, **kwargs):
+    if self.isEnabledFor(TRACE):
+        self._log(TRACE, msg, args, **kwargs)
+logging.trace = _logging_trace
+logging.Logger.trace = _logger_trace
+logging._levelToName[TRACE] = 'TRACE'
+logging._nameToLevel['TRACE'] = TRACE
+
+_behaviors = None
+def behaviors():
+    import os, yaml, string
+    global _behaviors
+    if _behaviors is None:
+        behaviors_yaml = os.path.join(
+                os.path.dirname(__file__), 'behaviors.yaml')
+        with open(behaviors_yaml) as fin:
+            _behaviors = yaml.load(fin)
+    return _behaviors
+
+def behavior_script(url, template_parameters=None):
+    '''
+    Returns the javascript behavior string populated with template_parameters.
+    '''
+    import re, logging
+    for behavior in behaviors():
+        if re.match(behavior['url_regex'], url):
+            parameters = dict()
+            if 'default_parameters' in behavior:
+                parameters.update(behavior['default_parameters'])
+            if template_parameters:
+                parameters.update(template_parameters)
+            template = jinja2_environment().get_template(
+                    behavior['behavior_js_template'])
+            script = template.render(parameters)
+            logging.info(
+                    'using template=%s populated with parameters=%s for %s',
+                    repr(behavior['behavior_js_template']), parameters, url)
+            return script
+    return None
+
+def thread_raise(thread, exctype):
+    '''
+    Raises the exception exctype in the thread.
+
+    Adapted from http://tomerfiliba.com/recipes/Thread2/ which explains:
+    "The exception will be raised only when executing python bytecode. If your
+    thread calls a native/built-in blocking function, the exception will be
+    raised only when execution returns to the python code."
+    '''
+    import ctypes, inspect, threading
+    if not thread.is_alive():
+        raise threading.ThreadError('thread %s is not running' % thread)
+    if not inspect.isclass(exctype):
+        raise TypeError(
+                'cannot raise %s, only exception types can be raised (not '
+                'instances)' % exc_type)
+    res = ctypes.pythonapi.PyThreadState_SetAsyncExc(
+            ctypes.c_long(thread.ident), ctypes.py_object(exctype))
+    if res == 0:
+        raise ValueError('invalid thread id? thread.ident=%s' % thread.ident)
+    elif res != 1:
+        # if it returns a number greater than one, you're in trouble,
+        # and you should call it again with exc=NULL to revert the effect
+        ctypes.pythonapi.PyThreadState_SetAsyncExc(thread.ident, 0)
+        raise SystemError('PyThreadState_SetAsyncExc failed')
+
+def sleep(duration):
+    '''
+    Sleeps for duration seconds in increments of 0.5 seconds.
+
+    Use this so that the sleep can be interrupted by thread_raise().
+    '''
+    import time
+    start = time.time()
+    while True:
+        elapsed = time.time() - start
+        if elapsed >= duration:
+            break
+        time.sleep(min(duration - elapsed, 0.5))
+
+_jinja2_env = None
+def jinja2_environment():
+    global _jinja2_env
+    if not _jinja2_env:
+        import jinja2, json
+        _jinja2_env = jinja2.Environment(
+                loader=jinja2.PackageLoader('brozzler', 'js-templates'))
+        _jinja2_env.filters['json'] = json.dumps
+    return _jinja2_env
+
+import urlcanon
+def _remove_query(url):
+    url.question_mark = b''
+    url.query = b''
+# XXX chop off path after last slash??
+site_surt_canon = urlcanon.Canonicalizer(
+        urlcanon.semantic.steps + [_remove_query])
+
+import doublethink
+import datetime
+EPOCH_UTC = datetime.datetime.utcfromtimestamp(0.0).replace(
+        tzinfo=doublethink.UTC)
+
+from brozzler.site import Page, Site
+from brozzler.worker import BrozzlerWorker
+from brozzler.robots import is_permitted_by_robots
+from brozzler.frontier import RethinkDbFrontier
+from brozzler.browser import Browser, BrowserPool, BrowsingException
+from brozzler.job import new_job, new_site, Job
+from brozzler.cli import suggest_default_chrome_exe
+
+__all__ = ['Page', 'Site', 'BrozzlerWorker', 'is_permitted_by_robots',
+           'RethinkDbFrontier', 'Browser', 'BrowserPool', 'BrowsingException',
+           'new_job', 'new_site', 'Job']
---
+++ packages/brozzler/brozzler-1.1b9.tar.gz/brozzler-1.1b9/brozzler/site.py
@@ -0,0 +1,182 @@
+'''
+brozzler/site.py - classes representing sites and pages
+
+Copyright (C) 2014-2017 Internet Archive
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+
+import urlcanon
+import json
+import logging
+import brozzler
+import hashlib
+import time
+import doublethink
+import datetime
+import re
+
+class Site(doublethink.Document):
+    logger = logging.getLogger(__module__ + "." + __qualname__)
+    table = 'sites'
+
+    def populate_defaults(self):
+        if not "status" in self:
+            self.status = "ACTIVE"
+        if not "claimed" in self:
+            self.claimed = False
+        if not "last_disclaimed" in self:
+            self.last_disclaimed = brozzler.EPOCH_UTC
+        if not "last_claimed" in self:
+            self.last_claimed = brozzler.EPOCH_UTC
+        if not "scope" in self:
+            self.scope = {}
+        if not "surt" in self.scope and self.seed:
+            self.scope["surt"] = brozzler.site_surt_canon(
+                    self.seed).surt().decode('ascii')
+
+        if not "starts_and_stops" in self:
+            if self.get("start_time"):   # backward compatibility
+                self.starts_and_stops = [{
+                    "start":self.get("start_time"),"stop":None}]
+                if self.get("status") != "ACTIVE":
+                    self.starts_and_stops[0]["stop"] = self.last_disclaimed
+                del self["start_time"]
+            else:
+                self.starts_and_stops = [
+                        {"start":doublethink.utcnow(),"stop":None}]
+
+    def __str__(self):
+        return 'Site({"id":"%s","seed":"%s",...})' % (self.id, self.seed)
+
+    def elapsed(self):
+        '''Returns elapsed crawl time as a float in seconds.'''
+        dt = 0
+        for ss in self.starts_and_stops[:-1]:
+            dt += (ss['stop'] - ss['start']).total_seconds()
+        ss = self.starts_and_stops[-1]
+        if ss['stop']:
+            dt += (ss['stop'] - ss['start']).total_seconds()
+        else: # crawl is active
+            dt += (doublethink.utcnow() - ss['start']).total_seconds()
+        return dt
+
+    def note_seed_redirect(self, url):
+        new_scope_surt = brozzler.site_surt_canon(url).surt().decode("ascii")
+        if not new_scope_surt.startswith(self.scope["surt"]):
+            self.logger.info("changing site scope surt from {} to {}".format(
+                self.scope["surt"], new_scope_surt))
+            self.scope["surt"] = new_scope_surt
+
+    def extra_headers(self):
+        hdrs = {}
+        if self.enable_warcprox_features and self.warcprox_meta:
+            hdrs["Warcprox-Meta"] = json.dumps(
+                    self.warcprox_meta, separators=(',', ':'))
+        return hdrs
+
+    def is_in_scope(self, url, parent_page=None):
+        if not isinstance(url, urlcanon.ParsedUrl):
+            url = urlcanon.semantic(url)
+        try_parent_urls = []
+        if parent_page:
+            try_parent_urls.append(urlcanon.semantic(parent_page.url))
+            if parent_page.redirect_url:
+                try_parent_urls.append(
+                        urlcanon.semantic(parent_page.redirect_url))
+
+        might_accept = False
+        if not url.scheme in (b'http', b'https'):
+            # XXX doesn't belong here maybe (where? worker ignores unknown
+            # schemes?)
+            return False
+        elif (parent_page and "max_hops" in self.scope
+                and parent_page.hops_from_seed >= self.scope["max_hops"]):
+            pass
+        elif url.surt().startswith(self.scope["surt"].encode("utf-8")):
+            might_accept = True
+        elif parent_page and parent_page.hops_off_surt < self.scope.get(
+                "max_hops_off_surt", 0):
+            might_accept = True
+        elif "accepts" in self.scope:
+            for accept_rule in self.scope["accepts"]:
+                rule = urlcanon.MatchRule(**accept_rule)
+                if try_parent_urls:
+                    for parent_url in try_parent_urls:
+                        if rule.applies(url, parent_url):
+                           might_accept = True
+                else:
+                    if rule.applies(url):
+                        might_accept = True
+
+        if might_accept:
+            if "blocks" in self.scope:
+                for block_rule in self.scope["blocks"]:
+                    rule = urlcanon.MatchRule(**block_rule)
+                    if try_parent_urls:
+                        for parent_url in try_parent_urls:
+                            if rule.applies(url, parent_url):
+                               return False
+                    else:
+                        if rule.applies(url):
+                            return False
+            return True
+        else:
+            return False
+
+class Page(doublethink.Document):
+    logger = logging.getLogger(__module__ + "." + __qualname__)
+    table = "pages"
+
+    @staticmethod
+    def compute_id(site_id, url):
+        digest_this = "site_id:%s,url:%s" % (site_id, url)
+        return hashlib.sha1(digest_this.encode("utf-8")).hexdigest()
+
+    def populate_defaults(self):
+        if not "hops_from_seed" in self:
+            self.hops_from_seed = 0
+        if not "brozzle_count" in self:
+            self.brozzle_count = 0
+        if not "claimed" in self:
+            self.claimed = False
+        if not "hops_off_surt" in self:
+            self.hops_off_surt = 0
+        if not "needs_robots_check" in self:
+            self.needs_robots_check = False
+        if not "priority" in self:
+            self.priority = self._calc_priority()
+        if not "id" in self:
+            self.id = self.compute_id(self.site_id, self.url)
+
+    def __str__(self):
+        return 'Page({"id":"%s","url":"%s",...})' % (self.id, self.url)
+
+    def note_redirect(self, url):
+        self.redirect_url = url
+
+    def _calc_priority(self):
+        if not self.url:
+            return None
+        priority = 0
+        priority += max(0, 10 - self.hops_from_seed)
+        priority += max(0, 6 - self.canon_url().count("/"))
+        return priority
+
+    def canon_url(self):
+        if not self.url:
+            return None
+        if self._canon_hurl is None:
+            self._canon_hurl = urlcanon.semantic(self.url)
+        return str(self._canon_hurl)
+
