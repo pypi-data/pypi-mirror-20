dc93e668bb29bae41bfb131579eb7042af24e47f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:08 +0000

    Add package Subury/1.0.1/Subury-1.0.1.tar.gz

---
+++ packages/Subury/Subury-1.0.1.tar.gz/Subury-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name='Subury',
+        version='1.0.1',
+        py_modules=['Subury_Print_lol'],
+        author='Subury',
+        author_email='1446849325@qq.com',
+        url='http://www.surbury.me',
+        description='A simple printer of nested lists',
+    )
