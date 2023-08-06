6163db85b3c336bb565c7287c97714fb20c1ba50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:57 +0000

    Add package flask-graphql-subscriptions-transport/0.1.4/flask_graphql_subscriptions_transport-0.1.4.tar.gz

---
+++ packages/flask-graphql-subscriptions-transport/flask_graphql_subscriptions_transport-0.1.4.tar.gz/flask_graphql_subscriptions_transport-0.1.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+required_packages = [
+    'flask_socketio',
+    'flask',
+]
+
+setup(name='flask_graphql_subscriptions_transport',
+      version='0.1.4',
+      description='Adds subscription transport layer for Flask applications using GraphQL',
+      url='https://github.com/willdeuschle/Flask-GraphQL-Subscriptions-Transport',
+      author='Will Deuschle',
+      author_email='wjdeuschle@gmail.com',
+      license='MIT',
+      packages=['flask_graphql_subscriptions_transport'],
+      install_requires=required_packages,
+      tests_require=['pytest'],
+      zip_safe=False)
