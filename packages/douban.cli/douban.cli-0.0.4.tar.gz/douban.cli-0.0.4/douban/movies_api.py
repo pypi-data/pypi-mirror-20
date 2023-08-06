47bbea4cdbe1fdda9dd96cbd7bee72c0e43bd37f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:55 +0000

    Add package douban.cli/0.0.4/douban.cli-0.0.4.tar.gz

---
+++ packages/douban.cli/douban.cli-0.0.4.tar.gz/douban.cli-0.0.4/douban/movies_api.py
@@ -0,0 +1,16 @@
+#-*- encoding: UTF-8 -*-
+import requests
+
+class MoviesAPI:
+
+    def __init__(self):
+        self.base_url = 'https://api.douban.com/v2/movie/'
+
+    # dic_obj = {'q':'movie_name', 'tag': 'movie_tag', 'start': offset, 'count': 100}
+    def search_movie(self, query_dic):
+        url = self.base_url + 'search'
+        movies = self.get(url=url, query=query_dic)
+        return movies
+
+    def get(self, url, query={}):
+        return requests.get(url, params=query).json()
