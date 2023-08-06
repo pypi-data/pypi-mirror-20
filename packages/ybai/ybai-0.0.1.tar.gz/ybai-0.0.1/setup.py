247afb24b1e013f79ac3158671856c00ffaed6b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:13 +0000

    Add package ybai/0.0.1/ybai-0.0.1.tar.gz

---
+++ packages/ybai/ybai-0.0.1.tar.gz/ybai-0.0.1/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import re
+
+from setuptools import find_packages, setup
+
+with open('ybai/__init__.py', encoding='utf-8') as fp:
+    version = re.search(r"__version__\s*=\s*'([\w\-.]+)'", fp.read()).group(1)
+
+with open('README.md', encoding='utf-8') as fp:
+    readme = fp.read()
+
+setup(
+    name='ybai',
+    version=version,
+    packages=find_packages(),
+    package_data={
+        '': ['*.md'],
+    },
+    include_package_data=True,
+    entry_points={
+            'console_scripts': [
+                'ybai = ybai.ybai:man'
+            ]
+        },
+    install_requires=[
+        'itchat>=1.2.32',
+        'requests',
+    ],
+    url='https://github.com/mengzhu716/ybai',
+    license='MIT',
+    author='Yinbing',
+    author_email='380711712@qq.com',
+    description='个人测试AI',
+    long_description=readme,
+    keywords=[
+        'AI',
+        'WeChat',
+        'API'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Topic :: Communications :: Chat',
+        'Topic :: Utilities',
+    ]
+)
---
+++ packages/ybai/ybai-0.0.1.tar.gz/ybai-0.0.1/ybai/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+"""
+
+
+注释
+
+
+"""
+import sys
+
+
+__title__ = 'ybai'
+__version__ = '0.0.1'
+__author__ = 'Yinbing'
+__license__ = 'MIT'
+__copyright__ = '2017, Yinbing'
+
+version_details = 'ybai {ver} from {path} (python {pv.major}.{pv.minor}.{pv.micro})'.format(
+    ver=__version__, path=__path__[0], pv=sys.version_info)
