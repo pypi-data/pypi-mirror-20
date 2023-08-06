f1a34976c66ca2e1d804f269c1903dc27f451411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:41 +0000

    Add package closer/0.2.1/closer-0.2.1.tar.gz

---
+++ packages/closer/closer-0.2.1.tar.gz/closer-0.2.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+README = 'close remote SSH processes automatically'
+
+requires = [ 'psutil' ]
+tests_require = []
+
+setup(name='closer',
+      version='0.2.1',
+      description=README,
+      long_description=README,
+      url='https://github.com/haarcuba/closer',
+      classifiers=[
+          "Programming Language :: Python",
+      ],
+      author='Yoav Kleinberger',
+      author_email='haarcuba@gmail.com',
+      keywords='subprocess',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      extras_require={
+          'testing': tests_require,
+      },
+      install_requires=requires,
+      entry_points="""\
+      [console_scripts]
+      closer = closer.closer:main
+      """,
+      )
