6c2696e11b33cf61f92b42afd9af47b4ef2230a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:44 +0000

    Add package sndict/0.1.1/sndict-0.1.1.linux-x86_64.tar.gz

---
+++ packages/sndict/sndict-0.1.1.linux-x86_64.tar.gz/usr/local/lib/python2.7/dist-packages/sndict/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '0.1.1'
+
+from nesteddict import NestedDict, ndict
+from structurednesteddict import StructuredNestedDict, sndict
+
+__all__ = (
+    'ndict', 'NestedDict',
+    'sn_dict', 'StructuredNestedDict',
+)
