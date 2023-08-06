d87df4122c10f9dbc7b7599048ba0dfa2118d78c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:40 +0000

    Add package habitica-todo/1.3/habitica-todo-1.3.tar.gz

---
+++ packages/habitica-todo/habitica-todo-1.3.tar.gz/habitica-todo-1.3/habitica-todo/__init__.py
@@ -0,0 +1,7 @@
+""" Habitica-todo 
+
+	Python scripts that should convert and sync tasks between Habitica and Todoist.
+"""
+
+from .api import *
+from .main import *
---
+++ packages/habitica-todo/habitica-todo-1.3.tar.gz/habitica-todo-1.3/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='habitica-todo',
+    version='1.3',
+    url='https://github.com/eringiglio/Habitica-todo',
+    description='An API app for syncing todoist and habitica tasks',
+    packages=['habitica-todo'],
+    zip_safe=False)
