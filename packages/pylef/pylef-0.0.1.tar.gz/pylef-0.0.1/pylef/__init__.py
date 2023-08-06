016a62bdacdd4020d0a43d3b434352f1430098ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:39 +0000

    Add package pylef/0.0.1/pylef-0.0.1.tar.gz

---
+++ packages/pylef/pylef-0.0.1.tar.gz/pylef-0.0.1/pylef/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+#####################################
+##
+## 
+##
+##
+##
+##
+##
+##
+##
+##
+##
+##
+##
+
+##################################
+###### methods needed #######
+#import visa   # interface with NI-Visa
+#import numpy as np # numpy for array manipulation
+#import os   # module for general OS manipulation
+#import time # module for time related funtions
+#import pandas as pd # module for general data analysis
+   
+## import instrument modules
+from .scope import TektronixTBS1062
+from .generator import BK4052
