4fb1eae7e59f57c7ea4a041518e6a9ceeb3ec6b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:33 +0000

    Add package plus1s/1.1.3/plus1s-1.1.3.tar.gz

---
+++ packages/plus1s/plus1s-1.1.3.tar.gz/plus1s-1.1.3/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='plus1s',
+      version='1.1.3',
+      description='Are you a real fans?',
+      author='DZ Chan',
+      author_email='cdzos97@gmail.com',
+      packages = ['plus1s'],
+      # py_modules=['plus1s'],
+      scripts=['bin/plus1s'],
+      package_data={
+            'plus1s': ['resources/*.txt', 'resources/images/*.jpg']
+      },
+      install_requires=[
+            'xtermcolor',
+            'Pillow',
+      ],
+      url='https://github.com/ChannelOne/plus1s',
+      license='LGPL-3.0',
+      )
