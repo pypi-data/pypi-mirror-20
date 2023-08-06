03878c5704bc6a5cbe5a336d8246d8e3f27a259e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:39 +0000

    Add package gidgethub/1.0.0.post1/gidgethub-1.0.0.post1-py3-none-any.whl

---
+++ packages/gidgethub/gidgethub-1.0.0.post1-py3-none-any.whl/gidgethub/test/test_aiohttp.py
@@ -0,0 +1,35 @@
+import datetime
+
+import aiohttp
+import pytest
+
+from .. import aiohttp as gh_aiohttp
+from .. import sansio
+
+
+async def call_aiohttp(what, *args, **kwargs):
+    async with aiohttp.ClientSession() as session:
+        gh = gh_aiohttp.GitHubAPI(session, "gidgethub")
+        return await getattr(gh, what)(*args, **kwargs)
+
+
+@pytest.mark.asyncio
+async def test_sleep():
+    delay = 1
+    start = datetime.datetime.now()
+    async with aiohttp.ClientSession() as session:
+        gh = gh_aiohttp.GitHubAPI(session, "gidgethub")
+        await gh._sleep(delay)
+    stop = datetime.datetime.now()
+    assert (stop - start) > datetime.timedelta(seconds=delay)
+
+
+@pytest.mark.asyncio
+async def test_request():
+    request_headers = sansio.create_headers("gidgethub")
+    async with aiohttp.ClientSession() as session:
+        gh = gh_aiohttp.GitHubAPI(session, "gidgethub")
+        aio_call = await gh._request("GET", "https://api.github.com/rate_limit",
+                                     request_headers)
+    data, rate_limit, _ = sansio.decipher_response(*aio_call)
+    assert "rate" in data
