8124f02ae2035b09dcb9d2126e4ce01c6f0d7ac9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:58 +0000

    Add package takumi-client/0.1.1/takumi_client-0.1.1-py2.py3-none-any.whl

---
+++ packages/takumi-client/takumi_client-0.1.1-py2.py3-none-any.whl/takumi_client/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+takumi_client
+~~~~~~~~~~~~~
+
+Thrift client pool implementation for Takumi.
+"""
+
+from .pool import clients
+
+__version__ = '0.1.1'
+__all__ = ['clients']
