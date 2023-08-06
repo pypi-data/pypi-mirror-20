5ab7b839bad0166160beb2b30d99cc84ca476eff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:49 +0000

    Add package friskby/0.61.0/friskby-0.61.0.tar.gz

---
+++ packages/friskby/friskby-0.61.0.tar.gz/friskby-0.61.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='friskby',
+    version='0.61.0',
+    description='The friskby module',
+    url='http://github.com/FriskbyBergen/friskby',
+    author='Friskby Bergen',
+    author_email='pgdr@statoil.com',
+    license='GPL v3',
+    packages=['friskby'],
+    zip_safe=False
+)
