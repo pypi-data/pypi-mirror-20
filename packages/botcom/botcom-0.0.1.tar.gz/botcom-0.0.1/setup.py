74dec7215d6fc26ed50da59460f140b649843792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:34 +0000

    Add package botcom/0.0.1/botcom-0.0.1.tar.gz

---
+++ packages/botcom/botcom-0.0.1.tar.gz/botcom-0.0.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(
+    name='botcom',
+    version='0.0.1',
+    description='command bus',
+    long_description=readme(),
+    author='Boris Ostretsov',
+    license='MIT',
+    author_email='ostrbor@gmail.com',
+    keywords='bot command bus',
+    url='https://github.com/ostrbor/botcom',
+    packages=['botcom'], )
