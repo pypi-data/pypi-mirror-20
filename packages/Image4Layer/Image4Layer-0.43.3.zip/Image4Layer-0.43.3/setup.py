2c0f32b8cb8d2ce5133fd1be159429709dbe4c50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:02 +0000

    Add package Image4Layer/0.43.3/Image4Layer-0.43.3.zip

---
+++ packages/Image4Layer/Image4Layer-0.43.3.zip/Image4Layer-0.43.3/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+from image4layer import __version__
+
+description = open("README.txt").read()
+
+setup(
+    name='Image4Layer',
+    version=__version__,
+    packages=['image4layer'],
+    package_dir={'image4layer': 'image4layer'},
+    url='https://github.com/pashango2/Image4Layer',
+    license='MIT',
+    author='Toshiyuki Ishii',
+    author_email='pashango2@gmail.com',
+    description=description
+)
