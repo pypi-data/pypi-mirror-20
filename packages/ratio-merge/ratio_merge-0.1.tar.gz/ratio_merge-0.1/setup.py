a3da82efe3f660815c4bcad024f2176ac5abf002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:37 +0000

    Add package ratio-merge/0.1/ratio_merge-0.1.tar.gz

---
+++ packages/ratio-merge/ratio_merge-0.1.tar.gz/ratio_merge-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'ratio_merge',
+  packages = ['ratio_merge'],
+  version = '0.1',
+  description = 'A small utility function for merging two lists by some ratio',
+  author = 'Adam Lev-Libfeld',
+  author_email = 'adam@tamarlabs.com',
+  url = 'https://github.com/daTokenizer/ratio-merge-python',
+  download_url = 'https://github.com/daTokenizer/ratio-merge-python/archive/0.1.tar.gz',
+  keywords = ['merge', 'ratio', 'lists'], # arbitrary keywords
+  classifiers = [],
+)
