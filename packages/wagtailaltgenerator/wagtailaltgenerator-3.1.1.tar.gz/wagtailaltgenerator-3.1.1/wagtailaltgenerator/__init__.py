1acc4fe2bc2af6de6ccdf5362d23d04cc9cda036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:53 +0000

    Add package wagtailaltgenerator/3.1.1/wagtailaltgenerator-3.1.1.tar.gz

---
+++ packages/wagtailaltgenerator/wagtailaltgenerator-3.1.1.tar.gz/wagtailaltgenerator-3.1.1/wagtailaltgenerator/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+wagtailaltgenerator
+----------
+Insert image description and tags with the help of computer vision
+"""
+
+__title__ = "wagtailaltgenerator"
+__version__ = '3.1.1'
+__build__ = 311
+__author__ = "Martin Sandström"
+__license__ = "MIT"
+__copyright__ = "Copyright 2016 Martin Sandström"
+
+default_app_config = 'wagtailaltgenerator.apps.AltGeneratorAppConfig'
