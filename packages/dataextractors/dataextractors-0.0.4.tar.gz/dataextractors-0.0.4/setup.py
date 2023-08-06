02fb97c68360f03d90113f56ac4f27dbc9b34111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:05 +0000

    Add package dataextractors/0.0.4/dataextractors-0.0.4.tar.gz

---
+++ packages/dataextractors/dataextractors-0.0.4.tar.gz/dataextractors-0.0.4/setup.py
@@ -0,0 +1,33 @@
+import sys
+from setuptools import setup, find_packages
+
+open_kwds = {}
+if sys.version_info > (3,):
+    open_kwds['encoding'] = 'utf-8'
+
+# with open('README.md', **open_kwds) as f:
+#     readme = f.read()
+
+# long_description=readme,
+
+setup(name='dataextractors',
+      version='0.0.4',
+      description='Extract pixels and metadata using shapefiles and georeferenced imagery.',
+      classifiers=[],
+      keywords='',
+      author='Nikki Aldeborgh',
+      author_email='nikki.aldeborgh@digitalglobe.com',
+      url='https://github.com/platformstories/dataextractors',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['geoio >= 1.1.1',
+                        'geojson >= 1.3.2',
+                        'geojsontools >= 0.0.2',
+                        'numpy >= 1.12.0',
+                        'scipy',
+                        'Pillow == 2.6.0',
+                        'bumpversion'
+                        ]
+      )
