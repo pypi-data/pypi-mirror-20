ccf903da37ceab6639b3c6751e960f6f911ded8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:43 +0000

    Add package tmper/0.5.3/tmper-0.5.4.tar.gz

---
+++ packages/tmper/tmper-0.5.4.tar.gz/tmper-0.5.4/setup.py
@@ -0,0 +1,40 @@
+import os
+import re
+import glob
+from setuptools import setup
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+try:
+    readme = read('README.md')
+except IOError as e:
+    readme = ''
+
+templates = glob.glob('templates/*')
+
+setup(name='tmper',
+      license='MIT License',
+      author='Matt Bierbaum',
+      url='https://github.com/mattbierbaum/tmper',
+      version='0.5.4',
+
+      install_requires=[
+          "tornado>=4.3",
+          "parsedatetime>=2.1",
+          "bcrypt>=3.1"
+        ],
+      packages=['tmper'],
+      entry_points={
+        'console_scripts': [
+            'tmper = tmper.__main__:main'
+        ]
+      },
+      data_files=[('templates', templates)],
+      package_data={'': ['README.md']},
+
+      platforms='osx, posix, linux, windows',
+      description='Temporary file sharing using simple two digit codes.',
+      long_description=readme,
+      zip_safe=False,
+)
