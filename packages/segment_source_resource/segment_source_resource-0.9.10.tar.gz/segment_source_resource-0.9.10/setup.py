58c2761eb2452a3f695b98de33b5eaf905f7d913
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:18 +0000

    Add package segment_source_resource/0.9.10/segment_source_resource-0.9.10.tar.gz

---
+++ packages/segment_source_resource/segment_source_resource-0.9.10.tar.gz/segment_source_resource-0.9.10/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+setup(
+    name='segment_source_resource',
+    packages=['segment_source_resource'],
+    version='0.9.10',
+    description='Abstraction to make sources easier to write',
+    author='Segment',
+    author_email='friends@segment.com',
+    url='https://github.com/segmentio/python-source-resource',
+    install_requires=[
+        'pydash==3.4.3',
+        'gevent==1.2.1',
+        'python-dateutil==2.6.0',
+        'segment_source>0.9'
+    ],
+    tests_require=['pytest'],
+    setup_requires=['pytest-runner'],
+)
