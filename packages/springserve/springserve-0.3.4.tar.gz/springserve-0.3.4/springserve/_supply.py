9b9b729bd89a4f58f3d25793daea15703472223c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:23 +0000

    Add package springserve/0.3.4/springserve-0.3.4.tar.gz

---
+++ packages/springserve/springserve-0.3.4.tar.gz/springserve-0.3.4/springserve/_supply.py
@@ -0,0 +1,18 @@
+
+from . import _VDAPIService
+
+class _SupplyTagAPI(_VDAPIService):
+
+    __API__ = "supply_tags"
+
+
+class _SupplyPartnerAPI(_VDAPIService):
+
+    __API__ = "supply_partners"
+
+class _SupplyGroupAPI(_VDAPIService):
+
+    __API__ = "supply_groups"
+
+
+
