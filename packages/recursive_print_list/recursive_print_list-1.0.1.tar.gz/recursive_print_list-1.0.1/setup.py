98cd59d1951935f87d881a4ffc8152bcb19b6862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:23 +0000

    Add package recursive_print_list/1.0.1/recursive_print_list-1.0.1.tar.gz

---
+++ packages/recursive_print_list/recursive_print_list-1.0.1.tar.gz/recursive_print_list-1.0.1/setup.py
@@ -0,0 +1,15 @@
+#encoding:utf-8
+#从python发布工具导入setup函数
+from distutils.core import setup
+
+setup(
+	name		= 'recursive_print_list',
+	version 	= '1.0.1',
+#将模块的元数据与setup函数的参数关联
+	py_modules	= ['recursive_print_list'],
+	author		=  'stephanie',
+	author_email	=  '13663788159@163.com',
+#	url		= 'http://',
+	description 	= 'A simple recursiving print list item fun',
+
+	)
