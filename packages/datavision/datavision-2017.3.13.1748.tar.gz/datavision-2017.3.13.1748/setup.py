7c41d69a4fb73106747dc5b291a76eb91f1014c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:22 +0000

    Add package datavision/2017.3.13.1748/datavision-2017.3.13.1748.tar.gz

---
+++ packages/datavision/datavision-2017.3.13.1748.tar.gz/datavision-2017.3.13.1748/setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+
+import setuptools
+
+def main():
+
+    setuptools.setup(
+        name             = "datavision",
+        version          = "2017.03.13.1748",
+        description      = "Python data visualisation",
+        long_description = long_description(),
+        url              = "https://github.com/wdbm/datavision",
+        author           = "Will Breaden Madden",
+        author_email     = "wbm@protonmail.ch",
+        license          = "GPLv3",
+        py_modules       = [
+                           "datavision"
+                           ],
+        install_requires = [
+                           "dataset",
+                           "matplotlib",
+                           "numpy",
+                           "Pillow",
+                           "pygame",
+                           "pyprel",
+                           "scipy",
+                           "shijian"
+                           ],
+        scripts          = [
+                           "view_database_SQLite.py"
+                           ],
+        entry_points     = """
+            [console_scripts]
+            datavision = datavision:datavision
+        """
+    )
+
+def long_description(
+    filename = "README.md"
+    ):
+
+    if os.path.isfile(os.path.expandvars(filename)):
+        try:
+            import pypandoc
+            long_description = pypandoc.convert_file(filename, "rst")
+        except ImportError:
+            long_description = open(filename).read()
+    else:
+        long_description = ""
+    return long_description
+
+if __name__ == "__main__":
+    main()
