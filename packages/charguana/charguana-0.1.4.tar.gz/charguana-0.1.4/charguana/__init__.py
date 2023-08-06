dd7f8253ae7ebc98d908c54361ecf43f4a1d55fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:41 +0000

    Add package charguana/0.1.4/charguana-0.1.4.tar.gz

---
+++ packages/charguana/charguana-0.1.4.tar.gz/charguana-0.1.4/charguana/__init__.py
@@ -0,0 +1,1 @@
+import charguana.cjk
---
+++ packages/charguana/charguana-0.1.4.tar.gz/charguana-0.1.4/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'charguana',
+  packages = ['charguana'], 
+  version = '0.1.4',
+  description = 'A character vommitting library.',
+  author = 'Liling Tan',
+  license = 'MIT',
+  url = 'https://github.com/alvations/charguana',
+  download_url = 'https://github.com/alvations/charguana/tarball/0.1.3', 
+  keywords = ['unicode', 'character', 'vommit', 'linguistics' ], 
+  classifiers = [],
+)
