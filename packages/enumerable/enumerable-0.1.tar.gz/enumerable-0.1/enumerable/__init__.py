eb65a8a1e677e128f1d8c5f2d451146715ad9354
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:34 +0000

    Add package enumerable/0.1/enumerable-0.1.tar.gz

---
+++ packages/enumerable/enumerable-0.1.tar.gz/enumerable-0.1/enumerable/__init__.py
@@ -0,0 +1,7 @@
+from . import iterators
+
+
+map = iterators.Map
+filter = iterators.Filter
+reduce = iterators.Reduce
+grep = iterators.Grep
---
+++ packages/enumerable/enumerable-0.1.tar.gz/enumerable-0.1/enumerable/iterators.py
@@ -0,0 +1,60 @@
+import functools
+from re import search
+
+
+def Map():
+    pass
+
+
+def Filter():
+    pass
+
+
+def Reduce():
+    pass
+
+
+def Grep():
+    pass
+
+
+class Enumerable:
+    def __init__(self, iterable):
+        self._iterable = iterable
+
+    def __iter__(self):
+        return self._iterable.__iter__()
+
+    def filter(self, func, *args, **kwargs):
+        return Filter(self._iterable, func, *args, **kwargs)
+
+    def grep(self, pattern):
+        return Grep(self._iterable, pattern)
+
+    def map(self, func, *args, **kwargs):
+        return Map(self._iterable, func, *args, **kwargs)
+
+    def reduce(self, func, *args, **kwargs):
+        return Reduce(self._iterable, func, *args, **kwargs)
+
+    def to(self, container_type):
+        return container_type(self._iterable)
+
+
+def Filter(iterable, func, *args, **kwargs):
+    return Enumerable((x for x in iterable if func(x, *args, **kwargs)))
+
+
+def Grep(iterable, pattern):
+    return Filter(iterable, lambda x: True if search(pattern, x) else False)
+
+
+def Map(iterable, func, *args, **kwargs):
+    return Enumerable((func(x, *args, **kwargs) for x in iterable))
+
+
+def Reduce(iterable, func, initializer=0, *args, **kwargs):
+    def reduce_func(accumulator, value):
+        return func(accumulator, value, *args, **kwargs)
+
+    return functools.reduce(reduce_func, iterable, initializer)
