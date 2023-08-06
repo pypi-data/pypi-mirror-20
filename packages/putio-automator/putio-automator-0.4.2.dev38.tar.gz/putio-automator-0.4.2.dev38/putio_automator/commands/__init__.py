8bf5d3bc0d688bee149029d5352194dd19746647
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:00 +0000

    Add package putio-automator/0.4.2.dev38/putio-automator-0.4.2.dev38.tar.gz

---
+++ packages/putio-automator/putio-automator-0.4.2.dev38.tar.gz/putio-automator-0.4.2.dev38/putio_automator/commands/__init__.py
@@ -0,0 +1,10 @@
+"""
+Flask commands
+"""
+import putio_automator.commands.account
+import putio_automator.commands.config
+import putio_automator.commands.db
+import putio_automator.commands.docker
+import putio_automator.commands.files
+import putio_automator.commands.torrents
+import putio_automator.commands.transfers
