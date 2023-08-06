1ba34d70eeffd6a97a754598e000df280a042484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:02 +0000

    Add package topser/0.2/topser-0.2.tar.gz

---
+++ packages/topser/topser-0.2.tar.gz/topser-0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+def readme():
+      with open('README.rst') as f:
+            return f.read()
+
+setup(name='topser',
+      version='0.2',
+      description='The simple package',
+      long_description=readme(),
+      classifiers=[
+            'Development Status :: 3 - Alpha',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3.4',
+            'Topic :: Text Processing :: Linguistic',
+      ],
+      url='http://github.com/vitakrya/topser',
+      author='Vita Kr',
+      author_email='vinder_2@mail.ru',
+      license='MIT',
+      packages=['topser'],
+      install_requires=[
+            'markdown',
+      ],
+      include_package_data=True,
+      scripts=['bin/topser-pont'],
+      zip_safe=False,
+      test_suite='nose.collector',
+      test_require=['nose'],)
---
+++ packages/topser/topser-0.2.tar.gz/topser-0.2/topser/__init__.py
@@ -0,0 +1,1 @@
+from .pont import pont
