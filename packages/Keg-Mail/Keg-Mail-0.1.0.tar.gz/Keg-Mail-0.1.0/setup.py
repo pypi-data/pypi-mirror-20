f3e0be5d0bdea3573e673ba19f8176d3545ec490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:22 +0000

    Add package Keg-Mail/0.1.0/Keg-Mail-0.1.0.tar.gz

---
+++ packages/Keg-Mail/Keg-Mail-0.1.0.tar.gz/Keg-Mail-0.1.0/setup.py
@@ -0,0 +1,32 @@
+import os.path as osp
+from setuptools import setup, find_packages
+
+
+cdir = osp.abspath(osp.dirname(__file__))
+README = open(osp.join(cdir, 'README.rst')).read()
+CHANGELOG = open(osp.join(cdir, 'CHANGELOG.rst')).read()
+
+setup(
+    name="Keg-Mail",
+    setup_requires=['setuptools_scm'],
+    use_scm_version=True,
+    description="A mail sending library for keg applications",
+    long_description='\n\n'.join((README, CHANGELOG)),
+    author="Level 12 Developers",
+    author_email="devteam@level12.io",
+    url='https://github.com/level12/keg-mail',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    license='BSD',
+    packages=find_packages(),
+    zip_safe=True,
+    install_requires=[
+        'flask_mail',
+    ],
+)
