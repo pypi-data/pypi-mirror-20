dd870c051a38b8136800bbc64a0fac94a3650e20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:21 +0000

    Add package dynarray/0.1.3/dynarray-0.1.3.tar.gz

---
+++ packages/dynarray/dynarray-0.1.3.tar.gz/dynarray-0.1.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='dynarray',
+    version='0.1.3',
+    description='Dynamically growable numpy arrays.',
+    url='https://github.com/maciejkula/dynarray',
+    download_url='https://github.com/maciejkula/dynarray/tarball/0.1.3',
+    packages=['dynarray'],
+    install_requires=['numpy'],
+    tests_require=['pytest', 'hypothesis[numpy]'],
+    author='Maciej Kula',
+    license='MIT',
+    classifiers=['Development Status :: 3 - Alpha',
+                 'License :: OSI Approved :: MIT License']
+)
