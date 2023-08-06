b6d27ede625f1fe192a43802bda2cc31f354bfcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:59 +0000

    Add package addict/2.1.0/addict-2.1.0.tar.gz

---
+++ packages/addict/addict-2.1.0.tar.gz/addict-2.1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+import addict
+
+SHORT='Addict is a dictionary whose items can be set using both attribute and item syntax.'
+LONG=('Addict is a module that exposes a dictionary subclass that allows items to be set like attributes. '
+     'Values are gettable and settable using both attribute and item syntax. '
+     'For more info check out the README at \'github.com/mewwts/addict\'.')
+
+setup(
+    name='addict',
+    version=addict.__version__,
+    packages=['addict'],
+    url='https://github.com/mewwts/addict',
+    author=addict.__author__,
+    author_email='mats@plysjbyen.net',
+    classifiers=(
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ),
+    description=SHORT,
+    long_description=LONG,
+    test_suite='test_addict'
+)
