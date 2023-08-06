1b3261d9597d77ecb85430b39409c8367279d46a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:05 +0000

    Add package lettherebe/0.0.2/lettherebe-0.0.2.tar.gz

---
+++ packages/lettherebe/lettherebe-0.0.2.tar.gz/lettherebe-0.0.2/lettherebe/registry.py
@@ -0,0 +1,26 @@
+from .utils import extract_arguments
+
+
+class HandlerRegistry(object):
+    """
+    Base container class to hold groups of handlers
+    """
+
+    def __init__(self):
+        self.members = {}
+
+    def __call__(self, identifier):
+        """
+        This is provided so that registry instances can be used
+        as decorators.
+        """
+
+        def wrapper(function):
+            arguments = extract_arguments(function)
+            self.members[identifier] = {'function': function, 'arguments': arguments}
+        return wrapper
+
+
+repository_host = HandlerRegistry()
+package_language = HandlerRegistry()
+service = HandlerRegistry()
---
+++ packages/lettherebe/lettherebe-0.0.2.tar.gz/lettherebe-0.0.2/lettherebe/utils.py
@@ -0,0 +1,18 @@
+from numpydoc.docscrape import NumpyDocString
+
+
+def extract_arguments(function):
+    """
+    Given a Python function, parse the numpydoc docstring and return a standard
+    list of arguments.
+    """
+    doc = NumpyDocString(function.__doc__)
+    arguments = []
+    for arg_name, arg_type, arg_description in doc['Parameters']:
+        argument = {}
+        argument['name'] = arg_name
+        argument['type'] = type(arg_type.split(',')[0].strip())
+        argument['optional'] = 'optional' in arg_type
+        argument['description'] = arg_description[0]
+        arguments.append(argument)
+    return arguments
