141550ed04411c0ca912ee9ff081c9aefc5871bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:19 +0000

    Add package reverpf/0.6.0/reverpf-0.6.0.tar.gz

---
+++ packages/reverpf/reverpf-0.6.0.tar.gz/reverpf-0.6.0/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+import reverpf
+
+setup(
+    name='reverpf',
+    version=reverpf.__version__,
+    packages=find_packages(),
+    author='penicolas',
+    author_email='png1981@gmail.com',
+    description='Reverse Printf',
+    long_description="README on github : https://github.com/penicolas/reverpf",
+    install_requires=[],
+    url='https://github.com/penicolas/reverpf',
+    classifiers=[
+        'Programming Language :: Python',
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Utilities',
+    ],
+    entry_points={
+        'console_scripts': [
+            'reverpf = reverpf.reverpf:main',
+        ],
+    },
+)
