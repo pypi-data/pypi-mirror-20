ca22e7588118ecc12b8a76da6656666e990cf315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:11 +0000

    Add package tgmagic/1.0/tgmagic-1.0-py3-none-any.whl

---
+++ packages/tgmagic/tgmagic-1.0-py3-none-any.whl/tgmagic/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from .bot import MagicFunction
+from .buttons import PrevButton, Buttons, ExtendNames
+from .menu import MagicMenu
+from .helper import singleton, menu
+
+__author__ = 'tezmen'
+__version__ = '1.0'
+__contact__ = 'https://t.me/tezmen'
