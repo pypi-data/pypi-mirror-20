c507ea0ae12bfde89673ff25e0f5f75292871c63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:55 +0000

    Add package VivaRedis/1.0/VivaRedis-1.0.tar.gz

---
+++ packages/VivaRedis/VivaRedis-1.0.tar.gz/VivaRedis-1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='VivaRedis',
+    version='1.0',
+    packages=[''],
+    url='https://www.github.com/xiyouMc/',
+    license='',
+    author='xiyouMc',
+    author_email='pythondevelopermc@gmail.com',
+    description='Redis For viva.'
+)
