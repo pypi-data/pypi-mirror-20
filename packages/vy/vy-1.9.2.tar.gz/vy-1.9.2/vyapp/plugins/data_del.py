af56b93cc469825bdb1aee8b62c28ba2f44d5658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:47 +0000

    Add package vy/1.9.2/vy-1.9.2.tar.gz

---
+++ packages/vy/vy-1.9.2.tar.gz/vy-1.9.2/vyapp/plugins/data_del.py
@@ -0,0 +1,44 @@
+"""
+Overview
+========
+
+This plugin implements keycommands to delete text selection, delete line, delete char.
+
+
+Key-Commands
+============
+
+Mode: NORMAL
+Event: <Key-d> 
+Description: Delete selection of text.
+
+
+Mode: NORMAL
+Event: <Key-x> 
+Description: Delete a line where the cursor is on.
+
+
+Mode: NORMAL
+Event: <Key-z> 
+Description: Delete a char from the cursor position.
+
+"""
+
+def install(area):
+    area.install(('NORMAL', '<Key-d>', lambda event: event.widget.del_sel()),
+                 ('NORMAL', '<Key-x>', lambda event: event.widget.del_line()),
+                 ('NORMAL', '<Key-z>', lambda event: event.widget.del_char()))
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ packages/vy/vy-1.9.2.tar.gz/vy-1.9.2/vyapp/plugins/text_jumps.py
@@ -0,0 +1,30 @@
+"""
+
+Overview
+========
+
+This plugin implements two Key-Commands to make the cursor jump to the begining/end of the file.
+
+Key-Commands
+============
+
+Mode: NORMAL
+Event: <Key-1> 
+Description: Place the cursor at the beginning of the file.
+
+
+Mode: NORMAL
+Event: <Key-2> 
+Description: Place the cursor at the end of the file.
+
+"""
+
+def install(area):
+    area.install(('NORMAL', '<Key-1>', lambda event: event.widget.go_text_start()),
+                 ('NORMAL', '<Key-2>', lambda event: event.widget.go_text_end()))
+
+
+
+
+
+
