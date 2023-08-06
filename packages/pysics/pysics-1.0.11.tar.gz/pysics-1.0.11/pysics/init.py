0c3ccceb3d237ab803ede099e00820d50660eb5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:28 +0000

    Add package pysics/1.0.11/pysics-1.0.11.tar.gz

---
+++ packages/pysics/pysics-1.0.11.tar.gz/pysics-1.0.11/pysics/init.py
@@ -0,0 +1,23 @@
+import math,sys,os
+try: import pygame
+except ImportError:
+	choice = input("The only dependency of this library is pygame. It seems like you haven't installed it though.\n do you want to install it now? Y/n")
+	if choice == 'Y': import pip; pip.main(['install','pygame']); import pygame
+	else: sys.exit()
+
+
+class version:
+	def __repr__(self):
+		return 'pysics version alpha-1.0.11'
+
+class init:
+	def __init__(self, sound=False):
+		pygame.init()
+		if sound:
+			pygame.mixer.init()
+
+class Camera:
+	def __init__(self, position = None):
+		pass
+
+
