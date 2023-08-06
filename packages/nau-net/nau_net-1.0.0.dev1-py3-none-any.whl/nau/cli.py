ca90264884a7defc9ad593fe53c53240719360e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:36 +0000

    Add package nau-net/1.0.0.dev1/nau_net-1.0.0.dev1-py3-none-any.whl

---
+++ packages/nau-net/nau_net-1.0.0.dev1-py3-none-any.whl/nau/cli.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import click
+from .network import login as nau_login
+from .network import logout as nau_logout
+from .network import test_network, network_use_condition
+from .config import get_config_status, add_user, remove_user
+
+@click.group()
+def cli():
+    """ NAU school network CLI."""
+    pass
+
+@click.command()
+def login():
+    """login to network"""
+    nau_login()
+
+@click.command()
+def logout():
+    """logout network"""
+    nau_logout()
+
+@click.command()
+def status():
+    """show current user and network status"""
+    username = get_config_status()
+    if username is None:
+        print("----------------NO USER!-------------")
+    else:
+        print("---------------Current User is %s---------" % username)
+    if test_network():
+        print("Current login to the network.")
+        network_use_condition()
+    else:
+        print("Current offline.")
+
+
+@click.command()
+@click.option('--username', prompt=True)
+@click.option('--password', prompt=True)
+def add(username, password):
+    """
+    add user for the network
+    --username
+    --password
+    """
+    add_user(username, password)
+    click.echo('Add user %s successful!' % username)
+
+@click.command()
+def remove():
+    """remove current account(include username and password)"""
+    remove_user()
+    click.echo("Done!")
+
+cli.add_command(login)
+cli.add_command(logout)
+cli.add_command(status)
+cli.add_command(add)
+cli.add_command(remove)
