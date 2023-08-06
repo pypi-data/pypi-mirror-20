59586b8e936f484ad0f6a58ee27518a44478c17c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:15 +0000

    Add package simtree-tmp/0.1/simtree-tmp-0.1.tar.gz

---
+++ packages/simtree-tmp/simtree-tmp-0.1.tar.gz/simtree-tmp-0.1/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017 Ben Lindsay <benjlindsay@gmail.com>
+
+from distutils.core import setup
+setup(
+  name = 'simtree-tmp',
+  packages = ['simtree'],
+  version = '0.1',
+  description = 'A module for automating hierarchical simulation studies',
+  long_description="Long Description",
+  requires=['pandas'],
+  author = 'Ben Lindsay',
+  author_email = 'benjlindsay@gmail.com',
+  url = 'https://github.com/benlindsay/simtree',
+  keywords = ['workflow', 'simulations'],
+  classifiers = [],
+)
