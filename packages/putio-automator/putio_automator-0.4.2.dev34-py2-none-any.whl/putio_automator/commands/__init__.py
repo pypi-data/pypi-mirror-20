1bff8dfec0316641d8f5853636cff38dd4810a12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:45 +0000

    Add package putio-automator/0.4.2.dev34/putio_automator-0.4.2.dev34-py2-none-any.whl

---
+++ packages/putio-automator/putio_automator-0.4.2.dev34-py2-none-any.whl/putio_automator/commands/__init__.py
@@ -0,0 +1,7 @@
+from account import manager as account
+from config import manager as config
+from db import manager as db
+from docker import manager as docker
+from files import manager as files
+from torrents import manager as torrents
+from transfers import manager as transfers
