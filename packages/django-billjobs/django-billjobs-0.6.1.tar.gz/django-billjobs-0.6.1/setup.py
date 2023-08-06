828b03109980ec5cd81a146b330a1ccbeed00518
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:16 +0000

    Add package django-billjobs/0.6.1/django-billjobs-0.6.1.tar.gz

---
+++ packages/django-billjobs/django-billjobs-0.6.1.tar.gz/django-billjobs-0.6.1/setup.py
@@ -0,0 +1,42 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-billjobs',
+    version='0.6.1',
+    packages=['billjobs'],
+    include_package_data=True,
+    install_requires=[
+        "django > 1.8",
+        "reportlab == 3.3.0",
+    ],
+    license='X11 License',
+    description='A django billing app for coworking space.',
+    long_description=README,
+    url='https://github.com/ioO/django-billjobs',
+    author='Lionel Chanson',
+    author_email='github@lionelchanson.fr',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Intended Audience :: Other Audience',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+)
