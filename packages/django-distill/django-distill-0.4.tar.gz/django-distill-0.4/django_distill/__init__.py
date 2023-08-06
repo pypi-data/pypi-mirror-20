7a6d062f447b308dbf9cce6e6315f62e0e16ac53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:23 +0000

    Add package django-distill/0.4/django-distill-0.4.tar.gz

---
+++ packages/django-distill/django-distill-0.4.tar.gz/django-distill-0.4/django_distill/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__version__ = 0.4
+
+from django_distill.distill import distill_url
+
+# eof
---
+++ packages/django-distill/django-distill-0.4.tar.gz/django-distill-0.4/django_distill/errors.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+class DistillError(Exception):
+    pass
+
+class DistillWarning(RuntimeWarning):
+    pass
+
+class DistillPublishError(Exception):
+    pass
+
+# eof
