1a8d64d405b068ee783ac519f8668aac9f0e8800
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:56 +0000

    Add package docker-buildtool/0.0.8/docker-buildtool-0.0.8.tar.gz

---
+++ packages/docker-buildtool/docker-buildtool-0.0.8.tar.gz/docker-buildtool-0.0.8/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='docker-buildtool',
+      version='0.0.8',
+      url='http://github.com/openai/docker-buildtool',
+      packages=[package for package in find_packages()
+                if package.startswith('docker_buildtool')],
+      scripts=['bin/docker-buildtool'],
+      install_requires=['glob2', 'six', 'PyYAML', 'pathlib'])
