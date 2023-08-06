696499fc9960f24f7cfc843cb545e6a6cc9d00d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:03 +0000

    Add package pkinter/1.28.20/pkinter-1.28.20.tar.gz

---
+++ packages/pkinter/pkinter-1.28.20.tar.gz/pkinter-1.28.20/pkinter/__init__.py
@@ -0,0 +1,31 @@
+from .toggledlabelframe import *
+from .labeledseparator import *
+from .roundingscale import *
+from .entrytext import *
+from .limitedentry import *
+from .colourpickerbutton import *
+from .editablelabel import *
+from .collapsiblepane import *
+from .hyperlink import *
+from .pageview import *
+from .toolbar import *
+from .statusbar import *
+from .linenumbers import *
+from .boundbutton import *
+from .validentry import *
+from .choicebook import *
+from .passwordentry import *
+from .invalidentry import *
+from .listbook import *
+from .accellabel import *
+from .infobar import *
+from .lockbutton import *
+from .togglebutton import *
+from .scaleswitch import *
+from .buttonswitch import *
+from .filepicker import *
+from .directorypicker import *
+
+__title__ = "pkinter"
+__author__ = "DeflatedPickle"
+__version__ = "1.28.20"
