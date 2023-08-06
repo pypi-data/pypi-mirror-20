1001bab0722aecd7680e812927e790e33d430077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:44 +0000

    Add package djangocms-link2/1.0.1/djangocms_link2-1.0.1-py2.py3-none-any.whl

---
+++ packages/djangocms-link2/djangocms_link2-1.0.1-py2.py3-none-any.whl/cmsplugin_filer_link2/cms_plugins.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.templatetags.static import static
+from django.utils.translation import ugettext as _
+
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+
+from .forms import FilerLink2Form
+from .models import FilerLink2Plugin as FilerLinkPluginModel
+
+
+class FilerLink2Plugin(CMSPluginBase):
+    form = FilerLink2Form
+    model = FilerLinkPluginModel
+    module = 'Filer'
+    name = _('Link')
+    raw_id_fields = ('page_link', )
+    render_template = "cmsplugin_filer_link/link.html"
+    text_enabled = True
+
+    fieldsets = (
+        (None, {
+            'fields': [
+                'name',
+                'url',
+                'page_link',
+                'mailto',
+                'file',
+                'link_style',
+                'new_window',
+            ]
+        }),
+        (_('Advanced'), {
+            'classes': ['collapse', ],
+            'fields': [
+                'link_attributes',
+            ]
+        })
+    )
+
+    def render(self, context, instance, placeholder):
+        context = super(FilerLink2Plugin, self).render(context, instance, placeholder)
+        link = instance.get_link()
+        context.update({
+            'link': link,
+            'style': instance.link_style,
+            'name': instance.name,
+            'new_window': instance.new_window,
+        })
+        try:
+            # check if we are in edit mode, so we show link health
+            if context['request'].toolbar.edit_mode:
+                state = instance.get_linkstate()
+                if state:
+                    context.update({
+                        'link_state': state,
+                    })
+        except (KeyError, AttributeError):
+            pass
+        return context
+
+    def icon_src(self, instance):
+        return static("cms/img/icons/plugins/link.png")
+
+
+plugin_pool.register_plugin(FilerLink2Plugin)
