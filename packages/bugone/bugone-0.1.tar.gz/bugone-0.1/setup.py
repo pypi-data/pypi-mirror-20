a432dd7531113bc6501366f53a0315295abaf240
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:15 +0000

    Add package bugone/0.1/bugone-0.1.tar.gz

---
+++ packages/bugone/bugone-0.1.tar.gz/bugone-0.1/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup
+import fastentrypoints
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+VERSION = "0.1"
+
+"""
+  entry_points = {
+      'console_scripts': [
+          'xaal-isalive = xaal.tools.isalive:run',
+          'xaal-info   = xaal.tools.info:run',
+          'xaal-dumper  = xaal.tools.dumper:run',
+          'xaal-tail    = xaal.tools.tail:run',
+          'xaal-walker  = xaal.tools.walker:run',
+          'xaal-keygen  = xaal.tools.keygen:run',
+      ],
+    },
+   
+""" 
+
+
+setup(
+    name='bugone',
+    version=VERSION,
+    license='GPL License',
+    author='Jerome Kerdreux',
+    author_email='Jerome.Kerdreux@Finix.eu.org',
+    url='http://dwb.ilhost.fr/',
+    description=('API to send/receive bugOne messages througt serial port'),
+    long_description=long_description,
+    classifiers=[
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords=['RFM12', 'bugOne','Wireless','Home-Automation'],
+    platforms='any',
+    packages=['bugone',],
+    include_package_data=True,
+    install_requires=['pyserial'],
+    entry_points = {
+      'console_scripts': [
+          'bugone-dumper = bugone.dumper:main',
+      ],
+    },
+
+    
+)
