33698d33de943a5ebe938de7166261187b1ee7b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:19 +0000

    Add package pair/0.1.0/pair-0.1.0.tar.gz

---
+++ packages/pair/pair-0.1.0.tar.gz/pair-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+    name = 'pair',
+    packages = ['pair'],
+    install_requires = ['networkx>=1.11',],
+    version = '0.1.0',
+    description = 'a ladder pairing/matching library',
+    author = 'knyte',
+    author_email = 'galactaknyte@gmail.com',
+    url = 'https://github.com/knyte/pair',
+    download_url = 'https://github.com/knyte/pair/tarball/0.1.0',
+    license = 'MIT',
+    keywords = ['ladder', 'pairing', 'matching', 'Munkres'],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Other Audience',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Games/Entertainment :: Board Games',
+        'Topic :: Games/Entertainment :: Real Time Strategy',
+        'Topic :: Games/Entertainment :: Turn Based Strategy',
+    ],
+)
