671709e415056345d37680f013b39adfe97ce65a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:25 +0000

    Add package pmm/0.2.0/pmm-0.2.0.tar.gz

---
+++ packages/pmm/pmm-0.2.0.tar.gz/pmm-0.2.0/pmm/data.py
@@ -0,0 +1,29 @@
+#-*-coding:utf-8-*-
+
+import requests
+import requests_cache
+from collections import OrderedDict
+
+# cache api result for 10min
+requests_cache.install_cache(expire_after=600)
+
+STATUS_LABELS = OrderedDict([
+    ('green', 'fresh'),
+    ('yellow', 'aging'),
+    ('red', 'old'),
+    ('unavailable', 'n/a'),
+])
+
+def fetch_mirrors_data():
+    api_url = 'https://www.pypi-mirrors.org/data.json'
+    r = requests.get(api_url, verify=False)
+    return r.json()
+
+
+def get_mirrors_data():
+    mirrors = []
+    mirrors_data = fetch_mirrors_data()
+    for mirror, data in mirrors_data.iteritems():
+        mirrors.append((mirror, data['status'].lower()))
+    mirrors.sort(key=lambda (mirror, status): list(STATUS_LABELS).index(status))
+    return [mirror for mirror, status in mirrors]
