1a3b962c540e487d8a86f64465b19d27bf20f9cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:48 +0000

    Add package aospy/0.1.1/aospy-0.1.1-py2.py3-none-any.whl

---
+++ packages/aospy/aospy-0.1.1-py2.py3-none-any.whl/aospy/__init__.py
@@ -0,0 +1,26 @@
+"""aospy: management, and analysis of gridded climate data."""
+from .__config__ import user_path
+from . import constants
+from .constants import Constant
+from . import utils
+from . import units
+from .units import Units
+from . import operator
+from .operator import Operator
+from . import var
+from .var import Var
+from . import region
+from .region import Region
+from . import run
+from .run import Run
+from . import model
+from .model import Model
+from . import proj
+from .proj import Proj
+from . import calc
+from .calc import CalcInterface, Calc
+from . import find_obj
+from .find_obj import to_iterable, to_proj, to_model, to_run, to_var, to_region
+
+__all__ = ['Proj', 'Model', 'Run', 'Var', 'Units', 'Constant', 'Region',
+           'units', 'calc', 'constants', 'utils']
