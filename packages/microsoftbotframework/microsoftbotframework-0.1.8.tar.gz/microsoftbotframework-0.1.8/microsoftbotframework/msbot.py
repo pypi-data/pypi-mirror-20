822430602973d42b7c00cabb5957b3b3f45e027d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:42 +0000

    Add package microsoftbotframework/0.1.8/microsoftbotframework-0.1.8.tar.gz

---
+++ packages/microsoftbotframework/microsoftbotframework-0.1.8.tar.gz/microsoftbotframework-0.1.8/microsoftbotframework/msbot.py
@@ -0,0 +1,33 @@
+from flask import Flask, request
+from celery.local import PromiseProxy
+from .config import Config
+
+
+class MsBot:
+    def __init__(self, host=None, port=None, debug=None):
+        self.processes = []
+        config = Config()
+        self.host = config.get_config(host, 'HOST', root='flask')
+        self.port = config.get_config(port, 'PORT', root='flask')
+        self.debug = config.get_config(debug, 'DEBUG', root='flask')
+
+        self.app = Flask(__name__)
+
+        @self.app.route('/api/messages', methods=['POST'])
+        def message_post():
+            # TODO: Confirm that it is from microsoft
+            json_message = request.get_json()
+            for process in self.processes:
+                if isinstance(process, PromiseProxy):
+                    self.app.logger.info('Processing task {} asynchronously.'.format(type(process).__name__))
+                    process.delay(json_message)
+                elif callable(process):
+                    self.app.logger.info('Processing task {} synchronously.'.format(process.__name__))
+                    process(json_message)
+            return "Success"
+
+    def add_process(self, process):
+        self.processes.append(process)
+
+    def run(self):
+        self.app.run(host=self.host, port=self.port, debug=self.debug)
