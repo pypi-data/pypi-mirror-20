b9c167e31dd5c116729b9781f928f4e7c8694c27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:57 +0000

    Add package RAMLLintBear/0.10.0/RAMLLintBear-0.10.0.tar.gz

---
+++ packages/RAMLLintBear/RAMLLintBear-0.10.0.tar.gz/RAMLLintBear-0.10.0/setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+import os
+import locale
+
+from setuptools import find_packages, setup
+
+
+try:
+    locale.getlocale()
+except (ValueError, UnicodeError):
+    locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+
+required = ''
+if os.path.exists('requirements.txt'):
+    with open('requirements.txt') as requirements:
+        required = requirements.read().splitlines()
+
+if __name__ == '__main__':
+    try:
+        setup(name='RAMLLintBear',
+              version='0.10.0',
+              description=''''RAMLLintBear' bear for coala (http://coala.rtfd.org/)''',
+              authors={'The coala developers'},
+              authors_emails={'coala-devel@googlegroups.com'},
+              maintainers={'The coala developers'},
+              maintainers_emails={'coala-devel@googlegroups.com'},
+              platforms={'any'},
+              license='AGPL-3.0',
+              packages=find_packages(exclude=['build.*']),
+              install_requires=required,
+              long_description='''
+    RAML Linter is a static analysis, linter-like, utility that will enforce
+    rules on a given RAML document, ensuring consistency and quality.
+    Note: Files should not have leading empty lines, else the bear fails to
+    identify the problems correctly.
+    ''',
+              entry_points={'coalabears': ['RAMLLintBear = coalaRAMLLintBear']},
+              classifiers=[
+                   'Development Status :: 4 - Beta',
+                   'Environment :: Console',
+                   'Environment :: Win32 (MS Windows)',
+                   'Intended Audience :: Science/Research',
+                   'Intended Audience :: Developers',
+                   'Programming Language :: '
+                   'Python :: Implementation :: CPython',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3 :: Only',
+                   'Topic :: Scientific/Engineering :: Information Analysis',
+                   'Topic :: Software Development :: Quality Assurance',
+                   'Topic :: Text Processing :: Linguistic'])
+
+    finally:
+        print('[WARN] If you do not install the bears using the coala '
+              'installation tool, there may be problems with the dependencies '
+              'and they may not work.')
