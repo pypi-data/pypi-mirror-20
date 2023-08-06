d2c3183836969cc7b8f2ed842c01ced384d0ae55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:36 +0000

    Add package pocketcasts-api/0.2.2/pocketcasts-api-0.2.2.tar.gz

---
+++ packages/pocketcasts-api/pocketcasts-api-0.2.2.tar.gz/pocketcasts-api-0.2.2/setup.py
@@ -0,0 +1,29 @@
+import pocketcasts
+from setuptools import setup, find_packages
+
+setup(
+    name="pocketcasts-api",
+
+    version=pocketcasts.api.__version__,
+
+    description=pocketcasts.api.__doc__,
+
+    url=pocketcasts.api.__url__,
+
+    author=pocketcasts.api.__author__,
+    author_email='ferguslongley@live.com',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    packages=find_packages(exclude=['testing']),
+
+    keywords='podcasts pocketcasts',
+
+    install_requires=['requests'],
+)
