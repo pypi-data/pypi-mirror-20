3786f5a97d2b4fe44b4098620bad512059b7c335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:51 +0000

    Add package golgi/1.5.2/golgi-1.5.2.tar.gz

---
+++ packages/golgi/golgi-1.5.2.tar.gz/golgi-1.5.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+version_parts = (1, 5, 2)
+version = '.'.join(map(str, version_parts))
+
+setup(
+    name='golgi',
+    description='app config and execution tools',
+    version=version,
+    author='Torsten Schmits',
+    author_email='torstenschmits@gmail.com',
+    license='MIT',
+    packages=find_packages(exclude=['integration', 'integration.*', 'unit',
+                                    'unit.*']),
+    install_requires=[
+        'amino>=9.5.7',
+    ],
+)
