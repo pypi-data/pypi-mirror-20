664bd19aaf56cc37d2dd35654a9eeca3f98664c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:36 +0000

    Add package PyBarCodeScan/0.1.63/PyBarCodeScan-0.1.63.tar.gz

---
+++ packages/PyBarCodeScan/PyBarCodeScan-0.1.63.tar.gz/PyBarCodeScan-0.1.63/barcodescan/Config.py
@@ -0,0 +1,20 @@
+import configparser
+import os
+
+class Config(object):
+  config_key = "BARCODE_SCANNER"
+  def __init__(self, path_to_config_file):
+    self.config = configparser.ConfigParser()
+    self.config["SERVER"] = {'push_url': 'http://localhost:8000/dummy_endpoint'}
+    self.config["READER"] = {'push_url': 'http://localhost:8000'}
+    if not os.path.isfile(path_to_config_file):
+      with open(path_to_config_file, 'w') as configfile:
+        self.config.write(configfile)
+    self.config.read(path_to_config_file, "UTF-8")
+
+  def server_push_url(self):
+    return self.config["SERVER"]['push_url']
+
+  def reader_push_url(self):
+    return self.config["READER"]['push_url']
+
