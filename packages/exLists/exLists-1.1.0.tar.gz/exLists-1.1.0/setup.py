410b76e69d556f7d0b1ad86582a6277c64f32ecf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:03 +0000

    Add package exLists/1.1.0/exLists-1.1.0.tar.gz

---
+++ packages/exLists/exLists-1.1.0.tar.gz/exLists-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name            ='exLists',
+        version         ='1.1.0',
+        py_modules      =['exLists'],
+        author          ='Rudy',
+        author_email    ='rodolfohill@yahoo.com',
+        url             ='http://www.headfirstlabs.com',
+        description     ='A simple module to def a function',
+    )
