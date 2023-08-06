9aa2687a478265a6e4518c120a271beba5be2261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:00 +0000

    Add package fastweb/1.3.5/fastweb-1.3.5.tar.gz

---
+++ packages/fastweb/fastweb-1.3.5.tar.gz/fastweb-1.3.5/fastweb/__init__.py
@@ -0,0 +1,40 @@
+# coding:utf8
+
+
+import tornado
+from tornado.locks import Condition
+from tornado.options import options
+from tornado.gen import coroutine, Return, Task, sleep
+from tornado import gen, web, httpserver, ioloop
+from tornado.web import UIModule as UI, asynchronous
+from tornado.httpclient import HTTPRequest as Request
+
+from fastweb.loader import app
+from fastweb.util.log import recorder
+from fastweb.util.thread import FThread
+from fastweb.pattern import SyncPattern, AsynPattern
+from fastweb.web import Api, Page, SyncComponents, AsynComponents, arguments
+
+
+def start_server(port, handlers, **settings):
+    """启动服务器"""
+
+    application = tornado.web.Application(
+        handlers,
+        **settings
+    )
+
+    http_server = httpserver.HTTPServer(
+        application, xheaders=settings.get('xheaders'))
+    http_server.listen(port)
+    recorder('INFO', 'server start on {port}'.format(port=port))
+    try:
+        ioloop.IOLoop.current().start()
+    except KeyboardInterrupt:
+        ioloop.IOLoop.current().stop()
+        FThread.stop(0)
+        recorder('INFO', 'server stop on {port}'.format(port=port))
+
+
+def set_errcode_handler(**kwargs):
+    pass
