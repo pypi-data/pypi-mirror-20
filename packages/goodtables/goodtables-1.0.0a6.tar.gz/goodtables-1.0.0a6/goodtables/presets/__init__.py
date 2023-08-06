71c7d4c7e1e58acfc9bc7c7640ac9530a5f8f78a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:56 +0000

    Add package goodtables/1.0.0a6/goodtables-1.0.0a6.tar.gz

---
+++ packages/goodtables/goodtables-1.0.0a6.tar.gz/goodtables-1.0.0a6/goodtables/presets/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from .datapackage import datapackage
+from .datapackages import datapackages
+from .table import table
+from .tables import tables
