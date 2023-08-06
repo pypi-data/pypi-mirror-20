9644674aaae26acab0b99f2fe9836f38358e0e00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:43 +0000

    Add package FlaskyTornado/0.0.5/FlaskyTornado-0.0.5.tar.gz

---
+++ packages/FlaskyTornado/FlaskyTornado-0.0.5.tar.gz/FlaskyTornado-0.0.5/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+
+setup(
+    name='FlaskyTornado',
+    version="0.0.5",
+    license='BSD',
+    author='Ali Arda Orhan',
+    author_email='arda.orhan@dogantv.com.tr',
+    description='A microframework based on Tornado and Flask '
+                'and good intentions',
+    long_description=__doc__,
+    packages=['flasky', 'flasky.plugins.json_schema', 'flasky.plugins.parameters'],
+    platforms='any',
+    install_requires=[
+        'tornado==4.4.2',
+        'jsonschema==2.6.0',
+        'PyJWT==1.4.2'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
