e224a6f98caec80e908bfc733ce088b00ac5adaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:52 +0000

    Add package ykdl/1.4.4/ykdl-1.4.4-py2.py3-none-any.whl

---
+++ packages/ykdl/ykdl-1.4.4-py2.py3-none-any.whl/ykdl/extractor.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class VideoExtractor():
+    def __init__(self):
+        self.url = None
+        self.vid = None
+
+    def parser(self, url):
+        self.__init__()
+        if isinstance(url, str) and url.startswith('http'):
+            self.url = url
+        else:
+            self.vid= url
+
+        # if info is returned by prepare, no need go extractor
+        # else go extractor.
+        # info is instance of VideoInfo
+        info = self.prepare()
+        return info
+
+    def parser_list(self, url):
+        self.url = url
+        video_list = self.prepare_list()
+        if not video_list:
+            raise NotImplementedError('playlist not support for {} with url: {}'.format(self.name, self.url))
+        return [self.parser(v) for v in video_list]
+
+    def prepare(self):
+        pass
+
+    def extractor(self):
+        pass
+
+    def prepare_list(self):
+        pass
