d02efd1892f684dcb51a68dea60dfb9e42c6a320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:27 +0000

    Add package django-prices-openexchangerates/0.1.13/django-prices-openexchangerates-0.1.13.tar.gz

---
+++ packages/django-prices-openexchangerates/django-prices-openexchangerates-0.1.13.tar.gz/django-prices-openexchangerates-0.1.13/django_prices_openexchangerates/templatetags/prices_multicurrency_i18n.py
@@ -0,0 +1,32 @@
+from django.template import Library
+from django_prices.templatetags import prices_i18n
+
+from .. import exchange_currency
+
+register = Library()
+
+
+@register.simple_tag  # noqa
+def gross_in_currency(price, currency, **kwargs):  # noqa
+    converted_price = exchange_currency(price, currency)
+    return prices_i18n.gross(converted_price, **kwargs)
+
+
+@register.simple_tag  # noqa
+def net_in_currency(price, currency, **kwargs):  # noqa
+    converted_price = exchange_currency(price, currency)
+    return prices_i18n.net(converted_price, **kwargs)
+
+
+@register.simple_tag  # noqa
+def tax_in_currency(price, currency, **kwargs):  # noqa
+    converted_price = exchange_currency(price, currency)
+    return prices_i18n.tax(converted_price, **kwargs)
+
+
+@register.simple_tag
+def discount_amount_in_currency(discount, price, currency):
+    price = exchange_currency(price, to_currency=currency)
+    discount_amount = exchange_currency(discount.amount, to_currency=currency)
+    discount.amount = discount_amount
+    return (price | discount) - price
