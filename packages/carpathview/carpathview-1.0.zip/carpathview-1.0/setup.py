dd2bb86fc4211543ea413f0f6451c5fff5e5ffa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:47 +0000

    Add package carpathview/1.0/carpathview-1.0.zip

---
+++ packages/carpathview/carpathview-1.0.zip/carpathview-1.0/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+  name = 'carpathview',
+  packages=['carpathview'],# this must be the same as the name above
+  version = '1.0',
+  scripts=['carpathview/pyScript.py'],
+  description = "there is a simple introduction",
+  long_description="details",
+  author = 'author',
+  author_email = 'author_email@gmail.com',
+  url = 'http://github_package', # use the URL to the github repo
+  download_url = 'http://github_can_download_package',
+  keywords = [], # arbitrary keywords
+  classifiers = [],
+  install_requires = ['', ''], # dependencies needed
+  license="Apache-2.0",
+#    entry_points = {
+#        'console_scripts': [
+#            'runname = carpathview.pyScript:main'
+#        ]
+#  },
+# you should change the runname
+)
