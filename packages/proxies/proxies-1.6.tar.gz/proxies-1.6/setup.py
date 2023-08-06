8932ee586b82c1420d486a44041ceb2c5aa4bf9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:38 +0000

    Add package proxies/1.6/proxies-1.6.tar.gz

---
+++ packages/proxies/proxies-1.6.tar.gz/proxies-1.6/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='proxies',
+    version='1.6',
+    keywords=('proxy', 'proxies', 'requests'),
+    description='Get latest http proxies.',
+    # long_description=read('README.rst'),
+    author='MyFaith',
+    author_email='faith0725@outlook.com',
+    url='https://github.com/MyFaith/proxies',
+    license='MIT',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    install_requires=['requests', 'pyquery', 'gevent']
+)
