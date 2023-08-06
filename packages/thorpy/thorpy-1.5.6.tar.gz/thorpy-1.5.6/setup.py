faff40353c096ee6cf581b072be7b385eb695b1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:50 +0000

    Add package thorpy/1.5.6/thorpy-1.5.6.tar.gz

---
+++ packages/thorpy/thorpy-1.5.6.tar.gz/thorpy-1.5.6/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(name='thorpy',
+      version='1.5.6',
+      description='pygame GUI library',
+      author='Yann Thorimbert',
+      author_email='yann.thorimbert@gmail.com',
+      url='http://www.thorpy.org/',
+      keywords=['pygame','gui','menus','buttons', 'widgets'],
+      packages=find_packages(),
+      include_package_data=True,
+      license='MIT')
