e1d45ad522ec1ba05dc1040aa566f656255df642
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:16 +0000

    Add package texttables/0.1.2/texttables-0.1.2.tar.gz

---
+++ packages/texttables/texttables-0.1.2.tar.gz/texttables-0.1.2/texttables/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright © 2016 Absolute Performance Inc <csteam@absolute-performance.com>.
+# All rights reserved.
+# This is proprietary software.
+# No warranty, explicit or implicit, provided.
+
+from __future__ import division, absolute_import, print_function, unicode_literals
+import six
+
+__author__ = 'Taylor C. Richberger <tcr@absolute-performance.com>'
+__description__ = 'A Python module for parsing and writing text-based tables'
+__email__ = 'tcr@absolute-performance.com'
+__license__ = 'MIT'
+__modulename__ = 'texttables'
+__version__ = '0.1.2'
+__website__ = 'https://github.com/Taywee/texttables'
+
+__all__ = ['fixed', 'dynamic']
