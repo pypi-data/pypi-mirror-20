ca84ff57aa171fe763e67c74317a80b51e30f435
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:02 +0000

    Add package sampen/0.0.12/sampen-0.0.12.tar.gz

---
+++ packages/sampen/sampen-0.0.12.tar.gz/sampen-0.0.12/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+
+setup(
+    name='sampen',
+    version='0.0.12',
+    description='A Python module to calculate Sample Entropy (SampEn) of a'
+                ' time series.',
+    url='http://www.physionet.org/physiotools/sampen',
+    download_url='https://github.com/jbergantine/sampen/tarball/0.0.11/',
+    author='Doug Lake',
+    author_email='dlake@virginia.edu',
+    maintainer='Joe Bergantine',
+    maintainer_email='joe@kinsa.us',
+    license='GNU',
+    packages=['sampen'],
+    install_requires=[
+        'numpy>=1.9.2',
+    ],
+    keywords=['sample entropy', 'sampen'],
+    test_suite="tests.test_sampen",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Mathematics',
+    ]
+)
