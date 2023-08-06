d0cfe3694bd615860581c9816ec6a6525defa3f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:34 +0000

    Add package py_knife/0.01.20/py_knife-0.01.20.zip

---
+++ packages/py_knife/py_knife-0.01.20.zip/py_knife-0.01.20/py_knife/logger.py
@@ -0,0 +1,33 @@
+"""
+Logger Class
+
+To use logger simply::
+
+    import sys
+    sys.stdout = Logger(log_file_path)
+
+"""
+
+
+### INCLUDES ###
+import sys
+
+
+### CONSTANTS ###
+## Meta Data ##
+__author__ = 'Kirill V. Belyayev'
+__license__ = 'GPL'
+
+
+### CLASSES ###
+class Logger(object):
+    def __init__(self, filename=None):
+        self.terminal = sys.stdout
+        self.filename = filename
+        if self.filename is not None:
+            self.log = open(filename, "w")
+
+    def write(self, message):
+        self.terminal.write(message)
+        if self.filename is not None:
+            self.log.write(message)
