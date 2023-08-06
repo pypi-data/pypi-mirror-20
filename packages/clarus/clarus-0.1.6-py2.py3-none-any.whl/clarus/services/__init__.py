ab7afe949f1d418fb0def5ddbf332890dcc0ac62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:04 +0000

    Add package clarus/0.1.6/clarus-0.1.6-py2.py3-none-any.whl

---
+++ packages/clarus/clarus-0.1.6-py2.py3-none-any.whl/clarus/services/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import print_function, unicode_literals
+import clarus
+import clarus.api
+from clarus.models import ApiResponse, ApiError
+
+__all__ = [u'compliance', u'frtb', u'hedge', u'margin', u'market', u'portfolio', u'profitloss', u'risk', u'sdr', u'simm', u'trade', u'upload', u'util', u'xva']
+
+def api_request(serviceCategory, service, output=None, **params):
+    httpresp = clarus.api.request(serviceCategory, service, output, **params);
+    if (httpresp.status_code != 200):
+        raise ApiError(httpresp)
+    else:
+        return ApiResponse(httpresp);
