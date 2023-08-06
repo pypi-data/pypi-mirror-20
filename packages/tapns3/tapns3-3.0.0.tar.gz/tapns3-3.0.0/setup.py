fa18e1099d119cecdde4ef9db2ff0495ace367ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:40 +0000

    Add package tapns3/3.0.0/tapns3-3.0.0.tar.gz

---
+++ packages/tapns3/tapns3-3.0.0.tar.gz/tapns3-3.0.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    author = 'ActiDoo GmbH',
+    author_email = 'info@actidoo.com',
+    description = 'Temporary Fork of PyAPNs to work with Python 3',
+    download_url = 'https://github.com/ActiDoo/PyAPNs',
+    license = 'unlicense.org',
+    name = 'tapns3',
+    py_modules = ['apns'],
+    scripts = ['apns-send'],
+    url = 'https://github.com/ActiDoo/PyAPNs',
+    version = '3.0.0',
+)
