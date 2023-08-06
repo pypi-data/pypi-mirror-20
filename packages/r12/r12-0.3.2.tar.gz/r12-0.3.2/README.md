9f926f6a5739af2b89bc8dabe0dc74273de9c20e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:53 +0000

    Add package r12/0.3.2/r12-0.3.2.tar.gz

---
+++ packages/r12/r12-0.3.2.tar.gz/r12-0.3.2/README.md
@@ -0,0 +1,18 @@
+# R12
+A Python package providing a low-level interface for the ST Robotics R12
+robotic arm.
+
+## Install
+Install using pip:
+```
+pip install r12
+```
+
+## Shell
+The `r12-shell` executable is automatically installed with the package. It
+provides a shell interface to the robot arm for testing.
+
+## Manuals
+See the ST Robotics manuals on the R12 arm and ROBOFORTH for more information.
+* [Arm Manual](http://www.strobotics.com/manuals/R12%20manual.pdf)
+* [ROBOFORTH Manual](http://www.strobotics.com/manuals/manual16.htm)
---
+++ packages/r12/r12-0.3.2.tar.gz/r12-0.3.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+version = open('VERSION').read().strip()
+
+setup(name='r12',
+      version=version,
+      description='Low-level interface for ST Robotics R12 robotic arm.',
+      url='https://github.com/adamheins/r12',
+      author='Adam Heins',
+      author_email='mail@adamheins.com',
+      license='MIT',
+      packages=['r12'],
+      install_requires=[
+          'colorama',
+          'pyusb',
+          'pyserial>=3',
+      ],
+      scripts=['r12/r12-shell'],
+      include_package_data=True,
+      zip_safe=False,
+)
+
