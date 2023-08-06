d092283104739d2c337841d826ab096cd3d689c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:31 +0000

    Add package askpy/0.0.3/askpy-0.0.3-py3-none-any.whl

---
+++ packages/askpy/askpy-0.0.3-py3-none-any.whl/askpy/__init__.py
@@ -0,0 +1,13 @@
+"""
+ASKpy Prompt library
+~~~~~~~~~~~~~~~~~~~~~
+Describe here...
+"""
+
+__title__ = 'askpy'
+__version__ = '0.0.3'
+__author__ = 'Dan Sackett'
+
+from .prompt import Prompt
+from .validation import Validator
+from .exceptions import ValidationError
