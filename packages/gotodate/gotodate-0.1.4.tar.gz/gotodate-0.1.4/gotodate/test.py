8d5cba4fd8bda53647dfde9c7867119fabb6f720
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:22 +0000

    Add package gotodate/0.1.4/gotodate-0.1.4.tar.gz

---
+++ packages/gotodate/gotodate-0.1.4.tar.gz/gotodate-0.1.4/gotodate/test.py
@@ -0,0 +1,33 @@
+# -*- coding: UTF-8 -*-
+__author__ = "chenshancang@163.com"
+
+from gotodate.gotodate import GotoDate
+a = '2016-10-11'
+d = GotoDate(date=a)
+print d.get_date()
+print d.before(1)
+print d.after(2)
+print d.to_format('%Y/%m/%d')
+print d.week_range()
+
+d = GotoDate()
+print d.get_date()
+print d.before(1)
+print d.after(2)
+print d.to_format('%Y年%m月%d日')
+print d.week_range()
+
+
+a = '2017年03月24日'
+d = GotoDate(date=a,format='%Y年%m月%d日')
+print d.get_date()
+print d.before(1)
+print d.after(2)
+print d.to_format('%Y-%m-%d')
+print d.week_range()
+print d.before_months(1)
+print d.after_months(3)
+
+
+
+
