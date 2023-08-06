a4df1c9b43e20f2df3e08e801ed632719cfb114a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:01 +0000

    Add package ahg48/0.1.1/ahg48-0.1.1.tar.gz

---
+++ packages/ahg48/ahg48-0.1.1.tar.gz/ahg48-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# @copyright: AlertAvert.com (c) 2016. All rights reserved.
+
+# https://github.com/pypa/sampleproject
+
+from setuptools import setup
+
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='ahg48',
+      description='AHG 48',
+      long_description=long_description,
+      version='0.1.1',
+      url='https://github.com/rch850/ahg48',
+      author='rch850',
+      author_email='rich850@gmail.com',
+      license='Apache2',
+      classifiers=[],
+      packages=['ahg48'],
+      entry_points={
+          'console_scripts': [
+              'encrypt=ahg48.main:run'
+          ]
+      }
+)
