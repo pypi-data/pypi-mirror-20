5305807926dbc5da709f0679abeb65bc4c80f443
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:07 +0000

    Add package hes/0.5/hes-0.5.tar.gz

---
+++ packages/hes/hes-0.5.tar.gz/hes-0.5/hes/hes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+permitted = "0123456789+-/*= "
+
+def cli():
+    print("""
+    ######## Calculator ########
+    #    Permitted operators   #
+    #            +             #
+    #            -             #
+    #            *             #
+    #            /             #
+    ############################
+    """)
+
+    while True:
+        data = input("Calculate: ")
+        for s in data:
+            if s not in permitted:
+                quit()
+
+        res = eval(data)
+
+        print(res)
