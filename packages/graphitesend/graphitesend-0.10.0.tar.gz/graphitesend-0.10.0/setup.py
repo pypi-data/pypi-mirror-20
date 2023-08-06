aa3264324555c444cd84ee1ba333ce837f420122
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:09 +0000

    Add package graphitesend/0.10.0/graphitesend-0.10.0.tar.gz

---
+++ packages/graphitesend/graphitesend-0.10.0.tar.gz/graphitesend-0.10.0/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='graphitesend',
+    version='0.10.0',
+    description='A simple interface for sending metrics to Graphite',
+    author='Danny Lawrence',
+    author_email='dannyla@linux.com',
+    url='https://github.com/daniellawrence/graphitesend',
+    packages=['graphitesend'],
+    long_description="https://github.com/daniellawrence/graphitesend",
+    entry_points={
+        'console_scripts': [
+            'graphitesend = graphitesend.graphitesend:cli',
+        ],
+    },
+    extras_require={
+        'asynchronous': ['gevent>=1.0.0'],
+    }
+)
