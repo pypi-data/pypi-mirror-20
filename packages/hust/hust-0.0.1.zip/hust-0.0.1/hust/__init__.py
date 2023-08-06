117cc2522d427578a8855613f4077ded16a300fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:01 +0000

    Add package hust/0.0.1/hust-0.0.1.zip

---
+++ packages/hust/hust-0.0.1.zip/hust-0.0.1/hust/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+'''
+Created on Mar 10, 2017
+
+@author: hustcc
+'''
+from __future__ import absolute_import
+
+
+__version__ = '0.0.1'
---
+++ packages/hust/hust-0.0.1.zip/hust-0.0.1/hust/cli.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+'''
+Created on Mar 10, 2017
+
+@author: hustcc
+'''
+from __future__ import absolute_import
+
+
+def run():
+    print('Hust')
