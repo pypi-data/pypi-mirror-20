3fc3afe1aa3d7e7851c1735cdd82672241ab9802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:01 +0000

    Add package RedLeader/0.1.9/RedLeader-0.1.9.tar.gz

---
+++ packages/RedLeader/RedLeader-0.1.9.tar.gz/RedLeader-0.1.9/setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup, find_packages
+
+install_requires = [
+    'botocore',
+      'boto3==1.4.2'
+]
+
+setup(
+    name='RedLeader',
+    version='0.1.9',
+    description='An automated EC2 & ECS deployment platform',
+    author='Morgan McDermott',
+    author_email='morganmcdermott@gmail.com',
+    packages=find_packages(),
+    data_files=[('redleader/resources/policies/', os.listdir('redleader/resources/policies/'))],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+        ]
+    },
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ]
+)
