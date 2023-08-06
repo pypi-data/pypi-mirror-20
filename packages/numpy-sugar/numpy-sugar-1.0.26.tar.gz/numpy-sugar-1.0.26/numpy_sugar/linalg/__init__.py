27febbfd4259031370e4dfd22e9fbc337ba2ab74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:23 +0000

    Add package numpy-sugar/1.0.26/numpy-sugar-1.0.26.tar.gz

---
+++ packages/numpy-sugar/numpy-sugar-1.0.26.tar.gz/numpy-sugar-1.0.26/numpy_sugar/linalg/__init__.py
@@ -0,0 +1,9 @@
+from .cho import cho_solve
+from .diag import sum2diag, trace2
+from .dot import ddot, dotd
+from .lu import lu_slogdet, lu_solve
+from .property import check_definite_positiveness, check_symmetry
+from .qs import economic_qs, economic_qs_linear
+from .solve import solve
+from .svd import economic_svd
+from .tri import stl
