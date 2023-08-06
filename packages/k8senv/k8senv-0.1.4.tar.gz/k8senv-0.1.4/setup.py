3dbaf3a42f00d5826d874fe3b20f8ba4ef55278f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:00 +0000

    Add package k8senv/0.1.4/k8senv-0.1.4.tar.gz

---
+++ packages/k8senv/k8senv-0.1.4.tar.gz/k8senv-0.1.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='k8senv',
+    version='0.1.4',
+    url='http://github.com/philipn/k8senv',
+    license='MIT',
+    description='A simple way to manage different Kubernetes contexts',
+    long_description=open('README.md').read(),
+    author='Philip Neustrom',
+    author_email='philipn@gmail.com',
+    zip_safe=False,
+    scripts=['bin/k8senv'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
