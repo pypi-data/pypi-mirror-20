9dc1fc57e029ab938b19644e92e28185c463a220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:14 +0000

    Add package scrapinghub/2.0.0.dev0/scrapinghub-2.0.0.dev0.tar.gz

---
+++ packages/scrapinghub/scrapinghub-2.0.0.dev0.tar.gz/scrapinghub-2.0.0.dev0/scrapinghub/__init__.py
@@ -0,0 +1,22 @@
+__all__ = ["APIError", "Connection", "HubstorageClient",
+           "ScrapinghubClient", "ScrapinghubAPIError",
+           "DuplicateJobError", "BadRequest", "NotFound",
+           "Unauthorized", "ValueTooLarge"]
+
+import pkgutil
+__version__ = pkgutil.get_data(__package__, 'VERSION')
+__version__ = str(__version__.decode('ascii').strip())
+del pkgutil
+
+
+from .legacy import *
+from .hubstorage import HubstorageClient
+from .client import ScrapinghubClient
+from .client.exceptions import (
+    ScrapinghubAPIError,
+    DuplicateJobError,
+    BadRequest,
+    NotFound,
+    Unauthorized,
+    ValueTooLarge,
+)
