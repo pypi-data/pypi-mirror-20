cd2b5f3ea2da1edae02842c9422ac697d6b215f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:13 +0000

    Add package shorttext/0.3.1/shorttext-0.3.1.tar.gz

---
+++ packages/shorttext/shorttext-0.3.1.tar.gz/shorttext-0.3.1/shorttext/__init__.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+thisdir, _ = os.path.split(__file__)
+sys.path.append(thisdir)
+
+import utils
+import data
+import classifiers
+import stack
---
+++ packages/shorttext/shorttext-0.3.1.tar.gz/shorttext-0.3.1/shorttext/utils/gensim_corpora.py
@@ -0,0 +1,22 @@
+import gensim
+from .textpreprocessing import spacy_tokenize as tokenize
+
+def generate_gensim_corpora(classdict, preprocess_and_tokenize=tokenize):
+    """ Generate gensim bag-of-words dictionary and corpus.
+
+    Given a text data, a dict with keys being the class labels, and the values
+    being the list of short texts, in the same format output by `shorttext.data.data_retrieval`,
+    return a gensim dictionary and corpus.
+
+    :param classdict: text data, a dict with keys being the class labels, and each value is a list of short texts
+    :param proprocess_and_tokenize: preprocessor function, that takes a short sentence, and return a list of tokens (Default: `shorttext.utils.tokenize`)
+    :return: a tuple, consisting of a gensim dictionary, a corpus, and a list of class labels
+    :type classdict: dict
+    :type proprocess_and_tokenize: function
+    :rtype: (gensim.corpora.Dictionary, list, list)
+    """
+    classlabels = sorted(classdict.keys())
+    doc = [preprocess_and_tokenize(' '.join(classdict[classlabel])) for classlabel in classlabels]
+    dictionary = gensim.corpora.Dictionary(doc)
+    corpus = [dictionary.doc2bow(doctokens) for doctokens in doc]
+    return dictionary, corpus, classlabels
