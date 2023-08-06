6fecfd6358e592ad88af86c09ef2cb83745203c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:53 +0000

    Add package deeperdighenrydist/1.0.0/deeperdighenrydist-1.0.0.tar.gz

---
+++ packages/deeperdighenrydist/deeperdighenrydist-1.0.0.tar.gz/deeperdighenrydist-1.0.0/deeperdighenrymodule.py
@@ -0,0 +1,13 @@
+﻿"""this is the module "deeperdighenrymodule.py" 
+	and it provides a function named print_lol, 
+	which prints list , even the list included list in itself."""
+
+def print_lol(the_list):
+	#this function takes one positional argument called "the_list", which is any Python list.
+	#each item in the list is printed to the screen on its own line.
+         
+	for each_item in the_list:
+		if isinstance(each_item,list):
+			print_lol(each_item)
+		else:
+			print(each_item)
---
+++ packages/deeperdighenrydist/deeperdighenrydist-1.0.0.tar.gz/deeperdighenrydist-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name = 'deeperdighenrydist',
+	version = '1.0.0',
+	py_modules = ['deeperdighenrymodule'],
+	author = 'henry',
+	author_email = 'henry@gmail.com',
+	description = 'deeper description',
+)
