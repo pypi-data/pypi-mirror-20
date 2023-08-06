e138ba326fcb1e84ae24a594f73cfdcf156f99f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:03 +0000

    Add package undoable/0.3.1/undoable-0.3.1.tar.gz

---
+++ packages/undoable/undoable-0.3.1.tar.gz/undoable-0.3.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='undoable',
+      version='0.3.1',
+      description='Simple Python list and dict with undo/redo',
+      long_description=open("README.rst").read(),
+      url='https://github.com/asrp/undoable',
+      author='asrp',
+      author_email='asrp@email.com',
+      py_modules=['undoable'],
+      keywords='callbacks undo observer')
