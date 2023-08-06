1b8d32105d1467a60e784e88b82a9a6664f75a76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:39 +0000

    Add package Fregger/0.10.1/Fregger-0.10.1.tar.gz

---
+++ packages/Fregger/Fregger-0.10.1.tar.gz/Fregger-0.10.1/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+setup(
+    name='Fregger',
+    version='0.10.1',
+    keywords=('flask', 'flask-restful', 'swagger', 'automatically'),
+    description='Automatically generate Swagger docs for Flask-Restful.',
+    license='MIT',
+    author='rain',
+    author_email='rain@joymud.com',
+    url='https://github.com/AristotleJunior/fregger',
+    platforms='any',
+    packages=['fregger'],
+    install_requires=['Flask-Restful'],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Flask',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
+
