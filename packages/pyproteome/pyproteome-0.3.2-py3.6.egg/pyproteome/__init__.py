9386ac9a561bea73bacde893d8f10627a0a0a7f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:25 +0000

    Add package pyproteome/0.3.2/pyproteome-0.3.2-py3.6.egg

---
+++ packages/pyproteome/pyproteome-0.3.2-py3.6.egg/pyproteome/__init__.py
@@ -0,0 +1,9 @@
+"""
+pyproteome is a Python package for interacting with proteomics data.
+
+It includes modules for loading, processing, and analyzing data by mass
+spectometry. Currently it only supports data produced by MASCOT / Discoverer
+and CAMV.
+"""
+
+from .version import __version__
