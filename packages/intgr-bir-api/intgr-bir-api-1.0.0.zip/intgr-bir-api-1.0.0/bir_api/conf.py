365412a4a48fcf551715e1236a21c19d5c35a647
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:59 +0000

    Add package intgr-bir-api/1.0.0/intgr-bir-api-1.0.0.zip

---
+++ packages/intgr-bir-api/intgr-bir-api-1.0.0.zip/intgr-bir-api-1.0.0/bir_api/conf.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from appconf import AppConf
+
+class BirAppConf(AppConf):
+    class Meta:
+        prefix = 'BIR'
+
+# GUS API key
+
+GUS_KEY = ''
+GUS_URL = 'https://wyszukiwarkaregontest.stat.gov.pl/wsBIR/UslugaBIRzewnPubl.svc'
