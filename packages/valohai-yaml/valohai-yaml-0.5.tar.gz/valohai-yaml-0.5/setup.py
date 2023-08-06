102e8ec528842af2888118c0c6a9d01d32e4dd61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:56 +0000

    Add package valohai-yaml/0.5/valohai-yaml-0.5.tar.gz

---
+++ packages/valohai-yaml/valohai-yaml-0.5.tar.gz/valohai-yaml-0.5/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+dev_dependencies = [
+    'flake8',
+    'isort',
+    'pydocstyle',
+    'pytest-cov',
+]
+
+if __name__ == '__main__':
+    setuptools.setup(
+        name='valohai-yaml',
+        description='Valohai.yaml validation and parsing',
+        version='0.5',
+        url='https://github.com/valohai/valohai-yaml',
+        author='Valohai',
+        maintainer='Aarni Koskela',
+        maintainer_email='akx@iki.fi',
+        license='MIT',
+        install_requires=['jsonschema', 'PyYAML', 'six'],
+        tests_require=dev_dependencies,
+        extras_require={'dev': dev_dependencies},
+        packages=setuptools.find_packages('.', exclude=('*tests*',)),
+        include_package_data=True,
+        entry_points={
+            'console_scripts': [
+                'valohai-yaml = valohai_yaml.__main__:main',
+            ],
+        },
+    )
