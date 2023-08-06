786cdafe19922b64a195e428c17b963803027d3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:22 +0000

    Add package hyperchamber/0.2.8/hyperchamber-0.2.8.tar.gz

---
+++ packages/hyperchamber/hyperchamber-0.2.8.tar.gz/hyperchamber-0.2.8/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='hyperchamber',
+    version='0.2.8',
+    description='Tune and optimize your hyperparameters',
+    author='Martyn Garcia',
+    author_email="martyn@255bits.com",
+    packages=['hyperchamber', 'hyperchamber.io'],
+    install_requires=['requests >= 2.4.2'],
+    url='https://github.com/255bits/hyperchamber',
+    license='MIT',
+    classifiers=[
+                 ],
+)
