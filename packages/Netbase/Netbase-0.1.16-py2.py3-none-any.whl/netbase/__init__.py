75c151d9014c8b71a29d6b6a10b2d41a27339e10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:08 +0000

    Add package Netbase/0.1.16/Netbase-0.1.16-py2.py3-none-any.whl

---
+++ packages/Netbase/Netbase-0.1.16-py2.py3-none-any.whl/netbase/__init__.py
@@ -0,0 +1,11 @@
+# import .netbase
+# import netbase.netbase
+from netbase import world
+world
+# world = net = netbase.Netbase()
+
+#, alle
+net = netbase.net
+cache = netbase.cache
+# import netbase
+# import netbase
