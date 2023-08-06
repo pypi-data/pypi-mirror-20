ab8bef961bf3f4b3a73e7fb1995c4be0bdcfc083
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:30 +0000

    Add package text_oynix/1.1.0/text_oynix-1.1.0.zip

---
+++ packages/text_oynix/text_oynix-1.1.0.zip/text_oynix-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'text_oynix',
+	version = '1.1.0',
+	py_modules = ['text_oynix'],
+	author = 'oynix',
+	author_emial = 'oynix@foxmail.com',
+	url = 'http://www.headfirstlabs.com',
+	description = 'A simple printer of nested lists by oynix@foxmail.com',
+	)
