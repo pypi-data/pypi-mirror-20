e7f7754a9c64a0f135124cdb2c24cec8504ff951
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:15 +0000

    Add package baseZhang/1.1.2/baseZhang-1.1.2.tar.gz

---
+++ packages/baseZhang/baseZhang-1.1.2.tar.gz/baseZhang-1.1.2/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+NAME = 'baseZhang'
+_MAJOR = 1
+_MINOR = 1
+_MICRO = 2
+VERSION = '%d.%d.%d' % (_MAJOR, _MINOR, _MICRO)
+DOWNLOAD = "https://github.com/zhangxulong/baseZhang/dist/baseZhang-" + VERSION + ".tar.gz"
+DESCRIPTION = "My own base util code"
+setup(
+    packages=['baseZhang'],
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description="""A python module for audio and music processing.""",
+    author="ZHANG Xu-long",
+    author_email="fudan0027zxl@gmail.com",
+    license="BSD",
+    url="http://zhangxulong.site",
+    download_url=DOWNLOAD,
+    keywords='audio music sound',
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Multimedia :: Sound/Audio :: Analysis",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+
+    ],
+    install_requires=['numpy',  'matplotlib', 'h5py', 'tqdm', 'pydub', 'scikit-learn', 'Keras'],
+    zip_safe=False,
+)
