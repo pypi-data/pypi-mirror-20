fd004122d0528c5b1265eb82cf0d45e1bc8283fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:12 +0000

    Add package sickrage/8.9.9/sickrage-8.9.9-py2.py3-none-any.whl

---
+++ packages/sickrage/sickrage-8.9.9-py2.py3-none-any.whl/sickrage/notifiers/__init__.py
@@ -0,0 +1,55 @@
+# Author: echel0n <echel0n@sickrage.ca>
+# URL: https://sickrage.ca
+#
+# This file is part of SickRage.
+#
+# SickRage is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SickRage is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with SickRage.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
+import sickrage
+
+
+class srNotifiers(object):
+    @staticmethod
+    def notify_download(ep_name):
+        for n in sickrage.srCore.notifiersDict.values():
+            try:
+                n._notify_download(ep_name)
+            except:
+                continue
+
+    @staticmethod
+    def notify_subtitle_download(ep_name, lang):
+        for n in sickrage.srCore.notifiersDict.values():
+            try:
+                n._notify_subtitle_download(ep_name, lang)
+            except:
+                continue
+
+    @staticmethod
+    def notify_snatch(ep_name):
+        for n in sickrage.srCore.notifiersDict.values():
+            try:
+                n._notify_snatch(ep_name)
+            except:
+                continue
+
+    @staticmethod
+    def notify_version_update(new_version=""):
+        for n in sickrage.srCore.notifiersDict.values():
+            try:
+                n._notify_version_update(new_version)
+            except:
+                continue
