c7f475588d03035cc5faac444fc038d1d282ed09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:31 +0000

    Add package django-afip/2.4.0/django-afip-2.4.0.tar.gz

---
+++ packages/django-afip/django-afip-2.4.0.tar.gz/django-afip-2.4.0/CHANGELOG.rst
@@ -0,0 +1,14 @@
+Changelog
+=========
+
+This file contains a brief summary of new features and dependency changes or
+releases, in reverse chronological order.
+
+2.4.0
+-----
+
+* Raise ``CertificateExpired``, ``UntrustedCertificate`` or
+  ``AuthenticationError`` when attempting an authentication fail.
+* The field ``ReceiptEntry.amount`` has been renamed to ``quantity``.
+* Add a links to documentation on where to obtain the AFIP WS certificates.
+* Introduce this changelog.
---
+++ packages/django-afip/django-afip-2.4.0.tar.gz/django-afip-2.4.0/django_afip/exceptions.py
@@ -0,0 +1,34 @@
+from . import parsers
+
+
+class AfipException(Exception):
+    """
+    Wraps around errors returned by AFIP's WS.
+    """
+
+    def __init__(self, response):
+        Exception.__init__(self, 'Error {}: {}'.format(
+            response.Errors.Err[0].Code,
+            parsers.parse_string(response.Errors.Err[0].Msg),
+        ))
+
+
+class AuthenticationError(Exception):
+    """
+    Raised when there is a non-specific error during an authentication attempt.
+    """
+    pass
+
+
+class CertificateExpired(AuthenticationError):
+    """
+    Raised when an authentication was attempted with an expired certificate.
+    """
+    pass
+
+
+class UntrustedCertificate(AuthenticationError):
+    """
+    Raise when an untrusted certificate is used in an authentication attempt.
+    """
+    pass
