ab767c9733b80a185f7376b6f9c2ae3abc188b1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:02 +0000

    Add package dsert/0.1.3/dsert-0.1.3.tar.gz

---
+++ packages/dsert/dsert-0.1.3.tar.gz/dsert-0.1.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dsert',
+    version='0.1.3',
+    packages=find_packages(),
+    url='https://github.com/paxos-bankchain/dsert',
+    license='MIT',
+    author='Paxos Trust Company, LLC',
+    author_email='pypi@paxos.com',
+    description='Test all fields of a python dictionary',
+    classifiers=[
+        'Topic :: Software Development :: Testing',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
