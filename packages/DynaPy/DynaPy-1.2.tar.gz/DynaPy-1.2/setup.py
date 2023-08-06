e21a112f749d48f51736f4d1fc5e1e5f07fe8b72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:09 +0000

    Add package DynaPy/1.2/DynaPy-1.2.tar.gz

---
+++ packages/DynaPy/DynaPy-1.2.tar.gz/DynaPy-1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='DynaPy',
+      version='1.2',
+      description='Structural dynamics made easy with Python',
+      url='https://github.com/MarioRaul/DynaPy',
+      author='Mario Freitas',
+      author_email='mariofreitas.enc@gmail.com',
+      license='MIT',
+      # packages=['DynaPy'],
+      install_requires=[
+          'numpy',
+          'matplotlib'
+      ])
