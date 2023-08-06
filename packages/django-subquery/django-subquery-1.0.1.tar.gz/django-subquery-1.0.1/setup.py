829ee820ab0baea2cf99228ab3d07b3e164fed34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:37 +0000

    Add package django-subquery/1.0.1/django-subquery-1.0.1.tar.gz

---
+++ packages/django-subquery/django-subquery-1.0.1.tar.gz/django-subquery-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='django-subquery',
+    version='1.0.1',
+    description='SubQuery support in Django < 1.11',
+    url='https://github.com/schinckel/django_subquery/',
+    author='Matthew Schinckel',
+    author_email='matt@schinckel.net',
+    packages=['django_subquery']
+)
