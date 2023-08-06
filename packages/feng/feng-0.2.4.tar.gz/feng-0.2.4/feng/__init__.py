7700ffecea8f89660bf25e537750769a6aec9570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:51 +0000

    Add package feng/0.2.4/feng-0.2.4.tar.gz

---
+++ packages/feng/feng-0.2.4.tar.gz/feng-0.2.4/feng/__init__.py
@@ -0,0 +1,7 @@
+from . import pipeline
+from . import importance
+from . import preprocessing
+
+__title__ = 'feng'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2016 Alexander Svanevik, Anders Aagard, Mats Julian Olsen'
