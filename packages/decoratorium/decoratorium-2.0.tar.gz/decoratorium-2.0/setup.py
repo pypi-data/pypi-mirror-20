9822425b2eda5b34a0cfb499f19aef09c7320996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:20 +0000

    Add package decoratorium/2.0/decoratorium-2.0.tar.gz

---
+++ packages/decoratorium/decoratorium-2.0.tar.gz/decoratorium-2.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+setup(
+    name='decoratorium',
+    version='2.0',
+    description='Decorator class implementation.',
+    url='https://github.com/lig/decoratorium',
+    author='Serge Matveenko',
+    author_email='s@matveenko.ru',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only'
+    ],
+    keywords='python decorator class klass',
+    py_modules=['decoratorium'],
+    install_requires=['decorator'],
+    extras_require={
+        'test': ['pytest'],
+    },
+)
