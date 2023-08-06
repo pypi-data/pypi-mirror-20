7fe067b435b00ceeb4016d415473f3bc6b38cd08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:55 +0000

    Add package youngshine_nester/1.2.1/youngshine_nester-1.2.1.zip

---
+++ packages/youngshine_nester/youngshine_nester-1.2.1.zip/youngshine_nester-1.2.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name = 'youngshine_nester',
+	version = '1.2.1',
+	py_modules = ['youngshine_nester'],
+	author = 'youngshine',
+	author_email = '850759073@qq.com',
+	url = 'http://www.headfirstlabs.com',
+	description = 'A Simple printer of nested lists')
---
+++ packages/youngshine_nester/youngshine_nester-1.2.1.zip/youngshine_nester-1.2.1/youngshine_nester.py
@@ -0,0 +1,14 @@
+#coding:utf-8
+"""
+zhushi
+"""
+def print_lol(the_list,level=0):
+        """zhushi"""
+        for each_item in the_list:
+                if isinstance(each_item,list):
+                        print_lol(each_item,level+1);
+                else:
+                	for tab_stop in range(level):
+                		print("\t",end='');
+                	print(each_item)
+			
