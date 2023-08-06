46678dcde1a89c1ff4fcf6bc70755c653bb2e175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:43 +0000

    Add package documenteer/0.1.11/documenteer-0.1.11.tar.gz

---
+++ packages/documenteer/documenteer-0.1.11.tar.gz/documenteer-0.1.11/setup.py
@@ -0,0 +1,56 @@
+from setuptools import setup, find_packages
+import os
+
+
+packagename = 'documenteer'
+description = 'Tools for LSST DM documentation projects'
+author = 'Jonathan Sick'
+author_email = 'jsick@lsst.org'
+license = 'MIT'
+url = 'https://github.com/lsst-sqre/documenteer'
+version = '0.1.11'
+
+
+def read(filename):
+    full_filename = os.path.join(
+        os.path.abspath(os.path.dirname(__file__)),
+        filename)
+    return open(full_filename).read()
+
+
+long_description = read('README.rst')
+
+
+setup(
+    name=packagename,
+    version=version,
+    description=description,
+    long_description=long_description,
+    url=url,
+    author=author,
+    author_email=author_email,
+    license=license,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='sphinx documentation lsst',
+    packages=find_packages(exclude=['docs', 'tests*']),
+    install_requires=['future',
+                      'Sphinx>=1.5.1',
+                      'PyYAML',
+                      'sphinx-prompt',
+                      'sphinxcontrib-bibtex',
+                      'GitPython',
+                      'astropy-helpers>=0.2.0',
+                      'breathe==4.4.0',
+                      'lsst-dd-rtd-theme==0.1.0',
+                      'lsst-sphinx-bootstrap-theme>=0.1.0'],
+    tests_require=['pytest',
+                   'pytest-cov',
+                   'pytest-flake8',
+                   'pytest-mock'],
+)
