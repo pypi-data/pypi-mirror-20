e9df27e5f8538de7567a7cf610003e7ec18e23fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:21 +0000

    Add package springserve/0.3.5/springserve-0.3.5.tar.gz

---
+++ packages/springserve/springserve-0.3.5.tar.gz/springserve-0.3.5/springserve/_demand.py
@@ -0,0 +1,20 @@
+
+from . import _VDAPIService
+
+class _DemandTagAPI(_VDAPIService):
+
+    __API__ = "demand_tags"
+
+class _DemandPartnerAPI(_VDAPIService):
+
+    __API__ = "demand_partners"
+
+class _DemandGroupAPI(_VDAPIService):
+
+    __API__ = "demand_groups"
+
+
+
+
+
+
