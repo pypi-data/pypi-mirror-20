ea619836d0fd5ccbbeb39dd7230a1b99750d2083
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:07 +0000

    Add package polymerParser/0.2/polymerParser-0.2.tar.gz

---
+++ packages/polymerParser/polymerParser-0.2.tar.gz/polymerParser-0.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'polymerParser',
+  scripts=['polymerParser'],
+  version = '0.2',
+  description = 'A small json parser for imports in polymer proyects',
+  author = 'Guillermo Carrera Trasobares',
+  author_email = 'guillermo@arkandas.com',
+  url = 'https://github.com/arkandas/pythonparser', # use the URL to the github repo
+  keywords = ['testing', 'logging'], # arbitrary keywords
+  classifiers = [],
+)
