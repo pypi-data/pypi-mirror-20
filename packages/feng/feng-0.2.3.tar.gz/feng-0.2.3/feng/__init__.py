3192360f92fdf9a712b0cfed27e95b34ca39e9e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:43 +0000

    Add package feng/0.2.3/feng-0.2.3.tar.gz

---
+++ packages/feng/feng-0.2.3.tar.gz/feng-0.2.3/feng/__init__.py
@@ -0,0 +1,9 @@
+from . import pipeline
+from . import importance
+from . import preprocessing
+
+__title__ = 'feng'
+__version__ = '0.2.3'
+__author__ = 'Alexander Svanevik, Anders Aagard, Mats Julian Olsen'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2016 Alexander Svanevik, Anders Aagard, Mats Julian Olsen'
---
+++ packages/feng/feng-0.2.3.tar.gz/feng-0.2.3/setup.py
@@ -0,0 +1,36 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import feng
+
+SHORT = 'Feng is a module for feature engineering.'
+LONG = SHORT + ' For more info check out the README at \'github.com/mewwts/feng\'.'
+
+setup(
+    name='feng',
+    version=feng.__version__,
+    install_requires=[
+        'scipy',
+        'numpy',
+        'pandas',
+        'scikit-learn==0.18'
+    ],
+    packages=[
+        'feng',
+        'feng.importance',
+        'feng.pipeline',
+        'feng.preprocessing'
+    ],
+    url='https://github.com/mewwts/feng',
+    author=feng.__author__,
+    author_email='mats@plysjbyen.net',
+    classifiers=(
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+    ),
+    description=SHORT,
+    long_description=LONG,
+    test_suite='test_feng'
+)
