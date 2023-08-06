298b637e205b6a20cde61ce72eb3bb21dd62f967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:03 +0000

    Add package symmetry-representation/0.0.0b1/symmetry-representation-0.0.0b1.tar.gz

---
+++ packages/symmetry-representation/symmetry-representation-0.0.0b1.tar.gz/symmetry-representation-0.0.0b1/symmetry_representation/_sym_op.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Author:  Dominik Gresch <greschd@gmx.ch>
+
+from collections import namedtuple
+
+from fsc.export import export
+
+@export
+class SymmetryOperation(namedtuple('SymmetryOperationBase', ['rotation_matrix', 'repr'])):
+    """
+    Describes a symmetry operation.
+
+    :param rotation_matrix: Real-space rotation matrix of the symmetry.
+    :type rotation_matrix: array
+
+    :param repr_matrix: Matrix of the representation corresponding to the symmetry operation.
+    :type repr_matrix: array
+
+    :param repr_has_cc: Specifies whether the representation contains a complex conjugation.
+    :type repr_has_cc: bool
+
+    :ivar rotation_matrix: Real-space rotation matrix.
+    :vartype rotation_matrix: array
+
+    :ivar repr: Symmetry representation.
+    :vartype repr: :class:`.Representation`
+
+    .. note :: Currently, only point-group symmetries are implemented.
+    """
+    def __new__(cls, *, rotation_matrix, repr_matrix, repr_has_cc=False):
+        return super().__new__(
+            cls,
+            rotation_matrix=rotation_matrix,
+            repr=Representation(matrix=repr_matrix, has_cc=repr_has_cc)
+        )
+
+@export
+class Representation(namedtuple('RepresentationBase', ['matrix', 'has_cc'])):
+    """
+    Describes an (anti-)unitary representation of a symmetry operation.
+    """
+    def __new__(cls, matrix, has_cc=False):
+        return super().__new__(
+            cls,
+            matrix=matrix,
+            has_cc=has_cc
+        )
