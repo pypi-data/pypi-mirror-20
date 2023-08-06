9437593faa01c543b133be9f21a34a21064728f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:58 +0000

    Add package django-froala-editor/2.5.1/django-froala-editor-2.5.1.tar.gz

---
+++ packages/django-froala-editor/django-froala-editor-2.5.1.tar.gz/django-froala-editor-2.5.1/froala_editor/__init__.py
@@ -0,0 +1,10 @@
+PLUGINS = (
+    'align', 'char_counter', 'code_beautifier', 'code_view', 'colors', 'draggable', 'emoticons', 'entities', 'file', 'font_family',
+    'font_size', 'fullscreen', 'help', 'image', 'image_manager', 'inline_style', 'line_breaker', 'link', 'lists',
+    'paragraph_format', 'paragraph_style', 'print', 'quick_insert', 'quote', 'save', 'special_characters', 'table', 'url', 'video',          'word_paste'
+)
+
+PLUGINS_WITH_CSS = (
+    'char_counter', 'code_view', 'colors', 'draggable', 'emoticons', 'file', 'fullscreen', 'image', 'image_manager', 'line_breaker',
+    'quick_insert', 'table', 'special_characters', 'video', 'help'
+)
