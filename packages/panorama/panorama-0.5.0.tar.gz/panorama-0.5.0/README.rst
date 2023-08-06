4b842c933e521be5028600f4e9f89fd62d9c4e3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:47 +0000

    Add package panorama/0.5.0/panorama-0.5.0.tar.gz

---
+++ packages/panorama/panorama-0.5.0.tar.gz/panorama-0.5.0/README.rst
@@ -0,0 +1,28 @@
+panorama
+========
+
+Create panorama in one run. Uses hugin for the real work.
+
+Installation
+------------
+.. code-block:: shell
+
+    pip install panorama
+
+    # or if you love programming
+
+    pip install -e git+https://github.com/guettli/panorama.git#egg=panorama
+
+Usage
+-----
+.. code-block:: shell
+
+    create-panorama-from-jpg-files-in-directory file_containing_jpg_files
+
+Feedback
+--------
+
+Please use the github issue tracker for feedback.
+
+https://github.com/guettli/panorama/issues
+
---
+++ packages/panorama/panorama-0.5.0.tar.gz/panorama-0.5.0/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import os
+from codecs import open
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='panorama',
+
+    version='0.5.0',
+
+    description='Create panorama in one run. Uses hugin for the real work.',
+
+    long_description=long_description,
+
+    url='https://github.com/guettli/panorama',
+
+    # Author details
+    author='Thomas Guettler',
+    author_email='guettliml.panorama@thomas-guettler.de',
+
+    license='Apache Software License',
+
+    packages=find_packages(),
+
+    install_requires=[
+        'python-resize-image',
+    ],
+
+    classifiers=[
+        'Programming Language :: Python :: 2',
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'create-panorama-from-jpg-files-in-directory=panorama.panorama:main',
+        ],
+    },
+)
