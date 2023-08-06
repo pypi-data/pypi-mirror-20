aa0312430247a597a7a74b70d3f86c1fbca2e057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:58 +0000

    Add package job_tree/0.2.2/job_tree-0.2.2.tar.gz

---
+++ packages/job_tree/job_tree-0.2.2.tar.gz/job_tree-0.2.2/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017 Ben Lindsay <benjlindsay@gmail.com>
+
+from distutils.core import setup
+
+desc = 'A module for automating flat or hierarchical job creation and submission'
+
+with open('README.rst', 'r') as f:
+    long_desc = f.read()
+
+setup(
+  name = 'job_tree',
+  packages = ['job_tree'],
+  version = '0.2.2',
+  description = desc,
+  long_description = long_desc,
+  requires = ['pandas'],
+  install_requires = ['pandas'],
+  author = 'Ben Lindsay',
+  author_email = 'benjlindsay@gmail.com',
+  url = 'https://github.com/benlindsay/job_tree',
+  keywords = ['workflow', 'simulations'],
+  classifiers = [],
+)
