77efe952af7f8f750c7706ed469672c17c4c5e62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:25 +0000

    Add package tariff-russianpost/2.0.3/tariff-russianpost-2.0.3.tar.gz

---
+++ packages/tariff-russianpost/tariff-russianpost-2.0.3.tar.gz/tariff-russianpost-2.0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tariff-russianpost',
+    version='2.0.3',
+    packages=find_packages(),
+    author='Dmitry Kalinin',
+    author_email='vladimirovich@pbrf.ru',
+    url='https://github.com/pbrf/tariff-russianpost',
+)
