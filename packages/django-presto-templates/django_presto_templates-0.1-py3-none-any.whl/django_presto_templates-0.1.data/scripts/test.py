ecc1e81af6c731635352e040364ec31dac2b7f81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:16 +0000

    Add package django-presto-templates/0.1/django_presto_templates-0.1-py3-none-any.whl

---
+++ packages/django-presto-templates/django_presto_templates-0.1-py3-none-any.whl/django_presto_templates-0.1.data/scripts/test.py
@@ -0,0 +1,9 @@
+print("Project import!")
+
+def test_message():
+    '''
+    This is my first PIP package, so I'm seeing that it works.
+    This will just print out "Hello World" but I promise I'm not just going
+    to take up space on pip: I'll turn this into something useful soon!
+    '''
+    return "Hello World!"
