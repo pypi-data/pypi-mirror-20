d2bcc32308f60966a67ad165f0e17c5ba60c8517
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:17 +0000

    Add package baseZhang/0.1.4b/baseZhang-0.1.4b.tar.gz

---
+++ packages/baseZhang/baseZhang-0.1.4b.tar.gz/baseZhang-0.1.4b/demo.py
@@ -0,0 +1,9 @@
+from basecodes import *
+from getMFCC import *
+
+data_dir = init_data_dir()
+sig, rate = wavread(data_dir + '/test.wav')
+savefig(data_dir + '/test.png', [sig], False)
+
+mfcc_feat = calcMFCC_delta_delta(sig, rate)
+print(mfcc_feat.shape)
