92353eb916825db3ac0510fbae0c7dba389dd082
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:51 +0000

    Add package hierarchical/0.0.8/hierarchical-0.0.8.tar.gz

---
+++ packages/hierarchical/hierarchical-0.0.8.tar.gz/hierarchical-0.0.8/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='hierarchical',
+    version='0.0.8',
+    description='Hierarchical interface to Python dictionaries',
+    long_description='This package provides a file-system like interface for Python dictionaries, providing file/directory key access.',
+    author='Eric Wong',
+    author_email='ericwong@cs.cmu.edu',
+    platforms=['any'],
+    license="Apache 2.0",
+    url='https://github.com/locuslab/hierarchical',
+    packages=find_packages(),
+    install_requires=[
+    ]
+)
