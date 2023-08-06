68a66963d4c9ee9f546feef980bbea99590398de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:20 +0000

    Add package response-mocker/1.0.1/response_mocker-1.0.1.tar.gz

---
+++ packages/response-mocker/response_mocker-1.0.1.tar.gz/response_mocker-1.0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name='response_mocker',
+    version='1.0.1',
+    description='A simple mocking library for Requests',
+    url='https://github.com/menge101/response_mocker',
+    author='Nathan Menge',
+    author_email='nathan.menge@gmail.com',
+    license='MIT',
+    packages=setuptools.find_packages(exclude=['tests*']),
+    zip_safe=False,
+    install_requires=[]
+)
