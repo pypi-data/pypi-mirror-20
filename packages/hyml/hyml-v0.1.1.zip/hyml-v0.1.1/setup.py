cac9c772b4e0a2a6b51d13a89a9a351e6e8c9d27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:26 +0000

    Add package hyml/v0.1.1/hyml-v0.1.1.zip

---
+++ packages/hyml/hyml-v0.1.1.zip/hyml-v0.1.1/setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+
+install_requires = ['hy>=0.12.1']
+
+#python setup.py register -r pypitest
+#python setup.py register -r pypi
+
+setup(
+  name = 'hyml',
+  
+  packages = ['hyml'],
+  package_dir = {'hyml': 'hyml'},
+  package_data = {
+    'hyml': ['*.hy']
+  },
+  
+  version = 'v0.1.1',
+  description = 'HyML - XML / (X)HTML generator for Hy',
+  author = 'Marko Manninen',
+  author_email = 'elonmedia@gmail.com',
+
+  url = 'https://github.com/markomanninen/hyml', # use the URL to the github repo
+  download_url = 'https://github.com/markomanninen/hyml/archive/v0.1.1.tar.gz', # I'll explain this in a second
+  keywords = ['hylang', 'python', 'markup language', 'dsl', 'xml', 'html', 'xhtml'], # arbitrary keywords
+  platforms = ['any'],
+  
+  classifiers = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Lisp",
+    "Topic :: Software Development :: Libraries",
+  ]
+)
