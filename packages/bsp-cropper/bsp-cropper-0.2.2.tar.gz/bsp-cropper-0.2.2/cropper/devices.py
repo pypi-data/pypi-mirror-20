88573dd77c1087c77e9fd29e69e728ee8fe4e550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:21 +0000

    Add package bsp-cropper/0.2.2/bsp-cropper-0.2.2.tar.gz

---
+++ packages/bsp-cropper/bsp-cropper-0.2.2.tar.gz/bsp-cropper-0.2.2/cropper/devices.py
@@ -0,0 +1,30 @@
+DEVICES = {
+    "iPhone": {
+        "iOS-3.5-in": {
+            "resize": (852, 1136),
+            "crop": (212, 176)
+        },
+        "iOS-4-in": {
+            "resize": (852, 1136),
+            "crop": (212, 0)
+        },
+        "iOS-4.7-in": {
+            "resize": (1000, 1334),
+            "crop": (250, 0)
+        },
+        "iOS-5.5-in": {
+            "resize": (1656, 2208),
+            "crop": (414, 0)
+        },
+    },
+    "iPad": {
+        "iOS-iPad": {
+            "resize": (1536, 2048),
+            "crop": None
+        },
+        "iOS-iPad-Pro": {
+            "resize": None,
+            "crop": None
+        }
+    }
+}
