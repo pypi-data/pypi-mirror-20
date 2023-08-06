b28ab8122eb6370312e2fe210e78804c5e18c703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:47 +0000

    Add package pilote/0.0.5/pilote-0.0.5.tar.gz

---
+++ packages/pilote/pilote-0.0.5.tar.gz/pilote-0.0.5/pilote/delhivery/services.py
@@ -0,0 +1,109 @@
+"""
+File: services
+Author: Vineet Kumar Dubey <vineetdubey@gofynd.com>
+Date: 04/03/2017
+
+It contains all the core service methods required for the delhivery.
+"""
+
+from . import DelhiveryBase
+from constants import DELHIVERY_CREATE_PACKAGE, DELHIVERY_CANCEL_PACKAGE, DELHIVERY_CREATE_PICKUP, \
+    DELHIVERY_CREATE_PACKAGE_HEADERS, DELHIVERY_CANCEL_PACKAGE_HEADERS, DELHIVERY_BASE_URL, DELHIVERY_DEBUG_BASE_URL
+from helper import DelhiveryXMLHelper
+
+
+class CreateShipment(DelhiveryBase):
+
+    """
+    Create the new shipment in delhivery.
+    """
+    def __init__(self, user_profile):
+        super(CreateShipment, self).__init__(user_profile)
+
+    def _prepare_pre_request_data(self, data):
+
+        """
+        Prepare data for request.
+        TODO: Serialization to be added.
+        :return:
+        """
+        self.prepared_data = data
+        self.url = DELHIVERY_BASE_URL + DELHIVERY_CREATE_PACKAGE.format(self.profile.api_token)
+        if self.profile.debug:
+            self.url = DELHIVERY_DEBUG_BASE_URL + DELHIVERY_CREATE_PACKAGE.format(self.profile.api_token)
+        self.method = 'POST'
+        self.headers = DELHIVERY_CREATE_PACKAGE_HEADERS
+        self.logger.info("Payload received for creating package\n{}".format(self.prepared_data))
+
+    def _prepare_response(self):
+        if self.response:
+            self.response = self.response.json()
+
+
+class CancelShipment(DelhiveryBase):
+    def __init__(self, user_profile):
+        super(CancelShipment, self).__init__(user_profile)
+
+    def _prepare_pre_request_data(self, data):
+
+        """
+        Prepare data for request.
+        TODO: Serialization to be added.
+        :return:
+        """
+        self.prepared_data = data
+        self.url = DELHIVERY_BASE_URL + DELHIVERY_CANCEL_PACKAGE
+        if self.profile.debug:
+            self.url = DELHIVERY_DEBUG_BASE_URL + DELHIVERY_CANCEL_PACKAGE
+        self.method = 'POST'
+        self.headers['Authorization'] = DELHIVERY_CANCEL_PACKAGE_HEADERS['Authorization'].format(self.profile.api_token)
+        self.logger.info("Payload received to cancel package\n{}".format(self.prepared_data))
+
+    def _prepare_response(self):
+        """
+        This method is overridden.
+        Prepare response in json.
+        :return: None
+        """
+        formatted_response = {}
+        if self.response:
+            xml_response = self.response.content
+            formatted_response = DelhiveryXMLHelper(xml_response).parse()
+            if not formatted_response.get('status'):
+                formatted_response['status'] = 'False'
+        self.response = formatted_response
+
+
+class CreatePickup(DelhiveryBase):
+    def __init__(self, user_profile):
+        super(CreatePickup, self).__init__(user_profile)
+
+    def _prepare_pre_request_data(self, data):
+
+        """
+        Prepare data for request.
+        TODO: Serialization to be added.
+        :return:
+        """
+        self.prepared_data = data
+        self.url = DELHIVERY_BASE_URL + DELHIVERY_CREATE_PICKUP
+        if self.profile.debug:
+            self.url = DELHIVERY_DEBUG_BASE_URL + DELHIVERY_CREATE_PICKUP
+        self.method = 'POST'
+        self.headers = DELHIVERY_CREATE_PACKAGE_HEADERS
+        self.logger.info("Payload received for creating pickup\n{}".format(self.prepared_data))
+
+    def _prepare_response(self):
+        """
+        This method is overridden.
+        Prepare response in json.
+        :return: None
+        """
+        if self.response.ok:
+            formatted_response = self.response.json()
+            formatted_response['success'] = True
+        else:
+            formatted_response = self.response.json()
+            formatted_response['success'] = False
+
+        self.response = formatted_response
---
+++ packages/pilote/pilote-0.0.5.tar.gz/pilote-0.0.5/test/test_ecomm.py
@@ -0,0 +1,42 @@
+from ecomm.services import CreateShipment, CancelShipment
+from ecomm.constants import TEST_CREDS
+
+
+def test_ecomm_create_package_success():
+    """
+    TODO: Success case to be written with a dummy set of valid data.
+    :return:
+    """
+    create_package = CreateShipment(TEST_CREDS)
+    response = create_package.send_request({})
+    return response
+
+
+def test_ecomm_create_package_failure():
+    """
+    failure case for ecomm create package.
+    :return:
+    """
+    create_package = CreateShipment(TEST_CREDS)
+    response = create_package.send_request({})
+    return response
+
+
+def test_ecomm_cancel_package_success():
+    """
+    TODO: Success case to be written with a dummy set of valid data.
+    :return:
+    """
+    cancel_package = CancelShipment(TEST_CREDS)
+    response = cancel_package.send_request({})
+    return response
+
+
+def test_ecomm_cancel_package_failure():
+    """
+    failure case for ecomm cancel package.
+    :return:
+    """
+    cancel_package = CancelShipment(TEST_CREDS)
+    response = cancel_package.send_request({})
+    return response
