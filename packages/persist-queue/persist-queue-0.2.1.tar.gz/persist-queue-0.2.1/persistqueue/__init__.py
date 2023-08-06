935474b7df320dba40654756db85d1baec377fc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:25 +0000

    Add package persist-queue/0.2.1/persist-queue-0.2.1.tar.gz

---
+++ packages/persist-queue/persist-queue-0.2.1.tar.gz/persist-queue-0.2.1/persistqueue/__init__.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+__author__ = 'Peter Wang'
+__license__ = 'BSD License'
+__version__ = '0.2.1'
+
+
+from .exceptions import Empty, Full  # noqa
+
+from .queue import Queue  # noqa
+from .sqlqueue import SQLiteQueue, FIFOSQLiteQueue, FILOSQLiteQueue  # noqa
+
+__all__ = ["Queue", "SQLiteQueue", "FIFOSQLiteQueue", "FILOSQLiteQueue",
+           "Empty", "Full", "__author__", "__license__", "__version__"]
