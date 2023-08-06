c1fa46e7f93bf707d69997f0489f8097e59d7a9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:46 +0000

    Add package pumpwood-viewutils/0.3.9.6/pumpwood-viewutils-0.3.9.6.tar.gz

---
+++ packages/pumpwood-viewutils/pumpwood-viewutils-0.3.9.6.tar.gz/pumpwood-viewutils-0.3.9.6/pumpwood_viewutils/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import unicode_literals
+
+from pumpwood_viewutils import views, query, routers, serializers, rest_function, serve
+
+__all__ = ['views', 'query', 'routers', 'serializers', 'rest_function', 'serve']
