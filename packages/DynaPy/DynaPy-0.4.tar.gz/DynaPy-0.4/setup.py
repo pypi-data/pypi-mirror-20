8093599edb1daeb238130c9c9528a0adc704a07e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:05 +0000

    Add package DynaPy/0.4/DynaPy-0.4.tar.gz

---
+++ packages/DynaPy/DynaPy-0.4.tar.gz/DynaPy-0.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='DynaPy',
+      version='0.4',
+      description='Structural dynamics made easy with Python',
+      url='https://github.com/MarioRaul/DynaPy',
+      # download_url='https://github.com/nasseralkmim/elastopy/releases/tag/0.2',
+      author='Mario Freitas',
+      author_email='mariofreitas.enc@gmail.com',
+      license='MIT',
+      # packages=['DynaPy'],
+      install_requires=[
+          'numpy',
+          'matplotlib'
+      ])
