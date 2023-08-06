a71a3f7b4de79eace631b28a86abe95430c1c8e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:56 +0000

    Add package parquet2hive_server/0.0.1/parquet2hive_server-0.0.1.tar.gz

---
+++ packages/parquet2hive_server/parquet2hive_server-0.0.1.tar.gz/parquet2hive_server-0.0.1/parquet2hive_server/settings.py
@@ -0,0 +1,6 @@
+debug = True
+server_dns = 'ec2-54-214-108-235.us-west-2.compute.amazonaws.com'
+server_port = 5129
+secret_key = 'secret'
+secret_value_bucket = 'telemetry-test-bucket'
+secret_value_key = 'frank/secret'
