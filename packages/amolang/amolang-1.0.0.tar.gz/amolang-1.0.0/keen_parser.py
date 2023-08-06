734985dae9596fb60a6a87a2f5e8b43b55ed2bc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:02 +0000

    Add package amolang/1.0.0/amolang-1.0.0.tar.gz

---
+++ packages/amolang/amolang-1.0.0.tar.gz/amolang-1.0.0/keen_parser.py
@@ -0,0 +1,7 @@
+''' 주석이랍니다 '''
+def print_lol(the_list):
+    for item in the_list:
+        if isinstance(item, list):
+            print_lol(item)
+        else:
+            print(item)
---
+++ packages/amolang/amolang-1.0.0.tar.gz/amolang-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'amolang',
+    version ='1.0.0',
+    py_modules = ['keen_parser'],
+    author = 'keen',
+    author_email= 'ehrudxo@gmail.com',
+    url='http://headfirst.com',
+    description= 'list_parser'
+)
