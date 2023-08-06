17c1660038a21d99011508eb4a501bda2a47e7d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:37 +0000

    Add package networkutil/1.5.0/networkutil-1.5.0.tar.gz

---
+++ packages/networkutil/networkutil-1.5.0.tar.gz/networkutil-1.5.0/networkutil/resources/schema/__init__.py
@@ -0,0 +1,7 @@
+import os
+
+base_dir = u'{dir}{sep}'.format(dir=os.path.dirname(os.path.realpath(__file__)),
+                                sep=os.sep)
+
+endpoints = u'{base}{filename}'.format(base=base_dir, filename=u'endpoints.json')
+jumpbox = u'{base}{filename}'.format(base=base_dir, filename=u'jumpbox.json')
