b8ce245fb610a19141fbf6467ebfdee12cef918f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:35 +0000

    Add package tock/0.15/tock-0.15.tar.gz

---
+++ packages/tock/tock-0.15.tar.gz/tock-0.15/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='tock',
+      version='0.15',
+      description='Theory of Computing Toolkit',
+      author='David Chiang',
+      author_email='dchiang@nd.edu',
+      url='https://github.org/ND-CSE-30151/tock',
+      license='MIT',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Education',
+        'Topic :: Education',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        ],
+      packages=['tock'],
+      install_requires=['six', 'openpyxl'],
+      )
