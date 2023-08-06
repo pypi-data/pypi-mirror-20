118c15db31f3525839ad1d214c8209c58f4972be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:54 +0000

    Add package BroadCast/1.1.1/BroadCast-1.1.1.tar.gz

---
+++ packages/BroadCast/BroadCast-1.1.1.tar.gz/BroadCast-1.1.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='BroadCast',
+    version= '1.1.1',
+    description='A Python Package which can be used to BroadCast email .',
+    author='Nishchith Shetty',
+    author_email='inishchith@gmail.com',
+    url='https://github.com/inishchith/BroadCastMail',
+    packages=['BroadCast'],
+)
