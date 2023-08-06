9526ed64c8c4647c960d8b0d376210902359e55b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:32 +0000

    Add package feincms-mailchimp/0.1.2/feincms_mailchimp-0.1.2.tar.gz

---
+++ packages/feincms-mailchimp/feincms_mailchimp-0.1.2.tar.gz/feincms_mailchimp-0.1.2/feincms_mailchimp/models.py
@@ -0,0 +1,63 @@
+from django.conf import settings
+from django.db import models
+from django.template.loader import render_to_string
+from django.utils.translation import ugettext_lazy as _
+
+
+LANGUAGES = settings.LANGUAGES
+
+
+class MailchimpFormContent(models.Model):
+
+    class Meta:
+        abstract = True
+        verbose_name = "Mailchimp Form"
+        verbose_name_plural = "Mailchimp Forms"
+
+    post_url = models.CharField(
+        _('POST URL'),
+        max_length=2100,
+        null=True)
+
+    user_id = models.CharField(
+        _('User ID'),
+        max_length=32,
+        null=True)
+
+    list_id = models.CharField(
+        _('List ID'),
+        max_length=32,
+        null=True)
+
+    placeholder = models.CharField(
+        _('Placeholder'),
+        max_length=50,
+        blank=True, null=True)
+
+    description = models.TextField(
+        _('Description'),
+        blank=True, null=True)
+
+    def process(self, request, **kwargs):
+        if request.method == 'GET':
+            self.email = request.GET.get('email')
+
+        email = getattr(self, 'email', '')
+        template_vars = {
+            'post_url': self.post_url,
+            'user_id': self.user_id,
+            'list_id': self.list_id,
+            'description': self.description,
+            'placeholder': self.placeholder,
+            'lang': self.lang,
+            'form_id': id(self),
+            'email': email,
+        }
+
+        self.rendered_output = render_to_string(
+            'feincms_mailchimp/signup.html',
+            template_vars
+        )
+
+    def render(self, **kwargs):
+        return getattr(self, 'rendered_output', '')
