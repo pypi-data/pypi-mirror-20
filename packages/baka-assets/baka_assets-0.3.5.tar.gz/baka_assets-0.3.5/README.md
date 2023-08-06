b6eda689db88dab8f77a9db3bf77fb27aeea1395
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:50 +0000

    Add package baka-assets/0.3.5/baka_assets-0.3.5.tar.gz

---
+++ packages/baka-assets/baka_assets-0.3.5.tar.gz/baka_assets-0.3.5/README.md
@@ -0,0 +1,3 @@
+## Baka assets add-ons
+
+[baka_assets](https://github.com/suryakencana/baka_assets) is add-ons baka framework built top of pyramid for webassets.
---
+++ packages/baka-assets/baka_assets-0.3.5.tar.gz/baka_assets-0.3.5/setup.py
@@ -0,0 +1,44 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.md')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'pyramid',
+    'webassets',
+    'six',
+    'PyYAML',
+    'pyScss',
+    'PyExecJS',
+    'jsmin',
+    'cssmin'
+    ]
+
+setup(name='baka_assets',
+      version='0.3.5',
+      description='baka_assets',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web pyramid baka_assets',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=requires,
+      test_suite="baka_assets",
+      entry_points="""\
+      """,
+      )
