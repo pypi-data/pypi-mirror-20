cdce6d7e27101b82980b6830b827edba4d9a7d69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:55 +0000

    Add package strict-hint/0.2.1/strict-hint-0.2.1.tar.gz

---
+++ packages/strict-hint/strict-hint-0.2.1.tar.gz/strict-hint-0.2.1/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(
+    name='strict-hint',
+    packages=find_packages(exclude=['tests']),
+    version='0.2.1',
+    description='Strict type runtime checks for arguments and return types.',
+    author='Michal Wachowski',
+    author_email='wachowski.michal@gmail.com',
+    url='https://github.com/potfur/strict-hint',
+    download_url='https://github.com/potfur/strict-hint/archive/0.2.1.tar.gz',
+    keywords=['type hint', 'type validation', 'type declaration',
+              'argument type', 'return type'],
+    tests_require=[
+        'pytest',
+        'pytest-flake8',
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
