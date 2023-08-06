8665bfd1bb9e1f5be96167a912fd43ee1e71044c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:32 +0000

    Add package manga-dl/0.1.3/manga_dl-0.1.3-py2-none-any.whl

---
+++ packages/manga-dl/manga_dl-0.1.3-py2-none-any.whl/manga_dl/metadata.py
@@ -0,0 +1,38 @@
+u"""
+LICENSE:
+Copyright 2016 Hermann Krumrey
+
+This file is part of manga_dl.
+
+    manga_dl is a program that allows downloading manga files from various
+    sources
+
+    manga_dl is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    manga_dl is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with manga_dl.  If not, see <http://www.gnu.org/licenses/>.
+LICENSE
+"""
+
+u"""
+The metadata is stored here. It can be used by any other module in this project this way, most
+notably by the setup.py file
+"""
+
+version = u"0.1.3"
+u"""
+The current version of the program
+"""
+
+sentry_dsn = u"https://9bbb8f516b2b4abdb7750e9e7721c4e3:b002cc415d9c4d9080a27ef97fe8c688@sentry.io/112434"
+u"""
+The DSN used for Sentry Error Logging
+"""
