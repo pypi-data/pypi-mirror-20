e38a446467472a80a1a02d324737b5851e380b53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:28 +0000

    Add package freezedata/2.2.6/freezedata-2.2.6.tar.gz

---
+++ packages/freezedata/freezedata-2.2.6.tar.gz/freezedata-2.2.6/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+description = (
+'Recursively convert lists to tuples, sets to frozensets, dicts to mappingproxy etc.')
+
+
+with open('README.rst') as readme:
+    long_description = readme.read()
+
+setup(name='freezedata',
+      version='2.2.6',
+      description=description,
+      long_description=long_description,
+      url='https://github.com/topper-123/freezedata',
+      author='Terji Petersen',
+      author_email='terji78@gmail.com',
+      packages=['freezedata'],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],
+      test_suite='tests',
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+      )
