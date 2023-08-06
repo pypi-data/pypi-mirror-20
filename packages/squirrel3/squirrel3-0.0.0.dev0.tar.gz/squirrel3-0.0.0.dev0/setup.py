ed7dbd4b89b3fb594cfbe7cf3915f5fdcae4c489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:06 +0000

    Add package squirrel3/0.0.0.dev0/squirrel3-0.0.0.dev0.tar.gz

---
+++ packages/squirrel3/squirrel3-0.0.0.dev0.tar.gz/squirrel3-0.0.0.dev0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import with_statement
+
+from setuptools import setup
+
+
+setup(
+    name='squirrel3',
+    version='0.0.0.dev',
+    license='BSD',
+    author='Heungsub Lee',
+    author_email='sub@subl.ee',
+    url='https://github.com/sublee/squirrel3-python',
+    platforms='any',
+    py_module='squirrel3',
+    classifiers=['Development Status :: 4 - Beta',
+                 'Intended Audience :: Developers',
+                 'Intended Audience :: Science/Research',
+                 'License :: OSI Approved :: BSD License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 2',
+                 'Programming Language :: Python :: 2.5',
+                 'Programming Language :: Python :: 2.6',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.1',
+                 'Programming Language :: Python :: 3.2',
+                 'Programming Language :: Python :: 3.3',
+                 'Programming Language :: Python :: Implementation :: CPython',
+                 'Programming Language :: Python :: Implementation :: Jython',
+                 'Programming Language :: Python :: Implementation :: PyPy',
+                 'Topic :: Scientific/Engineering :: Mathematics'],
+)
