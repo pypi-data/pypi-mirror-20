041171b9b1f1f35802720431d86b6deb22d1010c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:41 +0000

    Add package rancidcmd/0.1.11/rancidcmd-0.1.11.tar.gz

---
+++ packages/rancidcmd/rancidcmd-0.1.11.tar.gz/rancidcmd-0.1.11/rancidcmd.py
@@ -0,0 +1,289 @@
+# -*- coding: utf-8 -*-
+
+"""RancidCmd."""
+
+from subprocess import Popen
+from subprocess import PIPE
+from os.path import expanduser
+import os
+import re
+import stat
+import tempfile
+
+
+class RancidCmd(object):
+
+    """Class RancidCmd.
+
+    Attributes:
+
+        :login (str): RANCID login command(clogin, jlogin, etc).
+        :user (str): Login username.
+        :password (str): Login password.
+        :address (str): Host name or ip address.
+        :enable_password (str, optional): Enable password for clogin.
+            Default is None.
+        :option(int, optional): Option example: '-d -x "commands.txt"'.
+            Default is None.
+        :encoding(str, optional): Encoding type.
+            Default is 'utf-8'.
+
+    Using example:
+
+        * Please see README.
+            https://github.com/mtoshi/rancidcmd/blob/master/README.rst
+
+        * Sample code.
+            https://github.com/mtoshi/rancidcmd/blob/master/samples/sample.py
+
+    """
+
+    def __init__(self, **kwargs):
+        """Constructor.
+
+        Args:
+
+            :login (str): RANCID login command(clogin, jlogin, etc).
+            :user (str): Login username.
+            :password (str): Login password.
+            :address (str): Host name or ip address.
+            :enable_password (str, optional): Enable password for clogin.
+                Default is None.
+            :option(int, optional): Option example: '-d -x "commands.txt"'.
+                Default is None.
+            :encoding(str, optional): Encoding type.
+                Default is 'utf-8'.
+
+        """
+        self.login = kwargs['login']
+        self.address = kwargs['address']
+        self.encoding = 'utf-8'
+        self.default_native_cloginrc_path = RancidCmd.make_default_native_cloginrc_path()  # NOQA
+        self.native_cloginrc_path = kwargs.get(
+            u'native_cloginrc_path', self.default_native_cloginrc_path)
+        native_cloginrc = self.check_native_cloginrc()
+
+        if native_cloginrc is None:
+            self.user = kwargs['user']
+            self.password = kwargs['password']
+            self.port = kwargs.get('port', 23)
+            self.method = kwargs.get('method', u'telnet')
+            self.enable_password = kwargs.get('enable_password', None)
+            self.option = kwargs.get('option', None)
+        self.cloginrc = self.make_cloginrc(native_cloginrc)
+
+    def is_option_x(self):
+        """Check -x option.
+
+        "-c" gets commands from command-line.
+        "-x" gets commnads from file.
+
+        These are for command option and exclusive.
+        If "-x" option is specified, then "-c" command is ignored.
+        """
+        if hasattr(self, u'option'):
+            if self.option:
+                pat = re.compile(r'(\s+)?-x\s+')
+                if pat.search(self.option):
+                    return True
+        return False
+
+    def generate_cmd(self, command):
+        """Generate command.
+
+        Args:
+
+            :command (str): Example is "show version".
+
+        Returns:
+
+            :str: Return the command string.
+
+            If there is the "enable_password". ::
+
+                'xlogin -u admin -c "show version"'
+
+        """
+        if self.is_option_x():
+            command = ''
+        else:
+            command = '-c "%s"' % command
+
+        res = []
+        if hasattr(self, "login"):
+            res.append(self.login)
+        if hasattr(self, u'option'):
+            if self.option:
+                res.append(self.option)
+        if command:
+            res.append(command)
+        if hasattr(self, "cloginrc"):
+            res.append('-f')
+            res.append(self.cloginrc.name)
+        if hasattr(self, "address"):
+            res.append(self.address)
+
+        return u' '.join(res)
+
+    def decode_bytes(self, byte_data):
+        """Change string with encoding setting.
+
+        Args:
+
+            :byte_data (bytes): Popen output.
+
+        """
+        return byte_data.decode(self.encoding)
+
+    def cmd_exec(self, command):
+        """Login and command execution.
+
+        Args:
+
+            :command (str): Command for execution.
+
+        Returns:
+
+            :dict: Example is below. ::
+
+            {
+                'std_err': '',
+                'std_out': '',
+                'rtn_code': '',
+            }
+        """
+        env = os.environ.copy()
+        env['HOME'] = RancidCmd.get_home_path()
+        proc = Popen(command,
+                     shell=True,
+                     env=env,
+                     stdout=PIPE,
+                     stderr=PIPE)
+        std_out, std_err = proc.communicate()
+        rtn_code = proc.returncode
+        return {'std_out': self.decode_bytes(std_out),
+                'std_err': self.decode_bytes(std_err),
+                'rtn_code': rtn_code}
+
+    def show_config(self):
+        """cloginrc string check.
+
+        Returns:
+
+            :str: Return cloginrc config string.
+        """
+        print(self.cloginrc.read().decode('utf-8'))
+
+    def show_command(self, command):
+        """Execute command string check.
+
+        Args:
+
+            :command (str): Example is "show version".
+
+        Returns:
+
+            :str: Return the command string.
+        """
+        print(self.generate_cmd(command))
+
+    def execute(self, command):
+        """Command execution.
+
+        Args:
+
+            :command (str): Example is "show version".
+
+        Returns:
+
+            :dict: Example is below. ::
+
+            {
+                'std_err': '',
+                'std_out': '',
+                'rtn_code': '',
+            }
+
+        """
+        cmd = self.generate_cmd(command)
+        res = self.cmd_exec(cmd)
+        self.cloginrc.close()
+        return res
+
+    @staticmethod
+    def get_home_path():
+        """Get home directory path.
+        Returns:
+            :str: User home directory path.
+        """
+        return expanduser("~")
+
+    def make_cloginrc(self, cloginrc_config):
+
+        """Check rancid settings file.
+
+        Note:
+
+            If RANCID settings file is not exists,
+            then make temporary settings file.
+
+        Returns:
+
+            :NamedTemporaryFile: RANCID settings tempfile object.
+
+        """
+        temp = tempfile.NamedTemporaryFile()
+
+        if cloginrc_config is None:
+            user = self.user
+            host = self.address
+            port = self.port
+            passwd = self.password
+            epasswd = self.enable_password
+            method = self.method
+
+            add_user = u'add user {host} {user}'.format(
+                host=host, user=user)
+
+            add_method = u'add method {host} {{{method}:{port}}}'.format(
+                host=host, method=method, port=port)
+
+            if self.enable_password:
+                add_passwd = u'add password {host} {passwd} {epasswd}'.format(
+                    host=host, passwd=passwd, epasswd=epasswd)
+            else:
+                add_passwd = u'add password {host} {passwd}'.format(
+                    host=host, passwd=passwd)
+
+            cloginrc_config = '\n'.join([add_user, add_method, add_passwd])
+
+        temp.write(cloginrc_config.encode('utf-8'))
+        temp.seek(0)
+
+        os.chmod(temp.name, stat.S_IRUSR)
+
+        return temp
+
+    def check_native_cloginrc(self):
+        """Check native settings file.
+        Note:
+            If RANCID settings file is not exists or empty,
+            then make temporary cloginrc file.
+            So this method is for check cloginrc file.
+
+        Returns:
+            :str: RANCID settings file text.
+        """
+        path = self.native_cloginrc_path
+        if os.path.isfile(path):
+            if os.path.getsize(path) == 0:
+                return None
+            with open(path, u'r') as _file:
+                return _file.read()
+        return None
+
+    @staticmethod
+    def make_default_native_cloginrc_path(name='.cloginrc'):
+        """Make native settings file."""
+        home = RancidCmd.get_home_path()
+        return os.path.join(home, name)
---
+++ packages/rancidcmd/rancidcmd-0.1.11.tar.gz/rancidcmd-0.1.11/tests/test_rancidcmd.py
@@ -0,0 +1,296 @@
+# -*- coding: utf-8 -*-
+
+"""UnitTests for rancidcmd."""
+
+
+import unittest
+import os
+import pwd
+from rancidcmd import RancidCmd
+
+
+class UnitTests(unittest.TestCase):
+
+    """The :class:`UnitTests <UnitTests>`.
+
+    UnitTests
+
+    """
+
+    def setUp(self):
+        """setup."""
+        self.obj1 = RancidCmd(
+            login='clogin',
+            user='rancid',
+            password='password',
+            enable_password='enable_password',
+            address='192.168.1.1')
+
+        self.obj2 = RancidCmd(
+            login='clogin',
+            user='rancid',
+            password='password',
+            enable_password='enable_password',
+            address='192.168.1.2',
+            port=23,
+            method=u'telnet')
+
+        self.obj3 = RancidCmd(
+            login='jlogin',
+            user='rancid',
+            password='password',
+            address='192.168.1.3',
+            port=23,
+            method=u'telnet')
+
+        self.obj4 = RancidCmd(
+            login='clogin',
+            user='rancid',
+            password='password',
+            option='-d',
+            address='192.168.1.4',
+            port=22,
+            method=u'ssh')
+
+        self.obj5 = RancidCmd(
+            login='clogin',
+            user='rancid',
+            password='password',
+            option='-t 30 -d -x "commands.txt"',
+            address='192.168.1.5',
+            port=22,
+            method=u'ssh')
+
+        self.obj10 = RancidCmd(
+            login='clogin',
+            user='admin',
+            password='zebra',
+            enable_password='zebra',
+            address='127.0.0.1',
+            port=2601,
+            method=u'telnet')
+
+    def test_init(self):
+        """check init value."""
+        self.assertEqual(self.obj1.login, 'clogin')
+        self.assertEqual(self.obj1.user, 'rancid')
+        self.assertEqual(self.obj1.password, 'password')
+        self.assertEqual(self.obj1.enable_password, 'enable_password')
+        self.assertEqual(self.obj1.address, '192.168.1.1')
+        self.assertEqual(self.obj1.port, 23)
+        self.assertEqual(self.obj1.method, 'telnet')
+        self.assertEqual(self.obj1.option, None)
+        self.assertEqual(self.obj1.encoding, 'utf-8')
+
+        self.assertEqual(self.obj2.login, 'clogin')
+        self.assertEqual(self.obj2.user, 'rancid')
+        self.assertEqual(self.obj2.password, 'password')
+        self.assertEqual(self.obj2.enable_password, 'enable_password')
+        self.assertEqual(self.obj2.address, '192.168.1.2')
+        self.assertEqual(self.obj2.port, 23)
+        self.assertEqual(self.obj2.method, 'telnet')
+        self.assertEqual(self.obj2.option, None)
+        self.assertEqual(self.obj2.encoding, 'utf-8')
+
+        self.assertEqual(self.obj3.login, 'jlogin')
+        self.assertEqual(self.obj3.user, 'rancid')
+        self.assertEqual(self.obj3.password, 'password')
+        self.assertEqual(self.obj3.enable_password, None)
+        self.assertEqual(self.obj3.address, '192.168.1.3')
+        self.assertEqual(self.obj3.port, 23)
+        self.assertEqual(self.obj3.method, 'telnet')
+        self.assertEqual(self.obj3.option, None)
+        self.assertEqual(self.obj3.encoding, 'utf-8')
+
+        self.assertEqual(self.obj4.login, 'clogin')
+        self.assertEqual(self.obj4.user, 'rancid')
+        self.assertEqual(self.obj4.password, 'password')
+        self.assertEqual(self.obj4.enable_password, None)
+        self.assertEqual(self.obj4.address, '192.168.1.4')
+        self.assertEqual(self.obj4.port, 22)
+        self.assertEqual(self.obj4.method, 'ssh')
+        self.assertEqual(self.obj4.option, '-d')
+        self.assertEqual(self.obj4.encoding, 'utf-8')
+
+        self.assertEqual(self.obj5.login, 'clogin')
+        self.assertEqual(self.obj5.user, 'rancid')
+        self.assertEqual(self.obj5.password, 'password')
+        self.assertEqual(self.obj5.enable_password, None)
+        self.assertEqual(self.obj5.address, '192.168.1.5')
+        self.assertEqual(self.obj5.port, 22)
+        self.assertEqual(self.obj5.method, 'ssh')
+        self.assertEqual(self.obj5.option, '-t 30 -d -x "commands.txt"')
+        self.assertEqual(self.obj5.encoding, 'utf-8')
+
+        self.assertEqual(self.obj10.login, 'clogin')
+        self.assertEqual(self.obj10.user, 'admin')
+        self.assertEqual(self.obj10.password, 'zebra')
+        self.assertEqual(self.obj10.enable_password, 'zebra')
+        self.assertEqual(self.obj10.address, '127.0.0.1')
+        self.assertEqual(self.obj10.port, 2601)
+        self.assertEqual(self.obj10.method, 'telnet')
+        self.assertEqual(self.obj10.option, None)
+        self.assertEqual(self.obj10.encoding, 'utf-8')
+
+    def test_is_option_x(self):
+        """Check command command option for "-x"."""
+        self.assertEqual(self.obj1.is_option_x(), False)
+        self.assertEqual(self.obj2.is_option_x(), False)
+        self.assertEqual(self.obj3.is_option_x(), False)
+        self.assertEqual(self.obj4.is_option_x(), False)
+        self.assertEqual(self.obj5.is_option_x(), True)
+        self.assertEqual(self.obj10.is_option_x(), False)
+
+    def test_generate_cmd(self):
+        """Check command format."""
+        cmd = 'show version'
+
+        # clogin
+        cmd1 = self.obj1.generate_cmd(cmd)
+        temp = self.obj1.cloginrc
+        cmd2 = " ".join(['clogin',
+                         '-c',
+                         '"show version"',
+                         '-f',
+                         temp.name,
+                         '192.168.1.1'])
+
+        self.assertEqual(cmd1, cmd2)
+
+        # clogin
+        cmd1 = self.obj2.generate_cmd(cmd)
+        temp = self.obj2.cloginrc
+        cmd2 = " ".join(['clogin',
+                         '-c',
+                         '"show version"',
+                         '-f',
+                         temp.name,
+                         '192.168.1.2'])
+
+        self.assertEqual(cmd1, cmd2)
+
+        # jlogin
+        cmd1 = self.obj3.generate_cmd(cmd)
+        temp = self.obj3.cloginrc
+        cmd2 = " ".join(['jlogin',
+                         '-c',
+                         '"show version"',
+                         '-f',
+                         temp.name,
+                         '192.168.1.3'])
+
+        self.assertEqual(cmd1, cmd2)
+
+        # clogin
+        cmd1 = self.obj4.generate_cmd(cmd)
+        temp = self.obj4.cloginrc
+        cmd2 = " ".join(['clogin',
+                         '-d',
+                         '-c',
+                         '"show version"',
+                         '-f',
+                         temp.name,
+                         '192.168.1.4'])
+
+        self.assertEqual(cmd1, cmd2)
+
+        # clogin
+        cmd1 = self.obj5.generate_cmd(cmd)
+        temp = self.obj5.cloginrc
+        cmd2 = " ".join(['clogin',
+                         '-t 30',
+                         '-d',
+                         '-x "commands.txt"',
+                         '-f',
+                         temp.name,
+                         '192.168.1.5'])
+
+        self.assertEqual(cmd1, cmd2)
+
+        # clogin
+        cmd1 = self.obj10.generate_cmd(cmd)
+        temp = self.obj10.cloginrc
+        cmd2 = " ".join(['clogin',
+                         '-c',
+                         '"show version"',
+                         '-f',
+                         temp.name,
+                         '127.0.0.1'])
+
+        self.assertEqual(cmd1, cmd2)
+
+    def test_show_config(self):
+        """Check config string."""
+        import sys
+        try:
+            from StringIO import StringIO
+            out = StringIO()
+        except ImportError:
+            import io
+            out = io.StringIO()
+
+        sys.stdout = out
+        self.obj1.show_config()
+        output = out.getvalue().strip()
+
+        vals = [u'add user 192.168.1.1 rancid',
+                u'add method 192.168.1.1 {telnet:23}',
+                u'add password 192.168.1.1 password enable_password']
+
+        self.assertEqual(output, "\n".join(vals))
+
+    def test_show_command(self):
+        """Check command string."""
+        import sys
+        cmd = 'show version'
+        try:
+            from StringIO import StringIO
+            out = StringIO()
+        except ImportError:
+            import io
+            out = io.StringIO()
+
+        sys.stdout = out
+        self.obj1.show_command(cmd)
+        temp = self.obj1.cloginrc
+        output = out.getvalue().strip()
+
+        val = u'clogin -c "show version" -f {0} 192.168.1.1'.format(temp.name)
+
+        self.assertEqual(output, val)
+
+    def test_get_home_path(self):
+        """Check user directory path."""
+        path = RancidCmd.get_home_path()
+        _path = pwd.getpwuid(os.getuid())[5]
+        self.assertEqual(path, _path)
+
+    def test_cmd_exec(self):
+        """Check excecuter result."""
+        # Execute true command.
+        res = self.obj1.cmd_exec('echo test')
+        self.assertEqual(res['rtn_code'], 0)
+
+        # Execute not found command.
+        res = self.obj1.cmd_exec('_echo test')
+        self.assertNotEqual(res['rtn_code'], 0)
+
+    def test_execute(self):
+        """Check excecute."""
+        obj = RancidCmd(login='clogin',
+                        user='admin',
+                        password='password',
+                        address='127.0.0.1')
+        res = obj.execute('show version')
+        if res['std_err'] == '':
+            self.assertEqual(res['std_err'], '')
+            self.assertNotEqual(res['std_out'], '')
+        self.assertNotEqual(res['std_err'], '')
+        self.assertEqual(res['std_out'], '')
+
+    def test_decode_bytes(self):
+        """Check byte and str changing."""
+        test_str = 'abcdABCD01234$&=+-*%[]#!/"@'
+        byte_data = str.encode(test_str)
+        decod_data = self.obj1.decode_bytes(byte_data)
+        self.assertEqual(decod_data, test_str)
