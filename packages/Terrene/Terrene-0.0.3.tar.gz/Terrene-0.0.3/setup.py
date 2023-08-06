a0947b67b0fe08a73bbb225b124a804f51ff9f1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:34 +0000

    Add package Terrene/0.0.3/Terrene-0.0.3.tar.gz

---
+++ packages/Terrene/Terrene-0.0.3.tar.gz/Terrene-0.0.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Terrene",
+    version="0.0.3",
+    description="Terrene's Python SDK",
+    url="https://docs.terrene.co",
+    author="Terrene",
+    author_email="developers@terrene.co",
+    license='MIT',
+    keywords="terrene",
+    download_url="https://git.terrene.co/General/python-sdk/repository/archive.tar.gz?ref=release",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ]
+)
