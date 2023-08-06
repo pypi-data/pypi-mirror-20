d3c9bf4b37816d79d180c8730fd6669628450ff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:44 +0000

    Add package tempemail/1.0.1/TempEmail-1.0.1.tar.gz

---
+++ packages/tempemail/TempEmail-1.0.1.tar.gz/TempEmail-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='TempEmail',
+    version='1.0.1',
+    packages=['tempemail', 'tempemail.test'],
+    url='',
+    license='',
+    author='sonar235',
+    author_email='',
+    description='Create a temporary email address.'
+)
