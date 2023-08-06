fd9c7f3cecfb334b36032d88a5ece2f9cdfffbcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:06 +0000

    Add package distancerasters/0.1.0a3/distancerasters-0.1.0a3.tar.gz

---
+++ packages/distancerasters/distancerasters-0.1.0a3.tar.gz/distancerasters-0.1.0a3/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='distancerasters',
+    packages=['distancerasters'],
+    version='0.1.0a3',
+    description='Generate distance raster using arbitrary sets of spatial features',
+    author='Seth Goodman',
+    author_email='sgoodman@aiddata.wm.edu',
+    url='https://github.com/itpir/distance-rasters',
+    keywords=['raster', 'distance', 'spatial'],
+    classifiers=[],
+    package_dir={'': 'src'}
+)
