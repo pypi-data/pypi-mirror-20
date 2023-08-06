cadc158969dceeb0bea970d858c25adb6ed112e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:39 +0000

    Add package hpdr/0.16/hpdr-0.16.tar.gz

---
+++ packages/hpdr/hpdr-0.16.tar.gz/hpdr-0.16/hpdr/api.py
@@ -0,0 +1,164 @@
+# pylint: disable=too-many-arguments
+"""Doco
+"""
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from future import standard_library
+from hpdr.models import Spec
+from hpdr import utils
+from datetime import timedelta
+standard_library.install_aliases()
+
+def build(begin,
+          end,
+          izone='UTC',
+          qzone='UTC',
+          slop=None,
+          lslop=None,
+          rslop=None,
+          years='YYYY',
+          months='MM',
+          days='DD',
+          hours='HH',
+          minutes='MIN'):
+    """Build a specification for a date range.
+
+    Args:
+        begin: begin date of range, a datetime or yyyy[mm[dd[mm[ss]]]] string
+        end: end date of range, a datetime or yyyy[mm[dd[mm[ss]]]] string
+        izone: (optional) time zone for input dates, defaults to UTC
+        qzone: (optional) time zone to use in query, defaults to UTC
+        slop: (optional) duration to add to both ends of the partition range,
+            specified as \\d+[years|months|days|hours|minutes],
+            for example, 5hours
+        lslop: (optional) duration to add to the front end of the partition range,
+            specified as \\d+[years|months|days|hours|minutes],
+            for example, 5hours
+        rslop: (optional) duration to add to the back end of the partition range,
+            specified as \\d+[years|months|days|hours|minutes],
+            for example, 5hours
+        years: (optional) name for years partition, defaults to YYYY
+        months: (optional) name for months partition, defaults to MM
+        days: (optional) name for days partition, defaults to DD
+        hours: (optional) name for hours partition, defaults to HH
+        minutes: (optional) name for hours partition, defaults to MIN
+
+    Returns: 
+       A Spec object representing the date range
+    """
+
+    specs = build_with_steps(begin,
+                             end,
+                             izone=izone,
+                             qzone=qzone,
+                             slop=slop,
+                             lslop=lslop,
+                             rslop=rslop,
+                             years=years,
+                             months=months,
+                             days=days,
+                             hours=hours,
+                             minutes=minutes)
+
+    return specs[0]
+
+
+def build_with_steps(begin,
+                     end,
+                     step=None,
+                     izone='UTC',
+                     qzone='UTC',
+                     slop=None,
+                     lslop=None,
+                     rslop=None,
+                     years='YYYY',
+                     months='MM',
+                     days='DD',
+                     hours='HH',
+                     minutes='MIN'):
+    """Build a lists of specification for a date.
+
+    The specifications in the list are contiguous, chronological pieces of the list.
+    Left slop followed by the begin-to-end range broken into parts of step size
+    followed by right slop.
+
+    Args:
+        begin: begin date of range, a datetime or yyyy[mm[dd[mm[ss]]]] string
+        end: end date of range, a datetime or yyyy[mm[dd[mm[ss]]]] string
+        step: (optional) duration to break individual Spec objects into,
+            specified as \\d+[years|months|days|hours|minutes],
+            for example, 5hours. If None, one Spec is returned.
+        izone: (optional) time zone for input dates, defaults to UTC
+        qzone: (optional) time zone to use in query, defaults to UTC
+        slop: (optional) duration to add to both ends of the partition range,
+            specified as \\d+[years|months|days|hours|minutes],
+            for example, 5hours
+        lslop: (optional) duration to add to the front end of the partition range,
+            specified as \\d+[years|months|days|hours|minutes],
+            for example, 5hours
+        rslop: (optional) duration to add to the back end of the partition range,
+            specified as \\d+[years|months|days|hours|minutes],
+            for example, 5hours
+        years: (optional) name for years partition, defaults to YYYY
+        months: (optional) name for months partition, defaults to MM
+        days: (optional) name for days partition, defaults to DD
+        hours: (optional) name for hours partition, defaults to HH
+        minutes: (optional) name for hours partition, defaults to MIN
+
+    Returns: 
+       A list of Spec objects representing the date range. For example,
+
+       build_with_steps(begin='20160901', end=20161001, step=10days, --slop=1hours)
+
+       returns a list of five Spec objects, representing these ranges:
+
+       (YYYY=2016 AND MM=08 AND DD=31 AND HH>=23) [left slop of 1 hour]
+       (YYYY=2016 AND MM=09 AND DD>=01 AND DD<11) [10 days]
+       (YYYY=2016 AND MM=09 AND DD>=11 AND DD<21) [10 days]
+       (YYYY=2016 AND MM=09 AND DD>=21)           [10 days]
+       (YYYY=2016 AND MM=10 AND DD=01 AND HH=00)  [right slop of 1 hour]
+    
+    """
+
+    keywords = {}
+    if izone:
+        keywords['izone'] = izone
+    if qzone:
+        keywords['qzone'] = qzone
+    if slop:
+        keywords['slop'] = slop
+    if lslop:
+        keywords['lslop'] = lslop
+    if rslop:
+        keywords['rslop'] = rslop
+    if years:
+        keywords['years'] = years
+    if months:
+        keywords['months'] = months
+    if days:
+        keywords['days'] = days
+    if hours:
+        keywords['hours'] = hours
+    if minutes:
+        keywords['minutes'] = minutes
+
+    specs = []
+    main_spec = Spec(begin, end, **keywords)
+
+    if step is None:
+        specs.append(main_spec)
+    else:
+        if not isinstance(step, timedelta):
+            step = utils.deltastr_to_td(step)
+
+        # Break into steps from begin to end, not from slop_begin
+        # to slop_end, then add slop to each step
+        walk_begin = main_spec.begin
+        while walk_begin < main_spec.end:
+            walk_end = min(main_spec.end, walk_begin + step)
+            specs.append(Spec(walk_begin, walk_end, **keywords))
+            walk_begin = walk_end
+
+    return specs
---
+++ packages/hpdr/hpdr-0.16.tar.gz/hpdr-0.16/setup.py
@@ -0,0 +1,42 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='hpdr',
+    packages=find_packages(),
+    version='0.16',
+    description='Hive date ranges simplified.',
+    author='Mark Libucha',
+    author_email='mlibucha@gmail.com',
+    url='http://hpdrdoc.blogspot.com/',
+    license='Apache 2.0',
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    test_suite='test',
+    zip_safe=False,
+    classifiers=(
+        "Development Status :: 3 - Alpha",
+        "Operating System :: OS Independent",
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython'
+    ),
+    install_requires=[
+        "attrs>=16.1.0, <17.0.0",
+        "enum34>=1.1.6, <2.0.0",
+        "funcparserlib>=0.3.6, <1.0.0",
+        "future>=0.15.2, <1.0.0",
+        "pendulum>=0.5.5, <1.0.0",
+        "pytest>=3.0.2, <4.0.0",
+        "python-dateutil>=2.5.3, <3.0.0",
+        "pytz>=2016.6.1, <2017.0.0",
+        "six>=1.10.0, <2.0.0",
+        "tabulate>=0.7.5, <2.0.0"
+    ]
+)
