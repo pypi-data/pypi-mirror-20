53eea9cfbacae733d9d7256c308ddab47310f044
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:11 +0000

    Add package buttercms-python/0.7/buttercms-python-0.7.tar.gz

---
+++ packages/buttercms-python/buttercms-python-0.7.tar.gz/buttercms-python-0.7/butter_cms/post.py
@@ -0,0 +1,19 @@
+from .client import Client
+
+
+class Post(Client):
+    """Post"""
+    def __init__(self, auth_token):
+        Client.__init__(self, auth_token)
+        self.path = 'posts/'
+
+    def all(self, params=None):
+        return self.api_get(params=params)
+
+    def search(self, query='', page=1, page_size=10):
+        params = {
+            'query': query,
+            'page': page,
+            'page_size': page_size,
+        }
+        return self.api_get(params=params)
