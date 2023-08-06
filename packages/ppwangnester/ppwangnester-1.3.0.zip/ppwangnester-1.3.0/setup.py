d57256816cd8bca9cd71a9d08db1b0d4b285fe55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:36 +0000

    Add package ppwangnester/1.3.0/ppwangnester-1.3.0.zip

---
+++ packages/ppwangnester/ppwangnester-1.3.0.zip/ppwangnester-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name        = 'ppwangnester',
+    version     = '1.3.0',
+    py_modules  =['nester'],
+    author      = 'ppwang',
+    author_email = 'ppwang@grandstream.cn',
+    url         = 'http://www.headfirstlabs.com',
+    description  = 'A simple printer of nested lists'        
+    )
