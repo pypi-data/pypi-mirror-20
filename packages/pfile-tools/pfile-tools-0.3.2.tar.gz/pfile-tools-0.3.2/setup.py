eebf22fee0ed996efb332831d5e459b89cc427d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:03 +0000

    Add package pfile-tools/0.3.2/pfile-tools-0.3.2.tar.gz

---
+++ packages/pfile-tools/pfile-tools-0.3.2.tar.gz/pfile-tools-0.3.2/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(
+    name='pfile-tools',
+    version='0.3.2',
+    author='Nathan Vack',
+    author_email='njvack@wisc.edu',
+    license='BSD License',
+    url='https://github.com/njvack/pfile-tools',
+    packages=['pfile_tools'],
+    entry_points={
+        'console_scripts': [
+            'dump_pfile_header = pfile_tools.scripts.dump_pfile_header:main',
+            'anonymize_pfile = pfile_tools.scripts.anonymize_pfile:main'
+        ]}
+    )
+
+
+
+
+# setup(
+#     name='pfile-tools',
+#     version=pfile_tools.VERSION,
+#     packages=setuptools.find_packages(),
+#     data_files=[('', ['distribute_setup.py'])],
+#     license='BSD License',
+#     long_description=read('README'),
+#     url="https://github.com/njvack/pfile-tools",
+#     author="Nathan Vack",
+#     author_email="njvack@wisc.edu",
+#     entry_points = {
+#         'console_scripts': [
+#             'dump_pfile_header = pfile_tools.scripts.dump_pfile_header:main',
+#             'anonymize_pfile = pfile_tools.scripts.anonymize_pfile:main'
+#         ]
+#     }
+# )
