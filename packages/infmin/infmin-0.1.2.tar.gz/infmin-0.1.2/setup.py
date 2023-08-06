c97131fb9e54537c8563df741afb9bb6c9756b00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:56 +0000

    Add package infmin/0.1.2/infmin-0.1.2.tar.gz

---
+++ packages/infmin/infmin-0.1.2.tar.gz/infmin-0.1.2/setup.py
@@ -0,0 +1,60 @@
+from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='infmin',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.1.2',
+
+    description='Text file minimizing CLI tool',
+    long_description=long_description,
+    url="https://github.com/Scottz0r/infmin",
+    author="Scott Clewell",
+    author_email='scott.clewell@gmail.com',
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        "Intended Audience :: Information Technology",
+        'Topic :: Utilities',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        # 'Programming Language :: Python :: 2',
+        # 'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='infmin file minimize',
+
+    py_modules=["infmin_main"],
+    entry_points={
+        'console_scripts': [
+            'infmin=infmin_main:main',
+        ],
+    },
+)
