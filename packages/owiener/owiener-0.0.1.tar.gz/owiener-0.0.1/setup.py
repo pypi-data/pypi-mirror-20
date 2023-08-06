1da20f0c6040c65f4b194fab49ed287bb8d82638
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:22 +0000

    Add package owiener/0.0.1/owiener-0.0.1.tar.gz

---
+++ packages/owiener/owiener-0.0.1.tar.gz/owiener-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="owiener",
+    version="0.0.1",
+    author="orisano",
+    author_email="owan.orisano@gmail.com",
+    description="A Python3 implementation of the Wiener attack on RSA",
+    license="MIT",
+    url="https://github.com/orisano/owiener",
+    py_modules=["owiener"],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Security :: Cryptography",
+    ],
+)
