033fc29d4f07397f4af83e6614499b57a7b565fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:44 +0000

    Add package wagtail-sharing/0.3/wagtail-sharing-0.3.tar.gz

---
+++ packages/wagtail-sharing/wagtail-sharing-0.3.tar.gz/wagtail-sharing-0.3/setup.py
@@ -0,0 +1,43 @@
+from setuptools import find_packages, setup
+
+
+install_requires = [
+    'Django>=1.8,<1.11',
+    'wagtail>=1.6,<1.9',
+]
+
+
+testing_extras = [
+    'coverage>=3.7.0',
+    'flake8>=2.2.0',
+    'mock>=1.0.0',
+]
+
+
+short_description = 'Easier sharing of Wagtail drafts'
+
+
+def read_long_description():
+    try:
+        import pypandoc
+        return pypandoc.convert('README.md', 'rst')
+    except (IOError, ImportError):
+        pass
+
+
+setup(
+    name='wagtail-sharing',
+    url='https://github.com/cfpb/wagtail-sharing',
+    author='CFPB',
+    author_email='tech@cfpb.gov',
+    license='CCO',
+    version='0.3',
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=install_requires,
+    extras_require={
+        'testing': testing_extras,
+    },
+    description=short_description,
+    long_description=read_long_description()
+)
