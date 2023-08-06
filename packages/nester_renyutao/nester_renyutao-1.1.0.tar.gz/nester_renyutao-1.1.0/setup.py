9c1e995231f04c4e5f8b1edac1184487d7a312c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:58 +0000

    Add package nester_renyutao/1.1.0/nester_renyutao-1.1.0.tar.gz

---
+++ packages/nester_renyutao/nester_renyutao-1.1.0.tar.gz/nester_renyutao-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester_renyutao',
+    version = '1.1.0',
+    py_modules = ['nester_renyutao'],
+    author = 'dimeng',
+    author_email = 'renyutao_demon@163.com',
+    url = 'http://www.renyutao.com',
+    description = 'A simple printer of nested lists',
+    )
