cb31c71d67493bf62eb902d5ca6a0d9423a47ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:03 +0000

    Add package modoboa-postfix-autoreply/1.2.0/modoboa-postfix-autoreply-1.2.0.tar.gz

---
+++ packages/modoboa-postfix-autoreply/modoboa-postfix-autoreply-1.2.0.tar.gz/modoboa-postfix-autoreply-1.2.0/modoboa_postfix_autoreply/management/commands/autoreply.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import datetime
+import email
+from email.mime.text import MIMEText
+import fileinput
+import logging
+from logging.handlers import SysLogHandler
+import StringIO
+import smtplib
+import sys
+
+from django.core.management.base import BaseCommand, CommandError
+from django.utils import timezone
+
+from modoboa.admin.models import Mailbox
+from modoboa.lib.email_utils import split_mailbox, set_email_headers
+from modoboa.parameters import tools as param_tools
+
+from ...models import ARmessage, ARhistoric
+from ...modo_extension import PostfixAutoreply
+
+logger = logging.getLogger()
+logger.addHandler(SysLogHandler(address="/dev/log"))
+logger.setLevel(logging.ERROR)
+
+
+def send_autoreply(sender, mailbox, armessage, original_msg):
+    """Send an autoreply message."""
+    if armessage.fromdate > timezone.now():
+        return
+
+    if armessage.untildate is not None \
+            and armessage.untildate < timezone.now():
+        armessage.enabled = False
+        armessage.save()
+        return
+
+    try:
+        lastar = ARhistoric.objects.get(armessage=armessage.id, sender=sender)
+        timeout = param_tools.get_global_parameter(
+            "autoreplies_timeout", app="modoboa_postfix_autoreply")
+        delta = datetime.timedelta(seconds=int(timeout))
+        now = timezone.make_aware(datetime.datetime.now(),
+                                  timezone.get_default_timezone())
+        if lastar.last_sent + delta > now:
+            logger.debug(
+                "no autoreply message sent because delta (%s) < timetout (%s)",
+                delta, timeout
+            )
+            sys.exit(0)
+
+    except ARhistoric.DoesNotExist:
+        lastar = ARhistoric()
+        lastar.armessage = armessage
+        lastar.sender = sender
+
+    msg = MIMEText(armessage.content.encode("utf-8"), _charset="utf-8")
+    set_email_headers(
+        msg,
+        u"Auto: {} Re: {}".format(armessage.subject, original_msg["Subject"]),
+        mailbox.user.encoded_address, sender
+    )
+    msg["Auto-Submitted"] = "auto-replied"
+    msg["Precedence"] = "bulk"
+    message_id = original_msg.get("Message-ID")
+    if message_id:
+        msg["In-Reply-To"] = message_id
+        msg["References"] = message_id
+
+    try:
+        s = smtplib.SMTP("localhost")
+        s.sendmail(mailbox.user.encoded_address, [sender], msg.as_string())
+        s.quit()
+    except smtplib.SMTPException as exp:
+        logger.error("Failed to send autoreply message: %s", exp)
+        sys.exit(1)
+
+    logger.debug(
+        "autoreply message sent to %s" % mailbox.user.encoded_address)
+
+    lastar.last_sent = datetime.datetime.now()
+    lastar.save()
+
+
+class Command(BaseCommand):
+    args = "<sender> <recipient ...>"
+    help = "Send autoreply emails"
+
+    def add_arguments(self, parser):
+        """Add extra arguments to command line."""
+        parser.add_argument(
+            "--debug", action="store_true", dest="debug", default=False
+        )
+
+    def handle(self, *args, **options):
+        if options["debug"]:
+            logger.setLevel(logging.DEBUG)
+
+        if len(args) < 2:
+            logger.debug("autoreply %s", " ".join(args))
+
+            raise CommandError(
+                "usage: ./manage.py autoreply <sender> <recipient ...>")
+
+        logger.debug(
+            "autoreply sender=%s recipient=%s", args[0], ",".join(args[1:])
+        )
+
+        # Mailing list filter based on
+        # https://tools.ietf.org/html/rfc5230#page-7
+        sender = args[0]
+
+        sender_localpart = split_mailbox(sender.lower())[0]
+        if (
+            (sender_localpart in ('mailer-daemon', 'listserv', 'majordomo')) or
+            (sender_localpart.startswith('owner-')) or
+            (sender_localpart.endswith('-request'))
+        ):
+            logger.debug("Skip auto reply, this mail comes from mailing list")
+            sys.exit(0)
+
+        content = StringIO.StringIO()
+        for line in fileinput.input([]):
+            content.write(line)
+        content.seek(0)
+
+        original_msg = email.message_from_file(content)
+        ml_known_headers = [
+            "List-Id", "List-Help", "List-Subscribe", "List-Unsubscribe",
+            "List-Post", "List-Owner", "List-Archive"
+        ]
+        from_ml = False
+        for header in ml_known_headers:
+            if header in original_msg:
+                from_ml = True
+                break
+        conditions = (
+            original_msg.get("Precedence") == "bulk",
+            original_msg.get("X-Mailer") == "PHPMailer",
+            from_ml
+        )
+        if any(conditions):
+            logger.debug(
+                "Skip auto reply, this mail comes from mailing list")
+            sys.exit(0)
+
+        PostfixAutoreply().load()
+        for fulladdress in args[1:]:
+            address, domain = split_mailbox(fulladdress)
+            try:
+                mbox = Mailbox.objects.get(
+                    address=address, domain__name=domain)
+            except Mailbox.DoesNotExist:
+                msg = "Unknown recipient %s" % (fulladdress)
+                logger.debug("autoreply %s", msg)
+                continue
+            try:
+                armessage = ARmessage.objects.get(mbox=mbox.id, enabled=True)
+                logger.debug("autoreply message found")
+            except ARmessage.DoesNotExist:
+                logger.debug("autoreply message not found")
+                continue
+
+            send_autoreply(sender, mbox, armessage, original_msg)
---
+++ packages/modoboa-postfix-autoreply/modoboa-postfix-autoreply-1.2.0.tar.gz/modoboa-postfix-autoreply-1.2.0/modoboa_postfix_autoreply/tests.py
@@ -0,0 +1,251 @@
+"""modoboa-postfix-autoreply unit tests."""
+
+import datetime
+
+from django.core import management
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+from django.utils import timezone
+
+from modoboa.core.models import User
+from modoboa.lib.tests import ModoTestCase
+from modoboa.lib.test_utils import MapFilesTestCaseMixin
+
+from modoboa.admin import factories as admin_factories
+from modoboa.admin import models as admin_models
+
+from . import factories
+from .models import Transport, ARmessage
+
+
+class EventsTestCase(ModoTestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        """Create test data."""
+        super(EventsTestCase, cls).setUpTestData()
+        admin_factories.populate_database()
+
+    def test_domain_created_event(self):
+        values = {
+            "name": "domain.tld", "quota": 100, "default_mailbox_quota": 1,
+            "create_dom_admin": False,
+            "stepid": 'step3', "type": "domain"
+        }
+        self.ajax_post(
+            reverse("admin:domain_add"), values
+        )
+        self.assertTrue(
+            Transport.objects.filter(domain='autoreply.domain.tld').exists()
+        )
+
+    def test_domain_deleted_event(self):
+        dom = admin_models.Domain.objects.get(name="test.com")
+        self.ajax_post(
+            reverse("admin:domain_delete", args=[dom.id]),
+            {}
+        )
+        with self.assertRaises(Transport.DoesNotExist):
+            Transport.objects.get(domain='autoreply.test.com')
+
+    def test_domain_modified_event(self):
+        values = {
+            "name": "test.fr", "quota": 100, "default_mailbox_quota": 1,
+            "enabled": True, "type": "domain"
+        }
+        dom = admin_models.Domain.objects.get(name="test.com")
+        self.ajax_post(
+            reverse("admin:domain_change", args=[dom.id]),
+            values
+        )
+        self.assertTrue(
+            Transport.objects.filter(domain='autoreply.test.fr').exists())
+        self.assertEqual(
+            admin_models.Alias.objects.filter(
+                domain=dom, internal=True)
+            .count(), 2
+        )
+        for alr in admin_models.AliasRecipient.objects.filter(
+                address__contains='@test.fr'):
+            self.assertIn('autoreply.test.fr', alr.address)
+
+    def test_armessage_postsave_event(self):
+        values = {
+            'username': "leon@test.com",
+            'first_name': 'Tester', 'last_name': 'Toto',
+            'role': 'SimpleUsers', 'quota_act': True,
+            'is_active': True, 'email': 'leon@test.com',
+            'subject': 'test', 'content': 'test', 'enabled': True
+        }
+        account = User.objects.get(username="user@test.com")
+        self.ajax_post(
+            reverse("admin:account_change", args=[account.id]),
+            values
+        )
+        self.assertTrue(
+            admin_models.AliasRecipient.objects.filter(
+                alias__address="leon@test.com", alias__internal=True,
+                address="leon@test.com@autoreply.test.com").exists()
+        )
+        values["enabled"] = False
+        self.ajax_post(
+            reverse("admin:account_change", args=[account.id]),
+            values
+        )
+        self.assertFalse(
+            admin_models.AliasRecipient.objects.filter(
+                alias__address="leon@test.com", alias__internal=True,
+                address="leon@test.com@autoreply.test.com").exists()
+        )
+
+    def test_mailbox_deleted_event(self):
+        account = User.objects.get(username="user@test.com")
+        self.ajax_post(
+            reverse("admin:account_delete", args=[account.id]),
+            {}
+        )
+        self.assertFalse(
+            admin_models.Alias.objects.filter(
+                address="user@test.com", internal=True).exists()
+        )
+        self.assertFalse(
+            ARmessage.objects.filter(
+                mbox__address="user", mbox__domain__name="test.com").exists()
+        )
+
+    def test_modify_mailbox_event(self):
+        """Rename mailbox."""
+        account = User.objects.get(username="user@test.com")
+        factories.ARmessageFactory(mbox=account.mailbox)
+        values = {
+            'username': "leon@test.com",
+            'first_name': 'Tester', 'last_name': 'Toto',
+            'role': 'SimpleUsers', 'quota_act': True,
+            'is_active': True, 'email': 'leon@test.com',
+            'subject': 'test', 'content': 'test', 'enabled': True
+        }
+        self.ajax_post(
+            reverse("admin:account_change", args=[account.id]),
+            values
+        )
+        self.assertFalse(
+            admin_models.AliasRecipient.objects.filter(
+                alias__address="user@test.com", alias__internal=True,
+                address="user@test.com@autoreply.test.com").exists()
+        )
+        self.assertTrue(
+            admin_models.AliasRecipient.objects.filter(
+                alias__address="leon@test.com", alias__internal=True,
+                address="leon@test.com@autoreply.test.com").exists()
+        )
+
+
+class FormTestCase(ModoTestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        """Create test data."""
+        super(FormTestCase, cls).setUpTestData()
+        admin_factories.populate_database()
+
+    def setUp(self):
+        """Initialize tests."""
+        super(FormTestCase, self).setUp()
+        self.client.logout()
+        self.client.login(username="user@test.com", password="toto")
+
+    def test_form_display(self):
+        """Check if form is displayed."""
+        url = reverse("core:user_index")
+        response = self.client.get(url)
+        self.assertContains(response, "function autoreply_cb()")
+        self.assertContains(response, 'name="autoreply"')
+
+    def test_set_autoreply(self):
+        values = {
+            'subject': 'test', 'content': "I'm off", "enabled": True
+        }
+        self.ajax_post(reverse('autoreply'), values)
+        account = User.objects.get(username="user@test.com")
+        arm = ARmessage.objects.get(mbox=account.mailbox)
+        self.assertEqual(arm.subject, 'test')
+        self.assertTrue(arm.enabled)
+        self.assertFalse(arm.untildate)
+        self.assertTrue(arm.fromdate)
+
+    def test_set_autoreply_in_past(self):
+        """Create an autoreply with from date expired."""
+        fromdate = timezone.localtime(
+            timezone.now().replace(
+                year=2014, month=1, day=1, hour=12, microsecond=0))
+        values = {
+            'subject': 'test', 'content': "I'm off",
+            "enabled": True,
+            "fromdate": fromdate.strftime("%Y-%m-%d %H:%M:%S"),
+        }
+        self.ajax_post(reverse('autoreply'), values)
+        account = User.objects.get(username="user@test.com")
+        arm = ARmessage.objects.get(mbox=account.mailbox)
+        self.assertEqual(
+            timezone.localtime(arm.fromdate), fromdate)
+
+    def test_set_autoreply_dates(self):
+        """Create an autoreply with from and to dates."""
+        fromdate = timezone.localtime(timezone.now())
+        untildate = fromdate + datetime.timedelta(days=1)
+        values = {
+            'subject': 'test', 'content': "I'm off", "enabled": True,
+            "fromdate": fromdate.strftime("%Y-%m-%d %H:%M:%S"),
+            "untildate": untildate.strftime("%Y-%m-%d %H:%M:%S"),
+        }
+        self.ajax_post(reverse('autoreply'), values)
+        account = User.objects.get(username="user@test.com")
+        arm = ARmessage.objects.get(mbox=account.mailbox)
+        self.assertEqual(
+            timezone.localtime(arm.fromdate), fromdate.replace(microsecond=0))
+        self.assertEqual(
+            timezone.localtime(arm.untildate),
+            untildate.replace(microsecond=0))
+
+
+class MapFilesTestCase(MapFilesTestCaseMixin, TestCase):
+
+    """Test case for modoboa_postfix_autoreply."""
+
+    extension = "modoboa_postfix_autoreply"
+
+    MAP_FILES = [
+        "sql-autoreplies-transport.cf",
+    ]
+
+
+class RepairTestCase(ModoTestCase):
+    """Check repair command."""
+
+    @classmethod
+    def setUpTestData(cls):
+        """Create some data."""
+        super(RepairTestCase, cls).setUpTestData()
+        admin_factories.populate_database()
+
+    def test_management_command(self):
+        """Check if problems are fixed."""
+        mbox = admin_models.Mailbox.objects.get(user__username="user@test.com")
+        alias = admin_models.Alias.objects.get(
+            internal=True, address=mbox.full_address)
+        arm = factories.ARmessageFactory(mbox=mbox, enabled=False)
+        ar_address = "{}@autoreply.{}".format(
+            mbox.full_address, mbox.domain.name)
+        admin_factories.AliasRecipientFactory(
+            alias=alias, address=ar_address)
+        management.call_command("modo", "repair", "--quiet")
+        self.assertFalse(
+            admin_models.AliasRecipient.objects.filter(
+                address=ar_address).exists())
+        admin_factories.AliasRecipientFactory(
+            alias=alias, address=ar_address)
+        arm.delete()
+        management.call_command("modo", "repair", "--quiet")
+        self.assertFalse(
+            admin_models.AliasRecipient.objects.filter(
+                address=ar_address).exists())
