9dca6f13261b6915d2f429538340e8dae422adfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:29 +0000

    Add package sphinxcontrib-jsonschema/0.9.3/sphinxcontrib-jsonschema-0.9.3.tar.gz

---
+++ packages/sphinxcontrib-jsonschema/sphinxcontrib-jsonschema-0.9.3.tar.gz/sphinxcontrib-jsonschema-0.9.3/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+long_desc = open('README.rst').read()
+
+requires = ['Sphinx>=0.6']
+
+setup(
+    name='sphinxcontrib-jsonschema',
+    version='0.9.3',
+    url='https://github.com/tk0miya/sphinxcontrib-jsonschema',
+    license='BSD',
+    author='Takeshi KOMIYA',
+    author_email='i.tkomiya@gmail.com',
+    description='Sphinx extension to define data structure using JSON Schema',
+    long_description=long_desc,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Framework :: Sphinx :: Extension',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Documentation',
+        'Topic :: Documentation :: Sphinx',
+    ],
+    platforms='any',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requires,
+    namespace_packages=['sphinxcontrib'],
+)
