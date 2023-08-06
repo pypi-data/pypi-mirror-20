aee31df27196565c81c601d170ff6954f42596b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:55 +0000

    Add package pandapower/1.2.0/pandapower-1.2.0.zip

---
+++ packages/pandapower/pandapower-1.2.0.zip/pandapower-1.2.0/pandapower/__init__.py
@@ -0,0 +1,12 @@
+from pandapower.create import *
+from pandapower.run import *
+from pandapower.file_io import *
+from pandapower.toolbox import *
+from pandapower.auxiliary import *
+from pandapower.std_types import *
+from pandapower.diagnostic import *
+
+import pandas as pd
+pd.options.mode.chained_assignment = None  # default='warn'
+
+__version__ = "1.2.0"
