4e10589b8f4b77433cfae78e2a65a63958317f33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:36 +0000

    Add package mignis/0.9.3.post1/mignis-0.9.3.post1.tar.gz

---
+++ packages/mignis/mignis-0.9.3.post1.tar.gz/mignis-0.9.3.post1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='mignis',
+    py_modules=['mignis', 'ipaddr_ext'],
+    version='0.9.3.post1',
+    license='MIT',
+    description='Mignis is a semantic based tool for firewall configuration',
+    url='https://github.com/segroup/Mignis',
+    keywords=['iptables', 'firewall', 'semantic firewall configuration', 'netfilter'],
+    install_requires=['ipaddr'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Security',
+        'Topic :: System :: Networking :: Firewalls'
+    ],
+    entry_points={'console_scripts': ['mignis = mignis:main']},
+)
