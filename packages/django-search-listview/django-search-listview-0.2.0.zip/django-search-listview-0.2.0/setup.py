873c1df2c93b6802920d6f99f7ec4481f531a487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:46 +0000

    Add package django-search-listview/0.2.0/django-search-listview-0.2.0.zip

---
+++ packages/django-search-listview/django-search-listview-0.2.0.zip/django-search-listview-0.2.0/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup
+
+with open('README.md') as readme:
+    long_description = readme.read()
+
+setup(
+    name='django-search-listview',
+    version='0.2.0',
+    packages = ["search_listview"],
+    package_dir={'':'.'},
+    include_package_data=True,
+    package_data={
+        "" : ["templates/search_listview/*.html", "static/search_listview/js/*.js"],
+    },
+    long_description=long_description,
+    description='Searchable and paginable ListView',
+    author='Quentin Schroter',
+    author_email='qschroter@gmail.com',
+    maintainer='Quentin Schroter',
+    maintainer_email='qschroter@gmail.com',
+    url='https://github.com/SchroterQuentin/django-search-listview',
+    download_url='https://github.com/SchroterQuentin/django-search-listview/tarball/0.1',
+    license='PSF',
+    keywords=['django', 'listView', 'search'],
+    
+)
