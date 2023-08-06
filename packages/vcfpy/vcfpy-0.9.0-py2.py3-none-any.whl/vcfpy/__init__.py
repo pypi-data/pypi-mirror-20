556416d5319fe7bb49e349a4d030b1bbe02a92fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:32 +0000

    Add package vcfpy/0.9.0/vcfpy-0.9.0-py2.py3-none-any.whl

---
+++ packages/vcfpy/vcfpy-0.9.0-py2.py3-none-any.whl/vcfpy/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+try:
+    from cyordereddict import OrderedDict
+except ImportError:
+    from collections import OrderedDict
+
+from .exceptions import VCFPyException, InvalidHeaderException, \
+    InvalidRecordException, IncorrectVCFFormat, HeaderNotFound
+
+from .header import Header, HeaderLine, SimpleHeaderLine, \
+    AltAlleleHeaderLine, ContigHeaderLine, FilterHeaderLine, MetaHeaderLine, \
+    PedigreeHeaderLine, SampleHeaderLine, CompoundHeaderLine, InfoHeaderLine, \
+    FormatHeaderLine, SamplesInfos, FieldInfo, header_without_lines
+
+from .record import Record, Call, AltRecord, Substitution, BreakEnd, \
+    SingleBreakEnd, SymbolicAllele
+
+from .record import SNV, MNV, DEL, INS, INDEL, SV, BND, SYMBOLIC, MIXED
+from .record import HOM_REF, HET, HOM_ALT
+from .record import FIVE_PRIME, THREE_PRIME, FORWARD, REVERSE
+
+from .reader import Reader
+
+from .writer import Writer
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
