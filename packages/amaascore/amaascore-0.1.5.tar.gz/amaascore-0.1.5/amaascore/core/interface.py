7850858d3df92aed0290a7c894508ce561fb992f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:23 +0000

    Add package amaascore/0.1.5/amaascore-0.1.5.tar.gz

---
+++ packages/amaascore/amaascore-0.1.5.tar.gz/amaascore-0.1.5/amaascore/core/interface.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+
+class Interface(object):
+    """
+    Currently this class doesn't do anything - but I anticipate it will be needed in the future.
+    """
+
+    def __init__(self, endpoint):
+        self.endpoint = endpoint
