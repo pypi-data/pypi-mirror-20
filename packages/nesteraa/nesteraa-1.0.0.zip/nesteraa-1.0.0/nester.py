21d5d8d6c3f5ca06321ffc46da8daa3e7e9e73a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:31 +0000

    Add package nesteraa/1.0.0/nesteraa-1.0.0.zip

---
+++ packages/nesteraa/nesteraa-1.0.0.zip/nesteraa-1.0.0/nester.py
@@ -0,0 +1,8 @@
+'''This is the "nester.py" module, and it provides one function called print_lol() which prints lists may or may not include nested lists'''
+def print_lol(the_list):
+    """This function takes one positional argument called the lists, which is any Python list(of-possibly-nested lists). Each data item in the provided list is(rectusively) printed to the screen on its own line."""
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item)
+        else:
+            print(each_item)
---
+++ packages/nesteraa/nesteraa-1.0.0.zip/nesteraa-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'nesteraa',
+    version = '1.0.0',
+    py_modules = ['nester'],
+    author = 'aiaileen',
+    author_email = 'aileen_zhang95@hotmail.com',
+    url = 'http://www.nenu.edu.cn',
+    description = 'A simple printer of nested lists',
+)
