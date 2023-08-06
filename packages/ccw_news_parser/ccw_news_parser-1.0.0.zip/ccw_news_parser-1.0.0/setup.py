363086ae65990b7058b32d06a4114ae75128befd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:15 +0000

    Add package ccw_news_parser/1.0.0/ccw_news_parser-1.0.0.zip

---
+++ packages/ccw_news_parser/ccw_news_parser-1.0.0.zip/ccw_news_parser-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+#from astroid.__pkginfo__ import author, author_email, description
+
+setup(
+      name = 'ccw_news_parser',
+      version = '1.0.0',
+      py_modules = ['ccw_news_parser'],
+      author = 'ccw',
+      author_email = 'ccw3435@naver.com',
+      description = 'a simple news parser.'
+      )
