6c8cbfb15207c9cbe6568b9f799af3c70ac8c775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:36 +0000

    Add package telegraphapi/0.5/telegraphapi-0.5.tar.gz

---
+++ packages/telegraphapi/telegraphapi-0.5.tar.gz/telegraphapi-0.5/setup.py
@@ -0,0 +1,43 @@
+# _*_ coding: utf-8 _*_
+
+"""
+@author: MonsterDeveloper
+@contact: https://github.com/MonsterDeveloper
+@license: MIT License, see LICENSE file
+
+Copyright (C) 2017
+"""
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(name='telegraphapi',
+      description='Python TelegraphAPI wrapper',
+      url='https://github.com/MonsterDeveloper/python-telegraphapi',
+      author="Andrew Developer",
+      author_email="slider7259@gmail.com",
+      packages=['telegraphapi'],
+      install_requires=[
+          'requests',
+      ],
+      license="MIT",
+      classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: Implementation :: CPython'
+      ],
+      zip_safe=False,
+      version="0.5",
+      include_package_data=True,
+      keywords='telegraph api rest http telegraphapi telegram')
