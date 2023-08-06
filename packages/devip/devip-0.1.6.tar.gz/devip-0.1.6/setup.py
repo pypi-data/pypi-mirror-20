a3f7cf24df8db720b282c803bb103368ade97489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:34 +0000

    Add package devip/0.1.6/devip-0.1.6.tar.gz

---
+++ packages/devip/devip-0.1.6.tar.gz/devip-0.1.6/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='devip',
+    packages=['devip'],
+    version='0.1.6',
+    description='CLI that updates your public IP address in external services you use and depend on IP-authentication.',
+    author='Phivos Stylianides',
+    author_email='stphivos@gmail.com',
+    url='https://github.com/stphivos/devip',
+    keywords=['ip-authentication', 'aws'],
+    classifiers=[],
+    entry_points={
+        'console_scripts': [
+            'devip = devip.__main__:run',
+        ],
+    },
+)
