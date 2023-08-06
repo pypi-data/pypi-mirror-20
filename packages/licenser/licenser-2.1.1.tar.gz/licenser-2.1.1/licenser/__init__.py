75671b4113561b258a3bff7217410774264a448a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:07 +0000

    Add package licenser/2.1.1/licenser-2.1.1.tar.gz

---
+++ packages/licenser/licenser-2.1.1.tar.gz/licenser-2.1.1/licenser/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+'''
+licenser
+========
+
+Tool for adding open source licenses to your projects
+'''
+
+__copyright__ = 'Copyright (c) 2017 Ty-Lucas Kelley'
+__license__ = 'MIT License (see LICENSE)'
+__title__ = 'licenser'
+__version__ = '2.1.1'
