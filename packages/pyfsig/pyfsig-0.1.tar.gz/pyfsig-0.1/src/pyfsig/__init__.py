36a2c9c9064df8575224a417e222d2bc3dbc8af6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:51 +0000

    Add package pyfsig/0.1/pyfsig-0.1.tar.gz

---
+++ packages/pyfsig/pyfsig-0.1.tar.gz/pyfsig-0.1/src/pyfsig/__init__.py
@@ -0,0 +1,6 @@
+# constants
+from file_signatures import signatures
+# classes
+from file_signatures import Signature, Matches, NoMatchException
+# defs
+from file_signatures import compare_sig, get_from_file, get_from_path
