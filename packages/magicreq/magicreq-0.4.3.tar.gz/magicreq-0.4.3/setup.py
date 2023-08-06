116bdd2391a45ea50f256a03a041d769fdcc70fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:13 +0000

    Add package magicreq/0.4.3/magicreq-0.4.3.tar.gz

---
+++ packages/magicreq/magicreq-0.4.3.tar.gz/magicreq-0.4.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.4.3'
+
+setup(
+    name='magicreq',
+    version=VERSION,
+    author='Justin Patrin',
+    author_email='papercrane@reversefold.com',
+    maintainer='Justin Patrin',
+    maintainer_email='papercrane@reversefold.com',
+    description='magicreq',
+    long_description="""magicreq""",
+    packages=find_packages(),
+    url='https://github.com/reversefold/magicreq',
+)
---
+++ packages/magicreq/magicreq-0.4.3.tar.gz/magicreq-0.4.3/test/test.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+"""Usage:
+    %(script)s <url>
+"""
+from __future__ import print_function
+import os
+import subprocess
+import sys
+
+REQUIREMENTS = [line.strip() for line in iter(open('requirements.txt').readline, '') if line.strip()]
+
+try:
+    import pkg_resources
+    pkg_resources.require(REQUIREMENTS)
+    # import docopt
+    # import requests
+except Exception as exc:
+    # PIP_OPTIONS = '--index-url http://localhost:8080/pypi/simple/ --trusted-host localhost'
+    # VENV_VERSION = '15.0.2'
+    # PYPI_URL = 'http://localhost:8080/api/pypi/pypi'
+    # GET_PIP_URL = 'http://localhost:8080/get-pip.py'
+    if __name__ != '__main__':
+        raise
+    try:
+        import magicreq
+        magicreq.magic(
+            REQUIREMENTS,
+            # pip_options=PIP_OPTIONS,
+            # pypi_url=PYPI_URL,
+            # venv_version=VENV_VERSION,
+            # get_pip_url=GET_PIP_URL
+        )
+    except ImportError:
+        url = 'https://raw.githubusercontent.com/reversefold/magicreq/0.4.3/magicreq/bootstrap.py'
+        # url = 'https://raw.githubusercontent.com/reversefold/magicreq/master/magicreq/bootstrap.py'
+        # url = 'http://localhost:8000/magicreq/bootstrap.py'
+        curl = subprocess.Popen(['curl', '-sS', url], stdout=subprocess.PIPE)
+        python = subprocess.Popen(
+            [
+                sys.executable,
+                '-',
+                # 'PIP_OPTIONS:%s' % (PIP_OPTIONS,),
+                # 'VENV_VERSION:%s' % (VENV_VERSION,),
+                # 'PYPI_URL:%s' % (PYPI_URL,),
+                # 'GET_PIP_URL:%s' % (GET_PIP_URL,),
+            ] + sys.argv,
+            stdin=curl.stdout
+        )
+        curl.wait()
+        python.wait()
+        sys.exit(curl.returncode or python.returncode)
+
+
+import docopt
+import requests
+
+
+def main():
+    args = docopt.docopt(__doc__ % {'script': os.path.basename(__file__)})
+    print(requests.get(args['<url>']).text)
+    sys.exit(13)
+
+
+if __name__ == '__main__':
+    main()
