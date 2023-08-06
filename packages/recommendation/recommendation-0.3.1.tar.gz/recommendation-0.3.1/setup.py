2f5eb9b12ac4642c5c3623795acae43e30dc5634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:42 +0000

    Add package recommendation/0.3.1/recommendation-0.3.1.tar.gz

---
+++ packages/recommendation/recommendation-0.3.1.tar.gz/recommendation-0.3.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='recommendation',
+    version='0.3.1',
+    url='https://github.com/wikimedia/research-recommendation-api',
+    license='Apache Software License',
+    maintainer='Wikimedia Research',
+    maintainer_email='nschaaf@wikimedia.org',
+    description='Provide recommendations in Wikimedia projects',
+    long_description='',
+    packages=find_packages(exclude=['test', 'test.*', '*.test']),
+    install_requires=['flask',
+                      'flask-restplus',
+                      'requests',
+                      'numpy',
+                      'scipy',
+                      'sklearn'],
+    package_data={'recommendation.web': ['static/*.*',
+                                         'static/i18n/*',
+                                         'static/images/*',
+                                         'static/suggest-searches/*',
+                                         'templates/*'],
+                  'recommendation': ['data/*']},
+    zip_safe=False,
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest',
+                   'responses'])
