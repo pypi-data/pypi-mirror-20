9e970ed9b06793782f40d0a916a37af08c47aaad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:10 +0000

    Add package tap-shippo/0.1.0/tap-shippo-0.1.0.tar.gz

---
+++ packages/tap-shippo/tap-shippo-0.1.0.tar.gz/tap-shippo-0.1.0/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='tap-shippo',
+      version='0.1.0',
+      description='Singer.io tap for extracting data from the Shippo API',
+      author='Robert J. Moore',
+      url='http://singer.io',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['tap_shippo'],
+      install_requires=['singer-python>=0.2.1',
+                        'backoff==1.3.2',
+                        'requests==2.12.4'],
+      entry_points='''
+          [console_scripts]
+          tap-shippo=tap_shippo:main
+      ''',
+)
+
