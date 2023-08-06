db7037420a4100812f6f437f9893e6ec45859f55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:23 +0000

    Add package graaf/0.1.0/graaf-0.1.0.tar.gz

---
+++ packages/graaf/graaf-0.1.0.tar.gz/graaf-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+
+setup(
+    name='graaf',
+    version='0.1.0',
+    description='Simple static site generator',
+    author='Curtis Maloney',
+    author_email='curtis@tinbrain.net',
+    url='https://github.com/funkybob/graaf',
+    packages=['graaf',],
+    install_requires=[
+        'stencil-template',
+        'Markdown',
+        'pyyaml',
+    ],
+    scripts=['scripts/graaf'],
+)
