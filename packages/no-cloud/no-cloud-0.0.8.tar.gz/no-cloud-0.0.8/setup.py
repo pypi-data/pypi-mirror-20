809f8f062bc910261373672b5a95fed8b7ae8251
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:23 +0000

    Add package no-cloud/0.0.8/no-cloud-0.0.8.tar.gz

---
+++ packages/no-cloud/no-cloud-0.0.8.tar.gz/no-cloud-0.0.8/setup.py
@@ -0,0 +1,68 @@
+# Copyright (C) 2016 Benoit Myard <myardbenoit@gmail.com>
+# Released under the terms of the BSD license.
+
+# To release a new version:
+#
+#   $ python setup.py sdist upload
+#   $ python setup.py bdist_egg upload
+#   $ python setup.py bdist_wheel upload
+
+import re
+
+from setuptools import setup
+
+VERSION_RE = re.compile(r"__version__ = '(.*)'")
+
+
+def version(filename):
+    with open(filename) as file:
+        source = file.read()
+
+    return VERSION_RE.search(source).group(1)
+
+
+def requirements(filename):
+    with open(filename) as file:
+        source = file.read()
+
+    return source.split()
+
+
+version = version('no_cloud/__init__.py')
+requirements = requirements('requirements.txt')
+
+
+setup(
+    name='no-cloud',
+    version=version,
+    url='http://github.com/saalaa/no-cloud',
+    license='BSD',
+    author='Benoit Myard',
+    author_email='myardbenoit@gmail.com',
+    description='There is no cloud',
+    long_description='See http://github.com/saalaa/no-cloud.',
+    install_requires=requirements,
+    zip_safe=False,
+    platforms='any',
+    data_files=[
+        ('assets', ['assets/stylesheet.css'])
+    ],
+    packages=[
+        'no_cloud',
+        'no_cloud.remote'
+    ],
+    entry_points={
+        'console_scripts': [
+            'no-cloud = no_cloud.cli:main'
+        ]
+    },
+    classifiers=[
+        'Topic :: Utilities',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3'
+    ]
+)
