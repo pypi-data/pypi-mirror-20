c59313e90272d96d8fa8da18930653d44ad0f75a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:15 +0000

    Add package aioelasticsearch/0.1.2/aioelasticsearch-0.1.2.tar.gz

---
+++ packages/aioelasticsearch/aioelasticsearch-0.1.2.tar.gz/aioelasticsearch-0.1.2/aioelasticsearch/compat.py
@@ -0,0 +1,30 @@
+import asyncio
+import sys
+from functools import partial
+
+PY_344 = sys.version_info >= (3, 4, 4)
+PY_350 = sys.version_info >= (3, 5, 0)
+PY_352 = sys.version_info >= (3, 5, 2)
+
+
+def create_task(*, loop=None):
+    if loop is None:
+        loop = asyncio.get_event_loop()
+
+    if PY_352:
+        return loop.create_task
+
+    if PY_344:
+        return partial(asyncio.ensure_future, loop=loop)
+
+    return partial(getattr(asyncio, 'async'), loop=loop)
+
+
+def create_future(*, loop=None):
+    if loop is None:
+        loop = asyncio.get_event_loop()
+
+    if PY_352:
+        return loop.create_future()
+
+    return asyncio.Future(loop=loop)
