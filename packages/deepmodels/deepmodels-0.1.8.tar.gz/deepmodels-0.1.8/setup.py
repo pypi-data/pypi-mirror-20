a903f7339fd19f54435e3bb9692c833f67303ef0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:16 +0000

    Add package deepmodels/0.1.8/deepmodels-0.1.8.tar.gz

---
+++ packages/deepmodels/deepmodels-0.1.8.tar.gz/deepmodels-0.1.8/setup.py
@@ -0,0 +1,24 @@
+"""For distribution.
+"""
+
+from setuptools import setup, find_packages
+
+# build with sdist to make sure platform compatibility.
+setup(
+    name="deepmodels",
+    version="0.1.8",
+    description="framework for build, train and test deep learning models",
+    url="https://github.com/flyfj/deepmodels",
+    author="Jie Feng",
+    author_email="jiefengdev@gmail.com",
+    license="MIT",
+    include_package_data=True,
+    packages=find_packages(exclude=["docs"]),
+    install_requires=["numpy==1.11.2",
+		      "scipy==0.18.1",
+                      "scikit_image==0.12.3",
+                      "scikit_learn==0.18.1",
+                      "tensorflow==0.12.1",
+                      "tqdm==4.10.0",
+                      "Pillow==4.0.0", 
+		      "lasagne==0.1"])
