28fc1be5669bcfc16e55b67365e1cc5387fa3a96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:09 +0000

    Add package easydev/0.9.34/easydev-0.9.34.tar.gz

---
+++ packages/easydev/easydev-0.9.34.tar.gz/easydev-0.9.34/test/test_platform.py
@@ -0,0 +1,26 @@
+from easydev.platform import get_platform, linux_distribution, is_windows
+from easydev.platform import is_linux, is_mac
+
+try:
+    from unittest.mock import patch
+except:
+    from mock import patch
+
+
+
+def test_platform():
+
+    assert get_platform() in ['Linux', 'Windows', 'Mac']
+
+    linux_distribution()
+
+    is_windows()
+    is_linux()
+    is_mac()
+
+
+
+    def func():
+        raise Exception
+    with patch("platform.linux_distribution", func):
+        get_platform()
