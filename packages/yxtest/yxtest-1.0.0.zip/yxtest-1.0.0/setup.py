ed95a2d7d87901067a94a8f8d99ae3ac47d299a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:00 +0000

    Add package yxtest/1.0.0/yxtest-1.0.0.zip

---
+++ packages/yxtest/yxtest-1.0.0.zip/yxtest-1.0.0/setup.py
@@ -0,0 +1,14 @@
+
+
+from distutils.core import setup
+
+
+setup(
+    name='yxtest',
+    version='1.0.0',
+    py_modules='yxtest',
+    author='yxtest',
+    author_email='yxtest@163.com',
+    #url='http://www.34miao.com',
+    description='yxtest',
+)
