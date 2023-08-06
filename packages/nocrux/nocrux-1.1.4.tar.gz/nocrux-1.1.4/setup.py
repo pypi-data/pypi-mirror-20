267e62f436bd360d795255b1cd87784db51b10a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:48 +0000

    Add package nocrux/1.1.4/nocrux-1.1.4.tar.gz

---
+++ packages/nocrux/nocrux-1.1.4.tar.gz/nocrux-1.1.4/setup.py
@@ -0,0 +1,47 @@
+# Copyright (c) 2016  Niklas Rosenstein
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import sys
+from setuptools import setup
+from os import system, path
+from codecs import open
+
+if path.isfile('README.md') and 'dist' in sys.argv:
+  assert 0 == system('pandoc -s README.md -o README.rst')
+  with open('README.rst', encoding='utf8') as fp:
+    long_description = fp.read()
+else:
+  long_description = None
+
+setup(
+  name='nocrux',
+  version='1.1.4',
+  description='painless per-user daemon manager',
+  long_description=long_description,
+  author='Niklas Rosenstein',
+  author_email='rosensteinniklas@gmail.com',
+  url='https://github.com/NiklasRosenstein/nocrux',
+  py_modules=['nocrux'],
+  entry_points=dict(
+    console_scripts=[
+      'nocrux=nocrux:main'
+    ]
+  ),
+)
