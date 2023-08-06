92edc5b8f14c51568818c2b5560eff977175e1b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:24 +0000

    Add package netforce-test/0.1/netforce_test-0.1.tar.gz

---
+++ packages/netforce-test/netforce_test-0.1.tar.gz/netforce_test-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='netforce_test',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['netforce_test'],
+      zip_safe=False)
