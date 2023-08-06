e0e93dcd332a7ffc9bdc0d86bd9286f1d64f9785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:07 +0000

    Add package cvbase/0.1.1/cvbase-0.1.1-py2.py3-none-any.whl

---
+++ packages/cvbase/cvbase-0.1.1-py2.py3-none-any.whl/cvbase/opencv.py
@@ -0,0 +1,15 @@
+import cv2
+
+
+def use_opencv3():
+    return cv2.__version__.split('.')[0] == '3'
+
+
+USE_OPENCV3 = use_opencv3()
+
+if USE_OPENCV3:
+    from cv2 import IMREAD_COLOR, IMREAD_GRAYSCALE, IMREAD_ANYDEPTH
+else:
+    from cv2 import CV_LOAD_IMAGE_COLOR as IMREAD_COLOR
+    from cv2 import CV_LOAD_IMAGE_GRAYSCALE as IMREAD_GRAYSCALE
+    from cv2 import CV_LOAD_IMAGE_ANYDEPTH as IMREAD_ANYDEPTH
