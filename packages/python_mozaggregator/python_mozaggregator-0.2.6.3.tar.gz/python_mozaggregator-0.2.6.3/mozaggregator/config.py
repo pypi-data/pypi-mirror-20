4a47dd8a51dc9786a3772bbe104573b231032dc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:55 +0000

    Add package python_mozaggregator/0.2.6.3/python_mozaggregator-0.2.6.3.tar.gz

---
+++ packages/python_mozaggregator/python_mozaggregator-0.2.6.3.tar.gz/python_mozaggregator-0.2.6.3/mozaggregator/config.py
@@ -0,0 +1,10 @@
+REGION="us-west-2"
+BUCKET="telemetry-spark-emr-2"
+SECRET="aggregator_credentials"
+RDS="telemetry-aggregates"
+TIMEOUT=24*60*60
+MINCONN=4
+MAXCONN=64
+CACHETYPE="simple"
+USE_PRODUCTION_DB=True
+
