520a6f3b640b1bd84fd0255ef864756f983bab45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:42 +0000

    Add package playlistfromsong/0.7/playlistfromsong-0.7.tar.gz

---
+++ packages/playlistfromsong/playlistfromsong-0.7.tar.gz/playlistfromsong-0.7/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+setup(
+  name = 'playlistfromsong',
+  packages = ['playlistfromsong'], 
+  version = '0.7',
+  description = 'An offline music station generator',
+  author = 'Zack Scholl',
+  author_email = 'hypercube.platforms@gmail.com',
+  url = 'https://github.com/schollz/playlistfromsong',
+  download_url = 'https://github.com/schollz/playlistfromsong/archive/v0.7.tar.gz', 
+  keywords = ['music', 'youtube', 'playlist'], 
+  classifiers = [],
+  install_requires=[
+        "requests",
+        "youtube_dl",
+  ],
+  entry_points={'console_scripts': 
+        [
+        'playlistfromsong = playlistfromsong.__main__:main', 
+        ], 
+  },
+)
