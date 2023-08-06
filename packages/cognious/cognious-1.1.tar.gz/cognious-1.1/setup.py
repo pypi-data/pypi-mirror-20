302ff925557cb384959f8ad41aeadf5da9c85cea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:14 +0000

    Add package cognious/1.1/cognious-1.1.tar.gz

---
+++ packages/cognious/cognious-1.1.tar.gz/cognious-1.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'cognious',
+  packages = ['cognious'], # this must be the same as the name above
+  version = '1.1',
+  description = 'Cognious API Python Client',
+  author = 'Tarun Paul',
+  author_email = 'tarunpaul@cognious.com',
+  url = 'https://github.com/tarunpaul/cognious', # use the URL to the github repo
+  download_url = 'https://github.com/tarunpaul/cognious/tarball/1.1', # tarball
+  keywords = ['cognious', 'api', 'machinelearning'], # arbitrary keywords
+  license="Apache 2.0",
+  classifiers = [],
+)
