c20fb6f18e67babd3f535445ac25aecb983181df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:09 +0000

    Add package recursive_print_list/1.0.0/recursive_print_list-1.0.0.tar.gz

---
+++ packages/recursive_print_list/recursive_print_list-1.0.0.tar.gz/recursive_print_list-1.0.0/recursive_print_list.py
@@ -0,0 +1,13 @@
+#encoding:utf-8
+"""此模块提供一个递归打印list内容函数"""
+# you could use this model recursive print list item 
+
+def printList(Alist):
+	if isinstance(Alist,list):
+		for each_item in Alist:
+			if isinstance(each_item,list):
+				printList(each_item)
+			else:
+				print(each_item)
+
+
