43ccfabd2c9130b35cd29effbc1b758c66353bf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:17 +0000

    Add package pygster_logparsers/0.1.7/pygster_logparsers-0.1.7.tar.gz

---
+++ packages/pygster_logparsers/pygster_logparsers-0.1.7.tar.gz/pygster_logparsers-0.1.7/README.md
@@ -0,0 +1,10 @@
+# pygster_logparsers
+Parsers for pygster https://pypi.python.org/pypi/pygster
+
+Usage:
+
+`pygster [options] pygster_logparsers.PARSER logfile`
+
+PARSER is only HAProxyPygster on this moment...
+
+pygster options like as `logster` https://github.com/etsy/logster
---
+++ packages/pygster_logparsers/pygster_logparsers-0.1.7.tar.gz/pygster_logparsers-0.1.7/setup.py
@@ -0,0 +1,23 @@
+
+
+__title__ = 'pygster_logparsers'
+__version__ = '0.1.7'
+__author__ = 'Serg Belyakov  <serg@itlbs.ru>'
+
+
+from distutils.core import setup
+
+setup(
+  name = __title__,
+  packages = [__title__, ],
+  version = __version__,
+  description = 'HAProxy parser for pygster',
+  long_description = open('README.md').read(),
+  author = 'Serg Belyakov',
+  author_email = 'serg@itlbs.ru',
+  url = 'https://github.com/serg-it/pygster-parsers',
+  license=open('LICENSE').read(),
+  install_requires=['pygster==1.0.1', 'raven==6.0.0'],
+  package_dir={'pygster_logparsers': 'pygster_logparsers'},
+  zip_safe=False,
+)
