7acae32cf42ea8ea952f54212636cc36f79407ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:46 +0000

    Add package BinField/0.7.4/BinField-0.7.4.tar.gz

---
+++ packages/BinField/BinField-0.7.4.tar.gz/BinField-0.7.4/setup.py
@@ -0,0 +1,58 @@
+#    Copyright 2016 - 2017 Alexey Stepanov aka penguinolog
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Python BinField implementation"""
+
+import setuptools
+
+import binfield
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='BinField',
+    version=binfield.__version__,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+        'License :: OSI Approved :: Apache Software License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: Implementation :: Jython',
+    ],
+    keywords='binary binfield development',
+    url='https://github.com/penguinolog/binfield',
+    license='Apache License, Version 2.0',
+    author='Alexey Stepanov',
+    author_email='penguinolog@gmail.com',
+    description='Python BinField implementation for binary data manipulation',
+    long_description=long_description,
+    install_requires=['six'],
+)
