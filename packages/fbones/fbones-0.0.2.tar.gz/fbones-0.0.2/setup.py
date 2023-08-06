c652b73e408455148e5c4a2666a82f222385a1dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:15 +0000

    Add package fbones/0.0.2/fbones-0.0.2.tar.gz

---
+++ packages/fbones/fbones-0.0.2.tar.gz/fbones-0.0.2/setup.py
@@ -0,0 +1,23 @@
+# coding=utf8
+
+
+from setuptools import setup
+
+setup(name='fbones',
+      version='0.0.2',
+      description='A bootstrap toolkit to kickoff a flask project',
+      url='https://github.com/ipconfiger/fbones',
+      author='Alexander.Li',
+      author_email='superpowerlee@gmail.com',
+      license='GNU GENERAL PUBLIC LICENSE',
+      packages=['fbones'],
+      install_requires=[
+          'flask',
+          'click',
+          'alembic',
+          'flask_doc'
+      ],
+      entry_points={
+        'console_scripts': ['fbones=fbones.fbones:main'],
+      },
+      zip_safe=False)
