cce43458ccd7cce2db5234eb340377011d0dda64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:44 +0000

    Add package pylimit/0.1.7/pylimit-0.1.7.tar.gz

---
+++ packages/pylimit/pylimit-0.1.7.tar.gz/pylimit-0.1.7/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pylimit',
+  packages = ['pylimit'],
+  version = '0.1.7',
+  description = 'A distributed rate limiting library for python using leaky bucket algorithm and Redis',
+  author='Biplap Sarkar',
+  author_email='biplap.sarkar@gmail.com',
+  url='https://github.com/biplap-sarkar/pylimit',
+  download_url='https://github.com/biplap-sarkar/pylimit/archive/v0.1.7.tar.gz',
+  keywords=['rate limiting', 'throttle', 'redis'],
+  classifiers=[],
+)
