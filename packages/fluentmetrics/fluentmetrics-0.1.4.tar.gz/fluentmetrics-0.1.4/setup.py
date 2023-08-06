b7f6e5e0ae370cbebb9f7019a2d1121888f1407f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:03 +0000

    Add package fluentmetrics/0.1.4/fluentmetrics-0.1.4.tar.gz

---
+++ packages/fluentmetrics/fluentmetrics-0.1.4.tar.gz/fluentmetrics-0.1.4/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name='fluentmetrics',
+  packages=['fluentmetrics'],
+  version='0.1.4',
+  description='Fluent AWS Metrics Logging',
+  author='Troy Larson',
+  author_email='troylar@gmail.com',
+  url='https://github.com/troylar/FluentMetrics',
+  download_url='https://github.com/troylar/FluentMetrics/tarball/0.1',
+  keywords=['metrics', 'logging', 'aws'],
+  install_requires=['arrow', 'boto3'],
+  classifiers=[],
+)
