a8a4e48d9d3310bbf1a7c9260448cbaf1f8c8b46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:17 +0000

    Add package voiceplay/0.2.5.3/voiceplay-0.2.5.3.tar.gz

---
+++ packages/voiceplay/voiceplay-0.2.5.3.tar.gz/voiceplay-0.2.5.3/voiceplay/webapp/pages/indexpage/index.py
@@ -0,0 +1,11 @@
+import time
+from flask import render_template
+from flask_classy import FlaskView, route
+
+class IndexView(FlaskView):
+    route_base = '/'
+    @route('/')
+    @route('/index.html')
+    def index(self):
+        timestamp = int(time.time())
+        return render_template('my_index.html', timestamp=timestamp)
