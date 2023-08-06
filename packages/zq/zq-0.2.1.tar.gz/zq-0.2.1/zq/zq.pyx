1039acdf2a5a1b79142352743c35d9eedec3b81c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:00 +0000

    Add package zq/0.2.1/zq-0.2.1.tar.gz

---
+++ packages/zq/zq-0.2.1.tar.gz/zq-0.2.1/zq/zq.pyx
@@ -0,0 +1,32 @@
+from gevent import monkey
+monkey.patch_all()
+import sys
+import UserDict
+import time
+import simplejson
+import urllib
+import types
+import gevent
+import uuid
+from hy.lex import parser, lexer
+from hy.importer import hy_eval
+from hy import HyList
+
+
+include "zq_symbols.pyx"
+include "zq_constants.pyi"
+include "zq_lib.pyx"
+include "zq_pyzabbix.pyx"
+include "zq_cfg.pyx"
+include "zq_env.pyx"
+include "zq_query.pyx"
+include "zq_query_status.pyx"
+include "zq_query_vars.pyx"
+include "zq_query_stackless.pyx"
+include "zq_query_words.pyx"
+include "zq_query_group.pyx"
+include "zq_query_output.pyx"
+include "zq_query_stack.pyx"
+include "zq_query_filters.pyx"
+
+
---
+++ packages/zq/zq-0.2.1.tar.gz/zq-0.2.1/zq/zq_query_words.pyx
@@ -0,0 +1,36 @@
+
+
+def Version(ctx):
+    ctx.push({'VERSION': ctx.zapi.api_version()})
+    return ctx
+
+def Hosts(ctx, **kw):
+    ctx.push({'HOST': ctx.zapi.host.get()})
+    return ctx
+
+def Interfaces(ctx):
+    ctx.push({'INTERFACES': ctx.zapi.hostinterface.get()})
+    return ctx
+
+def Hostgroups(ctx, **kw):
+    res = apply(ctx.zapi.hostgroup.get, (), kw)
+    ctx.push({'HOSTGROUPS': res})
+    return ctx
+
+
+def Templates(ctx):
+    ctx.push({'TEMPLATES': ctx.zapi.template.get()})
+    return ctx
+
+def Items(ctx):
+    ctx.push({'ITEMS': ctx.zapi.item.get()})
+    return ctx
+
+def Triggers(ctx):
+    ctx.push({'TRIGGERS': ctx.zapi.trigger.get()})
+    return ctx
+
+def Proxies(ctx):
+    ctx.push({'PROXIES': ctx.zapi.proxy.get()})
+    return ctx
+
