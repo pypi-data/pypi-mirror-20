82a2e212c1e5b7f66d7bee64955ec57219bf2880
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:57 +0000

    Add package pixelpin-auth-storage-sqlalchemy/1.0.0/pixelpin-auth-storage-sqlalchemy-1.0.0.tar.gz

---
+++ packages/pixelpin-auth-storage-sqlalchemy/pixelpin-auth-storage-sqlalchemy-1.0.0.tar.gz/pixelpin-auth-storage-sqlalchemy-1.0.0/CHANGELOG.md
@@ -0,0 +1,29 @@
+# Change Log
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/)
+and this project adheres to [Semantic Versioning](http://semver.org/).
+
+## [Unreleased]
+
+## [1.0.1](https://github.com/python-social-auth/social-storage-sqlalchemy/releases/tag/1.0.1) - 2017-01-29
+
+### Changed
+- Fixed partial instance deletion
+
+## [1.0.0](https://github.com/python-social-auth/social-storage-sqlalchemy/releases/tag/1.0.0) - 2017-01-22
+
+### Added
+- Added partial pipeline db storage solution
+
+### Changed
+- Fixed the JSON pickler implementation to complain SQLAlchemy
+  invocation of the `dumps` method
+
+## [0.0.1](https://github.com/python-social-auth/social-storage-sqlalchemy/releases/tag/0.0.1) - 2016-11-27
+
+### Changed
+
+- Split from the monolitic [python-social-auth](https://github.com/omab/python-social-auth)
+  codebase
---
+++ packages/pixelpin-auth-storage-sqlalchemy/pixelpin-auth-storage-sqlalchemy-1.0.0.tar.gz/pixelpin-auth-storage-sqlalchemy-1.0.0/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""Setup file for easy installation"""
+from os.path import join, dirname
+from setuptools import setup
+
+
+def long_description():
+    return open(join(dirname(__file__), 'README.md')).read()
+
+def load_requirements():
+    return open(join(dirname(__file__), 'requirements.txt')).readlines()
+
+setup(
+    name='pixelpin-auth-storage-sqlalchemy',
+    version=__import__('pixelpin_auth_sqlalchemy').__version__,
+    author=['Matias Aguirre', 'Callum Brankin'],
+    author_email="callum@pixelpin.co.uk",
+    description='Python Social Authentication, SQLAlchemy storage.',
+    license='BSD',
+    keywords='sqlalchemy, social auth, pixelpin auth',
+    url='https://github.com/python-social-auth/social-storage-sqlalchemy',
+    packages=['pixelpin_auth_sqlalchemy'],
+    long_description=long_description(),
+    install_requires=load_requirements(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Internet',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3'
+    ],
+    zip_safe=False
+)
