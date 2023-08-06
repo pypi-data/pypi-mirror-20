e72984eb8312d25439c6d590cd1f1dee973bd71e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:28 +0000

    Add package zsearch-definitions/0.0.56/zsearch-definitions-0.0.56.tar.gz

---
+++ packages/zsearch-definitions/zsearch-definitions-0.0.56.tar.gz/zsearch-definitions-0.0.56/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "zsearch-definitions",
+    description = "Egg version of files that can be used for interacting with zdb through python",
+    version = "0.0.56",
+    license = "Apache License, Version 2.0",
+    author = "Zakir Durumeric",
+    author_email = "zakird@gmail.com",
+    maintainer = "Zakir Durumeric",
+    maintainer_email = "zakird@gmail.com",
+    keywords = "python json zsearch",
+    zip_safe = False,
+    install_requires = [
+        "zschema"
+    ],
+    packages = [
+        "zsearch_definitions",
+    ]
+)
+
