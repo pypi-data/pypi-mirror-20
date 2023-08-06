0121b9149feff5264ae33101975190cc919acd19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:24 +0000

    Add package saysonichello/0.0.2/saysonichello-0.0.2.tar.gz

---
+++ packages/saysonichello/saysonichello-0.0.2.tar.gz/saysonichello-0.0.2/say_hello/hello.py
@@ -0,0 +1,14 @@
+# -*- coding:utf8 -*-
+import requests
+
+def hello(country):
+
+    upper_country = country.upper()
+    if upper_country == 'KOR':
+        print('안녕하세요!')
+    elif upper_country == 'ENG':
+        print('Hello!')
+    elif upper_country == 'AZ':
+        print("abc")
+    else:
+        print('안녕하세요, Hello!')
