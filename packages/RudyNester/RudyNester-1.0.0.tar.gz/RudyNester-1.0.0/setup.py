ed46d5eabea0bbd3776c28c972b88d754be925ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:01 +0000

    Add package RudyNester/1.0.0/RudyNester-1.0.0.tar.gz

---
+++ packages/RudyNester/RudyNester-1.0.0.tar.gz/RudyNester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name            ='RudyNester',
+        version         ='1.0.0',
+        py_modules      =['rudynester'],
+        author          ='Rudy',
+        author_email    ='rodolfohill@yahoo.com',
+        url             ='http://www.headfirstlabs.com',
+        description     ='A simple module to def a function',
+    )
