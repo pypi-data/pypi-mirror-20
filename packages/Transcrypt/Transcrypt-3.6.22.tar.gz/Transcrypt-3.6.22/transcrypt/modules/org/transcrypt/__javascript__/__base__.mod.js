68de25497247a2617281cd03c7b9c1458e5d70ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:24 +0000

    Add package Transcrypt/3.6.22/Transcrypt-3.6.22.tar.gz

---
+++ packages/Transcrypt/Transcrypt-3.6.22.tar.gz/Transcrypt-3.6.22/transcrypt/modules/org/transcrypt/__javascript__/__base__.mod.js
@@ -0,0 +1,23 @@
+	__nest__ (
+		__all__,
+		'org.transcrypt.__base__', {
+			__all__: {
+				__inited__: false,
+				__init__: function (__all__) {
+					var __Envir__ = __class__ ('__Envir__', [object], {
+						get __init__ () {return __get__ (this, function (self) {
+							self.interpreter_name = 'python';
+							self.transpiler_name = 'transcrypt';
+							self.transpiler_version = '3.6.22';
+							self.target_subdir = '__javascript__';
+						});}
+					});
+					var __envir__ = __Envir__ ();
+					__pragma__ ('<all>')
+						__all__.__Envir__ = __Envir__;
+						__all__.__envir__ = __envir__;
+					__pragma__ ('</all>')
+				}
+			}
+		}
+	);
