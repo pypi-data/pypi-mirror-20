d19999647fbf7117daa5344a37b0d00b1c8e9c11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:42 +0000

    Add package py-build-server/0.3.2/py-build-server-0.3.2.tar.gz

---
+++ packages/py-build-server/py-build-server-0.3.2.tar.gz/py-build-server-0.3.2/setup.py
@@ -0,0 +1,24 @@
+import sys
+from setuptools import setup, find_packages
+
+if sys.version > (3, ):
+    sys.exit('Sorry, Python3 is not supported at this stage.')
+setup(
+    name='py-build-server',
+    version='0.3.2',
+    packages=find_packages(),
+    license='GPL3',
+    author='chestm007',
+    url='https://github.com/chestm007/py_build_server',
+    author_email='chestm007@hotmail.com',
+    description='My build server i run on my home server to check for git repo tags, and push to pypi',
+    entry_points="""
+        [console_scripts]
+        py-build-server=py_build_server.main:main
+    """,
+    requires=[
+        'click',
+        'gitpython',
+        'pyyaml'
+    ]
+)
