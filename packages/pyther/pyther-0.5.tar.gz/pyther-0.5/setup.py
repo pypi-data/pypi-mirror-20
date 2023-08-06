80a70a32d505ac0718c384983e9c63e7d4024bc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:05 +0000

    Add package pyther/0.5/pyther-0.5.tar.gz

---
+++ packages/pyther/pyther-0.5.tar.gz/pyther-0.5/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+	name = "pyther",
+	packages = ["pyther_pysg"],
+	version = "0.5",
+	description = "A open source library for processing thermodynamics data",
+	author = "pysg",
+	author_email = "andres.pyther@gmail.com",
+	url = "https://github.com/pysg/pyther",
+	keyword = ["data analytics", "phases diagram", "thermodynamics"]
+
+)
