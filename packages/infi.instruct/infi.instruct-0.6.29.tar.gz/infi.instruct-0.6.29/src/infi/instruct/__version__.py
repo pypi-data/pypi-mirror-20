321991a4c21495bc2657dc59f12624bb31a16b5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:39 +0000

    Add package infi.instruct/0.6.29/infi.instruct-0.6.29.tar.gz

---
+++ packages/infi.instruct/infi.instruct-0.6.29.tar.gz/infi.instruct-0.6.29/src/infi/instruct/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.6.29"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.6.29)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '0b565b9897d1d27f787dec5476a9daa780e32eb5'
+__git_head_subject__ = u'Finished Release v0.6.29'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2017-03-01 12:10:30'
