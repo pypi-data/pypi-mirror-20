6c182deb38f4135ded0785ff1508dc53001a8e89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:55 +0000

    Add package instadb/0.1/instadb-0.1.tar.gz

---
+++ packages/instadb/instadb-0.1.tar.gz/instadb-0.1/setup.py
@@ -0,0 +1,35 @@
+
+from distutils.core import setup
+from setuptools import find_packages
+
+required = [
+    "pandas==0.19.2",
+    "psycopg2==2.6.2"
+]
+
+setup(
+    name="instadb",
+    version="0.1",
+    author="Mathieu Ripert",
+    author_email="mathieu@instacart.com",
+    url="https://github.com/mathieuripert/instadb",
+    license="BSD",
+    packages=find_packages(),
+    package_dir={"instadb": "instadb"},
+    description="A simple and light DB package",
+    install_requires=required,
+    classifiers=[
+        # Maturity
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+        # License
+        'License :: OSI Approved :: BSD License',
+        # Versions supported
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+)
