074772b3f139063244645d4fd348cb033eff7589
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:59 +0000

    Add package micromagneticmodel/0.7.1/micromagneticmodel-0.7.1.tar.gz

---
+++ packages/micromagneticmodel/micromagneticmodel-0.7.1.tar.gz/micromagneticmodel-0.7.1/micromagneticmodel/hamiltonian/exchange.py
@@ -0,0 +1,30 @@
+import joommfutil.typesystem as ts
+from .energyterm import EnergyTerm
+
+
+@ts.typesystem(A=ts.UnsignedReal,
+               name=ts.ObjectName)
+class Exchange(EnergyTerm):
+    _latex = ("$A [(\\nabla m_{x})^{2} + "
+              "(\\nabla m_{y})^{2} + "
+              "(\\nabla m_{z})^{2}]$")
+
+    def __init__(self, A, name="exchange"):
+        """An exchange energy class.
+
+        Args:
+            A (Real): exchange energy constant (J/m)
+
+        """
+        self.A = A
+        self.name = name
+
+    @property
+    def _repr(self):
+        """A representation string property.
+
+        Returns:
+           A representation string.
+
+        """
+        return "Exchange(A={})".format(self.A)
---
+++ packages/micromagneticmodel/micromagneticmodel-0.7.1.tar.gz/micromagneticmodel-0.7.1/micromagneticmodel/hamiltonian/uniaxialanisotropy.py
@@ -0,0 +1,31 @@
+import joommfutil.typesystem as ts
+from .energyterm import EnergyTerm
+
+
+@ts.typesystem(K=ts.Real,
+               u=ts.RealVector(size=3),
+               name=ts.ObjectName)
+class UniaxialAnisotropy(EnergyTerm):
+    _latex = "$K (\mathbf{m} \cdot \mathbf{u})^{2}$"
+
+    def __init__(self, K, u, name="uniaxialanisotropy"):
+        """A uniaxial anisotropy energy abstract class.
+
+        Args:
+            K (Real): Uniaxial anisotropy energy constant (J/m**3)
+            u (tuple, list, np.ndarray): Easy axis
+
+        """
+        self.K = K
+        self.u = u
+        self.name = name
+
+    @property
+    def _repr(self):
+        """A representation string property.
+
+        Returns:
+           A representation string.
+
+        """
+        return "UniaxialAnisotropy(K={}, u={})".format(self.K, self.u)
