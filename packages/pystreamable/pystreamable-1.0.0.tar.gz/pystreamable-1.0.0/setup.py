e575a4cc25c151fd49b7e5168c97f4b126472bc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:50 +0000

    Add package pystreamable/1.0.0/pystreamable-1.0.0.tar.gz

---
+++ packages/pystreamable/pystreamable-1.0.0.tar.gz/pystreamable-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name="pystreamable",
+    version="1.0.0",
+    author="Matej Repinc",
+    author_email="mrepinc@gmail.com",
+    description="streamable.com API wrapper",
+    license="MIT",
+    keywords="streamable streamable.com api wrapper",
+    url="https://github.com/jernejovc/pystreamable",
+    packages=['pystreamable'],
+    long_description=("A wrapper for streamable.com API, supports uploading "
+                      "videos, importing videos, retrieving videos to and "
+                      "from streamable.com."),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
