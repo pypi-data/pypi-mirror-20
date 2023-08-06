0b6ff97d034fe31bc88f7975d876ef0a298615a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:37 +0000

    Add package Tubes/0.1.0/Tubes-0.1.0.tar.gz

---
+++ packages/Tubes/Tubes-0.1.0.tar.gz/Tubes-0.1.0/setup.py
@@ -0,0 +1,24 @@
+"""
+Setup file for tubes.
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='Tubes',
+    version='0.1.0',
+    description="""
+    Flow control and backpressure for event-driven applications.
+    """,
+    license="MIT",
+    url="https://github.com/twisted/tubes/",
+
+    packages=find_packages(exclude=[]),
+    package_dir={'tubes': 'tubes'},
+    install_requires=[
+        "characteristic",
+        "six",
+        "Twisted",
+    ],
+    include_package_data=True,
+)
