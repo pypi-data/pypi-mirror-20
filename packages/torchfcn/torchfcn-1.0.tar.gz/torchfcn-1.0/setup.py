ec757064566e589ce817ae5bc0f6c57deb0bc345
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:58 +0000

    Add package torchfcn/1.0/torchfcn-1.0.tar.gz

---
+++ packages/torchfcn/torchfcn-1.0.tar.gz/torchfcn-1.0/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+__version__ = '1.0'
+
+
+setup(
+    name='torchfcn',
+    version=__version__,
+    packages=find_packages(),
+    description='PyTorch Implementation of Fully Convolutional Networks.',
+    long_description=open('README.md').read(),
+    package_data={'torchfcn': ['torchfcn/ext/*']},
+    include_package_data=True,
+    author='Kentaro Wada',
+    author_email='www.kentaro.wada@gmail.com',
+    license='MIT',
+    url='https://github.com/wkentaro/torchfcn',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+)
