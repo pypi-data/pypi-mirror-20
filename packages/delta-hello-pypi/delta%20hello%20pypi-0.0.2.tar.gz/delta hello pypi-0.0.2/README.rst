0a647b4e284cafae5c53ceac3f1f5056418f40b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:46 +0000

    Add package delta-hello-pypi/0.0.2/delta%20hello%20pypi-0.0.2.tar.gz

---
+++ packages/delta-hello-pypi/delta%20hello%20pypi-0.0.2.tar.gz/delta hello pypi-0.0.2/README.rst
@@ -0,0 +1,28 @@
+Hello PyPI
+==========
+
+An hello world package. Get familar with Pypi__.
+
+.. __: https://pypi.python.org/pypi
+
+Install
+=======
+
+::
+
+    pip install delta-hello-pypi
+
+
+Usage
+=====
+
+::
+
+    from hello import hello
+
+    print(hello("PyPI")) # => Hello PyPI
+
+Reference
+=========
+
+1. https://packaging.python.org
---
+++ packages/delta-hello-pypi/delta%20hello%20pypi-0.0.2.tar.gz/delta hello pypi-0.0.2/setup.py
@@ -0,0 +1,24 @@
+"""Hello PyPI
+
+A hello world package in python
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "delta hello pypi",
+    version = "0.0.2",
+    description = "a simple python package",
+    long_description = 'hello pypi, a simple python package',
+    url = "https://github.com/delta4d/hello-pypi",
+    author = "delta",
+    email = "delta4d@gmail.com",
+    license = "MIT",
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3"
+    ],
+    keywords = "hello-world pypi setuptools",
+    packages = find_packages(exclude = ["tests"])
+)
