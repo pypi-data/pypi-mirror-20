c23ba917abefd24d95e502cf116da15ef9d4a42d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:14 +0000

    Add package siridb-connector/2.0.5/siridb-connector-2.0.5.tar.gz

---
+++ packages/siridb-connector/siridb-connector-2.0.5.tar.gz/siridb-connector-2.0.5/setup.py
@@ -0,0 +1,36 @@
+from distutils.core import setup
+import setuptools
+
+VERSION = '2.0.5'
+
+setup(
+    name='siridb-connector',
+    packages=[
+        'siridb',
+        'siridb.connector',
+        'siridb.connector.lib'],
+    version=VERSION,
+    description='SiriDB Connector',
+    author='Jeroen van der Heijden',
+    author_email='jeroen@transceptor.technology',
+    url='https://github.com/transceptor-technology/siridb-connector',
+    download_url=
+        'https://github.com/transceptor-technology/'
+        'siridb-connector/tarball/{}'.format(VERSION),
+    keywords=['siridb', 'connector', 'database', 'client'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Database',
+        'Topic :: Software Development'
+    ],
+    install_requires=['qpack']
+)
